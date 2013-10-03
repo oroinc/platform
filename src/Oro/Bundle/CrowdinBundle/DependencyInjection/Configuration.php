cc58c0bda11520c537a62896c65c19c0049c3121
Ivan Shakuta <ishakuta@gmail.com>
2013-10-03 13:38:10 +0000

    BAP-1804: Oro Translations - oro crowdin bundle

---
+++ src/Oro/Bundle/CrowdinBundle/DependencyInjection/Configuration.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Oro\Bundle\CrowdinBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * This is the class that validates and merges configuration from your app/config files
+ *
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('oro_crowdin');
+
+        return $treeBuilder;
+    }
+}
---
+++ src/Oro/Bundle/CrowdinBundle/OroCrowdinBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Oro\Bundle\CrowdinBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class OroCrowdinBundle extends Bundle
+{
+}
