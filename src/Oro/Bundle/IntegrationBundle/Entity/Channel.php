a4679fae36dbba1237ebc4ee72a40b7319a4c3ce
Alexandr Smaga <asmaga@magecore.com>
2013-11-14 19:54:04 +0200

    CRM-590: Customer connector (base and for CRM)    - base configuration entities

--- src/Oro/Bundle/IntegrationBundle/Entity/Channel.php
+++ src/Oro/Bundle/IntegrationBundle/Entity/Channel.php
@@ -3,14 +3,19 @@
 namespace Oro\Bundle\IntegrationBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
 
 /**
- * @ORM\Entity()
  * @ORM\Table(name="oro_integration_channel")
+ * @ORM\Entity
+ * @ORM\InheritanceType("SINGLE_TABLE")
+ * @ORM\DiscriminatorColumn(name="type", type="string", length=30)
  */
-class Channel
+abstract class Channel
 {
     /**
+     * @var integer
+     *
      * @ORM\Id
      * @ORM\Column(type="smallint", name="id")
      * @ORM\GeneratedValue(strategy="AUTO")
@@ -18,12 +23,45 @@ class Channel
     protected $id;
 
     /**
+     * @var string
+     *
      * @ORM\Column(type="string", length=255)
      */
     protected $name;
 
     /**
-     * @param mixed $name
+     * @var ArrayCollection|Transport[]
+     *
+     * @ORM\OneToMany(targetEntity="Oro\Bundle\IntegrationBundle\Entity\Transport",
+     *     mappedBy="channel", cascade={"all"}, orphanRemoval=true
+     * )
+     */
+    protected $transports;
+
+    /**
+     * @var ArrayCollection|Connector[]
+     *
+     * @ORM\OneToMany(targetEntity="Oro\Bundle\IntegrationBundle\Entity\Connector",
+     *     mappedBy="channel", cascade={"all"}, orphanRemoval=true
+     * )
+     */
+    protected $connectors;
+
+    public function __construct()
+    {
+        $this->transports = new ArrayCollection();
+    }
+
+    /**
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $name
      *
      * @return $this
      */
@@ -35,10 +73,84 @@ class Channel
     }
 
     /**
-     * @return mixed
+     * @return string
      */
     public function getName()
     {
         return $this->name;
+    }
+
+    /**
+     * @param Transport $transport
+     *
+     * @return $this
+     */
+    public function addTransport(Transport $transport)
+    {
+        if (!$this->transports->contains($transport)) {
+            $this->transports->add($transport);
+            $transport->setChannel($this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param Transport $transport
+     *
+     * @return $this
+     */
+    public function removeTransport(Transport $transport)
+    {
+        if ($this->transports->contains($transport)) {
+            $this->transports->removeElement($transport);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return ArrayCollection|Transport[]
+     */
+    public function getTransports()
+    {
+        return $this->transports;
+    }
+
+    /**
+     * @param Connector $connector
+     *
+     * @return $this
+     */
+    public function addConnector(Connector $connector)
+    {
+        if (!$this->connectors->contains($connector)) {
+            $this->connectors->add($connector);
+            $connector->setChannel($this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param Connector $connector
+     *
+     * @return $this
+     */
+    public function removeConnector(Connector $connector)
+    {
+        if ($this->connectors->contains($connector)) {
+            $this->connectors->removeElement($connector);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return ArrayCollection|Connector[]
+     */
+    public function getConnectors()
+    {
+        return $this->connectors;
     }
 }
---
+++ src/Oro/Bundle/IntegrationBundle/Entity/Connector.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Oro\Bundle\IntegrationBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class Transport
+ *
+ * @package Oro\Bundle\IntegrationBundle\Entity
+ * @ORM\Table(name="oro_integration_connector")
+ * @ORM\Entity
+ * @ORM\InheritanceType("SINGLE_TABLE")
+ * @ORM\DiscriminatorColumn(name="type", type="string", length=30)
+ */
+abstract class Connector
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="smallint", name="id")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var Channel
+     *
+     * @ORM\ManyToOne(targetEntity="Oro\Bundle\IntegrationBundle\Entity\Channel", inversedBy="connectors")
+     */
+    protected $channel;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param Channel $channel
+     *
+     * @return $this
+     */
+    public function setChannel(Channel $channel)
+    {
+        $this->channel = $channel;
+
+        return $this;
+    }
+
+    /**
+     * @return Channel
+     */
+    public function getChannel()
+    {
+        return $this->channel;
+    }
+}
