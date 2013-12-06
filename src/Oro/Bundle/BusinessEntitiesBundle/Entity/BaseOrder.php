e54a83fbbba7fe37f90fa43ceb275f2fc66156a5
Alexandr Smaga <asmaga@magecore.com>
2013-12-06 18:06:56 +0200

    CRM-648: Create business entities    - order entiies created

--- src/Oro/Bundle/BusinessEntitiesBundle/Entity/BaseOrder.php
+++ src/Oro/Bundle/BusinessEntitiesBundle/Entity/BaseOrder.php
@@ -3,6 +3,8 @@
 namespace Oro\Bundle\BusinessEntitiesBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use Oro\Bundle\AddressBundle\Entity\AbstractAddress;
 
 /**
  * Class BaseOrder
@@ -22,6 +24,135 @@ class BaseOrder
     protected $id;
 
     /**
+     * @var integer
+     *
+     * @ORM\Column(name="original_id", type="integer")
+     */
+    protected $originalId;
+
+    /**
+     * @var ArrayCollection
+     *
+     * @ORM\ManyToOne(targetEntity="Oro\Bundle\BusinessEntitiesBundle\Entity\BasePerson", cascade={"persist"})
+     * @ORM\JoinColumn(name="owner_id", referencedColumnName="id", onDelete="CASCADE")
+     */
+    protected $owner;
+
+    /**
+     * @var ArrayCollection
+     *
+     * @ORM\OneToMany(targetEntity="Oro\Bundle\AddressBundle\Entity\AbstractAddress",
+     *     mappedBy="owner", cascade={"all"}, orphanRemoval=true
+     * )
+     * @ORM\OrderBy({"primary" = "DESC"})
+     */
+    protected $addresses;
+
+    /**
+     * @var string
+     * @ORM\Column(name="currency", type="string", length=10, nullable=true)
+     */
+    protected $currency;
+
+    /**
+     * @var string
+     * @ORM\Column(name="payment_method", type="string", length=255, nullable=true)
+     */
+    protected $paymentMethod;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="payment_details", type="string", length=255, nullable=true)
+     */
+    protected $paymentDetails;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="subtotal_amount", type="float", nullable=true)
+     */
+    protected $subtotalAmount;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="shipping_amount", type="float", nullable=true)
+     */
+    protected $shippingAmount;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="shipping_method", type="float", nullable=true)
+     */
+    protected $shippingMethod;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="tax_amount", type="float", nullable=true)
+     */
+    protected $taxAmount;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="discount_amount", type="float", nullable=true)
+     */
+    protected $discountAmount;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="discount_percent", type="float", nullable=true)
+     */
+    protected $discountPercent;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="total_amount", type="float", nullable=true)
+     */
+    protected $totalAmount;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="status", type="string", length=255, nullable=false)
+     */
+    protected $status;
+
+    /**
+     * @var \DateTime $createdAt
+     *
+     * @ORM\Column(name="created_at", type="datetime")
+     */
+    protected $createdAt;
+
+    /**
+     * @var \DateTime $updatedAt
+     *
+     * @ORM\Column(name="updated_at", type="datetime")
+     */
+    protected $updatedAt;
+
+    /**
+     * @var BaseOrderItem
+     *
+     * @ORM\OneToMany(targetEntity="BaseOrderItem", mappedBy="order",cascade={"all"})
+     */
+    protected $items;
+
+    /**
+     * init addresses with empty collection
+     */
+    public function __construct()
+    {
+        $this->addresses = new ArrayCollection();
+    }
+
+    /**
      * @param int $id
      *
      * @return $this
@@ -39,5 +170,383 @@ class BaseOrder
     public function getId()
     {
         return $this->id;
+    }
+
+    /**
+     * @param BasePerson $owner
+     */
+    public function setOwner(BasePerson $owner = null)
+    {
+        $this->owner = $owner;
+    }
+
+    /**
+     * @return BasePerson
+     */
+    public function getOwner()
+    {
+        return $this->owner;
+    }
+
+    /**
+     * Set addresses.
+     *
+     * This method could not be named setAddresses because of bug CRM-253.
+     *
+     * @param ArrayCollection|AbstractAddress[] $addresses
+     *
+     * @return $this
+     */
+    public function resetAddresses($addresses)
+    {
+        $this->addresses->clear();
+
+        foreach ($addresses as $address) {
+            $this->addAddress($address);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Add address
+     *
+     * @param AbstractAddress $address
+     *
+     * @return $this
+     */
+    public function addAddress(AbstractAddress $address)
+    {
+        if (!$this->addresses->contains($address)) {
+            $this->addresses->add($address);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Remove address
+     *
+     * @param AbstractAddress $address
+     *
+     * @return $this
+     */
+    public function removeAddress(AbstractAddress $address)
+    {
+        if ($this->addresses->contains($address)) {
+            $this->addresses->removeElement($address);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Get addresses
+     *
+     * @return ArrayCollection|AbstractAddress[]
+     */
+    public function getAddresses()
+    {
+        return $this->addresses;
+    }
+
+    /**
+     * @param AbstractAddress $address
+     *
+     * @return bool
+     */
+    public function hasAddress(AbstractAddress $address)
+    {
+        return $this->getAddresses()->contains($address);
+    }
+
+    /**
+     * @param int $originalId
+     *
+     * @return $this
+     */
+    public function setOriginalId($originalId)
+    {
+        $this->originalId = $originalId;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getOriginalId()
+    {
+        return $this->originalId;
+    }
+
+    /**
+     * @param \DateTime $createdAt
+     *
+     * @return $this
+     */
+    public function setCreatedAt(\DateTime $createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param \DateTime $updatedAt
+     *
+     * @return $this
+     */
+    public function setUpdatedAt(\DateTime $updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @param string $paymentDetails
+     *
+     * @return $this
+     */
+    public function setPaymentDetails($paymentDetails)
+    {
+        $this->paymentDetails = $paymentDetails;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPaymentDetails()
+    {
+        return $this->paymentDetails;
+    }
+
+    /**
+     * @param string $paymentMethod
+     *
+     * @return $this
+     */
+    public function setPaymentMethod($paymentMethod)
+    {
+        $this->paymentMethod = $paymentMethod;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPaymentMethod()
+    {
+        return $this->paymentMethod;
+    }
+
+    /**
+     * @param float $discountAmount
+     *
+     * @return $this
+     */
+    public function setDiscountAmount($discountAmount)
+    {
+        $this->discountAmount = $discountAmount;
+
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getDiscountAmount()
+    {
+        return $this->discountAmount;
+    }
+
+    /**
+     * @param float $discountPercent
+     *
+     * @return $this
+     */
+    public function setDiscountPercent($discountPercent)
+    {
+        $this->discountPercent = $discountPercent;
+
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getDiscountPercent()
+    {
+        return $this->discountPercent;
+    }
+
+    /**
+     * @param float $shippingAmount
+     *
+     * @return $this
+     */
+    public function setShippingAmount($shippingAmount)
+    {
+        $this->shippingAmount = $shippingAmount;
+
+        return $this;
+    }
+
+    /**
+     * @param string $currency
+     *
+     * @return $this
+     */
+    public function setCurrency($currency)
+    {
+        $this->currency = $currency;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCurrency()
+    {
+        return $this->currency;
+    }
+
+    /**
+     * @param float $shippingMethod
+     *
+     * @return $this
+     */
+    public function setShippingMethod($shippingMethod)
+    {
+        $this->shippingMethod = $shippingMethod;
+
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getShippingMethod()
+    {
+        return $this->shippingMethod;
+    }
+
+    /**
+     * @return float
+     */
+    public function getShippingAmount()
+    {
+        return $this->shippingAmount;
+    }
+
+    /**
+     * @param string $status
+     *
+     * @return $this
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param float $subtotalAmount
+     *
+     * @return $this
+     */
+    public function setSubtotalAmount($subtotalAmount)
+    {
+        $this->subtotalAmount = $subtotalAmount;
+
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getSubtotalAmount()
+    {
+        return $this->subtotalAmount;
+    }
+
+    /**
+     * @param float $taxAmount
+     *
+     * @return $this
+     */
+    public function setTaxAmount($taxAmount)
+    {
+        $this->taxAmount = $taxAmount;
+
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getTaxAmount()
+    {
+        return $this->taxAmount;
+    }
+
+    /**
+     * @param float $totalAmount
+     *
+     * @return $this
+     */
+    public function setTotalAmount($totalAmount)
+    {
+        $this->totalAmount = $totalAmount;
+
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getTotalAmount()
+    {
+        return $this->totalAmount;
+    }
+
+    /**
+     * Clone relations
+     */
+    public function __clone()
+    {
+        if ($this->addresses) {
+            $this->addresses = clone $this->addresses;
+        }
     }
 }
