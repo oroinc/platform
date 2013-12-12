615728255d45b9de1d9c219e8a9195164ef7daee
Vova Soroka <vsoroka@magecore.com>
2013-12-12 02:39:35 +0200

    BAP-2518: Dashboard with widgets BAP-2524: Recent Emails widget

---
+++ src/Oro/Bundle/UserBundle/Controller/Dashboard/DashboardController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Oro\Bundle\UserBundle\Controller\Dashboard;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Oro\Bundle\SecurityBundle\Annotation\AclAncestor;
+
+class DashboardController extends Controller
+{
+    /**
+     * @Route(
+     *      "/recent_emails/{activeTab}/{contentType}/{_format}",
+     *      name="oro_user_dashboard_recentemails",
+     *      requirements={"activeTab"="inbox|sent", "contentType"="full|tab", "_format"="html|json"},
+     *      defaults={"activeTab" = "inbox", "contentType" = "full", "_format" = "html"}
+     * )
+     */
+    public function recentEmailsAction($activeTab, $contentType)
+    {
+        $currentUserId    = $this->get('security.context')->getToken()->getUser()->getId();
+        $renderMethod     = ($contentType === 'tab') ? 'render' : 'renderView';
+        $activeTabContent = $this->$renderMethod(
+            'OroUserBundle:Dashboard:recentEmailsGrid.html.twig',
+            [
+                'currentUserId' => $currentUserId,
+                'gridName'      => sprintf('dashboard-recent-emails-%s-grid', $activeTab)
+            ]
+        );
+
+        if ($contentType === 'tab') {
+            return $activeTabContent;
+        } else {
+            return $this->render(
+                'OroUserBundle:Dashboard:recentEmails.html.twig',
+                [
+                    'currentUserId'    => $currentUserId,
+                    'activeTab'        => $activeTab,
+                    'activeTabContent' => $activeTabContent
+                ]
+            );
+        }
+    }
+}
