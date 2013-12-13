b9c0115c9d4a101cdd8ad855b71ca6af692f028c
Alexander Nezdoiminoga <alex@magecore.com>
2013-12-13 21:30:40 +0200

    config entities/fields translations

--- src/Oro/Bundle/EntityConfigBundle/Tools/ConfigHelper.php
+++ src/Oro/Bundle/EntityConfigBundle/Tools/ConfigHelper.php
@@ -7,8 +7,17 @@ use Doctrine\Common\Util\Inflector;
 class ConfigHelper
 {
     /**
-     * Returns translation key (placeholder) by entity class name and field name
-     * example: orocrm.contact.first_name
+     * Returns translation key (placeholder) by entity class name, field name and property code
+     * example:
+     *      [vendor].[bundle].[entity].[field].[config property]
+     *      oro.user.group.name.label
+     *
+     *      if [entity] == [bundle] -> skip it
+     *      oro.user.first_name.label
+     *
+     *      if NO fieldName -> add prefix 'entity_'
+     *      oro.user.entity_label
+     *      oro.user.group.entity_label
      *
      * @param string $className
      * @param string $fieldName
@@ -25,16 +34,6 @@ class ConfigHelper
 
             //example: className - OroCRM\Contact\ContactAddress
             $classArray = explode('\\', strtolower($class));
-
-            /**
-             * if entity name starts with bundle name -> remove bundle name from entity name
-             * example:
-             *  was  classArray - [OroCRM, Contact, ContactAddress]
-             *  will classArray - [OroCRM, Contact, Address]
-             */
-            if (strpos($classArray[2], $classArray[1]) === 0) {
-                $classArray[2] = str_replace($classArray[1], '', $classArray[2]);
-            }
             $classArray = array_filter($classArray);
 
             $keyArray = [];
@@ -44,12 +43,13 @@ class ConfigHelper
                 }
             }
 
-            if (!is_null($fieldName)) {
+            if ($fieldName) {
                 $keyArray[] = Inflector::tableize($fieldName);
             }
 
-            if (!is_null($propertyName)) {
-                $keyArray[] = Inflector::tableize($propertyName);
+            if ($propertyName) {
+                $propertyName = Inflector::tableize($propertyName);
+                $keyArray[] = $fieldName ? $propertyName : 'entity_' . $propertyName;
             }
 
             $transKey = implode('.', $keyArray);
