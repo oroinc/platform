bdd1c5e85e85590fa24c15239c73ff62a373eb4f
Hryhorii Hrebiniuk <grebinuk@gmail.com>
2013-12-19 16:17:47 +0200

    BAP-2531: Redesign page header
     - fixed button group in page header

--- src/Oro/Bundle/UIBundle/Resources/public/js/mobile/page-header.js
+++ src/Oro/Bundle/UIBundle/Resources/public/js/mobile/page-header.js
@@ -1,25 +1,39 @@
 /*jshint browser: true*/
 /*jslint browser: true*/
 /*global define*/
-define(['jquery', 'oro/mediator'], function ($, mediator) {
+define(['jquery', 'oro/mediator', 'oro/translator'], function ($, mediator, __) {
     'use strict';
 
-    function joinButtonsGroup($header) {
-        var group,
-            groups = $header.find('.separator-btn').parent().nextAll('.btn-group');
-        if (!groups.length) {
-            groups = $header.find('.btn-group');
+    function updateButtonsGroup($header) {
+        var $group, $buttons, $moreButtons, $mainButtons,
+            $container = $header.find('.title-buttons-container');
+        $container.find('.separator-btn').replaceWith('<li class="divider"></li>');
+        $buttons = $container.find('.btn, .divider');
+        if ($buttons.length <= 2) {
+            return;
         }
-        group = $(groups.get(0));
-        groups.not(group).each(function () {
-            var $el = $(this);
-            group.append($el.children());
-        }).remove();
+        $mainButtons = $buttons.filter('.main-group:not(.more-group)');
+        if (!$mainButtons.length) {
+            $mainButtons = $buttons.first();
+        }
+        $moreButtons = $buttons.not($mainButtons);
+        $group = $('<div class="btn-group pull-right"></div>')
+            .append(
+                $mainButtons,
+                $('<a class="btn dropdown-toggle" data-toggle="dropdown" href="#">' +
+                    __('More') + '<span class="caret"></span></a>'),
+                $('<ul class="dropdown-menu"></ul>').append($moreButtons)
+            );
+        $buttons.removeClass('pull-right');
+        $moreButtons.not('li').wrap('<li></li>').removeClass(function (index, css) {
+            return (css.match(/\bbtn(-\S+)?/g) || []).join(' ');
+        });
+        $container.find('.btn-group').remove().end().prepend($group);
     }
 
     function updatePageHeader() {
         var $el = $('.navigation.navbar-extra');
-        joinButtonsGroup($el);
+        updateButtonsGroup($el);
     }
 
     /**
