9e6acce43f6cb85edb75f615ff983436f023851a
eu-ge-ne <eugene.pobochny@gmail.com>
2013-12-19 14:37:48 +0200

    BAP-2043: widget settings refactoring

--- src/Oro/Bundle/SidebarBundle/Resources/public/js/widget-container/widget-add-view.js
+++ src/Oro/Bundle/SidebarBundle/Resources/public/js/widget-container/widget-add-view.js
@@ -52,30 +52,29 @@ define(function (require) {
                     return;
                 }
 
-                var moduleId = $(selected).closest('li').data('moduleid');
+                var widgetData = $(selected).closest('li').data('widget');
 
-                requirejs([moduleId], function (Widget) {
-                    var placement = null;
-                    if (model.position === constants.SIDEBAR_LEFT) {
-                        placement = 'left';
-                    } else if (model.position === constants.SIDEBAR_RIGHT) {
-                        placement = 'right';
-                    }
-                    var widget = new WidgetContainerModel({
-                        title: Widget.defaults.title,
-                        icon: Widget.defaults.icon,
-                        module: moduleId,
-                        settings: Widget.defaults.settings(),
-                        position: model.widgets.length,
-                        placement: placement
-                    });
+                var placement = null;
+                if (model.position === constants.SIDEBAR_LEFT) {
+                    placement = 'left';
+                } else if (model.position === constants.SIDEBAR_RIGHT) {
+                    placement = 'right';
+                }
 
-                    model.widgets.push(widget);
-
-                    widget.save();
-
-                    view.close();
+                var widget = new WidgetContainerModel({
+                    title: widgetData.title,
+                    icon: widgetData.icon,
+                    module: widgetData.module,
+                    settings: widgetData.settings,
+                    position: model.widgets.length,
+                    placement: placement
                 });
+
+                model.widgets.push(widget);
+
+                widget.save();
+
+                view.close();
             });
         }
     });
