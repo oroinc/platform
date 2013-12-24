23dcaa4eed013872280388b2592efaa4fb557543
Vadin Tyukov <vtykov@magecore.com>
2013-12-24 20:18:36 +0200

    ticket/BAP-2629: Init base security entities

---
+++ src/Oro/Bundle/DistributionBundle/Entity/Role.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Oro\Bundle\DistributionBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\Role\RoleInterface;
+
+/**
+ * Role Entity
+ *
+ * @ORM\Entity()
+ * @ORM\Table(name="oro_access_role")
+ */
+class Role implements RoleInterface
+{
+    /**
+     * @var int
+     *
+     * @ORM\Id
+     * @ORM\Column(type="smallint", name="id")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", unique=true, length=30, nullable=false)
+     */
+    protected $role;
+
+    /**
+     * Populate the role field
+     *
+     * @param string $role ROLE_FOO etc
+     */
+    public function __construct($role = '')
+    {
+        $this->role  = $role;
+    }
+
+    /**
+     * Return the role name field
+     *
+     * @return string
+     */
+    public function getRole()
+    {
+        return $this->role;
+    }
+}
---
+++ src/Oro/Bundle/DistributionBundle/Entity/User.php
@@ -0,0 +1,193 @@
+<?php
+
+namespace Oro\Bundle\DistributionBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\AdvancedUserInterface;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="oro_user")
+ */
+class User implements AdvancedUserInterface, \Serializable
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=255, unique=true)
+     */
+    protected $username;
+
+    /**
+     * Encrypted password
+     *
+     * @var string
+     *
+     * @ORM\Column(type="string")
+     */
+    protected $password;
+
+    /**
+     * The salt to use for hashing
+     *
+     * @var string
+     *
+     * @ORM\Column(type="string")
+     */
+    protected $salt;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(type="boolean")
+     */
+    protected $enabled = true;
+
+    /**
+     * @var Role[]
+     *
+     * @ORM\ManyToMany(targetEntity="Role")
+     * @ORM\JoinTable(name="oro_user_access_role",
+     *      joinColumns={@ORM\JoinColumn(name="user_id", referencedColumnName="id", onDelete="CASCADE")},
+     *      inverseJoinColumns={@ORM\JoinColumn(name="role_id", referencedColumnName="id", onDelete="CASCADE")}
+     * )
+     */
+    protected $roles;
+
+    /**
+     * @var Group[]
+     *
+     * ORM\ManyToMany(targetEntity="Group")
+     * ORM\JoinTable(name="oro_user_access_group",
+     *      joinColumns={@ORM\JoinColumn(name="user_id", referencedColumnName="id", onDelete="CASCADE")},
+     *      inverseJoinColumns={@ORM\JoinColumn(name="group_id", referencedColumnName="id", onDelete="CASCADE")}
+     * )
+     */
+//    protected $groups;
+
+    public function __construct()
+    {
+        $this->roles           = new ArrayCollection();
+        $this->groups          = new ArrayCollection();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isAccountNonExpired()
+    {
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isAccountNonLocked()
+    {
+        return $this->isEnabled();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isCredentialsNonExpired()
+    {
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isEnabled()
+    {
+        return $this->enabled;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getRoles()
+    {
+        $roles = $this->roles->toArray();
+
+        /** @var Group $group */
+        foreach ($this->groups as $group) {
+            $roles = array_merge($roles, $group->getRoles()->toArray());
+        }
+
+        return array_unique($roles);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSalt()
+    {
+        return $this->salt;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getUsername()
+    {
+        $this->username;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function eraseCredentials()
+    {
+    }
+
+    /**
+     * Serializes the user.
+     * The serialized data have to contain the fields used by the equals method and the username.
+     *
+     * @return string
+     */
+    public function serialize()
+    {
+        return serialize(
+            array(
+                $this->password,
+                $this->salt,
+                $this->username,
+                $this->enabled,
+                $this->id,
+            )
+        );
+    }
+
+    /**
+     * Unserializes the user
+     *
+     * @param string $serialized
+     */
+    public function unserialize($serialized)
+    {
+        list(
+            $this->password,
+            $this->salt,
+            $this->username,
+            $this->enabled,
+            $this->id
+            ) = unserialize($serialized);
+    }
+}
