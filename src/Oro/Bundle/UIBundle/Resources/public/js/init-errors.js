2ff6bc784d7a422c882963b7e0fdf6d589b8be35
eu-ge-ne <eugene.pobochny@gmail.com>
2013-12-24 17:47:44 +0200

    BAP-2660: Widget bars available on 500, 404 page

--- src/Oro/Bundle/UIBundle/Resources/public/js/init-errors.js
+++ src/Oro/Bundle/UIBundle/Resources/public/js/init-errors.js
@@ -1,11 +1,12 @@
 /* jshint browser:true */
 /* global require */
-require(['jquery'],
-function($) {
+require(['jquery', 'oro/mediator'],
+function($, mediator) {
     'use strict';
 
     $(function() {
         setTimeout(function() {
+            mediator.trigger('layout:adjustHeight');
             // emulates 'document ready state' for selenium tests
             document['page-rendered'] = true;
         }, 100);
