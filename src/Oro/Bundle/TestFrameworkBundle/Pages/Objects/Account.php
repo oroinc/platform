47df387c9c6be589b55c2b39260b35c48aeb6f28
Ignat Shcheglovskyi <ignat.scheglovskiy@gmail.com>
2013-12-24 14:05:07 +0200

    CRM-687: API fixes - fix Selenium tests

--- src/Oro/Bundle/TestFrameworkBundle/Pages/Objects/Account.php
+++ src/Oro/Bundle/TestFrameworkBundle/Pages/Objects/Account.php
@@ -73,9 +73,9 @@ class Account extends AbstractEntity implements Entity
             );
             $this->tags->clear();
         } else {
-            if ($this->isElementPresent("//div[@id='tags-holder']")) {
+            if ($this->isElementPresent("//div[contains(@class, 'tags-holder')]")) {
                 $this->assertElementPresent(
-                    "//div[@id='tags-holder']//li[contains(., '{$tag}')]",
+                    "//div[contains(@class, 'tags-holder')]//li[contains(., '{$tag}')]",
                     'Tag is not assigned to entity'
                 );
             } else {
