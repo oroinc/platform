7ea873fa219428b4087bf15382a1684991b50aee
Yevhen Shyshkin <shishkin.yevgeniy@gmail.com>
2013-12-25 18:39:27 +0200

    CRM-633: Call duration in datetime format in Google Chrome  - added form type oro_time_interval  - added DB converter for time values  - updated unit tests

---
+++ src/Oro/Bundle/LocaleBundle/Tests/Unit/DoctrineExtensions/DBAL/Types/UTCDateTimeTypeTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Oro\Bundle\LocaleBundle\Tests\Unit\DoctrineExtensions\DBAL\Types;
+
+use Oro\Bundle\LocaleBundle\DoctrineExtensions\DBAL\Types\UTCTimeType;
+
+class UTCDateTimeTypeTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var UTCTimeType
+     */
+    protected $type;
+
+    /**
+     * @var
+     */
+    protected $platform;
+
+    protected function setUp()
+    {
+        // class has private constructor
+        $this->type = $this->getMockBuilder('Oro\Bundle\LocaleBundle\DoctrineExtensions\DBAL\Types\UTCDateTimeType')
+            ->setMethods(null)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->platform = $this->getMockBuilder('Doctrine\DBAL\Platforms\AbstractPlatform')
+            ->disableOriginalConstructor()
+            ->setMethods(array('getDateTimeFormatString'))
+            ->getMockForAbstractClass();
+        $this->platform->expects($this->any())
+            ->method('getDateTimeFormatString')
+            ->will($this->returnValue('Y-m-d H:i:s'));
+    }
+
+    protected function tearDown()
+    {
+        unset($this->type);
+        unset($this->platform);
+    }
+
+    /**
+     * @param \DateTime $source
+     * @param string $expected
+     * @dataProvider convertToDatabaseValueDataProvider
+     */
+    public function testConvertToDatabaseValue($source, $expected)
+    {
+        $this->assertEquals($expected, $this->type->convertToDatabaseValue($source, $this->platform));
+    }
+
+    /**
+     * @return array
+     */
+    public function convertToDatabaseValueDataProvider()
+    {
+        return array(
+            'null' => array(
+                'source'   => null,
+                'expected' => null,
+            ),
+            'UTC' => array(
+                'source' => new \DateTime('2013-01-01 00:00:00', new \DateTimeZone('UTC')),
+                'expected' => '2013-01-01 00:00:00',
+            ),
+            'positive shift' => array(
+                'source' => new \DateTime('2013-01-01 00:00:00', new \DateTimeZone('Europe/Athens')),
+                'expected' => '2012-12-31 22:00:00',
+            ),
+            'negatuive shift' => array(
+                'source' => new \DateTime('2013-01-01 00:00:00', new \DateTimeZone('America/Los_Angeles')),
+                'expected' => '2013-01-01 08:00:00',
+            ),
+        );
+    }
+
+    public function testConvertToPHPValue()
+    {
+        $source = '2013-01-01 08:00:00';
+        $expected = new \DateTime('2013-01-01 08:00:00', new \DateTimeZone('UTC'));
+
+        $this->assertEquals($expected, $this->type->convertToPHPValue($source, $this->platform));
+    }
+}
---
+++ src/Oro/Bundle/LocaleBundle/Tests/Unit/DoctrineExtensions/DBAL/Types/UTCDateTypeTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Oro\Bundle\LocaleBundle\Tests\Unit\DoctrineExtensions\DBAL\Types;
+
+use Oro\Bundle\LocaleBundle\DoctrineExtensions\DBAL\Types\UTCTimeType;
+
+class UTCTimeTypeTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var UTCTimeType
+     */
+    protected $type;
+
+    /**
+     * @var
+     */
+    protected $platform;
+
+    protected function setUp()
+    {
+        // class has private constructor
+        $this->type = $this->getMockBuilder('Oro\Bundle\LocaleBundle\DoctrineExtensions\DBAL\Types\UTCDateType')
+            ->setMethods(null)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->platform = $this->getMockBuilder('Doctrine\DBAL\Platforms\AbstractPlatform')
+            ->disableOriginalConstructor()
+            ->setMethods(array('getDateFormatString'))
+            ->getMockForAbstractClass();
+        $this->platform->expects($this->any())
+            ->method('getDateFormatString')
+            ->will($this->returnValue('Y-m-d'));
+    }
+
+    protected function tearDown()
+    {
+        unset($this->type);
+        unset($this->platform);
+    }
+
+    /**
+     * @param \DateTime $source
+     * @param string $expected
+     * @dataProvider convertToDatabaseValueDataProvider
+     */
+    public function testConvertToDatabaseValue($source, $expected)
+    {
+        $this->assertEquals($expected, $this->type->convertToDatabaseValue($source, $this->platform));
+    }
+
+    /**
+     * @return array
+     */
+    public function convertToDatabaseValueDataProvider()
+    {
+        return array(
+            'null' => array(
+                'source'   => null,
+                'expected' => null,
+            ),
+            'UTC' => array(
+                'source' => new \DateTime('2013-01-01', new \DateTimeZone('UTC')),
+                'expected' => '2013-01-01',
+            ),
+            'positive shift' => array(
+                'source' => new \DateTime('2013-01-01', new \DateTimeZone('Europe/Athens')),
+                'expected' => '2012-12-31',
+            ),
+            'negatuive shift' => array(
+                'source' => new \DateTime('2013-01-01', new \DateTimeZone('America/Los_Angeles')),
+                'expected' => '2013-01-01',
+            ),
+        );
+    }
+
+    public function testConvertToPHPValue()
+    {
+        $source = '2013-01-01';
+        $expected = \DateTime::createFromFormat('!Y-m-d', '2013-01-01', new \DateTimeZone('UTC'));
+
+        $this->assertEquals($expected, $this->type->convertToPHPValue($source, $this->platform));
+    }
+}
