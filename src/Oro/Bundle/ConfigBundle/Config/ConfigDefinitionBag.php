69b4091317e3b4d5579dd92c16c2e82f2853e821
Alexandr Smaga <asmaga@magecore.com>
2013-12-26 20:43:13 +0200

    BAP-2304: OroConfig work only for tree named by prefix "oro_"     - added config definition bag

---
+++ src/Oro/Bundle/ConfigBundle/Config/ConfigDefinitionBag.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Oro\Bundle\ConfigBundle\Config;
+
+use Symfony\Component\HttpFoundation\ParameterBag;
+
+class ConfigDefinitionBag extends ParameterBag
+{
+}
--- src/Oro/Bundle/ConfigBundle/DependencyInjection/Compiler/ConfigPass.php
+++ src/Oro/Bundle/ConfigBundle/DependencyInjection/Compiler/ConfigPass.php
@@ -2,40 +2,37 @@
 
 namespace Oro\Bundle\ConfigBundle\DependencyInjection\Compiler;
 
+use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
-use Symfony\Component\Config\Definition\Processor;
 
 class ConfigPass implements CompilerPassInterface
 {
+    const CONFIG_DEFINITION_BAG_SERVICE = 'oro_config.config_definition_bag';
+
+    /**
+     * {@inheritdoc}
+     */
     public function process(ContainerBuilder $container)
     {
         $processor = new Processor();
-        $settings  = array();
+        $settings  = [];
 
         foreach ($container->getExtensions() as $name => $extension) {
-            if (strpos($name, 'oro_') !== false) {
-                if (!$config = $extension->getConfiguration(array(), $container)) {
-                    continue;
-                }
+            if (!$config = $extension->getConfiguration([], $container)) {
+                continue;
+            }
 
-                $config = $processor->processConfiguration(
-                    $config,
-                    $container->getExtensionConfig($name)
-                );
+            $config = $processor->processConfiguration(
+                $config,
+                $container->getExtensionConfig($name)
+            );
 
-                if (isset($config['settings'])) {
-                    $settings[$name] = $config['settings'];
-                }
+            if (isset($config['settings'])) {
+                $settings[$name] = $config['settings'];
             }
         }
 
-        $taggedServices = $container->findTaggedServiceIds('oro_config.manager');
-
-        foreach ($taggedServices as $id => $attributes) {
-            $container
-                ->getDefinition($id)
-                ->addArgument($settings);
-        }
+        $container->getDefinition(self::CONFIG_DEFINITION_BAG_SERVICE)->replaceArgument(0, $settings);
     }
 }
