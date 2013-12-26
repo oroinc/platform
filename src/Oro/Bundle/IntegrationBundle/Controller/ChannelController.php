27702312af68e5c251d2e0d123c1268ea282d110
Alexandr Smaga <asmaga@magecore.com>
2013-12-26 16:32:51 +0200

    BAP-2623: Sync channel from UI button    - refcatored run:sync command to allow sync for single channel    - added button on ui

--- src/Oro/Bundle/IntegrationBundle/Controller/ChannelController.php
+++ src/Oro/Bundle/IntegrationBundle/Controller/ChannelController.php
@@ -4,12 +4,17 @@ namespace Oro\Bundle\IntegrationBundle\Controller;
 
 use Symfony\Component\Form\FormInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
+use FOS\Rest\Util\Codes;
+use JMS\JobQueueBundle\Entity\Job;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 use Oro\Bundle\SecurityBundle\Annotation\Acl;
+use Oro\Bundle\SecurityBundle\Annotation\AclAncestor;
 use Oro\Bundle\IntegrationBundle\Entity\Channel;
+use Oro\Bundle\IntegrationBundle\Command\SyncCommand;
 use Oro\Bundle\IntegrationBundle\Form\Handler\ChannelHandler;
 
 /**
@@ -18,7 +23,7 @@ use Oro\Bundle\IntegrationBundle\Form\Handler\ChannelHandler;
 class ChannelController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", name="oro_integration_channel_index")
      * @Acl(
      *      id="oro_integration_channel_index",
      *      type="entity",
@@ -33,7 +38,7 @@ class ChannelController extends Controller
     }
 
     /**
-     * @Route("/create")
+     * @Route("/create", name="oro_integration_channel_create")
      * @Acl(
      *      id="oro_integration_channel_create",
      *      type="entity",
@@ -48,7 +53,7 @@ class ChannelController extends Controller
     }
 
     /**
-     * @Route("/update/{id}", requirements={"id"="\d+"}))
+     * @Route("/update/{id}", requirements={"id"="\d+"}), name="oro_integration_channel_update")
      * @Acl(
      *      id="oro_integration_channel_update",
      *      type="entity",
@@ -60,6 +65,28 @@ class ChannelController extends Controller
     public function updateAction(Channel $channel)
     {
         return $this->update($channel);
+    }
+
+    /**
+     * @Route("/schedule/{id}", requirements={"id"="\d+"}), name="oro_integration_channel_schedule")
+     * @AclAncestor("oro_integration_channel_update")
+     */
+    public function scheduleAction(Channel $channel)
+    {
+        $job = new Job(SyncCommand::COMMAND_NAME, ['--channel-id=' . $channel->getId(), '-v']);
+
+        $error = false;
+        try {
+            $em = $this->get('doctrine.orm.entity_manager');
+            $em->persist($job);
+            $em->flush($job);
+        } catch (\Exception $e) {
+            $error = $e->getMessage();
+        }
+
+        $status = $error === false ? Codes::HTTP_OK : Codes::HTTP_BAD_REQUEST;
+
+        return new JsonResponse(['job_id' => $job->getId()], $status);
     }
 
     /**
@@ -97,6 +124,7 @@ class ChannelController extends Controller
         $isUpdateOnly = $this->get('request')->get(ChannelHandler::UPDATE_MARKER, false);
 
         $form = $this->get('oro_integration.form.channel');
+        // take different form due to JS validation should be shown even in case when it was not validated on backend
         if ($isUpdateOnly) {
             $form = $this->get('form.factory')
                 ->createNamed('oro_integration_channel_form', 'oro_integration_channel_form', $form->getData());
