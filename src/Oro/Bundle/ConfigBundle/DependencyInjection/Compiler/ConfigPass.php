04704881c4a0e61c0343e054374c112f1191dabf
Alexandr Smaga <asmaga@magecore.com>
2013-12-27 09:43:16 +0000

    BAP-2304: OroConfig work only for tree named by prefix "oro_"     - added config resolver

--- src/Oro/Bundle/ConfigBundle/DependencyInjection/Compiler/ConfigPass.php
+++ src/Oro/Bundle/ConfigBundle/DependencyInjection/Compiler/ConfigPass.php
@@ -2,9 +2,11 @@
 
 namespace Oro\Bundle\ConfigBundle\DependencyInjection\Compiler;
 
+use Symfony\Component\Config\Definition\ConfigurationInterface;
 use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 
 class ConfigPass implements CompilerPassInterface
 {
@@ -18,15 +20,16 @@ class ConfigPass implements CompilerPassInterface
         $processor = new Processor();
         $settings  = [];
 
+        /** @var Extension $extension */
         foreach ($container->getExtensions() as $name => $extension) {
-            if (!$config = $extension->getConfiguration([], $container)) {
+            $configurationTree = $extension->getConfiguration([], $container);
+            $config            = $container->getExtensionConfig($name);
+            if (!($config && $configurationTree instanceof ConfigurationInterface)) {
+                // this extension was not called
                 continue;
             }
-
-            $config = $processor->processConfiguration(
-                $config,
-                $container->getExtensionConfig($name)
-            );
+            $config = $container->getParameterBag()->resolveValue($config);
+            $config = $processor->processConfiguration($configurationTree, $config);
 
             if (isset($config['settings'])) {
                 $settings[$name] = $config['settings'];
