f50a37122d5de78013abbee421b852d57001b995
Roman Grebenchuk <rgrebenchuk@magecore.com>
2014-01-08 16:42:30 +0200

    BAP-498: Investigate failed builds  - fixed delete actions

--- src/Oro/Bundle/TestFrameworkBundle/Pages/PageGrid.php
+++ src/Oro/Bundle/TestFrameworkBundle/Pages/PageGrid.php
@@ -174,7 +174,7 @@ class PageGrid extends Page
     {
         $entity = $this->getEntity($entityData);
         $entity->element($this->using('xpath')->value("td[@class = 'action-cell']//a[contains(., '...')]"))->click();
-        $entity->element($this->using('xpath')->value("td[@class = 'action-cell']//a[contains(., 'Delete')]"))->click();
+        $entity->element($this->using('xpath')->value("td[@class = 'action-cell']//a[contains(., 'Remove')]"))->click();
         $this->byXPath("//div[div[contains(., 'Delete Confirmation')]]//a[text()='Yes, Delete']")->click();
 
         $this->waitPageToLoad();
