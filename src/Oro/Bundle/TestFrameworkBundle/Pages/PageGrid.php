d40da102dd1255b5b27ee098dfec163d3c77783b
Roman Grebenchuk <rgrebenchuk@magecore.com>
2014-01-08 18:41:24 +0200

    BAP-498: Investigate failed builds  - CR changes

--- src/Oro/Bundle/TestFrameworkBundle/Pages/PageGrid.php
+++ src/Oro/Bundle/TestFrameworkBundle/Pages/PageGrid.php
@@ -173,8 +173,12 @@ class PageGrid extends Page
     public function deleteEntity($entityData = array())
     {
         $entity = $this->getEntity($entityData, $name = 'Delete');
-        $entity->element($this->using('xpath')->value("td[@class = 'action-cell']//a[contains(., '...')]"))->click();
-        $entity->element($this->using('xpath')->value("td[@class = 'action-cell']//a[contains(., '{$name}')]"))->click();
+        $entity->element($this->using('xpath')
+            ->value("td[@class = 'action-cell']//a[contains(., '...')]"))
+            ->click();
+        $entity->element($this->using('xpath')
+            ->value("td[@class = 'action-cell']//a[contains(., '{$name}')]"))
+            ->click();
         $this->byXPath("//div[div[contains(., 'Delete Confirmation')]]//a[text()='Yes, Delete']")->click();
 
         $this->waitPageToLoad();
