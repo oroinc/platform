73923da0b4af0313aa91c82ff7d0a0f918fa09f3
Filips Alpe <filips@akeneo.com>
2014-01-29 18:34:04 +0100

    Fix typo

--- src/Oro/Bundle/DataGridBundle/Datasource/Orm/OrmDatasource.php
+++ src/Oro/Bundle/DataGridBundle/Datasource/Orm/OrmDatasource.php
@@ -51,7 +51,7 @@ class OrmDatasource implements DatasourceInterface
                     $this->qb = $qb;
                 } else {
                     throw new \Exception(
-                        sprintf('%s::%s() must return an instance of Docrine\Orm\QueryBuilder, %s given',
+                        sprintf('%s::%s() must return an instance of Doctrine\ORM\QueryBuilder, %s given',
                             get_class($repository),
                             $method,
                             is_object($qb) ? get_class($qb) : gettype($qb)
