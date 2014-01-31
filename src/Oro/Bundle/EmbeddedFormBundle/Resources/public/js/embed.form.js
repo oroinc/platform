6895239ba53773c572295c6882668f0964dcf2f3
Ruslan Polutsygan <rpolytsygan@magecore.com>
2014-01-31 17:20:20 +0200

    ticket/BAP-2755: embedded forms  - fixes after demo

--- src/Oro/Bundle/EmbeddedFormBundle/Resources/public/js/embed.form.js
+++ src/Oro/Bundle/EmbeddedFormBundle/Resources/public/js/embed.form.js
@@ -1,12 +1,13 @@
-var EmbedForm = function (options) {
+var ORO = {
+    EmbedForm: function (options) {
 
-    var formContainer = document.getElementById(options.container);
-    var iframe = document.createElement('iframe');
-    iframe.src = options.iframe.src;
-    iframe.width = options.iframe.width;
-    iframe.height = options.iframe.height;
-    iframe.frameBorder = options.iframe.frameBorder;
+        var formContainer = document.getElementById(options.container);
+        var iframe = document.createElement('iframe');
+        iframe.src = options.iframe.src;
+        iframe.width = options.iframe.width;
+        iframe.height = options.iframe.height;
+        iframe.frameBorder = options.iframe.frameBorder;
 
-    formContainer.appendChild(iframe);
-
+        formContainer.appendChild(iframe);
+    }
 };
