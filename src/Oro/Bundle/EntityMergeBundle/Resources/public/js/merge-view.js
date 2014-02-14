0dd277aa6f0c1f42174dce496a9e9efc18647f6c
Alexandr <avreliy.89@gmail.com>
2014-02-14 13:29:16 +0200

    BAP-3121: Create Merge view page  -fix, js,css,html

--- src/Oro/Bundle/EntityMergeBundle/Resources/public/js/merge-view.js
+++ src/Oro/Bundle/EntityMergeBundle/Resources/public/js/merge-view.js
@@ -1,48 +1,53 @@
-var mergeView = {
-    /**
-     * @desc This callback change entity field values class in one of the form column
-     * @desc All field values in the column set to active
-     * @callback
-     * @desc {HTMLElement} this
-     */
-    entitySelectAllHandler: function () {
-        var entityId = $(this).data('entity-key');
-        $('.entity-merge-field-choice[value="' + entityId + '"]').click();
-    },
-    /**
-     * @desc This callback change entity field values class in one of the form rows
-     * @desc All other then "target" value will be lighter
-     * @callback
-     * @desc {HTMLElement} this
-     */
-    entityValueSelectHandler: function () {
-        var $this = $(this);
-        var fieldName = $this.attr('name');
-        var entityKey = $this.val();
-        $('.merge-entity-representative[data-entity-field-name="' + fieldName + '"]').each(function (index, item) {
-            var $this = $(item);
-            if ($this.data('entity-key') != entityKey) {
-                $this.addClass('entity-merge-not-selected');
-            } else {
-                $this.removeClass('entity-merge-not-selected');
-            }
-        });
-    },
+define(['jquery'], function($){
+    !function(){
+        /**
+         * @desc This callback change entity field values class in one of the form column
+         * @desc All field values in the column set to active
+         * @callback
+         * @desc {HTMLElement} this
+         */
+        this.entitySelectAllHandler = function () {
+            var entityId = $(this).data('entity-key');
+            $('.entity-merge-field-choice[value="' + entityId + '"]').click();
+        };
 
-    /**
-     * @desc reset entity values class states
-     * @desc All selected classes will be darker then not selected
-     */
-    resetViewState: function () {
-        $('input[type="radio"]:checked').click();
-    },
+        /**
+         * @desc This callback change entity field values class in one of the form rows
+         * @desc All other then "target" value will be lighter
+         * @callback
+         * @desc {HTMLElement} this
+         */
+        this.entityValueSelectHandler = function () {
+            var $this = $(this);
+            var fieldName = $this.attr('name');
+            var entityKey = $this.val();
+            $('.merge-entity-representative[data-entity-field-name="' + fieldName + '"]').each(function (index, item) {
+                var $this = $(item);
+                if ($this.data('entity-key') != entityKey) {
+                    $this.addClass('entity-merge-not-selected');
+                } else {
+                    $this.removeClass('entity-merge-not-selected');
+                }
+            });
+        };
 
-    /**
-     * @constructs
-     */
-    init: function () {
-        $('.entity-merge-select-all').click(mergeView.entitySelectAllHandler);
-        $('.entity-merge-field-choice').click(mergeView.entityValueSelectHandler);
-        mergeView.resetViewState();
-    }
-};
+        /**
+         * @desc reset entity values class states
+         * @desc All selected classes will be darker then not selected
+         */
+        this.resetViewState = function () {
+            $('input[type="radio"]:checked').click();
+        };
+
+        /**
+         * @constructs
+         */
+        var init = function () {
+            $('.entity-merge-select-all').click(this.entitySelectAllHandler);
+            $('.entity-merge-field-choice').click(this.entityValueSelectHandler);
+            this.resetViewState();
+        }.bind(this);
+
+        init();
+    }();
+});
