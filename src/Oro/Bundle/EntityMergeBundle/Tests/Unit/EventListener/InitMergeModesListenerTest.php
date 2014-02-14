750a7f2811c5ce8e24f0745656d78eba4f23284d
Sergey Zhuravel <zhuravel.sergey@gmail.com>
2014-02-14 18:23:03 +0200

    BAP-3194: Improve unit tests coverage - fix InitMergeModesListener

---
+++ src/Oro/Bundle/EntityMergeBundle/Tests/Unit/EventListener/InitMergeModesListenerTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Oro\Bundle\EntityMergeBundle\Tests\Unit\EventListener;
+
+use Oro\Bundle\EntityMergeBundle\Event\EntityMetadataEvent;
+use Oro\Bundle\EntityMergeBundle\EventListener\InitMergeModesListener;
+
+class InitMergeModesListenerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var InitMergeModesListener
+     */
+    protected $listener;
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $entityMetadata;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $fieldMetadata;
+
+    protected function setUp()
+    {
+        $this->entityMetadata = $this
+            ->getMockBuilder('Oro\Bundle\EntityMergeBundle\Metadata\EntityMetadata')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->fieldMetadata = $this
+            ->getMockBuilder('Oro\Bundle\EntityMergeBundle\Metadata\FieldMetadata')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->entityMetadata
+            ->expects($this->any())
+            ->method('getFieldsMetadata')
+            ->will($this->returnValue([$this->fieldMetadata]));
+
+        $this->listener = new InitMergeModesListener();
+    }
+
+    public function testOnCreateMetadata()
+    {
+        $event = new EntityMetadataEvent($this->entityMetadata);
+
+        $this->fieldMetadata
+            ->expects($this->atLeastOnce())
+            ->method('addMergeMode')
+            ->will($this->returnValue([$this->fieldMetadata]));
+
+
+        $this->listener->onCreateMetadata($event);
+    }
+
+    public function testOnCreateMetadataForCollection()
+    {
+        $event = new EntityMetadataEvent($this->entityMetadata);
+
+        $this->fieldMetadata
+            ->expects($this->atLeastOnce())
+            ->method('addMergeMode')
+            ->will($this->returnValue([$this->fieldMetadata]));
+
+        $this->fieldMetadata
+            ->expects($this->atLeastOnce())
+            ->method('isCollection')
+            ->will($this->returnValue(true));
+
+        $this->listener->onCreateMetadata($event);
+    }
+}
