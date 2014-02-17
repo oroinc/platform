4e4522f67026190df0354c4406010fd690dd8f64
Hryhorii Hrebiniuk <grebinuk@gmail.com>
2014-02-17 16:50:42 +0200

    BAP-3064: Adopt concept of Entity Edit View for Display Columns
     - added handler for exclude option in fieldChoice widget

--- src/Oro/Bundle/EntityBundle/Resources/public/js/field-choice.js
+++ src/Oro/Bundle/EntityBundle/Resources/public/js/field-choice.js
@@ -5,6 +5,23 @@ define(['jquery', 'underscore', 'oro/entity-field-select-util', 'oro/entity-fiel
     ], function ($, _, EntityFieldUtil, EntityFieldView) {
     'use strict';
 
+    function filterFields(fields, exclude) {
+        fields = _.filter(fields, function (item) {
+            var result;
+            if (item.children) {
+                item.children = filterFields(item.children, exclude);
+                result = !_.isEmpty(item.children);
+            } else {
+                result = !_.some(exclude, function (rule) {
+                    var cut = _.pick(item, _.keys(rule));
+                    return _.isEqual(cut, rule);
+                });
+            }
+            return result;
+        });
+        return fields;
+    }
+
     $.widget('oroentity.fieldChoice', {
         options: {
             entity: null,
@@ -13,7 +30,8 @@ define(['jquery', 'underscore', 'oro/entity-field-select-util', 'oro/entity-fiel
             select2: {
                 collapsibleResults: true,
                 dropdownAutoWidth: true
-            }
+            },
+            exclude: []
         },
 
         _create: function () {
@@ -31,6 +49,9 @@ define(['jquery', 'underscore', 'oro/entity-field-select-util', 'oro/entity-fiel
         _init: function () {
             $.extend(this.entityFieldUtil, this.options.util);
             this.options.fields = this.entityFieldUtil._convertData(this.options.fields, this.options.entity, null);
+            if (!_.isEmpty(this.options.exclude)) {
+                this.options.fields = filterFields(this.options.fields, this.options.exclude);
+            }
             this.element
                 .data('entity', this.options.entity)
                 .data('data', this.options.fields);
