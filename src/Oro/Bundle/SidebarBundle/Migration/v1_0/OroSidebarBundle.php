52fbdbb4023de8ab1cb9d92228c7b13cd6222c8d
Alexander Nezdoiminoga <alex@magecore.com>
2014-02-21 20:03:56 +0200

    BAP-3159: OroSidebarBundle migrations

--- src/Oro/Bundle/SidebarBundle/Migration/v1_0/OroSidebarBundle.php
+++ src/Oro/Bundle/SidebarBundle/Migration/v1_0/OroSidebarBundle.php
@@ -9,15 +9,48 @@ class OroSidebarBundle implements Migration
 {
     /**
      * @inheritdoc
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
      */
     public function up(Schema $schema)
     {
-        return [
-            "CREATE TABLE oro_sidebar_state (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, position VARCHAR(13) NOT NULL, state VARCHAR(17) NOT NULL, UNIQUE INDEX sidebar_state_unique_idx (user_id, position), INDEX IDX_AB2BC195A76ED395 (user_id), PRIMARY KEY(id))",
-            "CREATE TABLE oro_sidebar_widget (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, placement VARCHAR(50) NOT NULL, position SMALLINT NOT NULL, widget_name VARCHAR(50) NOT NULL, settings LONGTEXT DEFAULT NULL COMMENT '(DC2Type:array)', state VARCHAR(22) NOT NULL, INDEX IDX_2FFBEA9CA76ED395 (user_id), INDEX sidebar_widgets_user_placement_idx (user_id, placement), INDEX sidebar_widgets_position_idx (position), PRIMARY KEY(id))",
+        // @codingStandardsIgnoreStart
 
-            "ALTER TABLE oro_sidebar_state ADD CONSTRAINT FK_AB2BC195A76ED395 FOREIGN KEY (user_id) REFERENCES oro_user (id) ON DELETE CASCADE",
-            "ALTER TABLE oro_sidebar_widget ADD CONSTRAINT FK_2FFBEA9CA76ED395 FOREIGN KEY (user_id) REFERENCES oro_user (id) ON DELETE CASCADE"
-        ];
+        /** Generate table oro_sidebar_state **/
+        $table = $schema->createTable('oro_sidebar_state');
+        $table->addColumn('id', 'integer', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => true, 'comment' => '']);
+        $table->addColumn('user_id', 'integer', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('position', 'string', ['default' => null, 'notnull' => true, 'length' => 13, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('state', 'string', ['default' => null, 'notnull' => true, 'length' => 17, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->setPrimaryKey(['id']);
+        $table->addUniqueIndex(['user_id', 'position'], 'sidebar_state_unique_idx');
+        $table->addIndex(['user_id'], 'IDX_AB2BC195A76ED395', []);
+        /** End of generate table oro_sidebar_state **/
+
+        /** Generate table oro_sidebar_widget **/
+        $table = $schema->createTable('oro_sidebar_widget');
+        $table->addColumn('id', 'integer', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => true, 'comment' => '']);
+        $table->addColumn('user_id', 'integer', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('placement', 'string', ['default' => null, 'notnull' => true, 'length' => 50, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('position', 'smallint', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('widget_name', 'string', ['default' => null, 'notnull' => true, 'length' => 50, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('settings', 'array', ['default' => null, 'notnull' => false, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('state', 'string', ['default' => null, 'notnull' => true, 'length' => 22, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->setPrimaryKey(['id']);
+        $table->addIndex(['user_id'], 'IDX_2FFBEA9CA76ED395', []);
+        $table->addIndex(['user_id', 'placement'], 'sidebar_widgets_user_placement_idx', []);
+        $table->addIndex(['position'], 'sidebar_widgets_position_idx', []);
+        /** End of generate table oro_sidebar_widget **/
+
+        /** Generate foreign keys for table oro_sidebar_state **/
+        $table = $schema->getTable('oro_sidebar_state');
+        $table->addForeignKeyConstraint($schema->getTable('oro_user'), ['user_id'], ['id'], ['onDelete' => 'CASCADE', 'onUpdate' => null]);
+        /** End of generate foreign keys for table oro_sidebar_state **/
+
+        /** Generate foreign keys for table oro_sidebar_widget **/
+        $table = $schema->getTable('oro_sidebar_widget');
+        $table->addForeignKeyConstraint($schema->getTable('oro_user'), ['user_id'], ['id'], ['onDelete' => 'CASCADE', 'onUpdate' => null]);
+        /** End of generate foreign keys for table oro_sidebar_widget **/
+
+        // @codingStandardsIgnoreEnd
     }
 }
