455f29fab4c374b4be7a0ebc7d8eacbea6e5d05f
Alexander Nezdoiminoga <alex@magecore.com>
2014-02-21 18:58:11 +0200

    BAP-3159: OroCalendarBundle migrations

--- src/Oro/Bundle/CalendarBundle/Migration/v1_0/OroCalendarBundle.php
+++ src/Oro/Bundle/CalendarBundle/Migration/v1_0/OroCalendarBundle.php
@@ -9,18 +9,67 @@ class OroCalendarBundle implements Migration
 {
     /**
      * @inheritdoc
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
      */
     public function up(Schema $schema)
     {
-        return [
-            "CREATE TABLE oro_calendar (id INT AUTO_INCREMENT NOT NULL, user_owner_id INT DEFAULT NULL, name VARCHAR(255) DEFAULT NULL, INDEX IDX_1D171519EB185F9 (user_owner_id), PRIMARY KEY(id))",
-            "CREATE TABLE oro_calendar_connection (id INT AUTO_INCREMENT NOT NULL, connected_calendar_id INT NOT NULL, calendar_id INT NOT NULL, created DATETIME NOT NULL, color VARCHAR(6) DEFAULT NULL, background_color VARCHAR(6) DEFAULT NULL, UNIQUE INDEX oro_calendar_connection_uq (calendar_id, connected_calendar_id), INDEX IDX_25D13AB8A40A2C8 (calendar_id), INDEX IDX_25D13AB8F94143E3 (connected_calendar_id), PRIMARY KEY(id));",
-            "CREATE TABLE oro_calendar_event (id INT AUTO_INCREMENT NOT NULL, calendar_id INT NOT NULL, title LONGTEXT NOT NULL, start_at DATETIME NOT NULL, end_at DATETIME NOT NULL, all_day TINYINT(1) NOT NULL, reminder TINYINT(1) NOT NULL, remind_at DATETIME DEFAULT NULL, reminded TINYINT(1) DEFAULT '0' NOT NULL, INDEX IDX_2DDC40DDA40A2C8 (calendar_id), INDEX oro_calendar_event_idx (calendar_id, start_at, end_at), PRIMARY KEY(id))",
+        // @codingStandardsIgnoreStart
 
-            "ALTER TABLE oro_calendar ADD CONSTRAINT FK_1D171519EB185F9 FOREIGN KEY (user_owner_id) REFERENCES oro_user (id) ON DELETE SET NULL",
-            "ALTER TABLE oro_calendar_connection ADD CONSTRAINT FK_25D13AB8F94143E3 FOREIGN KEY (connected_calendar_id) REFERENCES oro_calendar (id) ON DELETE CASCADE",
-            "ALTER TABLE oro_calendar_connection ADD CONSTRAINT FK_25D13AB8A40A2C8 FOREIGN KEY (calendar_id) REFERENCES oro_calendar (id) ON DELETE CASCADE",
-            "ALTER TABLE oro_calendar_event ADD CONSTRAINT FK_2DDC40DDA40A2C8 FOREIGN KEY (calendar_id) REFERENCES oro_calendar (id) ON DELETE CASCADE"
-        ];
+        /** Generate table oro_calendar **/
+        $table = $schema->createTable('oro_calendar');
+        $table->addColumn('id', 'integer', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => true, 'comment' => '']);
+        $table->addColumn('user_owner_id', 'integer', ['default' => null, 'notnull' => false, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('name', 'string', ['default' => null, 'notnull' => false, 'length' => 255, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->setPrimaryKey(['id']);
+        $table->addIndex(['user_owner_id'], 'IDX_1D171519EB185F9', []);
+        /** End of generate table oro_calendar **/
+
+        /** Generate table oro_calendar_connection **/
+        $table = $schema->createTable('oro_calendar_connection');
+        $table->addColumn('id', 'integer', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => true, 'comment' => '']);
+        $table->addColumn('connected_calendar_id', 'integer', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('calendar_id', 'integer', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('created', 'datetime', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('color', 'string', ['default' => null, 'notnull' => false, 'length' => 6, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('background_color', 'string', ['default' => null, 'notnull' => false, 'length' => 6, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->setPrimaryKey(['id']);
+        $table->addUniqueIndex(['calendar_id', 'connected_calendar_id'], 'oro_calendar_connection_uq');
+        $table->addIndex(['calendar_id'], 'IDX_25D13AB8A40A2C8', []);
+        $table->addIndex(['connected_calendar_id'], 'IDX_25D13AB8F94143E3', []);
+        /** End of generate table oro_calendar_connection **/
+
+        /** Generate table oro_calendar_event **/
+        $table = $schema->createTable('oro_calendar_event');
+        $table->addColumn('id', 'integer', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => true, 'comment' => '']);
+        $table->addColumn('calendar_id', 'integer', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('title', 'text', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('start_at', 'datetime', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('end_at', 'datetime', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('all_day', 'boolean', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('reminder', 'boolean', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('remind_at', 'datetime', ['default' => null, 'notnull' => false, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('reminded', 'boolean', ['default' => '0', 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->setPrimaryKey(['id']);
+        $table->addIndex(['calendar_id'], 'IDX_2DDC40DDA40A2C8', []);
+        $table->addIndex(['calendar_id', 'start_at', 'end_at'], 'oro_calendar_event_idx', []);
+        /** End of generate table oro_calendar_event **/
+
+        /** Generate foreign keys for table oro_calendar **/
+        $table = $schema->getTable('oro_calendar');
+        $table->addForeignKeyConstraint($schema->getTable('oro_user'), ['user_owner_id'], ['id'], ['onDelete' => 'SET NULL', 'onUpdate' => null]);
+        /** End of generate foreign keys for table oro_calendar **/
+
+        /** Generate foreign keys for table oro_calendar_connection **/
+        $table = $schema->getTable('oro_calendar_connection');
+        $table->addForeignKeyConstraint($schema->getTable('oro_calendar'), ['connected_calendar_id'], ['id'], ['onDelete' => 'CASCADE', 'onUpdate' => null]);
+        $table->addForeignKeyConstraint($schema->getTable('oro_calendar'), ['calendar_id'], ['id'], ['onDelete' => 'CASCADE', 'onUpdate' => null]);
+        /** End of generate foreign keys for table oro_calendar_connection **/
+
+        /** Generate foreign keys for table oro_calendar_event **/
+        $table = $schema->getTable('oro_calendar_event');
+        $table->addForeignKeyConstraint($schema->getTable('oro_calendar'), ['calendar_id'], ['id'], ['onDelete' => 'CASCADE', 'onUpdate' => null]);
+        /** End of generate foreign keys for table oro_calendar_event **/
+
+        // @codingStandardsIgnoreEnd
     }
 }
