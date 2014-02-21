fe8c72b2a2b247c63ffa40de02fd11e349a8123a
Alexander Nezdoiminoga <alex@magecore.com>
2014-02-21 20:06:10 +0200

    BAP-3159: OroEmbeddedFormBundle migrations

--- src/Oro/Bundle/EmbeddedFormBundle/Migration/v1_0/OroEmbeddedFormBundle.php
+++ src/Oro/Bundle/EmbeddedFormBundle/Migration/v1_0/OroEmbeddedFormBundle.php
@@ -9,12 +9,31 @@ class OroEmbeddedFormBundle implements Migration
 {
     /**
      * @inheritdoc
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
      */
     public function up(Schema $schema)
     {
-        return [
-            "CREATE TABLE oro_embedded_form (id VARCHAR(255) NOT NULL, channel_id SMALLINT DEFAULT NULL, title LONGTEXT NOT NULL, css LONGTEXT NOT NULL, form_type VARCHAR(255) NOT NULL, success_message VARCHAR(255) NOT NULL, INDEX IDX_F7A34C172F5A1AA (channel_id), PRIMARY KEY(id))",
-            "ALTER TABLE oro_embedded_form ADD CONSTRAINT FK_F7A34C172F5A1AA FOREIGN KEY (channel_id) REFERENCES oro_integration_channel (id)"
-        ];
+        // @codingStandardsIgnoreStart
+
+        /** Generate table oro_embedded_form **/
+        $table = $schema->createTable('oro_embedded_form');
+        $table->addColumn('id', 'string', ['default' => null, 'notnull' => true, 'length' => 255, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('channel_id', 'smallint', ['default' => null, 'notnull' => false, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('title', 'text', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('css', 'text', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('form_type', 'string', ['default' => null, 'notnull' => true, 'length' => 255, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('success_message', 'string', ['default' => null, 'notnull' => true, 'length' => 255, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('created_at', 'datetime', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('updated_at', 'datetime', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->setPrimaryKey(['id']);
+        $table->addIndex(['channel_id'], 'IDX_F7A34C172F5A1AA', []);
+        /** End of generate table oro_embedded_form **/
+
+        /** Generate foreign keys for table oro_embedded_form **/
+        $table = $schema->getTable('oro_embedded_form');
+        $table->addForeignKeyConstraint($schema->getTable('oro_integration_channel'), ['channel_id'], ['id'], ['onDelete' => null, 'onUpdate' => null]);
+        /** End of generate foreign keys for table oro_embedded_form **/
+
+        // @codingStandardsIgnoreEnd
     }
 }
