f4ec4a30e6d76d542b0c842ae5af0f7970646463
Alexander Nezdoiminoga <alex@magecore.com>
2014-02-21 21:01:55 +0200

    BAP-3159: OroTranslationBundle migrations

--- src/Oro/Bundle/TranslationBundle/Migration/v1_0/OroTranslationBundle.php
+++ src/Oro/Bundle/TranslationBundle/Migration/v1_0/OroTranslationBundle.php
@@ -9,11 +9,24 @@ class OroTranslationBundle implements Migration
 {
     /**
      * @inheritdoc
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
      */
     public function up(Schema $schema)
     {
-        return [
-            "CREATE TABLE oro_translation (id INT AUTO_INCREMENT NOT NULL, value LONGTEXT DEFAULT NULL, locale VARCHAR(5) NOT NULL, domain VARCHAR(255) NOT NULL, `key` VARCHAR(500) NOT NULL, scope SMALLINT NOT NULL, INDEX MESSAGE_IDX (locale, domain, `key`, scope), PRIMARY KEY(id))",
-        ];
+        // @codingStandardsIgnoreStart
+
+        /** Generate table oro_translation **/
+        $table = $schema->createTable('oro_translation');
+        $table->addColumn('id', 'integer', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => true, 'comment' => '']);
+        $table->addColumn('key', 'string', ['default' => null, 'notnull' => true, 'length' => 500, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('value', 'text', ['default' => null, 'notnull' => false, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('locale', 'string', ['default' => null, 'notnull' => true, 'length' => 5, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('domain', 'string', ['default' => null, 'notnull' => true, 'length' => 255, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('scope', 'smallint', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->setPrimaryKey(['id']);
+        $table->addIndex(['locale', 'domain', 'key', 'scope'], 'MESSAGE_IDX', []);
+        /** End of generate table oro_translation **/
+
+        // @codingStandardsIgnoreEnd
     }
 }
