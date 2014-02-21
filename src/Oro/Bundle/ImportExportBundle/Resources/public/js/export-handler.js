cb8856be3899a8fcf46aba6d4e72ac971458db57
Hryhorii Hrebiniuk <grebinuk@gmail.com>
2014-02-21 15:36:45 +0200

    BAP-2853: Unify module names and naming convention in JS
     - renamed modules in ImportExportBundle
    (cherry picked from commit eae56d7)

--- src/Oro/Bundle/ImportExportBundle/Resources/public/js/export-handler.js
+++ src/Oro/Bundle/ImportExportBundle/Resources/public/js/export-handler.js
@@ -1,10 +1,10 @@
-/* global define */
-define(['underscore', 'oro/translator', 'oro/messenger'],
-function(_, __, messenger) {
+/*global define*/
+define(['underscore', 'oro/translator', 'oro/messenger'
+    ], function (_, __, messenger) {
     'use strict';
 
     /**
-     * @export oro/export-handler
+     * @export oroimportexport/js/export-handler
      * @name   oro.exportHandler
      */
     return {
