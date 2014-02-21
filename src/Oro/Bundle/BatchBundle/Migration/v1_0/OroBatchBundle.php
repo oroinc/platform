c5f2cb0c8beccef1fd8ebab52d0d4ff4716dc639
Alexander Nezdoiminoga <alex@magecore.com>
2014-02-21 18:47:52 +0200

    BAP-3159: OroBatchBundle migrations

--- src/Oro/Bundle/BatchBundle/Migration/v1_0/OroBatchBundle.php
+++ src/Oro/Bundle/BatchBundle/Migration/v1_0/OroBatchBundle.php
@@ -9,19 +9,97 @@ class OroBatchBundle implements Migration
 {
     /**
      * @inheritdoc
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
      */
     public function up(Schema $schema)
     {
-        return [
-            "CREATE TABLE oro_batch_job_execution (id INT AUTO_INCREMENT NOT NULL, job_instance_id INT NOT NULL, status INT NOT NULL, start_time DATETIME DEFAULT NULL, end_time DATETIME DEFAULT NULL, create_time DATETIME DEFAULT NULL, updated_time DATETIME DEFAULT NULL, exit_code VARCHAR(255) DEFAULT NULL, exit_description LONGTEXT DEFAULT NULL, failure_exceptions LONGTEXT DEFAULT NULL COMMENT '(DC2Type:array)', log_file VARCHAR(255) DEFAULT NULL, INDEX IDX_66BCFEA7593D6954 (job_instance_id), PRIMARY KEY(id))",
-            "CREATE TABLE oro_batch_job_instance (id INT AUTO_INCREMENT NOT NULL, code VARCHAR(100) NOT NULL, `label` VARCHAR(255) DEFAULT NULL, alias VARCHAR(50) NOT NULL, status INT NOT NULL, connector VARCHAR(255) NOT NULL, type VARCHAR(255) NOT NULL, rawConfiguration LONGTEXT NOT NULL COMMENT '(DC2Type:array)', UNIQUE INDEX UNIQ_35B1ECC777153098 (code), PRIMARY KEY(id))",
-            "CREATE TABLE oro_batch_mapping_field (id INT AUTO_INCREMENT NOT NULL, item_id INT DEFAULT NULL, source VARCHAR(255) NOT NULL, destination VARCHAR(255) NOT NULL, identifier TINYINT(1) NOT NULL, INDEX IDX_45243258126F525E (item_id), PRIMARY KEY(id))",
-            "CREATE TABLE oro_batch_mapping_item (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id))",
-            "CREATE TABLE oro_batch_step_execution (id INT AUTO_INCREMENT NOT NULL, job_execution_id INT DEFAULT NULL, step_name VARCHAR(100) DEFAULT NULL, status INT NOT NULL, read_count INT NOT NULL, write_count INT NOT NULL, filter_count INT NOT NULL, start_time DATETIME DEFAULT NULL, end_time DATETIME DEFAULT NULL, exit_code VARCHAR(255) DEFAULT NULL, exit_description LONGTEXT DEFAULT NULL, terminate_only TINYINT(1) DEFAULT NULL, failure_exceptions LONGTEXT DEFAULT NULL COMMENT '(DC2Type:array)', errors LONGTEXT NOT NULL COMMENT '(DC2Type:array)', warnings LONGTEXT NOT NULL COMMENT '(DC2Type:array)', summary LONGTEXT NOT NULL COMMENT '(DC2Type:array)', INDEX IDX_3B30CD3C5871C06B (job_execution_id), PRIMARY KEY(id))",
+        // @codingStandardsIgnoreStart
 
-            "ALTER TABLE oro_batch_job_execution ADD CONSTRAINT FK_66BCFEA7593D6954 FOREIGN KEY (job_instance_id) REFERENCES oro_batch_job_instance (id) ON DELETE CASCADE",
-            "ALTER TABLE oro_batch_mapping_field ADD CONSTRAINT FK_45243258126F525E FOREIGN KEY (item_id) REFERENCES oro_batch_mapping_item (id)",
-            "ALTER TABLE oro_batch_step_execution ADD CONSTRAINT FK_3B30CD3C5871C06B FOREIGN KEY (job_execution_id) REFERENCES oro_batch_job_execution (id) ON DELETE CASCADE"
-        ];
+        /** Generate table oro_batch_job_execution **/
+        $table = $schema->createTable('oro_batch_job_execution');
+        $table->addColumn('id', 'integer', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => true, 'comment' => '']);
+        $table->addColumn('job_instance_id', 'integer', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('status', 'integer', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('start_time', 'datetime', ['default' => null, 'notnull' => false, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('end_time', 'datetime', ['default' => null, 'notnull' => false, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('create_time', 'datetime', ['default' => null, 'notnull' => false, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('updated_time', 'datetime', ['default' => null, 'notnull' => false, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('exit_code', 'string', ['default' => null, 'notnull' => false, 'length' => 255, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('exit_description', 'text', ['default' => null, 'notnull' => false, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('failure_exceptions', 'array', ['default' => null, 'notnull' => false, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '(DC2Type:array)']);
+        $table->addColumn('log_file', 'string', ['default' => null, 'notnull' => false, 'length' => 255, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->setPrimaryKey(['id']);
+        $table->addIndex(['job_instance_id'], 'IDX_66BCFEA7593D6954', []);
+        /** End of generate table oro_batch_job_execution **/
+
+        /** Generate table oro_batch_job_instance **/
+        $table = $schema->createTable('oro_batch_job_instance');
+        $table->addColumn('id', 'integer', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => true, 'comment' => '']);
+        $table->addColumn('code', 'string', ['default' => null, 'notnull' => true, 'length' => 100, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('label', 'string', ['default' => null, 'notnull' => false, 'length' => 255, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('alias', 'string', ['default' => null, 'notnull' => true, 'length' => 50, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('status', 'integer', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('connector', 'string', ['default' => null, 'notnull' => true, 'length' => 255, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('type', 'string', ['default' => null, 'notnull' => true, 'length' => 255, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('rawConfiguration', 'array', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '(DC2Type:array)']);
+        $table->setPrimaryKey(['id']);
+        $table->addUniqueIndex(['code'], 'UNIQ_35B1ECC777153098');
+        /** End of generate table oro_batch_job_instance **/
+
+        /** Generate table oro_batch_mapping_field **/
+        $table = $schema->createTable('oro_batch_mapping_field');
+        $table->addColumn('id', 'integer', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => true, 'comment' => '']);
+        $table->addColumn('item_id', 'integer', ['default' => null, 'notnull' => false, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('source', 'string', ['default' => null, 'notnull' => true, 'length' => 255, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('destination', 'string', ['default' => null, 'notnull' => true, 'length' => 255, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('identifier', 'boolean', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->setPrimaryKey(['id']);
+        $table->addIndex(['item_id'], 'IDX_45243258126F525E', []);
+        /** End of generate table oro_batch_mapping_field **/
+
+        /** Generate table oro_batch_mapping_item **/
+        $table = $schema->createTable('oro_batch_mapping_item');
+        $table->addColumn('id', 'integer', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => true, 'comment' => '']);
+        $table->setPrimaryKey(['id']);
+        /** End of generate table oro_batch_mapping_item **/
+
+        /** Generate table oro_batch_step_execution **/
+        $table = $schema->createTable('oro_batch_step_execution');
+        $table->addColumn('id', 'integer', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => true, 'comment' => '']);
+        $table->addColumn('job_execution_id', 'integer', ['default' => null, 'notnull' => false, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('step_name', 'string', ['default' => null, 'notnull' => false, 'length' => 100, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('status', 'integer', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('read_count', 'integer', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('write_count', 'integer', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('filter_count', 'integer', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('start_time', 'datetime', ['default' => null, 'notnull' => false, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('end_time', 'datetime', ['default' => null, 'notnull' => false, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('exit_code', 'string', ['default' => null, 'notnull' => false, 'length' => 255, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('exit_description', 'text', ['default' => null, 'notnull' => false, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('terminate_only', 'boolean', ['default' => null, 'notnull' => false, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('failure_exceptions', 'array', ['default' => null, 'notnull' => false, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '(DC2Type:array)']);
+        $table->addColumn('errors', 'array', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '(DC2Type:array)']);
+        $table->addColumn('warnings', 'array', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '(DC2Type:array)']);
+        $table->addColumn('summary', 'array', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '(DC2Type:array)']);
+        $table->setPrimaryKey(['id']);
+        $table->addIndex(['job_execution_id'], 'IDX_3B30CD3C5871C06B', []);
+        /** End of generate table oro_batch_step_execution **/
+
+        /** Generate foreign keys for table oro_batch_job_execution **/
+        $table = $schema->getTable('oro_batch_job_execution');
+        $table->addForeignKeyConstraint($schema->getTable('oro_batch_job_instance'), ['job_instance_id'], ['id'], ['onDelete' => 'CASCADE', 'onUpdate' => null]);
+        /** End of generate foreign keys for table oro_batch_job_execution **/
+
+        /** Generate foreign keys for table oro_batch_mapping_field **/
+        $table = $schema->getTable('oro_batch_mapping_field');
+        $table->addForeignKeyConstraint($schema->getTable('oro_batch_mapping_item'), ['item_id'], ['id'], ['onDelete' => null, 'onUpdate' => null]);
+        /** End of generate foreign keys for table oro_batch_mapping_field **/
+
+        /** Generate foreign keys for table oro_batch_step_execution **/
+        $table = $schema->getTable('oro_batch_step_execution');
+        $table->addForeignKeyConstraint($schema->getTable('oro_batch_job_execution'), ['job_execution_id'], ['id'], ['onDelete' => 'CASCADE', 'onUpdate' => null]);
+        /** End of generate foreign keys for table oro_batch_step_execution **/
+
+        // @codingStandardsIgnoreEnd
     }
 }
