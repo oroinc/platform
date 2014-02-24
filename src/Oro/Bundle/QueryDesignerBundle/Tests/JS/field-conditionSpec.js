3692abbe800ca2cf067f042d11aea9858cb541e2
eu-ge-ne <eugene.pobochny@gmail.com>
2014-02-24 16:25:44 +0200

    BAP-2748: Cover "Compare Fields" with js-tests

--- src/Oro/Bundle/QueryDesignerBundle/Tests/JS/field-conditionSpec.js
+++ src/Oro/Bundle/QueryDesignerBundle/Tests/JS/field-conditionSpec.js
@@ -52,24 +52,9 @@ define(['jquery', 'oroquerydesigner/js/field-condition'], function ($) {
             });
         });
 
-        it('renders default filter', function (done) {
-            var $noneFilterTemplate = $('<script type="text/template" id="filter-wrapper-template">\
-    <div class="btn-group filter-item oro-drop">\
-        <button type="button" class="btn filter-criteria-selector oro-drop-opener oro-dropdown-toggle">\
-            <% if (showLabel) { %><%= label %>: <% } %>\
-            <strong class="filter-criteria-hint"><%= criteriaHint %></strong>\
-            <span class="caret"></span>\
-        </button>\
-        <% if (canDisable) { %>\
-            <a href="<%= nullLink %>" class="disable-filter">\
-                <i class="icon-remove hide-text"><%- _.__("Close") %></i>\
-            </a>\
-        <% } %>\
-        <div class="filter-criteria dropdown-menu" />\
-    </div>\
-</script>\
+        it('renders none filter', function (done) {
+            var $noneFilterTemplate = $('\
 <script type="text/template" id="none-filter-template">\
-    <div><%= popupHint %></div>\
 </script>');
 
             $el.append($noneFilterTemplate);
@@ -104,12 +89,188 @@ define(['jquery', 'oroquerydesigner/js/field-condition'], function ($) {
                     },
                     "util": {},
                     "fieldsLoaderSelector": "#fields_loader"
-                }
+                },
+                "filters": [
+                    {
+                        "name": "string",
+                        "label": "String",
+                        "choices": [
+                            {
+                                "data": 1,
+                                "value": "1",
+                                "label": "contains"
+                            },
+                            {
+                                "data": 2,
+                                "value": "2",
+                                "label": "does not contain"
+                            },
+                            {
+                                "data": 3,
+                                "value": "3",
+                                "label": "is equal to"
+                            },
+                            {
+                                "data": 4,
+                                "value": "4",
+                                "label": "starts with"
+                            },
+                            {
+                                "data": 5,
+                                "value": "5",
+                                "label": "ends with"
+                            },
+                            {
+                                "data": 6,
+                                "value": "6",
+                                "label": "is any of"
+                            },
+                            {
+                                "data": 7,
+                                "value": "7",
+                                "label": "is not any of"
+                            }
+                        ],
+                        "applicable": [
+                            {
+                                "type": "string"
+                            },
+                            {
+                                "type": "text"
+                            }
+                        ],
+                        "type": "string",
+                        "templateTheme": "embedded"
+                    }
+                ]
             });
             waitForFilter(function (timeout) {
-                expect($el.find('.active-filter')).toContainHtml('<div>Choose a column first</div>');
+                expect($el.find('.active-filter')).toContainHtml('<div></div>');
                 done();
             });
         });
+
+        it('renders choice filter', function (done) {
+            var $noneFilterTemplate = $('\
+<script type="text/template" id="choice-filter-template-embedded">\
+    <span> field value </span>\
+    <div class="dropdown">\
+        <a class="dropdown-toggle" data-toggle="dropdown" href="#"><%= selectedChoiceLabel %></a>:\
+        <ul class="dropdown-menu">\
+            <% _.each(choices, function (option) { %>\
+            <li<% if (selectedChoice == option.value) { %> class="active"<% } %>>\
+                <a class="choice_value" href="#" data-value="<%= option.value %>"><%= option.label %></a>\
+            </li>\
+            <% }); %>\
+        </ul>\
+        <input type="text" name="value" value="<%- value %>">\
+        <input class="name_input" type="hidden" name="<%= name %>" id="<%= name %>" value="<%- selectedChoice %>"/>\
+    </div>\
+</script>\
+');
+
+            $el.append($noneFilterTemplate);
+
+            var $fieldsLoader = $('<input id="fields_loader"></input>');
+            $el.append($fieldsLoader);
+            $fieldsLoader.val('OroCRM\\Bundle\\AccountBundle\\Entity\\Account');
+            $fieldsLoader.data('fields', [
+                {
+                    "name": "name",
+                    "type": "string",
+                    "label": "Account name"
+                },
+                {
+                    "name": "createdAt",
+                    "type": "datetime",
+                    "label": "Created"
+                }
+            ]);
+            $el.data('value', {
+                "columnName": "name",
+                "criterion": {
+                    "filter": "string",
+                    "data": {
+                        "value": "a",
+                        "type": "1"
+                    }
+                }
+            });
+            $el.fieldCondition({
+                "fieldChoice": {
+                    "select2": {
+                        "placeholder": "Choose a field...",
+                        "formatSelectionTemplate": "<% _.each(obj, function (column, index, list) { %>&#32;<%= column.entity.label %>&nbsp;<b><%= column.label %></b><% if (index < list.length - 1) { %>&nbsp;><% } %><% }) %>"
+                    },
+                    "util": {},
+                    "fieldsLoaderSelector": "#fields_loader"
+                },
+                "filters": [
+                    {
+                        "name": "string",
+                        "label": "String",
+                        "choices": [
+                            {
+                                "data": 1,
+                                "value": "1",
+                                "label": "contains"
+                            },
+                            {
+                                "data": 2,
+                                "value": "2",
+                                "label": "does not contain"
+                            },
+                            {
+                                "data": 3,
+                                "value": "3",
+                                "label": "is equal to"
+                            },
+                            {
+                                "data": 4,
+                                "value": "4",
+                                "label": "starts with"
+                            },
+                            {
+                                "data": 5,
+                                "value": "5",
+                                "label": "ends with"
+                            },
+                            {
+                                "data": 6,
+                                "value": "6",
+                                "label": "is any of"
+                            },
+                            {
+                                "data": 7,
+                                "value": "7",
+                                "label": "is not any of"
+                            }
+                        ],
+                        "applicable": [
+                            {
+                                "type": "string"
+                            },
+                            {
+                                "type": "text"
+                            }
+                        ],
+                        "type": "string",
+                        "templateTheme": "embedded"
+                    }
+                ]
+            });
+            waitForFilter(function (timeout) {
+                var $f = $el.find('.active-filter');
+                expect($f).toContainText('contains');
+                expect($f).toContainText('does not contain');
+                expect($f).toContainText('is equal to');
+                expect($f).toContainText('starts with');
+                expect($f).toContainText('ends with');
+                expect($f).toContainText('is any of');
+                expect($f).toContainText('is not any of');
+                done();
+            });
+        });
+
     });
 });
