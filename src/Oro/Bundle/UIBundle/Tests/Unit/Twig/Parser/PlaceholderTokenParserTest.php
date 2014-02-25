077c816d15390b4a4ada297c596ddd55b7e38ecd
Sergey Zhuravel <zhuravel.sergey@gmail.com>
2014-02-25 20:50:53 +0200

    CRM-843: Extend placeholders functionality to able to pass dynamic placeholder names - update placeholder to handle wrong operators

--- src/Oro/Bundle/UIBundle/Tests/Unit/Twig/Parser/PlaceholderTokenParserTest.php
+++ src/Oro/Bundle/UIBundle/Tests/Unit/Twig/Parser/PlaceholderTokenParserTest.php
@@ -28,6 +28,8 @@ class PlaceholderTokenParserTest extends \PHPUnit_Framework_TestCase
 
     /**
      * @param \Twig_TokenStream $stream
+     * @param string            $getExpressionParser
+     * @param string            $parseExpression
      *
      * @dataProvider streamProvider
      */
@@ -35,27 +37,9 @@ class PlaceholderTokenParserTest extends \PHPUnit_Framework_TestCase
     {
         $startToken = new \Twig_Token(\Twig_Token::NAME_TYPE, 'placeholder', self::LINE);
 
-        $expressionParser = $this->getMockBuilder('\Twig_ExpressionParser')
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        $parser = $this->getMockBuilder('\Twig_Parser')
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        $parser->expects($this->once())
-            ->method('getStream')
-            ->will($this->returnValue($stream));
-
-        $parser->expects($this->$getExpressionParser())
-            ->method('getExpressionParser')
-            ->will($this->returnValue($expressionParser));
-
-        $expressionParser->expects($this->$parseExpression())
-            ->method('parseExpression')
-            ->will($this->returnValue(null));
-
-        $this->placeholder->setParser($parser);
+        $this->placeholder->setParser(
+            $this->getParser($stream, $getExpressionParser, $parseExpression)
+        );
 
         $resultNode = $this->placeholder->parse($startToken);
 
@@ -116,5 +100,88 @@ class PlaceholderTokenParserTest extends \PHPUnit_Framework_TestCase
                 'parseExpression' => 'never'
             ]
         ];
+    }
+
+    /**
+     * @param \Twig_TokenStream $stream
+     * @param string            $exceptionMessage
+     * @dataProvider wrongStreamProvider
+     */
+    public function testValidation($stream, $exceptionMessage)
+    {
+        $this->setExpectedException(
+            '\Twig_Error_Syntax',
+            $exceptionMessage
+        );
+
+        $startToken = new \Twig_Token(\Twig_Token::NAME_TYPE, 'placeholder', self::LINE);
+
+        $this->placeholder->setParser($this->getParser($stream));
+        $this->placeholder->parse($startToken);
+    }
+
+    public function wrongStreamProvider()
+    {
+        return [
+            'punctuation' => [
+                'stream' => new \Twig_TokenStream(
+                    [
+                        new \Twig_Token(\Twig_Token::STRING_TYPE, 'test_position', self::LINE),
+                        new \Twig_Token(\Twig_Token::PUNCTUATION_TYPE, '.', self::LINE),
+                        new \Twig_Token(\Twig_Token::BLOCK_END_TYPE, '', self::LINE),
+                        new \Twig_Token(\Twig_Token::EOF_TYPE, '', self::LINE),
+                    ]
+                ),
+                'exceptionMessage' => 'Unexpected token "punctuation" of value "placeholder" at line 12'
+            ],
+            'operator' => [
+                'stream' => new \Twig_TokenStream(
+                    [
+                        new \Twig_Token(\Twig_Token::STRING_TYPE, 'test_position', self::LINE),
+                        new \Twig_Token(\Twig_Token::OPERATOR_TYPE, 'set', self::LINE),
+                        new \Twig_Token(\Twig_Token::BLOCK_END_TYPE, '', self::LINE),
+                        new \Twig_Token(\Twig_Token::EOF_TYPE, '', self::LINE),
+                    ]
+                ),
+                'exceptionMessage' => 'Unexpected token "operator" of value "placeholder" at line 12'
+            ],
+            'filter' => [
+                'stream' => new \Twig_TokenStream(
+                    [
+                        new \Twig_Token(\Twig_Token::STRING_TYPE, 'test_position', self::LINE),
+                        new \Twig_Token(\Twig_Token::PUNCTUATION_TYPE, '|', self::LINE),
+                        new \Twig_Token(\Twig_Token::NAME_TYPE, 'lower', self::LINE),
+                        new \Twig_Token(\Twig_Token::BLOCK_END_TYPE, '', self::LINE),
+                        new \Twig_Token(\Twig_Token::EOF_TYPE, '', self::LINE),
+                    ]
+                ),
+                'exceptionMessage' => 'Unexpected token "punctuation" of value "placeholder"'
+            ]
+        ];
+    }
+
+    protected function getParser($stream, $getExpressionParser = 'any', $parseExpression = 'any')
+    {
+        $expressionParser = $this->getMockBuilder('\Twig_ExpressionParser')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $parser = $this->getMockBuilder('\Twig_Parser')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $parser->expects($this->once())
+            ->method('getStream')
+            ->will($this->returnValue($stream));
+
+        $parser->expects($this->$getExpressionParser())
+            ->method('getExpressionParser')
+            ->will($this->returnValue($expressionParser));
+
+        $expressionParser->expects($this->$parseExpression())
+            ->method('parseExpression')
+            ->will($this->returnValue(null));
+
+        return $parser;
     }
 }
--- src/Oro/Bundle/UIBundle/Twig/Parser/PlaceholderTokenParser.php
+++ src/Oro/Bundle/UIBundle/Twig/Parser/PlaceholderTokenParser.php
@@ -27,32 +27,47 @@ class PlaceholderTokenParser extends \Twig_TokenParser
      */
     public function parse(\Twig_Token $token)
     {
-        $parser = $this->parser;
-        $stream = $parser->getStream();
-
-        if ($stream->test(\Twig_Token::NAME_TYPE)) {
-            $name = $stream->getCurrent()->getValue();
-            $stream->next();
-        } else {
-            $names = [];
-            while (!$stream->test(\Twig_Token::NAME_TYPE, 'with')
-                && !$stream->test(\Twig_Token::BLOCK_END_TYPE)
-            ) {
-                if ($stream->test(\Twig_Token::STRING_TYPE)) {
-                    $names[] = $stream->getCurrent()->getValue();
-                }
-
-                $stream->next();
-            }
-            $name = implode($names);
-        }
-
+        $stream    = $this->parser->getStream();
+        $names     = [];
         $variables = null;
-        if ($stream->test(\Twig_Token::NAME_TYPE, 'with')) {
-            $stream->next();
-            $variables = $this->parser->getExpressionParser()->parseExpression();
-        }
+        while (!$stream->test(\Twig_Token::BLOCK_END_TYPE)) {
+            if ($stream->test(\Twig_Token::NAME_TYPE, 'with')) {
+                $stream->next();
+                $variables = $this->parser->getExpressionParser()->parseExpression();
 
+                continue;
+            }
+
+            if ($stream->test(\Twig_Token::NAME_TYPE)) {
+                $names[] = $stream->getCurrent()->getValue();
+                $stream->next();
+
+                continue;
+            }
+
+            if ($stream->test(\Twig_Token::STRING_TYPE)) {
+                $names[] = $stream->getCurrent()->getValue();
+                $stream->next();
+
+                continue;
+            }
+
+            if ($stream->test(\Twig_Token::OPERATOR_TYPE, '~')) {
+                $stream->next();
+
+                continue;
+            }
+
+            throw new \Twig_Error_Syntax(
+                sprintf(
+                    'Unexpected token "%s" of value "%s"',
+                    \Twig_Token::typeToEnglish($stream->getCurrent()->getType(), $token->getLine()),
+                    $token->getValue()
+                ),
+                $token->getLine()
+            );
+        }
+        $name = implode($names);
         $stream->expect(\Twig_Token::BLOCK_END_TYPE);
 
         if (isset($this->placeholders[$name])) {
