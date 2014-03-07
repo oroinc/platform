5f7a974e89b37423f8208fea626e00d0a37f9be0
Sergey Zhuravel <zhuravel.sergey@gmail.com>
2014-03-07 13:57:27 +0200

    BAP-3442: Create Reminder entities - create repo method

--- src/Oro/Bundle/ReminderBundle/Entity/Repository/ReminderRepository.php
+++ src/Oro/Bundle/ReminderBundle/Entity/Repository/ReminderRepository.php
@@ -4,6 +4,18 @@ namespace Oro\Bundle\ReminderBundle\Entity\Repository;
 
 use Doctrine\ORM\EntityRepository;
 
+use Oro\Bundle\ReminderBundle\Entity\Reminder;
+
 class ReminderRepository extends EntityRepository
 {
+    /**
+     * @return Reminder[]
+     */
+    public function getRemindersToProcess()
+    {
+        return $this->createQueryBuilder('r')
+            ->where('r.isSent = false')
+            ->getQuery()
+            ->execute();
+    }
 }
