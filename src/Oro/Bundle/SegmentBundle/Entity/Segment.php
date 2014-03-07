a27e391a678fda5a016ac75d1dc7bad00b2e15cc
Ivan Shakuta <ishakuta@gmail.com>
2014-03-07 14:28:56 +0000

    BAP-3385: Segmentation entities - segment entity

---
+++ src/Oro/Bundle/SegmentBundle/Entity/Segment.php
@@ -0,0 +1,285 @@
+<?php
+
+namespace Oro\Bundle\SegmentBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+use JMS\Serializer\Annotation as JMS;
+
+use Oro\Bundle\EntityConfigBundle\Metadata\Annotation\Config;
+use Oro\Bundle\OrganizationBundle\Entity\BusinessUnit;
+use Oro\Bundle\QueryDesignerBundle\Model\AbstractQueryDesigner;
+
+/**
+ * Segment
+ *
+ * @ORM\Table(name="oro_segment")
+ * @ORM\Entity(repositoryClass="Oro\Bundle\SegmentBundle\Entity\Repository\SegmentRepository")
+ * @Config(
+ *      routeName="oro_segment_index",
+ *      defaultValues={
+ *          "ownership"={
+ *              "owner_type"="BUSINESS_UNIT",
+ *              "owner_field_name"="owner",
+ *              "owner_column_name"="business_unit_owner_id"
+ *          },
+ *          "security"={
+ *              "type"="ACL",
+ *              "group_name"=""
+ *          }
+ *      }
+ * )
+ */
+class Segment extends AbstractQueryDesigner
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="smallint", name="id")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @JMS\Type("integer")
+     * @JMS\Expose
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", unique=true, length=255, nullable=false)
+     * @JMS\Type("string")
+     * @JMS\Expose
+     */
+    protected $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text", nullable=true)
+     */
+    protected $description;
+
+    /**
+     * @ORM\Column(name="entity_name", type="string", unique=false, length=100, nullable=false)
+     * @JMS\Type("string")
+     * @JMS\Expose
+     */
+    protected $entityName;
+
+    /**
+     * @ORM\Column(type="string", unique=false, length=30, nullable=false)
+     * @JMS\Type("string")
+     * @JMS\Expose
+     */
+    protected $type;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="definition", type="text")
+     */
+    protected $definition;
+
+    /**
+     * @var BusinessUnit
+     * @ORM\ManyToOne(targetEntity="Oro\Bundle\OrganizationBundle\Entity\BusinessUnit")
+     * @ORM\JoinColumn(name="business_unit_owner_id", referencedColumnName="id", onDelete="SET NULL")
+     */
+    protected $owner;
+
+    /**
+     * @var \Datetime $lastRun
+     *
+     * @ORM\Column(name="last_run", type="datetime")
+     */
+    protected $lastRun;
+
+    /**
+     * @var \Datetime $created
+     *
+     * @ORM\Column(type="datetime")
+     */
+    protected $createdAt;
+
+    /**
+     * @var \Datetime $updated
+     *
+     * @ORM\Column(type="datetime")
+     */
+    protected $updatedAt;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Report
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Segment
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get report type
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set report type
+     *
+     * @param string $type
+     * @return Segment
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get the full name of an entity on which this report is based
+     *
+     * @return string
+     */
+    public function getEntity()
+    {
+        return $this->entity;
+    }
+
+    /**
+     * Set the full name of an entity on which this report is based
+     *
+     * @param string $entity
+     * @return Segment
+     */
+    public function setEntity($entity)
+    {
+        $this->entity = $entity;
+
+        return $this;
+    }
+
+    /**
+     * Get a business unit owning this report
+     *
+     * @return BusinessUnit
+     */
+    public function getOwner()
+    {
+        return $this->owner;
+    }
+
+    /**
+     * Set a business unit owning this report
+     *
+     * @param BusinessUnit $owningBusinessUnit
+     * @return BusinessUnit
+     */
+    public function setOwner($owningBusinessUnit)
+    {
+        $this->owner = $owningBusinessUnit;
+
+        return $this;
+    }
+
+    /**
+     * Get this segment definition in YAML format
+     *
+     * @return string
+     */
+    public function getDefinition()
+    {
+        return $this->definition;
+    }
+
+    /**
+     * Set this segment definition in YAML format
+     *
+     * @param string $definition
+     * @return Segment
+     */
+    public function setDefinition($definition)
+    {
+        $this->definition = $definition;
+
+        return $this;
+    }
+
+    /**
+     * Get created date/time
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Get last update date/time
+     *
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @param \DateTime $updated
+     * @return Segment
+     */
+    public function setUpdatedAt($updated)
+    {
+        $this->updatedAt = $updated;
+
+        return $this;
+    }
+}
