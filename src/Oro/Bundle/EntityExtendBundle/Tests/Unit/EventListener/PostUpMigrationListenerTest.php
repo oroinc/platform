2a8f70463be7bc536c1b8d39df91c76d9e01a92f
Alexander Nezdoiminoga <alex@magecore.com>
2014-03-07 21:54:41 +0200

    BAP-3420: unittests

---
+++ src/Oro/Bundle/EntityExtendBundle/Tests/Unit/EventListener/PostUpMigrationListenerTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Oro\Bundle\EntityExtendBundle\Tests\Unit\EventListener;
+
+use Doctrine\DBAL\Connection;
+
+use Oro\Bundle\EntityBundle\ORM\OroEntityManager;
+
+use Oro\Bundle\EntityConfigBundle\Config\ConfigManager;
+
+use Oro\Bundle\EntityExtendBundle\EventListener\PostUpMigrationListener;
+use Oro\Bundle\EntityExtendBundle\Migration\ExtendConfigProcessor;
+use Oro\Bundle\EntityExtendBundle\Tests\Unit\Fixtures\TestMigration;
+use Oro\Bundle\EntityExtendBundle\Tools\ExtendConfigDumper;
+
+use Oro\Bundle\MigrationBundle\Event\PostMigrationEvent;
+
+class PostUpMigrationListenerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testOnPostUp()
+    {
+        /** @var ConfigManager $cm */
+        $cm = $this->getMockBuilder('Oro\Bundle\EntityConfigBundle\Config\ConfigManager')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        /** @var OroEntityManager $em */
+        $em = $this->getMockBuilder('Oro\Bundle\EntityBundle\ORM\OroEntityManager')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        /** @var PostUpMigrationListener $postUpMigrationListener */
+        $postUpMigrationListener = new PostUpMigrationListener(
+            new ExtendConfigProcessor($cm),
+            new ExtendConfigDumper($em, '')
+        );
+
+        /** @var Connection $connection */
+        $connection = $this->getMockBuilder('Doctrine\DBAL\Connection')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        /** @var PostMigrationEvent $event */
+        $event = new PostMigrationEvent($connection);
+        $event->addMigration(new TestMigration());
+
+        $postUpMigrationListener->onPostUp($event);
+
+        $this->assertNotEmpty($event);
+        $this->assertInstanceOf(
+            'Oro\Bundle\EntityExtendBundle\Tests\Unit\Fixtures\TestMigration',
+            $event->getMigrations()[0]
+        );
+        $this->assertInstanceOf(
+            'Oro\Bundle\EntityExtendBundle\Migration\UpdateExtendConfigMigration',
+            $event->getMigrations()[1]
+        );
+    }
+}
