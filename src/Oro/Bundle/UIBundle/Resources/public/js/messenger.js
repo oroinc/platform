a97c67c768fc23afa4583f5144dbae7a9191a795
Dmitry Khrysev <x86demon@gmail.com>
2014-03-13 16:23:03 +0200

    BAP-3528: Fixes after demo - step save logic changed

--- src/Oro/Bundle/UIBundle/Resources/public/js/messenger.js
+++ src/Oro/Bundle/UIBundle/Resources/public/js/messenger.js
@@ -6,7 +6,8 @@ function($, _) {
     var defaults = {
             container: '',
             delay: false,
-            template: $.noop
+            template: $.noop,
+            insertMethod: 'appendTo'
         },
         queue = [],
         storageKey = 'flash',
@@ -16,7 +17,8 @@ function($, _) {
          */
         showMessage = function(type, message, options) {
             var opt = _.extend({}, defaults, options || {}),
-                $el = $(opt.template({type: type, message: message})).appendTo(opt.container),
+                a = console.log(opt),
+                $el = $(opt.template({type: type, message: message}))[opt.insertMethod](opt.container),
                 delay = opt.delay || (opt.flash && 5000),
                 actions = {close: _.bind($el.alert, $el, 'close')};
             if (delay) {
--- src/Oro/Bundle/WorkflowBundle/Resources/public/js/workflow-management/step/view/edit.js
+++ src/Oro/Bundle/WorkflowBundle/Resources/public/js/workflow-management/step/view/edit.js
@@ -1,10 +1,10 @@
 /* global define */
-define(['underscore', 'orotranslation/js/translator', 'backbone', 'oro/dialog-widget',
+define(['underscore', 'orotranslation/js/translator', 'backbone', 'oroui/js/messenger', 'oro/dialog-widget',
     'oroworkflow/js/workflow-management/helper',
     'oroui/js/mediator', 'oroworkflow/js/workflow-management/transition/view/list',
     'oroworkflow/js/workflow-management/transition/model'
 ],
-function(_, __, Backbone, DialogWidget, Helper, mediator, TransitionsListView, TransitionModel) {
+function(_, __, Backbone, messenger, DialogWidget, Helper, mediator, TransitionsListView, TransitionModel) {
     'use strict';
 
     var $ = Backbone.$;
@@ -33,14 +33,14 @@ function(_, __, Backbone, DialogWidget, Helper, mediator, TransitionsListView, T
             var template = this.options.template || $('#step-form-template').html();
             this.template = _.template(template);
             this.widget = null;
-            this.addedTransitions = [];
         },
 
         onStepAdd: function() {
             var formData = Helper.getFormData(this.widget.form);
             var order = parseInt(formData.order);
 
-            if (!this.model.get('name')) {
+            var isNew = !this.model.get('name');
+            if (isNew) {
                 this.model.set('name', Helper.getNameByString(formData.label, 'step_'));
             }
             this.model.set('order', order > 0 ? order : 0);
@@ -48,24 +48,27 @@ function(_, __, Backbone, DialogWidget, Helper, mediator, TransitionsListView, T
             this.model.set('label', formData.label);
 
             this.trigger('stepAdd', this.model);
-            this.widget.remove();
+
+            if (isNew) {
+                this.render();
+                messenger.notificationFlashMessage(
+                    'success',
+                    __('Step saved.'),
+                    {container: this.$el, insertMethod: 'prependTo'}
+                );
+            } else {
+                this.widget.remove();
+            }
         },
 
         addStepTransition: function() {
             var transition = new TransitionModel();
-            this.addedTransitions.push(transition);
             this.options.workflow.trigger('requestEditTransition', transition, this.model);
         },
 
         remove: function() {
             this.transitionsListView.remove();
             Backbone.View.prototype.remove.call(this);
-        },
-
-        onCancel: function() {
-            _.each(this.addedTransitions, function(transition) {
-                transition.destroy();
-            });
         },
 
         renderTransitions: function() {
@@ -78,36 +81,43 @@ function(_, __, Backbone, DialogWidget, Helper, mediator, TransitionsListView, T
             this.transitionsListView.render();
         },
 
-        render: function() {
-            var data = this.model.toJSON();
-            data.transitionsAllowed = (this.options.workflow.get('steps').length > 1 && this.model.get('name'));
-            this.$el.append(this.template(data));
-
-            if (data.transitionsAllowed) {
-                this.renderTransitions();
+        renderWidget: function() {
+            if (!this.widget) {
+                this.widget = new DialogWidget({
+                    'title': this.model.get('name') ? __('Edit step') : __('Add new step'),
+                    'el': this.$el,
+                    'stateEnabled': false,
+                    'incrementalPosition': false,
+                    'dialogOptions': {
+                        'close': _.bind(this.remove, this),
+                        'width': 800,
+                        'modal': true
+                    }
+                });
+                this.listenTo(this.widget, 'renderComplete', function(el) {
+                    mediator.trigger('layout.init', el);
+                });
+                this.widget.render();
+            } else {
+                this.widget._adoptWidgetActions();
             }
-
-            this.widget = new DialogWidget({
-                'title': this.model.get('name') ? __('Edit step') : __('Add new step'),
-                'el': this.$el,
-                'stateEnabled': false,
-                'incrementalPosition': false,
-                'dialogOptions': {
-                    'close': _.bind(this.onCancel, this),
-                    'width': 800,
-                    'modal': true
-                }
-            });
-            this.listenTo(this.widget, 'renderComplete', function(el) {
-                mediator.trigger('layout.init', el);
-            });
-            this.widget.render();
 
             // Disable widget submit handler and set our own instead
             this.widget.form.off('submit');
             this.widget.form.validate({
                 'submitHandler': _.bind(this.onStepAdd, this)
             });
+        },
+
+        render: function() {
+            var data = this.model.toJSON();
+            data.transitionsAllowed = (this.options.workflow.get('steps').length > 1 && this.model.get('name'));
+            this.$el.html(this.template(data));
+
+            if (data.transitionsAllowed) {
+                this.renderTransitions();
+            }
+            this.renderWidget();
 
             return this;
         }
