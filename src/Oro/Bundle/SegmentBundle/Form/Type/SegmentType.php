96b969e6ae110bf86e89b1c0d581a7aad3d892f2
Ivan Shakuta <ishakuta@gmail.com>
2014-03-13 16:19:33 +0000

    BAP-3190: Segments CRUD - create segment form type - unit test

--- src/Oro/Bundle/SegmentBundle/Form/Type/SegmentType.php
+++ src/Oro/Bundle/SegmentBundle/Form/Type/SegmentType.php
@@ -15,19 +15,19 @@ class SegmentType extends AbstractQueryDesignerType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name', 'text', array('required' => true))
-            ->add('entity', 'oro_segment_entity_choice', array('required' => true))
+            ->add('name', 'text', ['required' => true])
+            ->add('entity', 'oro_segment_entity_choice', ['required' => true])
             ->add(
                 'type',
                 'entity',
-                array(
+                [
                     'class'       => 'OroSegmentBundle:SegmentType',
                     'property'    => 'label',
                     'required'    => true,
                     'empty_value' => 'oro.segment.form.choose_segment_type'
-                )
+                ]
             )
-            ->add('description', 'textarea', array('required' => false));
+            ->add('description', 'textarea', ['required' => false]);
 
         parent::buildForm($builder, $options);
     }
@@ -52,11 +52,11 @@ class SegmentType extends AbstractQueryDesignerType
     {
         $options = array_merge(
             $this->getDefaultOptions(),
-            array(
+            [
                 'data_class'                  => 'Oro\Bundle\SegmentBundle\Entity\Segment',
                 'intention'                   => 'segment',
                 'cascade_validation'          => true
-            )
+            ]
         );
 
         $resolver->setDefaults($options);
