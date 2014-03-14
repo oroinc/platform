fb119f71d41e321951867ac805af8efcf7e5916f
Dmitry Khrysev <dkhrysev@magecore.com>
2014-03-14 15:52:06 +0200

    BAP-3528: Fixes after demo

---
+++ src/Oro/Bundle/WorkflowBundle/Datagrid/WorkflowDefinitionGridEntityNameProvider.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Oro\Bundle\WorkflowBundle\Datagrid;
+
+use Doctrine\ORM\EntityManager;
+use Oro\Bundle\EntityConfigBundle\Provider\ConfigProviderInterface;
+use Oro\Bundle\EntityConfigBundle\Tools\ConfigHelper;
+use Symfony\Component\Translation\TranslatorInterface;
+
+class WorkflowDefinitionGridEntityNameProvider
+{
+    /**
+     * @var array
+     */
+    protected $relatedEntities = array();
+
+    /**
+     * @var EntityManager
+     */
+    protected $em;
+
+    /**
+     * @var ConfigProviderInterface
+     */
+    protected $configProvider;
+
+    /**
+     * @var TranslatorInterface
+     */
+    protected $translator;
+
+    /**
+     * @param ConfigProviderInterface $configProvider
+     * @param EntityManager $em
+     * @param TranslatorInterface $translator
+     */
+    public function __construct(
+        ConfigProviderInterface $configProvider,
+        EntityManager $em,
+        TranslatorInterface $translator
+    ) {
+        $this->configProvider = $configProvider;
+        $this->em = $em;
+        $this->translator = $translator;
+    }
+
+    /**
+     * Get workflow definition related entities.
+     *
+     * @return array
+     */
+    public function getRelatedEntitiesChoice()
+    {
+        if (empty($this->relatedEntities)) {
+            $qb = $this->em->createQueryBuilder();
+            $qb->select('w.relatedEntity')
+                ->from('OroWorkflowBundle:WorkflowDefinition', 'w')
+                ->distinct('w.relatedEntity');
+
+            $result = (array)$qb->getQuery()->getArrayResult();
+
+            foreach ($result as $value) {
+                $className = $value['relatedEntity'];
+                $label = $className;
+                if ($this->configProvider->hasConfig($className)) {
+                    $config = $this->configProvider->getConfig($className);
+                    $label = $this->translator->trans($config->get('label'));
+                } else {
+                    $nameData = ConfigHelper::getModuleAndEntityNames($className);
+                    if (count($nameData) > 1 && $nameData[1]) {
+                        $label = $nameData[1];
+                    }
+                }
+
+                $this->relatedEntities[$className] = $label;
+            }
+        }
+
+        return $this->relatedEntities;
+    }
+}
