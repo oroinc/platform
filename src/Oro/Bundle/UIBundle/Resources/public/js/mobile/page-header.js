1e4e6469db1761e88e537af11a8f164bdee0db38
Hryhorii Hrebiniuk <grebinuk@gmail.com>
2014-03-14 16:35:22 +0200

    BAP-3471: move separate buttons to new row

--- src/Oro/Bundle/UIBundle/Resources/public/js/mobile/page-header.js
+++ src/Oro/Bundle/UIBundle/Resources/public/js/mobile/page-header.js
@@ -1,39 +1,13 @@
-/*jshint browser: true*/
-/*jslint browser: true*/
 /*global define*/
-define(['jquery', 'oroui/js/mediator', 'orotranslation/js/translator'], function ($, mediator, __) {
+define(['jquery', 'oroui/js/mediator', 'orotranslation/js/translator', '../content-processor/dropdown-button'
+    ], function ($, mediator, __) {
     'use strict';
 
-    function updateButtonsGroup($header) {
-        var $group, $buttons, $moreButtons, $mainButtons,
-            $container = $header.find('.title-buttons-container');
-        $container.find('.separator-btn').replaceWith('<li class="divider"></li>');
-        $buttons = $container.find('.btn, .divider');
-        if ($buttons.length <= 1) {
-            return;
-        }
-        $mainButtons = $buttons.filter('.main-group:not(.more-group)');
-        if (!$mainButtons.length) {
-            $mainButtons = $buttons.first();
-        }
-        $moreButtons = $buttons.not($mainButtons);
-        if ($moreButtons.length > 1) {
-            $moreButtons = $('<a class="btn dropdown-toggle" data-toggle="dropdown" href="#">' +
-                __('More') + '<span class="caret"></span></a>')
-                    .add($('<ul class="dropdown-menu"></ul>').append($moreButtons));
-            $moreButtons.find('.btn').wrap('<li></li>').removeClass(function (index, css) {
-                return (css.match(/\bbtn(-\S+)?/g) || []).join(' ');
-            });
-        }
-        $group = $('<div class="btn-group pull-right"></div>')
-            .append($mainButtons, $moreButtons);
-        $buttons.removeClass('pull-right');
-        $container.find('.btn-group').remove().end().prepend($group);
-    }
-
     function updatePageHeader() {
-        var $el = $('.navigation.navbar-extra');
-        updateButtonsGroup($el);
+        var $header = $('.navigation.navbar-extra');
+        $header.find('.title-buttons-container').dropdownButtonProcessor({
+            moreLabel: __('More')
+        });
     }
 
     /**
