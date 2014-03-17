9a0c006cc42e264abd195ca4f2d4ce3a1c7390df
Ivan Shakuta <ishakuta@gmail.com>
2014-03-17 18:28:50 +0000

    BAP-3402: Apply segments UI - update tests

--- src/Oro/Bundle/SegmentBundle/Tests/Unit/Entity/Manager/SegmentManagerTest.php
+++ src/Oro/Bundle/SegmentBundle/Tests/Unit/Entity/Manager/SegmentManagerTest.php
@@ -6,6 +6,7 @@ use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityRepository;
 
 use Oro\Bundle\SegmentBundle\Entity\Manager\SegmentManager;
+use Oro\Bundle\SegmentBundle\Entity\Segment;
 use Oro\Bundle\SegmentBundle\Entity\SegmentType;
 
 class SegmentManagerTest extends \PHPUnit_Framework_TestCase
@@ -22,13 +23,9 @@ class SegmentManagerTest extends \PHPUnit_Framework_TestCase
     public function setUp()
     {
         $this->em = $this->getMockBuilder('Doctrine\ORM\EntityManager')
-            ->disableOriginalConstructor()->getMock();
-
-        $this->repository = $this->getMockBuilder('Doctrine\ORM\EntityRepository')
-            ->disableOriginalConstructor()->getMock();
-
-        $this->em->expects($this->any())->method('getRepository')->with('OroSegmentBundle:SegmentType')
-            ->will($this->returnValue($this->repository));
+            ->disableOriginalConstructor()
+            ->setMethods(['createQueryBuilder', 'getRepository'])
+            ->getMock();
 
 
         $this->manager = new SegmentManager($this->em);
@@ -41,6 +38,13 @@ class SegmentManagerTest extends \PHPUnit_Framework_TestCase
 
     public function testGetSegmentTypeChoices()
     {
+        $this->repository = $this->getMockBuilder('Doctrine\ORM\EntityRepository')
+            ->disableOriginalConstructor()->getMock();
+
+        $this->em->expects($this->any())->method('getRepository')
+            ->with('OroSegmentBundle:SegmentType')
+            ->will($this->returnValue($this->repository));
+
         $type = new SegmentType('test');
         $type->setLabel('testLabel');
 
@@ -53,5 +57,68 @@ class SegmentManagerTest extends \PHPUnit_Framework_TestCase
         $this->assertCount(1, $result);
 
         $this->assertSame(['test' => 'testLabel'], $result);
+    }
+
+    public function testGetSegmentByEntityName()
+    {
+        $this->assertEmpty($this->manager->getSegmentByEntityName('test', null));
+
+        $entityName = 'Acme\Entity\Demo';
+
+        $segment = new Segment();
+        $segment->setName('test');
+        $segments = [
+            $segment
+        ];
+
+        $query = $this->getMockBuilder('Doctrine\ORM\AbstractQuery')
+            ->disableOriginalConstructor()
+            ->setMethods(['getResult'])
+            ->getMockForAbstractClass();
+
+        $query->expects($this->once())
+            ->method('getResult')
+            ->will($this->returnValue($segments));
+
+        $qb = $this->getMockBuilder('Doctrine\ORM\QueryBuilder')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $qb->expects($this->once())
+            ->method('where')
+            ->will($this->returnSelf());
+        $qb->expects($this->once())
+            ->method('andWhere')
+            ->will($this->returnSelf());
+        $qb->expects($this->exactly(2))
+            ->method('setParameter')
+            ->will($this->returnSelf());
+        $qb->expects($this->once())
+            ->method('setMaxResults')
+            ->with(20)
+            ->will($this->returnSelf());
+        $qb->expects($this->once())
+            ->method('getQuery')
+            ->will($this->returnValue($query));
+
+        $repo = $this->getMockBuilder('Doctrine\ORM\EntityRepository')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $repo->expects($this->once())
+            ->method('createQueryBuilder')
+            ->will($this->returnValue($qb));
+
+        $this->em->expects($this->once())
+            ->method('getRepository')
+            ->with($this->equalTo('OroSegmentBundle:Segment'))
+            ->will($this->returnValue($repo));
+
+        $result = $this->manager->getSegmentByEntityName(
+            $entityName,
+            'test'
+        );
+
+        $this->assertCount(1, $result);
+        $this->assertEquals('test', $result[0]['text']);
+        $this->assertEquals('segment', $result[0]['type']);
     }
 }
