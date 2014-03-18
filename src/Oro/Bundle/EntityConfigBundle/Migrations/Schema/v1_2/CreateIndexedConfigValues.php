5b7a745a88db4d96ab1b5b21b436e942c8142961
Vova Soroka <vsoroka@magecore.com>
2014-03-18 20:50:27 +0200

    BAP-3413: Create migration scripts

--- src/Oro/Bundle/EntityConfigBundle/Migrations/Schema/v1_2/CreateIndexedConfigValues.php
+++ src/Oro/Bundle/EntityConfigBundle/Migrations/Schema/v1_2/CreateIndexedConfigValues.php
@@ -61,8 +61,6 @@ class CreateIndexedConfigValues implements Migration, OrderedMigrationInterface,
     {
         return 'DELETE FROM oro_entity_config_value WHERE NOT ('
         . " (scope = 'dataaudit' AND code = 'auditable')" // for both entity and field
-        . " OR (scope = 'entity' AND code = 'label')" // for both entity and field
-        . " OR (scope = 'entity' AND code = 'label')" // for both entity and field
         . " OR (scope = 'extend' AND code IN ('owner', 'state', 'is_deleted'))" // for both entity and field
         . " OR (scope = 'extend' AND code = 'is_extend' AND field_id IS NULL)" // for entity only
         . " OR (scope = 'ownership' AND code = 'owner_type' AND field_id IS NULL)" // for entity only
