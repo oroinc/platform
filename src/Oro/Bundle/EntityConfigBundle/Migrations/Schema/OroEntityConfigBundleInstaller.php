132c42db94b00ee3d976a3cf023990fe932fd470
Vova Soroka <vsoroka@magecore.com>
2014-03-19 16:06:29 +0200

    BAP-3413: Create migration scripts

--- src/Oro/Bundle/EntityConfigBundle/Migrations/Schema/OroEntityConfigBundleInstaller.php
+++ src/Oro/Bundle/EntityConfigBundle/Migrations/Schema/OroEntityConfigBundleInstaller.php
@@ -23,17 +23,54 @@ class OroEntityConfigBundleInstaller implements Installation
     public function up(Schema $schema, QueryBag $queries)
     {
         OroEntityConfigBundle::oroEntityConfigTable($schema);
+        $schema->getTable('oro_entity_config')
+            ->addColumn('data', 'array', ['notnull' => false]);
+
         OroEntityConfigBundle::oroEntityConfigFieldTable($schema);
+        $schema->getTable('oro_entity_config_field')
+            ->addColumn('data', 'array', ['notnull' => false]);
+
+        /** Generate table oro_entity_config_index_value **/
+        $table = $schema->createTable('oro_entity_config_index_value');
+        $table->addColumn('id', 'integer', ['autoincrement' => true]);
+        $table->addColumn('field_id', 'integer', ['notnull' => false]);
+        $table->addColumn('entity_id', 'integer', ['notnull' => false]);
+        $table->addColumn('code', 'string', ['length' => 255]);
+        $table->addColumn('scope', 'string', ['length' => 255]);
+        $table->addColumn('value', 'string', ['length' => 255, 'notnull' => false]);
+        $table->setPrimaryKey(['id']);
+        $table->addIndex(['entity_id'], 'IDX_256E3E9B81257D5D');
+        $table->addForeignKeyConstraint(
+            $schema->getTable('oro_entity_config'),
+            ['entity_id'],
+            ['id'],
+            ['onDelete' => null, 'onUpdate' => null]
+        );
+        $table->addIndex(['field_id'], 'IDX_256E3E9B443707B0');
+        $table->addForeignKeyConstraint(
+            $schema->getTable('oro_entity_config_field'),
+            ['field_id'],
+            ['id'],
+            ['onDelete' => null, 'onUpdate' => null]
+        );
+        $table->addIndex(
+            ['scope', 'code', 'value', 'entity_id'],
+            'idx_entity_config_index_entity'
+        );
+        $table->addIndex(
+            ['scope', 'code', 'value', 'field_id'],
+            'idx_entity_config_index_field'
+        );
+        /** End of generate table oro_entity_config_index_value **/
+
         OroEntityConfigBundle::oroEntityConfigLogTable($schema);
         OroEntityConfigBundle::oroEntityConfigLogDiffTable($schema);
         OroEntityConfigBundle::oroEntityConfigOptionsetTable($schema);
         OroEntityConfigBundle::oroEntityConfigOptionsetRelationTable($schema, 'oro_entity_config_optset_rel');
-        OroEntityConfigBundle::oroEntityConfigValueTable($schema);
 
         OroEntityConfigBundle::oroEntityConfigLogDiffForeignKeys($schema);
         OroEntityConfigBundle::oroEntityConfigOptionsetForeignKeys($schema);
         OroEntityConfigBundle::oroEntityConfigOptionsetRelationForeignKeys($schema, 'oro_entity_config_optset_rel');
-        OroEntityConfigBundle::oroEntityConfigValueForeignKeys($schema);
         OroEntityConfigBundle::oroEntityConfigFieldForeignKeys($schema);
         OroEntityConfigBundle::oroEntityConfigLogForeignKeys($schema);
     }
