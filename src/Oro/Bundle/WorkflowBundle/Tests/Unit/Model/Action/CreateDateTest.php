644513dde1f07493e6832299014827d02594c5d7
Yevhen Shyshkin <shishkin.yevgeniy@gmail.com>
2014-03-20 20:29:12 +0200

    BAP-3567: Incorrect time shifting in Sales Funnel  - CR fixes

--- src/Oro/Bundle/WorkflowBundle/Tests/Unit/Model/Action/CreateDateTest.php
+++ src/Oro/Bundle/WorkflowBundle/Tests/Unit/Model/Action/CreateDateTest.php
@@ -88,7 +88,7 @@ class CreateDateTest extends \PHPUnit_Framework_TestCase
             'with_date' => array(
                 'options' => array(
                     'attribute' => new PropertyPath('test_attribute'),
-                    'date'      => '2014-01-01 00:00:00',
+                    'date'      => '2014-01-01',
                 ),
                 'expectedResult' => new \DateTime('2014-01-01 00:00:00', new \DateTimeZone('UTC'))
             ),
