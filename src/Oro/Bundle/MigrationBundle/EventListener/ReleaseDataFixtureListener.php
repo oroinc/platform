43f87d444643d6a7d4c90747a6f513bcee0c2f27
Yevhen Shyshkin <shishkin.yevgeniy@gmail.com>
2014-03-20 13:11:06 +0200

    BAP-3573: Impossible to upgrade exist instance   - added TODOs to listeners

--- src/Oro/Bundle/MigrationBundle/EventListener/ReleaseDataFixtureListener.php
+++ src/Oro/Bundle/MigrationBundle/EventListener/ReleaseDataFixtureListener.php
@@ -10,6 +10,10 @@ use Oro\Bundle\MigrationBundle\Migration\CreateMigrationTableMigration;
 use Oro\Bundle\MigrationBundle\Migration\Migration;
 use Oro\Bundle\MigrationBundle\Migration\ReleaseDataFixtureMigration;
 
+/**
+ * TODO: This listener is a temporary solution for migration of data fixtures.
+ * TODO: It should be removed in scope of https://magecore.atlassian.net/browse/BAP-3605
+ */
 class ReleaseDataFixtureListener
 {
     /**
