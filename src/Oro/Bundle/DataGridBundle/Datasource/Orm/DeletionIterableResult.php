c8d388ecd409a9df40d8b24468a6cef865edafc9
Alexandr <avreliy.89@gmail.com>
2014-03-22 13:16:11 +0200

    BAP-3539: OC: Bulk Delete Contacts  - OC-316: Bug: Bulk delete Contacts

--- src/Oro/Bundle/DataGridBundle/Datasource/Orm/DeletionIterableResult.php
+++ src/Oro/Bundle/DataGridBundle/Datasource/Orm/DeletionIterableResult.php
@@ -3,6 +3,7 @@
 namespace Oro\Bundle\DataGridBundle\Datasource\Orm;
 
 use Doctrine\ORM\Query;
+use Doctrine\ORM\QueryBuilder;
 
 /**
  * The aim of this class is provide an iterator which can be used for delete records.
@@ -11,6 +12,14 @@ use Doctrine\ORM\Query;
  */
 class DeletionIterableResult extends IterableResult
 {
+    public function __construct(QueryBuilder $source)
+    {
+        //in case if delete mass action param inset =0 (else frontend return coma separated ids)
+        $source->setMaxResults(null);
+
+        parent::__construct($source);
+    }
+
     /**
      * {@inheritdoc}
      */
