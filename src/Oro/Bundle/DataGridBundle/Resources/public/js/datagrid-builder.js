3a2a896f4f7a6432c26c88013fca47c16cb71746
Viktor Linkin <adrenalinkin@gmail.com>
2014-03-27 12:17:03 +0200

    fix/BAP-3554: Make filter bar render before grid rendering - revert changes

--- src/Oro/Bundle/DataGridBundle/Resources/public/js/datagrid-builder.js
+++ src/Oro/Bundle/DataGridBundle/Resources/public/js/datagrid-builder.js
@@ -121,24 +121,6 @@ define(function (require) {
              */
             afterBuild: function () {
                 mediator.trigger('datagrid_collection_set_after', this.grid.collection, this.$el);
-
-                if (
-                    this.metadata.hasOwnProperty('filters') && this.metadata.filters.length &&
-                    this.grid.collection.length && !this.grid.noColumnsFlag
-                ) {
-                    mediator.once('datagrid_filters:rendered', methods.showGrid);
-                    this.$el.hide();
-                }
-            },
-
-            /**
-             * Show is already rendered grid
-             *
-             * @param {Object} collection
-             * @param {Object} $el
-             */
-            showGrid: function(collection, $el) {
-                $el.show();
             },
 
             /**
