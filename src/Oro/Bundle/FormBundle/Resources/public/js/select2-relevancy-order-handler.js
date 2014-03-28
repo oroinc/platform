b2b89bc4a64c2cc17304a51d6f76d3d6e9e8fa16
Alexandr <avreliy.89@gmail.com>
2014-03-28 19:17:06 +0200

    BAP-3686: Select2 improvement doesn’t work as expected (best match should be first)

---
+++ src/Oro/Bundle/FormBundle/Resources/public/js/select2-relevancy-order-handler.js
@@ -0,0 +1,35 @@
+/*global define*/
+define([], function(){
+    /**
+     * @export  oroform/js/select2-relevancy-order-handler
+     * @class   oroform.select2RelevancyOrderHandler
+     */
+    return {
+        handle: function(configs){
+            if (configs['ajax']) {
+                configs.minimumInputLength = 0;
+                return;
+            }
+
+            configs['sortResults'] = function(results, container, query){
+                if (!query.term || query.term.length < 1) {
+                    return results;
+                }
+                var expression = new RegExp(query.term, 'im');
+
+                var sortIteratorDelegate = function (first, second) {
+                    var inFirst = first.text.search(expression);
+                    var inSecond = second.text.search(expression);
+
+                    if (inFirst == -1 || inSecond == -1) {
+                        return inSecond - inFirst;
+                    }
+
+                    return inFirst - inSecond;
+                };
+
+                return results.sort(sortIteratorDelegate);
+            }
+        }
+    };
+});
