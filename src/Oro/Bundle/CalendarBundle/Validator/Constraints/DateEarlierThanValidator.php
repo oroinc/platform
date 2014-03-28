b7959d860496db916302a2d021cf11ed54d87beb
Viktor Linkin <vlinkin@magecore.com>
2014-03-29 00:02:31 +0200

    BAP-3694: improve validtor  - added type exception

--- src/Oro/Bundle/CalendarBundle/Validator/Constraints/DateEarlierThanValidator.php
+++ src/Oro/Bundle/CalendarBundle/Validator/Constraints/DateEarlierThanValidator.php
@@ -4,6 +4,7 @@ namespace Oro\Bundle\CalendarBundle\Validator\Constraints;
 
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
 
 class DateEarlierThanValidator extends ConstraintValidator
 {
@@ -11,11 +12,18 @@ class DateEarlierThanValidator extends ConstraintValidator
     public function validate($value, Constraint $constraint)
     {
         /** @var DateEarlierThan $constraint */
-        if ($value instanceof \DateTime) {
-            $valueCompare = $this->context->getRoot()->get($constraint->field)->getData();
-            if ($valueCompare instanceof \DateTime && $value->getTimestamp() > $valueCompare->getTimestamp()) {
-                $this->context->addViolation($constraint->message, array('{{ field }}' => $constraint->field));
-            }
+        if (!$value instanceof \DateTime) {
+            throw new UnexpectedTypeException($value, 'DateTime');
+        }
+
+        $valueCompare = $this->context->getRoot()->get($constraint->field)->getData();
+
+        if (!$valueCompare instanceof \DateTime) {
+            throw new UnexpectedTypeException($value, 'DateTime');
+        }
+
+        if ($value->getTimestamp() > $valueCompare->getTimestamp()) {
+            $this->context->addViolation($constraint->message, array('{{ field }}' => $constraint->field));
         }
 
     }
