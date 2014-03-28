68fb856c5123c2543b97dfb1c7e16b1510fb9f3e
Alexandr Smaga <asmaga@magecore.com>
2014-03-28 13:15:17 +0200

    BAP-3725: After session expires user is not redirected to login after ajax requests    - added global ajax error handler    - remove Backbone.sync override

--- src/Oro/Bundle/UIBundle/Resources/public/js/error.js
+++ src/Oro/Bundle/UIBundle/Resources/public/js/error.js
@@ -1,93 +1,72 @@
 /*global define*/
-define(['underscore', 'orotranslation/js/translator', 'backbone', 'routing', 'oroui/js/app', 'oroui/js/modal',
-    'oronavigation/js/navigation'
-], function (_, __, Backbone, routing, app, Modal, Navigation) {
+define(['underscore', 'orotranslation/js/translator', 'jquery', 'routing', 'oroui/js/app', 'oroui/js/modal', 'oronavigation/js/navigation'
+], function (_, __, $, routing, app, Modal, Navigation) {
     'use strict';
 
     var defaults = {
             headerServerError: __('Server error'),
-            headerUserError:   __('User input error'),
-            message:           __('Error! Incorrect server response.')
+            headerUserError: __('User input error'),
+            message: __('Error! Incorrect server response.')
         },
 
         ERROR_USER_INPUT = 'user_input_error',
-
-        /**
-         * @export oroui/js/error
-         * @name oroui.error
-         */
-            error = {
-                dispatch: function (model, xhr, options) {
-                    var self = error.dispatch;
-                    self.init(model, xhr, _.extend({}, defaults, options));
+        errorHandler = {
+            /**
+             * Global error handler
+             *
+             * @param {Object} event
+             * @param {Object} xhr
+             */
+            handle: function (event, xhr) {
+                if (xhr.status === 401 || xhr.status === 403) {
+                    this._processRedirect();
+                } else if (xhr.readyState === 4 && (app.debug || typeof xhr.error !== 'function')) {
+                    // show error in modal window in following cases:
+                    // when custom error handling is not added
+                    // when in debug mode
+                    this._processModal(xhr);
                 }
-        },
-        sync = Backbone.sync;
+            },
 
-    // Override default Backbone.sync
-    Backbone.sync = function (method, model, options) {
-        options = options || {};
-        if (!_.has(options, 'error')) {
-            options.error = error.dispatch;
-        }
+            /**
+             * Shows modal window
+             * @param {Object} xhr
+             * @private
+             */
+            _processModal: function (xhr) {
+                var modal,
+                    message = defaults.message;
+                if (app.debug) {
+                    message += '<br><b>Debug:</b>' + xhr.responseText;
+                }
 
-        sync.call(Backbone, method, model, options);
-    };
+                var responseObject = xhr.responseJSON || {},
+                    errorType = responseObject.type;
 
-    _.extend(error.dispatch, {
-        /**
-         * Error dispatch
-         *
-         * @param {Object} model
-         * @param {Object} xhr
-         * @param {Object} options
-         */
-        init: function (model, xhr, options) {
-            if (xhr.status === 401) {
-                this._processRedirect();
-            } else if (xhr.readyState === 4) {
-                this._processModal(xhr, options);
+                modal = new Modal({
+                    title: errorType === ERROR_USER_INPUT ? defaults.headerUserError : defaults.headerServerError,
+                    content: responseObject.message || message,
+                    cancelText: false
+                });
+                modal.open();
+            },
+
+            /**
+             * Redirects to login
+             * @private
+             */
+            _processRedirect: function () {
+                var hashUrl = '';
+                if (Navigation.isEnabled()) {
+                    var navigation = Navigation.getInstance();
+                    hashUrl = '#url=' + navigation.getHashUrl();
+                }
+
+                window.location.href = routing.generate('oro_user_security_login') + hashUrl;
             }
-        },
+        };
 
-        /**
-         * Shows modal window
-         * @param {Object} xhr
-         * @param {Object} options
-         * @private
-         */
-        _processModal: function (xhr, options) {
-            var modal,
-                message = options.message;
-            if (app.debug) {
-                message += '<br><b>Debug:</b>' + xhr.responseText;
-            }
+    $(document).ajaxError(_.bind(errorHandler.handle, errorHandler));
 
-            var responseObject = xhr.responseJSON || {},
-                errorType = responseObject.type;
-
-            modal = new Modal({
-                title:      errorType === ERROR_USER_INPUT  ? options.headerUserError : options.headerServerError,
-                content:    responseObject.message || message,
-                cancelText: false
-            });
-            modal.open();
-        },
-
-        /**
-         * Redirects to login
-         * @private
-         */
-        _processRedirect: function () {
-            var hashUrl = '';
-            if (Navigation.isEnabled()) {
-                var navigation = Navigation.getInstance();
-                hashUrl = '#url=' + navigation.getHashUrl();
-            }
-
-            window.location.href = routing.generate('oro_user_security_login') + hashUrl;
-        }
-    });
-
-    return error;
+    return errorHandler;
 });
