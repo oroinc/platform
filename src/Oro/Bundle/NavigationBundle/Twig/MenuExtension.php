ba0535f29d0d6e719fecb5f9cea07004f9375591
Dmitry Khrysev <x86demon@gmail.com>
2014-03-31 14:33:28 +0300

    BAP-3788: User with Leads Development Rep permissions.

--- src/Oro/Bundle/NavigationBundle/Twig/MenuExtension.php
+++ src/Oro/Bundle/NavigationBundle/Twig/MenuExtension.php
@@ -101,6 +101,7 @@ class MenuExtension extends \Twig_Extension
             }
 
             $menu = $this->getMenu($menu, $path, $options);
+            $menu = $this->filterUnallowedItems($menu);
         }
 
         $menuType = $menu->getExtra('type');
@@ -110,6 +111,35 @@ class MenuExtension extends \Twig_Extension
         }
 
         return $this->helper->render($menu, $options, $renderer);
+    }
+
+    /**
+     * Get menu filtered by isAllowed children.
+     *
+     * @param ItemInterface|array $menu
+     * @return ItemInterface|array
+     */
+    protected function filterUnallowedItems($menu)
+    {
+        /** @var ItemInterface $item */
+        foreach ($menu as &$item) {
+            if ($item->hasChildren()) {
+                $filteredChildren = $this->filterUnallowedItems($item->getChildren());
+                $invisibleChildrenCount = 0;
+                /** @var ItemInterface $child */
+                foreach ($filteredChildren as $child) {
+                    if (!$child->getLabel() || !$child->getExtra('isAllowed')) {
+                        $invisibleChildrenCount++;
+                    }
+                }
+
+                if (count($filteredChildren) == $invisibleChildrenCount) {
+                    $item->setExtra('isAllowed', false);
+                }
+            }
+        }
+
+        return $menu;
     }
 
     /**
