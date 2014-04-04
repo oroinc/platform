9e66daa6dabcb9c5c6a7ac9ed6d992a199db13a8
hryhorii hrebiniuk <hhrebiniuk@magecore.com>
2014-04-04 19:34:11 +0300

    BAP-3877: Manage filters is disconnected from filters element
     - datepicker updates its position on scroll
     - select2-extend renamed

---
+++ src/Oro/Bundle/UIBundle/Resources/public/js/jquery-ui-extend.js
@@ -0,0 +1,98 @@
+/*global define*/
+/*jslint nomen:true, browser:true*/
+define(['jquery', 'jquery-ui'], function ($) {
+    'use strict';
+
+    /* datapicker extend:start */
+    (function () {
+
+        /**
+         * Combines space-separated line of events with widget's namespace
+         *  for updating datepicker's position
+         *
+         * @returns {string}
+         * @private
+         */
+        function getUpdatePosEvents(uuid) {
+            var events = ['scroll'],
+                ns = 'datepicker-' + uuid;
+
+            events = $.map(events, function (eventName) {
+                return eventName + '.' + ns;
+            });
+
+            return events.join(' ');
+        }
+
+        /**
+         * Process position update for datepicker element
+         */
+        function updatePos() {
+            var pos, isFixed, offset, inst,
+                input = this;
+
+            inst = $.datepicker._getInst(input);
+
+            if (!$.datepicker._pos) { // position below input
+                pos = $.datepicker._findPos(input);
+                pos[1] += input.offsetHeight; // add the height
+            }
+
+            isFixed = false;
+            $(input).parents().each(function () {
+                isFixed |= $(this).css("position") === "fixed";
+                return !isFixed;
+            });
+
+            offset = {left: pos[0], top: pos[1]};
+            offset = $.datepicker._checkOffset(inst, offset, isFixed);
+            inst.dpDiv.css({left: offset.left + "px", top: offset.top + "px"});
+        }
+
+        var _showDatepicker = $.datepicker.constructor.prototype._showDatepicker,
+            _hideDatepicker = $.datepicker.constructor.prototype._hideDatepicker;
+
+        /**
+         * Bind update position method after datepicker is opened
+         *
+         * @param elem
+         * @override
+         * @private
+         */
+        $.datepicker.constructor.prototype._showDatepicker = function (elem) {
+            var events, input;
+
+            _showDatepicker.apply(this, arguments);
+
+            input = elem.target || elem;
+            events = getUpdatePosEvents(input.id);
+
+            $(input).parents().add(window).each(function () {
+                $(this).on(events, $.proxy(updatePos, input));
+            });
+        };
+
+        /**
+         * Remove all handlers before closing datepicker
+         *
+         * @param elem
+         * @override
+         * @private
+         */
+        $.datepicker.constructor.prototype._hideDatepicker = function (elem) {
+            var events, input = elem;
+
+            if (!elem) {
+                input = $.datepicker._curInst.input.get(0);
+            }
+            events = getUpdatePosEvents(input.id);
+
+            $(input).parents().add(window).each(function () {
+                $(this).off(events);
+            });
+
+            _hideDatepicker.apply(this, arguments);
+        };
+    }());
+    /* datapicker extend:end */
+});
