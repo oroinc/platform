dff5712ad9aa03c53f07f281971a37812ad9067d
Yevhen Shyshkin <shishkin.yevgeniy@gmail.com>
2014-04-08 19:44:49 +0300

    BAP-3525: Incorrect filters on Entity Config grid  - updated EntityConfigModel and added unit test  - updated EntityConfigGridListener  - added schema and data migrations  - fixed translations in EntityConfigBundle

---
+++ src/Oro/Bundle/EntityConfigBundle/Migrations/Schema/v1_3/AddModuleAndEntityFields.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Oro\Bundle\EntityConfigBundle\Migrations\Schema\v1_3;
+
+use Doctrine\DBAL\Schema\Schema;
+
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+
+class AddModuleAndEntityFields implements Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        $table = $schema->getTable('oro_entity_config');
+        $table->addColumn('module_name', 'string', array('length' => 255));
+        $table->addColumn('entity_name', 'string', array('length' => 255));
+    }
+}
