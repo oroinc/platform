87fe52fa1491dfca6481c7241e9fc077bc75fccf
Yevhen Shyshkin <shishkin.yevgeniy@gmail.com>
2014-04-14 15:00:19 +0300

    BAP-3874: During initialization of twig environment all dependency tree are built  - fixed loading of lazy services

--- src/Oro/Bundle/PlatformBundle/DependencyInjection/Compiler/LazyServicesCompilerPass.php
+++ src/Oro/Bundle/PlatformBundle/DependencyInjection/Compiler/LazyServicesCompilerPass.php
@@ -2,31 +2,33 @@
 
 namespace Oro\Bundle\PlatformBundle\DependencyInjection\Compiler;
 
+use Oro\Component\Config\Loader\CumulativeConfigLoader;
+use Oro\Component\Config\Loader\YamlCumulativeFileLoader;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class LazyServicesCompilerPass implements CompilerPassInterface
 {
     /**
-     * @var array
-     */
-    protected $lazyServices = array(
-        'assetic.asset_manager',
-        'knp_menu.renderer.twig',
-        'templating',
-        'twig',
-        'templating.engine.twig',
-        'twig.controller.exception',
-    );
-
-    /**
      * {@inheritdoc}
      */
     public function process(ContainerBuilder $container)
     {
-        foreach ($this->lazyServices as $id) {
-            if ($container->hasDefinition($id)) {
-                $container->getDefinition($id)->setLazy(true);
+        $configLoader = new CumulativeConfigLoader(
+            'oro_lazy_services',
+            new YamlCumulativeFileLoader('Resources/config/oro/lazy_services.yml')
+        );
+
+        $lazyServices = array();
+        foreach ($configLoader->load($container) as $resource) {
+            if (!empty($resource->data['lazy_services']) && is_array($resource->data['lazy_services'])) {
+                $lazyServices = array_merge($lazyServices, $resource->data['lazy_services']);
+            }
+        }
+
+        foreach ($lazyServices as $serviceId) {
+            if ($container->hasDefinition($serviceId)) {
+                $container->getDefinition($serviceId)->setLazy(true);
             }
         }
     }
