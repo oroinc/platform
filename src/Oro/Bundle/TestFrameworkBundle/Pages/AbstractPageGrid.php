785c7588744d801d1ea69c9d7c6a5ff4f92bc029
Roman Grebenchuk <rgrebenchuk@magecore.com>
2014-04-14 12:04:34 +0300

    BAP-498: Investigate failed builds  - changed detecting row

--- src/Oro/Bundle/TestFrameworkBundle/Pages/AbstractPageGrid.php
+++ src/Oro/Bundle/TestFrameworkBundle/Pages/AbstractPageGrid.php
@@ -180,10 +180,10 @@ abstract class AbstractPageGrid extends AbstractPage
      * Verify entity exist on the current page
      *
      * @param array $entityData
-     * @param int $column
+     * @param null|int $column
      * @return \PHPUnit_Extensions_Selenium2TestCase_Element
      */
-    public function getEntity($entityData, $column = 1)
+    public function getEntity($entityData, $column = null)
     {
         $xpath = '';
         foreach ($entityData as $entityField) {
@@ -192,7 +192,12 @@ abstract class AbstractPageGrid extends AbstractPage
             }
             $xpath .=  "td[contains(.,'{$entityField}')]";
         }
-        $xpath = "{$this->gridPath}//table/tbody/tr[{$xpath}]/td[{$column}]";
+        $postFix = "";
+        if (!is_null($column)) {
+            $postFix = "/td[{$column}]";
+        }
+
+        $xpath = "{$this->gridPath}//table/tbody/tr[{$xpath}]{$postFix}";
         return $this->test->byXPath($xpath);
     }
 
