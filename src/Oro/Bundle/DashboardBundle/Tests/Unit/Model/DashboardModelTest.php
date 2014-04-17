9cb295eeb7d8e438c99c9121a36965acf4c7a290
Ignat Shcheglovskyi <ischeglovskiy@magecore.com>
2014-04-17 15:04:59 +0300

    BAP-3266: Refactoring dashboard manager - update tests

--- src/Oro/Bundle/DashboardBundle/Tests/Unit/Model/DashboardModelTest.php
+++ src/Oro/Bundle/DashboardBundle/Tests/Unit/Model/DashboardModelTest.php
@@ -2,28 +2,219 @@
 
 namespace Oro\Bundle\DashboardBundle\Tests\Unit\Model;
 
+use Doctrine\Common\Collections\ArrayCollection;
+
 use Oro\Bundle\DashboardBundle\Model\DashboardModel;
 
 class DashboardModelTest extends \PHPUnit_Framework_TestCase
 {
-    public function testAccessToInternalProperties()
-    {
-        $configLabel = 'sample';
-        $expectedConfig = array('label' => $configLabel);
-        $entityLabel = 'from_entity';
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $dashboardEntity;
 
-        $widgetsCollection = $this->getMockBuilder('Oro\Bundle\DashboardBundle\Model\WidgetCollection')
+    /**
+     * @var ArrayCollection
+     */
+    protected $widgets;
+
+    /**
+     * @var array
+     */
+    protected $config = array(
+        'label' => 'Dashboard label'
+    );
+
+    /**
+     * @var DashboardModel
+     */
+    protected $dashboardModel;
+
+    protected function setUp()
+    {
+        $this->dashboardEntity = $this->getMock('Oro\Bundle\DashboardBundle\Entity\Dashboard');
+
+        $this->widgets = new ArrayCollection(
+            array(
+                $this->getMockBuilder('Oro\Bundle\DashboardBundle\Model\WidgetModel')
+                    ->disableOriginalConstructor()->getMock(),
+                $this->getMockBuilder('Oro\Bundle\DashboardBundle\Model\WidgetModel')
+                    ->disableOriginalConstructor()
+                    ->getMock(),
+            )
+        );
+
+        $this->dashboardModel = new DashboardModel($this->dashboardEntity, $this->widgets, $this->config);
+    }
+
+    public function testGetConfig()
+    {
+        $this->assertEquals($this->config, $this->dashboardModel->getConfig());
+    }
+
+    public function testGetEntity()
+    {
+        $this->assertEquals($this->dashboardEntity, $this->dashboardModel->getEntity());
+    }
+
+    public function testGetWidgets()
+    {
+        $this->assertEquals($this->widgets, $this->dashboardModel->getWidgets());
+    }
+
+    public function testGetId()
+    {
+        $id = 100;
+        $this->dashboardEntity->expects($this->once())
+            ->method('getId')
+            ->will($this->returnValue($id));
+
+        $this->assertEquals($id, $this->dashboardModel->getId());
+    }
+
+    public function testGetName()
+    {
+        $name = 'Name';
+        $this->dashboardEntity->expects($this->once())
+            ->method('getName')
+            ->will($this->returnValue($name));
+
+        $this->assertEquals($name, $this->dashboardModel->getName());
+    }
+
+    public function testAddWidget()
+    {
+        $widgetEntity = $this->getMock('Oro\Bundle\DashboardBundle\Entity\Widget');
+        $widgetModel = $this->getMockBuilder('Oro\Bundle\DashboardBundle\Model\WidgetModel')
             ->disableOriginalConstructor()
             ->getMock();
-        $dashboard = $this->getMock('Oro\Bundle\DashboardBundle\Entity\Dashboard');
-        $dashboard->expects($this->at(0))->method('getLabel')->will($this->returnValue(''));
-        $dashboard->expects($this->at(1))->method('getLabel')->will($this->returnValue($entityLabel));
-        $model = new DashboardModel($dashboard, $widgetsCollection, $expectedConfig);
 
-        $this->assertEquals($model->getConfig(), $expectedConfig);
-        $this->assertSame($model->getEntity(), $dashboard);
-        $this->assertSame($model->getWidgets(), $widgetsCollection);
-        $this->assertEquals($model->getLabel(), $configLabel);
-        $this->assertEquals($model->getLabel(), $entityLabel);
+        $widgetModel->expects($this->once())
+            ->method('getEntity')
+            ->will($this->returnValue($widgetEntity));
+
+        $this->dashboardEntity->expects($this->once())
+            ->method('addWidget')
+            ->with($widgetEntity);
+
+        $this->dashboardModel->addWidget($widgetModel);
+        $this->assertEquals($widgetModel, $this->widgets[2]);
+    }
+
+    /**
+     * @dataProvider addWidgetRecalculatePositionDataProvider
+     */
+    public function testAddWidgetRecalculatePosition(array $layoutPositions, array $expectedLayoutPosition)
+    {
+        $widgetEntity = $this->getMock('Oro\Bundle\DashboardBundle\Entity\Widget');
+        $widgetModel = $this->getMockBuilder('Oro\Bundle\DashboardBundle\Model\WidgetModel')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $widgetModel->expects($this->once())
+            ->method('getEntity')
+            ->will($this->returnValue($widgetEntity));
+
+        $widgetModel->expects($this->once())
+            ->method('setLayoutPosition')
+            ->with($expectedLayoutPosition);
+
+        $this->dashboardEntity->expects($this->once())
+            ->method('addWidget')
+            ->with($widgetEntity);
+
+        foreach ($layoutPositions as $index => $layoutPosition) {
+            $this->widgets[$index]->expects($this->once())
+                ->method('getLayoutPosition')
+                ->will($this->returnValue($layoutPosition));
+        }
+
+        $this->dashboardModel->addWidget($widgetModel, true);
+        $this->assertEquals($widgetModel, $this->widgets[2]);
+
+    }
+
+    public function addWidgetRecalculatePositionDataProvider()
+    {
+        return array(
+            array(
+                'layoutPositions' => array(
+                    array(0, 50),
+                    array(0, 100),
+                ),
+                'expectedLayoutPosition' => array(0, 0)
+            ),
+            array(
+                'layoutPositions' => array(
+                    array(1, -100),
+                    array(1, 100),
+                ),
+                'expectedLayoutPosition' => array(0, 0)
+            ),
+            array(
+                'layoutPositions' => array(
+                    array(0, -100),
+                    array(0, 100),
+                ),
+                'expectedLayoutPosition' => array(0, -101)
+            ),
+        );
+    }
+
+    public function testGetWidgetById()
+    {
+        $firstWidgetId = 100;
+        $secondWidgetId = 101;
+        $this->widgets[0]->expects($this->once())
+            ->method('getId')
+            ->will($this->returnValue($firstWidgetId));
+        $this->widgets[1]->expects($this->once())
+            ->method('getId')
+            ->will($this->returnValue($secondWidgetId));
+
+        $this->assertEquals($this->widgets[1], $this->dashboardModel->getWidgetById($secondWidgetId));
+    }
+
+    public function testHasWidget()
+    {
+        $widgetModel = $this->getMockBuilder('Oro\Bundle\DashboardBundle\Model\WidgetModel')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $widgetEntity = $this->getMock('Oro\Bundle\DashboardBundle\Entity\Widget');
+
+        $widgetModel->expects($this->once())
+            ->method('getEntity')
+            ->will($this->returnValue($widgetEntity));
+
+        $this->dashboardEntity->expects($this->once())
+            ->method('hasWidget')
+            ->with($widgetEntity)
+            ->will($this->returnValue(true));
+
+        $this->assertTrue($this->dashboardModel->hasWidget($widgetModel));
+    }
+
+    public function testGetLabelFromEntity()
+    {
+        $label = 'Label';
+        $this->dashboardEntity->expects($this->once())
+            ->method('getLabel')
+            ->will($this->returnValue($label));
+
+        $this->assertEquals($label, $this->dashboardModel->getLabel());
+    }
+
+    public function testGetLabelFromConfig()
+    {
+        $this->dashboardEntity->expects($this->once())
+            ->method('getLabel')
+            ->will($this->returnValue(null));
+
+        $this->assertEquals($this->config['label'], $this->dashboardModel->getLabel());
+    }
+
+    public function testGetTemplate()
+    {
+        $this->assertEquals(DashboardModel::DEFAULT_TEMPLATE, $this->dashboardModel->getTemplate());
     }
 }
