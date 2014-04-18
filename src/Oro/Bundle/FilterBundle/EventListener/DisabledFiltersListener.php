1c6144ec2e316173362a5a5f0b5ca45f2666c394
Dmytro Naidonov <dnaidonov@magecore.com>
2014-04-18 16:24:50 +0300

    CRM-1079 Improvements

--- src/Oro/Bundle/FilterBundle/EventListener/DisabledFiltersListener.php
+++ src/Oro/Bundle/FilterBundle/EventListener/DisabledFiltersListener.php
@@ -22,7 +22,6 @@ class DisabledFiltersListener
         $params = $event->getParameters();
 
         if (isset($params[self::PARAM_NAME]) && $params[self::PARAM_NAME]) {
-
             $config = $event->getConfig();
             $config->offsetUnsetByPath(Configuration::FILTERS_PATH);
             $config->offsetAddToArray('options', ['toolbarOptions'=>['addResetAction'=>false]]);
