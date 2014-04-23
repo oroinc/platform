9aa815adf972f775a704a578e2aab0c041cbcf2c
Ignat Shcheglovskyi <ischeglovskiy@magecore.com>
2014-04-23 20:38:24 +0300

    BAP-3230: Add chart configuration - added configuration and tests

--- src/Oro/Bundle/ChartBundle/Tests/Unit/DependencyInjection/ConfigurationTest.php
+++ src/Oro/Bundle/ChartBundle/Tests/Unit/DependencyInjection/ConfigurationTest.php
@@ -11,7 +11,7 @@ class ConfigurationTest extends \PHPUnit_Framework_TestCase
     public function testGetConfigTreeBuilder()
     {
         $configuration = new Configuration();
-        $builder       = $configuration->getConfigTreeBuilder();
+        $builder = $configuration->getConfigTreeBuilder();
 
         $this->assertInstanceOf('Symfony\Component\Config\Definition\Builder\TreeBuilder', $builder);
     }
@@ -22,7 +22,7 @@ class ConfigurationTest extends \PHPUnit_Framework_TestCase
     public function testProcessConfiguration($configs, $expected)
     {
         $configuration = new Configuration();
-        $processor     = new Processor();
+        $processor = new Processor();
         $this->assertEquals($expected, $processor->processConfiguration($configuration, $configs));
     }
 
@@ -30,8 +30,92 @@ class ConfigurationTest extends \PHPUnit_Framework_TestCase
     {
         return array(
             'empty' => array(
-                'configs'  => array(array()),
-                'expected' => array()
+                'configs'  => array(
+                    array(
+                        'foo_chart' => array(
+                            'label' => 'Foo',
+                            'data_schema' => array(
+                                array(
+                                    'name' => 'label',
+                                    'label' => 'Category (X axis)',
+                                    'required' => true,
+                                ),
+                                array(
+                                    'name' => 'value',
+                                    'label' => 'Value (Y axis)',
+                                    'required' => true,
+                                ),
+                            ),
+                            'settings_schema' => array(
+                                array(
+                                    'name' => 'connect_dots_with_line',
+                                    'label' => 'Connect line with dots',
+                                    'type' => 'boolean',
+                                ),
+                                array(
+                                    'name' => 'advanced_option',
+                                    'label' => 'Advanced option',
+                                    'type' => 'string',
+                                    'options' => array(
+                                        'foo' => 'bar'
+                                    )
+                                ),
+                            ),
+                            'data_transformer' => 'foo_data_transformer_service',
+                            'template' => 'FooTemplate.html.twig'
+                        )
+                    ),
+                    array(
+                        'bar_chart' => array(
+                            'label' => 'Bar',
+                            'template' => 'BarTemplate.html.twig'
+                        )
+                    ),
+                ),
+                'expected' => array(
+                    'foo_chart' => array(
+                        'label' => 'Foo',
+                        'data_schema' => array(
+                            array(
+                                'name' => 'label',
+                                'label' => 'Category (X axis)',
+                                'required' => true,
+                            ),
+                            array(
+                                'name' => 'value',
+                                'label' => 'Value (Y axis)',
+                                'required' => true,
+                            ),
+                        ),
+                        'settings_schema' => array(
+                            array(
+                                'name' => 'connect_dots_with_line',
+                                'label' => 'Connect line with dots',
+                                'type' => 'boolean',
+                                'options' => array(),
+                            ),
+                            array(
+                                'name' => 'advanced_option',
+                                'label' => 'Advanced option',
+                                'type' => 'string',
+                                'options' => array(
+                                    'foo' => 'bar'
+                                ),
+                            ),
+                        ),
+                        'default_settings' => array(),
+                        'data_transformer' => 'foo_data_transformer_service',
+                        'template' => 'FooTemplate.html.twig'
+                    ),
+                    'bar_chart' => array(
+                        'label' => 'Bar',
+                        'data_schema' => array(),
+                        'settings_schema' => array(),
+                        'default_settings' => array(),
+                        'data_transformer' => Configuration::DEFAULT_DATA_TRANSFORMER_SERVICE,
+                        'template' => 'BarTemplate.html.twig',
+                    ),
+                )
             )
         );
     }
