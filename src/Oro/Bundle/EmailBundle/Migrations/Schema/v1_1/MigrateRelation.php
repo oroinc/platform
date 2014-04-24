fbc7f2906bf01d964c4dccd8dd67985c2033632e
Ivan Shakuta <ishakuta@magecore.com>
2014-04-24 19:16:05 +0300

    CRM-1064: Filter duplicate emails on all Email related grids  - migrations

---
+++ src/Oro/Bundle/EmailBundle/Migrations/Schema/v1_1/MigrateRelation.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Oro\Bundle\EmailBundle\Migrations\Schema\v1_1;
+
+use Doctrine\DBAL\Schema\Schema;
+
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\OrderedMigrationInterface;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+
+class MigrateRelation implements Migration, OrderedMigrationInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        // remove old relation
+        $table = $schema->getTable('oro_email');
+        $table->dropIndex('fk_oro_email_folder_id');
+        $table->dropIndex('IDX_2A30C171162CB942');
+        $table->dropColumn('folder_id');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getOrder()
+    {
+        return 2;
+    }
+} 
