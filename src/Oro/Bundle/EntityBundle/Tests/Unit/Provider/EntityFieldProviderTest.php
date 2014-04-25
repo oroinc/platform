4b78a865eaa60b58f45c9429d11c0cd1a81b7861
Alexander Nezdoiminoga <alex@magecore.com>
2014-04-25 20:45:28 +0300

    BAP-3965: unit tests

--- src/Oro/Bundle/EntityBundle/Tests/Unit/Provider/EntityFieldProviderTest.php
+++ src/Oro/Bundle/EntityBundle/Tests/Unit/Provider/EntityFieldProviderTest.php
@@ -419,7 +419,7 @@ class EntityFieldProviderTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($expected, $result);
     }
 
-    public function testGetFieldsWithRelationsAndWithUnidirectional()
+    public function testGetFieldsWithRelationsAndDeepLevelAndWithUnidirectional()
     {
         $this->prepareWithRelations();
 
@@ -427,70 +427,56 @@ class EntityFieldProviderTest extends \PHPUnit_Framework_TestCase
             ->method('getIds')
             ->will($this->returnValue([new EntityConfigId('entity', 'Acme\\Entity\\Test22')]));
 
-        $result   = $this->provider->getFields('Acme:Test', true, true, true, 1, true, false);
-
+        $result   = $this->provider->getFields('Acme:Test1', true, false, true, 1, false, false);
         $expected = [
             [
-                'name'  => 'field3',
+                'name'  => 'Test1field2',
                 'type'  => 'string',
-                'label' => 'A',
+                'label' => 'A'
             ],
             [
-                'name'  => 'field2',
-                'type'  => 'string',
-                'label' => 'B',
-            ],
-            [
-                'name'       => 'field1',
+                'name'       => 'id',
                 'type'       => 'integer',
-                'label'      => 'C',
+                'label'      => 'B',
                 'identifier' => true
             ],
             [
-                'name'  => 'field4',
-                'type'  => 'string',
-                'label' => 'field4',
+                'name' => 'rel1',
+                'type' => 'integer',
+                'label' => 'Rel11',
+                'relation_type' => 'ref-one',
+                'related_entity_name' => 'Acme\Entity\Test11',
+                'related_entity_fields' => [
+                    [
+                        'name' => 'Test11field2',
+                        'type' => 'string',
+                        'label' => 'A',
+                    ],
+                    [
+                        'name' => 'id',
+                        'type' => 'integer',
+                        'label' => 'B',
+                        'identifier' => 1
+                    ]
+                ]
             ],
             [
-                'name'                        => 'rel1',
-                'type'                        => 'integer',
-                'label'                       => 'Rel1',
-                'relation_type'               => 'ref-many',
-                'related_entity_name'         => 'Acme\Entity\Test1',
-                'related_entity_label'        => 'Test1 Label',
-                'related_entity_plural_label' => 'Test1 Plural Label',
-                'related_entity_icon'         => 'icon-test1',
-                'related_entity_fields'       => [
+                'name' => 'Acme\Entity\Test22::Test22field1',
+                'type' => 'ref-one',
+                'label' => 'Test22 Label (A)',
+                'relation_type'         => 'ref-one',
+                'related_entity_name'   => 'Acme\Entity\Test22',
+                'related_entity_fields' => [
                     [
-                        'name'  => 'Test1field2',
-                        'type'  => 'string',
+                        'name'  => 'Test22field1',
+                        'type'  => 'ref-one',
                         'label' => 'A'
                     ],
                     [
-                        'name'       => 'id',
-                        'type'       => 'integer',
-                        'label'      => 'B',
-                        'identifier' => true
-                    ],
-                    [
-                        'name' => 'rel1',
+                        'name' => 'id',
                         'type' => 'integer',
-                        'label' => 'Rel11',
-                        'relation_type' => 'ref-one',
-                        'related_entity_name' => 'Acme\Entity\Test11',
-                        'related_entity_label' => 'Test11 Label',
-                        'related_entity_plural_label' => 'Test11 Plural Label',
-                        'related_entity_icon' => 'icon-test11'
-                    ],
-                    [
-                        'name' => 'Acme\Entity\Test22::Test22field1',
-                        'type' => 'ref-one',
-                        'label' => 'Test22 Label (A)',
-                        'relation_type'               => 'ref-one',
-                        'related_entity_name'         => 'Acme\Entity\Test22',
-                        'related_entity_label'        => 'Test22 Label',
-                        'related_entity_plural_label' => 'Test22 Plural Label',
-                        'related_entity_icon'         => 'icon-test22',
+                        'label' => 'B',
+                        'identifier' => 1
                     ]
                 ]
             ]
