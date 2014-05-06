4d45db8ba188bdc93c63690bf2f98c96edb74fd0
Vova Soroka <vsoroka@magecore.com>
2014-05-07 01:43:40 +0300

    BAP-4143: Possible to define extended fields names with sql keywords

--- src/Oro/Bundle/EntityExtendBundle/Validator/Constraints/NotPhpKeyword.php
+++ src/Oro/Bundle/EntityExtendBundle/Validator/Constraints/NotPhpKeyword.php
@@ -4,6 +4,9 @@ namespace Oro\Bundle\EntityExtendBundle\Validator\Constraints;
 
 use Symfony\Component\Validator\Constraint;
 
+/**
+ * @Annotation
+ */
 class NotPhpKeyword extends Constraint
 {
     public $message = "This value should not be the reserved PHP word.";
--- src/Oro/Bundle/EntityExtendBundle/Validator/Constraints/NotSqlKeyword.php
+++ src/Oro/Bundle/EntityExtendBundle/Validator/Constraints/NotSqlKeyword.php
@@ -4,6 +4,9 @@ namespace Oro\Bundle\EntityExtendBundle\Validator\Constraints;
 
 use Symfony\Component\Validator\Constraint;
 
+/**
+ * @Annotation
+ */
 class NotSqlKeyword extends Constraint
 {
     public $message = "This value should not be the reserved SQL word.";
