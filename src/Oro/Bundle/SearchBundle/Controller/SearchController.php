f62343f416852b9c0e136fd447305fcf5fc6a23a
Alexandr <avreliy.89@gmail.com>
2014-05-13 17:50:19 +0300

    OC-261: Global Search

--- src/Oro/Bundle/SearchBundle/Controller/SearchController.php
+++ src/Oro/Bundle/SearchBundle/Controller/SearchController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
+use Oro\Bundle\EntityConfigBundle\Config\Id\EntityConfigId;
 use Oro\Bundle\SearchBundle\Provider\ResultStatisticsProvider;
 use Oro\Bundle\SecurityBundle\Annotation\Acl;
 use Oro\Bundle\SecurityBundle\Annotation\AclAncestor;
@@ -71,7 +72,40 @@ class SearchController extends Controller
         return array(
             'from'           => $from,
             'searchString'   => $string,
-            'groupedResults' => $resultProvider->getGroupedResults($string),
+            'groupedResults' => $this->sortResultGroups($resultProvider->getGroupedResults($string)),
         );
+    }
+
+    protected function sortResultGroups(array $results)
+    {
+        $entityConfigManager = $this->get('oro_entity_config.config_manager');
+        $translator = $this->get('translator');
+        foreach ($results as &$result) {
+            $result['label'] = '';
+            $result['icon'] = '';
+            if (!empty($result['class']) && $entityConfigManager->hasConfig($result['class'])) {
+                $entityConfigId = new EntityConfigId('entity', $result['class']);
+                $entityConfig = $entityConfigManager->getConfig($entityConfigId);
+                if ($entityConfig->has('plural_label')) {
+                    $result['label'] = $translator->trans($entityConfig->get('plural_label'));
+                }
+                if ($entityConfig->has('icon')) {
+                    $result['icon'] = $entityConfig->get('icon');
+                }
+            }
+        }
+
+        uasort(
+            $results,
+            function ($first, $second) {
+                if ($first['label'] == $second['label']) {
+                    return 0;
+                }
+
+                return $first['label'] > $second['label'] ? 1 : -1;
+            }
+        );
+
+        return $results;
     }
 }
