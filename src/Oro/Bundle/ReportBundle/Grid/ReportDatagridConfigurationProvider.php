3792d40636929bdbaf0abd694d23fd59d37a17f6
Alexandr <avreliy.89@gmail.com>
2014-05-14 18:49:20 +0300

    OC-264: Result Report - without tests

--- src/Oro/Bundle/ReportBundle/Grid/ReportDatagridConfigurationProvider.php
+++ src/Oro/Bundle/ReportBundle/Grid/ReportDatagridConfigurationProvider.php
@@ -9,6 +9,7 @@ use Oro\Bundle\DataGridBundle\Datagrid\Common\DatagridConfiguration;
 use Oro\Bundle\QueryDesignerBundle\QueryDesigner\FunctionProviderInterface;
 use Oro\Bundle\QueryDesignerBundle\QueryDesigner\VirtualFieldProviderInterface;
 use Oro\Bundle\QueryDesignerBundle\Exception\InvalidConfigurationException;
+use Oro\Bundle\EntityConfigBundle\Config\ConfigManager;
 
 class ReportDatagridConfigurationProvider implements ConfigurationProviderInterface
 {
@@ -35,20 +36,28 @@ class ReportDatagridConfigurationProvider implements ConfigurationProviderInterf
     private $configuration = null;
 
     /**
+     * @var ConfigManager
+     */
+    protected $configManager;
+
+    /**
      * Constructor
      *
      * @param FunctionProviderInterface     $functionProvider
      * @param VirtualFieldProviderInterface $virtualFieldProvider ,
      * @param ManagerRegistry               $doctrine
+     * @param ConfigManager $configManager
      */
     public function __construct(
         FunctionProviderInterface $functionProvider,
         VirtualFieldProviderInterface $virtualFieldProvider,
-        ManagerRegistry $doctrine
+        ManagerRegistry $doctrine,
+        ConfigManager $configManager
     ) {
         $this->functionProvider     = $functionProvider;
         $this->virtualFieldProvider = $virtualFieldProvider;
         $this->doctrine             = $doctrine;
+        $this->configManager = $configManager;
     }
 
     /**
@@ -77,9 +86,65 @@ class ReportDatagridConfigurationProvider implements ConfigurationProviderInterf
             );
 
             $this->configuration = $builder->getConfiguration();
+
+            $this->addViewActionToParams($report->getEntity());
         }
 
         return $this->configuration;
+    }
+
+    protected function addViewActionToParams($className)
+    {
+        $metadata = $this->configManager->getEntityMetadata($className);
+
+        if (!$metadata || empty($metadata->routeView)) {
+            return;
+        }
+
+        $fromPart = $this->configuration->offsetGetByPath('[source][query][from]');
+
+        $entityAlias = null;
+        $doctrineMetadata = $this->doctrine->getManager()->getClassMetadata($className);
+        $identifiers = $doctrineMetadata->getIdentifier();
+        $pkName = array_shift($identifiers);
+
+        foreach ($fromPart as $piece) {
+            if ($piece['table'] == $className) {
+                $entityAlias = $piece['alias'];
+            }
+        }
+
+        if (!$entityAlias || $pkName === null) {
+            return;
+        }
+
+        $viewAction = array(
+            'view' => array(
+                'type'         => 'navigate',
+                'label'        => 'View',
+                'icon'         => 'eye-open',
+                'link'         => 'view_link',
+                'rowAction'    => true
+            )
+        );
+
+        $idName = uniqid('id_');
+
+        $properties = array(
+            $idName        => array(),
+            'view_link' => array(
+                'type'   => 'url',
+                'route'  => $metadata->routeView,
+                'params' => array('id' => $idName)
+            )
+        );
+
+        $this->configuration->offsetAddToArrayByPath(
+            '[source][query][select]',
+            array("{$entityAlias}.{$pkName} as {$idName}")
+        );
+        $this->configuration->offsetAddToArrayByPath('[properties]', $properties);
+        $this->configuration->offsetAddToArrayByPath('[actions]', $viewAction);
     }
 
     /**
