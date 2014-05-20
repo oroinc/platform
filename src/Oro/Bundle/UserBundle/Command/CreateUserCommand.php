ac9585406237a55a20ad67f45920ba71203f0541
Sergey Zhuravel <zhuravel.sergey@gmail.com>
2014-05-20 19:25:16 +0300

    OC-271: Duplicate email address user creation - refactor create and update user command

--- src/Oro/Bundle/UserBundle/Command/CreateUserCommand.php
+++ src/Oro/Bundle/UserBundle/Command/CreateUserCommand.php
@@ -2,56 +2,131 @@
 
 namespace Oro\Bundle\UserBundle\Command;
 
+use Doctrine\DBAL\DBALException;
+use Doctrine\ORM\EntityManagerInterface;
+
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
+use Oro\Bundle\UserBundle\Entity\User;
+use Oro\Bundle\UserBundle\Entity\UserManager;
+use Oro\Bundle\UserBundle\Exception\InvalidArgumentException;
+
 class CreateUserCommand extends ContainerAwareCommand
 {
+    /**
+     * @var UserManager
+     */
+    protected $userManager;
+
+    /**
+     * @var EntityManagerInterface
+     */
+    protected $entityManager;
+
+    /**
+     * {@inheritdoc}
+     */
     protected function configure()
     {
         $this
             ->setName('oro:user:create')
             ->setDescription('Create user.')
-            ->addOption('user-name', null, InputOption::VALUE_REQUIRED, 'User name')
-            ->addOption('user-email', null, InputOption::VALUE_REQUIRED, 'User email')
-            ->addOption('user-firstname', null, InputOption::VALUE_REQUIRED, 'User first name')
-            ->addOption('user-lastname', null, InputOption::VALUE_REQUIRED, 'User last name')
-            ->addOption('user-password', null, InputOption::VALUE_REQUIRED, 'User password')
-            ->addOption('user-role', null, InputOption::VALUE_REQUIRED, 'User role')
-            ->addOption('user-business-unit', null, InputOption::VALUE_REQUIRED, 'User business unit');
+            ->addOption('role', null, InputOption::VALUE_REQUIRED, 'User role')
+            ->addOption('business-unit', null, InputOption::VALUE_REQUIRED, 'User business unit')
+            ->addOption('username', null, InputOption::VALUE_REQUIRED, 'User name')
+            ->addOption('email', null, InputOption::VALUE_REQUIRED, 'User email')
+            ->addOption('firstname', null, InputOption::VALUE_REQUIRED, 'User first name')
+            ->addOption('lastname', null, InputOption::VALUE_REQUIRED, 'User last name')
+            ->addOption('plain-password', null, InputOption::VALUE_REQUIRED, 'User password');
     }
 
     /**
-     * @param InputInterface $input
-     * @param OutputInterface $output
-     * @throws \InvalidArgumentException
+     * {@inheritdoc}
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $userManager = $this->getContainer()->get('oro_user.manager');
-        $entityManager = $this->getContainer()->get('doctrine.orm.entity_manager');
-
-        $user = $userManager->createUser();
+        $user = $this->getUserManager()->createUser();
+        $user->setEnabled(true);
 
         $options = $input->getOptions();
-        $role = $entityManager->getRepository('OroUserBundle:Role')->findOneBy(array('role' => $options['user-role']));
 
-        $businessUnit = $entityManager->getRepository('OroOrganizationBundle:BusinessUnit')
-            ->findOneBy(array('name' => $options['user-business-unit']));
+        try {
+            $this->updateUser($user, $options);
+        } catch (InvalidArgumentException $exception) {
+            $output->writeln($exception->getMessage());
+        } catch (DBALException $exception) {
+            $output->writeln('User exists');
+        }
+    }
 
-        $user
-            ->setUsername($options['user-name'])
-            ->setEmail($options['user-email'])
-            ->setFirstName($options['user-firstname'])
-            ->setLastName($options['user-lastname'])
-            ->setPlainPassword($options['user-password'])
-            ->setEnabled(true)
-            ->addRole($role)
-            ->setOwner($businessUnit)
-            ->addBusinessUnit($businessUnit);
+    /**
+     * @param User  $user
+     * @param array $options
+     * @throws InvalidArgumentException
+     */
+    protected function updateUser(User $user, $options)
+    {
+        if (!empty($options['role'])) {
+            $role = $this->getEntityManager()
+                ->getRepository('OroUserBundle:Role')
+                ->findOneBy(array('role' => $options['role']));
 
-        $userManager->updateUser($user);
+            if (!$role) {
+                throw new InvalidArgumentException('Invalid Role');
+            }
+
+            $user->addRole($role);
+        }
+
+        if (!empty($options['business-unit'])) {
+            $businessUnit = $this->getEntityManager()
+                ->getRepository('OroOrganizationBundle:BusinessUnit')
+                ->findOneBy(array('name' => $options['business-unit']));
+
+            if (!$businessUnit) {
+                throw new InvalidArgumentException('Invalid Business Unit');
+            }
+
+            $user
+                ->setOwner($businessUnit)
+                ->addBusinessUnit($businessUnit);
+        }
+
+        $properties = ['name', 'email', 'firstname', 'lastname', 'plain-password'];
+
+        foreach ($properties as $property) {
+            if (!empty($options[$property])) {
+                $user->{'set' . str_replace('-', '', $property)}($options[$property]);
+            }
+        }
+
+        $this->getUserManager()->updateUser($user);
+    }
+
+    /**
+     * @return UserManager
+     */
+    protected function getUserManager()
+    {
+        if (!$this->userManager) {
+            $this->userManager = $this->getContainer()->get('oro_user.manager');
+        }
+
+        return $this->userManager;
+    }
+
+    /**
+     * @return EntityManagerInterface
+     */
+    protected function getEntityManager()
+    {
+        if (!$this->entityManager) {
+            $this->entityManager = $this->getContainer()->get('doctrine.orm.entity_manager');
+        }
+
+        return $this->entityManager;
     }
 }
--- src/Oro/Bundle/UserBundle/Command/UpdateUserCommand.php
+++ src/Oro/Bundle/UserBundle/Command/UpdateUserCommand.php
@@ -2,54 +2,52 @@
 
 namespace Oro\Bundle\UserBundle\Command;
 
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class UpdateUserCommand extends ContainerAwareCommand
+use Oro\Bundle\UserBundle\Exception\InvalidArgumentException;
+
+class UpdateUserCommand extends CreateUserCommand
 {
+    /**
+     * {@inheritdoc}
+     */
     protected function configure()
     {
         $this
             ->setName('oro:user:update')
             ->setDescription('Update user.')
             ->addArgument(
-                'user-name',
+                'username',
                 InputArgument::OPTIONAL,
                 'Username of user to update'
             )
-            ->addOption('user-name', null, InputOption::VALUE_REQUIRED, 'User name')
-            ->addOption('user-email', null, InputOption::VALUE_REQUIRED, 'User email')
-            ->addOption('user-firstname', null, InputOption::VALUE_REQUIRED, 'User first name')
-            ->addOption('user-lastname', null, InputOption::VALUE_REQUIRED, 'User last name')
-            ->addOption('user-password', null, InputOption::VALUE_REQUIRED, 'User password');
+            ->addOption('username', null, InputOption::VALUE_REQUIRED, 'User name')
+            ->addOption('email', null, InputOption::VALUE_REQUIRED, 'User email')
+            ->addOption('firstname', null, InputOption::VALUE_REQUIRED, 'User first name')
+            ->addOption('lastname', null, InputOption::VALUE_REQUIRED, 'User last name')
+            ->addOption('plain-password', null, InputOption::VALUE_REQUIRED, 'User password');
     }
 
     /**
-     * @param InputInterface $input
-     * @param OutputInterface $output
-     * @throws \InvalidArgumentException
+     * {@inheritdoc}
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $username = $input->getArgument('user-name');
-        $userManager = $this->getContainer()->get('oro_user.manager');
-        $user = $userManager->findUserByUsername($username);
+        $username = $input->getArgument('username');
+        $user     = $this->getUserManager()->findUserByUsername($username);
+        $options  = $input->getOptions();
 
         if (!$user) {
             throw new \InvalidArgumentException(sprintf('User "%s" not found.', $username));
         }
 
-        $options = $input->getOptions();
-        $user
-            ->setUsername($options['user-name'])
-            ->setEmail($options['user-email'])
-            ->setFirstName($options['user-firstname'])
-            ->setLastName($options['user-lastname'])
-            ->setPlainPassword($options['user-password']);
-
-        $userManager->updateUser($user);
+        try {
+            $this->updateUser($user, $options);
+        } catch (InvalidArgumentException $exception) {
+            $output->writeln($exception->getMessage());
+        }
     }
 }
