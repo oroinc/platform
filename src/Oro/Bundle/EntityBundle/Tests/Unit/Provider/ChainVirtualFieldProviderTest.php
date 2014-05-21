42e4cddc0d42af3f3af0150b79ce58da379d7506
Alexander Nezdoiminoga <alex@magecore.com>
2014-05-21 23:46:03 +0300

    BAP-4172:  ChainVirtualFieldProvider unit test

--- src/Oro/Bundle/EntityBundle/Tests/Unit/Provider/ChainVirtualFieldProviderTest.php
+++ src/Oro/Bundle/EntityBundle/Tests/Unit/Provider/ChainVirtualFieldProviderTest.php
@@ -21,56 +21,76 @@ class ChainVirtualFieldProviderTest extends \PHPUnit_Framework_TestCase
     {
         $this->assertAttributeEquals([], 'providers', $this->chainVirtualFieldProvider);
 
+        $this->addProviders();
+
+        $this->assertAttributeCount(2, 'providers', $this->chainVirtualFieldProvider);
         $this->assertAttributeEquals($this->providers, 'providers', $this->chainVirtualFieldProvider);
     }
 
     public function testIsVirtualField()
     {
+        $this->addProviders();
 
+        $this->providers[0]
+            ->expects($this->at(0))
+            ->method('isVirtualField')
+            ->with('testClass', 'testField')
+            ->will($this->returnValue(true));
+        $this->providers[0]
+            ->expects($this->exactly(2))
+            ->method('isVirtualField');
+        $this->providers[1]
+            ->expects($this->exactly(1))
+            ->method('isVirtualField');
+
+
+        $this->assertTrue($this->chainVirtualFieldProvider->isVirtualField('testClass', 'testField'));
+        $this->assertFalse($this->chainVirtualFieldProvider->isVirtualField('testClass', 'testField2'));
     }
 
     protected function addProviders()
     {
-        $provider1 = $this->getMock('Oro\Bundle\EntityBundle\Provider\VirtualFieldProviderInterface');
-        $provider1
-            ->expects($this->any())
-            ->method('getVirtualFields')
-            ->with('testClass')
-            ->will($this->returnValue(['testField']));
-        $provider1
-            ->expects($this->any())
-            ->method('isVirtualField')
-            ->with('testClass', 'testField')
-            ->will($this->returnValue(true));
-        $provider1
-            ->expects($this->any())
-            ->method('getVirtualFieldQuery')
-            ->with('testClass', 'testField')
-            ->will(
-                $this->returnValue(
-                    [
-                        'select' => [
-                            'expr' => 'country.name',
-                            'return_type' => 'string'
-                        ],
-                        'join' => [
-                            'left' => [
-                                [
-                                    'join' => 'entity.country',
-                                    'alias' => 'country'
-                                ]
-                            ]
-                        ]
-                    ]
-                )
-            );
+        $provider1 = $this->getMockBuilder('Oro\Bundle\EntityBundle\Provider\VirtualFieldProviderInterface')
+            ->setMockClassName('VirtualFieldProvider1')
+            ->getMock();
 
-        $provider2 = $this->getMock('Oro\Bundle\EntityBundle\Provider\VirtualFieldProviderInterface');
-        $provider2
-            ->expects($this->any())
-            ->method('getVirtualFields')
-            ->with('testClass')
-            ->will($this->returnValue([]));
+//        $provider1
+//            ->expects($this->any())
+//            ->method('getVirtualFields')
+//            ->with('testClass')
+//            ->will($this->returnValue(['testField']));
+//
+//        $provider1
+//            ->expects($this->any())
+//            ->method('getVirtualFieldQuery')
+//            ->with('testClass', 'testField')
+//            ->will(
+//                $this->returnValue(
+//                    [
+//                        'select' => [
+//                            'expr' => 'country.name',
+//                            'return_type' => 'string'
+//                        ],
+//                        'join' => [
+//                            'left' => [
+//                                [
+//                                    'join' => 'entity.country',
+//                                    'alias' => 'country'
+//                                ]
+//                            ]
+//                        ]
+//                    ]
+//                )
+//            );
+
+        $provider2 = $this->getMockBuilder('Oro\Bundle\EntityBundle\Provider\VirtualFieldProviderInterface')
+            ->setMockClassName('VirtualFieldProvider2')
+            ->getMock();
+//        $provider2
+//            ->expects($this->any())
+//            ->method('getVirtualFields')
+//            ->with('testClass')
+//            ->will($this->returnValue(['testField2']));
 
         $providers = $this->providers = [$provider1, $provider2];
         foreach ($providers as $provider) {
