5180c6b6cc673971af6084557f55a6989bcd1781
Vova Soroka <vsoroka@magecore.com>
2014-05-22 04:32:19 +0300

    BAP-4172: Provide an easy way to use Country and States as filter

--- src/Oro/Bundle/WorkflowBundle/Field/FieldProvider.php
+++ src/Oro/Bundle/WorkflowBundle/Field/FieldProvider.php
@@ -5,13 +5,7 @@ namespace Oro\Bundle\WorkflowBundle\Field;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Mapping\ClassMetadata;
 
-use Symfony\Bridge\Doctrine\ManagerRegistry;
-use Symfony\Component\Translation\Translator;
-
-use Oro\Bundle\EntityBundle\ORM\EntityClassResolver;
 use Oro\Bundle\EntityBundle\Provider\EntityFieldProvider;
-use Oro\Bundle\EntityBundle\Provider\VirtualFieldProviderInterface;
-use Oro\Bundle\EntityConfigBundle\Provider\ConfigProvider;
 
 class FieldProvider extends EntityFieldProvider
 {
@@ -22,43 +16,6 @@ class FieldProvider extends EntityFieldProvider
         FieldGenerator::PROPERTY_WORKFLOW_ITEM,
         FieldGenerator::PROPERTY_WORKFLOW_STEP,
     );
-
-    /** @var ConfigProvider */
-    protected $groupingConfigProvider;
-
-    /**
-     * Constructor
-     *
-     * @param ConfigProvider                $entityConfigProvider
-     * @param ConfigProvider                $extendConfigProvider
-     * @param EntityClassResolver           $entityClassResolver
-     * @param ManagerRegistry               $doctrine
-     * @param Translator                    $translator
-     * @param VirtualFieldProviderInterface $virtualFieldProvider
-     * @param array                         $hiddenFields
-     * @param ConfigProvider                $groupingConfigProvider
-     */
-    public function __construct(
-        ConfigProvider $entityConfigProvider,
-        ConfigProvider $extendConfigProvider,
-        EntityClassResolver $entityClassResolver,
-        ManagerRegistry $doctrine,
-        Translator $translator,
-        VirtualFieldProviderInterface $virtualFieldProvider,
-        $hiddenFields,
-        ConfigProvider $groupingConfigProvider
-    ) {
-        parent::__construct(
-            $entityConfigProvider,
-            $extendConfigProvider,
-            $entityClassResolver,
-            $doctrine,
-            $translator,
-            $virtualFieldProvider,
-            $hiddenFields
-        );
-        $this->groupingConfigProvider = $groupingConfigProvider;
-    }
 
     /**
      * @param string $field
@@ -107,30 +64,10 @@ class FieldProvider extends EntityFieldProvider
         // skip workflow and collection relations
         if ($this->isWorkflowField($associationName)
             || !$metadata->isSingleValuedAssociation($associationName)
-            || $this->isDictionary($metadata->getAssociationTargetClass($associationName))
         ) {
             return true;
         }
 
         return parent::isIgnoredRelation($metadata, $associationName);
-    }
-
-    /**
-     * Indicates whether the entity is the dictionary one or not
-     *
-     * @param string $className
-     * @return bool
-     */
-    protected function isDictionary($className)
-    {
-        $result = false;
-        if ($this->groupingConfigProvider->hasConfig($className)) {
-            $groups = $this->groupingConfigProvider->getConfig($className)->get('groups');
-            if (!empty($groups) && in_array('dictionary', $groups)) {
-                $result = true;
-            }
-        }
-
-        return $result;
     }
 }
