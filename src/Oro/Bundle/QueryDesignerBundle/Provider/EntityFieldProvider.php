1affa14dafc40b253562253dea16394b43984ddf
Vova Soroka <vsoroka@magecore.com>
2014-05-23 15:30:17 +0300

    BAP-4172: Provide an easy way to use Country and States as filter

--- src/Oro/Bundle/QueryDesignerBundle/Provider/EntityFieldProvider.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Oro\Bundle\QueryDesignerBundle\Provider;
-
-use Oro\Bundle\EntityBundle\Provider\EntityFieldProvider as BaseEntityFieldProvider;
-
-class EntityFieldProvider extends BaseEntityFieldProvider
-{
-}
