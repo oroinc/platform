a9adb9bb6f08156af8597d836316b05253f8ccbe
Alexandr Smaga <asmaga@magecore.com>
2014-05-30 11:38:23 +0300

    CRM-1216: Add owner to Contact and Account entities when sync through the channel    - added owner helper    - added tests for owner helper    - code style fixes

---
+++ src/Oro/Bundle/IntegrationBundle/ImportExport/Helper/DefaultOwnerHelper.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Oro\Bundle\IntegrationBundle\ImportExport\Helper;
+
+use Doctrine\ORM\EntityManager;
+
+use Oro\Bundle\IntegrationBundle\Entity\Channel;
+use Oro\Bundle\SecurityBundle\Owner\Metadata\OwnershipMetadata;
+use Oro\Bundle\SecurityBundle\Owner\Metadata\OwnershipMetadataProvider;
+
+class DefaultOwnerHelper
+{
+    /** @var EntityManager */
+    protected $em;
+
+    /** @var OwnershipMetadataProvider */
+    protected $ownershipMetadataProvider;
+
+    public function __construct(EntityManager $em, OwnershipMetadataProvider $ownershipMetadataProvider)
+    {
+        $this->em                        = $em;
+        $this->ownershipMetadataProvider = $ownershipMetadataProvider;
+    }
+
+    /**
+     * Populate owner to target entity based on channel configuration and entity's ownership type
+     *
+     * @param object  $entity
+     * @param Channel $channel
+     */
+    public function populateChannelOwner($entity, Channel $channel)
+    {
+        $defaultUserOwner = $channel->getDefaultUserOwner();
+
+        $doctrineMetadata  = $this->em->getClassMetadata($entity);
+        $ownershipMetadata = $this->getMetadata($doctrineMetadata->name);
+
+        if ($defaultUserOwner && $ownershipMetadata->isUserOwned()) {
+            $doctrineMetadata->setFieldValue($entity, $ownershipMetadata->getOwnerFieldName(), $defaultUserOwner);
+        }
+    }
+
+    /**
+     * Get metadata for entity
+     *
+     * @param string $entityFQCN
+     *
+     * @return OwnershipMetadata
+     */
+    protected function getMetadata($entityFQCN)
+    {
+        return $this->ownershipMetadataProvider->getMetadata($entityFQCN);
+    }
+}
