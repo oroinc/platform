3aec6e8d069556fab208d45a9dceb2350163566c
Vova Soroka <vsoroka@magecore.com>
2014-05-30 18:07:32 +0300

    BAP-4259: Notes for entities. BAP-4274: Create Controller and TWIG template to render notes

---
+++ src/Oro/Bundle/NoteBundle/Controller/NoteController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Oro\Bundle\NoteBundle\Controller;
+
+use Oro\Bundle\EntityBundle\Exception\NotManageableEntityException;
+use Oro\Bundle\EntityBundle\ORM\DoctrineHelper;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+
+use Oro\Bundle\SecurityBundle\Annotation\Acl;
+use Oro\Bundle\SecurityBundle\Annotation\AclAncestor;
+
+use Oro\Bundle\NoteBundle\Entity\Note;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+
+/**
+ * @Route("/note")
+ */
+class NoteController extends Controller
+{
+    /**
+     * @Route(
+     *      "/widget/associates_notes/{entityClass}/{entityId}",
+     *      name="oro_note_widget_associated_notes"
+     * )
+     * @AclAncestor("oro_note_view")
+     * @Template
+     */
+    public function associatedNotesAction($entityClass, $entityId)
+    {
+        /** @var DoctrineHelper $doctrineHelper */
+        $doctrineHelper = $this->get('oro_entity.doctrine_helper');
+
+        $entity = null;
+        try {
+            $entity = $doctrineHelper->getEntity($entityClass, $entityId);
+        } catch (NotManageableEntityException $e) {
+            throw new BadRequestHttpException($e->getMessage(), $e);
+        }
+        if (!$entity) {
+            throw $this->createNotFoundException();
+        }
+
+        return array('entity' => $entity);
+    }
+}
