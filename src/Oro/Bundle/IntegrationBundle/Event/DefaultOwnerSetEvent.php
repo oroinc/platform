6973e0d3f3857e52e1e0a6c53fdcdf0e28bbb585
Alexandr Smaga <asmaga@magecore.com>
2014-05-31 12:46:11 +0000

    CRM-1227: Add set owner for existing data    - minor fixes    - imrpove functional test abstract test case

--- src/Oro/Bundle/IntegrationBundle/Event/DefaultOwnerSetEvent.php
+++ src/Oro/Bundle/IntegrationBundle/Event/DefaultOwnerSetEvent.php
@@ -7,6 +7,12 @@ use Symfony\Component\EventDispatcher\Event;
 use Oro\Bundle\UserBundle\Entity\User;
 use Oro\Bundle\IntegrationBundle\Entity\Channel;
 
+/**
+ * This event is dispatched default owner set for existing channel instance.
+ * It's aimed to handle situations when channel where created before 1.3(when channel did not have default owner)
+ *
+ * @package Oro\Bundle\IntegrationBundle\Event
+ */
 class DefaultOwnerSetEvent extends Event
 {
     const NAME = 'oro_integration.default_owner.set';
