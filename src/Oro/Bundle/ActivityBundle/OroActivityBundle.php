aaefda58f12dc3d83eef3d06618ac85c308b1de7
Ivan Shakuta <ishakuta@magecore.com>
2014-06-11 13:56:34 +0300

    BAP-4205: Create Activity bundle sceleton - bundle sceleton

---
+++ src/Oro/Bundle/ActivityBundle/OroActivityBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Oro\Bundle\ActivityBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class OroActivityBundle extends Bundle
+{
+
+}
---
+++ src/Oro/Bundle/ActivityBundle/README.md
@@ -0,0 +1,4 @@
+OroActivityBundle
+===================
+
+The `OroActivityBundle` provide ability to assign activities (calls, emails, tasks) to other entities.
