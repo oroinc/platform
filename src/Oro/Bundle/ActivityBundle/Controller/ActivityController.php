7dd606042c700ade9eb2337a48845a6ecbda83c0
Vova Soroka <vsoroka@magecore.com>
2014-06-11 14:49:43 +0300

    BAP-4206: Convert Email to extend entity

---
+++ src/Oro/Bundle/ActivityBundle/Controller/ActivityController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Oro\Bundle\NoteBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * @Route("/activities")
+ */
+class NoteController extends Controller
+{
+}
---
+++ src/Oro/Bundle/EmailBundle/Model/ExtendEmail.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Oro\Bundle\EmailBundle\Model;
+
+class ExtendEmail
+{
+}
