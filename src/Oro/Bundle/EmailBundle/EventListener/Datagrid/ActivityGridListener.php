d0298ccee53c14848a9783c3521aba8800e6db31
Vova Soroka <vsoroka@magecore.com>
2014-06-16 13:19:37 +0300

    BAP-4481: Create assigned emails grid

---
+++ src/Oro/Bundle/EmailBundle/EventListener/Datagrid/ActivityGridListener.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Oro\Bundle\EmailBundle\EventListener\Datagrid;
+
+use Oro\Bundle\DataGridBundle\Event\BuildAfter;
+
+class ActivityGridListener
+{
+    public function onBuildAfter(BuildAfter $event)
+    {
+
+    }
+}
