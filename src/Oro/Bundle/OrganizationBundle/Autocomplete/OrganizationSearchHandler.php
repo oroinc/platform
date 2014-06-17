7249124aea3f5af263c4d98df6177002e44eb2b5
Dmitry Naydenov <dnaidonov@magecore.com>
2014-06-17 16:18:38 +0300

    CRM-1314 Add organization field remove remarks

--- src/Oro/Bundle/OrganizationBundle/Autocomplete/OrganizationSearchHandler.php
+++ src/Oro/Bundle/OrganizationBundle/Autocomplete/OrganizationSearchHandler.php
@@ -2,25 +2,16 @@
 
 namespace Oro\Bundle\OrganizationBundle\Autocomplete;
 
-use Liip\ImagineBundle\Imagine\Cache\CacheManager;
+use Oro\Bundle\FormBundle\Autocomplete\SearchHandler;
 
-use Oro\Bundle\FormBundle\Autocomplete\FullNameSearchHandler;
-
-class OrganizationSearchHandler extends FullNameSearchHandler
+class OrganizationSearchHandler extends SearchHandler
 {
     /**
-     * @var CacheManager
-     */
-    protected $cacheManager;
-
-    /**
-     * @param CacheManager $cacheManager
      * @param string $entityName
      * @param array $properties
      */
-    public function __construct(CacheManager $cacheManager, $entityName, array $properties)
+    public function __construct($entityName, array $properties)
     {
-        $this->cacheManager = $cacheManager;
         parent::__construct($entityName, $properties);
     }
 }
