10c5e2a6dcae835f695193e685c13cf2eafa5766
Yevhen Shyshkin <shishkin.yevgeniy@gmail.com>
2014-06-17 20:00:30 +0300

    CRM-744: Post flush processing - update process listener  - updated process listener

--- src/Oro/Bundle/WorkflowBundle/EventListener/ProcessCollectorListener.php
+++ src/Oro/Bundle/WorkflowBundle/EventListener/ProcessCollectorListener.php
@@ -9,8 +9,14 @@ use Doctrine\ORM\Event\OnClearEventArgs;
 use Doctrine\ORM\Event\PostFlushEventArgs;
 use Doctrine\ORM\Event\PreUpdateEventArgs;
 
+use JMS\JobQueueBundle\Entity\Job;
+
+use Oro\Bundle\WorkflowBundle\Command\ExecuteProcessJobCommand;
+use Oro\Bundle\WorkflowBundle\Entity\ProcessJob;
 use Oro\Bundle\WorkflowBundle\Entity\ProcessTrigger;
 use Oro\Bundle\WorkflowBundle\Model\ProcessData;
+use Oro\Bundle\WorkflowBundle\Model\ProcessHandler;
+use Oro\Bundle\EntityBundle\ORM\DoctrineHelper;
 
 class ProcessCollectorListener
 {
@@ -18,6 +24,16 @@ class ProcessCollectorListener
      * @var ManagerRegistry
      */
     protected $registry;
+
+    /**
+     * @var DoctrineHelper
+     */
+    protected $doctrineHelper;
+
+    /**
+     * @var ProcessHandler
+     */
+    protected $handler;
 
     /**
      * @var array
@@ -30,11 +46,25 @@ class ProcessCollectorListener
     protected $scheduledProcesses = array();
 
     /**
-     * @param ManagerRegistry $registry
+     * @var ProcessJob[]
      */
-    public function __construct(ManagerRegistry $registry)
+    protected $queuedJobs = array();
+
+    /**
+     * @var array
+     */
+    protected $removedEntityHashes = array();
+
+    /**
+     * @param ManagerRegistry $registry
+     * @param DoctrineHelper $doctrineHelper
+     * @param ProcessHandler $handler
+     */
+    public function __construct(ManagerRegistry $registry, DoctrineHelper $doctrineHelper, ProcessHandler $handler)
     {
         $this->registry = $registry;
+        $this->doctrineHelper = $doctrineHelper;
+        $this->handler = $handler;
     }
 
     /**
@@ -132,10 +162,15 @@ class ProcessCollectorListener
     {
         $entity = $args->getEntity();
         $entityClass = $this->getClass($entity);
+        $entityId = $this->doctrineHelper->getSingleEntityIdentifier($entity, false);
 
         $triggers = $this->getTriggers($entityClass, ProcessTrigger::EVENT_DELETE);
         foreach ($triggers as $trigger) {
             $this->scheduleProcess($trigger, $entity);
+        }
+
+        if ($entityId) {
+            $this->removedEntityHashes[] = ProcessJob::generateEntityHash($entityClass, $entityId);
         }
     }
 
@@ -162,7 +197,75 @@ class ProcessCollectorListener
      */
     public function postFlush(PostFlushEventArgs $args)
     {
-        // TODO: Should be implemented in scope of CRM-744
+        $entityManager = $args->getEntityManager();
+
+        // handle processes
+        $hasHandledProcessed = false;
+        foreach ($this->scheduledProcesses as $entityClass => &$entityProcesses) {
+            while ($entityProcess = array_shift($entityProcesses)) {
+                /** @var ProcessTrigger $trigger */
+                $trigger = $entityProcess['trigger'];
+                /** @var ProcessData $data */
+                $data = $entityProcess['data'];
+
+                if ($trigger->isQueued()) {
+                    $processJob = $this->queueProcess($trigger, $data);
+                    $entityManager->persist($processJob);
+                    $this->queuedJobs[] = $processJob;
+                } else {
+                    $this->handler->handleTrigger($trigger, $data);
+                }
+
+                $hasHandledProcessed = true;
+            }
+        }
+
+        // save both handled entities and queued ProcessJobs
+        if ($hasHandledProcessed) {
+            $entityManager->flush();
+        }
+
+        // delete unused processes
+        if ($this->removedEntityHashes) {
+            $this->registry->getRepository('OroWorkflowBundle:ProcessJob')->deleteByHashes($this->removedEntityHashes);
+            $this->removedEntityHashes = array();
+        }
+
+        // create JMS Jobs for queued jobs
+        $hasQueuedJobs = false;
+        /** @var ProcessJob $processJob */
+        while ($processJob = array_shift($this->queuedJobs)) {
+            $jmsJob = new Job(ExecuteProcessJobCommand::NAME, ['--id=' . $processJob->getId()]);
+
+            $timeShiftInterval = $processJob->getProcessTrigger()->getTimeShiftInterval();
+            if ($timeShiftInterval) {
+                $executeAfter = new \DateTime('now', new \DateTimeZone('UTC'));
+                $executeAfter->add($timeShiftInterval);
+                $jmsJob->setExecuteAfter($executeAfter);
+            }
+
+            $entityManager->persist($jmsJob);
+            $hasQueuedJobs = true;
+        }
+
+        // save JMS Job instances
+        if ($hasQueuedJobs) {
+            $entityManager->flush();
+        }
+    }
+
+    /**
+     * @param ProcessTrigger $trigger
+     * @param ProcessData $data
+     * @return ProcessJob
+     */
+    protected function queueProcess(ProcessTrigger $trigger, ProcessData $data)
+    {
+        $processJob = new ProcessJob();
+        $processJob->setProcessTrigger($trigger)
+            ->setData($data);
+
+        return $processJob;
     }
 
     /**
