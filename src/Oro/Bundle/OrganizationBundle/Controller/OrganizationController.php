b42f7466dc79dd0f1d51b4d3f987d5de426583eb
De Key <de_key@ua.fm>
2014-06-17 09:04:09 +0300

    CRM-1314 Add organization field

--- src/Oro/Bundle/OrganizationBundle/Controller/OrganizationController.php
+++ src/Oro/Bundle/OrganizationBundle/Controller/OrganizationController.php
@@ -3,6 +3,7 @@
 namespace Oro\Bundle\OrganizationBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -11,18 +12,30 @@ use Oro\Bundle\SecurityBundle\Annotation\Acl;
 use Oro\Bundle\SecurityBundle\Annotation\AclAncestor;
 use Oro\Bundle\OrganizationBundle\Entity\Organization;
 
-/**
- * @Route("/organization")
- */
 class OrganizationController extends Controller
 {
     /**
-     * @Route("/view/{id}", name="oro_organisation_view", requirements={"id"="\d+"})
+     * @Route(
+     *      "/{_format}",
+     *      name="oro_organization_index",
+     *      requirements={"_format"="html|json"},
+     *      defaults={"_format" = "html"}
+     * )
+     * @AclAncestor("oro_organization_view")
+     * @Template
+     */
+    public function indexAction(Request $request)
+    {
+        return array();
+    }
+
+    /**
+     * @Route("/view/{id}", name="oro_organization_view", requirements={"id"="\d+"})
      * @Template
      * @Acl(
-     *      id="oro_organisation_view",
+     *      id="oro_organization_view",
      *      type="entity",
-     *      class="OroOrganizationBundle:Organisation",
+     *      class="OroOrganizationBundle:Organization",
      *      permission="VIEW"
      * )
      */
