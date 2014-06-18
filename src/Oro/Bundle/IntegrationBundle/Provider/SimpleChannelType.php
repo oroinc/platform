b8eeff8f66e12a72cab24abbf9e01f99414066d1
Dmitry Naydenov <dnaidonov@magecore.com>
2014-06-18 19:46:28 +0300

    CRM-1397 Add possibility to define integration icon

--- src/Oro/Bundle/IntegrationBundle/Provider/SimpleChannelType.php
+++ src/Oro/Bundle/IntegrationBundle/Provider/SimpleChannelType.php
@@ -2,7 +2,7 @@
 
 namespace Oro\Bundle\IntegrationBundle\Provider;
 
-class SimpleChannelType implements ChannelInterface, IconAwareIntegrationInterface
+class SimpleChannelType implements ChannelInterface
 {
     /**
      * {@inheritdoc}
@@ -10,13 +10,5 @@ class SimpleChannelType implements ChannelInterface, IconAwareIntegrationInterfa
     public function getLabel()
     {
         return 'oro.integration.integration_type.simple.label';
-    }
-
-    /**
-     * @return string
-     */
-    public function getIcon()
-    {
-        return 'simple.png';
     }
 }
