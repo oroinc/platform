d7b7950acf607faa02a3273b56d667c6423e2d08
Yevhen Shyshkin <shishkin.yevgeniy@gmail.com>
2014-06-18 19:29:33 +0300

    CRM-763: Improve ProcessJob to serialize/unserialize data  - small fix in DB structure  - updated functional test

--- src/Oro/Bundle/WorkflowBundle/Tests/Functional/Entity/Repository/ProcessJobRepositoryTest.php
+++ src/Oro/Bundle/WorkflowBundle/Tests/Functional/Entity/Repository/ProcessJobRepositoryTest.php
@@ -7,11 +7,11 @@ use Doctrine\Bundle\DoctrineBundle\Registry;
 use Doctrine\ORM\EntityManager;
 
 use Oro\Bundle\TestFrameworkBundle\Test\WebTestCase;
+use Oro\Bundle\UserBundle\Entity\User;
 use Oro\Bundle\WorkflowBundle\Entity\ProcessDefinition;
 use Oro\Bundle\WorkflowBundle\Entity\ProcessJob;
 use Oro\Bundle\WorkflowBundle\Entity\ProcessTrigger;
 use Oro\Bundle\WorkflowBundle\Entity\Repository\ProcessJobRepository;
-use Oro\Bundle\WorkflowBundle\Entity\WorkflowDefinition;
 use Oro\Bundle\WorkflowBundle\Tests\Functional\DataFixtures\LoadProcessEntities;
 
 /**
@@ -47,50 +47,32 @@ class ProcessJobRepositoryTest extends WebTestCase
 
     public function testFindEntity()
     {
-        $entityIdFirst  = 'first';
-        $entityIdSecond = 'second';
-
-        $user = $this->registry->getRepository('OroUserBundle:User')->find(1);
-
-        $entityFirst = new WorkflowDefinition();
-        $entityFirst
-            ->setLabel('labelFirst')
-            ->setName($entityIdFirst)
-            ->setRelatedEntity($user)
-            ->setEntityAttributeName('firstName');
-        $this->entityManager->persist($entityFirst);
-
-        $entitySecond = new WorkflowDefinition();
-        $entitySecond
-            ->setLabel('labelSecond')
-            ->setName($entityIdSecond)
-            ->setRelatedEntity($user)
-            ->setEntityAttributeName('secondName');
-        $this->entityManager->persist($entitySecond);
-
-        $entityClass = ClassUtils::getClass($entityFirst);
+        /** @var User $entity */
+        $entity = $this->registry->getRepository('OroUserBundle:User')
+            ->createQueryBuilder('user')
+            ->setMaxResults(1)
+            ->getQuery()
+            ->getSingleResult();
+        $entityClass = ClassUtils::getClass($entity);
+        $entityId = $entity->getId();
 
         $processDefinition = new ProcessDefinition();
         $processDefinition
             ->setName('test')
             ->setLabel('Test')
             ->setRelatedEntity($entityClass);
-        $this->entityManager->persist($processDefinition);
 
         $processTrigger = new ProcessTrigger();
         $processTrigger
             ->setDefinition($processDefinition)
             ->setEvent(ProcessTrigger::EVENT_UPDATE);
-        $this->entityManager->persist($processTrigger);
 
         $processJob = new ProcessJob();
         $processJob
             ->setProcessTrigger($processTrigger)
-            ->setEntityId($entityIdFirst);
-        $this->entityManager->persist($processJob);
+            ->setEntityId($entityId);
 
-        $foundEntity = $this->repository->findEntity($processJob);
-        $this->assertEquals($foundEntity, $entityFirst);
+        $this->assertEquals($entity, $this->repository->findEntity($processJob));
     }
 
     public function testDeleteByHashes()
