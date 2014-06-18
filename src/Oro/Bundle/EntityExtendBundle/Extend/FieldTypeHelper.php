ad0e34fde12699fff545ae2c558a7525fbdb2d5f
Alexander Nezdoiminoga <alex@magecore.com>
2014-06-18 16:30:10 +0300

    BAP-4456:  attachment migration extension

---
+++ src/Oro/Bundle/EntityExtendBundle/Extend/FieldTypeHelper.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Oro\Bundle\EntityExtendBundle\Extend;
+
+/**
+ * This class should help with detecting relation field types
+ *
+ * TODO: implement type mapping via configuration yamls
+ */
+class FieldTypeHelper
+{
+    protected $typeMap = [
+        'manyToOne' => [
+            'attachment',
+            'attachmnetImage'
+        ]
+    ];
+
+    /**
+     * @param string $type
+     *
+     * @return string
+     */
+    public function getUnderlyingType($type)
+    {
+        foreach ($this->typeMap as $key => $value) {
+            if (in_array($type, $value)) {
+                return $key;
+            }
+        }
+
+        return $type;
+    }
+}
