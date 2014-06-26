256953d662fdf8fce5ec8a38706eb9c3bfd8b907
Dmitry Khrysev <dkhrysev@magecore.com>
2014-06-26 21:14:05 +0400

    CRM-1484: Implement tracking settings

--- src/Oro/Bundle/TrackingBundle/DependencyInjection/Configuration.php
+++ src/Oro/Bundle/TrackingBundle/DependencyInjection/Configuration.php
@@ -2,6 +2,7 @@
 
 namespace Oro\Bundle\TrackingBundle\DependencyInjection;
 
+use Oro\Bundle\ConfigBundle\DependencyInjection\SettingsBuilder;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
@@ -14,6 +15,16 @@ class Configuration implements ConfigurationInterface
     {
         $treeBuilder = new TreeBuilder();
         $rootNode    = $treeBuilder->root('oro_tracking');
+
+        SettingsBuilder::append(
+            $rootNode,
+            array(
+                'dynamic_tracking_enabled' => ['value' => false],
+                'log_rotate_interval'      => ['value' => 60],
+                'piwik_host'               => ['value' => null],
+                'piwik_token_auth'         => ['value' => null]
+            )
+        );
 
         return $treeBuilder;
     }
