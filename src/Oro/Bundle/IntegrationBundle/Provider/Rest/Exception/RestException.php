6a66d586bbad3642f3f548b00295106399588486
Ignat Shcheglovskyi <ischeglovskiy@magecore.com>
2014-07-02 00:28:05 +0300

    CRM-1444: Transport for Zendesk: createUser, createTicket, updateTicket, addComment, updateComment - updating exception factory method - added test

--- src/Oro/Bundle/IntegrationBundle/Provider/Rest/Exception/RestException.php
+++ src/Oro/Bundle/IntegrationBundle/Provider/Rest/Exception/RestException.php
@@ -8,15 +8,59 @@ use Oro\Bundle\IntegrationBundle\Provider\Rest\Client\RestResponseInterface;
 class RestException extends TransportException
 {
     /**
+     * @var RestResponseInterface
+     */
+    protected $response;
+
+    /**
      * @param RestResponseInterface $response
+     * @param string|null $message
+     * @param \Exception|null $previous
      * @return RestException
      */
-    public static function createFromResponse(RestResponseInterface $response)
-    {
+    public static function createFromResponse(
+        RestResponseInterface $response,
+        $message = null,
+        \Exception $previous = null
+    ) {
+        if ($response->isClientError()) {
+            $label = 'Client error response';
+        } elseif ($response->isServerError()) {
+            $label = 'Server error response';
+        } else {
+            $label = 'Unsuccessful response';
+        }
+
+        if ($message) {
+            $message = $label . ': ' . $message;
+        } else {
+            $message = $label;
+        }
+
         $messageParts[] = '[status code] ' . $response->getStatusCode();
         $messageParts[] = '[reason phrase] ' . $response->getReasonPhrase();
-        $message = 'Server error response' . PHP_EOL . implode(PHP_EOL, $messageParts);
+        $message = $message . PHP_EOL . implode(PHP_EOL, $messageParts);
 
-        return new static($message, $response->getStatusCode());
+        /** @var RestException $result */
+        $result = new static($message, $response->getStatusCode(), $previous);
+        $result->setResponse($response);
+
+        return $result;
+    }
+
+    /**
+     * @param RestResponseInterface $response
+     */
+    public function setResponse(RestResponseInterface $response)
+    {
+        $this->response = $response;
+    }
+
+    /**
+     * @return RestResponseInterface|null
+     */
+    public function getResponse()
+    {
+        return $this->response;
     }
 }
