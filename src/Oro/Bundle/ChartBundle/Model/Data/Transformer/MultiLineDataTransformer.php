83de53d2532ba2a739a33b9841633095e1e22925
Sergey Zhuravel <zhuravel.sergey@gmail.com>
2014-07-04 16:05:15 +0300

    CRM-1332: Create plots for events using report grid - add chart multiline support

---
+++ src/Oro/Bundle/ChartBundle/Model/Data/Transformer/MultiLineDataTransformer.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Oro\Bundle\ChartBundle\Model\Data\Transformer;
+
+use Oro\Bundle\ChartBundle\Model\Data\ArrayData;
+use Oro\Bundle\ChartBundle\Model\Data\DataInterface;
+use Oro\Bundle\ChartBundle\Model\Data\MappedData;
+
+class MultiLineDataTransformer implements TransformerInterface
+{
+    /**
+     * @param DataInterface $data
+     * @param array         $chartOptions
+     *
+     * @return DataInterface
+     */
+    public function transform(DataInterface $data, array $chartOptions)
+    {
+        /** @var MappedData $data */
+        $sourceData = $data->getSourceData()->toArray();
+        $data       = $data->toArray();
+        $arrayData  = [];
+
+        foreach ($sourceData as $sourceDataKey => $sourceDataItem) {
+            $dataItem = $data[$sourceDataKey];
+
+            $keys = array_diff(
+                $sourceDataItem,
+                $dataItem
+            );
+
+            foreach ($keys as $key) {
+                $arrayData[$key][] = $dataItem;
+            }
+        }
+
+        return new ArrayData($arrayData);
+    }
+}
