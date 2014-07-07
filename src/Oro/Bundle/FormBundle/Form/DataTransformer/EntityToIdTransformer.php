8034fdf80058c599280e0e6c6f1020ccc66edb18
Alexandr Smaga <asmaga@magecore.com>
2014-07-07 17:54:25 +0300

    CRM-1513: Fix demo notes    - revert not needed changes

--- src/Oro/Bundle/FormBundle/Form/DataTransformer/EntityToIdTransformer.php
+++ src/Oro/Bundle/FormBundle/Form/DataTransformer/EntityToIdTransformer.php
@@ -91,7 +91,7 @@ class EntityToIdTransformer implements DataTransformerInterface
      */
     public function transform($value)
     {
-        if ('' === $value || null === $value) {
+        if (null === $value) {
             return null;
         }
 
