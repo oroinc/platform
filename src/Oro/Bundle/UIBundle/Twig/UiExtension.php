20942db83f75af842aad739de3c55374036abce5
Yurii Muratov <yurio@ua.fm>
2014-07-08 17:05:16 +0300

    BAP-4598: Redesign remove file action

--- src/Oro/Bundle/UIBundle/Twig/UiExtension.php
+++ src/Oro/Bundle/UIBundle/Twig/UiExtension.php
@@ -16,37 +16,11 @@ class UiExtension extends \Twig_Extension
         );
     }
 
-    public function getFilters()
-    {
-        return array(
-            'trimString' => new \Twig_Filter_Method($this, 'trimString'),
-        );
-    }
-
     /**
      * {@inheritdoc}
      */
     public function getName()
     {
         return 'oro_ui';
-    }
-
-    /**
-     * Trim text string
-     *
-     * @param string $string
-     * @param int    $symbolCount
-     *
-     * @return string
-     */
-    public function trimString($string, $symbolCount = 30)
-    {
-        $originalLength = strlen($string);
-        if ($originalLength > $symbolCount) {
-            $string = substr($string, 0, $symbolCount);
-            $string = sprintf('%s...', $string);
-        }
-
-        return $string;
     }
 }
