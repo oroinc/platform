8e16cbce7a68611e22d796f885b1b8dbcf499670
Dmitry Naydenov <dnaidonov@magecore.com>
2014-07-14 17:32:18 +0300

    CRM-1532 Automatic magento synchronization fails add unit tests

--- src/Oro/Bundle/ImportExportBundle/Tests/Unit/Serializer/SerializerTest.php
+++ src/Oro/Bundle/ImportExportBundle/Tests/Unit/Serializer/SerializerTest.php
@@ -90,4 +90,68 @@ class SerializerTest extends \PHPUnit_Framework_TestCase
 
         $this->serializer->supportsDenormalization(new \stdClass(), 'test');
     }
+
+    public function testDenormalizeUsingCache()
+    {
+        $normalizer = $this
+            ->getMock('Oro\Bundle\ImportExportBundle\Serializer\Normalizer\NormalizerInterface');
+        $normalizer
+            ->expects($this->never())
+            ->method('supportsNormalization')
+            ->will($this->returnValue(true));
+
+        $supportedDenormalizer = $this
+            ->getMock('Oro\Bundle\ImportExportBundle\Serializer\Normalizer\DenormalizerInterface');
+        $supportedDenormalizer
+            ->expects($this->exactly(2))
+            ->method('supportsDenormalization')
+            ->will($this->returnValue(true));
+
+        $nonSupportedDenormalizer = $this
+            ->getMock('Oro\Bundle\ImportExportBundle\Serializer\Normalizer\DenormalizerInterface');
+        $nonSupportedDenormalizer
+            ->expects($this->exactly(2))
+            ->method('supportsDenormalization')
+            ->will($this->returnValue(false));
+
+        $this->serializer = new Serializer([$normalizer, $nonSupportedDenormalizer, $supportedDenormalizer]);
+
+        $this->serializer->supportsDenormalization(new \stdClass(), 'test');
+
+        $this->serializer->denormalize(new \DateTime('now'), 'date');
+
+        $this->serializer->denormalize(new \DateTime('now'), 'date');
+    }
+
+    public function testDenormalizeWithoutCache()
+    {
+        $normalizer = $this
+            ->getMock('Oro\Bundle\ImportExportBundle\Serializer\Normalizer\NormalizerInterface');
+        $normalizer
+            ->expects($this->never())
+            ->method('supportsNormalization')
+            ->will($this->returnValue(true));
+
+        $supportedDenormalizer = $this
+            ->getMock('Oro\Bundle\ImportExportBundle\Serializer\Normalizer\DenormalizerInterface');
+        $supportedDenormalizer
+            ->expects($this->exactly(3))
+            ->method('supportsDenormalization')
+            ->will($this->returnValue(true));
+
+        $nonSupportedDenormalizer = $this
+            ->getMock('Oro\Bundle\ImportExportBundle\Serializer\Normalizer\DenormalizerInterface');
+        $nonSupportedDenormalizer
+            ->expects($this->exactly(3))
+            ->method('supportsDenormalization')
+            ->will($this->returnValue(false));
+
+        $this->serializer = new Serializer([$normalizer, $nonSupportedDenormalizer, $supportedDenormalizer]);
+
+        $this->serializer->supportsDenormalization(new \stdClass(), 'test');
+
+        $this->serializer->denormalize(new \DateTime('now'), 'date');
+
+        $this->serializer->denormalize(new \DateTime('now'), 'dateTime');
+    }
 }
