2476d38f4f1a1a05465b7209a6cc02fd281cc46c
Dmitry Khrysev <dkhrysev@magecore.com>
2014-07-16 21:30:44 +0400

    BAP-4750: Do not reload application menu on page load

--- src/Oro/Bundle/UIBundle/Resources/public/js/app/views/page/breadcrumb-view.js
+++ src/Oro/Bundle/UIBundle/Resources/public/js/app/views/page/breadcrumb-view.js
@@ -1,7 +1,9 @@
 /*global define*/
 define([
-    './../base/page-region-view'
-], function (PageRegionView) {
+    './../base/page-region-view',
+    'oroui/js/mediator',
+    'underscore'
+], function (PageRegionView, mediator, _) {
     'use strict';
 
     var PageBreadcrumbView;
@@ -10,7 +12,29 @@ define([
         template: function (data) {
             return data.breadcrumb;
         },
-        pageItems: ['breadcrumb']
+        breadcrumbsTemplate: _.template('<ul class="breadcrumb">' +
+            '<% for (var i =0; i < breadcrumbs.length; i++) { %>'
+                + '<li>'
+                    + '<%= breadcrumbs[i] %>'
+                    + '<%if (i+1 != breadcrumbs.length) { %><span class="divider">/&nbsp;</span><% } %>'
+                + '</li>'
+                +'<% } %>' +
+            '</ul>'),
+        pageItems: ['breadcrumb'],
+
+        initialize: function(options) {
+            mediator.on('mainMenuUpdated', this.update, this);
+            PageBreadcrumbView.__super__.initialize.call(this, options);
+        },
+
+        update: function(breadcrumbs) {
+            if (breadcrumbs.length) {
+                this.data = {
+                    'breadcrumb': this.breadcrumbsTemplate({'breadcrumbs': breadcrumbs})
+                };
+                this.render();
+            }
+        }
     });
 
     return PageBreadcrumbView;
--- src/Oro/Bundle/UIBundle/Resources/public/js/app/views/page/main-menu-view.js
+++ src/Oro/Bundle/UIBundle/Resources/public/js/app/views/page/main-menu-view.js
@@ -1,8 +1,11 @@
 /*jslint nomen:true*/
 /*global define*/
 define([
-    './../base/page-region-view'
-], function (PageRegionView) {
+    './../base/page-region-view',
+    'underscore',
+    'jquery',
+    'oroui/js/mediator'
+], function (PageRegionView, _, $, mediator) {
     'use strict';
 
     var PageMainMenuView;
@@ -11,11 +14,106 @@ define([
         template: function (data) {
             return data.mainMenu;
         },
-        pageItems: ['mainMenu'],
+        pageItems: ['mainMenu', 'currentRoute'],
+        routeMatchSearchers: [],
+        matchesCache: {},
 
         render: function () {
-            PageMainMenuView.__super__.render.call(this);
-            this.$el.trigger('mainMenuUpdated');
+            var data = this.getTemplateData();
+            if (data) {
+                if (!_.isUndefined(data.mainMenu)) {
+                    PageMainMenuView.__super__.render.call(this);
+                    this.initRouteMatches();
+                } else if (!_.isUndefined(data.currentRoute)) {
+                    this.toggleActiveMenuItem(data.currentRoute);
+                    mediator.trigger('mainMenuUpdated', this.getActiveItems());
+                }
+
+                this.$el.trigger('mainMenuUpdated');
+            } else {
+                this.initRouteMatches();
+            }
+        },
+
+        initRouteMatches: function() {
+            var createRouteSearchCallback = function(matchRule, $el) {
+                if (matchRule.indexOf('*') > -1 || matchRule.indexOf('/') > -1) {
+                    if (matchRule.indexOf('*') > -1) {
+                        matchRule = '^' + matchRule.replace('*', '\\w+') + '$';
+                    } else {
+                        matchRule = matchRule.replace(/^\/|\/$/g, '');
+                    }
+                    // RegExp matcher
+                    return function (route) {
+                        var matchRegExp = new RegExp(matchRule, 'ig');
+                        if (matchRegExp.test(route)) {
+                            return $el;
+                        }
+                    };
+                } else {
+                    // Simple equal matcher
+                    return function(route) {
+                        if (route === matchRule) {
+                            return $el;
+                        }
+                    };
+                }
+            };
+
+            var self = this;
+            this.$el
+                .find('[data-routes]')
+                .each(function(idx, el) {
+                    var $el = $(el);
+                    _.each($el.data('routes'), function (matchRule) {
+                        self.routeMatchSearchers.push(createRouteSearchCallback(matchRule, $el));
+                    });
+                });
+        },
+
+        getMatchesMenuItem: function(route) {
+            if (this.matchesCache.hasOwnProperty(route)) {
+                return this.matchesCache[route];
+            }
+
+            var match = this.$el.find('[data-route="' + route + '"]');
+            if (!match.length) {
+                for (var i = 0; i < this.routeMatchSearchers.length; i++) {
+                    match = this.routeMatchSearchers[i](route);
+                    if (!_.isUndefined(match)) {
+                        break;
+                    }
+                }
+            }
+
+            if (match && match.length) {
+                this.matchesCache[route] = match;
+                return match;
+            }
+
+            return false;
+        },
+
+        toggleActiveMenuItem: function(route) {
+            var item = this.getMatchesMenuItem(route);
+            if (item) {
+                this.$el
+                    .find('.active')
+                    .removeClass('active');
+                item.addClass('active');
+                item.parents('.dropdown').addClass('active');
+            }
+        },
+
+        getActiveItems: function() {
+            var breadcrumbs = [];
+            this.$el
+                .find('.active')
+                .each(function(idx, el) {
+                    breadcrumbs.push($.trim($(el).find('.title').first().text()));
+                });
+
+            return breadcrumbs;
         }
     });
 
