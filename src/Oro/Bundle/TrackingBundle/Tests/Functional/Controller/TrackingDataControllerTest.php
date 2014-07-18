31170ca4cead028627cbd1298f29d9b0194d34d8
Sergey Zhuravel <zhuravel.sergey@gmail.com>
2014-07-18 16:59:13 +0300

    CRM-1552: Visit Line is not displaying correctly in Graph. - fix test

--- src/Oro/Bundle/TrackingBundle/Tests/Functional/Controller/TrackingDataControllerTest.php
+++ src/Oro/Bundle/TrackingBundle/Tests/Functional/Controller/TrackingDataControllerTest.php
@@ -31,6 +31,7 @@ class TrackingDataControllerTest extends WebTestCase
         $result   = $this->getJsonResponseContent($response, 201);
         $this->assertArrayHasKey('success', $result);
         $this->assertArrayNotHasKey('errors', $result);
+        $this->assertArrayNotHasKey('validation', $result);
     }
 
     /**
@@ -43,6 +44,7 @@ class TrackingDataControllerTest extends WebTestCase
                 [
                     'param'    => 'value',
                     'url'      => 'example.com',
+                    'user'     => 'user',
                     'loggedAt' => '2014-07-18T15:00:00+0300'
                 ]
             ],
@@ -54,6 +56,65 @@ class TrackingDataControllerTest extends WebTestCase
                     'user'     => 'user',
                     'url'      => 'example.com',
                     'loggedAt' => '2014-07-18T15:00:00+0300'
+                ]
+            ],
+        ];
+    }
+
+    /**
+     * @param array $options
+     * @param array $expectedMessages
+     * @dataProvider validationProvider
+     */
+    public function testValidation(array $options, array $expectedMessages)
+    {
+        $this->client->request(
+            'GET',
+            $this->getUrl('oro_tracking_data_create', $options),
+            [],
+            [],
+            $this->generateBasicAuthHeader()
+        );
+        $response = $this->client->getResponse();
+        $result   = $this->getJsonResponseContent($response, 400);
+        $this->assertArrayHasKey('success', $result);
+        $this->assertArrayNotHasKey('errors', $result);
+        $errors = implode(', ', $result['validation']);
+        foreach ($expectedMessages as $expectedMessage) {
+            $this->assertContains($expectedMessage, $errors);
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public function validationProvider()
+    {
+        return [
+            'empty' => [
+                [],
+                [
+                    'event.user: This value should not be blank',
+                    'event.url: This value should not be blank',
+                    'event.loggedAt: This value should not be blank',
+                ]
+            ],
+            'user'  => [
+                [
+                    'user' => 'user'
+                ],
+                [
+                    'event.url: This value should not be blank',
+                    'event.loggedAt: This value should not be blank',
+                ]
+            ],
+            'url'   => [
+                [
+                    'user' => 'user',
+                    'url'  => 'example.com'
+                ],
+                [
+                    'event.loggedAt: This value should not be blank',
                 ]
             ],
         ];
