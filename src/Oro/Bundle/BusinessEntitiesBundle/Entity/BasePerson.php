c8d118bd83512f01e98c7c9e45259aa7bb893ba6
Viktor Linkin <vlinkin@magecore.com>
2014-07-21 18:09:54 +0300

    BAP-4784:Incorrect filters names  - missed namespace;

--- src/Oro/Bundle/BusinessEntitiesBundle/Entity/BasePerson.php
+++ src/Oro/Bundle/BusinessEntitiesBundle/Entity/BasePerson.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 use Oro\Bundle\AddressBundle\Entity\AbstractAddress;
 use Oro\Bundle\EmailBundle\Model\EmailHolderInterface;
+use Oro\Bundle\EntityConfigBundle\Metadata\Annotation\ConfigField;
 use Oro\Bundle\LocaleBundle\Model\FullNameInterface;
 
 /**
