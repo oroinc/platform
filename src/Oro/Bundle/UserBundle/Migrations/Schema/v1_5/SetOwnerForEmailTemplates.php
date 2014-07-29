c8062c2e630005abe81a1ca43f677fc83b000a14
Vova Soroka <vsoroka@magecore.com>
2014-07-29 16:42:46 +0300

    Update SetOwnerForEmailTemplates.php

--- src/Oro/Bundle/UserBundle/Migrations/Schema/v1_5/SetOwnerForEmailTemplates.php
+++ src/Oro/Bundle/UserBundle/Migrations/Schema/v1_5/SetOwnerForEmailTemplates.php
@@ -10,7 +10,7 @@ use Oro\Bundle\MigrationBundle\Migration\QueryBag;
 class SetOwnerForEmailTemplates implements Migration
 {
     /**
-     * @inheritdoc
+     * {@inheritdoc}
      */
     public function up(Schema $schema, QueryBag $queries)
     {
