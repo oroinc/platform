17a3f39b0a53a1d14ea6264865730cd05e6a29d5
Sergey Zhuravel <zhuravel.sergey@gmail.com>
2014-08-01 13:45:19 +0300

    CRM-1610: Create migrations for incompatible initial migrations - OroNotificationBundle

--- src/Oro/Bundle/NotificationBundle/Migrations/Schema/OroNotificationBundleInstaller.php
+++ src/Oro/Bundle/NotificationBundle/Migrations/Schema/OroNotificationBundleInstaller.php
@@ -17,7 +17,7 @@ class OroNotificationBundleInstaller implements Installation
      */
     public function getMigrationVersion()
     {
-        return 'v1_16';
+        return 'v1_1';
     }
 
     /**
