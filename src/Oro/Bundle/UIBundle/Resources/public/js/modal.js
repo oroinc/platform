cb428cc1b4765da95492e7384da898f2c20f6d78
hryhorii hrebiniuk <hhrebiniuk@magecore.com>
2014-08-11 20:08:18 +0300

    BAP-5004: Fix memory leaks in grids
     - refactored refactored grid listeners
     - fixed dispose method of modal pop-up

--- src/Oro/Bundle/UIBundle/Resources/public/js/modal.js
+++ src/Oro/Bundle/UIBundle/Resources/public/js/modal.js
@@ -1,7 +1,12 @@
-/* global define */
-define(['underscore', 'backbone', 'backbone-bootstrap-modal'],
-function(_, Backbone) {
+/*global define*/
+define([
+    'underscore',
+    'backbone',
+    'backbone-bootstrap-modal'
+], function (_, Backbone) {
     'use strict';
+
+    var Modal;
 
     /**
      * Implementation of Bootstrap Modal
@@ -11,16 +16,37 @@ function(_, Backbone) {
      * @class   oroui.Modal
      * @extends Backbone.BootstrapModal
      */
-    return Backbone.BootstrapModal.extend({
+    Modal = Backbone.BootstrapModal.extend({
         /** @property {String} */
         className: 'modal',
 
         open: function() {
-            Backbone.BootstrapModal.prototype.open.apply(this, arguments);
+            Modal.__super__.open.apply(this, arguments);
 
             this.once('cancel', _.bind(function() {
                 this.$el.trigger('hidden');
             }, this));
-        }
+        },
+
+        /**
+         * @inheritDoc
+         */
+        dispose: function () {
+            if (this.disposed) {
+                return;
+            }
+            Backbone.mediator.unsubscribe(null, null, this);
+            this.off();
+            this.stopListening();
+            if (this.$el) {
+                this.undelegateEvents();
+                this.$el.removeData();
+            }
+            delete this.$content;
+            delete this.$el;
+            delete this.el;
+        },
     });
+
+    return Modal;
 });
