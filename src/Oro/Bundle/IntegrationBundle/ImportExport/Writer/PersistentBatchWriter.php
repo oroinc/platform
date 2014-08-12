dc4c863fa446deb008db9678c45117cc3d0813ab
Alexandr Smaga <asmaga@magecore.com>
2014-08-12 19:53:53 +0300

    CRM-1789: Sync broken after sql exception during import    - added reopen of EM in catch

--- src/Oro/Bundle/IntegrationBundle/ImportExport/Writer/PersistentBatchWriter.php
+++ src/Oro/Bundle/IntegrationBundle/ImportExport/Writer/PersistentBatchWriter.php
@@ -43,6 +43,7 @@ class PersistentBatchWriter implements ItemWriterInterface, StepExecutionAwareIn
         $this->registry        = $registry;
         $this->eventDispatcher = $eventDispatcher;
         $this->contextRegistry = $contextRegistry;
+        $this->ensureEntityManagerReady();
     }
 
     /**
@@ -50,8 +51,6 @@ class PersistentBatchWriter implements ItemWriterInterface, StepExecutionAwareIn
      */
     public function write(array $items)
     {
-        $this->ensureEntityManagerReady();
-
         try {
             $this->em->beginTransaction();
 
