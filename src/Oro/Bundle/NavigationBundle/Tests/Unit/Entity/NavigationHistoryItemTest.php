4f75c80c56a046a82a19ca9e7ba79e73d4d203e5
Yurii Muratov <yurio@ua.fm>
2014-08-12 20:21:15 +0300

    OEE-58: Navigation: history & most viewed    - Fix cs

--- src/Oro/Bundle/NavigationBundle/Tests/Unit/Entity/NavigationHistoryItemTest.php
+++ src/Oro/Bundle/NavigationBundle/Tests/Unit/Entity/NavigationHistoryItemTest.php
@@ -27,7 +27,6 @@ class NavigationHistoryItemTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($values['user'], $item->getUser());
         $this->assertEquals($values['organization'], $item->getOrganization());
 
-
         $dateTime = new \DateTime();
         $item->setVisitedAt($dateTime);
         $this->assertEquals($dateTime, $item->getVisitedAt());
