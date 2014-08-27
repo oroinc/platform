2d34b51bc7fb3dc5390926f75f4e20b6da4c258f
Yurii Muratov <yurio@ua.fm>
2014-08-27 17:03:09 +0300

    OEE-201: Fix update

--- src/Oro/Bundle/DashboardBundle/Migrations/Schema/v1_2/OroDashboardBundle.php
+++ src/Oro/Bundle/DashboardBundle/Migrations/Schema/v1_2/OroDashboardBundle.php
@@ -15,18 +15,7 @@ class OroDashboardBundle implements Migration
     public function up(Schema $schema, QueryBag $queries)
     {
         self::addOrganizationDashboardTable($schema);
-        self::createOroDashboardActiveTable($schema);
-        self::addOroDashboardActiveForeignKeys($schema);
-
-        //Copy data from oro_dashboard_active into oro_dashboard_active_copy, rename and drop it
-        $queries->addPostQuery(
-            "INSERT INTO oro_dashboard_active_copy (user_id, dashboard_id)
-             SELECT user_id, dashboard_id
-             FROM oro_dashboard_active;
-
-             DROP TABLE oro_dashboard_active;
-             ALTER TABLE oro_dashboard_active_copy RENAME TO oro_dashboard_active;"
-        );
+        self::dropOroDashboardActiveTableIndexes($schema);
     }
 
     /**
@@ -48,54 +37,16 @@ class OroDashboardBundle implements Migration
     }
 
     /**
-     * Create oro_dashboard_active table
+     * Drop oro_dashboard_active table indexes
      *
      * @param Schema $schema
      */
-    protected function createOroDashboardActiveTable(Schema $schema)
+    protected function dropOroDashboardActiveTableIndexes(Schema $schema)
     {
         $table = $schema->getTable('oro_dashboard_active');
         if ($table->hasIndex('IDX_858BA17EB9D04D2B')) {
             $table->removeForeignKey('FK_858BA17EB9D04D2B');
             $table->dropIndex('IDX_858BA17EB9D04D2B');
         }
-        $table = $schema->createTable('oro_dashboard_active_copy');
-        $table->addColumn('id', 'integer', ['autoincrement' => true]);
-        $table->addColumn('organization_id', 'integer', ['notnull' => false]);
-        $table->addColumn('user_id', 'integer', ['notnull' => false]);
-        $table->addColumn('dashboard_id', 'integer', ['notnull' => false]);
-        $table->setPrimaryKey(['id']);
-        $table->addIndex(['user_id'], 'IDX_858BA17EA76ED395', []);
-        $table->addIndex(['dashboard_id'], 'IDX_858BA17EB9D04D2B', []);
-        $table->addIndex(['organization_id'], 'IDX_858BA17E32C8A3DE', []);
     }
-
-    /**
-     * Add oro_dashboard_active foreign keys.
-     *
-     * @param Schema $schema
-     */
-    protected function addOroDashboardActiveForeignKeys(Schema $schema)
-    {
-        $table = $schema->getTable('oro_dashboard_active_copy');
-        $table->addForeignKeyConstraint(
-            $schema->getTable('oro_user'),
-            ['user_id'],
-            ['id'],
-            ['onDelete' => 'SET NULL', 'onUpdate' => null]
-        );
-        $table->addForeignKeyConstraint(
-            $schema->getTable('oro_organization'),
-            ['organization_id'],
-            ['id'],
-            ['onDelete' => 'SET NULL', 'onUpdate' => null]
-        );
-        $table->addForeignKeyConstraint(
-            $schema->getTable('oro_dashboard'),
-            ['dashboard_id'],
-            ['id'],
-            ['onDelete' => 'SET NULL', 'onUpdate' => null]
-        );
-    }
-
 }
