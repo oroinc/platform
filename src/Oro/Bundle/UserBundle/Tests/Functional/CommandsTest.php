396c306afc1b6876f4502f2dd79f2f06a2dc5229
Alexander Nezdoiminoga <anezdoiminoga@magecore.com>
2014-09-16 22:20:38 +0300

    OEE-26: functional tests fixes

--- src/Oro/Bundle/UserBundle/Tests/Functional/CommandsTest.php
+++ src/Oro/Bundle/UserBundle/Tests/Functional/CommandsTest.php
@@ -2,12 +2,12 @@
 
 namespace Oro\Bundle\UserBundle\Tests\Functional;
 
-use Oro\Bundle\OrganizationBundle\Entity\Organization;
 use Symfony\Component\Console\Tester\CommandTester;
 use Symfony\Component\HttpKernel\Kernel;
 
 use Symfony\Component\Console\Application;
 
+use Oro\Bundle\OrganizationBundle\Entity\Organization;
 use Oro\Bundle\UserBundle\Command\GenerateWSSEHeaderCommand;
 use Oro\Bundle\TestFrameworkBundle\Test\WebTestCase;
 
