8a2ea4876f43de04add5aff7043049c90c237ff8
Alexander Nezdoiminoga <anezdoiminoga@magecore.com>
2014-09-16 18:33:11 +0300

    OEE-26: functional tests

--- src/Oro/Bundle/TestFrameworkBundle/Fixtures/LoadUserData.php
+++ src/Oro/Bundle/TestFrameworkBundle/Fixtures/LoadUserData.php
@@ -1,7 +1,6 @@
 <?php
 namespace Oro\Bundle\TestFrameworkBundle\Fixtures;
 
-use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -14,9 +13,7 @@ use Oro\Bundle\UserBundle\Entity\UserManager;
 
 class LoadUserData extends AbstractFixture implements ContainerAwareInterface, OrderedFixtureInterface
 {
-    /**
-     * @var ContainerInterface
-     */
+    /** @var ContainerInterface */
     private $container;
 
     public function setContainer(ContainerInterface $container = null)
@@ -29,48 +26,29 @@ class LoadUserData extends AbstractFixture implements ContainerAwareInterface, O
         /** @var UserManager $userManager */
         $userManager = $this->container->get('oro_user.manager');
 
-        $admin  = $manager
-            ->getRepository('OroUserBundle:User')
-            ->findOneBy(array('username' => 'admin'));
-
-        $role  = $manager
-            ->getRepository('OroUserBundle:Role')
-            ->findOneBy(array('role' => 'ROLE_ADMINISTRATOR'));
-        $group = $manager
-            ->getRepository('OroUserBundle:Group')
-            ->findOneBy(array('name' => 'Administrators'));
-
-        $unit = $manager
-            ->getRepository('OroOrganizationBundle:BusinessUnit')
-            ->findOneBy(array('name' => 'Main'));
-
+        $admin = $manager->getRepository('OroUserBundle:User')->findOneBy(array('username' => 'admin'));
         if (!$admin) {
+            $role  = $manager->getRepository('OroUserBundle:Role')->findOneBy(array('role' => 'ROLE_ADMINISTRATOR'));
             $admin = $userManager->createUser();
             $admin
                 ->setUsername('admin')
                 ->addRole($role);
         }
 
-        $organization = $this->getReference('default_organization');
-
-        $admin->setPlainPassword('admin')
+        $admin
+            ->setPlainPassword('admin')
             ->setFirstname('John')
             ->setLastname('Doe')
             ->setEmail('admin@example.com')
-            ->setOwner($unit)
-            ->addGroup($group)
-            ->setOrganization($organization)
-            ->addOrganization($organization)
-            ->setBusinessUnits(
-                new ArrayCollection(array($unit))
-            )
             ->setSalt('');
 
         if (!is_object($admin->getApiKeys())) {
-            $api   = new UserApi();
+            $organization = $manager->getRepository('OroOrganizationBundle:Organization')->getFirst();
+            $api          = new UserApi();
             $api->setApiKey('admin_api_key')
                 ->setUser($admin)
                 ->setOrganization($organization);
+
             $admin->addApiKey($api);
         }
 
