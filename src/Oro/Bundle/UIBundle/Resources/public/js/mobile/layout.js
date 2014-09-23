c50bc947fa596535ffe34c34479207761901bd90
Hryhorii Hrebiniuk <grebinuk@gmail.com>
2014-09-23 19:07:13 +0300

    CRM-1561: Mobile version. Page header is shifted

--- src/Oro/Bundle/UIBundle/Resources/public/js/mobile/layout.js
+++ src/Oro/Bundle/UIBundle/Resources/public/js/mobile/layout.js
@@ -4,6 +4,7 @@
 define(function (require) {
     'use strict';
     var $ = require('jquery'),
+        _ = require('underscore'),
         pageHeader = require('oroui/js/mobile/page-header');
     require('oroui/js/mobile/side-menu');
 
@@ -19,9 +20,30 @@ define(function (require) {
     }
 
     /**
+     * Fixes issue when header with position fixed loses its place after blur event on some input
+     *
+     * @see http://dansajin.com/2012/12/07/fix-position-fixed/
+     * @see http://stackoverflow.com/questions/14492613/ios-ipad-fixed-position-breaks-when-keyboard-is-opened
+     */
+    function fixFixedHeader() {
+        var $body = $('body');
+        $(document)
+            .on('focus', ':input', function () {
+                $body.addClass('fixfixed');
+            })
+            .on('blur', ':input', function () {
+                $body.removeClass('fixfixed');
+                _.debounce(function () {
+                    $(document).scrollTop($(document).scrollTop());
+                }, 1);
+            });
+    }
+
+    /**
      * Initiate mobile layout
      */
     function initLayout() {
+        fixFixedHeader();
         initMainMenu();
         pageHeader.init();
     }
