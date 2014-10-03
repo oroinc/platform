f75c0119bd1dc9684a36080f75b719af0ea72e39
Yurii Muratov <yurio@ua.fm>
2014-10-03 15:13:31 +0300

    BAP-3724: Add email owners to search index   - Fix cs

--- src/Oro/Bundle/EmailBundle/EventListener/SearchListener.php
+++ src/Oro/Bundle/EmailBundle/EventListener/SearchListener.php
@@ -12,19 +12,20 @@ use Oro\Bundle\OrganizationBundle\Entity\OrganizationAwareInterface;
 class SearchListener
 {
     const EMAIL_CLASS_NAME = 'Oro\Bundle\EmailBundle\Entity\Email';
+    const EMPTY_ORGANIZATION_ID = 0;
 
     /**
      * @param PrepareEntityMapEvent $event
      */
     public function prepareEntityMapEvent(PrepareEntityMapEvent $event)
     {
-        $data = $event->getData();
+        $data      = $event->getData();
         $className = $event->getClassName();
         if ($className === self::EMAIL_CLASS_NAME) {
             /** @var $entity Email */
-            $entity = $event->getEntity();
+            $entity          = $event->getEntity();
             $organizationsId = [];
-            $recipients = $entity->getRecipients();
+            $recipients      = $entity->getRecipients();
             /** @var  $recipient EmailRecipient */
             foreach ($recipients as $recipient) {
                 $owner = $recipient->getEmailAddress()->getOwner();
@@ -32,10 +33,13 @@ class SearchListener
                     $organizationsId[] = $owner->getOrganization()->getId();
                 }
             }
+            if (!isset ($data['integer'])) {
+                $data['integer'] = [];
+            }
             if (!empty($organizationsId)) {
                 $data['integer']['organization'] = array_unique($organizationsId);
             } else {
-                $data['integer']['organization'] = 0;
+                $data['integer']['organization'] = self::EMPTY_ORGANIZATION_ID;
             }
         }
 
