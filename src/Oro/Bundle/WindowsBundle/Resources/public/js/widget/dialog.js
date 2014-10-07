a8070a2612dc18cc4116d21f502e24079717acfd
dzyunchik <dzyunchik3@gmail.com>
2014-10-07 17:05:20 +0300

    bug/BAP-5396: clean dialog widget and add calling layout.updateResponsiveLayout

--- src/Oro/Bundle/WindowsBundle/Resources/public/js/widget/dialog.js
+++ src/Oro/Bundle/WindowsBundle/Resources/public/js/widget/dialog.js
@@ -1,9 +1,8 @@
 /*global define*/
 define(['jquery', 'underscore', 'oroui/js/tools', 'oroui/js/error',
-        'oroui/js/widget/abstract', 'orowindows/js/dialog/state/model', 'oroui/js/messenger',
-        'orotranslation/js/translator', 'jquery.dialog.extended',
-        'oroui/js/responsive-jquery-widget'
-    ], function ($, _, tools, error, AbstractWidget, StateModel, messenger, __) {
+        'oroui/js/widget/abstract', 'orowindows/js/dialog/state/model', 'oroui/js/messenger', 'oroui/js/layout',
+        'orotranslation/js/translator', 'jquery.dialog.extended'
+    ], function ($, _, tools, error, AbstractWidget, StateModel, messenger, layout, __) {
     'use strict';
 
     /**
@@ -263,6 +262,7 @@ define(['jquery', 'underscore', 'oroui/js/tools', 'oroui/js/error',
                     $el.height(height);
                 }
             },this));
+            layout.updateResponsiveLayout();
         },
 
         /**
