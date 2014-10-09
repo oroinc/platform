046e25cc6fcf0446413a4068f76d99295b6e26c3
Yevhen Shyshkin <shishkin.yevgeniy@gmail.com>
2014-10-09 20:29:21 +0300

    BAP-5564: Add console command listener to initialize security context in CLI  - added console token  - added console context listener  - added unit tests

---
+++ src/Oro/Bundle/SecurityBundle/Tests/Unit/Authentication/Token/ConsoleTokenTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Oro\Bundle\SecurityBundle\Tests\Unit\Authentication\Token;
+
+use Oro\Bundle\OrganizationBundle\Entity\Organization;
+use Oro\Bundle\SecurityBundle\Authentication\Token\ConsoleToken;
+
+class ConsoleTokenTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var ConsoleToken
+     */
+    protected $token;
+
+    protected function setUp()
+    {
+        $this->token = new ConsoleToken();
+    }
+
+    public function testGetCredentials()
+    {
+        $this->assertEmpty($this->token->getCredentials());
+    }
+
+    public function testSetGetOrganizationContext()
+    {
+        $this->assertEmpty($this->token->getOrganizationContext());
+
+        $organization = new Organization();
+        $organization->setName('test');
+
+        $this->token->setOrganizationContext($organization);
+
+        $this->assertEquals($organization, $this->token->getOrganizationContext());
+    }
+}
