30067f854a22169a0942c92a3f223c4bf457b593
Pavel Kargapoltsev <pkargapoltsev@magecore.com>
2014-10-24 13:11:29 +0300

    BAP-5729: Set event to User Calendar - set default calendar for User target entity in event - update base widget datagrid

--- src/Oro/Bundle/CalendarBundle/Form/Handler/CalendarEventHandler.php
+++ src/Oro/Bundle/CalendarBundle/Form/Handler/CalendarEventHandler.php
@@ -4,12 +4,16 @@ namespace Oro\Bundle\CalendarBundle\Form\Handler;
 
 use Doctrine\Common\Persistence\ObjectManager;
 
+use Symfony\Bundle\SecurityBundle\SecurityBundle;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 
 use Oro\Bundle\ActivityBundle\Manager\ActivityManager;
 use Oro\Bundle\EntityBundle\Tools\EntityRoutingHelper;
 use Oro\Bundle\CalendarBundle\Entity\CalendarEvent;
+use Oro\Bundle\CalendarBundle\Entity\Calendar;
+use Oro\Bundle\UserBundle\Entity\User;
+use Oro\Bundle\SecurityBundle\SecurityFacade;
 
 class CalendarEventHandler
 {
@@ -28,25 +32,31 @@ class CalendarEventHandler
     /** @var EntityRoutingHelper */
     protected $entityRoutingHelper;
 
+    /** @var SecurityFacade */
+    protected $securityFacade;
+
     /**
      * @param FormInterface       $form
      * @param Request             $request
      * @param ObjectManager       $manager
      * @param ActivityManager     $activityManager
      * @param EntityRoutingHelper $entityRoutingHelper
+     * @param SecurityFacade      $securityFacade
      */
     public function __construct(
         FormInterface $form,
         Request $request,
         ObjectManager $manager,
         ActivityManager $activityManager,
-        EntityRoutingHelper $entityRoutingHelper
+        EntityRoutingHelper $entityRoutingHelper,
+        SecurityFacade $securityFacade
     ) {
         $this->form                = $form;
         $this->request             = $request;
         $this->manager             = $manager;
         $this->activityManager     = $activityManager;
         $this->entityRoutingHelper = $entityRoutingHelper;
+        $this->securityFacade      = $securityFacade;
     }
 
     /**
@@ -77,10 +87,15 @@ class CalendarEventHandler
                 $targetEntityClass = $this->request->get('entityClass');
                 if ($targetEntityClass) {
                     $targetEntityId = $this->request->get('entityId');
-                    $this->activityManager->addActivityTarget(
-                        $entity,
-                        $this->entityRoutingHelper->getEntityReference($targetEntityClass, $targetEntityId)
-                    );
+                    $targetEntity = $this->entityRoutingHelper->getEntityReference($targetEntityClass, $targetEntityId);
+                    $this->activityManager->addActivityTarget($entity, $targetEntity);
+                    if ($targetEntity instanceof User && $targetEntityId != $this->securityFacade->getLoggedUserId()) {
+                        /** @var Calendar $defaultCalendar */
+                        $defaultCalendar = $this->manager
+                            ->getRepository('OroCalendarBundle:Calendar')
+                            ->findDefaultCalendar($targetEntity->getId(), $targetEntity->getOrganization()->getId());
+                        $entity->setCalendar($defaultCalendar);
+                    }
                 }
 
                 $this->onSuccess($entity);
