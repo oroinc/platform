d319787d532e6919acdf91f10955b59b107d7ccd
Vova Soroka <vsoroka@magecore.com>
2014-10-27 14:54:02 +0200

    BAP-5748: Create skeleton for ActivityListBundle

---
+++ src/Oro/Bundle/ActivityListBundle/Controller/ActivityListController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Oro\Bundle\ActivityListBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+/**
+ * @Route("/activity-list")
+ */
+class ActivityListController extends Controller
+{
+}
---
+++ src/Oro/Bundle/ActivityListBundle/Migrations/Schema/OroActivityListBundleInstaller.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Oro\Bundle\ActivityListBundle\Migrations\Schema;
+
+use Doctrine\DBAL\Schema\Schema;
+
+use Oro\Bundle\MigrationBundle\Migration\Installation;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+
+class OroActivityListBundleInstaller implements Installation
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getMigrationVersion()
+    {
+        return 'v1_0';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function up(Schema $schema, QueryBag $queries)
+    {
+    }
+}
