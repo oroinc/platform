7dd79c2dfa90cdc20299038f3bc10ab3ce5dd3dc
Vova Soroka <vsoroka@magecore.com>
2014-10-29 20:37:23 +0200

    BAP-5876: Please add limit and page filter parameters CalendarEventController::cgetAction REST API

--- src/Oro/Bundle/CalendarBundle/Entity/Repository/CalendarEventRepository.php
+++ src/Oro/Bundle/CalendarBundle/Entity/Repository/CalendarEventRepository.php
@@ -9,27 +9,29 @@ use Doctrine\ORM\QueryBuilder;
 class CalendarEventRepository extends EntityRepository
 {
     /**
-     * Returns a query builder which can be used to get a list of calendar events
+     * Returns a query builder which can be used to get a list of calendar events filtered by start and end dates
      *
-     * @param int       $calendarId
-     * @param \DateTime $startDate                   Start date
-     * @param \DateTime $endDate                     End date
-     * @param bool      $includingConnectedCalendars If true events from connected calendars will be returned as well
-     * @param array     $criteria                    Additional criteria
+     * @param int            $calendarId
+     * @param \DateTime      $startDate       Start date
+     * @param \DateTime      $endDate         End date
+     * @param bool           $withConnections If true events from connected calendars will be returned as well
+     * @param array|Criteria $filters         Additional filtering criteria, e.g. ['allDay' => true, ...]
+     *                                        or \Doctrine\Common\Collections\Criteria
+     *
      * @return QueryBuilder
      */
-    public function getEventListQueryBuilder(
+    public function getEventListByTimeIntervalQueryBuilder(
         $calendarId,
         $startDate,
         $endDate,
-        $includingConnectedCalendars = false,
-        $criteria = array()
+        $withConnections = false,
+        $filters = []
     ) {
+        $qb = $this->getEventListQueryBuilder($calendarId, $withConnections, $filters);
+
         /** @var QueryBuilder $qb */
-        $qb = $this->createQueryBuilder('e')
-            ->select('c.id as calendar, e.id, e.title, e.description, e.start, e.end, e.allDay')
-            ->innerJoin('e.calendar', 'c')
-            ->where(
+        $qb
+            ->andWhere(
                 '(e.start < :start AND e.end >= :start) OR '
                 . '(e.start <= :end AND e.end > :end) OR'
                 . '(e.start >= :start AND e.end < :end)'
@@ -37,20 +39,45 @@ class CalendarEventRepository extends EntityRepository
             ->orderBy('c.id, e.start')
             ->setParameter('start', $startDate)
             ->setParameter('end', $endDate);
-        if (is_array($criteria)) {
+
+        return $qb;
+    }
+
+    /**
+     * Returns a query builder which can be used to get a list of calendar events
+     *
+     * @param int            $calendarId
+     * @param bool           $withConnections If true events from connected calendars will be returned as well
+     * @param array|Criteria $filters         Additional filtering criteria, e.g. ['allDay' => true, ...]
+     *                                        or \Doctrine\Common\Collections\Criteria
+     *
+     * @return QueryBuilder
+     */
+    public function getEventListQueryBuilder(
+        $calendarId,
+        $withConnections = false,
+        $filters = []
+    ) {
+        /** @var QueryBuilder $qb */
+        $qb = $this->createQueryBuilder('e')
+            ->select(
+                'c.id as calendar, e.id, e.title, e.description, e.start, e.end, e.allDay, e.createdAt, e.updatedAt'
+            )
+            ->innerJoin('e.calendar', 'c');
+        if (is_array($filters)) {
             $newCriteria = new Criteria();
-            foreach ($criteria as $fieldName => $value) {
+            foreach ($filters as $fieldName => $value) {
                 $newCriteria->andWhere(Criteria::expr()->eq($fieldName, $value));
             }
 
-            $criteria = $newCriteria;
+            $filters = $newCriteria;
         }
-        if ($criteria) {
-            $qb->addCriteria($criteria);
+        if ($filters) {
+            $qb->addCriteria($filters);
         }
-        if ($includingConnectedCalendars) {
-            $calendarRepo = $this->getEntityManager()->getRepository('OroCalendarBundle:Calendar');
-            $qbAC         = $calendarRepo->createQueryBuilder('c1')
+        if ($withConnections) {
+            $calendarRepo  = $this->getEntityManager()->getRepository('OroCalendarBundle:Calendar');
+            $qbConnections = $calendarRepo->createQueryBuilder('c1')
                 ->select('ac.id')
                 ->innerJoin('c1.connections', 'a')
                 ->innerJoin('a.connectedCalendar', 'ac')
@@ -58,7 +85,7 @@ class CalendarEventRepository extends EntityRepository
                 ->setParameter('id', $calendarId);
 
             $qb
-                ->andWhere($qb->expr()->in('c.id', $qbAC->getDQL()))
+                ->andWhere($qb->expr()->in('c.id', $qbConnections->getDQL()))
                 ->setParameter('id', $calendarId);
         } else {
             $qb
