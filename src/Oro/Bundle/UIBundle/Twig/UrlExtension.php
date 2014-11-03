e51c76d81987027a58efe4f7641212182f23eb8b
Yevhen Shyshkin <shishkin.yevgeniy@gmail.com>
2014-11-03 13:09:28 +0200

    BAP-5888: Add entity pagination parameters to edit button  - added twig extension to handle URLs  - updated UpdataHandler to add query parameters to URL  - updated twig templates to add query parameters to URL

---
+++ src/Oro/Bundle/UIBundle/Twig/UrlExtension.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Oro\Bundle\UIBundle\Twig;
+
+use Symfony\Component\HttpFoundation\Request;
+
+class UrlExtension extends \Twig_Extension
+{
+    const NAME = 'oro_ui_url';
+
+    /**
+     * @var Request
+     */
+    protected $request;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return self::NAME;
+    }
+
+    public function getFunctions()
+    {
+        return [
+            new \Twig_SimpleFunction('oro_url_add_query', array($this, 'addQuery')),
+        ];
+    }
+
+    /**
+     * @param string $link
+     * @return string
+     */
+    public function addQuery($link)
+    {
+        if (!$this->request) {
+            return $link;
+        }
+
+        $parts = parse_url($link);
+        $urlQueryParts = [];
+        if (!empty($parts['query'])) {
+            parse_str($parts['query'], $urlQueryParts);
+        }
+
+        $requestQueryParts = $this->request->query->all();
+        if ($requestQueryParts && $requestQueryParts != $urlQueryParts) {
+            $mergedQueryParts = array_merge($requestQueryParts, $urlQueryParts);
+            $basicUrlPart = $parts['path'];
+            if (!empty($parts['query'])) {
+                $basicUrlPart .= '?' . $parts['query'];
+            }
+            $updatedUrlPart = $parts['path'] . '?' . http_build_query($mergedQueryParts);
+            $link = str_replace($basicUrlPart, $updatedUrlPart, $link);
+        }
+
+        return $link;
+    }
+
+    /**
+     * @param Request $request
+     */
+    public function setRequest(Request $request = null)
+    {
+        $this->request = $request;
+    }
+}
