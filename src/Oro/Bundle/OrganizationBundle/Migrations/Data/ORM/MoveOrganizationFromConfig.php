3a4d53c07a84e22c5d7179fb21db610b3a3301bd
Andrey Mishchenko <amishchenko@magecore.com>
2014-11-07 18:38:30 +0200

    BAP-5697: After upgrade tenant lost organization name     - added MoveOrganizationFromConfig migration

--- src/Oro/Bundle/OrganizationBundle/Migrations/Data/ORM/MoveOrganizationFromConfig.php
+++ src/Oro/Bundle/OrganizationBundle/Migrations/Data/ORM/MoveOrganizationFromConfig.php
@@ -1,17 +1,12 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: amishchenko
- * Date: 07.11.14
- * Time: 16:21
- */
-
 namespace Oro\Bundle\OrganizationBundle\Migrations\Data\ORM;
 
 
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\Persistence\ObjectManager;
+
 use Oro\Bundle\OrganizationBundle\Entity\Organization;
+
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -24,40 +19,35 @@ class MoveOrganizationFromConfig extends AbstractFixture implements ContainerAwa
     protected $container;
 
     /**
-     * Load data fixtures with the passed EntityManager
-     *
-     * @param Doctrine\Common\Persistence\ObjectManager $manager
+     * {@inheritdoc}
      */
-    function load(ObjectManager $manager)
+    public function load(ObjectManager $manager)
     {
+        $configManager = $this->container->get('oro_config.global');
+        $applicationTitle = $configManager->get('oro_ui.application_title');
+
+        if (!$applicationTitle) {
+            return;
+        }
+
         $repo = $manager->getRepository('OroOrganizationBundle:Organization');
         /** @var Organization $organization */
         $organization = $repo->findOneBy(['name' => LoadOrganizationAndBusinessUnitData::MAIN_ORGANIZATION]);
 
-        if (!$organization)
+        if (!$organization) {
             return;
-
-        $configManager = $this->container->get('oro_config.global');
-        $applicationTitle = $configManager->get('oro_ui.application_title');
-
-        if (!$applicationTitle)
-            return;
+        }
 
         $organization->setName($applicationTitle);
-        $manager->persist($organization);
         $manager->flush();
     }
 
+
     /**
-     * Sets the Container.
-     *
-     * @param ContainerInterface|null $container A ContainerInterface instance or null
-     *
-     * @api
+     * {@inheritdoc}
      */
     public function setContainer(ContainerInterface $container = null)
     {
         $this->container = $container;
     }
-
 }
