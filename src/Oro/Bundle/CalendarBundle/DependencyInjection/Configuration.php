2f73649ead31367cb16de3e2eb70b1cd19a5bc6d
Vova Soroka <vsoroka@magecore.com>
2014-11-14 03:57:26 +0200

    BAP-5854: Add to System configuration ability to define predefined colors into System Configuration

--- src/Oro/Bundle/CalendarBundle/DependencyInjection/Configuration.php
+++ src/Oro/Bundle/CalendarBundle/DependencyInjection/Configuration.php
@@ -2,9 +2,10 @@
 
 namespace Oro\Bundle\CalendarBundle\DependencyInjection;
 
-use Oro\Bundle\ConfigBundle\DependencyInjection\SettingsBuilder;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+use Oro\Bundle\ConfigBundle\DependencyInjection\SettingsBuilder;
 
 class Configuration implements ConfigurationInterface
 {
