450ffb4166ef5648f266df52c4b1af14b204faad
Andrii Muzalevskyi <amuzalevsky@magecore.com>
2014-11-19 15:17:32 +0200

    BAP-5622 Search results on mobile

--- src/Oro/Bundle/SearchBundle/Controller/SearchController.php
+++ src/Oro/Bundle/SearchBundle/Controller/SearchController.php
@@ -72,6 +72,7 @@ class SearchController extends Controller
             'from'           => $from,
             'searchString'   => $string,
             'groupedResults' => $resultProvider->getGroupedResults($string),
+            'entities'     => $this->get('oro_search.index')->getAllowedEntitiesListAliases(),
         );
     }
 }
