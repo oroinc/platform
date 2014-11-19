fdb739a12d8a06112577e437f07beaa9a2b43c69
Pavel Kargapoltsev <pkargapoltsev@magecore.com>
2014-11-19 16:25:24 +0200

    BAP-6155: Update calendar config and user calendar choose form type - updated datagrid for choose user select2 - fixed navigation bundle configbuildertest - updated autocompliter test

--- src/Oro/Bundle/CalendarBundle/Autocomplete/UserCalendarHandler.php
+++ src/Oro/Bundle/CalendarBundle/Autocomplete/UserCalendarHandler.php
@@ -2,12 +2,66 @@
 
 namespace Oro\Bundle\CalendarBundle\Autocomplete;
 
-use Symfony\Component\Security\Core\User\UserInterface;
-
+use Oro\Bundle\SecurityBundle\Acl\Domain\OneShotIsGrantedObserver;
 use Oro\Bundle\UserBundle\Autocomplete\UserAclHandler;
 
 class UserCalendarHandler extends UserAclHandler
 {
+    /**
+     * {@inheritdoc}
+     *
+     * @SuppressWarnings(PHPMD.NPathComplexity)
+     */
+    public function search($query, $page, $perPage, $searchById = false)
+    {
+        list ($search, $entityClass, $permission, $entityId, $excludeCurrentUser) = explode(';', $query);
+        $entityClass = $this->decodeClassName($entityClass);
+
+        $hasMore  = false;
+        $object   = $entityId
+            ? $this->em->getRepository($entityClass)->find((int)$entityId)
+            : 'entity:' . $entityClass;
+        $observer = new OneShotIsGrantedObserver();
+        $this->aclVoter->addOneShotIsGrantedObserver($observer);
+        if ($this->getSecurityContext()->isGranted($permission, $object)) {
+            $results = [];
+            if ($searchById) {
+                $results[] = $this->em->getRepository($entityClass)->find((int)$query);
+            } else {
+                $page        = (int)$page > 0 ? (int)$page : 1;
+                $perPage     = (int)$perPage > 0 ? (int)$perPage : 10;
+                $firstResult = ($page - 1) * $perPage;
+                $perPage += 1;
+
+                $user         = $this->getSecurityContext()->getToken()->getUser();
+                $organization = $this->getSecurityContext()->getToken()->getOrganizationContext();
+                $queryBuilder = $this->getSearchQueryBuilder($search);
+                if ((boolean)$excludeCurrentUser) {
+                    $this->excludeUser($queryBuilder, $user);
+                }
+                $queryBuilder
+                    ->setFirstResult($firstResult)
+                    ->setMaxResults($perPage);
+                $this->addAcl($queryBuilder, $observer->getAccessLevel(), $user, $organization);
+                $results = $queryBuilder->getQuery()->getResult();
+
+                $hasMore = count($results) == $perPage;
+            }
+
+            $resultsData = [];
+            foreach ($results as $user) {
+                $resultsData[] = $this->convertItem($user);
+            }
+        } else {
+            $resultsData = [];
+        }
+
+        return [
+            'results' => $resultsData,
+            'more'    => $hasMore
+        ];
+    }
+
     /**
      * {@inheritdoc}
      */
@@ -26,12 +80,7 @@ class UserCalendarHandler extends UserAclHandler
      */
     public function convertItem($calendar)
     {
-        if ($calendar instanceof UserInterface) {
-            $result = parent::convertItem($calendar);
-        } else {
-            $result = parent::convertItem($calendar->getOwner());
-        }
-
+        $result = parent::convertItem($calendar->getOwner());
         $result['id'] = $calendar->getId();
 
         return $result;
