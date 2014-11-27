e512ca758c346fc63e149089bd1ee17c215ab7de
Vova Soroka <vsoroka@magecore.com>
2014-11-27 05:14:46 +0200

    BAP-6622: IMAP Sync failed with exception

--- src/Oro/Bundle/ImapBundle/Tests/Unit/Manager/ImapEmailManagerTest.php
+++ src/Oro/Bundle/ImapBundle/Tests/Unit/Manager/ImapEmailManagerTest.php
@@ -2,8 +2,9 @@
 
 namespace Oro\Bundle\ImapBundle\Tests\Unit\Manager\DTO;
 
+use Zend\Mail\Header\HeaderInterface;
+
 use Oro\Bundle\ImapBundle\Connector\ImapMessageIterator;
-use Oro\Bundle\ImapBundle\Manager\ImapEmailIterator;
 use Oro\Bundle\ImapBundle\Manager\ImapEmailManager;
 
 class ImapEmailManagerTest extends \PHPUnit_Framework_TestCase
@@ -36,76 +37,49 @@ class ImapEmailManagerTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
+     * @dataProvider getEmailsProvider
      * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
      */
-    public function testGetEmails()
+    public function testGetEmails($strDate)
     {
-        $uid = $this->getMock('Zend\Mail\Header\HeaderInterface');
-        $uid->expects($this->once())->method('getFieldValue')->will($this->returnValue('123'));
-
-        $subject = $this->getMock('Zend\Mail\Header\HeaderInterface');
-        $subject->expects($this->once())->method('getFieldValue')->will($this->returnValue('Subject'));
-
-        $fromEmail = $this->getMock('Zend\Mail\Header\HeaderInterface');
-        $fromEmail->expects($this->once())->method('getFieldValue')->will($this->returnValue('fromEmail'));
-
-        $date = $this->getMock('Zend\Mail\Header\HeaderInterface');
-        $date->expects($this->once())->method('getFieldValue')->will($this->returnValue('2011-06-30 23:59:59 +0'));
-
-        $received = $this->getMock('Zend\Mail\Header\HeaderInterface');
-        $received->expects($this->once())->method('getFieldValue')
-            ->will($this->returnValue('by server to email; 2012-06-30 23:59:59 +0'));
-
-        $intDate = $this->getMock('Zend\Mail\Header\HeaderInterface');
-        $intDate->expects($this->once())->method('getFieldValue')->will($this->returnValue('2013-06-30 23:59:59 +0'));
-
-        $messageId = $this->getMock('Zend\Mail\Header\HeaderInterface');
-        $messageId->expects($this->once())->method('getFieldValue')->will($this->returnValue('MessageId'));
-
-        $xMsgId = $this->getMock('Zend\Mail\Header\HeaderInterface');
-        $xMsgId->expects($this->once())->method('getFieldValue')->will($this->returnValue('XMsgId'));
-
-        $xThrId = $this->getMock('Zend\Mail\Header\HeaderInterface');
-        $xThrId->expects($this->once())->method('getFieldValue')->will($this->returnValue('XThrId'));
-
         $toAddress = $this->getMock('Zend\Mail\Address\AddressInterface');
         $toAddress->expects($this->once())->method('toString')->will($this->returnValue('toEmail'));
         $toAddressList = $this->getMockForAbstractClass(
             'Zend\Mail\Header\AbstractAddressList',
-            array(),
+            [],
             '',
             false,
             false,
             true,
-            array('getAddressList')
+            ['getAddressList']
         );
-        $toAddressList->expects($this->once())->method('getAddressList')->will($this->returnValue(array($toAddress)));
+        $toAddressList->expects($this->once())->method('getAddressList')->will($this->returnValue([$toAddress]));
 
         $ccAddress = $this->getMock('Zend\Mail\Address\AddressInterface');
         $ccAddress->expects($this->once())->method('toString')->will($this->returnValue('ccEmail'));
         $ccAddressList = $this->getMockForAbstractClass(
             'Zend\Mail\Header\AbstractAddressList',
-            array(),
+            [],
             '',
             false,
             false,
             true,
-            array('getAddressList')
+            ['getAddressList']
         );
-        $ccAddressList->expects($this->once())->method('getAddressList')->will($this->returnValue(array($ccAddress)));
+        $ccAddressList->expects($this->once())->method('getAddressList')->will($this->returnValue([$ccAddress]));
 
         $bccAddress = $this->getMock('Zend\Mail\Address\AddressInterface');
         $bccAddress->expects($this->once())->method('toString')->will($this->returnValue('bccEmail'));
         $bccAddressList = $this->getMockForAbstractClass(
             'Zend\Mail\Header\AbstractAddressList',
-            array(),
+            [],
             '',
             false,
             false,
             true,
-            array('getAddressList')
+            ['getAddressList']
         );
-        $bccAddressList->expects($this->once())->method('getAddressList')->will($this->returnValue(array($bccAddress)));
+        $bccAddressList->expects($this->once())->method('getAddressList')->will($this->returnValue([$bccAddress]));
 
         $this->connector->expects($this->once())
             ->method('getUidValidity')
@@ -123,22 +97,22 @@ class ImapEmailManagerTest extends \PHPUnit_Framework_TestCase
             ->method('get')
             ->will(
                 $this->returnValueMap(
-                    array(
-                        array('UID', $uid),
-                        array('Subject', $subject),
-                        array('From', $fromEmail),
-                        array('Date', $date),
-                        array('Received', $received),
-                        array('InternalDate', $intDate),
-                        array('Importance', false),
-                        array('Message-ID', $messageId),
-                        array('X-GM-MSG-ID', $xMsgId),
-                        array('X-GM-THR-ID', $xThrId),
-                        array('X-GM-LABELS', false),
-                        array('To', $toAddressList),
-                        array('Cc', $ccAddressList),
-                        array('Bcc', $bccAddressList),
-                    )
+                    [
+                        ['UID', $this->getHeader('123')],
+                        ['Subject', $this->getHeader('Subject')],
+                        ['From', $this->getHeader('fromEmail')],
+                        ['Date', $this->getHeader($strDate)],
+                        ['Received', $this->getHeader('by server to email; ' . str_replace('59:', '58:', $strDate))],
+                        ['InternalDate', $this->getHeader(str_replace('59:', '57:', $strDate))],
+                        ['Importance', false],
+                        ['Message-ID', $this->getHeader('MessageId')],
+                        ['X-GM-MSG-ID', $this->getHeader('XMsgId')],
+                        ['X-GM-THR-ID', $this->getHeader('XThrId')],
+                        ['X-GM-LABELS', false],
+                        ['To', $toAddressList],
+                        ['Cc', $ccAddressList],
+                        ['Bcc', $bccAddressList],
+                    ]
                 )
             );
 
@@ -150,7 +124,7 @@ class ImapEmailManagerTest extends \PHPUnit_Framework_TestCase
             ->disableOriginalConstructor()
             ->getMock();
         $imap->expects($this->any())->method('getMessage')->will($this->returnValue($msg));
-        $messageIterator = new ImapMessageIterator($imap, array(1));
+        $messageIterator = new ImapMessageIterator($imap, [1]);
         $this->connector->expects($this->once())
             ->method('findItems')
             ->with($this->identicalTo($query))
@@ -172,11 +146,11 @@ class ImapEmailManagerTest extends \PHPUnit_Framework_TestCase
             $email->getSentAt()
         );
         $this->assertEquals(
-            new \DateTime('2012-06-30 23:59:59', new \DateTimeZone('UTC')),
+            new \DateTime('2011-06-30 23:58:59', new \DateTimeZone('UTC')),
             $email->getReceivedAt()
         );
         $this->assertEquals(
-            new \DateTime('2013-06-30 23:59:59', new \DateTimeZone('UTC')),
+            new \DateTime('2011-06-30 23:57:59', new \DateTimeZone('UTC')),
             $email->getInternalDate()
         );
         $this->assertEquals(0, $email->getImportance());
@@ -189,5 +163,27 @@ class ImapEmailManagerTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('ccEmail', $ccRecipients[0]);
         $bccRecipients = $email->getBccRecipients();
         $this->assertEquals('bccEmail', $bccRecipients[0]);
+    }
+
+    public function getEmailsProvider()
+    {
+        return [
+            ['Fri, 31 Jun 2011 10:59:59 +1100'],
+            ['Fri, 31 Jun 2011 10:59:59 +11:00 (GMT+11:00)'],
+            ['Fri, 31 06 2011 10:59:59 +1100'],
+        ];
+    }
+
+    /**
+     * @return HeaderInterface
+     */
+    protected function getHeader($value)
+    {
+        $header = $this->getMock('Zend\Mail\Header\HeaderInterface');
+        $header->expects($this->once())
+            ->method('getFieldValue')
+            ->will($this->returnValue($value));
+
+        return $header;
     }
 }
---
+++ src/Oro/Bundle/ImapBundle/Tests/Unit/Util/DateTimeParserTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Oro\Bundle\ImapBundle\Tests\Unit\Util;
+
+use Oro\Bundle\ImapBundle\Util\DateTimeParser;
+
+class DateTimeParserTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider parseProvider
+     */
+    public function testParse($strDate)
+    {
+        $this->assertEquals(
+            new \DateTime('2011-06-30 23:59:59', new \DateTimeZone('UTC')),
+            DateTimeParser::parse($strDate)
+        );
+    }
+
+    public function parseProvider()
+    {
+        return [
+            ['Thu, 30 Jun 2011 23:59:59'],
+            ['Thu, 30 Jun 2011 23:59:59 GMT'],
+            ['Thu, 30 Jun 2011 23:59:59 UTC'],
+            ['Thu, 30 Jun 2011 23:59:59 UT'],
+            ['Thu, 30 Jun 2011 23:59:59 UT (Universal Time)'],
+            ['Thu, 30 Jun 2011 12:59:59 -1100'],
+            ['Fri, 31 Jun 2011 10:59:59 +1100'],
+            ['Fri, 31 Jun 2011 10:59:59 +1100 (AEDT)'],
+            ['Fri, 31 Jun 2011 10:59:59 +1100 (GMT+11:00)'],
+            ['Fri, 31 Jun 2011 10:59:59 +1100 (Australian Eastern Daylight Time)'],
+            ['Fri, 31 Jun 2011 10:59:59 +11:00 (AEDT)'],
+            ['Fri, 31 Jun 2011 10:59:59 +11:00 (GMT+11:00)'],
+            ['Fri, 31 Jun 2011 10:59:59 +11:00 (Australian Eastern Daylight Time)'],
+            ['Thu, 30 06 2011 23:59:59'],
+            ['Fri, 31 06 2011 10:59:59 +1100'],
+            ['Fri, 31 06 2011 10:59:59 +1100 (GMT+11:00)'],
+        ];
+    }
+
+    /**
+     * @dataProvider parseFailureProvider
+     */
+    public function testParseFailure($strDate, $exceptionMessage)
+    {
+        $this->setExpectedException('\InvalidArgumentException', $exceptionMessage);
+        DateTimeParser::parse($strDate);
+    }
+
+    public function parseFailureProvider()
+    {
+        return [
+            [
+                'invalid',
+                'Failed to parse time string "invalid" at position 0: The timezone could not be found in the database.'
+            ],
+            [
+                'Fri, 31 June 2011 10:59:aa +1100',
+                'Failed to parse time string "Fri, 31 June 2011 10:59:aa +1100" at position 23: Unexpected character.'
+            ],
+            [
+                'Fri, 31 06 2011 10:59:aa +1100',
+                'Failed to parse time string "Fri, 31 06 2011 10:59:aa +1100" at position 5: Unexpected character.'
+            ],
+        ];
+    }
+}
