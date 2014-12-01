fb3f7f48b06ffcb6d5bd0f9f1a9900e290bc2591
Vova Soroka <vsoroka@magecore.com>
2014-12-01 19:27:15 +0200

    BAP-5990: Create view page for system calendars

--- src/Oro/Bundle/CalendarBundle/Controller/SystemCalendarEventController.php
+++ src/Oro/Bundle/CalendarBundle/Controller/SystemCalendarEventController.php
@@ -17,25 +17,6 @@ use Oro\Bundle\CalendarBundle\Entity\CalendarEvent;
 class SystemCalendarEventController extends Controller
 {
     /**
-     * @Route("/{id}/event", name="oro_system_calendar_event_index", requirements={"id"="\d+"})
-     * @Template
-     */
-    public function indexAction(SystemCalendar $entity)
-    {
-        $this->checkPermissionByConfig($entity);
-
-        if (!$entity->isPublic() && !$this->get('oro_security.security_facade')->isGranted('VIEW', $entity)) {
-            throw new AccessDeniedException('Access denied to system calendar events from another organization');
-        }
-
-        return [
-            'params'        => ['calendarId' => $entity->getId()],
-            'gridName'      => $entity->isPublic() ? 'public-system-calendar-event-grid' : 'system-calendar-event-grid',
-            'entity_class'  => $this->container->getParameter('oro_calendar.calendar_event.entity.class')
-        ];
-    }
-
-    /**
      * @Route("/event/view/{id}", name="oro_system_calendar_event_view", requirements={"id"="\d+"})
      * @Template
      * @AclAncestor("oro_calendar_event_view")
