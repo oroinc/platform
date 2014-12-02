c401d5a89ab54ffe54078c8ccd24cba75fc79f99
Valentine Velikiy <vvelykyi@magecore.com>
2014-12-02 20:07:46 +0200

    BAP-6575: Add Manage System Calendar Events ACL action. - Added check ACL permissions to normalizers. - Added unit tests.

---
+++ src/Oro/Bundle/CalendarBundle/Tests/Unit/Handler/DeleteHandlerTest.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace Oro\Bundle\CalendarBundle\Tests\Unit\Handler;
+
+use Oro\Bundle\CalendarBundle\Entity\SystemCalendar;
+use Oro\Bundle\CalendarBundle\Handler\DeleteHandler;
+
+class DeleteHandlerTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var \PHPUnit_Framework_MockObject_MockObject */
+    protected $securityFacade;
+
+    /** @var \PHPUnit_Framework_MockObject_MockObject */
+    protected $calendarConfig;
+
+    /** @var \PHPUnit_Framework_MockObject_MockObject */
+    protected $manager;
+
+    /** @var  DeleteHandler */
+    protected $handler;
+
+    protected function setUp()
+    {
+        $this->securityFacade = $this->getMockBuilder('Oro\Bundle\SecurityBundle\SecurityFacade')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->calendarConfig = $this->getMockBuilder('Oro\Bundle\CalendarBundle\Provider\SystemCalendarConfig')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->manager = $this->getMockBuilder('Oro\Bundle\SoapBundle\Entity\Manager\ApiEntityManager')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $objectManager = $this->getMockBuilder('Doctrine\Common\Persistence\ObjectManager')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->manager->expects($this->any())
+            ->method('getObjectManager')
+            ->will($this->returnValue($objectManager));
+        $ownerDeletionManager = $this->getMockBuilder('Oro\Bundle\OrganizationBundle\Ownership\OwnerDeletionManager')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->handler = new DeleteHandler();
+        $this->handler->setCalendarConfig($this->calendarConfig);
+        $this->handler->setSecurityFacade($this->securityFacade);
+        $this->handler->setOwnerDeletionManager($ownerDeletionManager);
+    }
+
+    /**
+     * @expectedException Oro\Bundle\SecurityBundle\Exception\ForbiddenException
+     * @expectedExceptionMessage Public Calendars does not supported.
+     */
+    public function testHandleDeleteWhenPublicCalendarDisabled()
+    {
+        $calendar = (new SystemCalendar())
+            ->setPublic(true);
+
+        $this->manager->expects($this->once())
+            ->method('find')
+            ->will($this->returnValue($calendar));
+        $this->calendarConfig->expects($this->once())
+            ->method('isPublicCalendarEnabled')
+            ->will($this->returnValue(false));
+
+        $this->handler->handleDelete(1, $this->manager);
+    }
+
+    /**
+     * @expectedException Oro\Bundle\SecurityBundle\Exception\ForbiddenException
+     * @expectedExceptionMessage System Calendars does not supported.
+     */
+    public function testHandleDeleteWhenSystemCalendarDisabled()
+    {
+        $calendar = (new SystemCalendar())
+            ->setPublic(false);
+
+        $this->manager->expects($this->once())
+            ->method('find')
+            ->will($this->returnValue($calendar));
+        $this->calendarConfig->expects($this->once())
+            ->method('isSystemCalendarEnabled')
+            ->will($this->returnValue(false));
+
+        $this->handler->handleDelete(1, $this->manager);
+    }
+
+    /**
+     * @expectedException Oro\Bundle\SecurityBundle\Exception\ForbiddenException
+     * @expectedExceptionMessage Access denied to system calendars from another organization.
+     */
+    public function testHandleDeleteWhenSystemCalendarDeleteNotGranted()
+    {
+        $calendar = (new SystemCalendar())
+            ->setPublic(false);
+
+        $this->manager->expects($this->once())
+            ->method('find')
+            ->will($this->returnValue($calendar));
+        $this->calendarConfig->expects($this->once())
+            ->method('isSystemCalendarEnabled')
+            ->will($this->returnValue(true));
+        $this->securityFacade->expects($this->once())
+            ->method('isGranted')
+            ->will($this->returnValue(false));
+
+        $this->handler->handleDelete(1, $this->manager);
+    }
+}
