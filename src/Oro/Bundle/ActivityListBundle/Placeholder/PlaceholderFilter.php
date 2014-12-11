458dd5a46fcdd6a4aacc42477cb81433b740853e
yurio <yurio@ua.fm>
2014-12-11 19:40:16 +0200

    BAP-6734: Not configurable Activity List available on entities

--- src/Oro/Bundle/ActivityListBundle/Placeholder/PlaceholderFilter.php
+++ src/Oro/Bundle/ActivityListBundle/Placeholder/PlaceholderFilter.php
@@ -2,18 +2,30 @@
 
 namespace Oro\Bundle\ActivityListBundle\Placeholder;
 
-use Doctrine\Common\Util\ClassUtils;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
 use Oro\Bundle\ActivityListBundle\Provider\ActivityListChainProvider;
+use Oro\Bundle\EntityBundle\ORM\DoctrineHelper;
 
 class PlaceholderFilter
 {
     /** @var ActivityListChainProvider */
     protected $activityListProvider;
 
-    public function __construct(ActivityListChainProvider $activityListChainProvider)
-    {
+    /** @var ManagerRegistry */
+    protected $doctrine;
+
+    /** @var DoctrineHelper */
+    protected $doctrineHelper;
+
+    public function __construct(
+        ActivityListChainProvider $activityListChainProvider,
+        ManagerRegistry $doctrine,
+        DoctrineHelper $doctrineHelper
+    ) {
         $this->activityListProvider = $activityListChainProvider;
+        $this->doctrine             = $doctrine;
+        $this->doctrineHelper       = $doctrineHelper;
     }
 
     /**
@@ -28,6 +40,11 @@ class PlaceholderFilter
             return false;
         }
 
-        return in_array(ClassUtils::getClass($entity), $this->activityListProvider->getTargetEntityClasses());
+        $entityClass      = $this->doctrineHelper->getEntityClass($entity);
+        $id               = $this->doctrineHelper->getSingleEntityIdentifier($entity);
+        $activityListRepo = $this->doctrine->getRepository('OroActivityListBundle:ActivityList');
+
+        return in_array($entityClass, $this->activityListProvider->getTargetEntityClasses())
+            || (bool)$activityListRepo->getRecordsCountForTargetClassAndId($entityClass, $id);
     }
 }
--- src/Oro/Bundle/ActivityListBundle/Tests/Unit/Placeholder/PlaceholderFilterTest.php
+++ src/Oro/Bundle/ActivityListBundle/Tests/Unit/Placeholder/PlaceholderFilterTest.php
@@ -3,24 +3,60 @@
 namespace Oro\Bundle\ActivityListBundle\Tests\Unit\Placeholder;
 
 use Oro\Bundle\ActivityListBundle\Placeholder\PlaceholderFilter;
+use Oro\Bundle\ActivityListBundle\Tests\Unit\Placeholder\Fixture\TestNonActiveTarget;
 use Oro\Bundle\ActivityListBundle\Tests\Unit\Placeholder\Fixture\TestTarget;
+use Oro\Bundle\EntityBundle\ORM\DoctrineHelper;
 
 class PlaceholderFilterTest extends \PHPUnit_Framework_TestCase
 {
-    public function testSsApplicable()
+    protected $activityListProvider;
+
+    protected $doctrine;
+
+    /** @var PlaceholderFilter */
+    protected $filter;
+
+    public function setUp()
     {
-        $activityListProvider = $this
+        $this->activityListProvider = $this
             ->getMockBuilder('Oro\Bundle\ActivityListBundle\Provider\ActivityListChainProvider')
             ->disableOriginalConstructor()
             ->getMock();
-        $filter = new PlaceholderFilter($activityListProvider);
-
-        $activityListProvider->expects($this->any())
+        $this->doctrine = $this
+            ->getMockBuilder('Doctrine\Common\Persistence\ManagerRegistry')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->activityListProvider->expects($this->any())
             ->method('getTargetEntityClasses')
             ->will($this->returnValue(['Oro\Bundle\ActivityListBundle\Tests\Unit\Placeholder\Fixture\TestTarget']));
 
-        $this->assertTrue($filter->isApplicable(new TestTarget()));
-        $this->assertFalse($filter->isApplicable(new \stdClass()));
-        $this->assertFalse($filter->isApplicable(null));
+        $doctrineHelper = new DoctrineHelper($this->doctrine);
+
+        $this->filter = new PlaceholderFilter($this->activityListProvider, $this->doctrine, $doctrineHelper);
+    }
+
+    public function testIsApplicable()
+    {
+        $this->assertTrue($this->filter->isApplicable(new TestTarget()));
+        $this->assertFalse($this->filter->isApplicable(null));
+    }
+
+    public function testIsApplicableOnNonSupportedTarget()
+    {
+        $repo = $this
+            ->getMockBuilder('Oro\Bundle\ActivityListBundle\Entity\Repository\ActivityListRepository')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->doctrine->expects($this->any())
+            ->method('getRepository')
+            ->will($this->returnValue($repo));
+        $repo->expects($this->any())
+            ->method('getRecordsCountForTargetClassAndId')
+            ->with('Oro\Bundle\ActivityListBundle\Tests\Unit\Placeholder\Fixture\TestNonActiveTarget', 123)
+            ->willReturn(true);
+
+        $entity = new TestNonActiveTarget(123);
+
+        $this->assertTrue($this->filter->isApplicable($entity));
     }
 }
