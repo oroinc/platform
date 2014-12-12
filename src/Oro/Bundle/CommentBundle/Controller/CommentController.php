c0c8d5ea5b59e721644a33348fc682ffab7e7f57
Valentine Velikiy <vvelykyi@magecore.com>
2014-12-12 12:43:02 +0200

    BAP-6814: Create Comment bundle skeleton. - Removed .gitignore. - Removed acl.yml. - Removed comments.html.twig. - Removed actions. - Updated php version requirement.

--- src/Oro/Bundle/CommentBundle/Controller/CommentController.php
+++ src/Oro/Bundle/CommentBundle/Controller/CommentController.php
@@ -16,29 +16,4 @@ use Oro\Bundle\EntityBundle\Tools\EntityRoutingHelper;
 class CommentController extends Controller
 {
 
-    /**
-     * @Route(
-     *      "/view/widget/{entityClass}/{entityId}",
-     *      name="oro_comment_widget_comments"
-     * )
-     *
-     * @AclAncestor("oro_comment_view")
-     * @Template("OroCommentBundle:Comment:comments.html.twig")
-     */
-    public function widgetAction($entityClass, $entityId)
-    {
-        $entity = $this->getEntityRoutingHelper()->getEntity($entityClass, $entityId);
-
-        return [
-            'entity' => $entity
-        ];
-    }
-
-    /**
-     * @return EntityRoutingHelper
-     */
-    protected function getEntityRoutingHelper()
-    {
-        return $this->get('oro_entity.routing_helper');
-    }
 }
