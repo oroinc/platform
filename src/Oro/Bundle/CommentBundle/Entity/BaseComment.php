b9f66121945ea073ba01d5a89ae49e09728ff504
Valentine Velikiy <vvelykyi@magecore.com>
2014-12-17 10:55:01 +0200

    BAP-6825: Replace comment on cases

--- src/Oro/Bundle/CommentBundle/Entity/BaseComment.php
+++ src/Oro/Bundle/CommentBundle/Entity/BaseComment.php
@@ -6,8 +6,37 @@ use Doctrine\ORM\Mapping as ORM;
 
 use Oro\Bundle\OrganizationBundle\Entity\Organization;
 use Oro\Bundle\UserBundle\Entity\User;
+use Oro\Bundle\EntityConfigBundle\Metadata\Annotation\Config;
 use Oro\Bundle\EntityConfigBundle\Metadata\Annotation\ConfigField;
 
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="oro_comment")
+ * @ORM\InheritanceType("SINGLE_TABLE")
+ * @ORM\DiscriminatorColumn(name="comments_type")
+ * @ORM\HasLifecycleCallbacks()
+ * @Config(
+ *      defaultValues={
+ *          "entity"={
+ *              "icon"="icon-comments"
+ *          },
+ *          "ownership"={
+ *              "owner_type"="USER",
+ *              "owner_field_name"="owner",
+ *              "owner_column_name"="user_owner_id",
+ *              "organization_field_name"="organization",
+ *              "organization_column_name"="organization_id"
+ *          },
+ *          "security"={
+ *              "type"="ACL",
+ *              "group_name"=""
+ *          },
+ *          "activity"={
+ *              "immutable"=true
+ *          }
+ *      }
+ * )
+ */
 abstract class BaseComment
 {
     /**
