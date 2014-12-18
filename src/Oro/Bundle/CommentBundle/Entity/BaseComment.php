911c57011fd20cbf68d53338e2fd3c8763a8f1fc
Valentine Velikiy <vvelykyi@magecore.com>
2014-12-18 13:23:00 +0200

    BAP-6825: Replace comment on cases. - Moved @Config annotation to child entities'

--- src/Oro/Bundle/CommentBundle/Entity/BaseComment.php
+++ src/Oro/Bundle/CommentBundle/Entity/BaseComment.php
@@ -6,7 +6,6 @@ use Doctrine\ORM\Mapping as ORM;
 
 use Oro\Bundle\OrganizationBundle\Entity\Organization;
 use Oro\Bundle\UserBundle\Entity\User;
-use Oro\Bundle\EntityConfigBundle\Metadata\Annotation\Config;
 use Oro\Bundle\EntityConfigBundle\Metadata\Annotation\ConfigField;
 
 /**
@@ -15,27 +14,6 @@ use Oro\Bundle\EntityConfigBundle\Metadata\Annotation\ConfigField;
  * @ORM\InheritanceType("SINGLE_TABLE")
  * @ORM\DiscriminatorColumn(name="comments_type")
  * @ORM\HasLifecycleCallbacks()
- * @Config(
- *      defaultValues={
- *          "entity"={
- *              "icon"="icon-comments"
- *          },
- *          "ownership"={
- *              "owner_type"="USER",
- *              "owner_field_name"="owner",
- *              "owner_column_name"="user_owner_id",
- *              "organization_field_name"="organization",
- *              "organization_column_name"="organization_id"
- *          },
- *          "security"={
- *              "type"="ACL",
- *              "group_name"=""
- *          },
- *          "activity"={
- *              "immutable"=true
- *          }
- *      }
- * )
  */
 abstract class BaseComment
 {
--- src/Oro/Bundle/CommentBundle/Entity/Comment.php
+++ src/Oro/Bundle/CommentBundle/Entity/Comment.php
@@ -5,11 +5,33 @@ namespace Oro\Bundle\CommentBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 use Oro\Bundle\CommentBundle\Model\ExtendComment;
+use Oro\Bundle\EntityConfigBundle\Metadata\Annotation\Config;
 
 /**
  * @ORM\Entity()
+ * @Config(
+ *      defaultValues={
+ *          "entity"={
+ *              "icon"="icon-comments"
+ *          },
+ *          "ownership"={
+ *              "owner_type"="USER",
+ *              "owner_field_name"="owner",
+ *              "owner_column_name"="user_owner_id",
+ *              "organization_field_name"="organization",
+ *              "organization_column_name"="organization_id"
+ *          },
+ *          "security"={
+ *              "type"="ACL",
+ *              "group_name"=""
+ *          },
+ *          "activity"={
+ *              "immutable"=true
+ *          }
+ *      }
+ * )
  */
+
 class Comment extends ExtendComment
 {
-
 }
