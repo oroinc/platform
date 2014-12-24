54c85cccf1267242ad465b76489000839feb314f
Dmitry Naydenov <dnaidonov@magecore.com1~>
2014-12-24 20:14:03 +0200

    minor fix

--- src/Oro/Bundle/CommentBundle/Placeholder/CommentPlaceholderFilter.php
+++ src/Oro/Bundle/CommentBundle/Placeholder/CommentPlaceholderFilter.php
@@ -23,6 +23,7 @@ class CommentPlaceholderFilter
      * Checks if the entity can have comments
      *
      * @param object|null $entity
+     *
      * @return bool
      */
     public function isApplicable($entity = null)
@@ -32,8 +33,31 @@ class CommentPlaceholderFilter
         }
 
         $className = ClassUtils::getRealClass($entity);
+
+        if (!$this->isExtendEntity($className)) {
+            return false;
+        }
+
         $config = $this->configManager->getProvider('comment')->getConfig($className);
 
         return $config->is('enabled');
+    }
+
+    /**
+     * @param string $className
+     *
+     * @return bool
+     */
+    protected function isExtendEntity($className)
+    {
+        $result = false;
+
+        if ($this->configManager->hasConfig($className)) {
+            $extendProvider = $this->configManager->getProvider('extend');
+
+            $result = $extendProvider->getConfig($className)->is('is_extend');
+        }
+
+        return $result;
     }
 }
