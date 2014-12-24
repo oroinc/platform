5c4b91c019a62cd3352ede51cf196744111b5f2b
Makar Sichevoy <sichevoy@gmail.com>
2014-12-24 15:54:33 +0200

    BAP-6820: Save attachment update

--- src/Oro/Bundle/CommentBundle/Resources/public/js/app/components/comment-component.js
+++ src/Oro/Bundle/CommentBundle/Resources/public/js/app/components/comment-component.js
@@ -68,8 +68,8 @@ define(function (require) {
             if (attrs.id) {
                 model = this.collection.get(attrs.id);
                 model.set(attrs);
-                options.method = 'PUT';
                 options.url = model.url();
+                //options.method = 'PUT';
                 model.save(null, options);
                 itemView = this.listView.getItemView(model);
                 itemView.render();
