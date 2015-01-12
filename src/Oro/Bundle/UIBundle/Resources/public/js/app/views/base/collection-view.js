fb1532e9c23cc63e0706f2711a85d235b446d293
Hryhorii Hrebiniuk <hhrebiniuk@magecore.com>
2015-01-12 18:38:08 +0200

    BAP-7145: Develop new Loading Mask view, based on fully CSS implementation
     - added support loading mask view in BaseCollectionView
     - minor fixes

--- src/Oro/Bundle/UIBundle/Resources/public/js/app/views/base/collection-view.js
+++ src/Oro/Bundle/UIBundle/Resources/public/js/app/views/base/collection-view.js
@@ -3,13 +3,22 @@
 define([
     'underscore',
     'chaplin',
-    './view'
-], function (_, Chaplin, View) {
+    './view',
+    'oroui/js/app/views/loading-mask-view',
+], function (_, Chaplin, View, LoadingMaskView) {
     'use strict';
 
     var BaseCollectionView;
 
     BaseCollectionView = Chaplin.CollectionView.extend({
+        /**
+         * Selector of the element that should be covered with loading mask
+         *
+         * @property {null|string|jQuery}
+         * @default null
+         */
+        loadingContainerSelector: null,
+
         /**
          * Show loader indicator on sync action even for not empty collections
          *
@@ -19,7 +28,8 @@ define([
         showLoadingForce: true,
 
         initialize: function (options) {
-            _.extend(this, _.pick(options, ['fallbackSelector', 'loadingSelector', 'itemSelector', 'listSelector']));
+            _.extend(this, _.pick(options, ['fallbackSelector', 'loadingSelector', 'loadingContainerSelector',
+                'itemSelector', 'listSelector']));
             BaseCollectionView.__super__.initialize.apply(this, arguments);
         },
 
@@ -40,17 +50,47 @@ define([
         },
 
         /**
+         * Initializes loading indicator
+         *
+         *  - added support loadingMask subview
+         *
+         * @returns {jQuery}
+         * @override
+         */
+        initLoadingIndicator: function() {
+            var loading;
+            if (this.loadingContainerSelector) {
+                loading = new LoadingMaskView({
+                    autoRender: true,
+                    container: this.$(this.loadingContainerSelector).first()
+                });
+                this.subview('loading', loading);
+                this.loadingSelector = loading.$el;
+            }
+            return BaseCollectionView.__super__.initLoadingIndicator.apply(this, arguments);
+        },
+
+
+        /**
          * Toggles loading indicator
          *
          *  - added extra flag showLoadingForce that shows loader event for not empty collections
+         *  - added support loadingMask subview
          *
          * @returns {jQuery}
          * @override
          */
         toggleLoadingIndicator: function () {
             var visible;
+
             visible = (this.collection.length === 0 || this.showLoadingForce) && this.collection.isSyncing();
-            return this.$loading.toggle(visible);
+            if (this.subview('loading')) {
+                this.subview('loading').toggle(visible);
+            } else {
+                this.$loading.toggle(visible);
+            }
+
+            return this.$loading;
         }
     });
 
--- src/Oro/Bundle/UIBundle/Resources/public/js/app/views/loading-mask-view.js
+++ src/Oro/Bundle/UIBundle/Resources/public/js/app/views/loading-mask-view.js
@@ -19,8 +19,13 @@ define(function (require) {
         /** @property {string} */
         loadingHint: 'Loading...',
 
+        initialize: function (options) {
+            _.extend(this, _.pick(options, ['loadingHint']));
+            LoadingMaskView.__super__.initialize.apply(this, arguments);
+        },
+
         /**
-         * Gets cached page data
+         * Gets data
          *
          * @returns {Object}
          * @override
@@ -32,17 +37,35 @@ define(function (require) {
             return data;
         },
 
+        /**
+         * Shows loading mask
+         */
         show: function () {
             this.$el.parent().addClass('loading');
             this.$el.addClass('shown');
         },
 
+        /**
+         * Hides loading mask
+         */
         hide: function () {
             this.$el.removeClass('shown');
             if (!this.$el.parent().find('>.loader-mask.shown').length) {
                 // there are no more loaders for the element
                 this.$el.parent().removeClass('loading');
             }
+        },
+
+        /**
+         * Toggles loading mask
+         *
+         * @param {boolean=} visible
+         */
+        toggle: function (visible) {
+            if (typeof visible === 'undefined') {
+                visible = !this.hasClass('shown');
+            }
+            this[visible ? 'show' : 'hide']();
         }
     });
 
