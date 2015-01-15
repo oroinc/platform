d76f1de5bf62452aeac31c27d6bf2ba31a3e23b9
Andrii Muzalevskyi <amuzalevsky@magecore.com>
2015-01-15 19:17:16 +0200

    BAP-6990 dropdown menu support. first approach

--- src/Oro/Bundle/UIBundle/Resources/public/js/extend/jquery.js
+++ src/Oro/Bundle/UIBundle/Resources/public/js/extend/jquery.js
@@ -54,6 +54,61 @@ define(['jquery'], function ($) {
             ($autoFocus.length ? $autoFocus : $input).first().focus();
         },
 
+        /*
+         * getStyleObject Plugin for jQuery JavaScript Library
+         * From: http://upshots.org/?p=112
+         *
+         * Copyright: Unknown, see source link
+         * Plugin version by Dakota Schneider (http://hackthetruth.org)
+         */
+        getStyleObject: function(){
+            var dom = this.get(0);
+            var style;
+            var returns = {};
+            if(window.getComputedStyle){
+                var camelize = function(a,b){
+                    return b.toUpperCase();
+                }
+                style = window.getComputedStyle(dom, null);
+                for(var i=0;i<style.length;i++){
+                    var prop = style[i];
+                    var camel = prop.replace(/\-([a-z])/g, camelize);
+                    var val = style.getPropertyValue(prop);
+                    returns[camel] = val;
+                }
+                return returns;
+            }
+            if(dom.currentStyle){
+                style = dom.currentStyle;
+                for(var prop in style){
+                    returns[prop] = style[prop];
+                }
+                return returns;
+            }
+            return this.css();
+        },
+
+        cloneWithStyles: function () {
+            // strange but only that works
+            var result = $('<div>');
+            this.each(function () {
+                var el = $(this),
+                    clone = el.clone().html('');
+                for (var i = 0; i < this.childNodes.length; i++) {
+                    var node = this.childNodes[i];
+                    if (node.nodeType === 1) { // ELEMENT_NODE
+                        clone[0].appendChild($(node).cloneWithStyles()[0]);
+                    } else {
+                        // all other nodes don't need any attention
+                        clone[0].appendChild(node.cloneNode());
+                    }
+                }
+                clone.css(el.getStyleObject());
+                result.append(clone);
+            });
+            return result.find('>*');
+        },
+
         focus: (function(orig) {
             return function() {
                 var $elem = $(this);
