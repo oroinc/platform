195394e89490865db2cee1cd48a1e5b9e428a197
yurio <yurio@ua.fm>
2015-01-16 20:36:30 +0200

    OEE-458: Wrong user list in some fields due multi organization

---
+++ src/Oro/Bundle/UserBundle/Form/Type/OrganizationUserAclMultiSelectType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Oro\Bundle\UserBundle\Form\Type;
+
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class OrganizationUserAclMultiSelectType extends UserMultiSelectType
+{
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(
+            [
+                'autocomplete_alias' => 'acl_users',
+                'configs'            => [
+                    'extra_config'            => 'acl_users_multiselect',
+                    'permission'              => 'VIEW',
+                    'entity_name'             => 'Oro\Bundle\UserBundle\Entity\User',
+                    'entity_id'               => 0,
+                    'multiple'                => true,
+                    'width'                   => '400px',
+                    'placeholder'             => 'oro.user.form.choose_user',
+                    'allowClear'              => true,
+                    'result_template_twig'    => 'OroUserBundle:User:Autocomplete/result.html.twig',
+                    'selection_template_twig' => 'OroUserBundle:User:Autocomplete/selection.html.twig',
+                ]
+            ]
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return 'oro_jqueryselect2_hidden';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'oro_user_organization_acl_multiselect';
+    }
+}
