42a3494a5d666ce52ae4a67059d6d69f6d51faf7
Makar Sichevoy <sichevoy@gmail.com>
2015-01-16 17:54:37 +0200

    BAP-7079: Reset password email templates

---
+++ src/Oro/Bundle/UserBundle/Migrations/Data/ORM/LoadEmailTemplates.php
@@ -0,0 +1,19 @@
+<?php
+namespace Oro\Bundle\UserBundle\Migrations\Data\ORM;
+
+use Oro\Bundle\EmailBundle\Migrations\Data\ORM\AbstractEmailFixture;
+
+class LoadEmailTemplates extends AbstractEmailFixture
+{
+    /**
+     * Return path to email templates
+     *
+     * @return string
+     */
+    public function getEmailsDir()
+    {
+        return $this->container
+            ->get('kernel')
+            ->locateResource('@OroUserBundle/Migrations/Data/ORM/emails');
+    }
+}
