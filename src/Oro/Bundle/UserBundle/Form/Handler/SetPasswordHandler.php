4d83b50260ea40aaa846b2b37117a0fbc943cd89
Makar Sichevoy <sichevoy@gmail.com>
2015-01-20 01:28:10 +0200

    BAP-7077: Fixes

--- src/Oro/Bundle/UserBundle/Form/Handler/SetPasswordHandler.php
+++ src/Oro/Bundle/UserBundle/Form/Handler/SetPasswordHandler.php
@@ -8,7 +8,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Oro\Bundle\UserBundle\Entity\User;
 use Oro\Bundle\UserBundle\Entity\UserManager;
 
-
 class SetPasswordHandler
 {
     /** @var Request */
