390d993c8092a2ef8e63aef2ec9756b037ef243c
Valentine Velikiy <vvelykyi@magecore.com>
2015-01-19 19:40:42 +0200

    BAP-7080: Send message with random gen pass on email

--- src/Oro/Bundle/UserBundle/Form/Handler/SetPasswordHandler.php
+++ src/Oro/Bundle/UserBundle/Form/Handler/SetPasswordHandler.php
@@ -2,17 +2,39 @@
 
 namespace Oro\Bundle\UserBundle\Form\Handler;
 
+use Doctrine\Common\Persistence\ObjectManager;
+
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 
+use Psr\Log\LoggerInterface;
+
+use Oro\Bundle\ConfigBundle\Config\ConfigManager;
+use Oro\Bundle\EmailBundle\Provider\EmailRenderer;
 use Oro\Bundle\UserBundle\Entity\User;
 use Oro\Bundle\UserBundle\Entity\UserManager;
 
-
 class SetPasswordHandler
 {
+    /**
+     * @var ObjectManager
+     */
+    protected $om;
+
+    /** @var  LoggerInterface */
+    protected $logger;
+
     /** @var Request */
     protected $request;
+
+    /** @var ConfigManager */
+    protected $configManager;
+
+    /** @var \Swift_Mailer */
+    protected $mailer;
+
+    /** @var EmailRenderer */
+    protected $renderer;
 
     /** @var  UserManager */
     protected $userManager;
@@ -21,18 +43,33 @@ class SetPasswordHandler
     protected $form;
 
     /**
-     * @param FormInterface       $form
+     * @param ObjectManager       $om
+     * @param LoggerInterface     $logger
      * @param Request             $request
+     * @param EmailRenderer       $renderer
+     * @param ConfigManager       $configManager
+     * @param \Swift_Mailer       $mailer
      * @param UserManager         $userManager
+     * @param FormInterface       $form
      */
     public function __construct(
-        Request         $request,
-        UserManager     $userManager,
-        FormInterface   $form
+        ObjectManager    $om,
+        LoggerInterface  $logger,
+        Request          $request,
+        ConfigManager    $configManager,
+        \Swift_Mailer    $mailer = null,
+        EmailRenderer    $renderer,
+        UserManager      $userManager,
+        FormInterface    $form
     ) {
-        $this->request      = $request;
-        $this->userManager  = $userManager;
-        $this->form         = $form;
+        $this->om            = $om;
+        $this->logger        = $logger;
+        $this->request       = $request;
+        $this->configManager = $configManager;
+        $this->mailer        = $mailer;
+        $this->renderer      = $renderer;
+        $this->userManager   = $userManager;
+        $this->form          = $form;
     }
 
     /**
@@ -47,10 +84,44 @@ class SetPasswordHandler
         if (in_array($this->request->getMethod(), array('POST', 'PUT'))) {
             $this->form->submit($this->request);
             if ($this->form->isValid()) {
+                if (in_array(null, [$this->configManager, $this->mailer], true)) {
+                    throw new \RuntimeException('Unable to send invitation email, unmet dependencies detected.');
+                }
+
                 $entity->setPlainPassword($this->form->get('password')->getData());
                 $this->userManager->updateUser($entity);
 
-                return true;
+                $emailTemplate = $this->om->getRepository('OroEmailBundle:EmailTemplate')->findByName('user_change_password');
+
+                try {
+                    list ($subjectRendered, $templateRendered) = $this->renderer->compileMessage(
+                        $emailTemplate,
+                        [
+                            'entity' => $entity,
+                            'plainPassword' => $this->form->get('password')->getData(),
+                        ]
+                    );
+
+                    $senderEmail = $this->configManager->get('oro_notification.email_notification_sender_email');
+                    $senderName  = $this->configManager->get('oro_notification.email_notification_sender_name');
+                    $type        = $emailTemplate->getType() == 'txt' ? 'text/plain' : 'text/html';
+                    $message     = \Swift_Message::newInstance()
+                        ->setSubject($subjectRendered)
+                        ->setFrom($senderEmail, $senderName)
+                        ->setTo($entity->getEmail())
+                        ->setBody($templateRendered, $type);
+                    $this->mailer->send($message);
+
+                    return true;
+                } catch (\Twig_Error $e) {
+                    $identity = method_exists($emailTemplate, '__toString')
+                        ? (string)$emailTemplate : $emailTemplate->getSubject();
+
+                    $this->logger->error(
+                        sprintf('Rendering of email template "%s" failed. %s', $identity, $e->getMessage()),
+                        ['exception' => $e]
+                    );
+                }
             }
         }
 
