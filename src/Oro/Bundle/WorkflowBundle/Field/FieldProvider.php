3adf3bd3c352d1c4c44636533518cf0b3ed477f1
Sergey Zhuravel <zhuravel.sergey@gmail.com>
2015-01-19 18:10:11 +0200

    BAP-7258: Create custom Workflow does not work - unidirectional relations in field and relation sections same time

--- src/Oro/Bundle/WorkflowBundle/Field/FieldProvider.php
+++ src/Oro/Bundle/WorkflowBundle/Field/FieldProvider.php
@@ -32,6 +32,37 @@ class FieldProvider extends EntityFieldProvider
     {
         // exclusions are not used in workflow
         parent::addFields($result, $className, false, $translate);
+
+        $metadata = $this->getMetadataFor($className);
+
+        // add single association fields
+        foreach ($metadata->getAssociationNames() as $associationName) {
+            if (!$this->isWorkflowField($associationName)
+                && $metadata->isSingleValuedAssociation($associationName)
+            ) {
+                if (isset($result[$associationName])) {
+                    // skip because a field with this name is already added, it could be a virtual field
+                    continue;
+                }
+                if (!$this->entityConfigProvider->hasConfig($metadata->getName(), $associationName)) {
+                    // skip non configurable relation
+                    continue;
+                }
+                if ($this->isIgnoredField($metadata, $associationName)) {
+                    continue;
+                }
+
+                $label = $this->getFieldLabel($className, $associationName);
+
+                $field = [
+                    'name' => $associationName,
+                    'type' => $this->getRelationFieldType($className, $associationName),
+                    'label' => $translate ? $this->translator->trans($label) : $label,
+                ];
+
+                $result[$associationName . '-field'] = $field;
+            }
+        }
     }
 
     /**
