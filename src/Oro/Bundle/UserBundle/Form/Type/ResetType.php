c141096d8ed7b8a944529d0ded818948cbfe2ed2
Makar Sichevoy <sichevoy@gmail.com>
2015-01-22 18:52:37 +0200

    BAP-7301: Design and texts changes

--- src/Oro/Bundle/UserBundle/Form/Type/ResetType.php
+++ src/Oro/Bundle/UserBundle/Form/Type/ResetType.php
@@ -26,12 +26,12 @@ class ResetType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('plainPassword', 'repeated', array(
+        $builder->add('plainPassword', 'repeated', [
             'type'            => 'password',
             'required'        => true,
-            'first_options'   => array('label' => 'Password'),
-            'second_options'  => array('label' => 'Again'),
-        ));
+            'first_options'   => ['label' => 'Password'],
+            'second_options'  => ['label' => 'Again'],
+        ]);
     }
 
     /**
@@ -39,11 +39,11 @@ class ResetType extends AbstractType
      */
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
-        $resolver->setDefaults(array(
+        $resolver->setDefaults([
             'data_class' => $this->class,
             'intention'  => 'reset',
             'dynamic_fields_disabled' => true
-        ));
+        ]);
     }
 
     /**
