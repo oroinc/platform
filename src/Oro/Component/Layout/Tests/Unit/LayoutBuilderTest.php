8011d6ff36b8b4316531b44f494c90019ac91577
Vova Soroka <vsoroka@magecore.com>
2015-01-25 07:01:18 +0200

    BAP-7168: Basic builder development - unit tests for LayoutBuilder

--- src/Oro/Component/Layout/Tests/Unit/LayoutBuilderTest.php
+++ src/Oro/Component/Layout/Tests/Unit/LayoutBuilderTest.php
@@ -7,6 +7,7 @@ use Oro\Component\Layout\BlockTypeRegistry;
 use Oro\Component\Layout\BlockView;
 use Oro\Component\Layout\DeferredLayoutManipulator;
 use Oro\Component\Layout\LayoutBuilder;
+use Oro\Component\Layout\LayoutData;
 use Oro\Component\Layout\LayoutViewFactory;
 use Oro\Component\Layout\Tests\Unit\Fixtures\BlockTypeFactoryStub;
 
@@ -71,6 +72,154 @@ class LayoutBuilderTest extends LayoutBuilderTestCase
             ],
             $view
         );
+    }
+
+    // @codingStandardsIgnoreStart
+    /**
+     * @expectedException \Oro\Component\Layout\Exception\LogicException
+     * @expectedExceptionMessage Cannot add "test" item to the layout. ParentId: root. BlockType: root. Reason: The "root" item does not exist.
+     */
+    // @codingStandardsIgnoreEnd
+    public function testAddToUnknownParent()
+    {
+        $this->layoutBuilder
+            ->add('test', 'root', 'root');
+    }
+
+    /**
+     * @expectedException \Oro\Component\Layout\Exception\LogicException
+     * @expectedExceptionMessage Cannot remove "root" item from the layout. Reason: The "root" item does not exist.
+     */
+    public function testRemoveUnknownItem()
+    {
+        $this->layoutBuilder
+            ->remove('root');
+    }
+
+    /**
+     * @expectedException \Oro\Component\Layout\Exception\LogicException
+     * @expectedExceptionMessage Cannot add "test" alias for "root" item. Reason: The "root" item does not exist.
+     */
+    public function testAddAliasForUnknownItem()
+    {
+        $this->layoutBuilder
+            ->addAlias('test', 'root');
+    }
+
+    /**
+     * @expectedException \Oro\Component\Layout\Exception\LogicException
+     * @expectedExceptionMessage Cannot remove "test" alias. Reason: The "test" item alias does not exist.
+     */
+    public function testRemoveUnknownAlias()
+    {
+        $this->layoutBuilder
+            ->removeAlias('test');
+    }
+
+    // @codingStandardsIgnoreStart
+    /**
+     * @dataProvider             emptyStringDataProvider
+     *
+     * @expectedException \Oro\Component\Layout\Exception\LogicException
+     * @expectedExceptionMessage Cannot set a value for "" option for "root" item. Reason: The option name must not be empty.
+     */
+    // @codingStandardsIgnoreEnd
+    public function testSetOptionWithEmptyName($name)
+    {
+        $this->layoutBuilder
+            ->setOption('root', $name, 123);
+    }
+
+    // @codingStandardsIgnoreStart
+    /**
+     * @expectedException \Oro\Component\Layout\Exception\LogicException
+     * @expectedExceptionMessage Cannot set a value for "test" option for "root" item. Reason: Cannot change already resolved options.
+     */
+    // @codingStandardsIgnoreEnd
+    public function testSetOptionForAlreadyResolvedItem()
+    {
+        $this->layoutBuilder
+            ->add('root', null, 'root');
+        $this->layoutBuilder->getLayout()->setProperty('root', LayoutData::RESOLVED_OPTIONS, []);
+
+        $this->layoutBuilder
+            ->setOption('root', 'test', 123);
+    }
+
+    // @codingStandardsIgnoreStart
+    /**
+     * @expectedException \Oro\Component\Layout\Exception\LogicException
+     * @expectedExceptionMessage Cannot set a value for "test" option for "root" item. Reason: The "root" item does not exist.
+     */
+    // @codingStandardsIgnoreEnd
+    public function testSetOptionForUnknownItem()
+    {
+        $this->layoutBuilder
+            ->setOption('root', 'test', 123);
+    }
+
+    /**
+     * @dataProvider             emptyStringDataProvider
+     *
+     * @expectedException \Oro\Component\Layout\Exception\LogicException
+     * @expectedExceptionMessage Cannot remove "" option for "root" item. Reason: The option name must not be empty.
+     */
+    public function testRemoveOptionWithEmptyName($name)
+    {
+        $this->layoutBuilder
+            ->removeOption('root', $name);
+    }
+
+    // @codingStandardsIgnoreStart
+    /**
+     * @expectedException \Oro\Component\Layout\Exception\LogicException
+     * @expectedExceptionMessage Cannot remove "test" option for "root" item. Reason: Cannot change already resolved options.
+     */
+    // @codingStandardsIgnoreEnd
+    public function testRemoveOptionForAlreadyResolvedItem()
+    {
+        $this->layoutBuilder
+            ->add('root', null, 'root');
+        $this->layoutBuilder->getLayout()->setProperty('root', LayoutData::RESOLVED_OPTIONS, []);
+
+        $this->layoutBuilder
+            ->removeOption('root', 'test');
+    }
+
+    /**
+     * @expectedException \Oro\Component\Layout\Exception\LogicException
+     * @expectedExceptionMessage Cannot remove "test" option for "root" item. Reason: The "root" item does not exist.
+     */
+    public function testRemoveOptionForUnknownItem()
+    {
+        $this->layoutBuilder
+            ->removeOption('root', 'test');
+    }
+
+    public function testGetOptions()
+    {
+        $this->layoutBuilder
+            ->add('root', null, 'root')
+            ->add('logo', 'root', 'logo', ['title' => 'test']);
+
+        $this->assertSame(
+            [],
+            $this->layoutBuilder->getOptions('root')
+        );
+        $this->assertSame(
+            ['title' => 'test'],
+            $this->layoutBuilder->getOptions('logo')
+        );
+    }
+
+    /**
+     * @expectedException \Oro\Component\Layout\Exception\LogicException
+     * @expectedExceptionMessage Cannot get options for "root" item. Reason: The "root" item does not exist.
+     */
+    public function testGetOptionsForUnknownItem()
+    {
+        $this->layoutBuilder
+            ->getOptions('root');
     }
 
     // @codingStandardsIgnoreStart
@@ -176,5 +325,13 @@ class LayoutBuilderTest extends LayoutBuilderTestCase
             $view,
             false
         );
+    }
+
+    public function emptyStringDataProvider()
+    {
+        return [
+            [null],
+            ['']
+        ];
     }
 }
