9c30ed1f5d59ab46a121b88e76da6d77260bd6ca
Vova Soroka <vsoroka@magecore.com>
2015-01-26 21:20:36 +0200

    BAP-7161: Style block type development

--- src/Oro/Bundle/LayoutBundle/Layout/Block/Type/StyleType.php
+++ src/Oro/Bundle/LayoutBundle/Layout/Block/Type/StyleType.php
@@ -2,11 +2,65 @@
 
 namespace Oro\Bundle\LayoutBundle\Layout\Block\Type;
 
-use Oro\Component\Layout\Block\Type\AbstractContainerType;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class StyleType extends AbstractContainerType
+use Oro\Component\Layout\Block\Type\AbstractType;
+use Oro\Component\Layout\BlockInterface;
+use Oro\Component\Layout\BlockView;
+
+class StyleType extends AbstractType
 {
     const NAME = 'style';
+
+    /** @var array */
+    protected static $attributes = [
+        'type'        => 'type',
+        'src'         => 'src',
+        'media'       => 'media',
+        'scoped'      => 'scoped',
+        'crossorigin' => 'crossorigin'
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $optionalOptions   = array_values(self::$attributes);
+        $optionalOptions[] = 'content';
+        $resolver->setOptional($optionalOptions);
+        $resolver->setAllowedTypes(
+            [
+                'scoped' => 'bool',
+            ]
+        );
+        $resolver->setAllowedValues(
+            [
+                'crossorigin' => ['anonymous', 'use-credentials']
+            ]
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildView(BlockView $view, BlockInterface $block, array $options)
+    {
+        foreach (self::$attributes as $attr => $opt) {
+            if (!empty($options[$opt])) {
+                switch ($opt) {
+                    case 'scoped':
+                        if ($options[$opt]) {
+                            $view->vars['attr'][$attr] = $attr;
+                        }
+                        break;
+                    default:
+                        $view->vars['attr'][$attr] = $options[$opt];
+                }
+            }
+        }
+        $view->vars['content'] = isset($options['content']) ? $options['content'] : '';
+    }
 
     /**
      * {@inheritdoc}
