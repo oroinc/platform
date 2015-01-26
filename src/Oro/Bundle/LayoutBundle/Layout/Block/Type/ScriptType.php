9f0bbd45551a6b248425b2c0d1bc3e673e58ec9f
Vova Soroka <vsoroka@magecore.com>
2015-01-26 19:21:51 +0200

    BAP-7160: Script block type development

--- src/Oro/Bundle/LayoutBundle/Layout/Block/Type/ScriptType.php
+++ src/Oro/Bundle/LayoutBundle/Layout/Block/Type/ScriptType.php
@@ -2,11 +2,67 @@
 
 namespace Oro\Bundle\LayoutBundle\Layout\Block\Type;
 
-use Oro\Component\Layout\Block\Type\AbstractContainerType;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class ScriptType extends AbstractContainerType
+use Oro\Component\Layout\Block\Type\AbstractType;
+use Oro\Component\Layout\BlockInterface;
+use Oro\Component\Layout\BlockView;
+
+class ScriptType extends AbstractType
 {
     const NAME = 'script';
+
+    /** @var array */
+    protected static $attributes = [
+        'type'        => 'type',
+        'src'         => 'src',
+        'async'       => 'async',
+        'defer'       => 'defer',
+        'crossorigin' => 'crossorigin'
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $optionalOptions   = array_values(self::$attributes);
+        $optionalOptions[] = 'content';
+        $resolver->setOptional($optionalOptions);
+        $resolver->setAllowedTypes(
+            [
+                'async' => 'bool',
+                'defer' => 'bool',
+            ]
+        );
+        $resolver->setAllowedValues(
+            [
+                'crossorigin' => ['anonymous', 'use-credentials']
+            ]
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildView(BlockView $view, BlockInterface $block, array $options)
+    {
+        foreach (self::$attributes as $attr => $opt) {
+            if (!empty($options[$opt])) {
+                switch ($opt) {
+                    case 'async':
+                    case 'defer':
+                        if ($options[$opt]) {
+                            $view->vars['attr'][$attr] = $attr;
+                        }
+                        break;
+                    default:
+                        $view->vars['attr'][$attr] = $options[$opt];
+                }
+            }
+        }
+        $view->vars['content'] = isset($options['content']) ? $options['content'] : '';
+    }
 
     /**
      * {@inheritdoc}
---
+++ src/Oro/Bundle/LayoutBundle/Tests/Unit/Layout/Block/Type/ScriptTypeTest.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace Oro\Bundle\LayoutBundle\Tests\Unit\Layout\Block\Type;
+
+use Oro\Component\Layout\Block\Type\BaseType;
+
+use Oro\Bundle\LayoutBundle\Layout\Block\Type\ScriptType;
+use Oro\Bundle\LayoutBundle\Tests\Unit\BlockTypeTestCase;
+
+class ScriptTypeTest extends BlockTypeTestCase
+{
+    public function testSetDefaultOptions()
+    {
+        $this->assertEquals(
+            [],
+            $this->resolveOptions(ScriptType::NAME, [])
+        );
+        $this->assertEquals(
+            ['type' => 'text/javascript', 'src' => 'test.js', 'async' => true],
+            $this->resolveOptions(
+                ScriptType::NAME,
+                ['type' => 'text/javascript', 'src' => 'test.js', 'async' => true]
+            )
+        );
+        $this->assertEquals(
+            ['content' => 'test content'],
+            $this->resolveOptions(ScriptType::NAME, ['content' => 'test content'])
+        );
+    }
+
+    /**
+     * @expectedException \Symfony\Component\OptionsResolver\Exception\InvalidOptionsException
+     * @expectedExceptionMessage The option "async" with value "async" is expected to be of type "bool"
+     */
+    public function testSetDefaultOptionsWithInvalidAsync()
+    {
+        $this->resolveOptions(ScriptType::NAME, ['src' => 'test.js', 'async' => 'async']);
+    }
+
+    /**
+     * @expectedException \Symfony\Component\OptionsResolver\Exception\InvalidOptionsException
+     * @expectedExceptionMessage The option "defer" with value "defer" is expected to be of type "bool"
+     */
+    public function testSetDefaultOptionsWithInvalidDefer()
+    {
+        $this->resolveOptions(ScriptType::NAME, ['src' => 'test.js', 'defer' => 'defer']);
+    }
+
+    // @codingStandardsIgnoreStart
+    /**
+     * @expectedException \Symfony\Component\OptionsResolver\Exception\InvalidOptionsException
+     * @expectedExceptionMessage The option "crossorigin" has the value "test", but is expected to be one of "anonymous", "use-credentials"
+     */
+    // @codingStandardsIgnoreEnd
+    public function testSetDefaultOptionsWithInvalidCrossorigin()
+    {
+        $this->resolveOptions(ScriptType::NAME, ['src' => 'test.js', 'crossorigin' => 'test']);
+    }
+
+    public function testBuildViewWithoutAsyncAndDefer()
+    {
+        $view = $this->getBlockView(
+            ScriptType::NAME,
+            ['type' => 'text/javascript', 'src' => 'test.js']
+        );
+
+        $this->assertEquals('text/javascript', $view->vars['attr']['type']);
+        $this->assertEquals('test.js', $view->vars['attr']['src']);
+        $this->assertSame('', $view->vars['content']);
+    }
+
+    public function testBuildViewWithFalseValueForAsyncAndDefer()
+    {
+        $view = $this->getBlockView(
+            ScriptType::NAME,
+            ['src' => 'test.js', 'async' => false, 'defer' => false]
+        );
+
+        $this->assertEquals('test.js', $view->vars['attr']['src']);
+        $this->assertFalse(isset($view->vars['attr']['async']), 'Unexpected \'async\' attribute');
+        $this->assertFalse(isset($view->vars['attr']['defer']), 'Unexpected \'defer\' attribute');
+    }
+
+    public function testBuildViewWithTrueValueForAsyncAndDefer()
+    {
+        $view = $this->getBlockView(
+            ScriptType::NAME,
+            ['src' => 'test.js', 'async' => true, 'defer' => true]
+        );
+
+        $this->assertEquals('test.js', $view->vars['attr']['src']);
+        $this->assertEquals('async', $view->vars['attr']['async']);
+        $this->assertEquals('defer', $view->vars['attr']['defer']);
+    }
+
+    public function testBuildViewWithContent()
+    {
+        $view = $this->getBlockView(
+            ScriptType::NAME,
+            ['content' => 'test content']
+        );
+
+        $this->assertEquals('test content', $view->vars['content']);
+    }
+
+    public function testGetName()
+    {
+        $type = $this->factory->createBlockType(ScriptType::NAME);
+
+        $this->assertSame(ScriptType::NAME, $type->getName());
+    }
+
+    public function testGetParent()
+    {
+        $type = $this->factory->createBlockType(ScriptType::NAME);
+
+        $this->assertSame(BaseType::NAME, $type->getParent());
+    }
+}
