c22f28373eac2ed01f3d7cf2c31f8cb2f76e6127
Vova Soroka <vsoroka@magecore.com>
2015-01-28 02:09:20 +0200

    BAP-7157: Root block type development

--- src/Oro/Bundle/LayoutBundle/Layout/Block/Type/RootType.php
+++ src/Oro/Bundle/LayoutBundle/Layout/Block/Type/RootType.php
@@ -19,7 +19,7 @@ class RootType extends AbstractContainerType
     {
         $resolver->setDefaults(
             [
-                'doctype' => 'html'
+                'doctype' => ''
             ]
         );
     }
--- src/Oro/Bundle/LayoutBundle/Tests/Unit/Layout/Block/Type/RootTypeTest.php
+++ src/Oro/Bundle/LayoutBundle/Tests/Unit/Layout/Block/Type/RootTypeTest.php
@@ -3,8 +3,8 @@
 namespace Oro\Bundle\LayoutBundle\Tests\Unit\Layout\Block\Type;
 
 use Oro\Component\Layout\Block\Type\ContainerType;
+
 use Oro\Bundle\LayoutBundle\Layout\Block\Type\RootType;
-use Oro\Bundle\LayoutBundle\Layout\Block\Type\HeadType;
 use Oro\Bundle\LayoutBundle\Tests\Unit\BlockTypeTestCase;
 
 class RootTypeTest extends BlockTypeTestCase
@@ -12,7 +12,7 @@ class RootTypeTest extends BlockTypeTestCase
     public function testSetDefaultOptions()
     {
         $this->assertEquals(
-            ['doctype' => 'html'],
+            ['doctype' => ''],
             $this->resolveOptions(RootType::NAME, [])
         );
         $this->assertEquals(
@@ -24,15 +24,9 @@ class RootTypeTest extends BlockTypeTestCase
     public function testBuildView()
     {
         $view = $this->getBlockBuilder(RootType::NAME, ['doctype' => 'html'])
-            ->add(HeadType::NAME, ['title' => 'test'])
             ->getBlockView();
 
         $this->assertEquals('html', $view->vars['doctype']);
-
-        $this->assertEquals(
-            ['root_id_head_id1'],
-            array_keys($view->children)
-        );
     }
 
     public function testGetName()
