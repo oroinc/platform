bea25476be6aef58557a65cd68a17e7bf19ae0ba
Makar Sichevoy <sichevoy@gmail.com>
2015-01-28 13:24:53 +0200

    CRM-2452: Functional test

--- src/Oro/Bundle/EmailBundle/Tests/Functional/EmailControllerTest.php
+++ src/Oro/Bundle/EmailBundle/Tests/Functional/EmailControllerTest.php
@@ -10,7 +10,20 @@ use Oro\Bundle\TestFrameworkBundle\Test\WebTestCase;
  */
 class EmailControllerTest extends WebTestCase
 {
-// view
+    protected function setUp()
+    {
+        $this->initClient(array(), $this->generateBasicAuthHeader());
+        $this->loadFixtures(['Oro\Bundle\EmailBundle\Tests\Functional\DataFixtures\LoadEmailData']);
+    }
+
+    public function testView()
+    {
+        $url = $this->getUrl('oro_email_view', ['id' => $this->getReference('email_1')->getId()]);
+        $this->client->request('GET', $url);
+        $result = $this->client->getResponse();
+        $this->assertHtmlResponseStatusCodeEquals($result, 200);
+    }
+
 // activity
 // create
 // body
