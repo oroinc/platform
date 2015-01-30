968700726a19b052aa660e7c17b6ea54c9bde9ec
Pavel Kargapoltsev <pkargapoltsev@magecore.com>
2015-01-30 18:37:35 +0200

    BAP-7346: Loader config layout - added test - updated after review

--- src/Oro/Component/Layout/Loader/RawLayoutLoader.php
+++ src/Oro/Component/Layout/Loader/RawLayoutLoader.php
@@ -11,55 +11,62 @@ class RawLayoutLoader
     /** @var array */
     protected $items = [];
 
-    /**
-     * @param LayoutManipulatorInterface  $manipulator
-     * @param array                       $config      Full config from merged configuration files
-     * @return LayoutManipulatorInterface
-     */
-    public function load(LayoutManipulatorInterface $manipulator, array $config)
+    /** @var LayoutManipulatorInterface */
+    protected $layoutManipulator;
+
+    public function __construct(LayoutManipulatorInterface $layoutManipulator)
     {
-        if (!$config) {
-            return $manipulator;
-        }
-
-        if (isset($config[self::LAYOUT_CONFIG_KEY])) {
-            if (array_key_exists('items', $config)) {
-                $this->items = $config['items'];
-            }
-            if (array_key_exists('tree', $config)) {
-                foreach ($config['tree'] as $block => $configPart) {
-                    $this->appendBlock($manipulator, $block, $configPart);
-                }
-            }
-            if (array_key_exists('aliases', $config)) {
-                foreach ($config['aliases'] as $id => $aliasConfig) {
-                    $manipulator->addAlias(isset($aliasConfig['alias']) ? : null, $id);
-                }
-            }
-        }
-
-        return $manipulator;
+        $this->layoutManipulator = $layoutManipulator;
     }
 
     /**
-     * @param LayoutManipulatorInterface $manipulator
-     * @param string                     $block
-     * @param array                      $configPart
-     * @param string|null                $parentBlock
+     * @param array $config Full config from merged configuration files
+     *
+     * @return LayoutManipulatorInterface
      */
-    protected function appendBlock(LayoutManipulatorInterface $manipulator, $block, $configPart, $parentBlock = null)
+    public function load(array $config)
+    {
+        if (!$config) {
+            return $this->layoutManipulator;
+        }
+
+        if (isset($config[self::LAYOUT_CONFIG_KEY])) {
+            if (array_key_exists('items', $config[self::LAYOUT_CONFIG_KEY])) {
+                $this->items = $config[self::LAYOUT_CONFIG_KEY]['items'];
+            }
+            if (array_key_exists('tree', $config[self::LAYOUT_CONFIG_KEY])) {
+                foreach ($config[self::LAYOUT_CONFIG_KEY]['tree'] as $block => $configPart) {
+                    $this->appendBlock($block, $configPart);
+                }
+            }
+            if (array_key_exists('aliases', $config[self::LAYOUT_CONFIG_KEY])) {
+                foreach ($config[self::LAYOUT_CONFIG_KEY]['aliases'] as $alias => $id) {
+                    $this->layoutManipulator->addAlias($alias, $id);
+                }
+            }
+        }
+
+        return $this->layoutManipulator;
+    }
+
+    /**
+     * @param string      $block
+     * @param array       $configPart
+     * @param string|null $parentBlock
+     */
+    protected function appendBlock($block, $configPart, $parentBlock = null)
     {
         if (isset($this->items[$block])) {
-            $manipulator->add(
+            $this->layoutManipulator->add(
                 $block,
                 $parentBlock,
-                $this->items[$block]['type'] ? : null,
-                $this->items[$block]['options'] ? : null
+                isset($this->items[$block]['type']) ? $this->items[$block]['type'] : null,
+                isset($this->items[$block]['options']) ? $this->items[$block]['options'] : []
             );
 
-            if (!empty($configPart['children'])) {
+            if (isset($configPart['children']) && !empty($configPart['children'])) {
                 foreach ($configPart['children'] as $childName => $childConfig) {
-                    $this->appendBlock($manipulator, $childName, $childConfig, $block);
+                    $this->appendBlock($childName, $childConfig, $block);
                 }
             }
         }
---
+++ src/Oro/Component/Layout/Tests/Unit/Loader/RawLayoutLoaderTest.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Oro\Component\Layout\Tests\Unit\Loader;
+
+use Oro\Component\Layout\Tests\Unit\DeferredLayoutManipulatorTestCase;
+use Oro\Component\Layout\Loader\RawLayoutLoader;
+
+class RawLayoutLoaderTest extends DeferredLayoutManipulatorTestCase
+{
+    /** @var RawLayoutLoader */
+    protected $rawLayoutLoader;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->rawLayoutLoader = new RawLayoutLoader($this->layoutManipulator);
+    }
+
+    public function testSimpleLayout()
+    {
+        $config = [
+            'oro_layout' => [
+                'items' => [
+                    'root' => [
+                        'type' => 'root'
+                    ]
+                ],
+                'tree' => [
+                    'root' => []
+                ]
+            ]
+        ];
+
+        $this->rawLayoutLoader->load($config);
+        $view = $this->getLayoutView();
+
+        $this->assertBlockView(
+            [ // root
+                'vars' => ['id' => 'root'],
+            ],
+            $view
+        );
+    }
+
+    public function testLayoutWithChildrenAndOptions()
+    {
+        $config = [
+            'oro_layout' => [
+                'items' => [
+                    'root' => ['type' => 'root'],
+                    'header' => ['type' => 'header'],
+                    'logo' => ['type' => 'logo', 'options' => ['title' => 'test']],
+
+                ],
+                'tree' => [
+                    'root' => [
+                        'children' => [
+                            'header' => [
+                                'children' => [
+                                    'logo' => []
+                                ]
+                            ],
+                        ]
+                    ]
+                ]
+            ]
+        ];
+
+        $this->rawLayoutLoader->load($config);
+        $view = $this->getLayoutView();
+
+        $this->assertBlockView(
+            [ // root
+                'vars' => ['id' => 'root'],
+                'children' => [
+                    [ // header
+                        'vars'     => ['id' => 'header'],
+                        'children' => [
+                            [ // logo
+                                'vars' => ['id' => 'logo', 'title' => 'test']
+                            ]
+                        ]
+                    ]
+                ]
+            ],
+            $view
+        );
+    }
+}
