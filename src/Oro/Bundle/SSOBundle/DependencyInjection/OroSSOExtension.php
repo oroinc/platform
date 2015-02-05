7d6c68f5ab9c44cca5ae4ecb9cb68a79e91a1c1d
Miloslav Nenadal <miloslav.nenadal@imatic.cz>
2015-02-05 09:14:04 +0100

    Specify security configuration inside app.yml

--- src/Oro/Bundle/SSOBundle/DependencyInjection/OroSSOExtension.php
+++ src/Oro/Bundle/SSOBundle/DependencyInjection/OroSSOExtension.php
@@ -2,15 +2,13 @@
 
 namespace Oro\Bundle\SSOBundle\DependencyInjection;
 
-use Oro\Bundle\DistributionBundle\DependencyInjection\OroContainerBuilder;
 use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-class OroSSOExtension extends Extension implements PrependExtensionInterface
+class OroSSOExtension extends Extension
 {
     /**
      * Load
@@ -37,41 +35,5 @@ class OroSSOExtension extends Extension implements PrependExtensionInterface
     public function getAlias()
     {
         return 'oro_sso';
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function prepend(ContainerBuilder $container)
-    {
-        if (!$container instanceof OroContainerBuilder) {
-            throw new \RuntimeException('Oro\Bundle\DistributionBundle\DependencyInjection\OroContainerBuilder is expected to be passed into OroSSOExtension');
-        }
-
-        $originalConfig = $container->getExtensionConfig('security');
-        if (!count($originalConfig)) {
-            $originalConfig[] = array();
-        }
-
-        $oautConfig = [
-            'firewalls' => [
-                'main' => [
-                    'oauth' => [
-                        'resource_owners' => [
-                            'google' => '/login/check-google',
-                        ],
-                        'login_path' => '/user/login',
-                        'failure_path' => '/user/login',
-                        'oauth_user_provider'=> [
-                            'service' => 'oro_sso.oauth_provider',
-                        ],
-                    ],
-                ],
-            ],
-        ];
-
-        $mergedConfig = [array_merge_recursive($originalConfig[0], $oautConfig)];
-
-        $container->setExtensionConfig('security', $mergedConfig);
     }
 }
