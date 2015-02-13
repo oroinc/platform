e31b9708869c3621d2e98c36ba72d2896662297d
Makar Sichevoy <sichevoy@gmail.com>
2015-02-13 14:44:57 +0200

    CRM-2504: Set main visible Email in thread to ActivityList

--- src/Oro/Bundle/EmailBundle/Tests/Unit/Entity/Provider/EmailThreadProviderTest.php
+++ src/Oro/Bundle/EmailBundle/Tests/Unit/Entity/Provider/EmailThreadProviderTest.php
@@ -118,4 +118,58 @@ class EmailThreadProviderTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals($treadId, $this->provider->getEmailThreadId($entityManager, $email));
     }
+
+    public function testGetThreadEmailWithoutThread()
+    {
+        $email = $this->getMock('Oro\Bundle\EmailBundle\Entity\Email');
+        $email->expects($this->exactly(1))
+            ->method('getThreadId')
+            ->will($this->returnValue(''));
+        $entityManager = $this->getMockBuilder('Doctrine\ORM\EntityManager')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $entityManager->expects($this->never())
+            ->method('getRepository');
+
+        $this->assertEquals([], $this->provider->getThreadEmails($entityManager, $email));
+    }
+
+    public function testGetThreadEmailWithThread()
+    {
+        $treadId = 'testTreadId';
+        $email = $this->getMock('Oro\Bundle\EmailBundle\Entity\Email');
+        $email->expects($this->exactly(1))
+            ->method('getThreadId')
+            ->will($this->returnValue($treadId));
+        $entityManager = $this->getMockBuilder('Doctrine\ORM\EntityManager')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $repository = $this->getMockBuilder('Doctrine\ORM\EntityRepository')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $queryBuilder = $this->getMockBuilder('Doctrine\ORM\QueryBuilder')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $query = $this->getMockBuilder('Doctrine\ORM\AbstractQuery')
+            ->disableOriginalConstructor()
+            ->setMethods(['getResult'])
+            ->getMockForAbstractClass();
+
+        $query->expects($this->exactly(1))
+            ->method('getResult')
+            ->will($this->returnValue([new \stdClass(), new \stdClass()]));
+        $queryBuilder->expects($this->exactly(1))
+            ->method('getQuery')
+            ->will($this->returnValue($query));
+        $repository->expects($this->exactly(1))
+            ->method('createQueryBuilder')
+            ->with('e')
+            ->willReturn($queryBuilder);
+        $entityManager->expects($this->exactly(1))
+            ->method('getRepository')
+            ->with('OroEmailBundle:Email')
+            ->will($this->returnValue($repository));
+
+        $this->assertCount(2, $this->provider->getThreadEmails($entityManager, $email));
+    }
 }
