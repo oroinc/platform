0a40b5d5719651da1a91716de1d4484a7846a19e
Miloslav Nenadal <miloslav.nenadal@imatic.cz>
2015-02-27 10:38:33 +0100

    Configure credentials via config manager

--- src/Oro/Bundle/SSOBundle/OroSSOBundle.php
+++ src/Oro/Bundle/SSOBundle/OroSSOBundle.php
@@ -2,8 +2,15 @@
 
 namespace Oro\Bundle\SSOBundle;
 
+use Oro\Bundle\SSOBundle\DependencyInjection\Compiler\HwiConfigurationPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class OroSSOBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+        $container->addCompilerPass(new HwiConfigurationPass());
+    }
 }
