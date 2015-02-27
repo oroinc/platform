5065558673ee90b7c45c5cf359e40f34ce48f995
Vova Soroka <vsoroka@magecore.com>
2015-02-27 13:33:26 +0200

    BAP-7587: Layout updates for hash navigation

---
+++ src/Oro/Bundle/UIBundle/Layout/Extension/HashNavContextConfigurator.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Oro\Bundle\UIBundle\Layout\Extension;
+
+use Symfony\Component\HttpFoundation\Request;
+
+use Oro\Component\Layout\ContextInterface;
+use Oro\Component\Layout\ContextConfiguratorInterface;
+
+use Oro\Bundle\NavigationBundle\Event\ResponseHashnavListener;
+use Symfony\Component\OptionsResolver\Options;
+
+class HashNavContextConfigurator implements ContextConfiguratorInterface
+{
+    const HASH_NAVIGATION_HEADER = ResponseHashnavListener::HASH_NAVIGATION_HEADER;
+
+    /** @var Request|null */
+    protected $request;
+
+    /**
+     * Synchronized DI method call, sets current request for further usage
+     *
+     * @param Request $request
+     */
+    public function setRequest(Request $request = null)
+    {
+        $this->request = $request;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureContext(ContextInterface $context)
+    {
+        $context->getDataResolver()
+            ->setOptional(['hash_navigation'])
+            ->setNormalizers(
+                [
+                    'hash_navigation' => function (Options $options, $hashNavigation) {
+                        if ($hashNavigation === null) {
+                            $hashNavigation =
+                                $this->request
+                                && (
+                                    $this->request->headers->get(self::HASH_NAVIGATION_HEADER) == true
+                                    || $this->request->get(self::HASH_NAVIGATION_HEADER) == true
+                                );
+                        }
+
+                        return $hashNavigation;
+                    }
+                ]
+            );
+    }
+}
