9458b0107a249779f70fe91f9180afc449734589
Valentine Velikiy <vvelykyi@magecore.com>
2015-03-04 17:16:02 +0200

    CRM-2690: Upgrade database with emails fails

--- src/Oro/Bundle/EmailBundle/Migrations/Data/ORM/AddEmailActivityGrouping.php
+++ src/Oro/Bundle/EmailBundle/Migrations/Data/ORM/AddEmailActivityGrouping.php
@@ -6,7 +6,9 @@ use Doctrine\Common\Collections\Criteria;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\Util\ClassUtils;
 use Doctrine\ORM\QueryBuilder;
+use Doctrine\ORM\UnitOfWork;
 
 use Oro\Bundle\BatchBundle\ORM\Query\BufferedQueryResultIterator;
 use Oro\Bundle\EmailBundle\Entity\Email;
@@ -54,24 +56,35 @@ class AddEmailActivityGrouping extends AbstractFixture implements DependentFixtu
             $queryBuilder->addCriteria($criteria);
             $queryBuilder->setFirstResult(0);
             $emails = $queryBuilder->getQuery()->execute();
-            if (count($emails)) {
+            if (count($emails) > 1) {
+                $itemsCount++;
                 $newThread = new EmailThread();
                 $manager->persist($newThread);
                 foreach ($emails as $key => $email) {
                     /** @var Email $email */
-                    $email->setThread($newThread);
                     if ($key == 0) {
                         $email->setHead(true);
                     } else {
                         $email->setHead(false);
                     }
                     $itemsCount++;
-                    $entities[] = $email;
+                    $entities[] = [
+                        'thread' => $newThread,
+                        'email'  => $email,
+                    ];
                     if (0 == $itemsCount % self::BATCH_SIZE) {
                         $this->saveEntities($manager, $entities);
                         $entities = [];
                     }
                 }
+            } elseif (count($emails) == 1) {
+                $email = $emails[0];
+                $email->setHead(true);
+                $itemsCount++;
+                $entities[] = [
+                    'thread' => null,
+                    'email'  => $email,
+                ];
             }
         }
 
@@ -82,12 +95,23 @@ class AddEmailActivityGrouping extends AbstractFixture implements DependentFixtu
 
     /**
      * @param ObjectManager $manager
-     * @param array $entities
+     * @param array         $entities
      */
     protected function saveEntities(ObjectManager $manager, array $entities)
     {
-        foreach ($entities as $entity) {
-            $manager->persist($entity);
+        foreach ($entities as $pair) {
+            $uow = $manager->getUnitOfWork();
+            if ($uow->getEntityState($pair['email'], UnitOfWork::STATE_DETACHED) === UnitOfWork::STATE_DETACHED) {
+                $isHead = $pair['email']->isHead();
+                $pair['email'] = $manager->find(ClassUtils::getClass($pair['email']), $pair['email']->getId());
+                $pair['email']->setHead($isHead);
+            }
+            if ($pair['thread']) {
+                $manager->persist($pair['thread']);
+                $pair['email']->setThread($pair['thread']);
+            }
+
+            $manager->persist($pair['email']);
         }
         $manager->flush();
         $manager->clear();
