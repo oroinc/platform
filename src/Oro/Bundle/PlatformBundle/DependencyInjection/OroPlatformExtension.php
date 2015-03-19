81d416d88ca9bd5808f3cb7e7d67cb7f442ce725
Miloslav Nenadal <miloslav.nenadal@imatic.cz>
2015-03-19 17:49:43 +0100

    BAP-7907: WSSE cache needs cleanup. Merge OroContainerBuilder into 1.6

--- src/Oro/Bundle/PlatformBundle/DependencyInjection/OroPlatformExtension.php
+++ src/Oro/Bundle/PlatformBundle/DependencyInjection/OroPlatformExtension.php
@@ -10,6 +10,7 @@ use Symfony\Component\DependencyInjection\Loader;
 
 use Oro\Component\Config\Loader\CumulativeConfigLoader;
 use Oro\Component\Config\Loader\YamlCumulativeFileLoader;
+use Oro\Bundle\DistributionBundle\DependencyInjection\OroContainerBuilder;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -32,10 +33,45 @@ class OroPlatformExtension extends Extension implements PrependExtensionInterfac
         foreach ($resources as $resource) {
             foreach ($resource->data as $name => $config) {
                 if (!empty($extensions[$name])) {
-                    $container->prependExtensionConfig($name, $config);
+                    if ($name === 'security') {
+                        $this->mergeConfigIntoOne($container, $name, $config);
+                    } else {
+                        $container->prependExtensionConfig($name, $config);
+                    }
                 }
             }
         }
+    }
+
+    /**
+     * Merge configuration into one config
+     *
+     * @param ContainerBuilder $container
+     * @param string $name
+     * @param array $config
+     *
+     * @throws \RuntimeException
+     */
+    private function mergeConfigIntoOne(ContainerBuilder $container, $name, array $config = [])
+    {
+        if (!$container instanceof OroContainerBuilder) {
+            throw new \RuntimeException(
+                sprintf(
+                    '%s is expected to be passed into OroPlatformExtension',
+                    'Oro\Bundle\DistributionBundle\DependencyInjection\OroContainerBuilder'
+                )
+            );
+        }
+
+        $originalConfig = $container->getExtensionConfig($name);
+        if (!count($originalConfig)) {
+            $originalConfig[] = [];
+        }
+
+        $mergedConfig = array_merge_recursive($originalConfig[0], $config);
+        $originalConfig[0] = $mergedConfig;
+
+        $container->setExtensionConfig('security', $originalConfig);
     }
 
     /**
