3a59bea7800e979b4835f9cc67f3e886135d9c77
David Korm <david.korm@das-medienkombinat.de>
2015-03-23 15:15:33 +0100

    Add spaces for indent the method declaration

--- src/Oro/Bundle/AddressBundle/Model/ExtendCountry.php
+++ src/Oro/Bundle/AddressBundle/Model/ExtendCountry.php
@@ -4,7 +4,14 @@ namespace Oro\Bundle\AddressBundle\Model;
 
 class ExtendCountry 
 {
-public function __construct()
+    /**
+     * Constructor
+     *
+     * The real implementation of this method is auto generated.
+     *
+     * IMPORTANT: If the derived class has own constructor it must call parent constructor.
+     */
+    public function __construct()
     {
     }
 }
