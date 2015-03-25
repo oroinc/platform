547cade38c25f6218e1ff6fbf7cf4c66abe59068
Makar Sichevoy <sichevoy@gmail.com>
2015-03-25 20:43:28 +0200

    CRM-2892: Grid changes for 'My Emails' page

---
+++ src/Oro/Bundle/EmailBundle/Datagrid/EmailFolderViewList.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Oro\Bundle\EmailBundle\Datagrid;
+
+use Oro\Bundle\DataGridBundle\Extension\GridViews\View;
+use Oro\Bundle\DataGridBundle\Extension\GridViews\AbstractViewsList;
+use Oro\Bundle\EmailBundle\Model\FolderType;
+
+class EmailFolderViewList extends AbstractViewsList
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function getViewsList()
+    {
+        return [
+            new View(
+                'oro.email.datagrid.emailfolder.view.inbox',
+                [
+                    'folders' => ['value' => FolderType::INBOX]
+                ]
+            ),
+            new View(
+                'oro.email.datagrid.emailfolder.view.sent',
+                [
+                    'folders' => ['value' => FolderType::SENT]
+                ]
+            ),
+            new View(
+                'oro.email.datagrid.emailfolder.view.all'
+            )
+        ];
+    }
+}
