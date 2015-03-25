cf451d582f95a74eb2fa4a059a57ad9ff8903930
Makar Sichevoy <sichevoy@gmail.com>
2015-03-25 10:55:16 +0200

    CRM-2896: View email thread. Initial view of thread

--- src/Oro/Bundle/EmailBundle/Controller/EmailController.php
+++ src/Oro/Bundle/EmailBundle/Controller/EmailController.php
@@ -73,13 +73,7 @@ class EmailController extends Controller
     public function threadWidgetAction(Email $entity)
     {
         $config = $this->get('oro_config.global');
-        $ids = $this->getRequest()->get('ids');
-        if ($ids) {
-            if (!is_array($ids)) {
-                $ids = [$ids];
-            }
-            $emails = $this->get('doctrine')->getRepository("OroEmailBundle:Email")->findInIds($ids);
-        } else if ($config->get('oro_email.use_threads_in_emails')) {
+        if ($config->get('oro_email.use_threads_in_emails')) {
             $emails = $this->get('oro_email.email.thread.provider')->getThreadEmails(
                 $this->get('doctrine')->getManager(),
                 $entity
