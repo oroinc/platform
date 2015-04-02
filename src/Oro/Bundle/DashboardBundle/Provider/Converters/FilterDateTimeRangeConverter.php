f755b3855ab945b993af2efd77ae1b4c0f8244f4
yurio <yurio@ua.fm>
2015-04-02 17:19:47 +0300

    BAP-7921: Chart date helper

--- src/Oro/Bundle/DashboardBundle/Provider/Converters/FilterDateTimeRangeConverter.php
+++ src/Oro/Bundle/DashboardBundle/Provider/Converters/FilterDateTimeRangeConverter.php
@@ -2,7 +2,10 @@
 
 namespace Oro\Bundle\DashboardBundle\Provider\Converters;
 
+use \Datetime;
+
 use Oro\Bundle\DashboardBundle\Provider\ConfigValueConverter;
+use Oro\Bundle\FilterBundle\Expression\Date\Compiler;
 use Oro\Bundle\LocaleBundle\Formatter\DateTimeFormatter;
 
 class FilterDateTimeRangeConverter implements ConfigValueConverter
@@ -10,9 +13,13 @@ class FilterDateTimeRangeConverter implements ConfigValueConverter
     /** @var DateTimeFormatter */
     protected $formatter;
 
-    public function __construct(DateTimeFormatter $formatter)
+    /** @var Compiler */
+    protected $dateCompiler;
+
+    public function __construct(DateTimeFormatter $formatter, Compiler $dateCompiler)
     {
         $this->formatter = $formatter;
+        $this->dateCompiler = $dateCompiler;
     }
 
     /**
@@ -21,12 +28,15 @@ class FilterDateTimeRangeConverter implements ConfigValueConverter
     public function getConvertedValue($value = null)
     {
         if (is_null($value)) {
-            $end = new \DateTime('now', new \DateTimeZone('UTC'));
+            $end = new DateTime('now', new \DateTimeZone('UTC'));
             $start = clone $end;
             $start = $start->sub(new \DateInterval('P1M'));
         } else {
-            $start = $value['value']['start'];
-            $end = $value['value']['end'];
+            $startValue = $value['value']['start'];
+            $endValue = $value['value']['end'];
+
+            $start = $startValue instanceof DateTime ? $startValue : $this->dateCompiler->compile($startValue);
+            $end = $endValue instanceof DateTime ? $endValue : $this->dateCompiler->compile($endValue);
         }
 
         return [
