fc8265021f2246681f861059c3cd2ef20c8584b4
zebimax <zebimax@gmail.com>
2015-04-03 04:33:25 +0300

    BAP-6931: - Use EmptyArrayLoader to replace database loader

--- src/Oro/Bundle/TranslationBundle/Command/OroTranslationUpdateCustomCommand.php
+++ src/Oro/Bundle/TranslationBundle/Command/OroTranslationUpdateCustomCommand.php
@@ -3,12 +3,14 @@
 namespace Oro\Bundle\TranslationBundle\Command;
 
 use Oro\Bundle\TranslationBundle\Entity\Translation;
+use Oro\Bundle\TranslationBundle\Translation\EmptyArrayLoader;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class OroTranslationUpdateCustomCommand extends ContainerAwareCommand
 {
+    const BATCH_SIZE = 1000;
     /**
      * {@inheritdoc}
      */
@@ -25,13 +27,21 @@ class OroTranslationUpdateCustomCommand extends ContainerAwareCommand
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $locale = $this->getContainer()->getParameter('kernel.default_locale');
-        $oroEntityManager = $this->getContainer()->get('doctrine.orm.entity_manager');
+        $container = $this->getContainer();
+        $oroEntityManager = $container->get('doctrine.orm.entity_manager');
         $translationRepository = $oroEntityManager->getRepository(Translation::ENTITY_NAME);
-        $translator = $this->getContainer()->get('translator');
+        /**
+         * disable database loader to not get translations from database
+         */
+        $container->set(
+            'oro_translation.database_translation.loader',
+            new EmptyArrayLoader()
+        );
 
-        $translations = $translator->getTranslations([], $locale);
-        $customTranslations = $translationRepository->findAll();
+        $translations = $container->get('translator.default')->getTranslations();
+        $customTranslations = $translationRepository->findBy(['locale' => $locale]);
         $updated = 0;
+
         foreach ($customTranslations as $customTranslation) {
             if (isset (
                 $translations[$customTranslation->getDomain()][$customTranslation->getKey()])
@@ -39,12 +49,17 @@ class OroTranslationUpdateCustomCommand extends ContainerAwareCommand
                 $customTranslation->setValue(
                     $translations[$customTranslation->getDomain()][$customTranslation->getKey()]
                 );
-                $oroEntityManager->persist($customTranslation);
                 $updated++;
+                if (($updated % self::BATCH_SIZE) === 0) {
+                    $oroEntityManager->flush();
+                    $oroEntityManager->clear();
+                }
             }
         }
-        $oroEntityManager->flush();
 
-        $output->writeln('<info>updated ' . $updated . ' values</info>');
+        $oroEntityManager->flush();
+        $oroEntityManager->clear();
+
+        $output->writeln('<info>Updated ' . $updated . ' values</info>');
     }
 }
