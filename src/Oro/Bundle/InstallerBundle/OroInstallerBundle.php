283b75d52b2efdbfc8079b05dfbf793c2effbe58
Vitaliy Berdylo <vberdylo@magecore.com>
2015-04-06 19:30:37 +0300

    BB-362: Refactor directory structure of /app/ directory for installer - CR fixes

--- src/Oro/Bundle/InstallerBundle/OroInstallerBundle.php
+++ src/Oro/Bundle/InstallerBundle/OroInstallerBundle.php
@@ -2,20 +2,8 @@
 
 namespace Oro\Bundle\InstallerBundle;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-use Oro\Bundle\InstallerBundle\DependencyInjection\Compiler\InstallerPass;
 
 class OroInstallerBundle extends Bundle
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function build(ContainerBuilder $container)
-    {
-        parent::build($container);
-
-        $container->addCompilerPass(new InstallerPass());
-    }
 }
