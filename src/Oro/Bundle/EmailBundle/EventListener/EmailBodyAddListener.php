f70824986b2bf4b58f315d7c7a7101a25036612b
Makar Sichevoy <sichevoy@gmail.com>
2015-04-07 16:17:17 +0300

    CRM-3028: Reattach to scope automatic on send

--- src/Oro/Bundle/EmailBundle/EventListener/EmailBodyAddListener.php
+++ src/Oro/Bundle/EmailBundle/EventListener/EmailBodyAddListener.php
@@ -3,14 +3,18 @@
 namespace Oro\Bundle\EmailBundle\EventListener;
 
 use Doctrine\Common\Util\ClassUtils;
+
 use Oro\Bundle\EmailBundle\Event\EmailBodyAdded;
 use Oro\Bundle\EmailBundle\Manager\EmailAttachmentManager;
 use Oro\Bundle\EmailBundle\Provider\EmailActivityListProvider;
 use Oro\Bundle\EntityConfigBundle\Provider\ConfigProvider;
+use Oro\Bundle\EntityConfigBundle\DependencyInjection\Utils\ServiceLink;
+use Oro\Bundle\SecurityBundle\SecurityFacade;
 
 class EmailBodyAddListener
 {
     const LINK_ATTACHMENT_CONFIG_OPTION = 'auto_link_attachments';
+    const ATTACHMENT_CLASS = 'Oro\Bundle\AttachmentBundle\Entity\Attachment';
 
     /** @var ConfigProvider */
     protected $configProvider;
@@ -21,19 +25,25 @@ class EmailBodyAddListener
     /** @var EmailActivityListProvider */
     protected $activityListProvider;
 
+    /** @var SecurityFacade */
+    protected $securityFacade;
+
     /**
      * @param EmailAttachmentManager $attachmentManager
      * @param ConfigProvider $configProvider
      * @param EmailActivityListProvider $activityListProvider
+     * @param ServiceLink $securityFacadeLink
      */
     public function __construct(
         EmailAttachmentManager $attachmentManager,
         ConfigProvider $configProvider,
-        EmailActivityListProvider $activityListProvider
+        EmailActivityListProvider $activityListProvider,
+        ServiceLink $securityFacadeLink
     ) {
         $this->attachmentManager = $attachmentManager;
         $this->configProvider = $configProvider;
         $this->activityListProvider = $activityListProvider;
+        $this->securityFacade = $securityFacadeLink->getService();
     }
 
     /**
@@ -41,6 +51,9 @@ class EmailBodyAddListener
      */
     public function linkToScopeEvent(EmailBodyAdded $event)
     {
+        if (!$this->securityFacade->isGranted('CREATE', 'entity:' . self::ATTACHMENT_CLASS)) {
+            return;
+        }
         $email = $event->getEmail();
         $entities = $this->activityListProvider->getTargetEntities($email);
         foreach ($entities as $entity) {
