3e11544e7d8b780567f25292c633363c1447d504
Valentine Velikiy <vvelykyi@magecore.com>
2015-04-16 13:54:41 +0300

    CRM-3166: Context not refreshed after send email on thread view page

--- src/Oro/Bundle/EmailBundle/Resources/public/js/app/views/email-context-activity-view.js
+++ src/Oro/Bundle/EmailBundle/Resources/public/js/app/views/email-context-activity-view.js
@@ -37,6 +37,7 @@ define([
             * on adding activity item listen to "widget:doRefresh:email-context-activity-list-widget"
             */
             mediator.on('widget:doRefresh:email-context-activity-list-widget', this.doRefresh, this );
+            mediator.on('widget:doRefresh:email-thread-context', this.doRefresh, this );
             EmailContextActivityView.__super__.initialize.apply(this, arguments);
         },
 
