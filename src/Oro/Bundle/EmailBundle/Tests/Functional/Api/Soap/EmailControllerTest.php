eaa69527bc24035c2183ff370ced7bf8f9cf7c7c
Dmytro Krushelnytkyi <dkrushelnytskyi@magecore.com>
2015-04-23 18:35:09 +0300

    CRM-3217: Added email context appears after refresh page -added function test for soap api

--- src/Oro/Bundle/EmailBundle/Tests/Functional/Api/Soap/EmailControllerTest.php
+++ src/Oro/Bundle/EmailBundle/Tests/Functional/Api/Soap/EmailControllerTest.php
@@ -2,6 +2,7 @@
 namespace Oro\Bundle\EmailBundle\Tests\Functional\Api\Soap;
 
 use Oro\Bundle\TestFrameworkBundle\Test\WebTestCase;
+use Rhumsaa\Uuid\Console\Exception;
 
 /**
  * @outputBuffering enabled
@@ -9,6 +10,8 @@ use Oro\Bundle\TestFrameworkBundle\Test\WebTestCase;
  */
 class EmailControllerTest extends WebTestCase
 {
+    const INCORRECT_ID = 1111;
+
     protected function setUp()
     {
         $this->initClient(array(), $this->generateWsseAuthHeader());
@@ -20,6 +23,10 @@ class EmailControllerTest extends WebTestCase
     {
         $emailId = (int)$this->getReference('email_1')->getId();
         $userId = $this->getReference('simple_user2')->getId();
+
+        $this->setExpectedException('\SoapFault', printf('Email with id "%s" can not be found', self::INCORRECT_ID));
+        $this->soapClient->postAssociation(self::INCORRECT_ID, 'Oro_Bundle_UserBundle_Entity_User', $userId);
+
         $this->soapClient->deleteAssociation($emailId, 'Oro_Bundle_UserBundle_Entity_User', $userId);
         $result = $this->soapClient->postAssociation($emailId, 'Oro_Bundle_UserBundle_Entity_User', $userId);
 
@@ -28,8 +35,12 @@ class EmailControllerTest extends WebTestCase
 
     public function testDeleteAssociation()
     {
-        $emailId = (int)$this->getReference('email_1')->getId();
         $userId = $this->getReference('simple_user2')->getId();
+
+        $this->setExpectedException('\SoapFault', printf('Email with id "%s" can not be found', self::INCORRECT_ID));
+        $this->soapClient->deleteAssociation(self::INCORRECT_ID, 'Oro_Bundle_UserBundle_Entity_User', $userId);
+
+        $emailId = (int)$this->getReference('email_1')->getId();
         $result = $this->soapClient->deleteAssociation($emailId, 'Oro_Bundle_UserBundle_Entity_User', $userId);
 
         $this->assertTrue($result);
