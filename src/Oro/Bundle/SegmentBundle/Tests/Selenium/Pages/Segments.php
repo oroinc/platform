bb477a9e419badd164c43dd60ad61cd7a969d5f1
Roman Grebenchuk <rgrebenchuk@magecore.com>
2015-05-19 14:04:40 +0300

    BAP-8237: Selenium Tests for Segments  - added initial pages

---
+++ src/Oro/Bundle/SegmentBundle/Tests/Selenium/Pages/Segments.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Oro\Bundle\SegmentBundle\Tests\Selenium\Pages;
+
+use Oro\Bundle\TestFrameworkBundle\Pages\AbstractPageFilteredGrid;
+
+/**
+ * Class Segments
+ *
+ * @package Oro\Bundle\SegmentBundle\Tests\Selenium\Pages
+ * @method Segments openSegments openSegments(string)
+ * {@inheritdoc}
+ */
+class Segments extends AbstractPageFilteredGrid
+{
+    const URL = 'segment';
+
+    public function __construct($testCase, $redirect = true)
+    {
+        $this->redirectUrl = self::URL;
+        parent::__construct($testCase, $redirect);
+    }
+
+    public function open($entityData = array())
+    {
+        $contact = $this->getEntity($entityData);
+        $contact->click();
+        sleep(1);
+        $this->waitPageToLoad();
+        $this->waitForAjax();
+
+        return new SegmentData($this->test);
+    }
+}
