44623c437bf2c0421a9191b87f6c2bbe0235ab20
Valentine Velikiy <vvelykyi@magecore.com>
2015-05-21 16:53:07 +0300

    CRM-3326: My Emails ACL

--- src/Oro/Bundle/EmailBundle/EventListener/Datagrid/EmailGridListener.php
+++ src/Oro/Bundle/EmailBundle/EventListener/Datagrid/EmailGridListener.php
@@ -4,19 +4,9 @@ namespace Oro\Bundle\EmailBundle\EventListener\Datagrid;
 
 use Oro\Bundle\DataGridBundle\Datasource\Orm\OrmDatasource;
 use Oro\Bundle\DataGridBundle\Event\BuildAfter;
-use Oro\Bundle\SecurityBundle\ORM\Walker\AclHelper;
 
 class EmailGridListener
 {
-    /**
-     * @var AclHelper
-     */
-    protected $aclHelper;
-
-    public function __construct(AclHelper $aclHelper)
-    {
-        $this->aclHelper = $aclHelper;
-    }
 
     /**
      * Add required filters
@@ -30,8 +20,6 @@ class EmailGridListener
         $queryBuilder = $ormDataSource->getQueryBuilder();
         $parameters = $event->getDatagrid()->getParameters();
 
-        $this->applyAcl($queryBuilder);
-
         if ($parameters->has('emailIds')) {
             $emailIds = $parameters->get('emailIds');
             if (!is_array($emailIds)) {
@@ -39,13 +27,5 @@ class EmailGridListener
             }
             $queryBuilder->andWhere($queryBuilder->expr()->in('e.id', $emailIds));
         }
-    }
-
-    /**
-     * @param $queryBuilder
-     */
-    protected function applyAcl($queryBuilder)
-    {
-        $this->aclHelper->apply($queryBuilder, 'VIEW');
     }
 }
