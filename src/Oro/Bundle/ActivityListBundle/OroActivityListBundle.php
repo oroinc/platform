4fbb73825956c9d066691e8f40a6fd0008ddc862
Maksym Perepelytsya <mperepelytsya@magecore.com>
2015-05-22 16:29:21 +0300

    CRM-3343: Remove imports and service declarations

--- src/Oro/Bundle/ActivityListBundle/OroActivityListBundle.php
+++ src/Oro/Bundle/ActivityListBundle/OroActivityListBundle.php
@@ -6,8 +6,6 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 use Oro\Bundle\ActivityListBundle\DependencyInjection\Compiler\ActivityListProvidersPass;
-use Oro\Bundle\ActivityListBundle\DependencyInjection\Compiler\AfterWidgetProviderPass;
-use Oro\Bundle\ActivityListBundle\DependencyInjection\Compiler\BeforeWidgetProviderPass;
 
 class OroActivityListBundle extends Bundle
 {
@@ -18,7 +16,6 @@ class OroActivityListBundle extends Bundle
     {
         parent::build($container);
 
-        $container
-            ->addCompilerPass(new ActivityListProvidersPass());
+        $container->addCompilerPass(new ActivityListProvidersPass());
     }
 }
