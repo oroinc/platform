4d43913c90b2368387ce82ef31307e541ca63a97
Denys Voronin <dvoronin@magecore.com>
2015-05-27 19:39:48 +0300

    BAP-8267: Fix migration version

--- src/Oro/Bundle/EntityConfigBundle/Migrations/Schema/OroEntityConfigBundleInstaller.php
+++ src/Oro/Bundle/EntityConfigBundle/Migrations/Schema/OroEntityConfigBundleInstaller.php
@@ -14,7 +14,7 @@ class OroEntityConfigBundleInstaller implements Installation
      */
     public function getMigrationVersion()
     {
-        return 'v1_2';
+        return 'v1_3';
     }
 
     /**
