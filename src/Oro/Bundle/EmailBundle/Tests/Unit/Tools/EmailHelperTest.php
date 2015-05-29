55471a5c2c5ac50a239881423f3eeb332a920e9b
Makar Sichevoy <sichevoy@gmail.com>
2015-05-29 11:03:23 +0300

    CRM-2480: User Mailbox Sync with Ownership Type "User" for Emails - Remove unused methods from helper

--- src/Oro/Bundle/EmailBundle/Tests/Unit/Tools/EmailHelperTest.php
+++ src/Oro/Bundle/EmailBundle/Tests/Unit/Tools/EmailHelperTest.php
@@ -31,41 +31,6 @@ class EmailHelperTest extends \PHPUnit_Framework_TestCase
         $this->helper = new EmailHelper($securityFacadeLink);
     }
 
-    public function testGetStrippedBody()
-    {
-        $actualString = '<style type="text/css">H1 {border-width: 1;}</style><div class="new">test</div>';
-        $expectedString = 'test';
-
-        $this->assertEquals($expectedString, $this->helper->getStrippedBody($actualString));
-    }
-
-    /**
-     * @dataProvider shortBodiesProvider
-     */
-    public function testGetShortBody($expected, $actual, $maxLength)
-    {
-        $shortBody = $this->helper->getShortBody($actual, $maxLength);
-        $this->assertEquals($expected, $shortBody);
-    }
-
-    public static function bodiesProvider()
-    {
-        return [
-            ['<p>Hello</p>', '<p>Hello</p>', false],
-            ['<p>Hello</p>', '<p>Hello</p><div class="quote">Other content</div>', false],
-            ['<p>H</p><div class="quote">H</div>', '<p>H</p><div class="quote">H</div>', true]
-        ];
-    }
-
-    public static function shortBodiesProvider()
-    {
-        return [
-            ['abc abc abc', 'abc abc abc abc ', 12],
-            ['abc abc', 'abc abc abc abc abc', 8],
-            ['abcab', 'abcabcabcabc', 5],
-        ];
-    }
-
     public function testIsEmailActionGranted()
     {
         $entity = new Email();
--- src/Oro/Bundle/EmailBundle/Tools/EmailHelper.php
+++ src/Oro/Bundle/EmailBundle/Tools/EmailHelper.php
@@ -3,62 +3,21 @@
 namespace Oro\Bundle\EmailBundle\Tools;
 
 use Oro\Bundle\EmailBundle\Entity\Email;
-use Oro\Bundle\FormBundle\Form\DataTransformer\SanitizeHTMLTransformer;
 use Oro\Bundle\EntityConfigBundle\DependencyInjection\Utils\ServiceLink;
 
 class EmailHelper
 {
-    const MAX_DESCRIPTION_LENGTH = 500;
-
     /**
      * @var ServiceLink
      */
     protected $securityFacadeLink;
 
     /**
-     * @var string
-     */
-    protected $cacheDir;
-
-    /**
      * @param ServiceLink $securityFacadeLink
-     * @param string|null $cacheDir
      */
-    public function __construct(ServiceLink $securityFacadeLink, $cacheDir = null)
+    public function __construct(ServiceLink $securityFacadeLink)
     {
         $this->securityFacadeLink = $securityFacadeLink;
-        $this->cacheDir = $cacheDir;
-    }
-
-    /**
-     * @param string $content
-     * @return string
-     */
-    public function getStrippedBody($content)
-    {
-        $transformer = new SanitizeHTMLTransformer(null, $this->cacheDir);
-        $content = $transformer->transform($content);
-        return strip_tags($content);
-    }
-
-    /**
-     * Get shorter email body
-     *
-     * @param string $content
-     * @param int $maxLength
-     * @return string
-     */
-    public function getShortBody($content, $maxLength = self::MAX_DESCRIPTION_LENGTH)
-    {
-        if (mb_strlen($content) > $maxLength) {
-            $content = mb_substr($content, 0, $maxLength);
-            $lastOccurrencePos = strrpos($content, ' ');
-            if ($lastOccurrencePos !== false) {
-                $content = mb_substr($content, 0, $lastOccurrencePos);
-            }
-        }
-
-        return $content;
     }
 
     /**
