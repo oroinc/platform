18f83f3f70734288a6ed150ee0ab971678b0a711
Dmitry Khrysev <x86demon@gmail.com>
2015-06-10 21:07:00 +0300

    BB-597: Move roles unique name generation to separate listener and depend on RoleInterface

---
+++ src/Oro/Bundle/UserBundle/Entity/AbstractRole.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Oro\Bundle\UserBundle\Entity;
+
+use Symfony\Component\Security\Core\Role\Role as BaseRole;
+
+abstract class AbstractRole extends BaseRole
+{
+    protected $role;
+
+    /**
+     * @return string
+     */
+    abstract public function getPrefix();
+
+    /**
+     * Set role name only for newly created role
+     *
+     * @param  string $role Role name
+     * @return $this
+     */
+    public function setRole($role)
+    {
+        $this->role = (string)strtoupper($role);
+
+        // every role should be prefixed with role prefix
+        if (strpos($this->role, $this->getPrefix()) !== 0) {
+            $this->role = $this->getPrefix() . $this->role;
+        }
+
+        return $this;
+    }
+}
