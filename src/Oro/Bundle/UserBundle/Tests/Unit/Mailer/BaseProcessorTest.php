d80c21e5c03d7fc548681e6e1cea1de74c408b5c
Mykhailo Sulyma <msulima@magecore.com>
2015-06-17 22:03:27 +0300

    BB-611: Refactor email processor to support buyers  - added abstract test for testing mail processors

--- src/Oro/Bundle/UserBundle/Tests/Unit/Mailer/BaseProcessorTest.php
+++ src/Oro/Bundle/UserBundle/Tests/Unit/Mailer/BaseProcessorTest.php
@@ -2,180 +2,26 @@
 
 namespace Oro\Bundle\UserBundle\Tests\Unit\Mailer;
 
-use Doctrine\Common\Persistence\ManagerRegistry;
-use Doctrine\Common\Persistence\ObjectManager;
-
-use Oro\Bundle\ConfigBundle\Config\ConfigManager;
-use Oro\Bundle\EmailBundle\Model\EmailTemplateInterface;
-use Oro\Bundle\EmailBundle\Provider\EmailRenderer;
-use Oro\Bundle\EmailBundle\Entity\Repository\EmailTemplateRepository;
-use Oro\Bundle\EmailBundle\Tools\EmailHolderHelper;
-use Oro\Bundle\UserBundle\Mailer\BaseProcessor;
 use Oro\Bundle\UserBundle\Entity\User;
 
-class BaseProcessorTest extends \PHPUnit_Framework_TestCase
+class BaseProcessorTest extends AbstractProcessorTest
 {
-    const FROM_EMAIL = 'email_from@example.com';
-    const FROM_NAME = 'Email From Name';
-
     /**
-     * @var EmailTemplateRepository|\PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $objectRepository;
-
-    /**
-     * @var ConfigManager|\PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $configManager;
-
-    /**
-     * @var ManagerRegistry|\PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $managerRegistry;
-
-    /**
-     * @var ObjectManager|\PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $objectManager;
-
-    /**
-     * @var EmailRenderer|\PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $renderer;
-
-    /**
-     * @var EmailHolderHelper|\PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $emailHolderHelper;
-
-    /**
-     * @var \Swift_Mailer|\PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $mailer;
-
-    /**
-     * @var BaseProcessor|\PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $mailProcessor;
-
-    /**
-     * @var EmailTemplateInterface|\PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $emailTemplate;
-
-    protected function setUp()
-    {
-        $this->objectRepository = $this->getMockForClass(
-            'Oro\Bundle\EmailBundle\Entity\Repository\EmailTemplateRepository'
-        );
-
-        $this->objectManager = $this->getMockForClass('Doctrine\Common\Persistence\ObjectManager');
-        $this->objectManager->expects($this->any())
-            ->method('getRepository')
-            ->with('OroEmailBundle:EmailTemplate')
-            ->willReturn($this->objectRepository);
-
-        $this->managerRegistry = $this->getMock('Doctrine\Common\Persistence\ManagerRegistry');
-        $this->managerRegistry->expects($this->any())
-            ->method('getManagerForClass')
-            ->with('OroEmailBundle:EmailTemplate')
-            ->willReturn($this->objectManager);
-
-        $this->configManager = $this->getMockForClass('Oro\Bundle\ConfigBundle\Config\ConfigManager');
-        $this->configManager->expects($this->any())
-            ->method('get')
-            ->willReturnMap(
-                [
-                    ['oro_notification.email_notification_sender_email', false, false, self::FROM_EMAIL],
-                    ['oro_notification.email_notification_sender_name', false, false, self::FROM_NAME]
-                ]
-            );
-
-        $this->renderer = $this->getMockForClass('Oro\Bundle\EmailBundle\Provider\EmailRenderer');
-
-        $this->emailHolderHelper = $this->getMockForClass('Oro\Bundle\EmailBundle\Tools\EmailHolderHelper');
-
-        $this->mailer = $this->getMockForClass('\Swift_Mailer');
-
-        $this->mailProcessor = new BaseProcessor(
-            $this->managerRegistry,
-            $this->configManager,
-            $this->renderer,
-            $this->emailHolderHelper,
-            $this->mailer
-        );
-
-        $this->emailTemplate = $this->getMock('Oro\Bundle\EmailBundle\Model\EmailTemplateInterface');
-    }
-
-    protected function tearDown()
-    {
-        unset(
-            $this->user, $this->objectRepository, $this->objectManager, $this->managerRegistry, $this->configManager,
-            $this->renderer, $this->emailHolderHelper, $this->mailer, $this->mailProcessor, $this->emailTemplate
-        );
-    }
-
-    /**
-     *
      * @dataProvider sendEmailResultProvider
      *
-     * @param User           $user
-     * @param string         $templateName
-     * @param array          $templateParams
-     * @param array          $templateData
-     * @param string         $emailType
-     * @param \Swift_Message $expectedMessage
-     * @param string         $mailerResult
+     * @param User $user
+     * @param string $emailType
+     * @param string $expectedEmailType
      */
-    public function testSendEmail(
-        User $user,
-        $templateName,
-        array $templateParams,
-        array $templateData,
-        $emailType,
-        \Swift_Message $expectedMessage,
-        $mailerResult
-    ) {
-        $this->emailTemplate->expects($this->once())
-            ->method('getType')
-            ->willReturn($emailType);
+    public function testSendEmail(User $user, $emailType, $expectedEmailType)
+    {
+        $templateName = 'email_template_name';
+        $templateParams = ['entity' => $user];
+        $expectedMessage = $this->buildMessage($user->getEmail(), 'email subject', 'email body', $expectedEmailType);
 
-        $this->objectRepository->expects($this->once())
-            ->method('findOneBy')
-            ->with(['name' => $templateName])
-            ->willReturn($this->emailTemplate);
+        $this->assertSendCalled($templateName, $templateParams, $expectedMessage, $emailType);
 
-        $this->renderer->expects($this->once())
-            ->method('compileMessage')
-            ->with($this->emailTemplate, $templateParams)
-            ->willReturn($templateData);
-
-        $this->emailHolderHelper->expects($this->once())
-            ->method('getEmail')
-            ->with($user)
-            ->willReturn($user->getEmail());
-
-        $this->mailer->expects($this->once())
-            ->method('send')
-            ->with(
-                $this->callback(
-                    function (\Swift_Message $message) use ($expectedMessage) {
-                        $this->assertEquals($expectedMessage->getSubject(), $message->getSubject());
-                        $this->assertEquals($expectedMessage->getFrom(), $message->getFrom());
-                        $this->assertEquals($expectedMessage->getTo(), $message->getTo());
-                        $this->assertEquals($expectedMessage->getBody(), $message->getBody());
-
-                        return true;
-                    }
-                )
-            )
-            ->willReturn($mailerResult);
-
-        $this->assertEquals(
-            $mailerResult,
-            $this->mailProcessor->getEmailTemplateAndSendEmail($user, $templateName, $templateParams)
-        );
+        $this->mailProcessor->getEmailTemplateAndSendEmail($user, $templateName, $templateParams);
     }
 
     /**
@@ -186,49 +32,17 @@ class BaseProcessorTest extends \PHPUnit_Framework_TestCase
         $user = new User();
         $user->setEmail('email_to@example.com');
 
-        $subject = 'rendered subject';
-        $email = 'rendered template';
-
-        $typePlain = 'text/plain';
-        $typeHtml = 'text/html';
-
-        $messagePlain = \Swift_Message::newInstance()
-            ->setSubject($subject)
-            ->setFrom(self::FROM_EMAIL, self::FROM_NAME)
-            ->setTo($user->getEmail())
-            ->setBody($email, $typePlain);
-
-        $messageHtml = clone $messagePlain;
-        $messageHtml->setBody($email, $typeHtml);
-
         return [
             [
                 'user' => $user,
-                'templateName' => 'test_email',
-                'templateParams' => ['entity' => $user],
-                'templateData' => [$subject, $email],
                 'emailType' => 'txt',
-                'expectedMessage' => $messagePlain,
-                'mailerResult' => 1
+                'expectedEmailType' => 'text/plain'
             ],
             [
                 'user' => $user,
-                'templateName' => 'test_email',
-                'templateParams' => ['entity' => $user],
-                'templateData' => [$subject, $email],
                 'emailType' => 'html',
-                'expectedMessage' => $messageHtml,
-                'mailerResult' => 0
+                'expectedEmailType' => 'text/html'
             ]
         ];
-    }
-
-    /**
-     * @param string $className
-     * @return \PHPUnit_Framework_MockObject_MockObject
-     */
-    protected function getMockForClass($className)
-    {
-        return $this->getMockBuilder($className)->disableOriginalConstructor()->getMock();
     }
 }
--- src/Oro/Bundle/UserBundle/Tests/Unit/Mailer/ProcessorTest.php
+++ src/Oro/Bundle/UserBundle/Tests/Unit/Mailer/ProcessorTest.php
@@ -5,76 +5,69 @@ namespace Oro\Bundle\UserBundle\Tests\Unit\Mailer;
 use Oro\Bundle\UserBundle\Mailer\Processor;
 use Oro\Bundle\UserBundle\Entity\User;
 
-class ProcessorTest extends \PHPUnit_Framework_TestCase
+class ProcessorTest extends AbstractProcessorTest
 {
-    /**
-     * @var Processor|\PHPUnit_Framework_MockObject_MockObject
-     */
+    /** @var Processor|\PHPUnit_Framework_MockObject_MockObject */
     protected $mailProcessor;
 
-    /**
-     * @var User
-     */
+    /** @var User */
     protected $user;
 
     protected function setUp()
     {
-        $this->user = new User();
+        parent::setUp();
 
-        $this->mailProcessor = $this->getMockBuilder('Oro\Bundle\UserBundle\Mailer\Processor')
-            ->disableOriginalConstructor()
-            ->setMethods(['getEmailTemplateAndSendEmail'])
-            ->getMock();
+        $this->user = new User();
+        $this->user
+            ->setEmail('email_to@example.com')
+            ->setPlainPassword('TestPassword');
+
+        $this->mailProcessor = new Processor(
+            $this->managerRegistry,
+            $this->configManager,
+            $this->renderer,
+            $this->emailHolderHelper,
+            $this->mailer
+        );
     }
 
     protected function tearDown()
     {
-        unset($this->user, $this->mailProcessor);
+        parent::tearDown();
+
+        unset($this->user);
     }
 
     public function testSendChangePasswordEmail()
     {
-        $password = 'TestPassword';
+        $this->assertSendCalled(
+            Processor::TEMPLATE_USER_CHANGE_PASSWORD,
+            ['entity' => $this->user, 'plainPassword' => $this->user->getPlainPassword()],
+            $this->buildMessage($this->user->getEmail())
+        );
 
-        $this->user->setPlainPassword($password);
-
-        $this->mailProcessor->expects($this->once())
-            ->method('getEmailTemplateAndSendEmail')
-            ->with(
-                $this->user,
-                Processor::TEMPLATE_USER_CHANGE_PASSWORD,
-                ['entity' => $this->user, 'plainPassword' => $password]
-            )
-            ->willReturn(true);
-
-        $this->assertTrue($this->mailProcessor->sendChangePasswordEmail($this->user));
+        $this->mailProcessor->sendChangePasswordEmail($this->user);
     }
 
     public function testSendResetPasswordEmail()
     {
-        $this->mailProcessor->expects($this->once())
-            ->method('getEmailTemplateAndSendEmail')
-            ->with(
-                $this->user,
-                Processor::TEMPLATE_USER_RESET_PASSWORD,
-                ['entity' => $this->user]
-            )
-            ->willReturn(true);
+        $this->assertSendCalled(
+            Processor::TEMPLATE_USER_RESET_PASSWORD,
+            ['entity' => $this->user],
+            $this->buildMessage($this->user->getEmail())
+        );
 
-        $this->assertTrue($this->mailProcessor->sendResetPasswordEmail($this->user));
+        $this->mailProcessor->sendResetPasswordEmail($this->user);
     }
 
     public function testSendResetPasswordAsAdminEmail()
     {
-        $this->mailProcessor->expects($this->once())
-            ->method('getEmailTemplateAndSendEmail')
-            ->with(
-                $this->user,
-                Processor::TEMPLATE_USER_RESET_PASSWORD_AS_ADMIN,
-                ['entity' => $this->user]
-            )
-            ->willReturn(true);
+        $this->assertSendCalled(
+            Processor::TEMPLATE_USER_RESET_PASSWORD_AS_ADMIN,
+            ['entity' => $this->user],
+            $this->buildMessage($this->user->getEmail())
+        );
 
-        $this->assertTrue($this->mailProcessor->sendResetPasswordAsAdminEmail($this->user));
+        $this->mailProcessor->sendResetPasswordAsAdminEmail($this->user);
     }
 }
