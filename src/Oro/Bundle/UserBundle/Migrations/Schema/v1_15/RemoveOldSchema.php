b3262215f67086b1ad17454ec6d2a9345aa80179
Andrey Mishchenko <amishchenko@magecore.com>
2015-06-17 12:24:12 +0300

    User Mailbox Sync with Ownership Type User for Emails - Migration fix

--- src/Oro/Bundle/UserBundle/Migrations/Schema/v1_15/RemoveOldSchema.php
+++ src/Oro/Bundle/UserBundle/Migrations/Schema/v1_15/RemoveOldSchema.php
@@ -18,7 +18,7 @@ class RemoveOldSchema implements Migration, OrderedMigrationInterface
         $queries->addPreQuery('UPDATE oro_email_origin eo SET eo.owner_id =
           (SELECT ueo.user_id FROM oro_user_email_origin ueo WHERE ueo.origin_id = eo.id)');
         $queries->addPreQuery('UPDATE oro_email_origin eo SET eo.organization_id =
-          (SELECT u.organization_id FROM oro_user u WHERE u.id = eo.user_id)');
+          (SELECT u.organization_id FROM oro_user u WHERE u.id = eo.owner_id)');
 
         $schema->dropTable('oro_user_email_origin');
     }
