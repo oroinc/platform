17deedb1483baebe26f87a7e89b4d0e1a1b95f50
Maksym Perepelytsya <mperepelytsya@magecore.com>
2015-06-18 16:49:42 +0300

    CRM-3549: Add migration to installer

--- src/Oro/Bundle/EmailBundle/Migrations/Schema/v1_12/OroEmailBundle.php
+++ src/Oro/Bundle/EmailBundle/Migrations/Schema/v1_12/OroEmailBundle.php
@@ -7,12 +7,17 @@ use Doctrine\DBAL\Schema\Schema;
 use Oro\Bundle\MigrationBundle\Migration\Migration;
 use Oro\Bundle\MigrationBundle\Migration\QueryBag;
 
-class AddEmbeddedContentIdField implements Migration
+class OroEmailBundle implements Migration
 {
     /**
      * {@inheritdoc}
      */
     public function up(Schema $schema, QueryBag $queries)
+    {
+        self::addEmbeddedContentIdField($schema);
+    }
+
+    public static function addEmbeddedContentIdField(Schema $schema)
     {
         $table = $schema->getTable('oro_email_attachment');
         $table->addColumn('embedded_content_id', 'string', ['length' => 255]);
