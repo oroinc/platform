e71f809dc2567b290ba99331d7e97542cbb0805b
Valentine Velikiy <vvelykyi@magecore.com>
2015-06-24 17:06:22 +0300

    AEIV-76: Create entities for ACL tables to allow use them in DQL, also need to update a migration in SecurityBundle
    Conflicts:
    	src/Oro/Bundle/SecurityBundle/Resources/config/services.yml

---
+++ src/Oro/Bundle/SecurityBundle/Migrations/Schema/v1_1/OroSecurityBundle.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Oro\Bundle\SecurityBundle\Migrations\Schema\v1_1;
+
+use Doctrine\DBAL\Schema\Schema;
+
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+
+class OroSecurityBundle implements Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        // remove platform-depended sql parts, for example "ON UPDATE CASCADE" for MySql
+        $aclEntriesTable = $schema->getTable('acl_entries');
+        $aclEntriesTable->removeForeignKey('FK_46C8B806DF9183C9');
+        $aclEntriesTable->addForeignKeyConstraint(
+            $schema->getTable('acl_security_identities'),
+            ['security_identity_id'],
+            ['id'],
+            ['onDelete' => 'CASCADE'],
+            'FK_46C8B806DF9183C9'
+        );
+
+
+        $aclEntriesTable->removeForeignKey('FK_46C8B8063D9AB4A6');
+        $aclEntriesTable->addForeignKeyConstraint(
+            $schema->getTable('acl_object_identities'),
+            ['object_identity_id'],
+            ['id'],
+            ['onDelete' => 'CASCADE'],
+            'FK_46C8B8063D9AB4A6'
+        );
+
+        $aclEntriesTable->removeForeignKey('FK_46C8B806EA000B10');
+        $aclEntriesTable->addForeignKeyConstraint(
+            $schema->getTable('acl_classes'),
+            ['class_id'],
+            ['id'],
+            ['onDelete' => 'CASCADE'],
+            'FK_46C8B806EA000B10'
+        );
+
+        $aclObjectIdentityAncestorsTable = $schema->getTable('acl_object_identity_ancestors');
+        $aclObjectIdentityAncestorsTable->removeForeignKey('FK_825DE299C671CEA1');
+        $aclObjectIdentityAncestorsTable->addForeignKeyConstraint(
+            $schema->getTable('acl_object_identities'),
+            ['ancestor_id'],
+            ['id'],
+            ['onDelete' => 'CASCADE'],
+            'FK_825DE299C671CEA1'
+        );
+
+        $aclObjectIdentityAncestorsTable->removeForeignKey('FK_825DE2993D9AB4A6');
+        $aclObjectIdentityAncestorsTable->addForeignKeyConstraint(
+            $schema->getTable('acl_object_identities'),
+            ['object_identity_id'],
+            ['id'],
+            ['onDelete' => 'CASCADE'],
+            'FK_825DE2993D9AB4A6'
+        );
+    }
+}
