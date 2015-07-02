09f71391d913c30f9bed3f163c5834a7b8741ceb
Yevhen Shyshkin <yshyshkin@magecore.com>
2015-07-02 19:05:56 +0300

    BB-690: Limit user capabilities form with default scope  - updated template usage

--- src/Oro/Bundle/SecurityBundle/Form/Type/AclAccessLevelSelectorType.php
+++ src/Oro/Bundle/SecurityBundle/Form/Type/AclAccessLevelSelectorType.php
@@ -12,6 +12,8 @@ use Oro\Bundle\SecurityBundle\Model\AclPrivilege;
 
 class AclAccessLevelSelectorType extends AbstractType
 {
+    const NAME = 'oro_acl_access_level_selector';
+
     /**
      * {@inheritdoc}
      */
@@ -25,7 +27,7 @@ class AclAccessLevelSelectorType extends AbstractType
      */
     public function getName()
     {
-        return 'oro_acl_access_level_selector';
+        return self::NAME;
     }
 
     /**
@@ -35,7 +37,6 @@ class AclAccessLevelSelectorType extends AbstractType
     {
         $resolver->setDefaults(array(
             'choices' => AccessLevel::getAccessLevelNames(),
-            'translation_prefix' => 'oro.security.access-level.',
         ));
     }
 
@@ -51,6 +52,6 @@ class AclAccessLevelSelectorType extends AbstractType
             $view->vars['level_label'] = AccessLevel::getAccessLevelName($form->getData());
         }
 
-        $view->vars['translation_prefix'] = $options['translation_prefix'];
+        $view->vars['translation_prefix'] = 'oro.security.access-level.';
     }
 }
