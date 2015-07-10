bb564d9b6976715841d49012de1295c17193f1fb
syzovao <syzovao@gmail.com>
2015-07-10 11:14:31 -0400

    CRM-3627: Create Unique private channel id generator

---
+++ src/Oro/Bundle/UserBundle/Tests/Unit/Provider/UserChannelTokenProviderTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Oro\Bundle\UserBundle\Provider;
+
+//use Symfony\Component\Routing\RouterInterface;
+
+use Oro\Bundle\UserBundle\Entity\User;
+use OroCRM\Bundle\ChannelBundle\Provider\UserChannelTokenProvider;
+
+class UserChannelTokenProviderTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var UserChannelTokenProvider
+     */
+    protected $tokenProvider;
+
+    protected function setUp()
+    {
+        $this->tokenProvider = new UserChannelTokenProvider();
+    }
+
+    protected function getTokenProvider()
+    {
+        return new UserChannelTokenProvider();
+    }
+
+    public function testGetToken()
+    {
+        $user = new User(2);
+        $user->setPlainPassword('qa123123');
+        $token = $this->tokenProvider->getToken($user);
+        $this->assertSame($token, $this->getTokenProvider()->getToken($user));
+    }
+}
