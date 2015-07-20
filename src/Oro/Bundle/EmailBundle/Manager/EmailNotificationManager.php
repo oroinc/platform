c5b1e7de66373b8e084d089331f032ff0ef1ca7e
Dmytro Krushelnytkyi <dkrushelnytskyi@magecore.com>
2015-07-20 16:46:58 +0300

    CRM-2385: Flash message notifications on email synchronization + on the top menu

--- src/Oro/Bundle/EmailBundle/Manager/EmailNotificationManager.php
+++ src/Oro/Bundle/EmailBundle/Manager/EmailNotificationManager.php
@@ -10,6 +10,7 @@ use Oro\Bundle\EmailBundle\Entity\Email;
 use Oro\Bundle\EmailBundle\Exception\LoadEmailBodyException;
 use Oro\Bundle\EmailBundle\Cache\EmailCacheManager;
 use Oro\Bundle\UserBundle\Entity\User;
+use Oro\Bundle\EntityConfigBundle\Config\ConfigManager;
 
 /**
  * Class EmailNotificationManager
@@ -25,17 +26,21 @@ class EmailNotificationManager
     protected $router;
     /** @var EmailCacheManager */
     protected $emailCacheManager;
+    /** @var ConfigManager */
+    protected $configManager;
 
     public function __construct(
         EmailProvider $emailProvider,
         HtmlTagHelper $htmlTagHelper,
         Router $router,
-        EmailCacheManager $emailCacheManager
+        EmailCacheManager $emailCacheManager,
+        ConfigManager $configManager
     ) {
         $this->emailProvider = $emailProvider;
         $this->htmlTagHelper = $htmlTagHelper;
         $this->router = $router;
         $this->emailCacheManager = $emailCacheManager;
+        $this->configManager = $configManager;
     }
 
     public function getEmails(User $user, $maxEmailsDisplay)
@@ -51,22 +56,37 @@ class EmailNotificationManager
             try {
                 $this->emailCacheManager->ensureEmailBodyCached($email);
                 $bodyContent = $this->htmlTagHelper->shorten(
-                    $this->htmlTagHelper->stripTags($this->htmlTagHelper->purify($email->getEmailBody()->getBodyContent()))
+                    $this->htmlTagHelper->stripTags(
+                        $this->htmlTagHelper->purify($email->getEmailBody()->getBodyContent())
+                    )
                 );
             } catch (LoadEmailBodyException $e) {
                 // no content
             }
             $emailsData[] = [
-                'route'=> $this->router->generate('oro_email_email_reply', ['id' => $email->getId()]),
+                'route' => $this->router->generate('oro_email_email_reply', ['id' => $email->getId()]),
                 'id' => $email->getId(),
                 'seen' => $isSeen,
                 'subject' => $email->getSubject(),
                 'bodyContent' => $bodyContent,
-                'fromName' => $email->getFromName()
+                'fromName' => $email->getFromName(),
+                'linkFromName' => $this->getFromNameLink($email)
             ];
         }
 
         return $emailsData;
+    }
+
+    protected function getFromNameLink(Email $email)
+    {
+        $path = false;
+        if ($email->getFromEmailAddress() && $email->getFromEmailAddress()->getOwner()) {
+            $className = $email->getFromEmailAddress()->getOwner()->getClass();
+            $routeName = $this->configManager->getEntityMetadata($className)->getRoute('view', false);
+            $path = $this->router->generate($routeName, ['id' => $email->getFromEmailAddress()->getOwner()->getId()]);
+        }
+
+        return $path;
     }
 
     public function getCountNewEmails(User $user)
