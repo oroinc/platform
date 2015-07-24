25e6622730b23b6535e16789e5a0c57580af7111
Mykhailo Kudelia <mkudelia@magecore.com>
2015-07-24 11:06:03 +0300

    BAP-8613: add abstract multi choice and extent miltienum and dictionary from it

---
+++ src/Oro/Bundle/EntityExtendBundle/Filter/AbstractMultiChoiceFilter.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Oro\Bundle\EntityExtendBundle\Filter;
+
+use Symfony\Component\Form\FormFactoryInterface;
+
+use Oro\Bundle\FilterBundle\Datasource\ManyRelationBuilder;
+use Oro\Bundle\FilterBundle\Filter\ChoiceFilter;
+use Oro\Bundle\FilterBundle\Filter\FilterUtility;
+
+class AbstractMultiChoiceFilter extends ChoiceFilter
+{
+    /** @var ManyRelationBuilder */
+    protected $manyRelationBuilder;
+
+    /**
+     * Constructor
+     *
+     * @param FormFactoryInterface $factory
+     * @param FilterUtility        $util
+     */
+    public function __construct(
+        FormFactoryInterface $factory,
+        FilterUtility $util
+    ) {
+        parent::__construct($factory, $util);
+    }
+}
