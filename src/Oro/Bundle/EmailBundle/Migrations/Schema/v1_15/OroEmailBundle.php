f705d5916cf9dd6e4dcf441b3d59cc819a06a523
krlove <amishchenko@magecore.com>
2015-08-03 12:03:28 +0300

    CRM-3776: Move data migration from fixture to schema - Remove UpdateOriginName migration - Update mailbox_name in schema query

--- src/Oro/Bundle/EmailBundle/Migrations/Schema/v1_15/OroEmailBundle.php
+++ src/Oro/Bundle/EmailBundle/Migrations/Schema/v1_15/OroEmailBundle.php
@@ -4,6 +4,7 @@ namespace Oro\Bundle\EmailBundle\Migrations\Schema\v1_15;
 
 use Doctrine\DBAL\Schema\Schema;
 
+use Oro\Bundle\EmailBundle\Entity\InternalEmailOrigin;
 use Oro\Bundle\MigrationBundle\Migration\Migration;
 use Oro\Bundle\MigrationBundle\Migration\QueryBag;
 
@@ -16,6 +17,7 @@ class OroEmailBundle implements Migration
     {
         self::addEmailFolderFields($schema);
         self::addEmailOriginFields($schema);
+        self::updateMailboxName($queries);
         self::updateEmailRecipientConstraint($schema);
     }
 
@@ -50,6 +52,24 @@ class OroEmailBundle implements Migration
 
         $table->addColumn('mailbox_name', 'string', ['length' => 64, 'notnull' => true, 'default' => '']);
         $table->addIndex(['mailbox_name'], 'IDX_mailbox_name', []);
+    }
+
+    /**
+     * @param QueryBag $queries
+     */
+    public static function updateMailboxName(QueryBag $queries)
+    {
+        $sql = 'UPDATE oro_email_origin SET mailbox_name = %s WHERE name = %s';
+        $originFields = [
+            'internalemailorigin' => '\'' . InternalEmailOrigin::MAILBOX_NAME . '\'',
+            'imapemailorigin' => 'imap_user',
+            'useremailorigin' => 'imap_user',
+            'ewsemailorigin' => 'ews_user_email',
+        ];
+
+        foreach ($originFields as $name => $value) {
+            $queries->addQuery(sprintf($sql, $value, $name));
+        }
     }
 
     /**
