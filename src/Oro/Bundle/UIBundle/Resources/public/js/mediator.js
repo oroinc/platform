29ee9b8377c5027ada0b3bf1f30370abee12a847
dmitrosh <dshykalenko@magecore.com>
2015-08-13 19:53:44 +0300

    OEE-655: 1.8 version of OroCRM EE application doesn't work in FF

--- src/Oro/Bundle/UIBundle/Resources/public/js/mediator.js
+++ src/Oro/Bundle/UIBundle/Resources/public/js/mediator.js
@@ -7,11 +7,20 @@ define([
 ], function (_, Backbone, Chaplin) {
     'use strict';
 
-    Backbone.mediator = _.extend(Chaplin.mediator, Backbone.Events);
+    var mediator = Backbone.mediator = Chaplin.mediator;
+
+    _.extend(mediator, Backbone.Events);
+    /**
+     * Listen Id should be defined before Chaplin.mediator get sealed
+     * on application start
+     */
+    if (!mediator._listenId) {
+        mediator._listenId = _.uniqueId('l');
+    }
 
     /**
      * @export oroui/js/mediator
      * @name   oro.mediator
      */
-    return Chaplin.mediator;
+    return mediator;
 });
