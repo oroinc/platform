afc3e4fb819234f08effcfd9e97cb1c30e84196b
Hryhorii Hrebiniuk <grebinuk@gmail.com>
2015-08-19 19:23:31 +0300

    BAP-8761: JS error is occurred on Add Event/Send Email from user page

--- src/Oro/Bundle/InstallerBundle/Resources/public/js/javascript.js
+++ src/Oro/Bundle/InstallerBundle/Resources/public/js/javascript.js
@@ -5,14 +5,11 @@ $(function() {
     $('.progress-bar li:last-child em.fix-bg').width($('.progress-bar li:last-child').width() / 2);
     $('.progress-bar li:first-child em.fix-bg').width($('.progress-bar li:first-child').width() / 2);
 
-    var $body = $('body');
-
     if (window.localStorage && !localStorage.getItem('oroInstallSplash')) {
-        $body.addClass('start-box-open');
         localStorage.setItem('oroInstallSplash', true);
+        $('body').addClass('start-box-open');
+        $('#begin-install').click(function() {
+            $('body').removeClass('start-box-open');
+        });
     }
-
-    $('#begin-install').click(function() {
-        $body.removeClass('start-box-open');
-    });
 });
