50cd07b00227e03ae60f4fd7fdf519eed0ee715d
Vova Soroka <vsoroka@magecore.com>
2015-08-27 18:23:21 +0300

    BAP-8858: Calendar event guest list doesn't show extra records

--- src/Oro/Bundle/FormBundle/Autocomplete/SearchHandler.php
+++ src/Oro/Bundle/FormBundle/Autocomplete/SearchHandler.php
@@ -57,11 +57,6 @@ class SearchHandler implements SearchHandlerInterface
     protected $aclHelper;
 
     /**
-     * @var int
-     */
-    protected $latestFoundIdsCount = 0;
-
-    /**
      * @var PropertyAccessor
      */
     protected $propertyAccessor;
@@ -149,7 +144,7 @@ class SearchHandler implements SearchHandlerInterface
             $items = $this->searchEntities($query, $firstResult, $perPage);
         }
 
-        $hasMore = $this->latestFoundIdsCount === $perPage;
+        $hasMore = count($items) === $perPage;
         if ($hasMore) {
             $items = array_slice($items, 0, $perPage - 1);
         }
@@ -220,8 +215,6 @@ class SearchHandler implements SearchHandlerInterface
         foreach ($elements as $element) {
             $ids[] = $element->getRecordId();
         }
-
-        $this->latestFoundIdsCount = count($ids);
 
         return $ids;
     }
