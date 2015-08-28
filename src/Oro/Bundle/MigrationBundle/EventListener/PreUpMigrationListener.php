a0f0f4804ca68a8eb6c5aeb9bd9d8a91d940131a
Serhii Pererva <spererva@magecore.com>
2015-08-28 13:51:51 +0300

    BAP-3605: Remove data fixtures data after release  - removed some migrations and listeners supposed to run migrations to remove functionality intended to manage release candidates migrations

--- src/Oro/Bundle/MigrationBundle/EventListener/PreUpMigrationListener.php
+++ src/Oro/Bundle/MigrationBundle/EventListener/PreUpMigrationListener.php
@@ -43,30 +43,6 @@ class PreUpMigrationListener
             }
         } else {
             $event->addMigration(new CreateMigrationTableMigration());
-
-            // load MigrationTable initial data for BAP and OroCRM bundles installed before migrations is introduced
-            // @todo: this transient solution can be removed in a future
-            // when we ensure RC1 and RC2 are updated for all clients
-            if ($event->isTableExist('oro_installer_bundle_version')) {
-                $oroTableDataConfig = $this->kernel
-                    ->locateResource('@OroMigrationBundle/EventListener/MigrationTableData/Oro.yml');
-                $bundleVersions = Yaml::parse(realpath($oroTableDataConfig));
-
-                $oroCrmTableDataConfig = $this->kernel
-                    ->locateResource('@OroMigrationBundle/EventListener/MigrationTableData/OroCRM.yml');
-
-                if ($event->isTableExist('orocrm_account')) {
-                    $bundleVersions = array_merge(
-                        $bundleVersions,
-                        Yaml::parse(realpath($oroCrmTableDataConfig))
-                    );
-                }
-                foreach ($bundleVersions as $bundleName => $version) {
-                    $event->setLoadedVersion($bundleName, $version);
-                }
-                $event->addMigration(new UpdateBundleVersionMigration($bundleVersions));
-                $event->addMigration(new UpdateEntityConfigMigration());
-            }
         }
     }
 }
