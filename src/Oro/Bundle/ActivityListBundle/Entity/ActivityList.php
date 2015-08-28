0e81044752055f311c258dc86a0967dfa6b318c4
Dmytro Krushelnytskyi <dkrushelnytskyi@magecore.com>
2015-08-28 03:05:11 +0300

    CRM-3947: Cases are not added into context of an email they've been created from, nor they contain this email in activities. - fix code standart

--- src/Oro/Bundle/ActivityListBundle/Entity/ActivityList.php
+++ src/Oro/Bundle/ActivityListBundle/Entity/ActivityList.php
@@ -240,7 +240,8 @@ class ActivityList extends ExtendActivityList
     {
         /** @var $owner ActivityOwner */
         foreach ($this->getActivityOwners() as $owner) {
-            if ($owner->getUser() && $activityOwner->getUser() && $owner->getUser()->getId() === $activityOwner->getUser()->getId()
+            if ($owner->getUser() && $activityOwner->getUser() &&
+                $owner->getUser()->getId() === $activityOwner->getUser()->getId()
                 && $owner->getActivity()->getId() === $activityOwner->getActivity()->getId()
             ) {
                 return true;
