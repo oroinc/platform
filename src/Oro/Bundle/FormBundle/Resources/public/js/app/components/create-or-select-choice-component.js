8ab2a97f942cba705703313f7369a8aea7455486
jakubbabiak <jakub@babiak.cz>
2015-08-28 14:14:00 +0200

    CRM-4008: Update JS  - Fix component CS and add EDIT_MODE.

--- src/Oro/Bundle/FormBundle/Resources/public/js/app/components/create-or-select-choice-component.js
+++ src/Oro/Bundle/FormBundle/Resources/public/js/app/components/create-or-select-choice-component.js
@@ -1,21 +1,22 @@
 define([
     'underscore',
     'jquery',
-    'oroui/js/app/components/base/component',
+    'oroui/js/app/components/base/component'
 ], function(_, $, BaseComponent) {
     'use strict';
 
-    var CreateOrSelectChoiceComponent =  BaseComponent.extend({
+    return BaseComponent.extend({
 
         MODE_CREATE: 'create',
         MODE_VIEW: 'view',
+        MODE_EDIT: 'edit',
 
         requiredOptions: [
             'modeSelector',
             'newEntitySelector',
             'existingEntitySelector',
             'existingEntityInputSelector',
-            '_sourceElement',
+            '_sourceElement'
         ],
 
         $el: null,
@@ -23,7 +24,11 @@ define([
         $newEntity: null,
         $existingEntity: null,
         $dialog: null,
+        editable: false,
 
+        /**
+         * @param options
+         */
         initialize: function(options) {
             var missingProperties = _.filter(this.requiredOptions, _.negate(_.bind(options.hasOwnProperty, options)));
             if (missingProperties.length) {
@@ -40,6 +45,7 @@ define([
             this.$existingEntity = this.$el.find(options.existingEntitySelector);
             this.$dialog = this.$el.closest('.ui-dialog');
             this.$dialog.css('top', 0);
+            this.editable = options.editable == true;
 
             this.$existingEntity.on('change', _.bind(this._onEntityChange, this));
             this.$mode.on('change', _.bind(this._updateNewEntityVisibility, this));
@@ -47,19 +53,37 @@ define([
             this._onEntityChange({val: $(options.existingEntityInputSelector).val()});
         },
 
+        /**
+         * Processes change in selected entity field.
+         *
+         * @param e
+         * @private
+         */
         _onEntityChange: function(e) {
-            var mode = e.val ? this.MODE_VIEW : this.MODE_CREATE;
+            var mode = this.MODE_CREATE;
+            if (e.val) {
+                mode = this.editable ? this.MODE_EDIT : this.MODE_VIEW;
+            }
             this._setMode(mode);
         },
 
+        /**
+         * Updates entity form visibility according to mode.
+         *
+         * @private
+         */
         _updateNewEntityVisibility: function() {
-            if (this._isInCreateMode()) {
+            if (this._isInMode(this.MODE_CREATE) || this._isInMode(this.MODE_EDIT)) {
                 this.$newEntity.show();
             } else {
                 this.$newEntity.hide();
             }
         },
 
+        /**
+         * @param mode
+         * @private
+         */
         _setMode: function(mode) {
             if (this.$mode.val() === mode) {
                 return;
@@ -68,10 +92,15 @@ define([
             this.$mode.val(mode).change();
         },
 
-        _isInCreateMode: function() {
-            return this.$mode.val() === this.MODE_CREATE;
+        /**
+         * Checks if is in provided mode.
+         *
+         * @param mode
+         * @returns Boolean
+         * @private
+         */
+        _isInMode: function(mode) {
+            return this.$mode.val() === mode;
         }
     });
-
-    return CreateOrSelectChoiceComponent;
 });
