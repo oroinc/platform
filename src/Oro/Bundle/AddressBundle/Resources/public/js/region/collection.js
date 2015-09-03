e222350a3faea75d4a58533af323b09c6a909f95
Dmitry Khrysev <x86demon@gmail.com>
2015-09-03 13:00:16 +0300

    BB-1049: Address form om frontend does not work

--- src/Oro/Bundle/AddressBundle/Resources/public/js/region/collection.js
+++ src/Oro/Bundle/AddressBundle/Resources/public/js/region/collection.js
@@ -11,15 +11,18 @@ define([
      * @extends Backbone.Collection
      */
     return Backbone.Collection.extend({
-        route: 'oro_api_country_get_regions',
+        defaultOptions: {
+            route: 'oro_api_country_get_regions'
+        },
         url: null,
         model: RegionModel,
 
         /**
          * Constructor
          */
-        initialize: function() {
-            this.url = routing.generate(this.route);
+        initialize: function(models, options) {
+            this.options = _.extend({}, this.defaultOptions, options);
+            this.url = routing.generate(this.options.route);
         },
 
         /**
@@ -28,7 +31,7 @@ define([
          * @param id {string}
          */
         setCountryId: function(id) {
-            this.url = routing.generate(this.route, {country: id});
+            this.url = routing.generate(this.options.route, {country: id});
         }
     });
 });
