0970224cf581f445e5be5e50f8c874b427ff2271
Miloslav Nenadal <miloslav.nenadal@imatic.cz>
2015-09-03 16:07:19 +0200

    CRM-4047: Fix comments
    - show "Custom" if template is not selected

--- src/Oro/Bundle/EmailBundle/Form/Type/AutoResponseTemplateChoiceType.php
+++ src/Oro/Bundle/EmailBundle/Form/Type/AutoResponseTemplateChoiceType.php
@@ -39,6 +39,7 @@ class AutoResponseTemplateChoiceType extends AbstractType
             },
             'configs' => [
                 'allowClear'  => true,
+                'placeholder' => 'oro.form.custom_value',
             ]
         ]);
     }
