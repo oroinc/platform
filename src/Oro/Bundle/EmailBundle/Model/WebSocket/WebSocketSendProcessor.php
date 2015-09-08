702d3a8dbf833fb22b1e989ab3066df1d1d38009
krlove <amishchenko@magecore.com>
2015-09-08 15:09:01 +0300

    BAP-8874: Emails from System Email box are shown in Dashboard and in Notifications - Apply mailbox users and roles for Clank publishing

--- src/Oro/Bundle/EmailBundle/Model/WebSocket/WebSocketSendProcessor.php
+++ src/Oro/Bundle/EmailBundle/Model/WebSocket/WebSocketSendProcessor.php
@@ -2,6 +2,8 @@
 
 namespace Oro\Bundle\EmailBundle\Model\WebSocket;
 
+use Doctrine\Bundle\DoctrineBundle\Registry;
+
 use Oro\Bundle\EmailBundle\Entity\EmailUser;
 use Oro\Bundle\OrganizationBundle\Entity\Organization;
 use Oro\Bundle\SyncBundle\Wamp\TopicPublisher;
@@ -17,11 +19,18 @@ class WebSocketSendProcessor
     protected $publisher;
 
     /**
-     * @param TopicPublisher $publisher
+     * @var Registry
      */
-    public function __construct(TopicPublisher $publisher)
+    protected $doctrine;
+
+    /**
+     * @param TopicPublisher $publisher
+     * @param Registry $doctrine
+     */
+    public function __construct(TopicPublisher $publisher, Registry $doctrine)
     {
         $this->publisher = $publisher;
+        $this->doctrine = $doctrine;
     }
 
     /**
@@ -47,18 +56,55 @@ class WebSocketSendProcessor
             foreach ($usersWithNewEmails as $item) {
                 /** @var EmailUser $emailUser */
                 $emailUser = $item['entity'];
+
+                $topics = $this->getTopicsForPublishing($emailUser);
                 $messageData = [
                     'hasNewEmail' => array_key_exists('new', $item) === true && $item['new'] > 0 ? : false
                 ];
 
-                $this->publisher->send(
-                    self::getUserTopic(
-                        $emailUser->getOwner(),
-                        $emailUser->getOrganization()
-                    ),
-                    json_encode($messageData)
-                );
+                foreach ($topics as $topic) {
+                    $this->publisher->send($topic, json_encode($messageData));
+                }
             }
         }
+    }
+
+    /**
+     * @param EmailUser $emailUser
+     *
+     * @return array
+     */
+    protected function getTopicsForPublishing(EmailUser $emailUser)
+    {
+        $topics = [];
+
+        $organization = $emailUser->getOrganization();
+        $owner = $emailUser->getOwner();
+
+        if ($owner !== null) {
+            $topics[] = self::getUserTopic($owner, $organization);
+        } else {
+            $em = $this->doctrine->getManager();
+
+            $mailbox = $emailUser->getMailboxOwner();
+            $authorizedUsers = $mailbox->getAuthorizedUsers();
+
+            foreach ($authorizedUsers as $user) {
+                $topics[] = self::getUserTopic($user, $organization);
+            }
+
+            $authorizedRoles = $mailbox->getAuthorizedRoles();
+            foreach ($authorizedRoles as $role) {
+                $users = $em->getRepository('OroUserBundle:Role')
+                    ->getUserQueryBuilder($role)
+                    ->getQuery()->getResult();
+
+                foreach ($users as $user) {
+                    $topics[] = self::getUserTopic($user, $organization);
+                }
+            }
+        }
+
+        return $topics;
     }
 }
