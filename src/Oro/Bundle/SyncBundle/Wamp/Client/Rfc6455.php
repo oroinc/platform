8a5390242b8b3af1caaa5db270a2c050da871f91
Miloslav Nenadal <miloslav.nenadal@imatic.cz>
2015-09-08 11:45:18 +0200

    CRM-3859: Number of unread emails not updated immediately
    - Replace Hixie76 with Rfc6455 as the former is poorly implemented in ratchet library

---
+++ src/Oro/Bundle/SyncBundle/Wamp/Client/Rfc6455.php
@@ -0,0 +1,181 @@
+<?php
+
+namespace Oro\Bundle\SyncBundle\Wamp\Client;
+
+class Rfc6455
+{
+    /**
+     * Default socket connection timeout, in seconds
+     */
+    const SOCKET_TIMEOUT = 2;
+
+    /** @var resource */
+    protected $socket;
+
+    /**
+     *
+     * @param string $host
+     * @param int $port
+     *
+     * @return resource
+     */
+    public function connect($host, $port)
+    {
+        if ($this->socket) {
+            return $this->socket;
+        }
+
+        $websocketKey = base64_encode($this->generateRandomString(16));
+
+        $header  = "GET /echo HTTP/1.1\r\n";
+        $header .= 'Host: ' . $host . ':' . $port . "\r\n";
+        $header .= "Upgrade: websocket\r\n";
+        $header .= "Connection: Upgrade\r\n";
+        $header .= "Sec-WebSocket-Key: " . $websocketKey . "\r\n";
+        $header .= "Sec-WebSocket-Version: 13\r\n";
+        $header .= "\r\n";
+
+        $socket = @stream_socket_client('tcp://' . $host . ':' . $port, $errno, $errstr, self::SOCKET_TIMEOUT);
+
+        if (!$socket) {
+            throw new \RuntimeException(sprintf('WebSocket connection error (%u): %s', $errno, $errstr));
+        }
+
+        if (!fwrite($socket, $header)) {
+            throw new \RuntimeException('WebSocket write error');
+        }
+
+        stream_set_blocking($socket, false);
+
+        return $socket;
+    }
+
+    /**
+     * Disconnect
+     */
+    public function disconnect()
+    {
+        stream_socket_shutdown($this->socket, STREAM_SHUT_RDWR);
+    }
+
+    /**
+     * Hoa
+     *
+     *
+     * @license
+     *
+     * New BSD License
+     *
+     * Copyright © 2007-2015, Hoa community. All rights reserved.
+     *
+     * Redistribution and use in source and binary forms, with or without
+     * modification, are permitted provided that the following conditions are met:
+     *     * Redistributions of source code must retain the above copyright
+     *       notice, this list of conditions and the following disclaimer.
+     *     * Redistributions in binary form must reproduce the above copyright
+     *       notice, this list of conditions and the following disclaimer in the
+     *       documentation and/or other materials provided with the distribution.
+     *     * Neither the name of the Hoa nor the names of its contributors may be
+     *       used to endorse or promote products derived from this software without
+     *       specific prior written permission.
+     *
+     * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+     * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+     * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+     * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS AND CONTRIBUTORS BE
+     * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+     * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+     * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+     * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+     * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+     * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+     * POSSIBILITY OF SUCH DAMAGE.
+     *
+     * ============
+     * Link to the original method:
+     * https://github.com/hoaproject/Websocket/blob/bec469034ab9da8d09368f5e4d7e86b395f2af03/Protocol/Rfc6455.php#L226
+     *
+     * @param   string  $message    Message.
+     * @param   int     $opcode     Opcode.
+     * @param   bool    $end        Whether it is the last frame of the message.
+     * @param   bool    $mask       Whether the message will be masked or not.
+     *
+     * @return string
+     */
+    public function createFrame($message, $opcode = 0x1, $end = true, $mask = true)
+    {
+        $fin = true === $end ? 0x1 : 0x0;
+        $rsv1 = 0x0;
+        $rsv2 = 0x0;
+        $rsv3 = 0x0;
+        $mask = true === $mask ? 0x1 : 0x0;
+        $length = strlen($message);
+        $out = chr(
+            ($fin << 7) | ($rsv1 << 6) | ($rsv2 << 5) | ($rsv3 << 4) | $opcode
+        );
+        if (0xffff < $length) {
+            $out .= chr(($mask << 7) | 0x7f) . pack('NN', 0, $length);
+        } elseif (0x7d < $length) {
+            $out .= chr(($mask << 7) | 0x7e) . pack('n', $length);
+        } else {
+            $out .= chr(($mask << 7) | $length);
+        }
+        if (0x0 === $mask) {
+            $out .= $message;
+        } else {
+            $maskingKey = [];
+            if (function_exists('openssl_random_pseudo_bytes')) {
+                $maskingKey = array_map(
+                    'ord',
+                    str_split(
+                        openssl_random_pseudo_bytes(4)
+                    )
+                );
+            } else {
+                for ($i = 0; $i < 4; ++$i) {
+                    $maskingKey[] = mt_rand(1, 255);
+                }
+            }
+            for ($i = 0, $max = strlen($message); $i < $max; ++$i) {
+                $message[$i] = chr(ord($message[$i]) ^ $maskingKey[$i % 4]);
+            }
+            $out .= implode('', array_map('chr', $maskingKey)) . $message;
+        }
+
+        return $out;
+    }
+
+    /**
+     * Generate random string for a WebSocket handshake request headers
+     *
+     * @param  int    $length
+     * @param  bool   $addSpaces
+     * @param  bool   $addNumbers
+     * @return string
+     */
+    protected function generateRandomString($length = 10, $addSpaces = true, $addNumbers = true)
+    {
+        $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"§$%&/()=[]{}';
+        $useChars   = array();
+
+        // select some random chars:
+        for ($i = 0; $i < $length; $i++) {
+            $useChars[] = $characters[mt_rand(0, strlen($characters)-1)];
+        }
+
+        // add spaces and numbers:
+        if ($addSpaces === true) {
+            array_push($useChars, ' ', ' ', ' ', ' ', ' ', ' ');
+        }
+
+        if ($addNumbers === true) {
+            array_push($useChars, rand(0, 9), rand(0, 9), rand(0, 9));
+        }
+
+        shuffle($useChars);
+
+        $randomString = trim(implode('', $useChars));
+
+        return substr($randomString, 0, $length);
+    }
+}
--- src/Oro/Bundle/SyncBundle/Wamp/WebSocket.php
+++ src/Oro/Bundle/SyncBundle/Wamp/WebSocket.php
@@ -2,17 +2,15 @@
 
 namespace Oro\Bundle\SyncBundle\Wamp;
 
+use Oro\Bundle\SyncBundle\Wamp\Client\Rfc6455;
+
 class WebSocket
 {
-    /**
-     * Default socket connection timeout, in seconds
-     */
-    const SOCKET_TIMEOUT = 2;
-
-    /**
-     * @var resource
-     */
+    /** @var resource */
     protected $socket = null;
+
+    /** @var Rfc6455 */
+    protected $version;
 
     /**
      * Initialize web socket connection
@@ -22,12 +20,13 @@ class WebSocket
      */
     public function __construct($host = '127.0.0.1', $port = 8080)
     {
-        $this->connect($host, $port);
+        $this->version = new Rfc6455();
+        $this->socket = $this->version->connect($host, $port);
     }
 
     public function __destruct()
     {
-        $this->disconnect();
+        $this->version->disconnect();
     }
 
     /**
@@ -39,94 +38,12 @@ class WebSocket
      */
     public function sendData($data)
     {
-        if (!@fwrite($this->socket, "\x00" . $data . "\xff")) {
+        if (!@fwrite($this->socket, $this->version->createFrame($data))) {
             throw new \RuntimeException('WebSocket write error');
         }
 
         $wsData = fread($this->socket, 2000);
 
         return trim($wsData, "\x00\xff");
-    }
-
-    /**
-     *
-     * @param  string            $host Host to connect to. Default is localhost (127.0.0.1).
-     * @param  int               $port Port to connect to. Default is 8080.
-     * @return boolean           True on success
-     * @throws \RuntimeException
-     */
-    protected function connect($host, $port)
-    {
-        $key1 = $this->generateRandomString(32);
-        $key2 = $this->generateRandomString(32);
-        $key3 = $this->generateRandomString(8, false, true);
-
-        $header  = "GET /echo HTTP/1.1\r\n";
-        $header .= "Upgrade: WebSocket\r\n";
-        $header .= "Connection: Upgrade\r\n";
-        $header .= "Host: " . $host . ":" . $port . "\r\n";
-        $header .= "Sec-WebSocket-Key1: " . $key1 . "\r\n";
-        $header .= "Sec-WebSocket-Key2: " . $key2 . "\r\n";
-        $header .= "\r\n";
-        $header .= $key3;
-
-        $this->socket = @stream_socket_client('tcp://' . $host . ':' . $port, $errno, $errstr, self::SOCKET_TIMEOUT);
-
-        if (!$this->socket) {
-            throw new \RuntimeException(sprintf('WebSocket connection error (%u): %s', $errno, $errstr));
-        }
-
-        stream_set_blocking($this->socket, false);
-
-        // do a handshake
-        if (!fwrite($this->socket, $header)) {
-            throw new \RuntimeException('WebSocket write error');
-        }
-
-        /**
-         * @todo: check response here. Currently not implemented cause "2 key handshake" is already deprecated.
-         * See: http://en.wikipedia.org/wiki/WebSocket#WebSocket_Protocol_Handshake
-         */
-        // $response = fread($this->socket, 2000);
-        return true;
-    }
-
-    protected function disconnect()
-    {
-        stream_socket_shutdown($this->socket, STREAM_SHUT_RDWR);
-    }
-
-    /**
-     * Generate random string for a WebSocket handshake request headers
-     *
-     * @param  int    $length
-     * @param  bool   $addSpaces
-     * @param  bool   $addNumbers
-     * @return string
-     */
-    protected function generateRandomString($length = 10, $addSpaces = true, $addNumbers = true)
-    {
-        $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"§$%&/()=[]{}';
-        $useChars   = array();
-
-        // select some random chars:
-        for ($i = 0; $i < $length; $i++) {
-            $useChars[] = $characters[mt_rand(0, strlen($characters)-1)];
-        }
-
-        // add spaces and numbers:
-        if ($addSpaces === true) {
-            array_push($useChars, ' ', ' ', ' ', ' ', ' ', ' ');
-        }
-
-        if ($addNumbers === true) {
-            array_push($useChars, rand(0, 9), rand(0, 9), rand(0, 9));
-        }
-
-        shuffle($useChars);
-
-        $randomString = trim(implode('', $useChars));
-
-        return substr($randomString, 0, $length);
     }
 }
