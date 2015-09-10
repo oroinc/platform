1c8a1798c15a6faa459e6917e81517d2cd497445
Vova Soroka <vsoroka@magecore.com>
2015-09-10 19:44:56 +0300

    OEE-713: Do not link User to an email on Outlook's side panel

--- src/Oro/Bundle/ActivityBundle/Entity/Manager/ActivityEntityApiEntityManager.php
+++ src/Oro/Bundle/ActivityBundle/Entity/Manager/ActivityEntityApiEntityManager.php
@@ -2,10 +2,7 @@
 
 namespace Oro\Bundle\ActivityBundle\Entity\Manager;
 
-use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
-
 use Doctrine\Common\Persistence\ObjectManager;
-use Doctrine\Common\Util\ClassUtils;
 use Doctrine\ORM\Query;
 
 use Oro\Bundle\ActivityBundle\Manager\ActivityManager;
@@ -16,22 +13,14 @@ class ActivityEntityApiEntityManager extends ApiEntityManager
     /** @var ActivityManager */
     protected $activityManager;
 
-    /** @var TokenStorageInterface */
-    protected $securityTokenStorage;
-
     /**
-     * @param ObjectManager         $om
-     * @param ActivityManager       $activityManager
-     * @param TokenStorageInterface $securityTokenStorage
+     * @param ObjectManager   $om
+     * @param ActivityManager $activityManager
      */
-    public function __construct(
-        ObjectManager $om,
-        ActivityManager $activityManager,
-        TokenStorageInterface $securityTokenStorage
-    ) {
+    public function __construct(ObjectManager $om, ActivityManager $activityManager)
+    {
         parent::__construct(null, $om);
         $this->activityManager = $activityManager;
-        $this->securityTokenStorage = $securityTokenStorage;
     }
 
     /**
@@ -39,7 +28,7 @@ class ActivityEntityApiEntityManager extends ApiEntityManager
      */
     public function getListQueryBuilder($limit = 10, $page = 1, $criteria = [], $orderBy = null, $joins = [])
     {
-        $queryBuilder = $this->activityManager->getActivityTargetsQueryBuilder(
+        return $this->activityManager->getActivityTargetsQueryBuilder(
             $this->class,
             $criteria,
             $joins,
@@ -47,21 +36,5 @@ class ActivityEntityApiEntityManager extends ApiEntityManager
             $page,
             $orderBy
         );
-
-        /**
-         * Need to exclude current user from result because of email context.
-         * @see \Oro\Bundle\EmailBundle\Entity\Manager\EmailApiEntityManager::getEmailContext
-         */
-        if ($queryBuilder) {
-            $currentUser = $this->securityTokenStorage->getToken()->getUser();
-            // @todo: Filter aliases should be refactored in BAP-8979.
-            $queryBuilder->andWhere(
-                'NOT (id_1 = :userId AND sclr_2 =:userClass)'
-            );
-            $queryBuilder->setParameter('userId', $currentUser->getId());
-            $queryBuilder->setParameter('userClass', ClassUtils::getClass($currentUser));
-        }
-
-        return $queryBuilder;
     }
 }
