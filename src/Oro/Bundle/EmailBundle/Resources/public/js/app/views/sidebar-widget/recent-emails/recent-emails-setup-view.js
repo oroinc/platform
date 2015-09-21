8d094c376706b5b8a32f2ce7825cdb0c071d3b11
dmitrosh <dshykalenko@magecore.com>
2015-09-21 15:46:57 +0300

    CRM-4126: customize select2 control and plug it to sidebar widget

--- src/Oro/Bundle/EmailBundle/Resources/public/js/app/views/sidebar-widget/recent-emails/recent-emails-setup-view.js
+++ src/Oro/Bundle/EmailBundle/Resources/public/js/app/views/sidebar-widget/recent-emails/recent-emails-setup-view.js
@@ -2,11 +2,50 @@ define(function(require) {
     'use strict';
 
     var RecentEmailsContentView;
+    var $ = require('jquery');
+    var _ = require('underscore');
     var __ = require('orotranslation/js/translator');
+    var routing = require('routing');
     var BaseWidgetSetupView = require('orosidebar/js/app/views/base-widget/base-widget-setup-view');
 
     RecentEmailsContentView = BaseWidgetSetupView.extend({
         template: require('tpl!oroemail/templates/sidebar-widget/recent-emails/recent-emails-setup-view.html'),
+
+        foldersData: [],
+
+        initialize: function() {
+            RecentEmailsContentView.__super__.initialize.apply(this, arguments);
+            $.getJSON(routing.generate('oro_api_get_emailorigins'),
+                _.bind(function(data) {
+                    this.foldersData = this.parseFoldersData(data);
+                    this.render();
+                }, this)
+            );
+        },
+
+        parseFoldersData: function(data) {
+            var mailboxes = [];
+            _.each(data, function(mailbox) {
+                var text = mailbox.properties.user;
+                if (text) {
+                    mailboxes.push({
+                        text: text,
+                        children: mailbox.folders.map(function(folder) {
+                            return {
+                                id: folder.id,
+                                text: folder.fullName
+                            };
+                        })
+                    });
+                }
+            });
+            return mailboxes;
+        },
+        getTemplateData: function() {
+            var data = this.model.toJSON();
+            data.foldersData = JSON.stringify(this.foldersData);
+            return data;
+        },
 
         widgetTitle: function() {
             return __('oro.email.recent_emails_widget.settings');
