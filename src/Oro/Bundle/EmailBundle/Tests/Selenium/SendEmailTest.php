8da909e0f364da116ac133a6bd1e1f2ab92ea4c3
Michael Banin <mbanin@magecore.com>
2015-09-21 11:51:12 +0300

    BAP-9005 Implement selenium tests for send Email functionality - test fix

--- src/Oro/Bundle/EmailBundle/Tests/Selenium/SendEmailTest.php
+++ src/Oro/Bundle/EmailBundle/Tests/Selenium/SendEmailTest.php
@@ -69,7 +69,7 @@ class SendEmailTest extends Selenium2TestCase
             ->setLastName('Last_'.$username)
             ->setEmail($username.'@example.com')
             ->setRoles(['Administrator'], true)
-            ->setBusinessUnit(['OroCRM'])
+            ->setBusinessUnit(['Main'])
             ->uncheckInviteUser()
             ->setImap($imapSetting)
             ->save()
@@ -141,7 +141,7 @@ class SendEmailTest extends Selenium2TestCase
             ->setLastName($lastName)
             ->setEmail($username.'@example.com')
             ->setRoles(['Administrator'], true)
-            ->setBusinessUnit(['OroCRM'])
+            ->setBusinessUnit(['Main'])
             ->uncheckInviteUser()
             ->save();
         /** @var Emails $login */
