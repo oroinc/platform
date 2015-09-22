f256c3a16580c05705323e2a1a7680ab7a383332
Andrii Muzalevskyi <amuzalevsky@magecore.com>
2015-09-22 17:30:11 +0300

    CRM-4103 Inline editing support

---
+++ src/Oro/Bundle/DataGridBundle/Resources/public/js/app/components/cell-popup-editor-component.js
@@ -0,0 +1,73 @@
+define(function(require) {
+    'use strict';
+
+    var CellPopupEditorComponent;
+    var _ = require('underscore');
+    var $ = require('jquery');
+    var BaseComponent = require('oroui/js/app/components/base/component');
+    var overlayTool = require('oroui/js/tools/overlay');
+
+    CellPopupEditorComponent = BaseComponent.extend({
+        OVERLAY_TOOL_DEFAULTS: {
+            position: {
+                my: 'left top',
+                at: 'left-5 top-8',
+                collision: 'flipfit'
+            }
+        },
+
+        initialize: function(options) {
+            this.options = options || {};
+
+            this.view = this.createView(options);
+
+            this.listenTo(this.view, 'saveAction', this.onSave);
+            this.listenTo(this.view, 'cancelAction', this.onCancel);
+
+            CellPopupEditorComponent.__super__.initialize.apply(this, arguments);
+
+            this.view.focus();
+        },
+
+        createView: function(options) {
+            var View = options.view;
+            var viewInstance = new View(_.extend({}, options.viewOptions, {
+                autoRender: true,
+                el: $('<form class="inline-editor-wrapper"></form>'),
+                model: options.cell.model,
+                cell: options.cell,
+                column: options.cell.column
+            }));
+
+            var overlayOptions = $.extend(true, {}, this.OVERLAY_TOOL_DEFAULTS, {
+                position: {
+                    of: options.cell.$el
+                }
+            });
+            overlayTool.createOverlay(viewInstance.$el, overlayOptions);
+            return viewInstance;
+        },
+
+        removeView: function() {
+            overlayTool.removeOverlay(this.view.$el);
+        },
+
+        dispose: function() {
+            if (this.disposed) {
+                return;
+            }
+            this.removeView();
+            CellPopupEditorComponent.__super__.dispose.call(this);
+        },
+
+        onSave: function(data) {
+            this.trigger('saveAction', data);
+        },
+
+        onCancel: function() {
+            this.trigger('cancelAction');
+        }
+    });
+
+    return CellPopupEditorComponent;
+});
