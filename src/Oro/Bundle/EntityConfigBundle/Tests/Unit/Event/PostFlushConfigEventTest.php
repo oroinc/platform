ca5939175e2ad785d6a4d855ff0e1f2b0887429c
Vova Soroka <vsoroka@magecore.com>
2015-09-24 23:10:45 +0300

    BAP-8834: Entity Configs Performance Optimization. BAP-8893: Refactor ConfigManager events

--- src/Oro/Bundle/EntityConfigBundle/Tests/Unit/Event/PostFlushConfigEventTest.php
+++ src/Oro/Bundle/EntityConfigBundle/Tests/Unit/Event/PostFlushConfigEventTest.php
@@ -2,19 +2,22 @@
 
 namespace Oro\Bundle\EntityConfigBundle\Tests\Unit\Event;
 
+use Oro\Bundle\EntityConfigBundle\Entity\EntityConfigModel;
 use Oro\Bundle\EntityConfigBundle\Event\PostFlushConfigEvent;
 
 class PostFlushConfigEventTest extends \PHPUnit_Framework_TestCase
 {
     public function testEvent()
     {
-        $models = [];
+        $models = [
+            new EntityConfigModel()
+        ];
         $configManager = $this->getMockBuilder('Oro\Bundle\EntityConfigBundle\Config\ConfigManager')
             ->disableOriginalConstructor()
             ->getMock();
 
         $event = new PostFlushConfigEvent($models, $configManager);
-        $this->assertSame($models, $event->getModels());
+        $this->assertEquals($models, $event->getModels());
         $this->assertSame($configManager, $event->getConfigManager());
     }
 }
