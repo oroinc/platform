075f20f4b022ec87509c7da300f2cd0335eb4625
dmitrosh <dshykalenko@magecore.com>
2015-09-30 15:45:35 +0300

    CRM-4194: Fix You have a new email message

--- src/Oro/Bundle/EmailBundle/Resources/public/js/app/modules/views-module.js
+++ src/Oro/Bundle/EmailBundle/Resources/public/js/app/modules/views-module.js
@@ -1,16 +1,31 @@
-require([
-    'oroui/js/app/controllers/base/controller'
-], function(BaseController) {
+define([
+    'oroui/js/app/controllers/base/controller', 'module'
+], function(BaseController, module) {
     'use strict';
 
     BaseController.loadBeforeAction([
         'jquery',
+        'underscore',
+        'orosync/js/sync',
+        'oroui/js/messenger',
+        'orotranslation/js/translator',
         'oroemail/js/app/components/email-notification-component',
         'oroemail/js/app/models/email-notification/email-notification-count-model'
-    ], function($, EmailNotificationComponent, EmailNotificationCountModel) {
+    ], function($, _, sync, messenger, __, EmailNotificationComponent, EmailNotificationCountModel) {
         BaseController.addToReuse('emailNotification', {
             compose: function() {
                 var $menu = $('.email-notification-menu');
+                var channel = module.config().clankEvent;
+                var onNewEmailRecieved = _.debounce(function() {
+                    var $popover = $menu.find('.new-email-notification');
+                    if ($popover.length > 0) {
+                        if ($menu.hasClass('open') === false) {
+                            $popover.show().delay(5000).fadeOut(1000);
+                        }
+                    } else {
+                        messenger.notificationMessage('success', __('oro.email.notification.new_email'));
+                    }
+                }, 6000, true);
                 if ($menu.length !== 0) {
                     var options = $menu.data('page-component-options');
                     options._sourceElement = $menu.find('.dropdown-menu');
@@ -18,6 +33,12 @@ require([
                     options.countModel = new EmailNotificationCountModel({'unreadEmailsCount': options.count});
                     this.component = new EmailNotificationComponent(options);
                 }
+                sync.subscribe(channel, function(response) {
+                    var message = JSON.parse(response);
+                    if (_.result(message, 'hasNewEmail') === true) {
+                        onNewEmailRecieved();
+                    }
+                });
             }
         });
     });
