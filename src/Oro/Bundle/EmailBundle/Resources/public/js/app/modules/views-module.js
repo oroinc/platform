a01fac7c6dc5979b98d1206b91d8798cdfbcf33e
Hryhorii Hrebiniuk <grebinuk@gmail.com>
2015-10-02 17:55:16 +0300

    CRM-4194: Fix "You have a new email" message
    fixed compositions concept

--- src/Oro/Bundle/EmailBundle/Resources/public/js/app/modules/views-module.js
+++ src/Oro/Bundle/EmailBundle/Resources/public/js/app/modules/views-module.js
@@ -6,13 +6,11 @@ require([
     BaseController.loadBeforeAction([
         'jquery',
         'oroemail/js/app/components/email-notification-component',
-        'oroemail/js/app/components/new-email-message-component',
         'oroemail/js/app/models/email-notification/email-notification-count-model'
-    ], function($, EmailNotificationComponent, NewEmailMessageComponent, EmailNotificationCountModel) {
+    ], function($, EmailNotificationComponent, EmailNotificationCountModel) {
         BaseController.addToReuse('emailNotification', {
             compose: function() {
                 var $menu = $('.email-notification-menu');
-                var $notification = $menu.find('.new-email-notification');
                 if ($menu.length !== 0) {
                     var options = $menu.data('page-component-options');
                     options._sourceElement = $menu.find('.dropdown-menu');
@@ -20,7 +18,18 @@ require([
                     options.countModel = new EmailNotificationCountModel({'unreadEmailsCount': options.count});
                     this.component = new EmailNotificationComponent(options);
                 }
-                this.emailNotificationComponent = new NewEmailMessageComponent({
+            }
+        });
+    });
+
+    BaseController.loadBeforeAction([
+        'jquery',
+        'oroemail/js/app/components/new-email-message-component'
+    ], function($, NewEmailMessageComponent) {
+        BaseController.addToReuse('mewEmailMessage', {
+            compose: function() {
+                var $notification = $('.email-notification-menu .new-email-notification');
+                this.component = new NewEmailMessageComponent({
                     notificationElement: $notification.length > 0 ? $notification : null
                 });
             }
