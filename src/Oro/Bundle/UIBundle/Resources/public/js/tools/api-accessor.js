ef96d56fbc14da7257623eb556e5b35f39c8622a
Andrii Muzalevskyi <amuzalevsky@magecore.com>
2015-10-06 12:29:18 +0300

    CRM-4103 Grid inline editing

--- src/Oro/Bundle/UIBundle/Resources/public/js/tools/api-accessor.js
+++ src/Oro/Bundle/UIBundle/Resources/public/js/tools/api-accessor.js
@@ -6,18 +6,18 @@ define(function(require) {
      * Abstraction of api access point. This class is designed to create from server configuration.
      *
      * Options:
-     * Name | Description
-     * -----|------------
+     * Name  | Description
+     * ------|------------
      * route | Route name
      * http_method | Http method to access this route. (GET|POST|PUT|PATCH...)
-     * form_name | Wraps request body into form_name, so request will look like
+     * form_name | Wraps request body into form_name, so request will look like \
      *             `{<form_name>:{<field_name>: <new_value>}}`
      * headers | Allows to provide additional http headers
-     * default_route_parameters | provides default parameters values for route creation,
+     * default_route_parameters | provides default parameters values for route creation, \
      *                            this defaults will be merged with row model data to get url
-     * query_parameter_names | array of parameter names to put into query string
-     *                         (e.g. ?<parameter-name>=<value>&<parameter-name>=<value>).
-     *                         (The reason is that FOSRestBundle doesn’t provides them for client usage,
+     * query_parameter_names | array of parameter names to put into query string \
+     *                         (e.g. ?<parameter-name>=<value>&<parameter-name>=<value>). \
+     *                         (The reason is that FOSRestBundle doesn’t provides them for client usage, \
      *                         so it is required to specify list of available query parameters)
      *
      * @class
