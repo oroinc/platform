3a626a4d672ae1d4a9d1fb17017ee9cf7a7399f7
Vova Soroka <vsoroka@magecore.com>
2015-10-08 22:07:07 +0300

    BAP-8109: Incorrect pagination on grids with one-to-many relations. Move CuntQueryBuilderOptimizerTest from functional to unit tests

---
+++ src/Oro/Bundle/BatchBundle/Tests/Unit/Fixtures/Entity/Group.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Oro\Bundle\BatchBundle\Tests\Unit\Fixtures\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ */
+class Group
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="BusinessUnit")
+     * @ORM\JoinColumn(name="business_unit_owner_id", referencedColumnName="id", onDelete="SET NULL")
+     */
+    protected $owner;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Organization")
+     * @ORM\JoinColumn(name="organization_id", referencedColumnName="id", onDelete="SET NULL")
+     */
+    protected $organization;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="Role")
+     * @ORM\JoinTable(name="oro_user_access_group_role",
+     *      joinColumns={@ORM\JoinColumn(name="group_id", referencedColumnName="id", onDelete="CASCADE")},
+     *      inverseJoinColumns={@ORM\JoinColumn(name="role_id", referencedColumnName="id", onDelete="CASCADE")}
+     * )
+     */
+    protected $roles;
+}
