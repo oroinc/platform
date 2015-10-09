a6f1a4a60da2b976474fa80861adc49c803a91eb
Sergey Zhuravel <zhuravel.sergey@gmail.com>
2015-10-09 15:14:48 +0300

    BB-1334: Add STI for audit - fix migrations

--- src/Oro/Bundle/DataAuditBundle/Migrations/Schema/v1_6/SetNotNullable.php
+++ src/Oro/Bundle/DataAuditBundle/Migrations/Schema/v1_6/SetNotNullable.php
@@ -5,21 +5,28 @@ namespace Oro\Bundle\DataAuditBundle\Migrations\Schema\v1_6;
 use Doctrine\DBAL\Schema\Schema;
 
 use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\OrderedMigrationInterface;
 use Oro\Bundle\MigrationBundle\Migration\QueryBag;
 
-class OroDataAuditBundle implements Migration
+class SetNotNullable implements Migration, OrderedMigrationInterface
 {
+    /** {@inheritdoc} */
+    public function getOrder()
+    {
+        return 30;
+    }
+
     /**
      * {@inheritdoc}
      */
     public function up(Schema $schema, QueryBag $queries)
     {
         $auditTable = $schema->getTable('oro_audit');
-        $auditTable->addColumn('type', 'string', ['length' => 255]);
+        $auditTable->getColumn('type')->setOptions(['length' => 255]);
         $auditTable->addIndex(['type'], 'idx_oro_audit_type');
 
         $auditFieldTable = $schema->getTable('oro_audit_field');
-        $auditFieldTable->addColumn('type', 'string', ['length' => 255]);
+        $auditFieldTable->getColumn('type')->setOptions(['length' => 255]);
         $auditFieldTable->addIndex(['type'], 'idx_oro_audit_field_type');
     }
 }
