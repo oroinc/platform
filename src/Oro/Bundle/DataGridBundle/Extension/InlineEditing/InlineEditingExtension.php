5fd765b32c11b6f9461549c06db35170d4e3c2db
Ievgenii Myshusta <imyshusta@magecore.com>
2015-10-09 20:03:14 +0300

    CRM-4225: Column configuration implementation

--- src/Oro/Bundle/DataGridBundle/Extension/InlineEditing/InlineEditingExtension.php
+++ src/Oro/Bundle/DataGridBundle/Extension/InlineEditing/InlineEditingExtension.php
@@ -2,25 +2,40 @@
 
 namespace Oro\Bundle\DataGridBundle\Extension\InlineEditing;
 
+use Oro\Bundle\EntityBundle\Helper\DictionaryHelper;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\Translation\TranslatorInterface;
-
-use Oro\Bundle\DataGridBundle\Event\GridViewsLoadEvent;
 use Oro\Bundle\DataGridBundle\Extension\AbstractExtension;
-use Oro\Bundle\DataGridBundle\Datagrid\ParameterBag;
 use Oro\Bundle\DataGridBundle\Datagrid\Common\MetadataObject;
 use Oro\Bundle\DataGridBundle\Datagrid\Common\DatagridConfiguration;
-
-use Oro\Bundle\SecurityBundle\SecurityFacade;
+use Oro\Bundle\DataGridBundle\Extension\Formatter\Configuration as FormatterConfiguration;
+use Oro\Bundle\EntityBundle\ORM\OroEntityManager;
+use Oro\Bundle\DataGridBundle\Extension\Formatter\Property\PropertyInterface;
 
 class InlineEditingExtension extends AbstractExtension
 {
+    /**
+     * @var OroEntityManager
+     */
+    protected $entityManager;
+
+    /**
+     * @var DictionaryHelper
+     */
+    protected $dictionaryHelper;
+
+    public function __construct(OroEntityManager $entityManager, DictionaryHelper $dictionaryHelper)
+    {
+        $this->entityManager = $entityManager;
+        $this->dictionaryHelper = $dictionaryHelper;
+    }
+
     /**
      * {@inheritDoc}
      */
     public function isApplicable(DatagridConfiguration $config)
     {
-        return true;
+        return $config->offsetGetByPath(Configuration::ENABLED_CONFIG_PATH);
     }
 
     /**
@@ -30,9 +45,34 @@ class InlineEditingExtension extends AbstractExtension
      */
     public function processConfigs(DatagridConfiguration $config)
     {
-        $config    = $config->offsetGetOr(Configuration::BASE_CONFIG_KEY, []);
+        $configItems    = $config->offsetGetOr(Configuration::BASE_CONFIG_KEY, []);
         $configuration   = new Configuration(Configuration::BASE_CONFIG_KEY);
-        parent::validateConfiguration($configuration, [Configuration::BASE_CONFIG_KEY => $config]);
+
+        $normalizedConfigItems = $this->validateConfiguration(
+            $configuration,
+            [Configuration::BASE_CONFIG_KEY => $configItems]
+        );
+
+        // replace config values by normalized, extra keys passed directly
+        $config->offsetSet(
+            Configuration::BASE_CONFIG_KEY,
+            array_replace_recursive($configItems, $normalizedConfigItems)
+        );
+
+        //add inline editing where it is possible
+        $columns = $config->offsetGetOr(FormatterConfiguration::COLUMNS_KEY, []);
+        $configParams = $config->offsetGet(Configuration::BASE_CONFIG_KEY);
+        $metadata = $this->entityManager->getClassMetadata($configParams['entity_name']);
+
+        foreach ($columns as $columnName => &$column) {
+            if ($metadata->hasField($columnName)) {
+                $column[Configuration::BASE_CONFIG_KEY] = ['enable' => true];
+            } elseif ($metadata->hasAssociation($columnName)) {
+                //create select list if possible
+            }
+        }
+
+        $config->offsetSet(FormatterConfiguration::COLUMNS_KEY, $columns);
     }
 
     /**
@@ -40,7 +80,21 @@ class InlineEditingExtension extends AbstractExtension
      */
     public function visitMetadata(DatagridConfiguration $config, MetadataObject $data)
     {
-        $data->offsetSet(Configuration::BASE_CONFIG_KEY,
-            $config->offsetGetOr(Configuration::BASE_CONFIG_KEY, []));
+        $data->offsetSet(
+            Configuration::BASE_CONFIG_KEY,
+            $config->offsetGetOr(Configuration::BASE_CONFIG_KEY, [])
+        );
+    }
+
+    protected function prepareChoices($results, $keyField, $labelField)
+    {
+        $resultsData = [];
+        $methodGetPK = 'get' . ucfirst($keyField);
+        $methodGetLabel = 'get' . ucfirst($labelField);
+        foreach ($results as $result) {
+            $resultsData[$result->$methodGetPK()] = $result->$methodGetLabel();
+        }
+
+        return $resultsData;
     }
 }
