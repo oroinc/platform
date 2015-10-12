ea86d5d449ab448dd7ecc108a10a02afe36f66ab
Denis Voronin <dvoronin@magecore.com>
2015-10-12 21:44:17 +0300

    BAP-9064: Possible to created extended fields with identical names  - verify special symbols which are deleted during setter or getters names generation

---
+++ src/Oro/Bundle/EntityExtendBundle/Validator/Constraints/UniqueExtendEntityField.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Oro\Bundle\EntityExtendBundle\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+
+class UniqueExtendEntityField extends Constraint
+{
+    /** @var string  */
+    public $message = 'This value is already used.';
+
+    public $path = 'fieldName';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function validatedBy()
+    {
+        return 'oro_entity_extend.validator.unique_extend_entity_field';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTargets()
+    {
+        return self::CLASS_CONSTRAINT;
+    }
+}
