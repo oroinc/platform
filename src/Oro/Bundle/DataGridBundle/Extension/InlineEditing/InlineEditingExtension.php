2ffb0ab8be447ee8e2cd2845730b727e41b4ebdb
Ievgenii Myshusta <imyshusta@magecore.com>
2015-10-14 19:26:12 +0300

    CRM-4079: Inline editing of string, numeric, and date values in grids - refactored

--- src/Oro/Bundle/DataGridBundle/Extension/InlineEditing/InlineEditingExtension.php
+++ src/Oro/Bundle/DataGridBundle/Extension/InlineEditing/InlineEditingExtension.php
@@ -2,56 +2,31 @@
 
 namespace Oro\Bundle\DataGridBundle\Extension\InlineEditing;
 
-use Doctrine\ORM\Mapping\ClassMetadata;
-use Oro\Bundle\DataGridBundle\Extension\Formatter\Property\PropertyInterface;
-use Oro\Bundle\SecurityBundle\ORM\Walker\AclHelper;
 use Oro\Bundle\SecurityBundle\SecurityFacade;
 use Oro\Bundle\DataGridBundle\Extension\AbstractExtension;
 use Oro\Bundle\DataGridBundle\Datagrid\Common\MetadataObject;
 use Oro\Bundle\DataGridBundle\Datagrid\Common\DatagridConfiguration;
 use Oro\Bundle\DataGridBundle\Extension\Formatter\Configuration as FormatterConfiguration;
-use Oro\Bundle\EntityBundle\ORM\OroEntityManager;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
-use Symfony\Component\Validator\Mapping\ClassMetadata as ValidatorMetadata;
+use Oro\Bundle\DataGridBundle\Extension\Formatter\Property\PropertyInterface;
 
 class InlineEditingExtension extends AbstractExtension
 {
     /**
-     * @var OroEntityManager
+     * @var InlineEditColumnOptionsGuesser
      */
-    protected $entityManager;
+    protected $guesser;
 
     /**
      * @var SecurityFacade
      */
     protected $securityFacade;
 
-    /**
-     * @var AclHelper
-     */
-    protected $aclHelper;
-
-    /**
-     * @var ValidatorInterface
-     */
-    protected $validator;
-
-    /**
-     * @param OroEntityManager   $entityManager
-     * @param SecurityFacade     $securityFacade
-     * @param AclHelper          $aclHelper
-     * @param ValidatorInterface $validator
-     */
     public function __construct(
-        OroEntityManager $entityManager,
-        SecurityFacade $securityFacade,
-        AclHelper $aclHelper,
-        ValidatorInterface $validator
+        InlineEditColumnOptionsGuesser $inlineEditColumnOptionsGuesser,
+        SecurityFacade $securityFacade
     ) {
-        $this->entityManager = $entityManager;
         $this->securityFacade = $securityFacade;
-        $this->aclHelper = $aclHelper;
-        $this->validator = $validator;
+        $this->guesser = $inlineEditColumnOptionsGuesser;
     }
 
     /**
@@ -95,7 +70,20 @@ class InlineEditingExtension extends AbstractExtension
             $columns = $config->offsetGetOr(FormatterConfiguration::COLUMNS_KEY, []);
 
             $blackList = $configuration->getBlackList();
-            $columns = $this->guessInlineEditingForColumns($columns, $configItems['entity_name'], $blackList);
+
+            foreach ($columns as $columnName => &$column) {
+                if (!in_array($columnName, $blackList)) {
+                    $newColumn = $this->guesser->getColumnOptions($columnName, $configItems['entity_name'], $column);
+
+                    //frontend type key must not be replaced with default value
+                    $typeKey = PropertyInterface::FRONTEND_TYPE_KEY;
+                    if (!empty($newColumn[$typeKey])) {
+                        $column[$typeKey] = $newColumn[$typeKey];
+                    }
+
+                    $column = array_replace_recursive($newColumn, $column);
+                }
+            }
 
             $config->offsetSet(FormatterConfiguration::COLUMNS_KEY, $columns);
         }
@@ -110,138 +98,5 @@ class InlineEditingExtension extends AbstractExtension
             Configuration::BASE_CONFIG_KEY,
             $config->offsetGetOr(Configuration::BASE_CONFIG_KEY, [])
         );
-    }
-
-    /**
-     * @param ClassMetadata $metadata
-     * @param string        $columnName
-     *
-     * @return string
-     *
-     * @throws \Exception
-     */
-    protected function guessLabelField($metadata, $columnName)
-    {
-        $labelField = '';
-
-        if ($metadata->hasField('label')) {
-            $labelField = 'label';
-        } elseif ($metadata->hasField('name')) {
-            $labelField = 'name';
-        } else {
-            //get first field with type "string"
-            $isStringFieldPresent = false;
-            foreach ($metadata->getFieldNames() as $fieldName) {
-                if ($metadata->getTypeOfField($fieldName) === "string") {
-                    $labelField = $fieldName;
-                    $isStringFieldPresent = true;
-                    break;
-                }
-            }
-
-            if (!$isStringFieldPresent) {
-                throw new \Exception(
-                    "Could not find any field for using as label for 'choices' of '$columnName' column."
-                );
-            }
-        }
-
-        return $labelField;
-    }
-
-    /**
-     * @param string $entity
-     * @param string $keyField
-     * @param string $labelField
-     *
-     * @return array
-     */
-    protected function getChoices($entity, $keyField, $labelField)
-    {
-        $queryBuilder = $this->entityManager
-            ->getRepository($entity)
-            ->createQueryBuilder('e');
-        //select only id and label fields
-        $queryBuilder->select("e.$keyField, e.$labelField");
-
-        $result = $this->aclHelper->apply($queryBuilder)->getResult();
-        $choices = [];
-        foreach ($result as $item) {
-            $choices[$item[$keyField]] = $item[$labelField];
-        }
-
-        return $choices;
-    }
-
-    /**
-     * @param array  $columns
-     * @param string $entityName
-     * @param array  $blackList
-     *
-     * @return mixed
-     */
-    protected function guessInlineEditingForColumns($columns, $entityName, $blackList)
-    {
-        $metadata = $this->entityManager->getClassMetadata($entityName);
-        /** @var ValidatorMetadata $validatorMetadata */
-        $validatorMetadata = $this->validator->getMetadataFor($entityName);
-
-        foreach ($columns as $columnName => &$column) {
-            $newColumn = [];
-            if ($metadata->hasField($columnName)
-                && !in_array($columnName, $blackList)
-                && !$metadata->hasAssociation($columnName)
-            ) {
-                $newColumn[Configuration::BASE_CONFIG_KEY] = ['enable' => true];
-                if ($validatorMetadata->hasPropertyMetadata($columnName)) {
-                    $newColumn[Configuration::BASE_CONFIG_KEY]['validation_rules'] =
-                        $this->getValidationRules($validatorMetadata, $columnName);
-                }
-            } elseif ($metadata->hasAssociation($columnName)) {
-                $mapping = $metadata->getAssociationMapping($columnName);
-                if ($mapping['type'] === ClassMetadata::MANY_TO_ONE) {
-                    $targetEntity = $metadata->getAssociationTargetClass($columnName);
-
-                    $targetEntityMetadata = $this->entityManager->getClassMetadata($targetEntity);
-                    if (isset($newColumn[Configuration::BASE_CONFIG_KEY]['view_options']['value_field_name'])) {
-                        $labelField = $newColumn[Configuration::BASE_CONFIG_KEY]['view_options']['value_field_name'];
-                    } else {
-                        $labelField = $this->guessLabelField($targetEntityMetadata, $columnName);
-                    }
-
-                    $newColumn[Configuration::BASE_CONFIG_KEY] = ['enable' => true];
-                    if ($validatorMetadata->hasPropertyMetadata($columnName)) {
-                        $newColumn[Configuration::BASE_CONFIG_KEY]['validation_rules'] =
-                            $this->getValidationRules($validatorMetadata, $columnName);
-                    }
-                    $newColumn[PropertyInterface::FRONTEND_TYPE_KEY] = 'select';
-                    $keyField = $targetEntityMetadata->getSingleIdentifierFieldName();
-                    $newColumn['choices'] = $this->getChoices($targetEntity, $keyField, $labelField);
-                }
-            }
-
-            $column = array_replace_recursive($newColumn, $column);
-        }
-
-        return $columns;
-    }
-
-    /**
-     * @param ValidatorMetadata $validatorMetadata
-     * @param string            $columnName
-     * @return array
-     */
-    protected function getValidationRules($validatorMetadata, $columnName)
-    {
-        $metadata = $validatorMetadata->getPropertyMetadata($columnName);
-        $metadata = is_array($metadata) && isset($metadata[0]) ? $metadata[0] : $metadata;
-
-        $rules = [];
-        foreach ($metadata->getConstraints() as $constraint) {
-            $reflectionClass = new \ReflectionClass($constraint);
-            $rules[$reflectionClass->getShortName()] = (array)$constraint;
-        }
-
-        return $rules;
     }
 }
