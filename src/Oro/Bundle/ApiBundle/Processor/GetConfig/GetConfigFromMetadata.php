d0b940aff937097f6f1cf25236e58f2f528cae4a
Vova Soroka <vsoroka@magecore.com>
2015-10-20 01:37:27 +0300

    BAP-9170: EntitySerializer config generator based on metadata - relation types

--- src/Oro/Bundle/ApiBundle/Processor/GetConfig/GetConfigFromMetadata.php
+++ src/Oro/Bundle/ApiBundle/Processor/GetConfig/GetConfigFromMetadata.php
@@ -37,15 +37,24 @@ class GetConfigFromMetadata implements ProcessorInterface
             return;
         }
 
-        $config = [
-            'exclusion_policy' => 'all'
-        ];
-
         $metadata = $this->doctrineHelper->getEntityMetadata($entityClass);
 
-        $fields = array_fill_keys($metadata->getFieldNames(), null);
+        $fields       = array_fill_keys($metadata->getFieldNames(), null);
+        $associations = $metadata->getAssociationMappings();
+        foreach ($associations as $fieldName => $mapping) {
+            $targetIdFields     = $this->doctrineHelper->getEntityIdentifierFieldNames($mapping['targetEntity']);
+            $fields[$fieldName] = [
+                'exclusion_policy' => 'all',
+                'fields'           => count($targetIdFields) === 1
+                    ? reset($targetIdFields)
+                    : $targetIdFields
+            ];
+        }
 
-        $config['fields'] = $fields;
+        $config = [
+            'exclusion_policy' => 'all',
+            'fields'           => $fields
+        ];
 
         $context->setResult($config);
     }
