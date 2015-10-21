6c86edc8a57da0829acdffa7024057566691527b
Hryhorii Hrebiniuk <hhrebiniuk@magecore.com>
2015-10-21 18:06:03 +0300

    CRM-4370: Missed part of email validation on frontend side

--- src/Oro/Bundle/UIBundle/Resources/public/js/tools/patterns.js
+++ src/Oro/Bundle/UIBundle/Resources/public/js/tools/patterns.js
@@ -6,7 +6,7 @@ define(function() {
 
     return {
         // source http://www.regular-expressions.info/email.html
-        email: /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/
+        email: /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/i
     };
 
     /* jshint ignore:end */
