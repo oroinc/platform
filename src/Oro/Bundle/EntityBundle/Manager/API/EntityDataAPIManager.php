6f9a71b50f7495904e4635b2d014e841f9fe1c53
Dmytro Krushelnytskyi <dkrushelnytskyi@magecore.com>
2015-10-21 14:07:34 +0300

    CRM-4079: Inline editing of string, numeric, and date values in grids

--- src/Oro/Bundle/EntityBundle/Manager/API/EntityDataAPIManager.php
+++ src/Oro/Bundle/EntityBundle/Manager/API/EntityDataAPIManager.php
@@ -7,6 +7,7 @@ use FOS\RestBundle\Util\Codes;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\Security\Core\Authorization\AuthorizationChecker;
 
+use Oro\Bundle\EntityBundle\Tools\EntityRoutingHelper;
 use Oro\Bundle\EntityBundle\Entity\Manager\Field\EntityFieldManager;
 
 class EntityDataAPIManager
@@ -17,28 +18,55 @@ class EntityDataAPIManager
     /** @var  AuthorizationChecker */
     protected $securityService;
 
+    /** @var EntityRoutingHelper */
+    protected $entityRoutingHelper;
+
     public function __construct(
         EntityFieldManager $entityDataManager,
-        AuthorizationChecker $securityService
+        AuthorizationChecker $securityService,
+        EntityRoutingHelper $entityRoutingHelper
     ) {
         $this->entityDataManager = $entityDataManager;
         $this->securityService = $securityService;
+        $this->entityRoutingHelper = $entityRoutingHelper;
     }
 
-    public function patch($entity, $data)
+    /**
+     * @param $className
+     * @param $id
+     * @param $data
+     *
+     * @return array
+     * @throws \Exception
+     */
+    public function patch($className, $id, $data)
     {
+        $entity = $this->getEntity($className, $id);
+
         if (!$this->securityService->isGranted('EDIT', $entity)) {
             throw new AccessDeniedException();
         }
 
         $result = $this->entityDataManager->update($entity, $data);
-        $form = $result['form'];
-        $changeSet = $result['changeSet'];
 
-        if ($form->getErrors()->count() > 0) {
-            throw new \Exception($form->getErrors()->first()->getMessage(), Codes::HTTP_BAD_REQUEST);
+        return $result;
+    }
+
+    /**
+     * @param $className
+     * @param $id
+     *
+     * @return object
+     * @throws \Exception
+     */
+    protected function getEntity($className, $id)
+    {
+        try {
+            $entity = $this->entityRoutingHelper->getEntity($className, $id);
+        } catch (\Exception $e) {
+            throw new \Exception($e->getMessage(), Codes::HTTP_NOT_FOUND);
         }
 
-        return $changeSet;
+        return $entity;
     }
 }
