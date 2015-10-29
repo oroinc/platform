1f0f23ce12944045e4bdfc5e2ab04bb4379468ba
Mykhailo Kudelia <mkudelia@magecore.com>
2015-10-29 13:26:26 +0200

    BAP-9268: Custom Tooltips

--- src/Oro/Bundle/FormBundle/Form/Extension/TooltipFormExtension.php
+++ src/Oro/Bundle/FormBundle/Form/Extension/TooltipFormExtension.php
@@ -6,6 +6,11 @@ use Symfony\Component\Form\AbstractTypeExtension;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Translation\TranslatorInterface;
+
+use Oro\Bundle\EntityBundle\Provider\EntityFieldProvider;
+use Oro\Bundle\EntityConfigBundle\Provider\ConfigProvider;
+use Oro\Bundle\FormBundle\Utils\FormUtils;
 
 class TooltipFormExtension extends AbstractTypeExtension
 {
@@ -20,6 +25,30 @@ class TooltipFormExtension extends AbstractTypeExtension
         'tooltip_placement',
         'tooltip_parameters'
     );
+
+    /** @var EntityFieldProvider */
+    protected $entityFieldProvider;
+
+    /** @var ConfigProvider */
+    protected $entityConfigProvider;
+
+    /** @var TranslatorInterface */
+    protected $translator;
+
+    /**
+     * @param EntityFieldProvider $entityFieldProvider
+     * @param ConfigProvider $entityConfigProvider
+     * @param TranslatorInterface $translator
+     */
+    public function __construct(
+        EntityFieldProvider $entityFieldProvider,
+        ConfigProvider $entityConfigProvider,
+        TranslatorInterface $translator
+    ) {
+        $this->entityFieldProvider = $entityFieldProvider;
+        $this->entityConfigProvider = $entityConfigProvider;
+        $this->translator = $translator;
+    }
 
     /**
      * {@inheritdoc}
@@ -37,6 +66,49 @@ class TooltipFormExtension extends AbstractTypeExtension
         foreach ($this->optionalParameters as $parameter) {
             if (isset($options[$parameter])) {
                 $view->vars[$parameter] = $options[$parameter];
+            }
+        }
+
+        $this->prepareModifyFieldTooltip($form, $options);
+    }
+
+    /**
+     * @param FormInterface $form
+     * @param array $options
+     */
+    public function prepareModifyFieldTooltip($form, $options)
+    {
+        if (empty($options['data_class'])) {
+            return;
+        }
+
+        $className = $options['data_class'];
+        $fields = $this->entityFieldProvider->getFields($className);
+
+        foreach ($fields as $value) {
+            $fieldName = $value['name'];
+            $skipField = !$form->has($fieldName) || !$this->entityConfigProvider->hasConfig($className, $fieldName);
+            //skip field if it doesn't contain in form, config or contains own custom tooltip in form
+            if ($skipField || $form->get($fieldName)->getConfig()->getOption('tooltip') !== null) {
+                continue;
+            }
+            $this->modifyFieldTooltip($className, $form, $form->get($fieldName));
+        }
+    }
+
+    /**
+     * @param string $className
+     * @param FormInterface $form
+     * @param FormInterface $field
+     */
+    public function modifyFieldTooltip($className, $form, $field)
+    {
+        if (!$field->getConfig()->getOption('description')) {
+            $tooltipRaw = $this->entityConfigProvider->getConfig($className, $field->getName())->get('description');
+            $tooltip = $this->translator->trans($tooltipRaw);
+            //if text has been added by user in gui
+            if ($tooltipRaw !== $tooltip) {
+                FormUtils::replaceField($form, $field->getName(), ['tooltip' => $tooltip]);
             }
         }
     }
--- src/Oro/Bundle/FormBundle/Tests/Unit/Form/Extension/TooltipFormExtensionTest.php
+++ src/Oro/Bundle/FormBundle/Tests/Unit/Form/Extension/TooltipFormExtensionTest.php
@@ -7,6 +7,41 @@ use Symfony\Component\Form\FormView;
 
 class TooltipFormExtensionTest extends \PHPUnit_Framework_TestCase
 {
+    /** @var \PHPUnit_Framework_MockObject_MockObject */
+    protected $fieldProvider;
+
+    /** @var \PHPUnit_Framework_MockObject_MockObject */
+    protected $configProvider;
+
+    /** @var \PHPUnit_Framework_MockObject_MockObject */
+    protected $translator;
+
+    /** @var \PHPUnit_Framework_MockObject_MockObject */
+    protected $form;
+
+    protected function setUp()
+    {
+        $this->fieldProvider = $this
+            ->getMockBuilder('Oro\Bundle\EntityBundle\Provider\EntityFieldProvider')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->configProvider = $this
+            ->getMockBuilder('Oro\Bundle\EntityConfigBundle\Provider\ConfigProvider')
+            ->setMethods(['hasConfig', 'getConfig', 'get'])
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->form = $this
+            ->getMockBuilder('Symfony\Component\Form\Form')
+            ->setMethods(['has', 'get', 'getConfig', 'getOption', 'getName', 'getOptions', 'getType'])
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->translator = $this
+            ->getMockBuilder('Symfony\Component\Translation\TranslatorInterface')
+            ->setMethods(['trans', 'transChoice', 'setLocale', 'getLocale'])
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
     public function testSetDefaultOptions()
     {
         $resolver = $this->getMockBuilder('Symfony\Component\OptionsResolver\OptionsResolverInterface')
@@ -24,7 +59,7 @@ class TooltipFormExtensionTest extends \PHPUnit_Framework_TestCase
                 )
             );
 
-        $extension = new TooltipFormExtension();
+        $extension = new TooltipFormExtension($this->fieldProvider, $this->configProvider, $this->translator);
         $extension->setDefaultOptions($resolver);
         $this->assertEquals('form', $extension->getExtendedType());
     }
@@ -42,12 +77,89 @@ class TooltipFormExtensionTest extends \PHPUnit_Framework_TestCase
         $form = $this->getMockBuilder('Symfony\Component\Form\Form')
             ->disableOriginalConstructor()
             ->getMock();
-        $extension = new TooltipFormExtension();
+        $extension = new TooltipFormExtension($this->fieldProvider, $this->configProvider, $this->translator);
         $extension->buildView($view, $form, $options);
 
         foreach ($options as $option => $value) {
             $this->assertArrayHasKey($option, $view->vars);
             $this->assertEquals($options[$option], $view->vars[$option]);
+        }
+    }
+
+    /**
+     * @param string $formTooltipValue
+     * @param bool $valueIsOverriden
+     *
+     * @dataProvider modifyTooltipFieldProvider
+     */
+    public function testModifyTooltipField($formTooltipValue, $valueIsOverriden)
+    {
+        $fields = [['name' => 'test_field']];
+        $options['data_class'] = 'Oro\Bundle\UserBundle\Entity\User';
+
+        $this->fieldProvider
+            ->expects($this->once())
+            ->method('getFields')
+            ->with($options['data_class'])
+            ->will($this->returnValue($fields));
+        $this->configProvider
+            ->expects($this->once())
+            ->method('hasConfig')
+            ->will($this->returnValue(true));
+        $this->form
+            ->expects($this->once())
+            ->method('has')
+            ->with('test_field')
+            ->will($this->returnValue(true));
+        $this->form
+            ->expects($this->exactly((int) $valueIsOverriden))
+            ->method('getName');
+        $this->translator
+            ->expects($this->exactly((int) $valueIsOverriden))
+            ->method('trans')
+            ->will($this->returnValue('raw_tooltip'));
+
+        $this->mockMethods($this->form, ['get', 'getConfig', 'getOption'], $formTooltipValue);
+        $this->mockMethods($this->configProvider, ['getConfig', 'get'], 'raw_tooltip');
+        $extension = new TooltipFormExtension($this->fieldProvider, $this->configProvider, $this->translator);
+        $extension->buildView(new FormView(), $this->form, $options);
+    }
+
+    /**
+     * @return array
+     */
+    public function modifyTooltipFieldProvider()
+    {
+        return [
+            'Value isnt overriden' => [
+                'formTooltipValue' => 'Tooltip description',
+                'valueIsOverriden' => false
+            ],
+            'Value is overriden' => [
+                'formTooltipValue' => null,
+                'valueIsOverriden' => true
+            ]
+        ];
+    }
+
+    /**
+     * Mock methods recursively
+     *
+     * @param object $object
+     * @param array $methods
+     * @param mixed $lastReturnValue
+     */
+    protected function mockMethods($object, $methods, $lastReturnValue)
+    {
+        if ($methods) {
+            foreach ($methods as $key => $method) {
+                $mock = $object->expects($this->any())->method($method);
+                if ($key >= count($methods) - 1) {
+                    $mock->will($this->returnValue($lastReturnValue));
+                } else {
+                    $mock->will($this->returnValue($object));
+                }
+            }
         }
     }
 }
