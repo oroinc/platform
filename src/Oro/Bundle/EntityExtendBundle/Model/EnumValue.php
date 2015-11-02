6ef8bc08c861fe2ffbc4fabeaaa01d745b7a7380
Yauheni Kasenka <beta.screech@gmail.com>
2015-11-02 14:57:42 +0300

    BB-1412: Apply validation rules for import data - updated Import Strategy - created EnumValueValidator - created EnumValue model and added validation rules to validation.yml - moved constraints to buildForm() and configureOptioins() on EnumValueType

---
+++ src/Oro/Bundle/EntityExtendBundle/Model/EnumValue.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace Oro\Bundle\EntityExtendBundle\Model;
+
+class EnumValue
+{
+    /**
+     * @var int
+     */
+    protected $id;
+
+    /**
+     * @var string
+     */
+    protected $label;
+
+    /**
+     * @var bool
+     */
+    //protected $is_default = false;
+    protected $is_default;
+
+    /**
+     * @var int
+     */
+    protected $priority;
+
+    /**
+     *
+     * @param array $data
+     * @return EnumValue
+     */
+    public function fromArray(array $data)
+    {
+        if (isset($data['id'])) {
+            $this->setLabel($data['id']);
+        }
+
+        if (isset($data['label'])) {
+            $this->setLabel($data['label']);
+        }
+
+        if (isset($data['is_default'])) {
+            $this->setIsDefault((bool)$data['is_default']);
+        }
+
+        if (isset($data['priority'])) {
+            $this->setPriority($data['priority']);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function toArray()
+    {
+        $data = [];
+
+        if ($this->getId()) {
+            $data['id'] = $this->getId();
+        }
+        if ($this->getLabel()) {
+            $data['label'] = $this->getLabel();
+        }
+        if ($this->getIsDefault()) {
+            $data['is_default'] = $this->getIsDefault();
+        }
+        if ($this->getPriority()) {
+            $data['priority'] = $this->getPriority();
+        }
+
+        return $data;
+    }
+
+    /**
+     * Set id
+     *
+     * @param integer $id
+     *
+     * @return EnumValue
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set label
+     *
+     * @param string $label
+     *
+     * @return EnumValue
+     */
+    public function setLabel($label)
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    /**
+     * Get label
+     *
+     * @return string
+     */
+    public function getLabel()
+    {
+        return $this->label;
+    }
+
+    /**
+     * Set isDefault
+     *
+     * @param boolean $isDefault
+     *
+     * @return EnumValue
+     */
+    public function setIsDefault($isDefault)
+    {
+        $this->is_default = $isDefault;
+
+        return $this;
+    }
+
+    /**
+     * Get isDefault
+     *
+     * @return boolean
+     */
+    public function getIsDefault()
+    {
+        return $this->is_default;
+    }
+
+    /**
+     * Set priority
+     *
+     * @param integer $priority
+     *
+     * @return EnumValue
+     */
+    public function setPriority($priority)
+    {
+        $this->priority = $priority;
+
+        return $this;
+    }
+
+    /**
+     * Get priority
+     *
+     * @return integer
+     */
+    public function getPriority()
+    {
+        return $this->priority;
+    }
+}
---
+++ src/Oro/Bundle/EntityExtendBundle/Validator/Constraints/EnumValueValidator.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Oro\Bundle\EntityExtendBundle\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
+
+use Oro\Bundle\EntityExtendBundle\Model\EnumValue as EnumValueEntity;
+use Oro\Bundle\EntityExtendBundle\Tools\ExtendHelper;
+
+class EnumValueValidator extends ConstraintValidator
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function validate($entity, Constraint $constraint)
+    {
+        if (!$entity instanceof EnumValueEntity) {
+            throw new UnexpectedTypeException(
+                $entity,
+                'Oro\Bundle\EntityExtendBundle\Model\EnumValue'
+            );
+        }
+
+        /* @var $entity EnumValueEntity */
+        if ($entity->getId()) {
+            return;
+        }
+
+        $valueId = ExtendHelper::buildEnumValueId($entity->getLabel(), false);
+
+        if (empty($valueId)) {
+            $this->context->addViolationAt('label', $constraint->message, ['{{ value }}' => $entity->getLabel()]);
+        }
+    }
+}
