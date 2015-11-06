72a771b6ff54b3a15cf1b092b63e91f4e07bc5b5
Dmytro Krushelnytskyi <dkrushelnytskyi@magecore.com>
2015-11-06 17:02:54 +0200

    CRM-4459: MailChimp Campaings is not worked if at least one active MailChimp Export job fix add comments

--- src/Oro/Bundle/IntegrationBundle/Provider/BlockingJobsInterface.php
+++ src/Oro/Bundle/IntegrationBundle/Provider/BlockingJobsInterface.php
@@ -4,5 +4,10 @@ namespace Oro\Bundle\IntegrationBundle\Provider;
 
 interface BlockingJobsInterface
 {
+    /**
+     * Get array of command name that can block start command oro:cron:integration:sync
+     *
+     * @return array
+     */
     public function getCommandName();
 }
