aede2f9b52ee452fd137ccd10a80f7d673429085
dmitrosh <dshykalenko@magecore.com>
2015-11-17 13:53:33 +0200

    CRM-4539: add SelectAll functionality to column manager

--- src/Oro/Bundle/DataGridBundle/Resources/public/js/app/views/column-manager/column-manager-view.js
+++ src/Oro/Bundle/DataGridBundle/Resources/public/js/app/views/column-manager/column-manager-view.js
@@ -7,7 +7,60 @@ define(function(require) {
     ColumnManagerView = BaseView.extend({
         template: require('tpl!orodatagrid/templates/column-manager/column-manager.html'),
         autoRender: true,
-        className: 'dropdown-menu'
+        className: 'dropdown-menu',
+        events: {
+            'click [data-role="column-manager-select-all"]': 'onSelectAll',
+            'click [data-role="column-manager-unselect-all"]': 'onUnselectAll'
+        },
+
+        listen: {
+            'change:renderable collection': 'updateView'
+        },
+
+        initialize: function(options) {
+            ColumnManagerView.__super__.initialize.call(this, options);
+            this.filterer = _.bind(options.columnFilterModel.filterer);
+            this.updateView = _.debounce(this.updateView, 50);
+            this.listenTo(options.columnFilterModel, 'change', this.updateView);
+        },
+
+        render: function() {
+            ColumnManagerView.__super__.render.call(this);
+            this.updateView();
+            return this;
+        },
+
+        updateView: function() {
+            var models = this._getFilteredModels();
+            var hasUnrenderable = Boolean(_.find(models, function(model) {return !model.get('renderable')}));
+            this.$('[data-role="column-manager-select-all"]').toggleClass('active', hasUnrenderable);
+            this.$('[data-role="column-manager-unselect-all"]')
+                .toggleClass('active', models.length > 0 && !hasUnrenderable);
+        },
+
+        onSelectAll: function(e) {
+            e.preventDefault();
+            _.each(this._getFilteredModels(), function(model) {
+                model.set('renderable', true);
+            });
+            this.updateView();
+        },
+
+        onUnselectAll: function(e) {
+            e.preventDefault();
+            _.each(this._getFilteredModels().reverse(), function(model) {
+                if (!model.get('disabledVisibilityChange')) {
+                    model.set('renderable', false);
+                }
+            });
+            this.updateView();
+        },
+
+        _getFilteredModels: function() {
+            return _.filter(this.collection.filter(this.filterer), function(model) {
+                return !model.get('disabledVisibilityChange');
+            });
+        }
     });
 
     return ColumnManagerView;
