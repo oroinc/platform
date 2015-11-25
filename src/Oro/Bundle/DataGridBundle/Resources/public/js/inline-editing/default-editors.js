e9a08d8fc0eba303cc3736d6a97c1d0e2a9711af
Sergii Zhdanyuk <szhdanyuk@magecore.com>
2015-11-25 18:08:19 +0200

    CRM-4551 Fixed markdown links.

--- src/Oro/Bundle/DataGridBundle/Resources/public/js/inline-editing/default-editors.js
+++ src/Oro/Bundle/DataGridBundle/Resources/public/js/inline-editing/default-editors.js
@@ -10,70 +10,70 @@ define(function(require) {
      */
     var defaultEditors = {
         /**
-         * Please see [text-editor-view](../frontend/editor/text-editor-view.md) for details
+         * Please see [text-editor-view](../../../../../FormBundle/Resources/doc/editor/text-editor-view.md) for details
          *
          * @type {function(new:TextEditorView)}
          */
         string: require('oroform/js/app/views/editor/text-editor-view'),
 
         /**
-         * Please see [text-editor-view](../frontend/editor/text-editor-view.md) for details
+         * Please see [text-editor-view](../../../../../FormBundle/Resources/doc/editor/text-editor-view.md) for details
          *
          * @type {function(new:TextEditorView)}
          */
         phone: require('oroform/js/app/views/editor/text-editor-view'),
 
         /**
-         * Please see [datetime-editor-view](../frontend/editor/datetime-editor-view.md) for details
+         * Please see [datetime-editor-view](../../../../../FormBundle/Resources/doc/editor/datetime-editor-view.md) for details
          *
          * @type {Function}
          */
         datetime: require('oroform/js/app/views/editor/datetime-editor-view'),
 
         /**
-         * Please see [date-editor-view](../frontend/editor/date-editor-view.md) for details
+         * Please see [date-editor-view](../../../../../FormBundle/Resources/doc/editor/date-editor-view.md) for details
          *
          * @type {Function}
          */
         date: require('oroform/js/app/views/editor/date-editor-view'),
 
         /**
-         * Please see [number-editor-view](../frontend/editor/number-editor-view.md) for details
+         * Please see [number-editor-view](../../../../../FormBundle/Resources/doc/editor/number-editor-view.md) for details
          *
          * @type {Function}
          */
         currency: require('oroform/js/app/views/editor/number-editor-view'),
 
         /**
-         * Please see [number-editor-view](../frontend/editor/number-editor-view.md) for details
+         * Please see [number-editor-view](../../../../../FormBundle/Resources/doc/editor/number-editor-view.md) for details
          *
          * @type {Function}
          */
         number: require('oroform/js/app/views/editor/number-editor-view'),
 
         /**
-         * Please see [number-editor-view](../frontend/editor/number-editor-view.md) for details
+         * Please see [number-editor-view](../../../../../FormBundle/Resources/doc/editor/number-editor-view.md) for details
          *
          * @type {Function}
          */
         integer: require('oroform/js/app/views/editor/number-editor-view'),
 
         /**
-         * Please see [number-editor-view](../frontend/editor/number-editor-view.md) for details
+         * Please see [number-editor-view](../../../../../FormBundle/Resources/doc/editor/number-editor-view.md) for details
          *
          * @type {Function}
          */
         decimal: require('oroform/js/app/views/editor/number-editor-view'),
 
         /**
-         * Please see [percent-editor-view](../frontend/editor/percent-editor-view.md) for details
+         * Please see [percent-editor-view](../../../../../FormBundle/Resources/doc/editor/percent-editor-view.md) for details
          *
          * @type {Function}
          */
         percent: require('oroform/js/app/views/editor/percent-editor-view'),
 
         /**
-         * Please see [select-editor-view](../frontend/editor/select-editor-view.md) for details
+         * Please see [select-editor-view](../../../../../FormBundle/Resources/doc/editor/select-editor-view.md) for details
          *
          * @type {Function}
          */
