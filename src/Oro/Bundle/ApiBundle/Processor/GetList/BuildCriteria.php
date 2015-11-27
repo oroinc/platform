e3aeefbc0a59e8f059e08ac25a87207904def672
Yurii Muratov <yurio@ua.fm>
2015-11-27 15:24:19 +0200

    BAP-9409: Restrict sorters by indexed fields only

--- src/Oro/Bundle/ApiBundle/Processor/GetList/BuildCriteria.php
+++ src/Oro/Bundle/ApiBundle/Processor/GetList/BuildCriteria.php
@@ -2,6 +2,8 @@
 
 namespace Oro\Bundle\ApiBundle\Processor\GetList;
 
+use Oro\Bundle\ApiBundle\Filter\SortFilter;
+use Oro\Bundle\ApiBundle\Util\ConfigUtil;
 use Oro\Component\ChainProcessor\ContextInterface;
 use Oro\Component\ChainProcessor\ProcessorInterface;
 
@@ -26,6 +28,20 @@ class BuildCriteria implements ProcessorInterface
             $filterValue = null;
             if ($filterValues->has($filterKey)) {
                 $filterValue = $filterValues->get($filterKey);
+            }
+
+            if ($filter instanceof SortFilter) {
+                $sortersConfigFields = $context->getConfigOfSorters()[ConfigUtil::FIELDS];
+                foreach ($filterValue->getValue() as $field => $direction) {
+                    if (!array_key_exists($field, $sortersConfigFields)) {
+                        throw new \InvalidArgumentException(
+                            sprintf(
+                                'Sorting by the field "%s" does not supports',
+                                $field
+                            )
+                        );
+                    }
+                }
             }
             $filter->apply($criteria, $filterValue);
         }
