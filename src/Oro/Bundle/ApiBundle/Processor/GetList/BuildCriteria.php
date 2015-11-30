0fcca1d4b04df6a1f3dda29e8981244887b4589c
Vova Soroka <vsoroka@magecore.com>
2015-11-30 23:36:12 +0200

    BAP-9409: Restrict sorters by indexed fields only

--- src/Oro/Bundle/ApiBundle/Processor/GetList/BuildCriteria.php
+++ src/Oro/Bundle/ApiBundle/Processor/GetList/BuildCriteria.php
@@ -2,10 +2,10 @@
 
 namespace Oro\Bundle\ApiBundle\Processor\GetList;
 
-use Oro\Bundle\ApiBundle\Filter\SortFilter;
-use Oro\Bundle\ApiBundle\Util\ConfigUtil;
 use Oro\Component\ChainProcessor\ContextInterface;
 use Oro\Component\ChainProcessor\ProcessorInterface;
+use Oro\Bundle\ApiBundle\Filter\SortFilter;
+use Oro\Bundle\ApiBundle\Util\ConfigUtil;
 
 class BuildCriteria implements ProcessorInterface
 {
@@ -30,20 +30,34 @@ class BuildCriteria implements ProcessorInterface
                 $filterValue = $filterValues->get($filterKey);
             }
 
-            if ($filter instanceof SortFilter) {
-                $sortersConfigFields = $context->getConfigOfSorters()[ConfigUtil::FIELDS];
-                foreach ($filterValue->getValue() as $field => $direction) {
-                    if (!array_key_exists($field, $sortersConfigFields)) {
-                        throw new \InvalidArgumentException(
-                            sprintf(
-                                'Sorting by the field "%s" is not supported',
-                                $field
-                            )
-                        );
-                    }
+            if ($filter instanceof SortFilter && $context->hasConfigOfSorters()) {
+                $this->validateSortValue(
+                    null !== $filterValue ? $filterValue->getValue() : null,
+                    $context->hasConfigOfSorters() ? $context->getConfigOfSorters() : null
+                );
+            }
+
+            $filter->apply($criteria, $filterValue);
+        }
+    }
+
+    /**
+     * @param array|null $orderBy
+     * @param array|null $sorters
+     */
+    protected function validateSortValue($orderBy, $sorters)
+    {
+        if (!empty($orderBy)) {
+            $sortFields = !empty($sorters) && !empty($sorters[ConfigUtil::FIELDS])
+                ? $sorters[ConfigUtil::FIELDS]
+                : [];
+            foreach ($orderBy as $field => $direction) {
+                if (!array_key_exists($field, $sortFields)) {
+                    throw new \RuntimeException(
+                        sprintf('Sorting by "%s" is not supported.', $field)
+                    );
                 }
             }
-            $filter->apply($criteria, $filterValue);
         }
     }
 }
