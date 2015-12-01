d8dad11216f30cfa001301383fe9396957c87343
Vova Soroka <vsoroka@magecore.com>
2015-12-01 11:51:38 +0200

    BAP-8910: REST API for GET requests. Allow to use configuration of parent entity

--- src/Oro/Bundle/ApiBundle/Processor/Config/GetConfig/LoadFromConfigBag.php
+++ src/Oro/Bundle/ApiBundle/Processor/Config/GetConfig/LoadFromConfigBag.php
@@ -6,18 +6,25 @@ use Oro\Component\ChainProcessor\ContextInterface;
 use Oro\Component\ChainProcessor\ProcessorInterface;
 use Oro\Bundle\ApiBundle\Processor\Config\ConfigContext;
 use Oro\Bundle\ApiBundle\Provider\ConfigBag;
+use Oro\Bundle\ApiBundle\Util\ConfigUtil;
+use Oro\Bundle\EntityBundle\Provider\EntityHierarchyProviderInterface;
 
 class LoadFromConfigBag implements ProcessorInterface
 {
     /** @var ConfigBag */
     protected $configBag;
 
+    /** @var EntityHierarchyProviderInterface */
+    protected $entityHierarchyProvider;
+
     /**
-     * @param ConfigBag $configBag
+     * @param ConfigBag                        $configBag
+     * @param EntityHierarchyProviderInterface $entityHierarchyProvider
      */
-    public function __construct(ConfigBag $configBag)
+    public function __construct(ConfigBag $configBag, EntityHierarchyProviderInterface $entityHierarchyProvider)
     {
-        $this->configBag = $configBag;
+        $this->configBag               = $configBag;
+        $this->entityHierarchyProvider = $entityHierarchyProvider;
     }
 
     /**
@@ -32,9 +39,47 @@ class LoadFromConfigBag implements ProcessorInterface
             return;
         }
 
-        $config = $this->configBag->getConfig($context->getClassName(), $context->getVersion());
+        $config = $this->loadConfig($context->getClassName(), $context->getVersion());
         if (null !== $config) {
             $context->setResult($config);
         }
+    }
+
+    /**
+     * @param string $entityClass
+     * @param string $version
+     *
+     * @return array|null
+     */
+    protected function loadConfig($entityClass, $version)
+    {
+        $config = $this->configBag->getConfig($entityClass, $version);
+        if (null === $config || ConfigUtil::isInherit($config)) {
+            $parentClasses = $this->entityHierarchyProvider->getHierarchyForClassName($entityClass);
+            foreach ($parentClasses as $parentClass) {
+                $parentConfig = $this->configBag->getConfig($parentClass, $version);
+                if (!empty($parentConfig)) {
+                    $config = $this->mergeConfigs($parentConfig, $config);
+                    if (!ConfigUtil::isInherit($parentConfig)) {
+                        break;
+                    }
+                }
+            }
+        }
+
+        return $config;
+    }
+
+    /**
+     * @param array      $parentConfig
+     * @param array|null $config
+     *
+     * @return array
+     */
+    protected function mergeConfigs($parentConfig, $config)
+    {
+        return null === $config
+            ? $parentConfig
+            : array_merge_recursive($parentConfig, $config);
     }
 }
