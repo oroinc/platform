68ca30e32dc076317be8ee3256e5e09d5ed93885
Dmytro Krushelnytskyi <dkrushelnytskyi@magecore.com>
2015-12-01 20:50:59 +0200

    CRM-4459: MailChimp Campaings does not work if at least one active MailChimp Export job

--- src/Oro/Bundle/IntegrationBundle/Entity/Repository/ChannelRepository.php
+++ src/Oro/Bundle/IntegrationBundle/Entity/Repository/ChannelRepository.php
@@ -56,12 +56,12 @@ class ChannelRepository extends EntityRepository
 
     /**
      * @param string[]|string $commandName
-     * @param string $arguments
-     * @param string[]|string $states
+     * @param string[]|string|null $arguments
+     * @param string[]|string|null $states
      *
      * @return QueryBuilder
      */
-    protected function getQBSyncJobs($commandName, $arguments, $states)
+    protected function getQBSyncJobs($commandName, $arguments = null, $states = null)
     {
         /** @var QueryBuilder $qb */
         $qb = $this->getEntityManager()
@@ -74,31 +74,43 @@ class ChannelRepository extends EntityRepository
             $qb->andWhere('j.command=:commandName');
         }
 
-        if (is_array($states)) {
-            $qb->andWhere('j.state in (:stateName)');
-        } else {
-            $qb->andWhere('j.state=:stateName');
+        if (!empty($states)) {
+            if (is_array($states)) {
+                $qb->andWhere('j.state in (:stateName)');
+            } else {
+                $qb->andWhere('j.state=:stateName');
+            }
         }
 
         $qb->setParameter('stateName', $states)
             ->setParameter('commandName', $commandName);
 
         if (!empty($arguments)) {
-            $qb->andWhere($qb->expr()->like('cast(j.args as text)', ':args'))
-                ->setParameter('args', '%' . $arguments . '%');
+            if (is_array($arguments)) {
+                $orX = $qb->expr()->orX();
+                foreach($arguments as $key => $argument) {
+                    $orX->add($qb->expr()->like('cast(j.args as text)', ':args_'.$key));
+                    $qb->setParameter('args_'.$key, '%' . $argument . '%');
+                }
+
+                $qb->andWhere($orX);
+            } else {
+                $qb->andWhere($qb->expr()->like('cast(j.args as text)', ':args'))
+                    ->setParameter('args', '%' . $arguments . '%');
+            }
         }
 
         return $qb;
     }
 
     /**
-     * @param string $commandName
-     * @param string $arguments
-     * @param string[] $states
+     * @param string[]|string $commandName
+     * @param string[]|string|null $arguments
+     * @param string[]|string|null $states
      *
      * @return QueryBuilder
      */
-    protected function getQBSyncJobsCount($commandName, $arguments, $states)
+    protected function getQBSyncJobsCount($commandName, $arguments = null, $states = null)
     {
         $qb = $this->getQBSyncJobs($commandName, $arguments, $states);
         $qb->select('count(j.id)');
@@ -108,12 +120,12 @@ class ChannelRepository extends EntityRepository
 
     /**
      * @param string $commandName
-     * @param string[] $states
-     * @param string $arguments
+     * @param string[]|string|null $arguments
+     * @param string[]|string|null $states
      *
      * @return int
      */
-    public function getSyncJobsCount($commandName, $states, $arguments)
+    public function getSyncJobsCount($commandName, $states = null, $arguments = null)
     {
         /** @var QueryBuilder $qb */
         $qb = $this->getQBSyncJobsCount($commandName, $arguments, $states);
