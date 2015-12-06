2ad636d15925a8ee65ccbae4bb2214a3cfa7711e
ReenExe <ipourri@gmail.com>
2015-12-06 03:13:51 +0200

    clear code

--- src/Oro/Bundle/CalendarBundle/Provider/PublicCalendarProvider.php
+++ src/Oro/Bundle/CalendarBundle/Provider/PublicCalendarProvider.php
@@ -48,16 +48,11 @@ class PublicCalendarProvider implements CalendarProviderInterface
      */
     public function getCalendarDefaultValues($organizationId, $userId, $calendarId, array $calendarIds)
     {
-        $result = [];
-
         if (!$this->calendarConfig->isPublicCalendarEnabled()) {
-            foreach ($calendarIds as $id) {
-                $result[$id] = null;
-            }
-
-            return $result;
+            return array_fill_keys($calendarIds, null);
         }
 
+        $result = [];
         /** @var SystemCalendarRepository $repo */
         $repo = $this->doctrineHelper->getEntityRepository('OroCalendarBundle:SystemCalendar');
         $qb = $repo->getPublicCalendarsQueryBuilder();
