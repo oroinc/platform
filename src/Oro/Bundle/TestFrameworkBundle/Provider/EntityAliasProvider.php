9548427df362457d250ee05798a7ec6d70f2bbb0
Yurii Muratov <yurio@ua.fm>
2015-12-08 18:17:24 +0200

    BAP-9408: Fix API requests for B2B instance

--- src/Oro/Bundle/TestFrameworkBundle/Provider/EntityAliasProvider.php
+++ src/Oro/Bundle/TestFrameworkBundle/Provider/EntityAliasProvider.php
@@ -4,12 +4,18 @@ namespace Oro\Bundle\TestFrameworkBundle\Provider;
 
 use Oro\Bundle\EntityBundle\Provider\EntityAliasProviderInterface;
 
+/**
+ * This alias provider excludes aliases generation for test entities to avoid duplications of aliases.
+ * TestActivity entity does not covered by this class because it should have aliases.
+ */
 class EntityAliasProvider implements EntityAliasProviderInterface
 {
     protected static $classes = [
         'Oro\Bundle\TestFrameworkBundle\Entity\Item',
         'Oro\Bundle\TestFrameworkBundle\Entity\ItemValue',
-        'Oro\Bundle\TestFrameworkBundle\Entity\Product'
+        'Oro\Bundle\TestFrameworkBundle\Entity\Product',
+        'Oro\Bundle\TestFrameworkBundle\Entity\TestActivityTarget',
+        'Oro\Bundle\TestFrameworkBundle\Entity\WorkflowAwareEntity',
     ];
 
     /**
