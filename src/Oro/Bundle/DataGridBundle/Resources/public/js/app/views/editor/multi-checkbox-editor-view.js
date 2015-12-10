eda6cf78ac8ec7181c5f101dd47b4d1dc98afed5
dmitrosh <dshykalenko@magecore.com>
2015-12-10 15:33:51 +0200

    CRM-4698: CR improvements and documentation

--- src/Oro/Bundle/DataGridBundle/Resources/public/js/app/views/editor/multi-checkbox-editor-view.js
+++ src/Oro/Bundle/DataGridBundle/Resources/public/js/app/views/editor/multi-checkbox-editor-view.js
@@ -1,6 +1,52 @@
-/** @lends MultiselectEditorView */
+/** @lends MultiCheckboxEditorView */
 define(function(require) {
     'use strict';
+
+    /**
+     * Multi-select content editor. Please note that it requires column data format
+     * corresponding to multi-select-cell.
+     *
+     * ### Column configuration samples:
+     * ``` yml
+     * datagrid:
+     *   {grid-uid}:
+     *     inline_editing:
+     *       enable: true
+     *     # <grid configuration> goes here
+     *     columns:
+     *       # Sample 1. Full configuration
+     *       {column-name-1}:
+     *         inline_editing:
+     *           editor:
+     *             view: orodatagrid/js/app/views/editor/multi-checkbox-editor-view
+     *             view_options:
+     *               css_class_name: '<class-name>'
+     *           validation_rules:
+     *             NotBlank: true
+     * ```
+     *
+     * ### Options in yml:
+     *
+     * Column option name                                  | Description
+     * :---------------------------------------------------|:-----------
+     * inline_editing.editor.view_options.css_class_name   | Optional. Additional css class name for editor view DOM el
+     * inline_editing.editor.validation_rules | Optional. Validation rules. See [documentation](https://goo.gl/j9dj4Y)
+     *
+     * ### Constructor parameters
+     *
+     * @class
+     * @param {Object} options - Options container
+     * @param {Object} options.model - Current row model
+     * @param {Backgrid.Cell} options.cell - Current datagrid cell
+     * @param {Backgrid.Column} options.column - Current datagrid column
+     * @param {string} options.placeholder - Placeholder translation key for an empty element
+     * @param {string} options.placeholder_raw - Raw placeholder value. It overrides placeholder translation key
+     * @param {string} options.maximumSelectionLength - Maximum selection length
+     * @param {Object} options.validationRules - Validation rules. See [documentation here](https://goo.gl/j9dj4Y)
+     *
+     * @augments [SelectEditorView](./select-editor-view.md)
+     * @exports MultiSelectEditorView
+     */
 
     var MultiCheckboxEditorView;
     var SelectEditorView = require('./select-editor-view');
@@ -8,13 +54,16 @@ define(function(require) {
     require('jquery.multiselect');
     require('jquery.multiselect.filter');
 
-    MultiCheckboxEditorView = SelectEditorView.extend(/** @exports MultiSelectEditorView.prototype */{
+    MultiCheckboxEditorView = SelectEditorView.extend(/** @exports MultiCheckboxEditorView.prototype */{
         className: 'multi-checkbox-editor',
-        contextSearch: true,
         template: require('tpl!orodatagrid/templates/multi-checkbox-editor.html'),
-        inputSelector: 'select',
+
+        /**
+         * Jquery object that wraps select DOM element with initialized multiselect plugin
+         *
+         * @property
+         */
         multiselect: null,
-        containerSelector: '.inline-editor-wrapper',
 
         events: {
             'change select': 'onChange',
@@ -27,7 +76,7 @@ define(function(require) {
         },
 
         onShow: function() {
-            this.multiselect = this.$(this.inputSelector).multiselect({
+            this.multiselect = this.$('select').multiselect({
                 autoOpen: true,
                 classes: this.className,
                 header: '',
@@ -49,7 +98,7 @@ define(function(require) {
 
         onUpdatePosition: function() {
             if (this.multiselect) {
-                this.$(this.inputSelector).multiselect('updatePos');
+                this.multiselect.multiselect('updatePos');
             }
         },
 
@@ -76,7 +125,7 @@ define(function(require) {
             var data = MultiCheckboxEditorView.__super__.getTemplateData.call(this);
             _.extend(data, {
                 options: this.availableChoices,
-                selected: this.getValue()
+                selected: this.getModelValue()
             });
             return data;
         },
@@ -91,7 +140,13 @@ define(function(require) {
         },
 
         dispose: function() {
-            this.$(this.inputSelector).multiselect('destroy');
+            if (this.disposed) {
+                return;
+            }
+            if (this.multiselect) {
+                this.multiselect.multiselect('destroy');
+                this.multiselect = null;
+            }
             MultiCheckboxEditorView.__super__.dispose.call(this);
         }
     });
