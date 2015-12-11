ccd30236a7ff40e21d1158a45e5e85560694a7b6
Denis Voronin <dvoronin@magecore.com>
2015-12-11 20:06:04 +0200

    BAP-9504: Fatal error on trying to search anything exists  - Small refactoring of tag datagrid extension

---
+++ src/Oro/Bundle/TagBundle/Grid/AbstractTagsExtension.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace Oro\Bundle\TagBundle\Grid;
+
+use Oro\Bundle\DataGridBundle\Datagrid\Common\DatagridConfiguration;
+use Oro\Bundle\DataGridBundle\Datagrid\Common\ResultsObject;
+use Oro\Bundle\DataGridBundle\Datasource\ResultRecord;
+use Oro\Bundle\DataGridBundle\Extension\AbstractExtension;
+use Oro\Bundle\EntityBundle\ORM\EntityClassResolver;
+use Oro\Bundle\TagBundle\Entity\TagManager;
+use Oro\Bundle\TagBundle\Provider\TagVirtualFieldProvider;
+
+abstract class AbstractTagsExtension extends AbstractExtension
+{
+    const COLUMN_NAME = 'tags';
+
+    const GRID_EXTEND_ENTITY_PATH = '[extended_entity_name]';
+    const GRID_FROM_PATH          = '[source][query][from]';
+    const GRID_COLUMN_ALIAS_PATH  = '[source][query_config][column_aliases]';
+    const GRID_FILTERS_PATH       = '[filters][columns]';
+    const GRID_SORTERS_PATH       = '[sorters][columns]';
+    const GRID_NAME_PATH          = 'name';
+    const FILTER_COLUMN_NAME      = 'tagname';
+    const PROPERTY_ID_PATH        = '[properties][id]';
+
+    /** @var TagManager */
+    protected $tagManager;
+
+    /** @var EntityClassResolver */
+    protected $entityClassResolver;
+
+    /**
+     * @param TagManager          $tagManager
+     * @param EntityClassResolver $entityClassResolver
+     */
+    public function __construct(TagManager $tagManager, EntityClassResolver $entityClassResolver)
+    {
+        $this->tagManager          = $tagManager;
+        $this->entityClassResolver = $entityClassResolver;
+    }
+
+    /**
+     * Checks if configuration is for report or segment grid
+     *
+     * @param DatagridConfiguration $config
+     *
+     * @return bool
+     */
+    protected function isReportOrSegmentGrid(DatagridConfiguration $config)
+    {
+        $gridName = $config->offsetGetByPath(self::GRID_NAME_PATH);
+
+        return
+            strpos($gridName, 'oro_report') === 0 ||
+            strpos($gridName, 'oro_segment') === 0;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function visitResult(DatagridConfiguration $config, ResultsObject $result)
+    {
+        $aliases    = $config->offsetGetByPath(self::GRID_COLUMN_ALIAS_PATH);
+        $identifier = isset($aliases[TagVirtualFieldProvider::TAG_FIELD])
+            ? $aliases[TagVirtualFieldProvider::TAG_FIELD]
+            : 'id';
+
+        $rows = (array)$result->offsetGetOr('data', []);
+        $ids  = array_map(
+            function (ResultRecord $item) use ($identifier) {
+                return $item->getValue($identifier);
+            },
+            $rows
+        );
+
+        $tags = array_reduce(
+            $this->tagManager->getTagsByEntityIds($this->getEntityClassName($config), $ids),
+            function ($entitiesTags, $item) {
+                $entitiesTags[$item['entityId']][] = $item;
+
+                return $entitiesTags;
+            },
+            []
+        );
+
+        $result->offsetSet(
+            'data',
+            array_map(
+                function (ResultRecord $item) use ($tags, $identifier) {
+                    $id   = $item->getValue($identifier);
+                    $data = isset($tags[$id]) ? $tags[$id] : [];
+                    $item->addData(['tags' => $data]);
+
+                    return $item;
+                },
+                $rows
+            )
+        );
+    }
+
+    /**
+     * @param DatagridConfiguration $config
+     *
+     * @return string|null
+     */
+    protected function getEntityClassName(DatagridConfiguration $config)
+    {
+        $entityClassName = $config->offsetGetByPath(self::GRID_EXTEND_ENTITY_PATH);
+        if (!$entityClassName) {
+            $from = $config->offsetGetByPath(self::GRID_FROM_PATH);
+            if (!$from) {
+                return null;
+            }
+
+            $entityClassName = $this->entityClassResolver->getEntityClass($from[0]['table']);
+        }
+
+        return $entityClassName;
+    }
+
+    /**
+     * Get definition for tag column
+     *
+     * @param DatagridConfiguration $config
+     *
+     * @return array
+     */
+    abstract protected function getColumnDefinition(DatagridConfiguration $config);
+
+    /**
+     * Get definition for tag column filter
+     *
+     * @param DatagridConfiguration $config
+     *
+     * @return array
+     */
+    abstract protected function getColumnFilterDefinition(DatagridConfiguration $config);
+}
---
+++ src/Oro/Bundle/TagBundle/Grid/TagsReportExtension.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace Oro\Bundle\TagBundle\Grid;
+
+use Oro\Bundle\DataGridBundle\Datagrid\Common\DatagridConfiguration;
+use Oro\Bundle\DataGridBundle\Datasource\ResultRecordInterface;
+use Oro\Bundle\EntityBundle\ORM\EntityClassResolver;
+use Oro\Bundle\EntityBundle\Tools\EntityRoutingHelper;
+use Oro\Bundle\TagBundle\Entity\TagManager;
+use Oro\Bundle\TagBundle\Helper\TaggableHelper;
+use Oro\Bundle\TagBundle\Provider\TagVirtualFieldProvider;
+
+class TagsReportExtension extends AbstractTagsExtension
+{
+    /** @var TaggableHelper */
+    protected $taggableHelper;
+
+    /** @var EntityRoutingHelper */
+    protected $entityRoutingHelper;
+
+    /**
+     * @param TagManager          $tagManager
+     * @param EntityClassResolver $resolver
+     * @param TaggableHelper      $helper
+     * @param EntityRoutingHelper $entityRoutingHelper
+     */
+    public function __construct(
+        TagManager $tagManager,
+        EntityClassResolver $resolver,
+        TaggableHelper $helper,
+        EntityRoutingHelper $entityRoutingHelper
+    ) {
+        parent::__construct($tagManager, $resolver);
+
+        $this->taggableHelper      = $helper;
+        $this->entityRoutingHelper = $entityRoutingHelper;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isApplicable(DatagridConfiguration $config)
+    {
+        $aliases = $config->offsetGetByPath(self::GRID_COLUMN_ALIAS_PATH);
+
+        return isset($aliases[TagVirtualFieldProvider::TAG_FIELD]) && $this->isReportOrSegmentGrid($config);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function processConfigs(DatagridConfiguration $config)
+    {
+        $filters = $config->offsetGetByPath(self::GRID_FILTERS_PATH, []);
+        $columns = $config->offsetGetByPath('[columns]', []);
+        $sorters = $config->offsetGetByPath(self::GRID_SORTERS_PATH, []);
+
+        $column = [self::COLUMN_NAME => $this->getColumnDefinition($config)];
+        $filter = $this->getColumnFilterDefinition($config);
+
+        $aliases = $config->offsetGetByPath(self::GRID_COLUMN_ALIAS_PATH);
+        // Replace virtual tags filter and virtual tags column with properly configured one.
+        $tagAlias           = $aliases[TagVirtualFieldProvider::TAG_FIELD];
+        $filters[$tagAlias] = $filter;
+        unset($columns[$tagAlias]);
+        unset($sorters[$tagAlias]);
+
+        $config->offsetSetByPath('[columns]', array_merge($columns, $column));
+        $config->offsetSetByPath(self::GRID_FILTERS_PATH, $filters);
+        $config->offsetSetByPath(self::GRID_SORTERS_PATH, $sorters);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getColumnDefinition(DatagridConfiguration $config)
+    {
+        return [
+            'label'         => 'oro.tag.tags_label',
+            'type'          => 'callback',
+            'frontend_type' => 'tags',
+            'callable'      =>
+                function (ResultRecordInterface $record) {
+                    return $record->getValue('tags');
+                },
+            'editable'      => false,
+            'translatable'  => true,
+            'renderable'    => true
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getColumnFilterDefinition(DatagridConfiguration $config)
+    {
+        return [
+            'type'      => 'tag',
+            'data_name' => 'tag.id',
+            'label'     => 'oro.tag.entity_plural_label',
+            'enabled'   => true,
+            'options'   => [
+                'field_options' => [
+                    'entity_class' => $this->getEntityClassName($config),
+                ]
+            ]
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getPriority()
+    {
+        return 10;
+    }
+}
