d097afaf56e3feff41c69589eb481952cbcd87d9
Denis Voronin <dvoronin@magecore.com>
2015-12-17 21:31:01 +0200

    BAP-9112: Add mass delete actions for every entity grid where regular delete action is available

---
+++ src/Oro/Bundle/DataGridBundle/Extension/Action/MassDeleteActionExtension.php
@@ -0,0 +1,162 @@
+<?php
+
+namespace Oro\Bundle\DataGridBundle\Extension\Action;
+
+use Oro\Bundle\DataGridBundle\Datagrid\Common\DatagridConfiguration;
+use Oro\Bundle\DataGridBundle\Extension\AbstractExtension;
+use Oro\Bundle\DataGridBundle\Extension\Action\Actions\ActionInterface;
+use Oro\Bundle\DataGridBundle\Tools\GridConfigurationHelper;
+use Oro\Bundle\EntityBundle\ORM\DoctrineHelper;
+
+class MassDeleteActionExtension extends AbstractExtension
+{
+    const ACTION_KEY         = 'actions';
+    const MASS_ACTION_KEY    = 'mass_actions';
+    const ACTION_TYPE_KEY    = 'type';
+    const ACTION_TYPE_DELETE = 'delete';
+
+    /** @var DoctrineHelper */
+    protected $doctrineHelper;
+
+    /** @var GridConfigurationHelper */
+    protected $gridConfigurationHelper;
+
+    /** @var array */
+    protected $actions = [];
+
+    /** @var string|null */
+    protected $entityClassName;
+
+    /** @var array */
+    protected static $excludeParams = [ActionInterface::ACL_KEY];
+
+    /**
+     * @param DoctrineHelper          $doctrineHelper
+     * @param GridConfigurationHelper $gridConfigurationHelper
+     */
+    public function __construct(DoctrineHelper $doctrineHelper, GridConfigurationHelper $gridConfigurationHelper)
+    {
+        $this->doctrineHelper          = $doctrineHelper;
+        $this->gridConfigurationHelper = $gridConfigurationHelper;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isApplicable(DatagridConfiguration $config)
+    {
+        return
+            // Checks if mass delete action does not exists
+            !$this->isDeleteActionExists($config, static::MASS_ACTION_KEY) &&
+            // Checks if delete action exists
+            $this->isDeleteActionExists($config, static::ACTION_KEY) &&
+            $this->isApplicableForEntity($config);
+    }
+
+    /**
+     * Checks if we can apply mass delete action for the entity:
+     *  - extract entity class
+     *  - extract entity single identifier
+     *  - extract alias for this entity
+     *
+     * @param DatagridConfiguration $config
+     *
+     * @return bool
+     */
+    protected function isApplicableForEntity(DatagridConfiguration $config)
+    {
+        $entity = $this->getEntity($config);
+
+        return
+            $entity &&
+            $this->doctrineHelper->getSingleEntityIdentifierFieldName($entity, false) &&
+            $this->gridConfigurationHelper->getEntityRootAlias($config);
+    }
+
+    /**
+     * @param DatagridConfiguration $config
+     * @param string                $key
+     *
+     * @return bool
+     */
+    protected function isDeleteActionExists(DatagridConfiguration $config, $key)
+    {
+        $actions = $config->offsetGetOr($key, []);
+        foreach ($actions as $action) {
+            if ($action[static::ACTION_TYPE_KEY] == static::ACTION_TYPE_DELETE) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function processConfigs(DatagridConfiguration $config)
+    {
+        $actions = $config->offsetGetOr(static::MASS_ACTION_KEY, []);
+
+        $actions[] = $this->getMassDeleteActionConfig($config);
+
+        $config->offsetSet(static::MASS_ACTION_KEY, $actions);
+    }
+
+    /**
+     * @param DatagridConfiguration $config
+     *
+     * @return array
+     */
+    protected function getMassDeleteActionConfig(DatagridConfiguration $config)
+    {
+        $entity = $this->getEntity($config);
+
+        return [
+            'type'            => 'delete',
+            'icon'            => 'trash',
+            'label'           => 'oro.grid.action.delete',
+            'entity_name'     => $entity,
+            'data_identifier' => $this->getDataIdentifier($config),
+            'acl_resource'    => sprintf('DELETE;entity:%s', $entity),
+        ];
+    }
+
+    /**
+     * @param DatagridConfiguration $config
+     *
+     * @return string|null
+     */
+    protected function getEntity(DatagridConfiguration $config)
+    {
+        if ($this->entityClassName === null) {
+            $this->entityClassName = $this->gridConfigurationHelper->getEntity($config);
+        }
+
+        return $this->entityClassName;
+    }
+
+    /**
+     * @param DatagridConfiguration $config
+     *
+     * @return string
+     */
+    protected function getDataIdentifier(DatagridConfiguration $config)
+    {
+        $entity     = $this->getEntity($config);
+        $identifier = $this->doctrineHelper->getSingleEntityIdentifierFieldName($entity);
+        $rootAlias  = $this->gridConfigurationHelper->getEntityRootAlias($config);
+
+        return sprintf('%s.%s', $rootAlias, $identifier);
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getPriority()
+    {
+        // should be applied before mass action extension
+        return 210;
+    }
+}
