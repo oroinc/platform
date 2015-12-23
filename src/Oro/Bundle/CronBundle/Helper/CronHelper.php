84bd2dc02689860880e65fd163d6c45a1ba14457
Dmytro Krushelnytskyi <dkrushelnytskyi@magecore.com>
2015-12-23 13:06:10 +0200

    BAP-9073: Change cron strategy to prevent overflow jobs queue

--- src/Oro/Bundle/CronBundle/Helper/CronHelper.php
+++ src/Oro/Bundle/CronBundle/Helper/CronHelper.php
@@ -7,14 +7,22 @@ use Cron\CronExpression;
 class CronHelper
 {
     /**
-     * @param $definition
+     * Create a new CronExpression.
      *
-     * @return \Cron\CronExpression
+     * @param string $definition The CRON expression to create.  There are
+     *                           several special predefined values which can be used to substitute the
+     *                           CRON expression:
+     *
+     *      `@yearly`, `@annually` - Run once a year, midnight, Jan. 1 - 0 0 1 1 *
+     *      `@monthly` - Run once a month, midnight, first of month - 0 0 1 * *
+     *      `@weekly` - Run once a week, midnight on Sun - 0 0 * * 0
+     *      `@daily` - Run once a day, midnight - 0 0 * * *
+     *      `@hourly` - Run once an hour, first minute - 0 * * * *
+     *
+     * @return CronExpression
      */
     public function createCron($definition)
     {
-        $cron = CronExpression::factory($definition);
-
-        return $cron;
+        return CronExpression::factory($definition);
     }
 }
