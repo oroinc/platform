4c2251d1adaa75e52c5c74b16012529282e38c2c
Ievgenii Myshusta <imyshusta@magecore.com>
2015-12-24 17:42:28 +0200

    BAP-9557: In system configuration divide enable Google Single Sign-On and google setting Client Id, Client Secret. Add checkbox enable OAuth2 for IMAP - added checking imap sync

---
+++ src/Oro/Bundle/ImapBundle/Resources/public/js/app/components/google-sync-checkbox-component.js
@@ -0,0 +1,84 @@
+define(function(require) {
+    'use strict';
+
+    var $ = require('jquery');
+    var _ = require('underscore');
+    var BaseComponent = require('oroui/js/app/components/base/component');
+    var GoogleSyncCheckbox;
+
+    GoogleSyncCheckbox = BaseComponent.extend({
+        clientId: null,
+
+        scopes: ['https://www.googleapis.com/auth/gmail.readonly'],
+
+        $errorMessage: null,
+
+        $successMessage: null,
+
+        $googleSyncCheckbox: null,
+
+        $clientIdElement: null,
+
+        initialize: function(options) {
+            require(['//apis.google.com/js/client.js?onload=checkAuth']);
+            this.$errorMessage = this.findControlElement(options._sourceElement, options.errorMessage);
+            this.$successMessage = this.findControlElement(options._sourceElement, options.successMessage);
+            this.$googleSyncCheckbox = this.findControlElement(options._sourceElement, "input[type=checkbox]");
+            this.$clientIdElement = $('input[id*="client_id"]');
+            if (this.$googleSyncCheckbox.length) {
+                this.bindEvents();
+            }
+        },
+        bindEvents: function() {
+            var self = this;
+            this.$googleSyncCheckbox.on('change', function() {
+                if ($(this).is(':checked')) {
+                    gapi.auth.authorize(
+                        {
+                            'client_id': self.$clientIdElement.val(),
+                            'scope': self.scopes.join(' '),
+                            'immediate': true
+                        }, self.checkAuthorization);
+                } else {
+                    self.hideMessages();
+                }
+            });
+        },
+
+        hideMessages: function() {
+            this.$errorMessage.hide();
+            this.$successMessage.hide();
+        },
+
+        showSuccess: function() {
+            this.hideMessages();
+            this.$successMessage.show();
+        },
+
+        showError: function() {
+            this.hideMessages();
+            this.$errorMessage.show();
+        },
+
+        checkAuthorization: function(result) {
+            if (result && !result.error) {
+                this.showSuccess();
+            } else {
+                this.showError();
+            }
+        },
+
+        /**
+         * Looks for the control element relatively from the source element
+         *
+         * @param {jQuery} $sourceElement
+         * @param {string} selector
+         * @returns {jQuery}
+         */
+        findControlElement: function($sourceElement, selector) {
+            return $sourceElement.parent().find(selector);
+        }
+    });
+
+    return GoogleSyncCheckbox;
+});
