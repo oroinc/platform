ec25413d5208e13228b0f94b5ddb395bcad92490
Denis Voronin <dvoronin@magecore.com>
2015-12-25 18:56:37 +0200

    BAP-9114: Demo fixes  - Change confirmation messages.  - Hide "Yes, Delete" confirmation button in case when deleted entities count is 0.

--- src/Oro/Bundle/UIBundle/Resources/public/js/delete-confirmation.js
+++ src/Oro/Bundle/UIBundle/Resources/public/js/delete-confirmation.js
@@ -10,20 +10,54 @@ define(['underscore', 'orotranslation/js/translator', 'oroui/js/modal'
      * @extends oroui.Modal
      */
     return Modal.extend({
+
+        /** @property {String} */
+        template: '\
+    <% if (title) { %>\
+      <div class="modal-header">\
+        <% if (allowCancel) { %>\
+          <a class="close">×</a>\
+        <% } %>\
+        <h3>{{title}}</h3>\
+      </div>\
+    <% } %>\
+    <div class="modal-body">{{content}}</div>\
+    <div class="modal-footer">\
+      <% if (allowCancel) { %>\
+        <% if (cancelText) { %>\
+          <a href="#" class="btn cancel">{{cancelText}}</a>\
+        <% } %>\
+      <% } %>\
+      <% if (allowOk) { %>\
+        <a href="#" class="btn ok btn-primary">{{okText}}</a>\
+      <% } %>\
+    </div>\
+  ',
         /** @property {String} */
         className: 'modal oro-modal-danger',
 
         /** @property {String} */
         okButtonClass: 'btn-danger',
 
+        /** @property {Boolean} */
+        allowOk: true,
+
         /**
          * @param {Object} options
          */
         initialize: function(options) {
+            //Set custom template settings
+            var interpolate = {
+                interpolate: /\{\{(.+?)\}\}/g,
+                evaluate: /<%([\s\S]+?)%>/g
+            };
+
             options = _.extend({
                 title: __('Delete Confirmation'),
                 okText: __('Yes, Delete'),
-                cancelText: __('Cancel')
+                cancelText: __('Cancel'),
+                template: _.template(this.template, interpolate),
+                allowOk: this.allowOk
             }, options);
 
             arguments[0] = options;
