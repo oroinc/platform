0fc4d6af995139f43f007016d70f37dc647398ea
Denis Voronin <dvoronin@magecore.com>
2016-01-06 19:30:50 +0200

    CRM-3685: Can't add context for entity with newly enabled activities  - Define defaults grids

--- src/Oro/Bundle/OrganizationBundle/Entity/BusinessUnit.php
+++ src/Oro/Bundle/OrganizationBundle/Entity/BusinessUnit.php
@@ -43,6 +43,9 @@ use Oro\Bundle\EntityConfigBundle\Metadata\Annotation\ConfigField;
  *          "security"={
  *              "type"="ACL",
  *              "group_name"=""
+ *          },
+ *          "grid"={
+ *              "default"="business-unit-grid"
  *          }
  *      }
  * )
