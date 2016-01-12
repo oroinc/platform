02c503511a1eb61f2abff7c52c39e7e8aecc6ec4
Denis Voronin <dvoronin@magecore.com>
2016-01-12 15:15:28 +0200

    CRM-4822: Can't find tags which begins from special symbols

--- src/Oro/Bundle/TagBundle/Autocomplete/SearchHandler.php
+++ src/Oro/Bundle/TagBundle/Autocomplete/SearchHandler.php
@@ -3,9 +3,25 @@
 namespace Oro\Bundle\TagBundle\Autocomplete;
 
 use Oro\Bundle\FormBundle\Autocomplete\SearchHandler as BaseSearchHandler;
+use Oro\Bundle\SecurityBundle\SecurityFacade;
 
 class SearchHandler extends BaseSearchHandler
 {
+    /** @var SecurityFacade */
+    protected $securityFacade;
+
+    /**
+     * @param string         $entityName
+     * @param array          $properties
+     * @param SecurityFacade $securityFacade
+     */
+    public function __construct($entityName, array $properties, SecurityFacade $securityFacade)
+    {
+        parent::__construct($entityName, $properties);
+
+        $this->securityFacade = $securityFacade;
+    }
+
     /**
      * @param string $search
      * @param int    $firstResult
@@ -20,10 +36,16 @@ class SearchHandler extends BaseSearchHandler
         // http://dev.mysql.com/doc/refman/5.0/en/server-system-variables.html#sysvar_ft_min_word_len
         // http://dev.mysql.com/doc/refman/5.0/en/fulltext-stopwords.html
         // http://www.postgresql.org/docs/9.1/static/textsearch-dictionaries.html
-        if (empty($ids)) {
-            $object = $this->entityRepository->findOneBy(['name' => $search]);
-            if ($object !== null) {
-                $ids[] = $object->getId();
+        $object = $this->entityRepository->findOneBy(
+            [
+                'name'         => $search,
+                'organization' => $this->securityFacade->getOrganization()
+            ]
+        );
+        if ($object !== null) {
+            $id = $object->getId();
+            if (!in_array($id, $ids, true)) {
+                $ids[] = $id;
             }
         }
 
