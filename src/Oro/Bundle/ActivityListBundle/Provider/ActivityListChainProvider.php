df83d339a8dff5553116b1a99d4b2d0da3ba51dc
Vova Soroka <vsoroka@magecore.com>
2016-01-14 19:33:38 +0200

    BB-1944: No Activity tab on the entity editing page. Refactor ActivityListChainProvider::isApplicableTarget. Revert invalid comment

--- src/Oro/Bundle/ActivityListBundle/Provider/ActivityListChainProvider.php
+++ src/Oro/Bundle/ActivityListBundle/Provider/ActivityListChainProvider.php
@@ -389,8 +389,6 @@ class ActivityListChainProvider
      * @param string $className
      *
      * @return ActivityListProviderInterface
-     *
-     * @deprecated since 1.9.
      */
     public function getProviderByClass($className)
     {
