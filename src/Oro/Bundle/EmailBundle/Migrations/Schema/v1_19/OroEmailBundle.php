f7f4b7e7a850624b06653e72831b7a70a19ebb54
Yurii Muratov <yurio@ua.fm>
2016-01-27 19:44:17 +0200

    CRM-4904: Refactor email body sync  - Update migrations

--- src/Oro/Bundle/EmailBundle/Migrations/Schema/v1_19/OroEmailBundle.php
+++ src/Oro/Bundle/EmailBundle/Migrations/Schema/v1_19/OroEmailBundle.php
@@ -6,7 +6,6 @@ use Doctrine\DBAL\Schema\Schema;
 
 use Oro\Bundle\MigrationBundle\Migration\Migration;
 use Oro\Bundle\MigrationBundle\Migration\QueryBag;
-use Oro\Bundle\MigrationBundle\Migration\SqlMigrationQuery;
 
 class OroEmailBundle implements Migration
 {
@@ -15,32 +14,15 @@ class OroEmailBundle implements Migration
      */
     public function up(Schema $schema, QueryBag $queries)
     {
-        self::oroEmailTable($schema);
-        $this->deleteBodySyncProcess($queries);
+        static::oroEmailUserTable($schema);
     }
 
     /**
      * @param Schema $schema
      */
-    public static function oroEmailTable(Schema $schema)
+    public static function oroEmailUserTable(Schema $schema)
     {
-        $table = $schema->getTable('oro_email');
-        $table->addColumn('body_synced', 'boolean', ['notnull' => false, 'default' => false]);
-    }
-
-    /**
-     * Delete sync_email_body_after_email_synchronize process definition
-     *
-     * @param QueryBag $queries
-     */
-    protected function deleteBodySyncProcess(QueryBag $queries)
-    {
-        $sql = <<<SQL
-    DELETE
-    FROM
-      oro_process_definition
-    WHERE name = 'sync_email_body_after_email_synchronize'
-SQL;
-        $queries->addPostQuery(new SqlMigrationQuery($sql));
+        $table = $schema->getTable('oro_email_user');
+        $table->addColumn('unsyncedFlagCount', 'integer', ['default' => '0']);
     }
 }
---
+++ src/Oro/Bundle/EmailBundle/Migrations/Schema/v1_20/OroEmailBundle.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Oro\Bundle\EmailBundle\Migrations\Schema\v1_20;
+
+use Doctrine\DBAL\Schema\Schema;
+
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+use Oro\Bundle\MigrationBundle\Migration\SqlMigrationQuery;
+
+class OroEmailBundle implements Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        self::oroEmailTable($schema);
+        $this->deleteBodySyncProcess($queries);
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public static function oroEmailTable(Schema $schema)
+    {
+        $table = $schema->getTable('oro_email');
+        if (!$table->hasColumn('body_synced')) {
+            $table->addColumn('body_synced', 'boolean', ['notnull' => false, 'default' => false]);
+        }
+    }
+
+    /**
+     * Delete sync_email_body_after_email_synchronize process definition
+     *
+     * @param QueryBag $queries
+     */
+    protected function deleteBodySyncProcess(QueryBag $queries)
+    {
+        $sql = <<<SQL
+    DELETE
+    FROM
+      oro_process_definition
+    WHERE name = 'sync_email_body_after_email_synchronize'
+SQL;
+        $queries->addPostQuery(new SqlMigrationQuery($sql));
+    }
+}
