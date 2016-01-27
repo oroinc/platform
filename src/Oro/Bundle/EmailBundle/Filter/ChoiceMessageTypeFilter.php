e255052d1a2792f7efa6ed797abf5a672a7daedd
Miloslav Nenadal <miloslav.nenadal@imatic.cz>
2016-01-27 14:21:58 +0100

    CRM-4746: [Hours] Wrong icons for Emails on My Emails grid for Sent mail
    - fix message type filter

--- src/Oro/Bundle/EmailBundle/Filter/ChoiceMessageTypeFilter.php
+++ src/Oro/Bundle/EmailBundle/Filter/ChoiceMessageTypeFilter.php
@@ -2,9 +2,11 @@
 
 namespace Oro\Bundle\EmailBundle\Filter;
 
-use Oro\Bundle\FilterBundle\Filter\ChoiceFilter;
+use Doctrine\ORM\QueryBuilder;
 
+use Oro\Bundle\FilterBundle\Filter\ChoiceFilter;
 use Oro\Bundle\FilterBundle\Datasource\FilterDatasourceAdapterInterface;
+use Oro\Bundle\FilterBundle\Datasource\Orm\OrmFilterDatasourceAdapter;
 use Oro\Bundle\EmailBundle\Model\FolderType;
 
 class ChoiceMessageTypeFilter extends ChoiceFilter
@@ -21,14 +23,68 @@ class ChoiceMessageTypeFilter extends ChoiceFilter
 
         if (in_array(FolderType::INBOX, $data['value']) && in_array(FolderType::SENT, $data['value'])) {
             $data['value'] = [];
-        } elseif (in_array(FolderType::INBOX, $data['value'])) {
-            $data['value'][] = FolderType::SPAM;
-            $data['value'][] = FolderType::OTHER;
-            $data['value'][] = FolderType::TRASH;
-        } elseif (in_array(FolderType::SENT, $data['value'])) {
-            $data['value'][] = FolderType::DRAFTS;
+            return parent::apply($ds, $data);
+        } elseif (!in_array(FolderType::INBOX, $data['value']) && !in_array(FolderType::SENT, $data['value'])) {
+            return parent::apply($ds, $data);
         }
 
-        return parent::apply($ds, $data);
+        if (!$ds instanceof OrmFilterDatasourceAdapter) {
+            return false;
+        }
+
+        $qb = $ds->getQueryBuilder();
+        if (in_array(FolderType::INBOX, $data['value'])) {
+            $this->applyInboxFilter($qb);
+        } else {
+            $this->applySentFilter($qb);
+        }
+
+        return true;
+    }
+
+    /**
+     * @param QueryBuilder $qb
+     */
+    protected function applyInboxFilter(QueryBuilder $qb)
+    {
+        $qb
+            ->leftJoin('e.fromEmailAddress', '_fea')
+            ->leftJoin('_fea.owner1', '_fo')
+            ->leftJoin('eu.owner', '_eo')
+            ->andWhere(
+                $qb->expr()->orX(
+                    $qb->expr()->in('f.type', ':incoming_types'),
+                    $qb->expr()->andX(
+                        $qb->expr()->notIn('f.type', ':outcoming_types'),
+                        $qb->expr()->isNotNull('_eo.id'),
+                        $qb->expr()->neq('_fo.id', '_eo.id')
+                    )
+                )
+            )
+            ->setParameter('outcoming_types', FolderType::outcomingTypes())
+            ->setParameter('incoming_types', FolderType::incomingTypes());
+    }
+
+    /**
+     * @param QueryBuilder $qb
+     */
+    protected function applySentFilter(QueryBuilder $qb)
+    {
+        $qb
+            ->leftJoin('e.fromEmailAddress', '_fea')
+            ->leftJoin('_fea.owner1', '_fo')
+            ->leftJoin('eu.owner', '_eo')
+            ->andWhere(
+                $qb->expr()->orX(
+                    $qb->expr()->in('f.type', ':outcoming_types'),
+                    $qb->expr()->andX(
+                        $qb->expr()->notIn('f.type', ':incoming_types'),
+                        $qb->expr()->isNotNull('_eo.id'),
+                        $qb->expr()->eq('_fo.id', '_eo.id')
+                    )
+                )
+            )
+            ->setParameter('outcoming_types', FolderType::outcomingTypes())
+            ->setParameter('incoming_types', FolderType::incomingTypes());
     }
 }
