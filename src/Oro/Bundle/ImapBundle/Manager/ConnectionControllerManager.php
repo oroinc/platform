9ca94663a5ff7f1b8e671289de1b41fea0e51d23
Ignat Shcheglovskyi <ignat@orocrm.com>
2016-01-29 14:33:44 -0800

    AEIV-477: IMAP Sync - Google - Insecure Apps - inject parameters from DIC to ConnectionControllerManager to be able to override used form name and form type in customization

--- src/Oro/Bundle/ImapBundle/Manager/ConnectionControllerManager.php
+++ src/Oro/Bundle/ImapBundle/Manager/ConnectionControllerManager.php
@@ -2,7 +2,8 @@
 
 namespace Oro\Bundle\ImapBundle\Manager;
 
-use Doctrine\Bundle\DoctrineBundle\Registry;
+use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\ORM\EntityManager;
 
 use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\Form\FormFactory;
@@ -24,12 +25,6 @@ use Oro\Bundle\UserBundle\Entity\User;
  */
 class ConnectionControllerManager
 {
-    /**
-     * @deprecated Use parameter oro_imap.user_form_type instead
-     */
-    const ORO_USER_USER_FORM = 'oro_user_user_form';
-    const ORO_EMAIL_MAILBOX = 'oro_email_mailbox';
-
     /** @var FormInterface */
     protected $formUser;
 
@@ -39,37 +34,49 @@ class ConnectionControllerManager
     /** @var Mcrypt */
     protected $mcrypt;
 
-    /** @var Registry */
+    /** @var ManagerRegistry */
     protected $doctrine;
 
     /** @var ImapConnectorFactory */
     protected $imapConnectorFactory;
 
     /** @var string */
-    protected $userFormType;
+    protected $userFormName;
+
+    /** @var string */
+    protected $emailMailboxFormName;
+
+    /** @var string */
+    protected $emailMailboxFormType;
 
     /**
      * @param FormInterface $formUser
      * @param FormFactory $formFactory
      * @param Mcrypt $mcrypt
-     * @param Registry $doctrineHelper
+     * @param ManagerRegistry $doctrineHelper
      * @param ImapConnectorFactory $imapConnectorFactory
-     * @param string $userFormType
+     * @param string $userFormName
+     * @param string $emailMailboxFormName
+     * @param string $emailMailboxFormType
      */
     public function __construct(
         FormInterface $formUser,
         FormFactory $formFactory,
         Mcrypt $mcrypt,
-        Registry $doctrineHelper,
+        ManagerRegistry $doctrineHelper,
         ImapConnectorFactory $imapConnectorFactory,
-        $userFormType = 'oro_user_user_form'
+        $userFormName,
+        $emailMailboxFormName,
+        $emailMailboxFormType
     ) {
         $this->formUser = $formUser;
         $this->formFactory = $formFactory;
         $this->mcrypt = $mcrypt;
         $this->doctrine = $doctrineHelper;
         $this->imapConnectorFactory = $imapConnectorFactory;
-        $this->userFormType = $userFormType;
+        $this->userFormName = $userFormName;
+        $this->emailMailboxFormName = $emailMailboxFormName;
+        $this->emailMailboxFormType = $emailMailboxFormType;
     }
 
     /**
@@ -101,11 +108,9 @@ class ConnectionControllerManager
         );
 
         $connector = $this->imapConnectorFactory->createImapConnector($config);
-        $manager = new ImapEmailFolderManager(
-            $connector,
-            $this->doctrine->getManager(),
-            $origin
-        );
+        /** @var EntityManager $entityManager */
+        $entityManager = $this->doctrine->getManager();
+        $manager = new ImapEmailFolderManager($connector, $entityManager, $origin);
 
         $emailFolders = $manager->getFolders();
         $origin->setFolders($emailFolders);
@@ -145,15 +150,20 @@ class ConnectionControllerManager
     protected function prepareForm($formParentName, $accountTypeModel)
     {
         $form = null;
-        if ($formParentName === $this->userFormType) {
+        if ($formParentName === $this->userFormName) {
             $data = $user = new User();
             $data->setImapAccountType($accountTypeModel);
             $this->formUser->setData($data);
             $form = $this->formUser;
-        } elseif ($formParentName === self::ORO_EMAIL_MAILBOX) {
+        } elseif ($formParentName === $this->emailMailboxFormName) {
             $data = $user = new Mailbox();
             $data->setImapAccountType($accountTypeModel);
-            $form = $this->formFactory->create(self::ORO_EMAIL_MAILBOX, null, ['csrf_protection' => false]);
+            $form = $this->formFactory->createNamed(
+                $this->emailMailboxFormName,
+                $this->emailMailboxFormType,
+                null,
+                ['csrf_protection' => false]
+            );
             $form->setData($data);
         }
 
