56a6ee299a3eb89a1709677b44049b0ac1cb8cf6
Yauheni Kasenka <beta.screech@gmail.com>
2016-01-29 21:31:54 +0300

    BB-2004: Assigned To (Quotes, RFQs)

---
+++ src/Oro/Bundle/UserBundle/Form/Type/UserCollectionType.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Oro\Bundle\UserBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserCollectionType extends AbstractType
+{
+    const NAME = 'oro_user_collection';
+
+    /** @var string */
+    protected $dataClass;
+
+    /**
+     * @param string $dataClass
+     */
+    public function setDataClass($dataClass)
+    {
+        $this->dataClass = $dataClass;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return 'genemu_jqueryselect2_entity';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(
+            [
+                'class' => $this->dataClass,
+                'property' => 'fullName',
+                'multiple' => true,
+                'required' => false,
+            ]
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return self::NAME;
+    }
+}
---
+++ src/Oro/Bundle/UserBundle/Tests/Unit/Form/Type/UserCollectionTypeTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Oro\Bundle\UserBundle\Tests\Unit\Form\Type;
+
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Oro\Bundle\UserBundle\Form\Type\UserCollectionType;
+
+class UserCollectionTypeTest extends \PHPUnit_Framework_TestCase
+{
+    const CLASS_USER_ENTITY = 'Oro\Bundle\UserBundle\Entity\User';
+
+    /** @var UserCollectionType */
+    protected $formType;
+
+    protected function setUp()
+    {
+        $this->formType = new UserCollectionType();
+        $this->formType->setDataClass(self::CLASS_USER_ENTITY);
+    }
+
+    protected function tearDown()
+    {
+        unset($this->formType);
+    }
+
+    public function testSetDefaultOptions()
+    {
+        /* @var $resolver \PHPUnit_Framework_MockObject_MockObject|OptionsResolver */
+        $resolver = $this->getMock('Symfony\Component\OptionsResolver\OptionsResolver');
+        $resolver->expects($this->once())
+            ->method('setDefaults')
+            ->with(
+                [
+                    'required' => false,
+                    'class'  => self::CLASS_USER_ENTITY,
+                    'property' => 'fullName',
+                    'multiple' => true,
+                ]
+            );
+
+        $this->formType->setDefaultOptions($resolver);
+    }
+
+    public function testGetParent()
+    {
+        $this->assertEquals('genemu_jqueryselect2_entity', $this->formType->getParent());
+    }
+
+    public function testGetName()
+    {
+        $this->assertEquals(UserCollectionType::NAME, $this->formType->getName());
+    }
+}
