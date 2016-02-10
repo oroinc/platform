8a37299029a4260e03384fa1dbefd4bef7f2732b
Craig Menning <cmenning@novatexsolutions.com>
2016-02-10 11:54:11 -0600

    Remove pageSize strict type checking in DataGridBundle
    Non-default configurations for are provided as strings, and therefore fail the strict comparison. This leaves no elements in the currentSizeLabel array, and causes an exception that hangs the data grid display.

--- src/Oro/Bundle/DataGridBundle/Resources/public/js/datagrid/page-size.js
+++ src/Oro/Bundle/DataGridBundle/Resources/public/js/datagrid/page-size.js
@@ -110,7 +110,7 @@ define([
                 _.bind(
                     function(item) {
                         return item.size === undefined ?
-                            this.collection.state.pageSize === item : this.collection.state.pageSize === item.size;
+                            this.collection.state.pageSize == item : this.collection.state.pageSize == item.size;
                     },
                     this
                 )
