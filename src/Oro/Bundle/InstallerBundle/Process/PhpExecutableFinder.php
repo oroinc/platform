11056f6fa64050bb97c9abcca17ceca6cdf0cb90
Sergey Zhuravel <zhuravel.sergey@gmail.com>
2016-02-15 13:02:03 +0200

    CRM-4214: MailChimp performance investigation - set oro finder deprecated, use symfony executable finder instead

---
+++ src/Oro/Bundle/InstallerBundle/Process/PhpExecutableFinder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Oro\Bundle\InstallerBundle\Process;
+
+use Symfony\Component\Process\PhpExecutableFinder as BasePhpExecutableFinder;
+
+/**
+ * @deprecated since 1.9
+ */
+class PhpExecutableFinder
+{
+    /** @var BasePhpExecutableFinder */
+    protected $finder;
+
+    public function __construct()
+    {
+        $this->finder = new BasePhpExecutableFinder();
+    }
+
+    /**
+     * @see \Symfony\Component\Process\PhpExecutableFinder::find
+     */
+    public function find($includeArgs = true)
+    {
+        $php = getenv('ORO_PHP_PATH');
+        if ($php && is_executable($php)) {
+            return $php;
+        }
+
+        return $this->finder->find($includeArgs);
+    }
+}
