013c7c645a3e31e727663e87e7f4d5be971cb9d0
Miloslav Nenadal <miloslav.nenadal@imatic.cz>
2016-02-17 10:59:34 +0100

    BAP-9777: Fix frontend
    - fix validation of datetime field

--- src/Oro/Bundle/FilterBundle/Resources/public/js/app/views/datepicker/variable-datetimepicker-view.js
+++ src/Oro/Bundle/FilterBundle/Resources/public/js/app/views/datepicker/variable-datetimepicker-view.js
@@ -7,11 +7,53 @@ define(function(require) {
     var dateTimePickerViewMixin = require('oroui/js/app/views/datepicker/datetimepicker-view-mixin');
     var moment = require('moment');
 
+    function isBetween(value, min, max) {
+        value = parseInt(value);
+
+        return !_.isNaN(value) && value >= min && value <= max;
+    }
+
     VariableDateTimePickerView = VariableDatePickerView.extend(_.extend({}, dateTimePickerViewMixin, {
         /**
          * Default options
          */
         defaults: _.extend({}, VariableDatePickerView.prototype.defaults, dateTimePickerViewMixin.defaults),
+
+        partsValidation: {
+            value: function (date, time) {
+                return this.dateVariableHelper.isDateVariable(date)
+                    || (moment(date, this.getDateFormat(), true).isValid()
+                        && moment(time, this.getTimeFormat(), true).isValid());
+            },
+            dayofweek: function(date, time) {
+                return this.dateVariableHelper.isDateVariable(date)
+                    || isBetween(date, 1, 7);
+            },
+            week: function(date, time) {
+                return this.dateVariableHelper.isDateVariable(date)
+                    || isBetween(date, 1, 53);
+            },
+            day: function(date, time) {
+                return this.dateVariableHelper.isDateVariable(date)
+                    || isBetween(date, 1, 31);
+            },
+            month: function(date, time) {
+                return this.dateVariableHelper.isDateVariable(date)
+                    || isBetween(date, 1, 12);
+            },
+            quarter: function(date, time) {
+                return this.dateVariableHelper.isDateVariable(date)
+                    || isBetween(date, 1, 4);
+            },
+            dayofyear: function(date, time) {
+                return this.dateVariableHelper.isDateVariable(date)
+                    || isBetween(date, 1, 365);
+            },
+            year: function(date, time) {
+                return this.dateVariableHelper.isDateVariable(date)
+                    || !_.isNaN(parseInt(date));
+            }
+        },
 
         /**
          * Returns supper prototype for datetime picker view mixin
@@ -49,14 +91,21 @@ define(function(require) {
 
             var date = this.$frontDateField.val();
             var time = this.$frontTimeField.val();
-            var isVariable = this.dateVariableHelper.isDateVariable(date);
-            var isValidDate = moment(date, this.getDateFormat(), true).isValid();
-            var isValidTime = moment(time, this.getTimeFormat(), true).isValid();
 
-            if (!target && !isVariable && (!isValidDate || !isValidTime)) {
+            if (!target && !this._isValid(date, time)) {
                 this.$frontDateField.val('');
                 this.$frontTimeField.val('');
             }
+        },
+
+        _isValid: function(date, time) {
+            var part = this.$variables.dateVariables('getPart');
+            var validator = this.partsValidation[part];
+            if (!validator) {
+                return false;
+            }
+
+            return validator.call(this, date, time);
         },
 
         /**
