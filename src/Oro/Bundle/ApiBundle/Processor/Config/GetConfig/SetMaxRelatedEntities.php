8915005503db612ffa9c186e30ea3fc400b2a36c
Vova Soroka <vsoroka@magecore.com>
2016-02-23 08:52:38 +0200

    BAP-9376: Create unit tests for ApiBundle. Unit tests for SetMaxRelatedEntities

--- src/Oro/Bundle/ApiBundle/Processor/Config/GetConfig/SetMaxRelatedEntities.php
+++ src/Oro/Bundle/ApiBundle/Processor/Config/GetConfig/SetMaxRelatedEntities.php
@@ -53,29 +53,26 @@ class SetMaxRelatedEntities implements ProcessorInterface
             return;
         }
 
-        $this->setLimits($definition, $entityClass, $maxRelatedEntities);
+        $this->setLimits(
+            $definition,
+            $this->doctrineHelper->getEntityMetadataForClass($entityClass),
+            $maxRelatedEntities
+        );
     }
 
     /**
      * @param EntityDefinitionConfig $definition
-     * @param string                 $entityClass
+     * @param ClassMetadata          $metadata
      * @param int                    $limit
      */
-    protected function setLimits(EntityDefinitionConfig $definition, $entityClass, $limit)
+    protected function setLimits(EntityDefinitionConfig $definition, ClassMetadata $metadata, $limit)
     {
-        $metadata = $this->doctrineHelper->getEntityMetadataForClass($entityClass);
-        $fields   = $definition->getFields();
+        $fields = $definition->getFields();
         foreach ($fields as $fieldName => $field) {
             $propertyPath = $field->getPropertyPath() ?: $fieldName;
             $path         = ConfigUtil::explodePropertyPath($propertyPath);
             if (count($path) === 1) {
                 $this->setFieldLimit($field, $metadata, $propertyPath, $limit);
-            } else {
-                $linkedField    = array_pop($path);
-                $linkedMetadata = $this->doctrineHelper->findEntityMetadataByPath($entityClass, $path);
-                if (null !== $linkedMetadata) {
-                    $this->setFieldLimit($field, $linkedMetadata, $linkedField, $limit);
-                }
             }
         }
     }
@@ -100,7 +97,10 @@ class SetMaxRelatedEntities implements ProcessorInterface
                 }
             }
             if ($field->hasTargetEntity()) {
-                $this->setLimits($field->getTargetEntity(), $metadata->name, $limit);
+                $linkedMetadata = $this->doctrineHelper->getEntityMetadataForClass(
+                    $metadata->getAssociationTargetClass($fieldName)
+                );
+                $this->setLimits($field->getTargetEntity(), $linkedMetadata, $limit);
             }
         }
     }
