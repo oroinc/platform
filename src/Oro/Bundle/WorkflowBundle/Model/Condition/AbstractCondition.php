d7562f676a742deb4afbe6eab727ad7804c430f3
Ivan Shirko <ivan.shirko@effective-soft.com>
2016-02-25 18:02:44 +0300

    BB-2152: Move ActionsBundle to platform - fix copy_tagging action

--- src/Oro/Bundle/WorkflowBundle/Model/Condition/AbstractCondition.php
+++ src/Oro/Bundle/WorkflowBundle/Model/Condition/AbstractCondition.php
@@ -2,10 +2,10 @@
 
 namespace Oro\Bundle\WorkflowBundle\Model\Condition;
 
-use Oro\Component\ConfigExpression\Condition\AbstractCondition as BaseAbstractCondition;
+use Oro\Component\ConfigExpression\Condition\AbstractConfigurableCondition as BaseAbstractCondition;
 
 /**
- * @deprecated since 1.10. Use {@see Oro\Component\ConfigExpression\Condition\AbstractCondition} instead
+ * @deprecated since 1.10. Use {@see Oro\Component\ConfigExpression\Condition\AbstractConfigurableCondition} instead
  */
 abstract class AbstractCondition extends BaseAbstractCondition
 {
