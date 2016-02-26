6a1afbe28715faac1b2e024669f9d461a2f551b3
Ivan Shirko <ivan.shirko@effective-soft.com>
2016-02-26 15:13:46 +0300

    BB-2152: Move ActionsBundle to platform - update UPGRADE.md

--- src/Oro/Bundle/WorkflowBundle/Model/AbstractStorage.php
+++ src/Oro/Bundle/WorkflowBundle/Model/AbstractStorage.php
@@ -5,7 +5,7 @@ namespace Oro\Bundle\WorkflowBundle\Model;
 use Oro\Component\Action\Model\AbstractStorage as BaseAbstractStorage;
 
 /**
- * @deprecated since 1.10. Use {@see Oro\Component\ConfigExpression\Storage} instead
+ * @deprecated since 1.10. Use {@see Oro\Component\Action\Model\AbstractStorage} instead
  */
 abstract class AbstractStorage extends BaseAbstractStorage
 {
