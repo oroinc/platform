587586f274d224bc28878d03a4a315764b8cd4e0
Sergey Zhuravel <szhuravel@magecore.com>
2016-02-29 17:18:18 +0200

    BB-1544: Add schema and mapping test to check database is valid - get rid of schema update in tests

--- src/Oro/Bundle/SearchBundle/Tests/Functional/EventListener/UpdateSchemaListenerTest.php
+++ src/Oro/Bundle/SearchBundle/Tests/Functional/EventListener/UpdateSchemaListenerTest.php
@@ -2,8 +2,14 @@
 
 namespace Oro\Bundle\SearchBundle\Tests\Functional\EventListener;
 
-use Doctrine\DBAL\Connection;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\Console\ConsoleEvents;
+use Symfony\Component\Console\Event\ConsoleCommandEvent;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
 
+use Oro\Bundle\EntityBundle\ORM\DatabaseDriverInterface;
 use Oro\Bundle\TestFrameworkBundle\Test\WebTestCase;
 
 class UpdateSchemaListenerTest extends WebTestCase
@@ -12,56 +18,97 @@ class UpdateSchemaListenerTest extends WebTestCase
     {
         $this->initClient();
 
-        if ($this->getContainer()->getParameter('oro_search.engine') != 'orm') {
+        if ($this->getContainer()->getParameter('oro_search.engine') !== 'orm') {
             $this->markTestSkipped('Should be tested only with ORM search engine');
         }
     }
 
     /**
      * @dataProvider commandDataProvider
+     * @param string $platform
+     * @param string $commandName
+     * @param array $params
+     * @param string $expectedContent
      */
-    public function testCommand($commandName, array $params, $expectedContent, $postgreSQLContent)
+    public function testCommand($platform, $commandName, array $params, $expectedContent)
     {
-        $result = $this->runCommand($commandName, $params);
-
-        /** @var Connection $connection */
-        $connection = $this->getContainer()->get('doctrine')->getConnection();
-        if ($connection->getParams()['driver'] === 'pdo_pgsql') {
-            $expectedContent = $postgreSQLContent;
+        if ($this->getContainer()->getParameter('database_driver') !== $platform) {
+            $this->markTestSkipped(sprintf('TestCase for %s only', $platform));
         }
-        $this->assertContains($expectedContent, $result);
+
+        /** @var KernelInterface $kernel */
+        $kernel = $this->getContainer()->get('kernel');
+        $application = new Application($kernel);
+        $application->setAutoExit(false);
+
+        $command = $application->get($commandName);
+        $bufferedOutput = new BufferedOutput();
+
+        $this->getContainer()->get('event_dispatcher')->dispatch(
+            ConsoleEvents::TERMINATE,
+            new ConsoleCommandEvent($command, new ArrayInput($params), $bufferedOutput)
+        );
+
+        $this->assertContains($expectedContent, $bufferedOutput);
     }
 
+    /**
+     * @return array
+     */
     public function commandDataProvider()
     {
         // when we use PostgreSQL, during doctrine:schema:update, doctrine does not delete search index.
         $postgreSQLContent = 'Nothing to update - your database is already in sync with the current entity metadata.';
 
         return [
-            'otherCommand' => [
-                'commandName'     => 'doctrine:mapping:info',
-                'params'          => [],
+            'otherCommand mysql' => [
+                'platform' => DatabaseDriverInterface::DRIVER_MYSQL,
+                'commandName' => 'doctrine:mapping:info',
+                'params' => [],
                 'expectedContent' => 'OK',
-                'postgreSQLContent' => 'OK'
             ],
-            'commandWithoutOption' => [
-                'commandName'     => 'doctrine:schema:update',
-                'params'          => [],
+            'otherCommand psql' => [
+                'platform' => DatabaseDriverInterface::DRIVER_POSTGRESQL,
+                'commandName' => 'doctrine:mapping:info',
+                'params' => [],
+                'expectedContent' => 'OK',
+            ],
+            'commandWithoutOption mysql' => [
+                'platform' => DatabaseDriverInterface::DRIVER_MYSQL,
+                'commandName' => 'doctrine:schema:update',
+                'params' => [],
                 'expectedContent' => 'Please run the operation by passing one - or both - of the following options:',
-                'postgreSQLContent' => $postgreSQLContent
             ],
-            'commandWithAnotherOption' => [
-                'commandName'     => 'doctrine:schema:update',
-                'params'          => ['--dump-sql' => true],
-                'expectedContent' => 'ALTER TABLE',
-                'postgreSQLContent' => $postgreSQLContent
+            'commandWithoutOption psql' => [
+                'platform' => DatabaseDriverInterface::DRIVER_POSTGRESQL,
+                'commandName' => 'doctrine:schema:update',
+                'params' => [],
+                'expectedContent' => $postgreSQLContent,
+            ],
+            'commandWithAnotherOption mysql' => [
+                'platform' => DatabaseDriverInterface::DRIVER_MYSQL,
+                'commandName' => 'doctrine:schema:update',
+                'params' => ['--dump-sql' => true],
+                'expectedContent' => 'ALTER TABLE 1',
+            ],
+            'commandWithAnotherOption psql' => [
+                'platform' => DatabaseDriverInterface::DRIVER_POSTGRESQL,
+                'commandName' => 'doctrine:schema:update',
+                'params' => ['--dump-sql' => true],
+                'expectedContent' => $postgreSQLContent,
+            ],
+            'commandWithForceOption mysql' => [
+                'platform' => DatabaseDriverInterface::DRIVER_MYSQL,
+                'commandName' => 'doctrine:schema:update',
+                'params' => ['--force' => true],
+                'expectedContent' => 'Schema update and create index completed.',
             ],
             'commandWithForceOption' => [
-                'commandName'     => 'doctrine:schema:update',
-                'params'          => ['--force' => true],
-                'expectedContent' => "Schema update and create index completed.",
-                'postgreSQLContent' => $postgreSQLContent
-            ]
+                'platform' => DatabaseDriverInterface::DRIVER_POSTGRESQL,
+                'commandName' => 'doctrine:schema:update',
+                'params' => ['--force' => true],
+                'expectedContent' => $postgreSQLContent,
+            ],
         ];
     }
 }
