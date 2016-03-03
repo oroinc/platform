cd097441e38c009ac35f939357d01be694cb95f2
Vova Soroka <vsoroka@magecore.com>
2016-03-03 10:20:21 +0200

    BAP-9376: Create unit tests for ApiBundle. Fix InitializeConfigExtras

--- src/Oro/Bundle/ApiBundle/Tests/Unit/Processor/Get/InitializeConfigExtrasTest.php
+++ src/Oro/Bundle/ApiBundle/Tests/Unit/Processor/Get/InitializeConfigExtrasTest.php
@@ -5,6 +5,7 @@ namespace Oro\Bundle\ApiBundle\Tests\Unit\Processor\Get;
 use Oro\Bundle\ApiBundle\Config\FiltersConfigExtra;
 use Oro\Bundle\ApiBundle\Config\VirtualFieldsConfigExtra;
 use Oro\Bundle\ApiBundle\Processor\Get\InitializeConfigExtras;
+use Oro\Bundle\ApiBundle\Tests\Unit\Processor\TestConfigExtra;
 
 class InitializeConfigExtrasTest extends GetProcessorTestCase
 {
@@ -20,9 +21,14 @@ class InitializeConfigExtrasTest extends GetProcessorTestCase
 
     public function testProcess()
     {
+        $existingExtra = new TestConfigExtra('test');
+        $this->context->setConfigExtras([]);
+        $this->context->addConfigExtra($existingExtra);
+
         $this->processor->process($this->context);
 
-        $this->assertCount(2, $this->context->getConfigExtras());
+        $this->assertCount(3, $this->context->getConfigExtras());
+        $this->assertTrue($this->context->hasConfigExtra($existingExtra->getName()));
         $this->assertTrue($this->context->hasConfigExtra(VirtualFieldsConfigExtra::NAME));
         $this->assertTrue($this->context->hasConfigExtra(FiltersConfigExtra::NAME));
     }
