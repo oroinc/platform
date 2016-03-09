a269073e2129d5a9fae362dd2b71241d394c24b9
Maksym Perepelytsya <mperepelytsya@magecore.com>
2016-03-09 18:05:33 +0200

    CRM-5117: Unacceptable performance of Adore tenant  - Add comments

--- src/Oro/Bundle/UserBundle/Migrations/Schema/v1_19/AddFirstNameLastNameIndex.php
+++ src/Oro/Bundle/UserBundle/Migrations/Schema/v1_19/AddFirstNameLastNameIndex.php
@@ -3,6 +3,7 @@
 namespace Oro\Bundle\UserBundle\Migrations\Schema\v1_19;
 
 use Doctrine\DBAL\Schema\Schema;
+
 use Oro\Bundle\MigrationBundle\Migration\Migration;
 use Oro\Bundle\MigrationBundle\Migration\QueryBag;
 
@@ -23,6 +24,7 @@ class AddFirstNameLastNameIndex implements Migration
      */
     public static function addFirstNameLastNameIndex(Schema $schema)
     {
+        // Adding these index due to issue with mysql 5.6 version @see CRM-5117
         $table = $schema->getTable('oro_user');
         $table->addIndex(['first_name', 'last_name'], 'user_first_name_last_name_idx');
     }
