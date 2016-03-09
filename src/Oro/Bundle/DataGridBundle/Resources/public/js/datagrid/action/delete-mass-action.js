ff665df91e2c81706ca48dc6749e225fd23a100f
Makar Sichevoy <sichevoy@gmail.com>
2016-03-09 19:14:30 +0200

    BB-2254: Add to view Back end

--- src/Oro/Bundle/DataGridBundle/Resources/public/js/datagrid/action/delete-mass-action.js
+++ src/Oro/Bundle/DataGridBundle/Resources/public/js/datagrid/action/delete-mass-action.js
@@ -2,8 +2,9 @@ define([
     'oroui/js/delete-confirmation',
     'orotranslation/js/translator',
     'underscore',
-    './mass-action'
-], function(DeleteConfirmation, __, _, MassAction) {
+    './mass-action',
+    'oroui/js/mediator'
+], function(DeleteConfirmation, __, _, MassAction, mediator) {
     'use strict';
 
     var DeleteMassAction;
@@ -96,6 +97,8 @@ define([
                 return DeleteMassAction.__super__.execute.call(this);
             } else {
                 MassAction.__super__._onAjaxSuccess.apply(this, arguments);
+
+                mediator.trigger('datagrid:afterMassRemoveRow:' + this.datagrid.name);
             }
         },
 
