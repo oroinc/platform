eb46e13f0185f57734e4155f3ba33397a6e652e9
Yurii Muratov <yurio@ua.fm>
2016-03-15 21:10:40 +0200

    BAP-9875: Actions configuration   - fix cs

--- src/Oro/Bundle/ApiBundle/Config/ActionsConfigLoader.php
+++ src/Oro/Bundle/ApiBundle/Config/ActionsConfigLoader.php
@@ -14,17 +14,6 @@ class ActionsConfigLoader extends AbstractConfigLoader implements ConfigLoaderIn
         foreach ($config as $key => $value) {
             $actions->set($key, $value);
         }
-        //$actions->set('delete', 'dwq');
-
-//        foreach ($config as $key => $value) {
-//            if (isset($this->methodMap[$key])) {
-//                $this->callSetter($filters, $this->methodMap[$key], $value);
-//            } elseif (ConfigUtil::FIELDS === $key) {
-//                $this->loadFields($filters, $value);
-//            } else {
-//                $this->setValue($filters, $key, $value);
-//            }
-//        }
 
         return $actions;
     }
