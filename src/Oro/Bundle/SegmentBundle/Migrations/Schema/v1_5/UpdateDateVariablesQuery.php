b348016e4cf628356b745629a9b0fc086d43cb94
Miloslav Nenadal <nenadalm@gmail.com>
2016-03-17 17:07:19 +0100

    BAP-9351: Date variable couldn't be used in reports/segments
    - fix phpcs

--- src/Oro/Bundle/SegmentBundle/Migrations/Schema/v1_5/UpdateDateVariablesQuery.php
+++ src/Oro/Bundle/SegmentBundle/Migrations/Schema/v1_5/UpdateDateVariablesQuery.php
@@ -123,10 +123,10 @@ class UpdateDateVariablesQuery implements MigrationQuery, ConnectionAwareInterfa
     private function updateFilterValue(array $filter)
     {
         if (!isset(
-                $filter['criterion']['filter'],
-                $filter['criterion']['data'],
-                $filter['criterion']['data']['part']
-            ) &&
+            $filter['criterion']['filter'],
+            $filter['criterion']['data'],
+            $filter['criterion']['data']['part']
+        ) &&
             !in_array($filter['criterion']['filter'], ['date', 'datetime']) &&
             !$filter['criterion']['data']['part'] === 'month'
         ) {
