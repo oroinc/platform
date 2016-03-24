c27863184292c287ce2a3916158b5d47fff83f0f
Alexander Nezdoiminoga <alex@magecore.com>
2016-03-24 21:14:22 +0200

    BAP-9985: Add processor that will set total deleted items count into response header

---
+++ src/Oro/Bundle/ApiBundle/Processor/DeleteList/UnsetDeleteCountHeader.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Oro\Bundle\ApiBundle\Processor\DeleteList;
+
+use Oro\Component\ChainProcessor\ContextInterface;
+use Oro\Component\ChainProcessor\ProcessorInterface;
+
+/**
+ * Unset the "X-Include-Delete-Count" response header
+ *  in case if it was requested by "X-Include: deleteCount" request header and an error occurred in deletion process.
+ */
+class UnsetDeleteCountHeader implements ProcessorInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function process(ContextInterface $context)
+    {
+        /** @var DeleteListContext $context */
+
+        if (!$context->getResponseHeaders()->has(SetDeleteCountHeader::HEADER_NAME)) {
+            // total delete count header was not set
+            return;
+        }
+
+        if ($context->hasResult()) {
+            $context->getResponseHeaders()->remove(SetDeleteCountHeader::HEADER_NAME);
+        }
+    }
+}
