a8c4ae18383a2613829c852b362545fab70da3d8
Yurii Muratov <yurio@ua.fm>
2016-03-24 18:16:22 +0200

    BAP-9940: Create controller DELETE list action  - Refactor DeleteHandler processors  - Refactor input parameters process

--- src/Oro/Bundle/ApiBundle/Processor/DeleteList/DeleteListDataByDeleteHandler.php
+++ src/Oro/Bundle/ApiBundle/Processor/DeleteList/DeleteListDataByDeleteHandler.php
@@ -2,65 +2,30 @@
 
 namespace Oro\Bundle\ApiBundle\Processor\DeleteList;
 
-use Symfony\Component\DependencyInjection\Container;
-
-use Oro\Component\ChainProcessor\ProcessorInterface;
 use Oro\Component\ChainProcessor\ContextInterface;
-use Oro\Bundle\ApiBundle\Util\DoctrineHelper;
 use Oro\Bundle\SoapBundle\Handler\DeleteHandler;
+use Oro\Bundle\ApiBundle\Processor\Shared\DeleteDataByDeleteHandler as BaseProcessor;
 
-class DeleteListDataByDeleteHandler implements ProcessorInterface
+/**
+ * Deletes objects list by DeleteHandler.
+ */
+class DeleteListDataByDeleteHandler extends BaseProcessor
 {
-    const DEFAULT_DELETE_HANDLER = 'oro_soap.handler.delete';
-
-    /** @var DoctrineHelper */
-    protected $doctrineHelper;
-
-    /** @var Container */
-    protected $container;
-
-    /**
-     * @param DoctrineHelper $doctrineHelper
-     * @param Container      $container
-     */
-    public function __construct(DoctrineHelper $doctrineHelper, Container $container)
-    {
-        $this->doctrineHelper = $doctrineHelper;
-        $this->container = $container;
-    }
-
     /**
      * {@inheritdoc}
      */
-    public function process(ContextInterface $context)
+    protected function processDelete(ContextInterface $context, DeleteHandler $handler)
     {
-        /** @var DeleteListContext $context */
-
         $entityList = $context->getResult();
-        if (empty($entityList)) {
-            // list already deleted or not supported
-            return;
-        }
-
-        $deleteHandlerServiceId = $context->getConfig()->getDeleteHandler();
-        if (!$deleteHandlerServiceId) {
-            $deleteHandlerServiceId = self::DEFAULT_DELETE_HANDLER;
-        }
-
-        $deleteHandler = $this->container->get($deleteHandlerServiceId);
         $entityManager = $this->doctrineHelper->getEntityManagerForClass($context->getClassName());
-        if ($deleteHandler instanceof DeleteHandler) {
-            $entityManager->getConnection()->beginTransaction();
-            try {
-                foreach ($entityList as $entity) {
-                    $deleteHandler->processDelete($entity, $entityManager);
-                }
-                $entityManager->getConnection()->commit();
-            } catch (\Exception $e) {
-                $entityManager->getConnection()->rollBack();
+        $entityManager->getConnection()->beginTransaction();
+        try {
+            foreach ($entityList as $entity) {
+                $handler->processDelete($entity, $entityManager);
             }
-
-            $context->removeResult();
+            $entityManager->getConnection()->commit();
+        } catch (\Exception $e) {
+            $entityManager->getConnection()->rollBack();
         }
     }
 }
