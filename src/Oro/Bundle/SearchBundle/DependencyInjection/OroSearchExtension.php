13e957dd3f20d59c45a737afb47c8c16b1c4ecfe
AlexandrDmitriev <admitriev@magecore.com>
2016-03-29 12:45:37 +0300

    BAP-9962: Incorrect merge search config with custom config; - updated upgrade.md

--- src/Oro/Bundle/SearchBundle/DependencyInjection/OroSearchExtension.php
+++ src/Oro/Bundle/SearchBundle/DependencyInjection/OroSearchExtension.php
@@ -97,7 +97,7 @@ class OroSearchExtension extends Extension
      * @param array $firstConfig
      * @param array $secondConfig
      * @return array
-     * @deprecated Since 1.9, will be removed after 1.11.
+     * @deprecated Since 1.8, will be removed after 1.11.
      *
      * @todo: it is a temporary workaround to add ability to merge configs until improvement BAP-10010 is implemented
      */
