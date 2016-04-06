a544ab66f204401e610867e825ce25d1ac8f8e7f
Denis Voronin <dvoronin@magecore.com>
2016-04-06 16:37:00 +0300

    CRM-5234: Stripping unexpected Magento data

---
+++ src/Oro/Bundle/IntegrationBundle/Provider/SoapClient.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Oro\Bundle\IntegrationBundle\Provider;
+
+class SoapClient extends \SoapClient
+{
+    /**
+     * Overridden _doRequest method
+     *
+     * {@inheritdoc}
+     */
+    public function __doRequest($request, $location, $action, $version, $one_way = 0)
+    {
+        $response = parent::__doRequest($request, $location, $action, $version, $one_way);
+
+        // Remove all non printable characters except whitespace characters
+        $response = preg_replace('/[^[:print:][:space:]]/u', '', $response);
+
+        return $response;
+    }
+}
