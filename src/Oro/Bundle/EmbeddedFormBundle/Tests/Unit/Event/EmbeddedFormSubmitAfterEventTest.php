3c0041aa48d5dbc888a13ac5a6b5a5c42639fa20
Pavel Usachev <pusachev@magecore.com>
2016-04-08 18:13:22 +0300

    BAP-9463: Provide compatibility with PHP7 - fix error Failed asserting that exception of type "TypeError" matches expected exception

--- src/Oro/Bundle/EmbeddedFormBundle/Tests/Unit/Event/EmbeddedFormSubmitAfterEventTest.php
+++ src/Oro/Bundle/EmbeddedFormBundle/Tests/Unit/Event/EmbeddedFormSubmitAfterEventTest.php
@@ -3,20 +3,22 @@
 namespace Oro\Bundle\EmbeddedFormBundle\Tests\Unit\Event;
 
 use Oro\Bundle\EmbeddedFormBundle\Event\EmbeddedFormSubmitAfterEvent;
+use Oro\Bundle\EmbeddedFormBundle\Entity\EmbeddedForm;
 
 class EmbeddedFormSubmitAfterEventTest extends \PHPUnit_Framework_TestCase
 {
-    /**
-     * @expectedException \PHPUnit_Framework_Error
-     */
+
     public function testConstructorRequires()
     {
+        $expectedException = $this->getExpectedExceptionCode();
+        $this->setExpectedException($expectedException);
+
         new EmbeddedFormSubmitAfterEvent(null);
     }
 
     public function testGetter()
     {
-        $formEntity = $this->getMock('Oro\Bundle\EmbeddedFormBundle\Entity\EmbeddedForm');
+        $formEntity = new EmbeddedForm();
         $form = $this
             ->getMockBuilder('Symfony\Component\Form\Form')
             ->disableOriginalConstructor()
@@ -26,5 +28,13 @@ class EmbeddedFormSubmitAfterEventTest extends \PHPUnit_Framework_TestCase
         $this->assertSame($formEntity, $event->getFormEntity());
         $this->assertSame([], $event->getData());
         $this->assertSame($form, $event->getForm());
+    }
+
+    /**
+     * @return string
+     */
+    protected function getExpectedExceptionCode()
+    {
+        return version_compare(PHP_VERSION, '7.0.0', '>=') ? 'TypeError' : 'PHPUnit_Framework_Error';
     }
 }
--- src/Oro/Bundle/EmbeddedFormBundle/Tests/Unit/Event/EmbeddedFormSubmitBeforeEventTest.php
+++ src/Oro/Bundle/EmbeddedFormBundle/Tests/Unit/Event/EmbeddedFormSubmitBeforeEventTest.php
@@ -3,23 +3,32 @@
 namespace Oro\Bundle\EmbeddedFormBundle\Tests\Unit\Event;
 
 use Oro\Bundle\EmbeddedFormBundle\Event\EmbeddedFormSubmitBeforeEvent;
+use Oro\Bundle\EmbeddedFormBundle\Entity\EmbeddedForm;
 
 class EmbeddedFormSubmitBeforeEventTest extends \PHPUnit_Framework_TestCase
 {
-    /**
-     * @expectedException \PHPUnit_Framework_Error
-     */
     public function testConstructorRequires()
     {
+        $expectedException = $this->getExpectedExceptionCode();
+        $this->setExpectedException($expectedException);
+        
         new EmbeddedFormSubmitBeforeEvent(null);
     }
 
     public function testGetter()
     {
-        $formEntity = $this->getMock('Oro\Bundle\EmbeddedFormBundle\Entity\EmbeddedForm');
+        $formEntity = new EmbeddedForm();
         $event   = new EmbeddedFormSubmitBeforeEvent([], $formEntity);
 
         $this->assertSame($formEntity, $event->getFormEntity());
         $this->assertSame([], $event->getData());
+    }
+
+    /**
+     * @return string
+     */
+    protected function getExpectedExceptionCode()
+    {
+        return version_compare(PHP_VERSION, '7.0.0', '>=') ? 'TypeError' : 'PHPUnit_Framework_Error';
     }
 }
