6e11bf89b9a9c2a5b9d8a18ac5406a280bec6640
dmitrosh <dshykalenko@magecore.com>
2016-04-14 19:15:11 +0300

    BAP-10159: extend select2 component to input new value which not exists in choices

--- src/Oro/Bundle/FormBundle/Resources/public/js/app/components/select2-autocomplete-component.js
+++ src/Oro/Bundle/FormBundle/Resources/public/js/app/components/select2-autocomplete-component.js
@@ -6,7 +6,28 @@ define(function(require) {
     var Select2Component = require('oro/select2-component');
 
     Select2AutocompleteComponent = Select2Component.extend({
-        ViewType: Select2AutocompleteView
+        ViewType: Select2AutocompleteView,
+        setConfig: function(config) {
+            config = Select2AutocompleteComponent.__super__.setConfig.apply(this, arguments);
+            var propName = config.propertyNameForNewItem;
+            if (propName) {
+                config.createSearchChoice = function(value, results) {
+                    if (results.length === 0) {
+                        var item = {id: null};
+                        item[propName] = value;
+                        return item;
+                    }
+                };
+                config.id = function(e) {
+                    var val = {id: e.id};
+                    if (val.id === null) {
+                        val.value = e[propName];
+                    }
+                    return JSON.stringify(val);
+                };
+            }
+            return config;
+        }
     });
 
     return Select2AutocompleteComponent;
