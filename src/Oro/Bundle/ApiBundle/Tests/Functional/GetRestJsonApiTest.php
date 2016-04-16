391982b635fbb29ab1bc99aaee9e9a2f1d57afed
Vova Soroka <vsoroka@magecore.com>
2016-04-16 16:08:27 +0300

    BAP-10127: Unify result document builder. Fix functional tests

--- src/Oro/Bundle/ApiBundle/Tests/Functional/GetRestJsonApiTest.php
+++ src/Oro/Bundle/ApiBundle/Tests/Functional/GetRestJsonApiTest.php
@@ -130,14 +130,8 @@ class GetRestJsonApiTest extends ApiTestCase
                 if ($response->getStatusCode() !== 204) {
                     // process delete errors
                     $data = $this->jsonToArray($response->getContent());
-                    $errors = [
-                        'An operation is forbidden. Reason: has assignments',
-                        'An operation is forbidden. Reason: has users',
-                        'An operation is forbidden. Reason: self delete',
-                        'An operation is forbidden. Reason: organization has assignments'
-                    ];
-                    $this->assertContains($data['errors'][0]['detail'], $errors);
                     $this->assertEquals(403, $response->getStatusCode());
+                    $this->assertEquals($data['errors'][0]['title'], 'forbidden exception');
                 } elseif (!in_array('get', $excludedActions, true)) {
                     // check if entity was really deleted
                     $this->checkGetRequest($entityAlias, $id[0], 404);
@@ -169,13 +163,8 @@ class GetRestJsonApiTest extends ApiTestCase
         if ($response->getStatusCode() !== 204) {
             // process delete errors
             $data = $this->jsonToArray($response->getContent());
-            $errors = [
-                'An operation is forbidden. Reason: has assignments',
-                'An operation is forbidden. Reason: self delete',
-                'An operation is forbidden. Reason: organization has assignments'
-            ];
-            $this->assertContains($data['errors'][0]['detail'], $errors);
             $this->assertEquals(403, $response->getStatusCode());
+            $this->assertEquals($data['errors'][0]['title'], 'forbidden exception');
         } elseif (!in_array('get', $excludedActions, true)) {
             // check if entity was really deleted
             $this->checkGetRequest($entityAlias, $id, 404);
