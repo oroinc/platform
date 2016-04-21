5637ef3f078e692d73df04a22642c48e451a9929
Miloslav Nenadal <nenadalm@gmail.com>
2016-04-21 14:37:24 +0200

    BAP-9125: Exception is occurred during upgrade from old version
    - fix build

--- src/Oro/Bundle/EntityExtendBundle/Tests/Unit/Tools/ExtendConfigDumperTest.php
+++ src/Oro/Bundle/EntityExtendBundle/Tests/Unit/Tools/ExtendConfigDumperTest.php
@@ -6,7 +6,6 @@ use Oro\Bundle\EntityConfigBundle\Tests\Unit\ConfigProviderMock;
 use Oro\Bundle\EntityExtendBundle\Extend\FieldTypeHelper;
 use Oro\Bundle\EntityExtendBundle\Tools\ExtendConfigDumper;
 use Oro\Bundle\EntityExtendBundle\Tools\ExtendDbIdentifierNameGenerator;
-use Oro\Bundle\EntityExtendBundle\Tools\ExtendHelper;
 
 class ExtendConfigDumperTest extends \PHPUnit_Framework_TestCase
 {
@@ -42,7 +41,10 @@ class ExtendConfigDumperTest extends \PHPUnit_Framework_TestCase
         $this->cacheDir = __DIR__ . DIRECTORY_SEPARATOR . 'Fixtures' . DIRECTORY_SEPARATOR
             . 'Dumper' . DIRECTORY_SEPARATOR . 'cache';
 
+        $cm = $this->getMock('Oro\Bundle\EntityExtendBundle\Tools\ConfigManager');
+
         $this->dumper = new ExtendConfigDumper(
+            $cm,
             $this->configProvider,
             new ExtendDbIdentifierNameGenerator(),
             new FieldTypeHelper([]),
