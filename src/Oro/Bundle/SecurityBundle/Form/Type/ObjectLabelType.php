e51e3249feef88d53c1fc11caffe8663680acad3
Yurii Muratov <yurio@ua.fm>
2016-04-26 17:07:48 +0300

    BAP-10279: Entity should be a link only for entities for which field level ACL is enabled.

--- src/Oro/Bundle/SecurityBundle/Form/Type/ObjectLabelType.php
+++ src/Oro/Bundle/SecurityBundle/Form/Type/ObjectLabelType.php
@@ -2,6 +2,7 @@
 
 namespace Oro\Bundle\SecurityBundle\Form\Type;
 
+use Oro\Bundle\EntityBundle\Tools\EntityClassNameHelper;
 use Oro\Bundle\SecurityBundle\Acl\Domain\ObjectIdentityFactory;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormInterface;
@@ -9,6 +10,17 @@ use Symfony\Component\Form\FormView;
 
 class ObjectLabelType extends AbstractType
 {
+    /** @var EntityClassNameHelper */
+    protected $classNameHelper;
+
+    /**
+     * @param EntityClassNameHelper $classNameHelper
+     */
+    public function __construct(EntityClassNameHelper $classNameHelper)
+    {
+        $this->classNameHelper = $classNameHelper;
+    }
+
     /**
      * {@inheritdoc}
      */
@@ -23,13 +35,14 @@ class ObjectLabelType extends AbstractType
     public function buildView(FormView $view, FormInterface $form, array $options)
     {
         $identity = $view->parent->vars['value']->getId();
-        $className = str_replace(['entity:', '\\'], ['', '_'], $identity);
+        $className = str_replace('entity:', '', $identity);
 
         // add url params for field level aces
         if (strpos($identity, 'entity:') === 0 && $className != ObjectIdentityFactory::ROOT_IDENTITY_TYPE) {
             $role = $view->parent->parent->parent->parent->vars['value'];
 
             $view->vars['roleId'] = $role ? $role->getId() : null;
+            $view->vars['urlSafeClassName'] = $this->classNameHelper->getUrlSafeClassName($className);
             $view->vars['className'] = $className;
         }
     }
