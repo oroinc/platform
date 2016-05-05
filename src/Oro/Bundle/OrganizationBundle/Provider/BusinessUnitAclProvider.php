81a837245782bcd025aec832b03a89ada2041f10
Mykhailo Kudelia <mkudelia@magecore.com>
2016-05-05 19:24:11 +0300

    BAP-9756: List of BU doesn't displayed according ACL on the freshly added Forecast widget - resolve merge conflicts

---
+++ src/Oro/Bundle/OrganizationBundle/Provider/BusinessUnitAclProvider.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace Oro\Bundle\OrganizationBundle\Provider;
+
+use Oro\Bundle\OrganizationBundle\Entity\Manager\BusinessUnitManager;
+use Oro\Bundle\SecurityBundle\Acl\AccessLevel;
+use Oro\Bundle\SecurityBundle\Acl\Domain\OneShotIsGrantedObserver;
+use Oro\Bundle\SecurityBundle\Acl\Voter\AclVoter;
+use Oro\Bundle\SecurityBundle\SecurityFacade;
+use Oro\Bundle\SecurityBundle\Owner\OwnerTreeProvider;
+use Oro\Bundle\UserBundle\Entity\UserInterface;
+
+class BusinessUnitAclProvider
+{
+    /** @var BusinessUnitManager */
+    protected $businessUnitManager;
+
+    /** @var SecurityFacade */
+    protected $securityFacade;
+
+    /** @var bool */
+    protected $isAssignGranted;
+
+    /** @var int */
+    protected $accessLevel;
+
+    /** @var UserInterface */
+    protected $currentUser;
+
+    /** @var AclVoter */
+    protected $aclVoter;
+
+    /** @var OwnerTreeProvider */
+    protected $treeProvider;
+
+    /**
+     * @param BusinessUnitManager $businessUnitManager
+     * @param SecurityFacade      $securityFacade
+     * @param AclVoter            $aclVoter
+     * @param OwnerTreeProvider   $treeProvider
+     */
+    public function __construct(
+        BusinessUnitManager $businessUnitManager,
+        SecurityFacade $securityFacade,
+        AclVoter $aclVoter,
+        OwnerTreeProvider $treeProvider
+    ) {
+        $this->businessUnitManager       = $businessUnitManager;
+        $this->securityFacade            = $securityFacade;
+        $this->aclVoter                  = $aclVoter;
+        $this->treeProvider              = $treeProvider;
+    }
+
+    /**
+     * @param string $dataClassName
+     * @param string $permission
+     * @return array
+     */
+    public function getBusinessUnitIds($dataClassName, $permission = 'VIEW')
+    {
+        $this->getCurrentUser();
+        $this->checkIsGranted($permission, 'entity:' . $dataClassName);
+
+        if ($this->isAssignGranted) {
+            return $this->getIds();
+        }
+
+        return [null];
+    }
+
+    /**
+     * Check is granting user to object in given permission
+     *
+     * @param string        $permission
+     * @param object|string $object
+     */
+    protected function checkIsGranted($permission, $object)
+    {
+        $observer = new OneShotIsGrantedObserver();
+        $this->aclVoter->addOneShotIsGrantedObserver($observer);
+        $this->isAssignGranted = $this->securityFacade->isGranted($permission, $object);
+        $this->accessLevel     = $observer->getAccessLevel();
+    }
+
+    /**
+     * Get business units ids for current user for current access level
+     *
+     * @return array
+     */
+    protected function getIds()
+    {
+        if (AccessLevel::SYSTEM_LEVEL == $this->accessLevel) {
+            return $this->businessUnitManager->getBusinessUnitIds();
+        } elseif (AccessLevel::LOCAL_LEVEL == $this->accessLevel) {
+            return $this->treeProvider->getTree()->getUserBusinessUnitIds(
+                $this->currentUser->getId(),
+                $this->getOrganizationContextId()
+            );
+        } elseif (AccessLevel::DEEP_LEVEL === $this->accessLevel) {
+            return $this->treeProvider->getTree()->getUserSubordinateBusinessUnitIds(
+                $this->currentUser->getId(),
+                $this->getOrganizationContextId()
+            );
+        } elseif (AccessLevel::GLOBAL_LEVEL === $this->accessLevel) {
+            return $this->businessUnitManager->getBusinessUnitIds($this->getOrganizationContextId());
+        }
+
+        return [null];
+    }
+
+    /**
+     * @return null|UserInterface
+     */
+    protected function getCurrentUser()
+    {
+        if (null === $this->currentUser) {
+            $user = $this->securityFacade->getLoggedUser();
+            if ($user && is_object($user) && $user instanceof UserInterface) {
+                $this->currentUser = $user;
+            }
+        }
+
+        return $this->currentUser;
+    }
+
+    /**
+     * @return int
+     */
+    protected function getOrganizationContextId()
+    {
+        return $this->securityFacade->getOrganization()->getId();
+    }
+}
