64ca579a015441dc08cfe97fa7431a1ee42065cd
Aleksey Solonenko <asolonenko@magecore.com>
2016-05-06 13:21:03 +0300

    BAP-10342: Click on the link opens page in current window  - Change allowed for TinyMCE <a> element attribute target  - Add target as allowed for HTMLPurifier

--- src/Oro/Bundle/FormBundle/Form/DataTransformer/SanitizeHTMLTransformer.php
+++ src/Oro/Bundle/FormBundle/Form/DataTransformer/SanitizeHTMLTransformer.php
@@ -63,6 +63,7 @@ class SanitizeHTMLTransformer implements DataTransformerInterface
             'URI.AllowedSchemes',
             ['http' => true, 'https' => true, 'mailto' => true, 'ftp' => true, 'data' => true]
         );
+        $config->set('Attr.AllowedFrameTargets', ['_blank']);
         $purifier = new \HTMLPurifier($config);
 
         return $purifier->purify($value);
