a87ef9ac0b6975cbb0742f9ac3df5266971aa551
Vova Soroka <vsoroka@magecore.com>
2016-05-11 16:45:37 +0300

    BAP-10423: Restored fields are not available

--- src/Oro/Bundle/EntityExtendBundle/Tools/ExtendConfigDumper.php
+++ src/Oro/Bundle/EntityExtendBundle/Tools/ExtendConfigDumper.php
@@ -476,6 +476,9 @@ class ExtendConfigDumper
             $fieldConfig->set('is_deleted', true);
             $this->configManager->persist($fieldConfig);
         } elseif (!$fieldConfig->is('state', ExtendScope::STATE_ACTIVE)) {
+            if ($fieldConfig->is('state', ExtendScope::STATE_RESTORE)) {
+                $fieldConfig->set('is_deleted', false);
+            }
             $fieldConfig->set('state', ExtendScope::STATE_ACTIVE);
             $this->configManager->persist($fieldConfig);
         }
