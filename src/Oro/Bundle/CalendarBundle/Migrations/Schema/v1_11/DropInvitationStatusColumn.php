e3719e1c453d9cdc2302877a206b89a6ceef5b6a
Miloslav Nenadal <nenadalm@gmail.com>
2016-05-12 14:28:56 +0200

    AEIV-577: Update implementation of UI views to support updated data structure: Client side
    Conflicts:
    	src/Oro/Bundle/CalendarBundle/Migrations/Schema/v1_10/ConvertCalendarEventOwnerToAttendee.php
    	src/Oro/Bundle/CalendarBundle/Migrations/Schema/v1_10/OroCalendarBundle.php

---
+++ src/Oro/Bundle/CalendarBundle/Migrations/Schema/v1_11/DropInvitationStatusColumn.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Oro\Bundle\CalendarBundle\Migrations\Schema\v1_10;
+
+use Doctrine\DBAL\Schema\Schema;
+
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+
+class DropInvitationStatusColumn implements Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        $table = $schema->getTable('oro_calendar_event');
+        $table->dropColumn('invitation_status');
+    }
+}
---
+++ src/Oro/Bundle/CalendarBundle/Migrations/Schema/v1_11/OroCalendarBundle.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace Oro\Bundle\CalendarBundle\Migrations\Schema\v1_10;
+
+use Doctrine\DBAL\Schema\Schema;
+
+use Oro\Bundle\CalendarBundle\Entity\Attendee;
+use Oro\Bundle\CalendarBundle\Entity\CalendarEvent;
+use Oro\Bundle\EntityExtendBundle\EntityConfig\ExtendScope;
+use Oro\Bundle\EntityExtendBundle\Migration\Extension\ExtendExtension;
+use Oro\Bundle\EntityExtendBundle\Migration\Extension\ExtendExtensionAwareInterface;
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+
+class OroCalendarBundle implements Migration, ExtendExtensionAwareInterface
+{
+    /** @var ExtendExtension $extendExtension */
+    protected $extendExtension;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setExtendExtension(ExtendExtension $extendExtension)
+    {
+        $this->extendExtension = $extendExtension;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        $this->createAttendee($schema);
+        $this->addForeignKeys($schema);
+        $this->addEnums($schema);
+
+        $this->updateCalendarEvent($schema);
+
+        $queries->addQuery(new ConvertCalendarEventOwnerToAttendee());
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    protected function createAttendee(Schema $schema)
+    {
+        $table = $schema->createTable('oro_attendee');
+        $table->addColumn('id', 'integer', ['autoincrement' => true]);
+        $table->addColumn('user_owner_id', 'integer', ['notnull' => false]);
+        $table->addColumn('calendar_event_id', 'integer', ['notnull' => true]);
+        $table->addColumn('email', 'string', ['notnull' => true, 'length' => 255]);
+        $table->addColumn('display_name', 'string', ['notnull' => false, 'length' => 255]);
+        $table->addColumn('created_at', 'datetime', ['notnull' => true]);
+        $table->addColumn('updated_at', 'datetime', ['notnull' => true]);
+
+        $table->setPrimaryKey(['id']);
+
+        $table->addIndex(['user_owner_id']);
+        $table->addIndex(['calendar_event_id']);
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    protected function addForeignKeys(Schema $schema)
+    {
+        $table = $schema->getTable('oro_attendee');
+
+        $table->addForeignKeyConstraint(
+            $schema->getTable('oro_user'),
+            ['user_owner_id'],
+            ['id'],
+            ['onDelete' => 'SET NULL', 'onUpdate' => null]
+        );
+
+        $table->addForeignKeyConstraint(
+            $schema->getTable('oro_calendar_event'),
+            ['calendar_event_id'],
+            ['id'],
+            ['onDelete' => 'CASCADE', 'onUpdate' => null]
+        );
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    protected function addEnums(Schema $schema)
+    {
+        $table = $schema->getTable('oro_attendee');
+
+        $this->extendExtension->addEnumField(
+            $schema,
+            $table,
+            'status',
+            Attendee::STATUS_ENUM_CODE,
+            false,
+            false,
+            [
+                'extend' => ['owner' => ExtendScope::OWNER_CUSTOM]
+            ]
+        );
+
+        $this->extendExtension->addEnumField(
+            $schema,
+            $table,
+            'origin',
+            Attendee::ORIGIN_ENUM_CODE,
+            false,
+            false,
+            [
+                'extend' => ['owner' => ExtendScope::OWNER_CUSTOM]
+            ]
+        );
+
+        $this->extendExtension->addEnumField(
+            $schema,
+            $table,
+            'type',
+            Attendee::TYPE_ENUM_CODE,
+            false,
+            false,
+            [
+                'extend' => ['owner' => ExtendScope::OWNER_CUSTOM]
+            ]
+        );
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    protected function updateCalendarEvent(Schema $schema)
+    {
+        $table = $schema->getTable('oro_calendar_event');
+
+        $table->addColumn('related_attendee', 'integer', ['notnull' => false]);
+        $table->addIndex(['related_attendee']);
+
+        $table->addForeignKeyConstraint(
+            $schema->getTable('oro_attendee'),
+            ['related_attendee'],
+            ['id'],
+            ['onDelete' => 'SET NULL', 'onUpdate' => null]
+        );
+        
+        $this->extendExtension->addEnumField(
+            $schema,
+            $table,
+            'origin',
+            CalendarEvent::ORIGIN_ENUM_CODE,
+            false,
+            false,
+            [
+                'extend' => ['owner' => ExtendScope::OWNER_CUSTOM]
+            ]
+        );
+    }
+}
