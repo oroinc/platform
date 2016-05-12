cfab6b480863a9001ede4f39b6f3791e7d30c9af
Alexandr Parkhomenko <alexandr.parhomenk@gmail.com>
2016-05-12 15:51:21 +0300

    BAP-9463: Provide compatibility with PHP7 - Fixed unmeet dependency

--- src/Oro/Bundle/WorkflowBundle/Tests/Unit/Configuration/WorkflowConfigurationTest.php
+++ src/Oro/Bundle/WorkflowBundle/Tests/Unit/Configuration/WorkflowConfigurationTest.php
@@ -2,6 +2,8 @@
 
 namespace Oro\Bundle\WorkflowBundle\Tests\Unit\Configuration;
 
+use Symfony\Component\Yaml\Yaml;
+
 use Oro\Bundle\WorkflowBundle\Configuration\WorkflowConfiguration;
 
 class WorkflowConfigurationTest extends \PHPUnit_Framework_TestCase
