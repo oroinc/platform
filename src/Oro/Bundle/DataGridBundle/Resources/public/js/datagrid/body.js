b93c2404803fb8c5e9c2051fef74508b69366065
Andrii Muzalevskyi <amuzalevsky@magecore.com>
2016-05-13 13:01:57 +0300

    BAP-10434 Review comment fixes.

--- src/Oro/Bundle/DataGridBundle/Resources/public/js/datagrid/body.js
+++ src/Oro/Bundle/DataGridBundle/Resources/public/js/datagrid/body.js
@@ -102,8 +102,6 @@ define([
          * @inheritDoc
          */
         refresh: function() {
-            // fasten updates by detaching body tree from document
-            this.$el.empty();
             this._stopListeningToRowsEvents(this.rows);
             _.each(this.rows, function(row) {
                 // dispose in Chaplin's way, instead of Backbone's remove
