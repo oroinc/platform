40394ffd42df052b3efbaa27197b8946f37d2810
Miloslav Nenadal <nenadalm@gmail.com>
2016-05-17 11:17:13 +0200

    AEIV-572: Update implementation of UI views to support updated data structure: Server side
    - disable creation of attendees without user in crm

--- src/Oro/Bundle/CalendarBundle/Form/Type/CalendarEventAttendeesType.php
+++ src/Oro/Bundle/CalendarBundle/Form/Type/CalendarEventAttendeesType.php
@@ -52,7 +52,6 @@ class CalendarEventAttendeesType extends AbstractType
                 return array_merge(
                     $value,
                     [
-                        'allowCreateNew' => true,
                         'renderedPropertyName' => 'email',
                         'forceSelectedData' => true,
                     ]
