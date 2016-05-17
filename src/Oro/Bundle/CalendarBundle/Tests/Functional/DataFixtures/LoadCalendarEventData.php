1449d3d966631433346108355ec5322279a2f143
Valentine Velikiy <vvelykyi@magecore.com>
2016-05-17 17:00:41 +0300

    AEIV-605: Exception handling - Testing data moved into separate data-fixture file.

--- src/Oro/Bundle/CalendarBundle/Tests/Functional/DataFixtures/LoadCalendarEventData.php
+++ src/Oro/Bundle/CalendarBundle/Tests/Functional/DataFixtures/LoadCalendarEventData.php
@@ -5,12 +5,14 @@ namespace Oro\Bundle\CalendarBundle\Tests\Functional\DataFixtures;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
+use Symfony\Component\Yaml\Yaml;
+
 use Oro\Bundle\CalendarBundle\Entity\CalendarEvent;
 use Oro\Bundle\CalendarBundle\Entity\Recurrence;
 
 class LoadCalendarEventData extends AbstractFixture
 {
-    const CALENDAR_EVENT_TITLE = 'Test regular event not in start:end range';
+    const CALENDAR_EVENT_TITLE = 'Regular event not in start:end range';
 
     /**
      * {@inheritdoc}
@@ -23,141 +25,52 @@ class LoadCalendarEventData extends AbstractFixture
             $user->getId(),
             $organization->getId()
         );
+        $filePath = __DIR__ . DIRECTORY_SEPARATOR . 'calendar_event_fixture.yml';
+        $data = Yaml::parse($filePath);
 
-        // Test regular event not in start:end range.
-        $regularEventNotInRange = new CalendarEvent();
-        $regularEventNotInRange->setCalendar($calendar);
-        $regularEventNotInRange->setTitle(self::CALENDAR_EVENT_TITLE);
-        $regularEventNotInRange->setStart(
-            new \DateTime(gmdate(DATE_RFC3339, strtotime('-7 day')))
-        );
-        $regularEventNotInRange->setEnd(
-            new \DateTime(gmdate(DATE_RFC3339, strtotime('-7 day + 1 hour')))
-        );
-        $regularEventNotInRange->setAllDay(false);
-        $manager->persist($regularEventNotInRange);
-
-        // Test regular event in start:end range.
-        $regularEventInRange = new CalendarEvent();
-        $regularEventInRange->setCalendar($calendar);
-        $regularEventInRange->setTitle('Test regular event in start:end range');
-        $regularEventInRange->setStart(
-            new \DateTime(gmdate(DATE_RFC3339, strtotime('-3 day')))
-        );
-        $regularEventInRange->setEnd(
-            new \DateTime(gmdate(DATE_RFC3339, strtotime('-3 day + 1 hour')))
-        );
-        $regularEventInRange->setAllDay(false);
-        $manager->persist($regularEventInRange);
-
-        // Test recurring event not in start:end range.
-        $eventNotInRange= new CalendarEvent();
-        $eventNotInRange->setCalendar($calendar);
-        $eventNotInRange->setTitle('Test recurring event not in start:end range');
-        $eventNotInRange->setStart(
-            new \DateTime(gmdate(DATE_RFC3339, strtotime('-7 day')))
-        );
-        $eventNotInRange->setEnd(
-            new \DateTime(gmdate(DATE_RFC3339, strtotime('-7 day + 1 hour')))
-        );
-        $eventNotInRange->setAllDay(false);
-        $recurrenceNotInRange = new Recurrence();
-        $recurrenceNotInRange->setRecurrenceType(Recurrence::TYPE_DAILY);
-        $recurrenceNotInRange->setInterval(1);
-        $recurrenceNotInRange->setOccurrences(2);
-        $recurrenceNotInRange->setStartTime(
-            new \DateTime(gmdate(DATE_RFC3339, strtotime('-7 day')))
-        );
-        $eventNotInRange->setRecurrence($recurrenceNotInRange);
-        $manager->persist($eventNotInRange);
-
-        // Test recurring event in start:end range.
-        $eventInRange = new CalendarEvent();
-        $eventInRange->setCalendar($calendar);
-        $eventInRange->setTitle('Test recurring event in start:end range');
-        $eventInRange->setStart(
-            new \DateTime(gmdate(DATE_RFC3339, strtotime('-4 day')))
-        );
-        $eventInRange->setEnd(
-            new \DateTime(gmdate(DATE_RFC3339, strtotime('-4 day + 1 hour')))
-        );
-        $eventInRange->setAllDay(false);
-        $recurrenceInRange = new Recurrence();
-        $recurrenceInRange->setRecurrenceType(Recurrence::TYPE_DAILY);
-        $recurrenceInRange->setInterval(1);
-        $recurrenceInRange->setOccurrences(2);
-        $recurrenceInRange->setStartTime(
-            new \DateTime(gmdate(DATE_RFC3339, strtotime('-4 day')))
-        );
-        $eventInRange->setRecurrence($recurrenceInRange);
-        $manager->persist($eventInRange);
-
-        // Test recurring event not in start:end range with exception in range.
-        $eventNotInRangeWithExceptionInRange = new CalendarEvent();
-        $eventNotInRangeWithExceptionInRange->setCalendar($calendar);
-        $eventNotInRangeWithExceptionInRange->setTitle(
-            'Test recurring event not in start:end range with exception in range'
-        );
-        $eventNotInRangeWithExceptionInRangeStart = new \DateTime(gmdate(DATE_RFC3339, strtotime('-7 day')));
-        $eventNotInRangeWithExceptionInRange->setStart($eventNotInRangeWithExceptionInRangeStart);
-        $eventNotInRangeWithExceptionInRange->setEnd(
-            new \DateTime(gmdate(DATE_RFC3339, strtotime('-7 day + 1 hour')))
-        );
-        $eventNotInRangeWithExceptionInRange->setAllDay(false);
-        $recurrenceNotInRangeWithException = new Recurrence();
-        $recurrenceNotInRangeWithException->setRecurrenceType(Recurrence::TYPE_DAILY);
-        $recurrenceNotInRangeWithException->setInterval(1);
-        $recurrenceNotInRangeWithException->setOccurrences(2);
-        $recurrenceNotInRangeWithException->setStartTime($eventNotInRangeWithExceptionInRangeStart);
-        $eventNotInRangeWithExceptionInRange->setRecurrence($recurrenceNotInRangeWithException);
-        $exceptionInRange = new CalendarEvent();
-        $exceptionInRange->setCalendar($calendar);
-        $exceptionInRange->setTitle('Test recurring event exception in range');
-        $exceptionInRange->setStart(
-            new \DateTime(gmdate(DATE_RFC3339, strtotime('-4 day')))
-        );
-        $exceptionInRange->setEnd(
-            new \DateTime(gmdate(DATE_RFC3339, strtotime('-4 day + 1 hour')))
-        );
-        $exceptionInRange->setAllDay(false);
-        $exceptionInRange->setOriginalStart($eventNotInRangeWithExceptionInRangeStart);
-        $exceptionInRange->setRecurringEvent($eventNotInRangeWithExceptionInRange);
-        $exceptionInRange->setIsCancelled(false);
-        $eventNotInRangeWithExceptionInRange->addRecurringEventException($exceptionInRange);
-        $manager->persist($eventNotInRangeWithExceptionInRange);
-
-        // Test recurring event in start:end range with canceled exception.
-        $eventInRangeWithCancelledException = new CalendarEvent();
-        $eventInRangeWithCancelledException->setCalendar($calendar);
-        $eventInRangeWithCancelledException->setTitle(
-            'Test recurring event in start:end range with canceled exception'
-        );
-        $eventInRangeWithCancelledExceptionStart = new \DateTime(gmdate(DATE_RFC3339, strtotime('-2 day')));
-        $eventInRangeWithCancelledException->setStart($eventInRangeWithCancelledExceptionStart);
-        $eventInRangeWithCancelledException->setEnd(
-            new \DateTime(gmdate(DATE_RFC3339, strtotime('-2 day + 1 hour')))
-        );
-        $eventInRangeWithCancelledException->setAllDay(false);
-        $recurrenceInRangeWithException = new Recurrence();
-        $recurrenceInRangeWithException->setRecurrenceType(Recurrence::TYPE_DAILY);
-        $recurrenceInRangeWithException->setInterval(1);
-        $recurrenceInRangeWithException->setOccurrences(2);
-        $recurrenceInRangeWithException->setStartTime($eventInRangeWithCancelledExceptionStart);
-        $eventInRangeWithCancelledException->setRecurrence($recurrenceInRangeWithException);
-        $cancelledException = new CalendarEvent();
-        $cancelledException->setCalendar($calendar);
-        $cancelledException->setTitle('Test cancelled recurring event exception in range');
-        $cancelledException->setStart($eventInRangeWithCancelledExceptionStart);
-        $cancelledException->setEnd(
-            new \DateTime(gmdate(DATE_RFC3339, strtotime('-2 day + 1 hour')))
-        );
-        $cancelledException->setAllDay(false);
-        $cancelledException->setOriginalStart($eventInRangeWithCancelledExceptionStart);
-        $cancelledException->setRecurringEvent($eventInRangeWithCancelledException);
-        $cancelledException->setIsCancelled(true);
-        $eventInRangeWithCancelledException->addRecurringEventException($cancelledException);
-        $manager->persist($eventInRangeWithCancelledException);
-        $this->setReference('eventInRangeWithCancelledException', $eventInRangeWithCancelledException);
+        foreach ($data as $item) {
+            $start = new \DateTime(gmdate(DATE_RFC3339, strtotime($item['start'])));
+            $event = new CalendarEvent();
+            $event->setCalendar($calendar)
+                ->setTitle($item['title'])
+                ->setStart($start)
+                ->setEnd(
+                    new \DateTime(gmdate(DATE_RFC3339, strtotime($item['end'])))
+                )
+                ->setAllDay($item['allDay']);
+            if (!empty($item['recurrence'])) {
+                $recurrence = new Recurrence();
+                $recurrence->setRecurrenceType($item['recurrence']['type'])
+                    ->setInterval($item['recurrence']['interval'])
+                    ->setOccurrences($item['recurrence']['occurrences'])
+                    ->setStartTime(
+                        new \DateTime(gmdate(DATE_RFC3339, strtotime($item['recurrence']['startTime'])))
+                    );
+                $event->setRecurrence($recurrence);
+            }
+            if (!empty($item['exceptions'])) {
+                foreach ($item['exceptions'] as $exceptionItem) {
+                    $exception = new CalendarEvent();
+                    $exception->setCalendar($calendar)
+                        ->setTitle($exceptionItem['title'])
+                        ->setStart(
+                            new \DateTime(gmdate(DATE_RFC3339, strtotime($exceptionItem['start'])))
+                        )
+                        ->setEnd(
+                            new \DateTime(gmdate(DATE_RFC3339, strtotime($exceptionItem['end'])))
+                        )
+                        ->setAllDay($exceptionItem['allDay'])
+                        ->setIsCancelled($exceptionItem['isCancelled'])
+                        ->setOriginalStart($start)
+                        ->setRecurringEvent($event);
+                    $event->addRecurringEventException($exception);
+                }
+            }
+            $manager->persist($event);
+            if (!empty($item['reference'])) {
+                $this->setReference($item['reference'], $event);
+            }
+        }
 
         $manager->flush();
     }
