5936c8e27710e7215fa4174d77cd8de8a6066a4f
Miloslav Nenadal <nenadalm@gmail.com>
2016-05-24 10:21:28 +0200

    BAP-9125: Exception is occurred during upgrade from old version
    - remove invalid configs
    
    # Conflicts:
    #	src/Oro/Bundle/EntityExtendBundle/Migrations/Schema/OroEntityExtendBundleInstaller.php

--- src/Oro/Bundle/EntityExtendBundle/Migrations/Schema/OroEntityExtendBundleInstaller.php
+++ src/Oro/Bundle/EntityExtendBundle/Migrations/Schema/OroEntityExtendBundleInstaller.php
@@ -16,7 +16,7 @@ class OroEntityExtendBundleInstaller extends RenameExtendTablesAndColumns implem
      */
     public function getMigrationVersion()
     {
-        return 'v1_4_2';
+        return 'v1_8';
     }
 
     /**
