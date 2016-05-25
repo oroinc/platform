51a3a55c4f6d32e1e928f7347b0f3a223828d675
Andrii Muzalevskyi <amuzalevsky@magecore.com>
2016-05-25 19:54:32 +0300

    BAP-10528 rename simplified event list to cell event list

---
+++ src/Oro/Bundle/DataGridBundle/Resources/public/js/datagrid/cell-event-list.js
@@ -0,0 +1,41 @@
+define(['underscore', 'backbone'], function(_, Backbone) {
+    'use strict';
+
+    function CellEventList(columns) {
+        this.columns = columns;
+
+        // listener will be removed with columns instance
+        // no need to dispose this class
+        columns.on('change:renderable add remove reset change:columnEventList', function() {
+            delete this.cachedEventList;
+            this.trigger('change');
+        }, this);
+    }
+
+    CellEventList.prototype = {
+        getEventsMap: function() {
+            if (!this.cachedEventList) {
+                var simplifiedCellEvents = {};
+                this.columns.each(function(column) {
+                    if (!column.get('renderable')) {
+                        return;
+                    }
+                    var cellCtor = column.get('cell');
+                    if (cellCtor.prototype.simplifiedEventBinding) {
+                        for (var eventName in cellCtor.prototype.events) { // jshint ignore:line
+                            if (!simplifiedCellEvents.hasOwnProperty(eventName)) {
+                                simplifiedCellEvents[eventName] = true;
+                            }
+                        }
+                    }
+                });
+                this.cachedEventList = simplifiedCellEvents;
+            }
+            return this.cachedEventList;
+        }
+    };
+
+    _.extend(CellEventList.prototype, Backbone.Events);
+
+    return CellEventList;
+});
