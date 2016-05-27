ed8ec4b65f89f0fd4b9c0f4a7e2d208890e5955f
Ivan Shirko <ivan.shirko@effective-soft.com>
2016-05-27 19:29:46 +0300

    BAP-10613: Create Locale Entity - remove setId

--- src/Oro/Bundle/LocaleBundle/Entity/Localization.php
+++ src/Oro/Bundle/LocaleBundle/Entity/Localization.php
@@ -82,18 +82,6 @@ class Localization implements DatesAwareInterface
     }
 
     /**
-     * @param int $id
-     *
-     * @return $this
-     */
-    public function setId($id)
-    {
-        $this->id = $id;
-
-        return $this;
-    }
-
-    /**
      * @return int
      */
     public function getId()
