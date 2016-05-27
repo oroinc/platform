a856fc49b81fae2c715308dc9193810d004dd005
Miloslav Nenadal <miloslav.nenadal@imatic.cz>
2016-05-27 15:50:17 +0200

    AEIV-665: Fix review issues
    - fix uses
    
    Conflicts:
    	src/Oro/Bundle/CalendarBundle/Provider/UserCalendarEventNormalizer.php

--- src/Oro/Bundle/CalendarBundle/Provider/UserCalendarEventNormalizer.php
+++ src/Oro/Bundle/CalendarBundle/Provider/UserCalendarEventNormalizer.php
@@ -7,12 +7,12 @@ use Doctrine\ORM\AbstractQuery;
 use Symfony\Component\PropertyAccess\Exception\InvalidPropertyPathException;
 use Symfony\Component\PropertyAccess\Exception\NoSuchPropertyException;
 
-use Oro\Component\PropertyAccess\PropertyAccessor;
 use Oro\Bundle\CalendarBundle\Entity\CalendarEvent;
 use Oro\Bundle\CalendarBundle\Model\Recurrence;
 use Oro\Bundle\EntityBundle\ORM\DoctrineHelper;
 use Oro\Bundle\ReminderBundle\Entity\Manager\ReminderManager;
 use Oro\Bundle\SecurityBundle\SecurityFacade;
+use Oro\Component\PropertyAccess\PropertyAccessor;
 
 class UserCalendarEventNormalizer extends AbstractCalendarEventNormalizer
 {
