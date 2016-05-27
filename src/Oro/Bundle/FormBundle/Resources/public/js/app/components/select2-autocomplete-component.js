f94dd568d09289bee62ca6c694ddcc05ac4ebfec
Miloslav Nenadal <miloslav.nenadal@imatic.cz>
2016-05-27 11:58:00 +0200

    Revert "BAP-10159: extend select2 component to input new value which not exists in choices"
    This reverts commit ceb69d76905c5093c57ba86edc1172ac07c4407d.
    
    Conflicts:
    	src/Oro/Bundle/FormBundle/Resources/views/Autocomplete/fullName/result.html.twig

--- src/Oro/Bundle/FormBundle/Resources/public/js/app/components/select2-autocomplete-component.js
+++ src/Oro/Bundle/FormBundle/Resources/public/js/app/components/select2-autocomplete-component.js
@@ -6,28 +6,7 @@ define(function(require) {
     var Select2Component = require('oro/select2-component');
 
     Select2AutocompleteComponent = Select2Component.extend({
-        ViewType: Select2AutocompleteView,
-        setConfig: function(config) {
-            config = Select2AutocompleteComponent.__super__.setConfig.apply(this, arguments);
-            var propName = config.propertyNameForNewItem;
-            if (propName) {
-                config.createSearchChoice = function(value, results) {
-                    if (results.length === 0) {
-                        var item = {id: null};
-                        item[propName] = value;
-                        return item;
-                    }
-                };
-                config.id = function(e) {
-                    var val = {id: e.id};
-                    if (val.id === null) {
-                        val.value = e[propName];
-                    }
-                    return JSON.stringify(val);
-                };
-            }
-            return config;
-        }
+        ViewType: Select2AutocompleteView
     });
 
     return Select2AutocompleteComponent;
