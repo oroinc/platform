8d8dea3326afc10a7cb01a98b665bbd7e80d0851
Yurii Muratov <yurio@ua.fm>
2016-05-30 19:13:21 +0300

    BAP-10284: Make FieldAclExtension standalone   - Remove work with root

--- src/Oro/Bundle/SecurityBundle/Acl/Persistence/AclPrivilegeRepository.php
+++ src/Oro/Bundle/SecurityBundle/Acl/Persistence/AclPrivilegeRepository.php
@@ -63,13 +63,12 @@ class AclPrivilegeRepository
     public function getPermissionNames($extensionKeyOrKeys)
     {
         if (is_string($extensionKeyOrKeys)) {
-            return $this->manager->getExtensionSelector()->select($this->manager->getRootOid($extensionKeyOrKeys))
-                ->getPermissions();
+            return $this->manager->getExtensionSelector()->selectByExtensionKey($extensionKeyOrKeys)->getPermissions();
         }
 
         $result = array();
         foreach ($extensionKeyOrKeys as $extensionKey) {
-            $extension = $this->manager->getExtensionSelector()->select($this->manager->getRootOid($extensionKey));
+            $extension = $this->manager->getExtensionSelector()->select($extensionKey);
             foreach ($extension->getPermissions() as $permission) {
                 if (!in_array($permission, $result)) {
                     $result[] = $permission;
