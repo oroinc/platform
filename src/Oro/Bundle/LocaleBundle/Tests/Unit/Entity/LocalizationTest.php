079d7f6848c4fba03c2f431f503cde4ab2d43e29
Yauheni Kasenka <beta.screech@gmail.com>
2016-06-05 17:25:14 +0300

    BAP-10654: Prepare existing code for new Localization Entity - fixed unit tests

--- src/Oro/Bundle/LocaleBundle/Tests/Unit/Entity/LocalizationTest.php
+++ src/Oro/Bundle/LocaleBundle/Tests/Unit/Entity/LocalizationTest.php
@@ -64,7 +64,7 @@ class LocalizationTest extends \PHPUnit_Framework_TestCase
     public function testConstruct()
     {
         $entity = new Localization();
-        $this->assertInstanceOf('Doctrine\Common\Collections\Collection', $entity->getChils());
+        $this->assertInstanceOf('Doctrine\Common\Collections\Collection', $entity->getChilds());
         $this->assertEmpty($entity->getChilds()->toArray());
         $this->assertInstanceOf('Doctrine\Common\Collections\Collection', $entity->getTitles());
         $this->assertEmpty($entity->getTitles()->toArray());
@@ -84,7 +84,7 @@ class LocalizationTest extends \PHPUnit_Framework_TestCase
         $this->assertCount(3, $entity->getTitles()->toArray());
         $this->assertEquals([$defaultTitle, $firstTitle, $secondTitle], array_values($entity->getTitles()->toArray()));
 
-        $this->assertEquals($secondTitle, $entity->getTitle($secondTitle->getLocale()));
+        $this->assertEquals($secondTitle, $entity->getTitle($secondTitle->getLocalization()));
         $this->assertEquals($defaultTitle, $entity->getTitle());
 
         $entity->removeTitle($firstTitle)->removeTitle($firstTitle)->removeTitle($defaultTitle);
@@ -132,19 +132,19 @@ class LocalizationTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @param string $locale
+     * @param string $localization
      * @param bool|false $default
      * @return LocalizedFallbackValue
      */
-    protected function createLocalizedValue($locale, $default = false)
+    protected function createLocalizedValue($localization, $default = false)
     {
         $localized = (new LocalizedFallbackValue())->setString('some string');
 
         if (!$default) {
-            $locale = new Locale();
-            $locale->setTitle($locale);
+            $localization = new Localization();
+            $localization->setDefaultTitle($localization);
 
-            $localized->setLocale($locale);
+            $localized->setLocalization($localization);
         }
 
         return $localized;
