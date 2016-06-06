28c7a43c1a7c81e4a835747e09dd00066f37d5ef
Denis Voronin <dvoronin@magecore.com>
2016-06-06 22:00:38 +0300

    CRM-5271: Manage opportunities in a workflow  - Fix functional tests

--- src/Oro/Bundle/WorkflowBundle/Model/WorkflowPermissionRegistry.php
+++ src/Oro/Bundle/WorkflowBundle/Model/WorkflowPermissionRegistry.php
@@ -48,7 +48,6 @@ class WorkflowPermissionRegistry
         $this->configProvider = $configProvider;
     }
 
-
     /**
      * @param $class
      *
@@ -124,7 +123,9 @@ class WorkflowPermissionRegistry
     {
         $this->loadEntityAcls();
 
-        if (array_key_exists($identifier, $this->entityAcls[$class]['entities'])) {
+        if (isset($this->entityAcls[$class]['entities']) &&
+            array_key_exists($identifier, $this->entityAcls[$class]['entities'])
+        ) {
             return;
         }
 
