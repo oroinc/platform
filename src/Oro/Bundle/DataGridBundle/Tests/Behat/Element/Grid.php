4accbd342b083bb01d750a756907dab8a33dc126
Serhii Polishchuk <spolishchuk@magecore.com>
2016-06-07 14:04:47 +0300

    BAP-10459: Automate mass actions - Created Grid element - Created Pagination element - Added Grid context - Fixed MainMenu element - Automate mass actions

---
+++ src/Oro/Bundle/DataGridBundle/Tests/Behat/Element/Grid.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace Oro\Bundle\DataGridBundle\Tests\Behat\Element;
+
+use Behat\Mink\Element\NodeElement;
+use Behat\Mink\Exception\ExpectationException;
+use Oro\Bundle\TestFrameworkBundle\Behat\Element\Element;
+use WebDriver\Exception\ElementNotVisible;
+
+class Grid extends Element
+{
+    public function clickMassActionLink($title)
+    {
+        $massActionsButton = $this->getMassActionButton();
+        $massActionsButton->press();
+        $massActionLink = $this->getMassActionLink($title);
+        $massActionLink->click();
+    }
+
+    public function getRecordsNumber()
+    {
+        /** @var GridPaginator $paginator */
+        $paginator = $this->elementFactory->createElement('GridPaginator');
+
+        return $paginator->getTotalRecordsCount();
+    }
+
+    public function checkFirstRecords($number)
+    {
+        $rows = $this->getRows();
+
+        if (count($rows) < $number) {
+            throw new ExpectationException(
+                sprintf(
+                    'Can\'t check %s records, because grid has only %s records',
+                    $number,
+                    count($rows)
+                ),
+                $this->session->getDriver()
+            );
+        }
+
+        for ($i = 0; $i < $number; $i++) {
+            /** @var NodeElement $row */
+            $row = $rows[$i];
+            $massActionCell = $row->find('css', '.grid-body-cell-massAction');
+            $massActionCell->click();
+        }
+    }
+
+    /**
+     * @return NodeElement
+     * @throws \Exception
+     */
+    private function getMassActionButton()
+    {
+        $massActionsButton = $this->findButton('Mass Actions');
+        if (!$massActionsButton || !$massActionsButton->isVisible()) {
+            throw ElementNotVisible::factory(
+                ElementNotVisible::ELEMENT_NOT_VISIBLE,
+                'Mass Action dropdown is not prsent or not visible on page'
+            );
+        }
+
+        return $massActionsButton;
+    }
+
+    /**
+     * @param string $title
+     * @return NodeElement
+     * @throws \Exception
+     */
+    private function getMassActionLink($title)
+    {
+        return $this->elementFactory->createElement('GridFloatingMenu')->findLink($title);
+    }
+
+    /**
+     * @param $title
+     * @throws \Exception
+     */
+    public function massCheck($title)
+    {
+        $this->elementFactory->createElement('MassActionHeadCheckbox')->click();
+        $this->elementFactory->createElement('GridFloatingMenu')->clickLink($title);
+    }
+
+    public function selectPageSize($number)
+    {
+        $pageSizeElement = $this->elementFactory->createElement('PageSize');
+        $pageSizeElement->find('css', '.btn')->click();
+        $pageSizeElement->clickLink($number);
+    }
+
+    public function assertNoRecords()
+    {
+        expect($this->getRows())->toHaveCount(0);
+    }
+
+    public function clickActionLink($content, $action)
+    {
+        $row = $this->getRowByContent($content);
+
+        if ($showMoreLink = $row->find('named', ['link', '...'])) {
+            $showMoreLink->mouseOver();
+            $this->elementFactory
+                ->createElement('GridFloatingMenu')
+                ->find('named', ['link', ucfirst($action)])
+                ->click();
+        } else {
+            $row->find('named', ['link', $action])->click();
+        }
+
+    }
+
+    /**
+     * @return \Behat\Mink\Element\NodeElement[]
+     */
+    private function getRows()
+    {
+        return $this->findAll('css', 'tbody tr');
+    }
+
+    /**
+     * @param $content
+     * @return NodeElement
+     * @throws ExpectationException
+     */
+    protected function getRowByContent($content)
+    {
+        $rows = $this->getRows();
+
+        foreach ($rows as $row) {
+            if (false !== strpos($row->getText(), $content)) {
+                return $row;
+            }
+        }
+
+        throw new ExpectationException('Grid has no records', $this->session->getDriver());
+    }
+}
