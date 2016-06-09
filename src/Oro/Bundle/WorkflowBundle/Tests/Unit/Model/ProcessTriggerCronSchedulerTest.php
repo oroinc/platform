8e8c6a87926c841d819062732944ea2fafe657fd
Volodymyr Myrza <vmyrza@magecore.com>
2016-06-09 11:17:33 +0300

    BAP-10468 : Implement sync mechanism for cases with update or delete process   - more coverage

--- src/Oro/Bundle/WorkflowBundle/Tests/Unit/Model/ProcessTriggerCronSchedulerTest.php
+++ src/Oro/Bundle/WorkflowBundle/Tests/Unit/Model/ProcessTriggerCronSchedulerTest.php
@@ -10,9 +10,11 @@ use Oro\Bundle\CronBundle\Entity\Schedule;
 use Oro\Bundle\WorkflowBundle\Command\HandleProcessTriggerCommand;
 use Oro\Bundle\WorkflowBundle\Entity\ProcessTrigger;
 use Oro\Bundle\WorkflowBundle\Model\ProcessTriggerCronScheduler;
+use Oro\Component\Testing\Unit\EntityTrait;
 
 class ProcessTriggerCronSchedulerTest extends \PHPUnit_Framework_TestCase
 {
+    use EntityTrait;
     /** @var ScheduleManager|\PHPUnit_Framework_MockObject_MockObject */
     protected $scheduleManager;
 
@@ -38,7 +40,8 @@ class ProcessTriggerCronSchedulerTest extends \PHPUnit_Framework_TestCase
 
         $this->scheduleClass = 'Oro\Bundle\CronBundle\Entity\Schedule';
         $this->objectManager = $this->getMock('Doctrine\Common\Persistence\ObjectManager');
-        $this->registry->expects($this->any())->method('getManagerForClass')->willReturn($this->objectManager);
+
+        //$this->registry->expects($this->any())->method('getManagerForClass')->willReturn($this->objectManager);
 
         $this->processCronScheduler = new ProcessTriggerCronScheduler(
             $this->scheduleManager,
@@ -83,6 +86,8 @@ class ProcessTriggerCronSchedulerTest extends \PHPUnit_Framework_TestCase
             ->method('createSchedule')
             ->with(HandleProcessTriggerCommand::NAME, $arguments, $cronExpression)
             ->willReturn($scheduleEntity);
+
+        $this->registry->expects($this->once())->method('getManagerForClass')->willReturn($this->objectManager);
         $this->objectManager->expects($this->once())->method('persist')->with($scheduleEntity);
 
         $this->processCronScheduler->add($trigger);
@@ -124,6 +129,7 @@ class ProcessTriggerCronSchedulerTest extends \PHPUnit_Framework_TestCase
             ->with($foundMatchedSchedule)
             ->willReturn(true);
 
+        $this->registry->expects($this->any())->method('getManagerForClass')->willReturn($this->objectManager);
         $this->objectManager->expects($this->once())->method('remove')->with($foundMatchedSchedule);
 
         $this->processCronScheduler->removeSchedule($mockTrigger);
@@ -153,5 +159,18 @@ class ProcessTriggerCronSchedulerTest extends \PHPUnit_Framework_TestCase
 
         $this->setExpectedException('InvalidArgumentException');
         $this->processCronScheduler->add($trigger);
+    }
+
+    public function testUnmanageableEntityException(){
+        $this->setValue($this->processCronScheduler,'dirty', true);
+
+        $this->registry->expects($this->once())
+            ->method('getManagerForClass')
+            ->with($this->scheduleClass)
+            ->willReturn(null);
+
+        $this->setExpectedException('InvalidArgumentException', 'Please provide manageable schedule entity class');
+
+        $this->processCronScheduler->flush();
     }
 }
