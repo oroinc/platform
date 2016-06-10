2f7f89c0c047215ac4c61974abf0ad122e033569
Maxym Perepelytsya <mperepelytsya@magecore.com>
2016-06-10 18:10:26 +0300

    CRM-5609: Add possibility to create new opportunities through import/export with creating new account/contact/customer  - reuse created and not persisted entities with same identifiers fields values set

--- src/Oro/Bundle/ImportExportBundle/Strategy/Import/ConfigurableAddOrReplaceStrategy.php
+++ src/Oro/Bundle/ImportExportBundle/Strategy/Import/ConfigurableAddOrReplaceStrategy.php
@@ -15,6 +15,8 @@ use Oro\Bundle\ImportExportBundle\Field\DatabaseHelper;
 
 class ConfigurableAddOrReplaceStrategy extends AbstractImportStrategy
 {
+    const STRATEGY_CONTEXT = 'configurable_add_or_replace_strategy';
+
     /** @var ChainEntityClassNameProvider */
     protected $chainEntityClassNameProvider;
 
@@ -22,15 +24,19 @@ class ConfigurableAddOrReplaceStrategy extends AbstractImportStrategy
     protected $translator;
 
     /** @var array */
-    protected $cachedEntities = array();
+    protected $cachedEntities = [];
+
+    /** @var NewEntitiesHelper */
+    protected $newEntitiesHelper;
 
     /**
-     * @param EventDispatcherInterface $eventDispatcher
-     * @param ImportStrategyHelper $strategyHelper
-     * @param FieldHelper $fieldHelper
-     * @param DatabaseHelper $databaseHelper
+     * @param EventDispatcherInterface     $eventDispatcher
+     * @param ImportStrategyHelper         $strategyHelper
+     * @param FieldHelper                  $fieldHelper
+     * @param DatabaseHelper               $databaseHelper
      * @param ChainEntityClassNameProvider $chainEntityClassNameProvider
-     * @param TranslatorInterface $translator
+     * @param TranslatorInterface          $translator
+     * @param NewEntitiesHelper            $newEntitiesHelper
      */
     public function __construct(
         EventDispatcherInterface $eventDispatcher,
@@ -38,11 +44,13 @@ class ConfigurableAddOrReplaceStrategy extends AbstractImportStrategy
         FieldHelper $fieldHelper,
         DatabaseHelper $databaseHelper,
         ChainEntityClassNameProvider $chainEntityClassNameProvider,
-        TranslatorInterface $translator
+        TranslatorInterface $translator,
+        NewEntitiesHelper $newEntitiesHelper
     ) {
         parent::__construct($eventDispatcher, $strategyHelper, $fieldHelper, $databaseHelper);
         $this->chainEntityClassNameProvider = $chainEntityClassNameProvider;
-        $this->translator = $translator;
+        $this->translator                   = $translator;
+        $this->newEntitiesHelper            = $newEntitiesHelper;
     }
 
 
@@ -53,7 +61,7 @@ class ConfigurableAddOrReplaceStrategy extends AbstractImportStrategy
     {
         $this->assertEnvironment($entity);
 
-        $this->cachedEntities = array();
+        $this->cachedEntities = [];
 
         if (!$entity = $this->beforeProcessEntity($entity)) {
             return null;
@@ -71,12 +79,12 @@ class ConfigurableAddOrReplaceStrategy extends AbstractImportStrategy
     }
 
     /**
-     * @param object $entity
-     * @param bool   $isFullData
-     * @param bool   $isPersistNew
+     * @param object           $entity
+     * @param bool             $isFullData
+     * @param bool             $isPersistNew
      * @param mixed|array|null $itemData
-     * @param array $searchContext
-     * @param bool $entityIsRelation
+     * @param array            $searchContext
+     * @param bool             $entityIsRelation
      *
      * @return null|object
      */
@@ -85,7 +93,7 @@ class ConfigurableAddOrReplaceStrategy extends AbstractImportStrategy
         $isFullData = false,
         $isPersistNew = false,
         $itemData = null,
-        array $searchContext = array(),
+        array $searchContext = [],
         $entityIsRelation = false
     ) {
         $oid = spl_object_hash($entity);
@@ -105,7 +113,7 @@ class ConfigurableAddOrReplaceStrategy extends AbstractImportStrategy
             // if can't find entity and new entity can't be persisted
             if (!$isPersistNew) {
                 if ($entityIsRelation) {
-                    $class = $this->chainEntityClassNameProvider->getEntityClassName(ClassUtils::getClass($entity));
+                    $class         = $this->chainEntityClassNameProvider->getEntityClassName(ClassUtils::getClass($entity));
                     $errorMessages = [$this->translator->trans(
                         'oro.importexport.import.errors.not_found_entity',
                         ['%entity_name%' => $class]
@@ -138,21 +146,21 @@ class ConfigurableAddOrReplaceStrategy extends AbstractImportStrategy
     }
 
     /**
-     * @param object $entity
-     * @param object $existingEntity
+     * @param object           $entity
+     * @param object           $existingEntity
      * @param mixed|array|null $itemData
-     * @param array $excludedFields
+     * @param array            $excludedFields
      */
     protected function importExistingEntity(
         $entity,
         $existingEntity,
         $itemData = null,
-        array $excludedFields = array()
+        array $excludedFields = []
     ) {
-        $entityName = ClassUtils::getClass($entity);
-        $identifierName = $this->databaseHelper->getIdentifierFieldName($entityName);
+        $entityName       = ClassUtils::getClass($entity);
+        $identifierName   = $this->databaseHelper->getIdentifierFieldName($entityName);
         $excludedFields[] = $identifierName;
-        $fields = $this->fieldHelper->getFields($entityName, true);
+        $fields           = $this->fieldHelper->getFields($entityName, true);
 
         foreach ($fields as $key => $field) {
             $fieldName = $field['name'];
@@ -168,9 +176,10 @@ class ConfigurableAddOrReplaceStrategy extends AbstractImportStrategy
     /**
      * Exclude fields marked as "excluded" and skipped not identity fields
      *
-     * @param string $entityName
-     * @param string $fieldName
+     * @param string           $entityName
+     * @param string           $fieldName
      * @param array|mixed|null $itemData
+     *
      * @return bool
      */
     protected function isFieldExcluded($entityName, $fieldName, $itemData = null)
@@ -183,23 +192,23 @@ class ConfigurableAddOrReplaceStrategy extends AbstractImportStrategy
     }
 
     /**
-     * @param object $entity
+     * @param object     $entity
      * @param array|null $itemData
      */
     protected function updateRelations($entity, array $itemData = null)
     {
         $entityName = ClassUtils::getClass($entity);
-        $fields = $this->fieldHelper->getFields($entityName, true);
+        $fields     = $this->fieldHelper->getFields($entityName, true);
 
         foreach ($fields as $field) {
             if ($this->fieldHelper->isRelation($field)) {
-                $fieldName = $field['name'];
-                $isFullRelation = $this->fieldHelper->getConfigValue($entityName, $fieldName, 'full', false);
+                $fieldName         = $field['name'];
+                $isFullRelation    = $this->fieldHelper->getConfigValue($entityName, $fieldName, 'full', false);
                 $isPersistRelation = $this->databaseHelper->isCascadePersist($entityName, $fieldName);
                 $inversedFieldName = $this->databaseHelper->getInversedRelationFieldName($entityName, $fieldName);
 
                 // additional search parameters to find only related entities
-                $searchContext = array();
+                $searchContext = [];
                 if ($isPersistRelation && $inversedFieldName
                     && $this->databaseHelper->isSingleInversedRelation($entityName, $fieldName)
                 ) {
@@ -211,7 +220,7 @@ class ConfigurableAddOrReplaceStrategy extends AbstractImportStrategy
                     $relationEntity = $this->fieldHelper->getObjectValue($entity, $fieldName);
                     if ($relationEntity) {
                         $relationItemData = $this->fieldHelper->getItemData($itemData, $fieldName);
-                        $relationEntity = $this->processEntity(
+                        $relationEntity   = $this->processEntity(
                             $relationEntity,
                             $isFullRelation,
                             $isPersistRelation,
@@ -229,7 +238,7 @@ class ConfigurableAddOrReplaceStrategy extends AbstractImportStrategy
                         $collectionEntities = new ArrayCollection();
 
                         foreach ($relationCollection as $collectionEntity) {
-                            $entityItemData = $this->fieldHelper->getItemData(array_shift($collectionItemData));
+                            $entityItemData   = $this->fieldHelper->getItemData(array_shift($collectionItemData));
                             $collectionEntity = $this->processEntity(
                                 $collectionEntity,
                                 $isFullRelation,
@@ -249,10 +258,12 @@ class ConfigurableAddOrReplaceStrategy extends AbstractImportStrategy
                 }
             }
         }
+
     }
 
     /**
      * @param object $entity
+     *
      * @return null|object
      */
     protected function validateAndUpdateContext($entity)
@@ -278,11 +289,73 @@ class ConfigurableAddOrReplaceStrategy extends AbstractImportStrategy
      */
     protected function updateContextCounters($entity)
     {
-        $identifier = $this->databaseHelper->getIdentifier($entity);
-        if ($identifier) {
+        $identifier  = $this->databaseHelper->getIdentifier($entity);
+        $entityUsage = $this->newEntitiesHelper->getEntityUsage($this->getEntityHashKey($entity));
+        if ($identifier || $entityUsage > 1) {
             $this->context->incrementReplaceCount();
         } else {
             $this->context->incrementAddCount();
         }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function findExistingEntityByIdentityFields($entity, array $searchContext = [])
+    {
+        $entityName     = ClassUtils::getClass($entity);
+        $identityValues = $searchContext;
+        $identityValues += $this->fieldHelper->getIdentityValues($entity);
+        $notEmptyValues     = [];
+        $nullRequiredValues = [];
+        foreach ($identityValues as $fieldName => $value) {
+            if (null !== $value) {
+                if ('' !== $value) {
+                    $notEmptyValues[$fieldName] = $value;
+                }
+            } elseif ($this->fieldHelper->isRequiredIdentityField($entityName, $fieldName)) {
+                $nullRequiredValues[$fieldName] = null;
+            }
+        }
+        $existingEntity = null;
+        // if there are not empty identity values existing entity will be searched
+        // by these not empty values with required identity fields which have null value
+        // if existing entity will not be found in db currently processing will be stored to prevent duplicating
+        if (!empty($notEmptyValues)) {
+            $identityValues = array_merge($notEmptyValues, $nullRequiredValues);
+            $existingEntity = $this->findEntityByIdentityValues($entityName, $identityValues);
+            if (!$existingEntity) {
+                $newEntityKey   = sprintf('%s:%s', $entityName, serialize($identityValues));
+                $existingEntity = $this->newEntitiesHelper->getEntity($newEntityKey);
+                if (null === $existingEntity) {
+                    $this->newEntitiesHelper->setEntity($newEntityKey, $entity);
+                    $this->newEntitiesHelper->incrementEntityUsage($this->getEntityHashKey($entity));
+                } else {
+                    $this->newEntitiesHelper->incrementEntityUsage($this->getEntityHashKey($existingEntity));
+                }
+            }
+        }
+
+        return $existingEntity;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function findEntityByIdentityValues($entityName, array $identityValues)
+    {
+        return $this->databaseHelper->findOneBy($entityName, $identityValues);
+    }
+
+    /**
+     * @param object $entity
+     *
+     * @return string
+     */
+    protected function getEntityHashKey($entity)
+    {
+        $hashKey = self::STRATEGY_CONTEXT . spl_object_hash($entity);
+
+        return $hashKey;
     }
 }
---
+++ src/Oro/Bundle/WorkflowBundle/Tests/Unit/Entity/WorkflowRestrictionTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Oro\Bundle\WorkflowBundle\Tests\Unit\Entity;
+
+use Oro\Bundle\WorkflowBundle\Entity\WorkflowRestriction;
+use Symfony\Component\PropertyAccess\PropertyAccess;
+
+use Oro\Bundle\WorkflowBundle\Entity\WorkflowDefinition;
+use Oro\Bundle\WorkflowBundle\Entity\WorkflowStep;
+
+class WorkflowRestrictionTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider propertiesDataProvider
+     *
+     * @param string $property
+     * @param mixed  $value
+     */
+    public function testSettersAndGetters($property, $value)
+    {
+        $restriction = new WorkflowRestriction();
+        $accessor = PropertyAccess::createPropertyAccessor();
+        $accessor->setValue($restriction, $property, $value);
+        $this->assertEquals($value, $accessor->getValue($restriction, $property));
+    }
+
+    public function propertiesDataProvider()
+    {
+        return [
+            ['attribute', 'test'],
+            ['step', new WorkflowStep()],
+            ['definition', new WorkflowDefinition()],
+            ['entityClass', 'TestEntity'],
+            ['field', 'test'],
+            ['mode', 'full'],
+            ['values', []],
+        ];
+    }
+
+    public function testImport()
+    {
+        $restriction = new WorkflowRestriction();
+        $step = new WorkflowStep();
+        $step->setName('step');
+        $definition = new WorkflowDefinition();
+        $definition->addStep($step);
+
+        $restriction->setAttribute('attribute');
+        $restriction->setStep($step);
+        $restriction->setEntityClass('TestEntity');
+        $restriction->setField('test');
+        $restriction->setMode('allow');
+        $restriction->setValues(['1']);
+        $newRestriction = new WorkflowRestriction();
+        $newRestriction->setDefinition($definition);
+        $this->assertEquals($newRestriction, $newRestriction->import($restriction));
+
+        $this->assertEquals('attribute', $newRestriction->getAttribute());
+        $this->assertEquals($step, $newRestriction->getStep());
+        $this->assertEquals('TestEntity', $newRestriction->getEntityClass());
+        $this->assertEquals('test', $newRestriction->getField());
+        $this->assertEquals('allow', $newRestriction->getMode());
+        $this->assertEquals(['1'], $newRestriction->getValues());
+
+        $this->assertEquals($restriction->getHashKey(), $newRestriction->getHashKey());
+    }
+}
