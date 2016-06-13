26ddea432b0421743c692f35865a66bb7de01ea0
Vova Soroka <vsoroka@magecore.com>
2016-06-13 05:34:43 +0300

    BAP-9791: Support API resources for related entities. Update InitializeConfigExtras processors for add_relationship, delete_relationship and update_relationship actions

--- src/Oro/Bundle/ApiBundle/Processor/Subresource/UpdateRelationship/InitializeConfigExtras.php
+++ src/Oro/Bundle/ApiBundle/Processor/Subresource/UpdateRelationship/InitializeConfigExtras.php
@@ -5,6 +5,7 @@ namespace Oro\Bundle\ApiBundle\Processor\Subresource\UpdateRelationship;
 use Oro\Component\ChainProcessor\ContextInterface;
 use Oro\Component\ChainProcessor\ProcessorInterface;
 use Oro\Bundle\ApiBundle\Config\EntityDefinitionConfigExtra;
+use Oro\Bundle\ApiBundle\Config\FilterFieldsConfigExtra;
 use Oro\Bundle\ApiBundle\Processor\Subresource\SubresourceContext;
 
 /**
@@ -20,5 +21,6 @@ class InitializeConfigExtras implements ProcessorInterface
         /** @var SubresourceContext $context */
 
         $context->addConfigExtra(new EntityDefinitionConfigExtra($context->getAction()));
+        $context->addConfigExtra(new FilterFieldsConfigExtra([$context->getClassName() => ['id']]));
     }
 }
