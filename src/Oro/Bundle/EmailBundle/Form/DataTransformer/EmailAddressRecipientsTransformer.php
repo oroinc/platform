bcb3eeedb7305413a63c0628f9fed9b9949ee8a7
Andrey Agafonkin <aagafonkin@magecore.com>
2016-06-13 15:47:37 +0300

    CRM-5529: Recipient names which contain a comma may be perceived as two different recipients

--- src/Oro/Bundle/EmailBundle/Form/DataTransformer/EmailAddressRecipientsTransformer.php
+++ src/Oro/Bundle/EmailBundle/Form/DataTransformer/EmailAddressRecipientsTransformer.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of the GenemuFormBundle package.
- *
- * (c) Olivier Chauvel <olivier@generation-multiple.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Oro\Bundle\EmailBundle\Form\DataTransformer;
 
 use Symfony\Component\Form\DataTransformerInterface;
