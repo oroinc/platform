2ef0ff0fadb2851c89eb422c425fbbb99ed30b36
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2016-06-14 14:57:58 +0300

    BAP-7786: Integration with message queue
    - fix cs

--- src/Oro/Component/Testing/ClassExtensionTrait.php
+++ src/Oro/Component/Testing/ClassExtensionTrait.php
@@ -7,13 +7,19 @@ trait ClassExtensionTrait
     {
         $rc = new \ReflectionClass($actual);
 
-        $this->assertTrue($rc->isSubclassOf($expected), sprintf('Failed assert that class %s extends %s class.', $actual, $expected));
+        $this->assertTrue(
+            $rc->isSubclassOf($expected),
+            sprintf('Failed assert that class %s extends %s class.', $actual, $expected)
+        );
     }
 
     public function assertClassImplements($expected, $actual)
     {
         $rc = new \ReflectionClass($actual);
 
-        $this->assertTrue($rc->implementsInterface($expected), sprintf('Failed assert that class %s implements %s interface.', $actual, $expected));
+        $this->assertTrue(
+            $rc->implementsInterface($expected),
+            sprintf('Failed assert that class %s implements %s interface.', $actual, $expected)
+        );
     }
 }
