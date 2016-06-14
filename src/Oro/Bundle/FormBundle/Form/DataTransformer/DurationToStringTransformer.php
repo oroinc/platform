5b387541228686ad730df197996675a842079a6c
Ivaylo Badinov <venimus@users.noreply.github.com>
2016-06-14 15:15:46 +0300

    BAP-6927: Reduce NPATH complexity
    Improve PhpDoc

--- src/Oro/Bundle/FormBundle/Form/DataTransformer/DurationToStringTransformer.php
+++ src/Oro/Bundle/FormBundle/Form/DataTransformer/DurationToStringTransformer.php
@@ -8,10 +8,11 @@ use Symfony\Component\Form\Exception\UnexpectedTypeException;
 
 /**
  * Transforms between time duration in seconds (scalar) and duration encoded string.
- * Straight transform will generate JIRA style encoded string (0h 0m 0s)
+ * Straight transform converts a scalar (seconds) into a JIRA style encoded string (1h 2m 3s)
  * with zero values omitted (e.g. 3600 => 1h)
  *
- * Reverse transform supports:
+ * Reverse transform converts an encoded time string to an integer (seconds). Result is rounded up.
+ * Supported encodings:
  * - JIRA style encoding (0.0h 0.0m 0.0s).
  *   Supports fractions (with dot delimiter) to the first digit.
  *   Result is rounded to the second. Any part could be omitted.
@@ -20,11 +21,12 @@ use Symfony\Component\Form\Exception\UnexpectedTypeException;
  *   Parts are converted to int, thus trailing non-digits are trimmed, while leading will
  *   result to 0's (1a:b2:3.5m => 1:0:3). Missing leading zeros are also valid (:1: => 0:1:0).
  * In both styles time parts are cumulative, so '1m 120s' (or '1:120') becomes 3 min.
- * Returns integer representing duration in seconds
  */
 class DurationToStringTransformer implements DataTransformerInterface
 {
-    /** {@inheritdoc} */
+    /**
+     * {@inheritdoc}
+     */
     public function transform($value)
     {
         if (null === $value) {
@@ -35,34 +37,22 @@ class DurationToStringTransformer implements DataTransformerInterface
             throw new UnexpectedTypeException($value, 'scalar');
         }
 
-        $encoded = [];
+        // create \DateInterval from integer
         try {
             $dateInterval = new \DateInterval('PT' . (int) $value . 'S');
             // since \DateInterval does not handle carryovers, we need to use \DateTime::diff
-            $dateTime = new \DateTime();
-            $dateTimeDiff = clone $dateTime;
-            $timeInterval = $dateTimeDiff->diff($dateTime->add($dateInterval));
+            $dateTime = new \DateTimeImmutable();
+            $interval = $dateTime->diff($dateTime->add($dateInterval));
         } catch (\Exception $e) {
             throw new TransformationFailedException('Duration too long to convert.');
         }
 
-        $hours = $timeInterval->days * 24 + $timeInterval->h;
-        $minutes = $timeInterval->i;
-        $seconds = $timeInterval->s;
-        if ($hours) {
-            $encoded[] = $hours . 'h';
-        }
-        if ($minutes) {
-            $encoded[] = $minutes . 'm';
-        }
-        if ($seconds) {
-            $encoded[] = $seconds . 's';
-        }
-
-        return empty($encoded) ? '0s' : join(' ', $encoded);
+        return $this->dateIntervalToString($interval);
     }
 
-    /** {@inheritdoc} */
+    /**
+     * {@inheritdoc}
+     */
     public function reverseTransform($value)
     {
         if (null === $value || (is_string($value) && '' === trim($value))) {
@@ -113,5 +103,32 @@ class DurationToStringTransformer implements DataTransformerInterface
             'm' => (int)$parts[1],
             's' => (int)$parts[2],
         ];
+    }
+
+    /**
+     * Convert \DateInterval to JIRA style encoded time string (1h 2m 3s). Zero values are omitted.
+     *
+     * @param \DateInterval $dateInterval
+     *
+     * @return string
+     */
+    private function dateIntervalToString(\DateInterval $dateInterval)
+    {
+        $encoded = [];
+        $hours = $dateInterval->days * 24 + $dateInterval->h;
+        $minutes = $dateInterval->i;
+        $seconds = $dateInterval->s;
+
+        if ($hours) {
+            $encoded[] = $hours . 'h';
+        }
+        if ($minutes) {
+            $encoded[] = $minutes . 'm';
+        }
+        if ($seconds) {
+            $encoded[] = $seconds . 's';
+        }
+
+        return empty($encoded) ? '0s' : join(' ', $encoded);
     }
 }
