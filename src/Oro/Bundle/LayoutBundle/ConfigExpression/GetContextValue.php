536c15c7afa84587e4504d3b1af0fd3c8a39751b
anyt <yatsenco@gmail.com>
2016-06-16 17:47:34 +0300

    BB-3508: Create GetContextValue expression, in layout update named @context - oro_layout.config_expression.asset moved to config_expressions.yml - removed oro_layout.expression.pass.property_path - added GetContextValue expression

---
+++ src/Oro/Bundle/LayoutBundle/ConfigExpression/GetContextValue.php
@@ -0,0 +1,105 @@
+<?php
+
+
+namespace Oro\Bundle\LayoutBundle\ConfigExpression;
+
+use Oro\Component\ConfigExpression\Exception;
+use Oro\Component\ConfigExpression\Func\AbstractFunction;
+use Symfony\Component\PropertyAccess\PropertyPath;
+
+class GetContextValue extends AbstractFunction
+{
+    const PROPERTY_PATH_PREFIX = 'context.';
+
+    /** @var mixed */
+    protected $value;
+
+    /** @var mixed */
+    protected $default;
+
+    /** @var bool */
+    protected $hasDefault = false;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'context';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function toArray()
+    {
+        $params = [$this->value];
+        if ($this->hasDefault) {
+            $params[] = $this->default;
+        }
+
+        return $this->convertToArray($params);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function compile($factoryAccessor)
+    {
+        $params = [$this->value];
+        if ($this->hasDefault) {
+            $params[] = $this->default;
+        }
+
+        return $this->convertToPhpCode($params, $factoryAccessor);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function initialize(array $options)
+    {
+        $count = count($options);
+        if ($count >= 1 && $count <= 2) {
+            $this->value = reset($options);
+            if ($count > 1) {
+                $this->default = next($options);
+                $this->hasDefault = true;
+            }
+        } else {
+            throw new Exception\InvalidArgumentException(
+                sprintf('Options must have 1 or 2 elements, but %d given.', count($options))
+            );
+        }
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function doEvaluate($context)
+    {
+        if ($this->hasDefault) {
+            $value = $this->resolveValue($context, $this->createPropertyPath($this->value), false);
+
+            return null !== $value
+                ? $value
+                : $this->resolveValue($context, $this->createPropertyPath($this->default));
+        }
+
+        return $this->resolveValue($context, $this->createPropertyPath($this->value));
+    }
+
+    /**
+     * todo create paths cache, see \Oro\Component\ConfigExpression\ConfigurationPass\ReplacePropertyPath
+     *
+     * @param string $value
+     *
+     * @return PropertyPath
+     */
+    protected function createPropertyPath($value)
+    {
+        return new PropertyPath(self::PROPERTY_PATH_PREFIX.$value);
+    }
+}
