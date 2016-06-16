9a9cf964f38cb5679d4ea54bb49e363ce0d0a387
Makar Sichevoy <sichevoy@gmail.com>
2016-06-16 18:22:46 +0300

    OEE-779: Incorrect behavior of Custom and some Oro entities on Outlook sidebar panel

--- src/Oro/Bundle/EmailBundle/Controller/Api/Rest/EmailActivitySearchController.php
+++ src/Oro/Bundle/EmailBundle/Controller/Api/Rest/EmailActivitySearchController.php
@@ -107,10 +107,10 @@ class EmailActivitySearchController extends RestGetController
         $data = $this->getManager()->getSearchResult($limit, $page, $filters);
         foreach ($data['result'] as &$item) {
             $metadata = $this->get('oro_entity_config.config_manager')->getEntityMetadata($item['entity']);
-            if ($metadata && $metadata->hasRoute()) {
-                $item['url_view'] = $this->get('router')->generate($metadata->getRoute(), ['id' => $item['id']]);
+            if ($metadata) {
+                $item['urlView'] = $this->get('router')->generate($metadata->getRoute(), ['id' => $item['id']]);
             } else {
-                $item['url_view'] = '';
+                $item['urlView'] = '';
             }
         }
 
