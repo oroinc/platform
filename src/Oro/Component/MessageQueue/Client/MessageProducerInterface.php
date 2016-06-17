956622642d8b9d220e284597cef6104b2c5eb32f
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2016-06-17 11:00:30 +0300

    BAP-7786: Integration with message queue
    - correct MessageProducerInterface::send docblock.

--- src/Oro/Component/MessageQueue/Client/MessageProducerInterface.php
+++ src/Oro/Component/MessageQueue/Client/MessageProducerInterface.php
@@ -4,13 +4,11 @@ namespace Oro\Component\MessageQueue\Client;
 interface MessageProducerInterface
 {
     /**
-     * Sends a message to a topic channel.
-     * Body could be a string or array, in case of array it will be json encoded.
-     * For possible priority values @see MessagePriority constants
+     * Sends a message to a topic. There are some message processor may be subscribed to a topic.
      *
      * @param string $topic
-     * @param string|array $message
-     * @param string $priority
+     * @param string|array $message The message could be a string or array, in case of array it will be json encoded.
+     * @param string $priority Available priorities could be found in MessagePriority class.
      */
     public function send($topic, $message, $priority = MessagePriority::NORMAL);
 }
