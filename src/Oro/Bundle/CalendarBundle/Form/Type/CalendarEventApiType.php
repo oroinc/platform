2260895afc2d7e2770d8e255ad3142c28d6392eb
Ievgenii Myshusta <imyshusta@magecore.com>
2016-06-17 18:28:12 +0300

    AEIV-717: Cascade validation for recurrence

--- src/Oro/Bundle/CalendarBundle/Form/Type/CalendarEventApiType.php
+++ src/Oro/Bundle/CalendarBundle/Form/Type/CalendarEventApiType.php
@@ -187,7 +187,6 @@ class CalendarEventApiType extends CalendarEventType
                 'data_class'           => 'Oro\Bundle\CalendarBundle\Entity\CalendarEvent',
                 'intention'            => 'calendar_event',
                 'csrf_protection'      => false,
-                'cascade_validation'   => true,
                 'extra_fields_message' => 'This form should not contain extra fields: "{{ extra_fields }}"',
             )
         );
