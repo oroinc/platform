e3ac8ef30b2fbe290e76829acf6f05e154d5e13f
Vova Soroka <vsoroka@magecore.com>
2016-06-18 07:21:16 +0300

    BAP-9791: Support API resources for related entities. Functional tests for subresources API

--- src/Oro/Bundle/ApiBundle/Tests/Functional/ApiTestCase.php
+++ src/Oro/Bundle/ApiBundle/Tests/Functional/ApiTestCase.php
@@ -2,6 +2,8 @@
 
 namespace Oro\Bundle\ApiBundle\Tests\Functional;
 
+use Doctrine\ORM\EntityManager;
+
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Yaml\Parser;
@@ -75,19 +77,99 @@ abstract class ApiTestCase extends WebTestCase
     public function getEntities()
     {
         $this->initClient();
-        $entities = [];
-        $container = $this->getContainer();
-        $resourcesProvider = $container->get('oro_api.resources_provider');
+
+        $result = [];
+        $resourcesProvider = $this->getContainer()->get('oro_api.resources_provider');
         $resources = $resourcesProvider->getResources(Version::LATEST, $this->getRequestType());
         foreach ($resources as $resource) {
             $entityClass = $resource->getEntityClass();
-
-            $entities[$entityClass] = [$entityClass, $resource->getExcludedActions()];
+            $result[$entityClass] = [$entityClass, $resource->getExcludedActions()];
         }
 
-        return $entities;
+        return $result;
     }
 
+    /**
+     * @return array [[entity class, association name, ApiSubresource], ...]
+     */
+    public function getSubresources()
+    {
+        $this->initClient();
+
+        $result = [];
+        $resourcesProvider = $this->getContainer()->get('oro_api.resources_provider');
+        $subresourcesProvider = $this->getContainer()->get('oro_api.subresources_provider');
+        $resources = $resourcesProvider->getResources(Version::LATEST, $this->getRequestType());
+        foreach ($resources as $resource) {
+            $entityClass = $resource->getEntityClass();
+            $subresources = $subresourcesProvider->getSubresources(
+                $entityClass,
+                Version::LATEST,
+                $this->getRequestType()
+            );
+            if (null !== $subresources) {
+                foreach ($subresources->getSubresources() as $associationName => $subresource) {
+                    $result[] = [$entityClass, $associationName, $subresource];
+                }
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param string $entityClass
+     *
+     * @return mixed
+     */
+    protected function findEntityId($entityClass)
+    {
+        /** @var EntityManager|null $em */
+        $em = $this->getContainer()->get('doctrine')->getManagerForClass($entityClass);
+        if (!$em) {
+            return null;
+        }
+
+        $ids = $em->getRepository($entityClass)->createQueryBuilder('e')
+            ->select(
+                implode(
+                    ',',
+                    array_map(
+                        function ($fieldName) {
+                            return 'e.' . $fieldName;
+                        },
+                        $em->getClassMetadata($entityClass)->getIdentifierFieldNames()
+                    )
+                )
+            )
+            ->setMaxResults(1)
+            ->getQuery()
+            ->getArrayResult();
+        if (empty($ids)) {
+            return null;
+        }
+
+        $ids = reset($ids);
+
+        return 1 === count($ids)
+            ? reset($ids)
+            : $ids;
+    }
+
+    /**
+     * @param mixed $entityId
+     *
+     * @return string|null
+     */
+    protected function getRestApiEntityId($entityId)
+    {
+        if (null === $entityId) {
+            return null;
+        }
+
+        return $this->getContainer()->get('oro_api.rest.entity_id_transformer')
+            ->transform($entityId);
+    }
     /**
      * @param string $filename
      *
