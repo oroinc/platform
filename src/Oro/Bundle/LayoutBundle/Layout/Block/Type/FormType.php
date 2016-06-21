8d1aada4cc93f79908d93db2fb198911977d8dab
Oleg Popadko <oleg@popadko.com>
2016-06-21 17:10:37 +0300

    BB-2888: Create new block type FormType  - fixing setDefined options for Form block

--- src/Oro/Bundle/LayoutBundle/Layout/Block/Type/FormType.php
+++ src/Oro/Bundle/LayoutBundle/Layout/Block/Type/FormType.php
@@ -17,26 +17,23 @@ class FormType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefault('form_name', 'form');
-        $resolver->setDefined(
-            [
-                'form'                  => null,
-                'form_action'           => null,
-                'form_route_name'       => null,
-                'form_route_parameters' => null,
-                'form_method'           => null,
-                'form_enctype'          => null,
-                'form_data'             => null,
-                'form_prefix'           => null,
-                'form_field_prefix'     => null,
-                'form_group_prefix'     => null,
-                'render_rest'           => null,
-                'preferred_fields'      => null,
-                'groups'                => null,
-                'split_to_fields'       => null,
-            ]
-        );
+        $resolver->setDefined([
+            'form',
+            'form_action',
+            'form_route_name',
+            'form_route_parameters',
+            'form_method',
+            'form_enctype',
+            'form_data',
+            'form_prefix',
+            'form_field_prefix',
+            'form_group_prefix',
+            'render_rest',
+            'preferred_fields',
+            'groups',
+            'split_to_fields',
+        ]);
     }
-
 
     /**
      * {@inheritdoc}
--- src/Oro/Bundle/LayoutBundle/Tests/Unit/Layout/Block/Type/FormTypeTest.php
+++ src/Oro/Bundle/LayoutBundle/Tests/Unit/Layout/Block/Type/FormTypeTest.php
@@ -3,17 +3,18 @@
 namespace Oro\Bundle\LayoutBundle\Tests\Unit\Layout\Block\Type;
 
 use Oro\Component\Layout\Block\Type\BaseType;
+use Oro\Component\Layout\BlockBuilderInterface;
 
 use Oro\Bundle\LayoutBundle\Layout\Block\Type\FormType;
 use Oro\Bundle\LayoutBundle\Tests\Unit\BlockTypeTestCase;
 
 class FormTypeTest extends BlockTypeTestCase
 {
-
     public function testBuildBlock()
     {
         $formName = 'test_form';
 
+        /** @var BlockBuilderInterface|\PHPUnit_Framework_MockObject_MockObject $builder */
         $builder = $this->getMock('Oro\Component\Layout\BlockBuilderInterface');
         $builder->expects($this->any())
             ->method('getId')
@@ -31,27 +32,58 @@ class FormTypeTest extends BlockTypeTestCase
                     'form_id_form_start',
                     'form_id',
                     'form_start',
-                    ['form_name' => 'test_form'],
+                    [
+                        'form_name'             => 'test_form',
+                        'form_route_name'       => 'test_route',
+                        'form_route_parameters' => ['test_param' => true],
+                        'form_method'           => 'POST',
+                        'form_enctype'          => 'application/json',
+
+                    ],
                 ],
                 [
                     'form_id_form_fields',
                     'form_id',
                     'form_fields',
-                    ['form_name' => 'test_form'],
+                    [
+                        'form_name'         => 'test_form',
+                        'form_prefix'       => 'test_prefix',
+                        'form_field_prefix' => 'test_field_prefix',
+                        'form_group_prefix' => 'test_group_prefix',
+                        'groups'            => ['main', 'additional'],
+                        'split_to_fields'   => true,
+                        'form_data'         => ['test'],
+                        'preferred_fields'  => 'first_name',
+
+                    ],
                 ],
                 [
                     'form_id_form_end',
                     'form_id',
                     'form_end',
-                    ['form_name' => 'test_form']
+                    [
+                        'form_name'   => 'test_form',
+                        'render_rest' => true,
+                    ],
                 ]
-
             );
         $type = new FormType();
         $options = $this->resolveOptions(
             $type,
             [
-                'form_name' => $formName
+                'form_name'             => $formName,
+                'form_route_name'       => 'test_route',
+                'form_route_parameters' => ['test_param' => true],
+                'form_method'           => 'POST',
+                'form_enctype'          => 'application/json',
+                'form_data'             => ['test'],
+                'form_prefix'           => 'test_prefix',
+                'form_field_prefix'     => 'test_field_prefix',
+                'form_group_prefix'     => 'test_group_prefix',
+                'render_rest'           => true,
+                'preferred_fields'      => 'first_name',
+                'groups'                => ['main', 'additional'],
+                'split_to_fields'       => true,
             ]
         );
 
