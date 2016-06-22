abce2d5a07b220c73eb2efbe26f53fbc7b2c4139
Ievgenii Myshusta <imyshusta@magecore.com>
2016-06-22 13:06:31 +0300

    AEIV-708: Remove inconsistency in notification logic - fixed UT

--- src/Oro/Bundle/CalendarBundle/Tests/Unit/Form/Type/CalendarEventApiTypeTest.php
+++ src/Oro/Bundle/CalendarBundle/Tests/Unit/Form/Type/CalendarEventApiTypeTest.php
@@ -362,8 +362,7 @@ class CalendarEventApiTypeTest extends TypeTestCase
                     'data_class'           => 'Oro\Bundle\CalendarBundle\Entity\CalendarEvent',
                     'intention'            => 'calendar_event',
                     'csrf_protection'      => false,
-                    'extra_fields_message' => 'This form should not contain extra fields: "{{ extra_fields }}"',
-                    'cascade_validation'   => true
+                    'extra_fields_message' => 'This form should not contain extra fields: "{{ extra_fields }}"'
                 ]
             );
 
