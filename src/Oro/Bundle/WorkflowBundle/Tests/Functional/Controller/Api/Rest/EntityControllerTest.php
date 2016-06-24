17facffb17d2a4120ab8df3468e197df09f7f5c1
Nikita Makarov <mesaverde228@gmail.com>
2016-06-24 18:02:18 +0300

    BAP-10818: Add Functional test webtest cases - Covered `Oro\Bundle\WorkflowBundle\Controller\Api\Rest\EntityController`

---
+++ src/Oro/Bundle/WorkflowBundle/Tests/Functional/Controller/Api/Rest/EntityControllerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Oro\Bundle\WorkflowBundle\Tests\Functional\Controller\Api\Rest;
+
+use Oro\Bundle\EntityBundle\Provider\EntityWithFieldsProvider;
+use Oro\Bundle\TestFrameworkBundle\Test\WebTestCase;
+
+class EntityControllerTest extends WebTestCase
+{
+    /**
+     * @var EntityWithFieldsProvider
+     */
+    protected $provider;
+
+    protected function setUp()
+    {
+        $this->initClient([], $this->generateWsseAuthHeader());
+        $this->provider = $this->client->getContainer()->get('oro_entity.entity_field_list_provider');
+    }
+
+    public function testGetAction()
+    {
+        $this->client->request('GET', $this->getUrl('oro_api_workflow_entity_get'));
+
+        $this->assertEquals(
+            array_keys($this->provider->getFields()),
+            array_keys($this->getJsonResponseContent($this->client->getResponse(), 200))
+        );
+    }
+}
+ 
