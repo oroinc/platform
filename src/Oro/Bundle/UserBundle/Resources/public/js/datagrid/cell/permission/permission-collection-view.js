7f6aca211704b4f05a4181c5955c334141624693
Hryhorii Hrebiniuk <hhrebiniuk@magecore.com>
2016-06-24 11:59:48 +0300

    BAP-10897: Fix issues found on demo
     - fixed code style issue

--- src/Oro/Bundle/UserBundle/Resources/public/js/datagrid/cell/permission/permission-collection-view.js
+++ src/Oro/Bundle/UserBundle/Resources/public/js/datagrid/cell/permission/permission-collection-view.js
@@ -2,7 +2,6 @@ define(function(require) {
     'use strict';
 
     var PermissionCollectionView;
-    var mediator = require('oroui/js/mediator');
     var BaseCollectionView = require('oroui/js/app/views/base/collection-view');
     var PermissionView = require('./permission-view');
 
