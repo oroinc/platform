9c50c840c4a3989b2ddbbf63f60565beb0c6089f
Ivaylo Badinov <venimus@users.noreply.github.com>
2016-06-27 14:42:44 +0300

    CRM-5539: Move listeners to a form extension

--- src/Oro/Bundle/ConfigBundle/Form/Type/FormFieldType.php
+++ src/Oro/Bundle/ConfigBundle/Form/Type/FormFieldType.php
@@ -4,8 +4,6 @@ namespace Oro\Bundle\ConfigBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -43,67 +41,6 @@ class FormFieldType extends AbstractType
 
         $builder->add('use_parent_scope_value', $useParentType, $useParentOptions);
         $builder->add('value', $options['target_field_type'], $options['target_field_options']);
-
-        if ($options['resettable']) {
-            $this->addListeners($builder, $options);
-        }
-    }
-
-    /**
-     * Links the disabled state of the 'value' field and the checkbox 'use_parent_scope_value' state
-     *
-     * @param FormBuilderInterface $builder
-     * @param array $options
-     */
-    private function addListeners(FormBuilderInterface $builder, array $options)
-    {
-        $targetFieldType = $options['target_field_type'];
-        $targetFieldOptions = $options['target_field_options'];
-
-        // Initially disable/enable 'value' field depending on the checkbox 'use_parent_scope_value'
-        $builder->addEventListener(
-            FormEvents::PRE_SET_DATA,
-            function (FormEvent $event) use ($targetFieldType, $targetFieldOptions) {
-                $this->setTargetFieldDisabled(
-                    $event->getForm(),
-                    $event->getData(),
-                    $targetFieldType,
-                    $targetFieldOptions
-                );
-            }
-        );
-
-        // If 'use_parent_scope_value' was initially checked (then 'value' field was disabled),
-        // then if it gets unchecked, we also need to enable 'value', so it will be mapped to entity
-        // We have to bind to POST_SUBMIT of the checkbox to get its value and still be able to modify the form
-        $builder->get('use_parent_scope_value')->addEventListener(
-            FormEvents::POST_SUBMIT,
-            function (FormEvent $event) use ($targetFieldType, $targetFieldOptions) {
-                $this->setTargetFieldDisabled(
-                    $event->getForm()->getParent(),
-                    $event->getForm()->getData(),
-                    $targetFieldType,
-                    $targetFieldOptions
-                );
-            }
-        );
-    }
-
-    /**
-     * Modifies the form to set disabled state of 'value' field depending on 'use_parent_scope_value'
-     *
-     * @param FormInterface $form
-     * @param $targetFieldType
-     * @param $targetFieldOptions
-     */
-    private function setTargetFieldDisabled(FormInterface $form, $data, $targetFieldType, $targetFieldOptions)
-    {
-        if (isset($data['use_parent_scope_value'])) {
-            $targetFieldOptions['disabled'] = $data['use_parent_scope_value'];
-        }
-
-        // Re-add the 'value' field to override its definition
-        $form->add('value', $targetFieldType, $targetFieldOptions);
     }
 
     /**
