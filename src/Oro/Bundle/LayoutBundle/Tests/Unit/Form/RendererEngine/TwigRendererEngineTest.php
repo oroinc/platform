da64044dd85b833b8408b724bee41735b35cdd66
Vladimir Seniuk <vseniuk@magecore.com>
2016-07-01 18:58:22 +0300

    BB-3652: Add attribute data-layout-block-id - replace logic of adding attribute data-layout-block-id to layout TwigRendererEngine - renamed data-layout-block-id to data-layout-debug-block-id - added data-layout-debug-block-template attribute - removed layout ProfilerExtension

--- src/Oro/Bundle/LayoutBundle/Tests/Unit/Form/RendererEngine/TwigRendererEngineTest.php
+++ src/Oro/Bundle/LayoutBundle/Tests/Unit/Form/RendererEngine/TwigRendererEngineTest.php
@@ -2,7 +2,11 @@
 
 namespace Oro\Bundle\LayoutBundle\Tests\Unit\Form\RendererEngine;
 
+use Symfony\Bridge\Twig\Form\TwigRendererEngine as BaseEngine;
+use Symfony\Component\Form\FormView;
+
 use Oro\Bundle\LayoutBundle\Form\RendererEngine\TwigRendererEngine;
+use Oro\Bundle\LayoutBundle\Request\LayoutHelper;
 
 class TwigRendererEngineTest extends RendererEngineTest
 {
@@ -12,5 +16,73 @@ class TwigRendererEngineTest extends RendererEngineTest
     public function createRendererEngine()
     {
         return new TwigRendererEngine();
+    }
+
+    public function testRenderBlock()
+    {
+        $renderingEngine = $this->createRendererEngine();
+
+        $renderingEngine->setLayoutHelper($this->getMockLayoutHelper());
+
+        /** @var FormView|\PHPUnit_Framework_MockObject_MockObject $view */
+        $view = $this->getMock('Symfony\Component\Form\FormView', [], [], '', false);
+        $view->vars['cache_key'] = 'cache_key';
+        $template = $this->getMock('\Twig_Template', [], [], '', false);
+        $template->expects($this->once())
+            ->method('getTemplateName')
+            ->will($this->returnValue('theme'));
+
+        $class = new \ReflectionClass(BaseEngine::class);
+        $property = $class->getProperty('template');
+        $property->setAccessible(true);
+        $property->setValue($renderingEngine, $template);
+
+        $property = $class->getProperty('resources');
+        $property->setAccessible(true);
+        $property->setValue($renderingEngine, ['cache_key' => []]);
+
+        $variables = ['id' => 'root'];
+        $result = array_merge(
+            $variables,
+            [
+                'attr' => [
+                    'data-layout-debug-block-id' => 'root',
+                    'data-layout-debug-block-template' => 'theme'
+                ]
+            ]
+        );
+
+        /** @var \Twig_Environment|\PHPUnit_Framework_MockObject_MockObject $environment */
+        $environment = $this->getMock('\Twig_Environment', [], [], '', false);
+        $environment->expects($this->once())
+            ->method('mergeGlobals')
+            ->with($result)
+            ->will($this->returnValue([$template, 'root']));
+        $renderingEngine->setEnvironment($environment);
+
+        $renderingEngine->renderBlock($view, [$template, 'root'], 'root', $variables);
+    }
+
+    public function testSetLayoutHelper()
+    {
+        $renderingEngine = $this->createRendererEngine();
+
+        $renderingEngine->setLayoutHelper($this->getMockLayoutHelper());
+
+        $class = new \ReflectionClass(TwigRendererEngine::class);
+        $property = $class->getProperty('layoutHelper');
+        $property->setAccessible(true);
+        $this->assertInstanceOf(
+            'Oro\Bundle\LayoutBundle\Request\LayoutHelper',
+            $property->getValue($renderingEngine)
+        );
+    }
+
+    /**
+     * @return LayoutHelper|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected function getMockLayoutHelper()
+    {
+        return $this->getMock('Oro\Bundle\LayoutBundle\Request\LayoutHelper', [], [], '', false);
     }
 }
