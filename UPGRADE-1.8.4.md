4e894bac6744903ee172bce177bc0949ef980ce2
Olga Syzova <osizova@magecore.com>
2016-07-01 16:01:57 +0300

    CRM-5745: Calendar Event and Task Reminder Email Displaying Incorrect Time Zone; - ported to 1.10

--- UPGRADE-1.8.4.md
+++ UPGRADE-1.8.4.md
@@ -4,3 +4,9 @@ UPGRADE FROM 1.8.3 to 1.8.4
 ####EmailBundle
 - The constructor of the `Oro\Bundle\EmailBundle\Mailer\Processor` class was changed. Before: `__construct(DoctrineHelper $doctrineHelper, DirectMailer $mailer, EmailAddressHelper $emailAddressHelper, EmailEntityBuilder $emailEntityBuilder, EmailOwnerProvider $emailOwnerProvider, EmailActivityManager $emailActivityManager, ServiceLink $serviceLink, EventDispatcherInterface $eventDispatcher, Mcrypt $encryptor, EmailOriginHelper $emailOriginHelper)`. After: `__construct(DoctrineHelper $doctrineHelper, DirectMailer $mailer, EmailAddressHelper $emailAddressHelper, EmailEntityBuilder $emailEntityBuilder, EmailActivityManager $emailActivityManager, EventDispatcherInterface $eventDispatcher, Mcrypt $encryptor, EmailOriginHelper $emailOriginHelper)`.
 - Additional you should use origin as second parameter for `Oro\Bundle\EmailBundle\Mailer\Processor::process` if you want use specific transport different from system.
+
+####CalendarBundle
+- Added method `formatCalendarDateRangeUser` of `src/Oro/src/Oro/Bundle/CalendarBundle/Twig/DateFormatUserExtension.php`. Method `calendar_date_range_user` get additional param 'user' and return sate range according to user organization localization settings.
+ 
+####LocaleBundle:
+- Added `oro_format_datetime_user` twig extension - allows get formatted date and calendar date range by user organization localization settings. Deprecated since 1.11. Will be removed after 1.13.
--- src/Oro/Bundle/CalendarBundle/Tests/Unit/Twig/DateFormatExtensionTest.php
+++ src/Oro/Bundle/CalendarBundle/Tests/Unit/Twig/DateFormatExtensionTest.php
@@ -3,20 +3,59 @@
 namespace Oro\Bundle\CalendarBundle\Tests\Unit\Twig;
 
 use Oro\Bundle\CalendarBundle\Twig\DateFormatExtension;
+use Oro\Bundle\SecurityBundle\Tests\Unit\Acl\Domain\Fixtures\Entity\User;
+use Oro\Bundle\OrganizationBundle\Tests\Unit\Fixture\Entity\Organization;
+use Oro\Bundle\LocaleBundle\Formatter\DateTimeFormatter;
 
 class DateFormatExtensionTest extends \PHPUnit_Framework_TestCase
 {
-    /** @var \PHPUnit_Framework_MockObject_MockObject */
+    /** @var DateTimeFormatter|\PHPUnit_Framework_MockObject_MockObject */
     protected $formatter;
 
     /** @var DateFormatExtension */
     protected $extension;
 
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $configManager;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $localeSettings;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $translator;
+
     protected function setUp()
     {
+        $this->configManager = $this->getMockBuilder('Oro\Bundle\ConfigBundle\Config\ConfigManager')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->localeSettings = $this->getMockBuilder('Oro\Bundle\LocaleBundle\Model\LocaleSettings')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->translator = $this->getMockBuilder('Symfony\Bundle\FrameworkBundle\Translation\Translator')
+            ->disableOriginalConstructor()
+            ->getMock();
         $this->formatter = $this->getMockBuilder('Oro\Bundle\LocaleBundle\Formatter\DateTimeFormatter')
             ->disableOriginalConstructor()
             ->getMock();
+    }
+
+    /**
+     * @param string $start
+     * @param string|bool $end
+     * @param string|bool $skipTime
+     * @param string $expected
+     *
+     * @dataProvider formatCalendarDateRangeProvider
+     */
+    public function testFormatCalendarDateRange($start, $end, $skipTime, $expected)
+    {
         $this->formatter->expects($this->any())
             ->method('format')
             ->will($this->returnValue('DateTime'));
@@ -26,15 +65,8 @@ class DateFormatExtensionTest extends \PHPUnit_Framework_TestCase
         $this->formatter->expects($this->any())
             ->method('formatTime')
             ->will($this->returnValue('Time'));
-
         $this->extension = new DateFormatExtension($this->formatter);
-    }
 
-    /**
-     * @dataProvider formatCalendarDateRangeProvider
-     */
-    public function testFormatCalendarDateRange($start, $end, $skipTime, $expected)
-    {
         $startDate = new \DateTime($start);
         $endDate = $end === null ? null : new \DateTime($end);
 
@@ -55,5 +87,95 @@ class DateFormatExtensionTest extends \PHPUnit_Framework_TestCase
             array('2010-05-01T10:30:15+00:00', '2010-05-02T10:30:15+00:00', false, 'DateTime - DateTime'),
             array('2010-05-01T10:30:15+00:00', '2010-05-02T10:30:15+00:00', true, 'Date - Date'),
         );
+    }
+
+    /**
+     * @param string $start
+     * @param string $end
+     * @param array $config
+     * @param string|null $locale
+     * @param string|null $timeZone
+     * @param User $user
+     * @param string $expected
+     *
+     * @dataProvider formatCalendarDateRangeUserProvider
+     */
+    public function testFormatCalendarDateRangeUser($start, $end, array $config, $locale, $timeZone, $user, $expected)
+    {
+        $this->formatter = new DateTimeFormatter($this->localeSettings, $this->translator);
+        $this->extension = new DateFormatExtension($this->formatter);
+        $this->extension->setConfigManager($this->configManager);
+
+        $startDate = new \DateTime($start, new \DateTimeZone('UTC'));
+        $endDate = $end === null ? null : new \DateTime($end, new \DateTimeZone('UTC'));
+
+        $this->configManager->expects($this->any())
+            ->method('get')
+            ->will(
+                $this->returnValueMap(
+                    [
+                        ['oro_locale.locale', false, false, $config['locale']],
+                        ['oro_locale.timezone', false, false, $config['timeZone']],
+                    ]
+                )
+            );
+
+        $result = $this->extension->formatCalendarDateRangeUser(
+            $startDate,
+            $endDate,
+            false,
+            null,
+            null,
+            $locale,
+            $timeZone,
+            $user
+        );
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function formatCalendarDateRangeUserProvider()
+    {
+        $organization = new Organization(1);
+        $user = new User(1, null, $organization);
+
+        return [
+            'Localization settings from global scope' => [
+                '2016-05-01 10:30:15',
+                '2016-05-01 11:30:15',
+                ['locale' => 'en_US', 'timeZone' => 'UTC'], // config global scope
+                null,
+                null,
+                $user,
+                'May 1, 2016 10:30 AM - 11:30 AM'
+            ],
+            'Localization settings from global scope start=end' => [
+                '2016-05-01 10:30:15',
+                '2016-05-01 10:30:15',
+                ['locale' => 'en_US', 'timeZone' => 'UTC'], // config global scope
+                null,
+                null,
+                $user,
+                'May 1, 2016, 10:30 AM'
+            ],
+            'Localization settings from params values' => [
+                '2016-05-01 10:30:15',
+                '2016-05-01 11:30:15',
+                ['locale' => 'en_US', 'timeZone' => 'UTC'], // config global scope
+                'en_US',
+                'Europe/Athens',
+                null,
+                'May 1, 2016 1:30 PM - 2:30 PM'
+            ],
+            'Localization settings from params values start=end' => [
+                '2016-05-01 10:30:15',
+                '2016-05-01 10:30:15',
+                ['locale' => 'en_US', 'timeZone' => 'UTC'], // config global scope
+                'en_US',
+                'Europe/Athens',
+                null,
+                'May 1, 2016, 1:30 PM'
+            ]
+        ];
     }
 }
