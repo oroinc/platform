2fa1ecda36d46b132e839ec7ed8b41fafe126904
AlexandrDmitriev <admitriev@magecore.com>
2016-07-04 14:09:45 +0300

    CRM-5745: Calendar Event and Task Reminder Email Displaying Incorrect Time Zone - fix installer

--- src/Oro/Bundle/ReminderBundle/Migrations/Schema/OroReminderBundleBundleInstaller.php
+++ src/Oro/Bundle/ReminderBundle/Migrations/Schema/OroReminderBundleBundleInstaller.php
@@ -14,7 +14,7 @@ class OroReminderBundleBundleInstaller implements Installation
      */
     public function getMigrationVersion()
     {
-        return 'v1_3';
+        return 'v1_2_1';
     }
 
     /**
