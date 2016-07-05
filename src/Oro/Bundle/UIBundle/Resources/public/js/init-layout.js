9c2a56cf2635d434a6b659a08c626bbf3d960e5f
Alexander Pobereznichenko <opobereznichenko@magecore.com>
2016-07-05 09:41:56 +0300

    BB-3619: Add simple UI for price list rules     - update layout

--- src/Oro/Bundle/UIBundle/Resources/public/js/init-layout.js
+++ src/Oro/Bundle/UIBundle/Resources/public/js/init-layout.js
@@ -436,6 +436,8 @@ require(['jquery', 'underscore', 'orotranslation/js/translator', 'oroui/js/tools
         validateContainer($listContainer);
     });
 
+    //TODO: implement clone row
+
     $(document).on('click', '.addAfterRow', function(e) {
         e.preventDefault();
         if ($(this).attr('disabled')) {
