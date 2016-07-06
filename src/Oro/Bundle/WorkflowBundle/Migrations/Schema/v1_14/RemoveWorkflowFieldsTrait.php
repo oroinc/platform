c6093b2491a615aa5274da0bbeaebbc910f91b5e
Mykhailo Sulyma <msulima@magecore.com>
2016-07-06 15:51:25 +0300

    BAP-10820: Testing and updates  - migrations fix

---
+++ src/Oro/Bundle/WorkflowBundle/Migrations/Schema/v1_14/RemoveWorkflowFieldsTrait.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Oro\Bundle\WorkflowBundle\Migrations\Schema\v1_14;
+
+use Doctrine\DBAL\Schema\Table;
+
+trait RemoveWorkflowFieldsTrait
+{
+    /**
+     * @param Table $table
+     */
+    protected function removeWorkflowFields(Table $table)
+    {
+        $workflowTables = ['oro_workflow_item', 'oro_workflow_step'];
+
+        foreach ($table->getForeignKeys() as $foreignKey) {
+            if (!in_array($foreignKey->getForeignTableName(), $workflowTables, true)) {
+                continue;
+            }
+            
+            $table->removeForeignKey($foreignKey->getName());
+            foreach ($foreignKey->getLocalColumns() as $column) {
+                $table->dropColumn($column);
+            }
+        }
+    }
+}
