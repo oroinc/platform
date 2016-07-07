e5ceb7f16dae8be62767c176c96c7ef2e50306cb
Miloslav Nenadal <nenadalm@gmail.com>
2016-07-07 11:12:36 +0200

    AEIV-769: Event exception is not synced properly

--- src/Oro/Bundle/CalendarBundle/Handler/CalendarEventDeleteHandler.php
+++ src/Oro/Bundle/CalendarBundle/Handler/CalendarEventDeleteHandler.php
@@ -118,18 +118,18 @@ class CalendarEventDeleteHandler extends DeleteHandler
         }
 
         $em = $manager->getObjectManager();
-        $this->processDelete($entity, $em);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function processDelete($entity, ObjectManager $em)
-    {
+        $this->checkPermissions($entity, $em);
         if ($entity->getRecurrence() && $entity->getRecurrence()->getId()) {
             $em->remove($entity->getRecurrence());
         }
-        parent::processDelete($entity, $em);
+
+        if ($entity->getRecurringEvent()) {
+            $entity->cancelAll();
+        } else {
+            $this->deleteEntity($entity, $em);
+        }
+
+        $em->flush();
 
         if ($this->shouldSendNotification()) {
             $this->emailSendProcessor->sendDeleteEventNotification($entity);
