ef1035099ec21b1080b26ba15da78932ce8f840b
Nikita Makarov <mesaverde228@gmail.com>
2016-07-08 10:37:20 +0300

    BB-3742: Create LocalizationProvider - Added Provider

---
+++ src/Oro/Bundle/LocaleBundle/Provider/LocalizationProvider.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Oro\Bundle\LocaleBundle\Provider;
+
+use Doctrine\Bundle\DoctrineBundle\Registry;
+
+use Oro\Bundle\LocaleBundle\Entity\Localization;
+use Oro\Bundle\LocaleBundle\Entity\Repository\LocalizationRepository;
+
+class LocalizationProvider
+{
+    /**
+     * @var Registry
+     */
+    protected $registry;
+
+    /**
+     * @param Registry $registry
+     */
+    public function __construct(Registry $registry)
+    {
+        $this->registry = $registry;
+    }
+
+    /**
+     * @param $id
+     *
+     * @return null|Localization
+     */
+    public function getLocalization($id)
+    {
+        return $this->getRepository()->find($id);
+    }
+
+    /**
+     * @return array
+     */
+    public function getLocalizations()
+    {
+        return $this->getRepository()->findAll();
+    }
+
+    /**
+     * @return LocalizationRepository
+     */
+    protected function getRepository()
+    {
+        return $this->registry->getManagerForClass(Localization::class)->getRepository(Localization::class);
+    }
+}
