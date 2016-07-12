9f4f44360b4da6f8d3e8dc262e93f4ed693545f4
Vladimir Seniuk <vseniuk@magecore.com>
2016-07-12 16:05:24 +0300

    BB-3653: Add to symfony developer toolbar layout tree structure dump - added layout form TwigRendererEngine - fixed functional tests

--- src/Oro/Bundle/LayoutBundle/Form/RendererEngine/TwigRendererEngine.php
+++ src/Oro/Bundle/LayoutBundle/Form/RendererEngine/TwigRendererEngine.php
@@ -3,9 +3,6 @@
 namespace Oro\Bundle\LayoutBundle\Form\RendererEngine;
 
 use Symfony\Bridge\Twig\Form\TwigRendererEngine as BaseEngine;
-use Symfony\Component\Form\FormView;
-
-use Oro\Bundle\LayoutBundle\Request\LayoutHelper;
 
 use Oro\Component\Layout\Form\RendererEngine\FormRendererEngineInterface;
 
@@ -15,11 +12,6 @@ use Oro\Component\Layout\Form\RendererEngine\FormRendererEngineInterface;
 class TwigRendererEngine extends BaseEngine implements FormRendererEngineInterface
 {
     /**
-     * @var LayoutHelper
-     */
-    protected $layoutHelper;
-
-    /**
      * {@inheritdoc}
      */
     public function addDefaultThemes($themes)
@@ -27,27 +19,5 @@ class TwigRendererEngine extends BaseEngine implements FormRendererEngineInterfa
         $themes = is_array($themes) ? $themes : [$themes];
 
         $this->defaultThemes = array_merge($this->defaultThemes, $themes);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function renderBlock(FormView $view, $resource, $blockName, array $variables = [])
-    {
-        $twigTemplate = current($resource);
-        if ($this->layoutHelper->isProfilerEnabled() && $twigTemplate instanceof \Twig_Template) {
-            $variables['attr']['data-layout-debug-block-id'] = $variables['id'];
-            $variables['attr']['data-layout-debug-block-template'] = $twigTemplate->getTemplateName();
-        }
-
-        return parent::renderBlock($view, $resource, $blockName, $variables);
-    }
-
-    /**
-     * @param LayoutHelper $layoutHelper
-     */
-    public function setLayoutHelper(LayoutHelper $layoutHelper)
-    {
-        $this->layoutHelper = $layoutHelper;
     }
 }
