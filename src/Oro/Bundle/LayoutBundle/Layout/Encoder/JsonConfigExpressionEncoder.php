f6c4a7196065d455ae9b1e14e6c22e78cd6a8ffd
anyt <yatsenco@gmail.com>
2016-07-12 15:10:17 +0300

    BB-3766: Modify ConfigExpressionEncoderInterface::encodeExpr to apply ParsedExpression and modify JsonConfigExpressionEncoder, see example of basic serialization here

--- src/Oro/Bundle/LayoutBundle/Layout/Encoder/JsonConfigExpressionEncoder.php
+++ src/Oro/Bundle/LayoutBundle/Layout/Encoder/JsonConfigExpressionEncoder.php
@@ -2,7 +2,9 @@
 
 namespace Oro\Bundle\LayoutBundle\Layout\Encoder;
 
-use Oro\Component\ConfigExpression\ExpressionInterface;
+use Symfony\Component\ExpressionLanguage\Node\Node;
+use Symfony\Component\ExpressionLanguage\ParsedExpression;
+
 use Oro\Component\Layout\Action;
 
 class JsonConfigExpressionEncoder implements ConfigExpressionEncoderInterface
@@ -10,9 +12,9 @@ class JsonConfigExpressionEncoder implements ConfigExpressionEncoderInterface
     /**
      * {@inheritdoc}
      */
-    public function encodeExpr(ExpressionInterface $expr)
+    public function encodeExpr(ParsedExpression $expr)
     {
-        return json_encode($expr->toArray());
+        return json_encode($this->serializeExpression($expr));
     }
 
     /**
@@ -33,5 +35,35 @@ class JsonConfigExpressionEncoder implements ConfigExpressionEncoderInterface
                 )
             ]
         );
+    }
+
+    /**
+     * @param ParsedExpression $expr
+     * @return array
+     */
+    protected function serializeExpression(ParsedExpression $expr)
+    {
+        return [
+            'expression' => (string)$expr,
+            'node' => $this->serializeExpressionNode($expr->getNodes())
+        ];
+    }
+
+    /**
+     * @param Node $node
+     * @return array
+     */
+    protected function serializeExpressionNode(Node $node)
+    {
+        $result = [];
+        $class = get_class($node);
+        foreach ($node->attributes as $name => $value) {
+            $result[$class]['attributes'][$name] = $value;
+        }
+        foreach ($node->nodes as $name => $node) {
+            $result[$class]['nodes'][$name] = $this->serializeExpressionNode($node);
+        }
+
+        return $result;
     }
 }
--- src/Oro/Bundle/LayoutBundle/Tests/Unit/Layout/Encoder/JsonConfigExpressionEncoderTest.php
+++ src/Oro/Bundle/LayoutBundle/Tests/Unit/Layout/Encoder/JsonConfigExpressionEncoderTest.php
@@ -2,6 +2,11 @@
 
 namespace Oro\Bundle\LayoutBundle\Tests\Unit\Layout\Encoder;
 
+use Symfony\Component\ExpressionLanguage\Node\BinaryNode;
+use Symfony\Component\ExpressionLanguage\Node\ConstantNode;
+use Symfony\Component\ExpressionLanguage\Node\UnaryNode;
+use Symfony\Component\ExpressionLanguage\ParsedExpression;
+
 use Oro\Component\ConfigExpression\Condition;
 use Oro\Component\Layout\Action;
 
@@ -11,15 +16,17 @@ class JsonConfigExpressionEncoderTest extends \PHPUnit_Framework_TestCase
 {
     public function testEncodeExpr()
     {
+        $parsedExpression = $this->createParsedExpression();
+
         $encoder = new JsonConfigExpressionEncoder();
-        $result  = $encoder->encodeExpr(new Condition\TrueCondition());
-        $this->assertEquals('{"@true":null}', $result);
+        $result = $encoder->encodeExpr($parsedExpression);
+        $this->assertJsonStringEqualsJsonFile(__DIR__.'/expression.json', $result);
     }
 
     public function testEncodeActions()
     {
         $encoder = new JsonConfigExpressionEncoder();
-        $result  = $encoder->encodeActions(
+        $result = $encoder->encodeActions(
             [
                 new Action('add', ['val1']),
                 new Action('remove', ['val2'])
@@ -29,5 +36,16 @@ class JsonConfigExpressionEncoderTest extends \PHPUnit_Framework_TestCase
             '{"@actions":[{"name":"add","args":["val1"]},{"name":"remove","args":["val2"]}]}',
             $result
         );
+    }
+
+    /**
+     * @return ParsedExpression
+     */
+    protected function createParsedExpression()
+    {
+        $node = new BinaryNode('===', new UnaryNode('!', new ConstantNode(true)), new ConstantNode(false));
+        $parsedExpression = new ParsedExpression('!true === false', $node);
+
+        return $parsedExpression;
     }
 }
