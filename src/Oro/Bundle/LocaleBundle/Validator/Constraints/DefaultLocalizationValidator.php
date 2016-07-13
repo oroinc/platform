474130bf7fcf44f557cd8e11976852c357fa6dae
Yauheni Kasenka <beta.screech@gmail.com>
2016-07-13 20:54:11 +0300

    BB-3743: Implement available Localizations selector and default localization - moved logic from DefaultLocalizationSelectionType to DefaultLocalization constraint

---
+++ src/Oro/Bundle/LocaleBundle/Validator/Constraints/DefaultLocalizationValidator.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Oro\Bundle\LocaleBundle\Validator\Constraints;
+
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+use Oro\Bundle\LocaleBundle\Entity\Localization;
+use Oro\Bundle\LocaleBundle\Provider\LocalizationProvider;
+
+class DefaultLocalizationValidator extends ConstraintValidator
+{
+    const ENABLED_LOCALIZATIONS_NAME = 'oro_locale___enabled_localizations';
+
+    /**
+     * @var LocalizationProvider
+     */
+    protected $localizationProvider;
+
+    /**
+     * @param LocalizationProvider $localizationProvider
+     */
+    public function __construct(LocalizationProvider $localizationProvider)
+    {
+        $this->localizationProvider = $localizationProvider;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function validate($defaultLocalization, Constraint $constraint)
+    {
+        $rootForm = $this->context->getRoot();
+
+        if (!$this->isSyncApplicable($rootForm)) {
+            return;
+        }
+
+        $localizationData = $rootForm->getData();
+
+        $enabledLocalizations = $this->getEnabledLocalizations($localizationData[self::ENABLED_LOCALIZATIONS_NAME]);
+
+        if (in_array($defaultLocalization, $enabledLocalizations, true)) {
+            return;
+        }
+
+        $localization = $this->localizationProvider->getLocalization((int)$defaultLocalization);
+        if ($localization instanceof Localization) {
+            $message = 'oro.locale.validators.is_not_enabled';
+            $params = ['%localization%' => $localization->getName()];
+        } else {
+            $message =  'oro.locale.validators.unknown_localization';
+            $params = ['%localization_id%' => $defaultLocalization];
+        }
+
+        $this->context->buildViolation($message, $params)
+            ->addViolation();
+    }
+
+    /**
+     * @param array $enabledLocalizationsData
+     * @return array
+     */
+    protected function getEnabledLocalizations(array $enabledLocalizationsData)
+    {
+        return isset($enabledLocalizationsData['value']) ? $enabledLocalizationsData['value'] : [];
+    }
+
+    /**
+     * @param FormInterface $rootForm
+     * @return bool
+     */
+    protected function isSyncApplicable(FormInterface $rootForm)
+    {
+        return $rootForm && $rootForm->getName() == 'localization' && $rootForm->has(self::ENABLED_LOCALIZATIONS_NAME);
+    }
+}
