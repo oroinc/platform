d91356506f932387195cd4b97199fbdb298cef7f
dmitrosh <dshykalenko@magecore.com>
2016-07-13 18:56:39 +0300

    BAP-11042: Develop new row view for entity permissions including entity fields ACL (editor)

--- src/Oro/Bundle/UserBundle/Resources/public/js/datagrid/permission/permission-readonly-view.js
+++ src/Oro/Bundle/UserBundle/Resources/public/js/datagrid/permission/permission-readonly-view.js
@@ -8,7 +8,7 @@ define(function(require) {
     PermissionReadOnlyView = BaseView.extend({
         tagName: 'li',
         className: 'action-permissions__item dropdown',
-        template: require('tpl!orouser/templates/datagrid/cell/permission/permission-readonly-view.html'),
+        template: require('tpl!orouser/templates/datagrid/permission/permission-readonly-view.html'),
         getTemplateData: function() {
             var data = PermissionReadOnlyView.__super__.getTemplateData.apply(this, arguments);
             data.noAccess = accessLevels.NONE === this.model.get('access_level');
