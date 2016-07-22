347c6b2176d127fc6c2e43e8a483f8aeb268e15c
Alexander Pobereznichenko <opobereznichenko@magecore.com>
2016-07-22 12:43:20 +0300

    BB-3925: Add offer button unavailable on Quote create

--- src/Oro/Bundle/FormBundle/Resources/public/js/app/views/select2-autocomplete-view.js
+++ src/Oro/Bundle/FormBundle/Resources/public/js/app/views/select2-autocomplete-view.js
@@ -8,7 +8,7 @@ define([
     Select2AutocompleteView = Select2View.extend({
         events: {
             'change': function(e) {
-                var selectedData = $(this.$el).data().selectedData;
+                var selectedData = this.$el.data().selectedData || [];
                 if (e.added) {
                     selectedData = selectedData.push(e.added);
                     $(this).data('selected-data', selectedData);
