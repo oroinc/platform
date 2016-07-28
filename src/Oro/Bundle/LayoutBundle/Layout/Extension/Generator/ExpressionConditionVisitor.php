838cf9a5754de6c5402a1660cc6a547147b310fc
Denys Zakharov <dzakharov@magecore.com>
2016-07-28 18:25:32 +0300

    BB-3710: Use Symfony ExpressionLanguage in Layouts - rename config expression

---
+++ src/Oro/Bundle/LayoutBundle/Layout/Extension/Generator/ExpressionConditionVisitor.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Oro\Bundle\LayoutBundle\Layout\Extension\Generator;
+
+use Symfony\Component\ExpressionLanguage\ExpressionLanguage;
+use Symfony\Component\ExpressionLanguage\ParsedExpression;
+
+use Oro\Component\Layout\Loader\Generator\VisitContext;
+use Oro\Component\Layout\Loader\Generator\LayoutUpdateGeneratorInterface;
+use Oro\Component\Layout\Loader\Visitor\VisitorInterface;
+
+class ExpressionConditionVisitor implements VisitorInterface
+{
+    /** @var ParsedExpression */
+    protected $expression;
+
+    /** @var  ExpressionLanguage */
+    protected $expressionLanguage;
+
+    /**
+     * @param ParsedExpression $expression Compiled expression
+     * @param ExpressionLanguage $expressionLanguage
+     */
+    public function __construct(ParsedExpression $expression, ExpressionLanguage $expressionLanguage)
+    {
+        $this->expression = $expression;
+        $this->expressionLanguage = $expressionLanguage;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function startVisit(VisitContext $visitContext)
+    {
+        $visitContext->getUpdateMethodWriter()
+            ->writeln(
+                sprintf(
+                    '$context = $%s->getContext();',
+                    LayoutUpdateGeneratorInterface::PARAM_LAYOUT_ITEM
+                )
+            )
+            ->writeln(sprintf('if (%s) {', $this->expressionLanguage->compile($this->expression)))
+            ->indent();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function endVisit(VisitContext $visitContext)
+    {
+        $visitContext->getUpdateMethodWriter()
+            ->outdent()
+            ->writeln('}');
+    }
+}
