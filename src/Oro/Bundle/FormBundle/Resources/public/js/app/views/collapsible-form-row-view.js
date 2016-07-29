bf9cb6b922133cbd35d45e0c432e8ce288f8a95d
Miloslav Nenadal <miloslav.nenadal@imatic.cz>
2016-07-29 14:10:34 +0200

    CRM-5959: After demo fixes
    - 2. Make "Query filter" as a regular label and at the right side (and with same paddings as other fields on the form) show link "Show/Hide" or "Expand/Collapse".

--- src/Oro/Bundle/FormBundle/Resources/public/js/app/views/collapsible-form-row-view.js
+++ src/Oro/Bundle/FormBundle/Resources/public/js/app/views/collapsible-form-row-view.js
@@ -2,6 +2,7 @@ define(function(require) {
     'use strict';
 
     var CollapsibleFormRowView;
+    var $ = require('jquery');
     var _ = require('underscore');
     var BaseView = require('oroui/js/app/views/base/view');
     require('jquery.select2');
@@ -22,8 +23,16 @@ define(function(require) {
             return this;
         },
 
-        onCollapseToggle: function() {
-            var $controls = this.$('.controls');
+        onCollapseToggle: function(e) {
+            var $target = $(e.target);
+            var toggleLabel = $target.data('toggle-label');
+            if (toggleLabel) {
+                $target
+                    .data('toggle-label', $target.text())
+                    .text(toggleLabel);
+            }
+
+            var $controls = this.$('.controls.colla');
             if (this.$el.hasClass('collapsed')) {
                 $controls.slideDown(_.bind(function() {
                     this.$el.trigger('content:changed');
