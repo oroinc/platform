16b3a77e5ec2a7fedf2cbd9edfd21410bd0afcf4
Serhii Polishchuk <spolishchuk@magecore.com>
2016-07-31 19:49:01 +0300

    BAP-10576: Clean before behat suite

--- src/Oro/Bundle/TestFrameworkBundle/Behat/Listener/SessionsListener.php
+++ src/Oro/Bundle/TestFrameworkBundle/Behat/Listener/SessionsListener.php
@@ -5,11 +5,43 @@ namespace Oro\Bundle\TestFrameworkBundle\Behat\Listener;
 use Behat\Behat\EventDispatcher\Event\BeforeFeatureTested;
 use Behat\Behat\EventDispatcher\Event\BeforeScenarioTested;
 use Behat\Behat\EventDispatcher\Event\FeatureTested;
+use Behat\Mink\Mink;
 use Behat\MinkExtension\Listener\SessionsListener as MinkSessionsListener;
 use Behat\Testwork\EventDispatcher\Event\ExerciseCompleted;
 
 class SessionsListener extends MinkSessionsListener
 {
+    protected $mink;
+    protected $defaultSession;
+    protected $javascriptSession;
+
+    /**
+     * @var string[] The available javascript sessions
+     */
+    protected $availableJavascriptSessions;
+
+    /**
+     * Initializes initializer.
+     *
+     * @param Mink        $mink
+     * @param string      $defaultSession
+     * @param string|null $javascriptSession
+     * @param string[]    $availableJavascriptSessions
+     */
+    public function __construct(
+        Mink $mink,
+        $defaultSession,
+        $javascriptSession,
+        array $availableJavascriptSessions = array()
+    ) {
+        $this->mink              = $mink;
+        $this->defaultSession    = $defaultSession;
+        $this->javascriptSession = $javascriptSession;
+        $this->availableJavascriptSessions = $availableJavascriptSessions;
+
+        parent::__construct($mink, $defaultSession, $javascriptSession, $availableJavascriptSessions);
+    }
+
     /**
      * {@inheritdoc}
      */
@@ -23,6 +55,9 @@ class SessionsListener extends MinkSessionsListener
 
     public function prepareDefaultMinkSession(BeforeFeatureTested $event)
     {
+        $this->mink->stopSessions();
+        gc_collect_cycles();
+
         $newEvent = new BeforeScenarioTested(
             $event->getEnvironment(),
             $event->getFeature(),
