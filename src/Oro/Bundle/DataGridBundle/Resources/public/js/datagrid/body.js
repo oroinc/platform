769fc1864cbfead7abd6a66783f5b3a3733fca0b
Oleksandr Nechyporenko <onechyporenko@magecore.com>
2016-08-02 14:58:37 +0300

    BB-3965: Update UI styles  - create FooterAlignView

--- src/Oro/Bundle/DataGridBundle/Resources/public/js/datagrid/body.js
+++ src/Oro/Bundle/DataGridBundle/Resources/public/js/datagrid/body.js
@@ -1,11 +1,12 @@
 define([
     'underscore',
+    'oroui/js/mediator',
     'backbone',
     'chaplin',
     'backgrid',
     './row',
     '../pageable-collection'
-], function(_, Backbone, Chaplin, Backgrid, Row, PageableCollection) {
+], function(_, mediator, Backbone, Chaplin, Backgrid, Row, PageableCollection) {
     'use strict';
 
     var Body;
@@ -61,6 +62,12 @@ define([
             delete this.filteredColumns;
 
             Body.__super__.dispose.call(this);
+        },
+
+        renderAllItems: function() {
+            var result = Body.__super__.renderAllItems.call(this, arguments);
+            mediator.trigger('layout:adjustHeight');
+            return result;
         },
 
         initItemView: function(model) {
