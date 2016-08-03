bd81bc51a0efea8e28e1e3235618bcb98243c3b6
Serhii Polishchuk <spolishchuk@magecore.com>
2016-08-03 08:32:28 +0300

    BAP-11290: Added --applicable-suites parameter to behat command

--- src/Oro/Bundle/TestFrameworkBundle/Behat/ServiceContainer/OroTestFrameworkExtension.php
+++ src/Oro/Bundle/TestFrameworkBundle/Behat/ServiceContainer/OroTestFrameworkExtension.php
@@ -7,8 +7,11 @@ use Behat\MinkExtension\ServiceContainer\MinkExtension;
 use Behat\Symfony2Extension\ServiceContainer\Symfony2Extension;
 use Behat\Symfony2Extension\Suite\SymfonyBundleSuite;
 use Behat\Symfony2Extension\Suite\SymfonySuiteGenerator;
+use Behat\Testwork\Cli\ServiceContainer\CliExtension;
 use Behat\Testwork\ServiceContainer\Extension as TestworkExtension;
 use Behat\Testwork\ServiceContainer\ExtensionManager;
+use Behat\Testwork\Suite\ServiceContainer\SuiteExtension;
+use Oro\Bundle\TestFrameworkBundle\Behat\Cli\SuiteController;
 use Oro\Bundle\TestFrameworkBundle\Behat\Driver\OroSelenium2Factory;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\FileLocator;
@@ -63,6 +66,13 @@ class OroTestFrameworkExtension implements TestworkExtension
     {
         $builder
             ->children()
+                ->arrayNode('application_suites')
+                    ->prototype('scalar')->end()
+                    ->info(
+                        "Suites that applicable for application.\n".
+                        'This suites will be run with --applicable-suites key in console'
+                    )
+                ->end()
                 ->arrayNode('shared_contexts')
                     ->prototype('scalar')->end()
                     ->info('Contexts that added to all autoload bundles suites')
@@ -80,7 +90,9 @@ class OroTestFrameworkExtension implements TestworkExtension
         $loader->load('kernel_services.yml');
 
         $container->setParameter('oro_test.shared_contexts', $config['shared_contexts']);
+        $container->setParameter('oro_test.application_suites', $config['application_suites']);
         $this->loadSessionsListener($container);
+        $this->loadRegistryController($container);
     }
 
     public function processDbDumpers(ContainerBuilder $container)
@@ -94,6 +106,18 @@ class OroTestFrameworkExtension implements TestworkExtension
         $container
             ->getDefinition('mink.listener.sessions')
             ->setClass('Oro\Bundle\TestFrameworkBundle\Behat\Listener\SessionsListener');
+    }
+
+    private function loadRegistryController(ContainerBuilder $container)
+    {
+        $definition = new Definition(SuiteController::class, [
+            new Reference(SuiteExtension::REGISTRY_ID),
+            '%suite.configurations%',
+            '%oro_test.application_suites%'
+        ]);
+
+        $definition->addTag(CliExtension::CONTROLLER_TAG, array('priority' => 1100));
+        $container->setDefinition(CliExtension::CONTROLLER_TAG . '.suite', $definition);
     }
 
     /**
