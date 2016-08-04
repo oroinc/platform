2a23644e156b630fcd62c3e2760ad4d6c447253a
Dmytro Krushelnytskyi <dkrushelnytskyi@magecore.com>
2016-08-04 21:19:06 +0300

    BAP-11275: Status filter breaks grid functionality

--- src/Oro/Bundle/FilterBundle/Resources/public/js/filter/dictionary-filter.js
+++ src/Oro/Bundle/FilterBundle/Resources/public/js/filter/dictionary-filter.js
@@ -211,6 +211,7 @@ define(function(require) {
             }));
 
             this._appendFilter($filter);
+            this._refreshWidth();
         },
 
         /**
@@ -373,6 +374,18 @@ define(function(require) {
                 type: this._getInputValue(this.criteriaValueSelectors.type),
                 value: value
             };
+        },
+
+        /**
+         * Update width of filter
+         */
+         _refreshWidth: function() {
+            var valueFrame = this.$('.value-field-frame');
+            // update left and right margins of value field frame
+            var leftWidth = this.$('.choice-filter .dropdown-toggle').outerWidth();
+            var rightWidth = this.$('.filter-update').outerWidth();
+            valueFrame.css('margin-left', leftWidth);
+            valueFrame.css('padding-right', rightWidth);
         },
 
         /**
