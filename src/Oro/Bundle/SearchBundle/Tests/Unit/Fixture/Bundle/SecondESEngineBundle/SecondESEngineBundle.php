4390c5fa6425eb60003ede2b58a0fc64ec719d59
Igor Galaev <igalayev@magecore.com>
2016-08-09 15:00:06 +0300

    BB-4071: Make sure OroSeachBundle loads all required files from oro/search_engine directories - add loading yaml engine configuration files from all bundles - add unit test

---
+++ src/Oro/Bundle/SearchBundle/Tests/Unit/Fixture/Bundle/SecondESEngineBundle/SecondESEngineBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Oro\Bundle\SearchBundle\Tests\Unit\Fixture\Bundle\SecondESEngineBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SecondESEngineBundle extends Bundle
+{
+}
