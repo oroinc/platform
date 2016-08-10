f59c93a06a906fceebe07fc3a8d9e3e0b05b63d7
Yauheni Kasenka <beta.screech@gmail.com>
2016-08-10 14:44:51 +0300

    BAP-11409: Create Language entity - created entity

---
+++ src/Oro/Bundle/TranslationBundle/Entity/Language.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace Oro\Bundle\TranslationBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+use Oro\Bundle\EntityBundle\EntityProperty\DatesAwareInterface;
+use Oro\Bundle\EntityBundle\EntityProperty\DatesAwareTrait;
+use Oro\Bundle\EntityConfigBundle\Metadata\Annotation\Config;
+use Oro\Bundle\EntityConfigBundle\Metadata\Annotation\ConfigField;
+use Oro\Bundle\OrganizationBundle\Entity\OrganizationAwareInterface;
+use Oro\Bundle\UserBundle\Entity\Ownership\AuditableUserAwareTrait;
+
+/**
+ * @ORM\Table(name="oro_language")
+ * @ORM\Entity
+ * @ORM\HasLifecycleCallbacks()
+ * @Config(
+ *      defaultValues={
+ *          "entity"={
+ *              "icon"="icon-flag"
+ *          },
+ *          "ownership"={
+ *              "owner_type"="USER",
+ *              "owner_field_name"="owner",
+ *              "owner_column_name"="user_owner_id",
+ *              "organization_field_name"="organization",
+ *              "organization_column_name"="organization_id"
+ *          },
+ *          "security"={
+ *              "type"="ACL",
+ *              "group_name"=""
+ *          }
+ *      }
+ * )
+ */
+class Language implements DatesAwareInterface, OrganizationAwareInterface
+{
+    use AuditableUserAwareTrait;
+    use DatesAwareTrait;
+
+    /**
+     * @var int
+     *
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=16, nullable=false)
+     */
+    protected $code;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(type="boolean")
+     */
+    protected $enabled;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="installed_build_date", type="date", nullable=true)
+     */
+    protected $instaledBuildDate;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $code
+     * @return $this
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * @param \DateTime $instaledBuildDate
+     * @return $this
+     */
+    public function setInstalledBuildDate(\DateTime $instaledBuildDate = null)
+    {
+        $this->instaledBuildDate = $instaledBuildDate;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getInstalledBuildDate()
+    {
+        return $this->instaledBuildDate;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isEnabled()
+    {
+        return $this->enabled;
+    }
+
+    /**
+     * @param bool $enabled
+     *
+     * @return $this
+     */
+    public function setEnabled($enabled)
+    {
+        $this->enabled = (bool)$enabled;
+
+        return $this;
+    }
+}
