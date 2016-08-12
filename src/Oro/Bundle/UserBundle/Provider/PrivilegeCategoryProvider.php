b2f7c47bc1ca580ae174dc8ed92f027248d2874f
Dragos Andries <dandries@pitechnologies.ro>
2016-08-12 12:35:39 +0300

    BB-3970: Account user role permission groups - added new categories in PrivilegeCategoryProvider - updated acls for entity permissions or actions (capabilities) with corresponding categories

--- src/Oro/Bundle/UserBundle/Provider/PrivilegeCategoryProvider.php
+++ src/Oro/Bundle/UserBundle/Provider/PrivilegeCategoryProvider.php
@@ -22,18 +22,61 @@ class PrivilegeCategoryProvider implements PrivilegeCategoryProviderInterface
     public function getRolePrivilegeCategory()
     {
         $categoryList = [];
-        $categoryList[] =
-            new PrivilegeCategory(
-                PrivilegeCategoryProviderInterface::DEFAULT_ACTION_CATEGORY,
-                'oro.user.privilege.category.account_management.label',
-                true,
-                0
-            );
-        $categoryList[] = new PrivilegeCategory('address', 'oro.user.privilege.category.address.label', false, 10);
-        $categoryList[] =
-            new PrivilegeCategory('application', 'oro.user.privilege.category.application.label', false, 20);
-        $categoryList[] = new PrivilegeCategory('calendar', 'oro.user.privilege.category.calendar.label', false, 30);
-        $categoryList[] = new PrivilegeCategory('entity', 'oro.user.privilege.category.entity.label', false, 40);
+        $categoryList[] = new PrivilegeCategory(
+            PrivilegeCategoryProviderInterface::DEFAULT_ACTION_CATEGORY,
+            'oro.user.privilege.category.account_management.label',
+            true,
+            0
+        );
+        $categoryList[] = new PrivilegeCategory(
+            'shopping',
+            'orob2b.account.accountuserrole.privilege.category.shopping.label',
+            true,
+            10
+        );
+        $categoryList[] = new PrivilegeCategory(
+            'quotes',
+            'orob2b.account.accountuserrole.privilege.category.quotes.label',
+            true,
+            20
+        );
+        $categoryList[] = new PrivilegeCategory(
+            'checkout',
+            'orob2b.account.accountuserrole.privilege.category.checkout.label',
+            true,
+            30
+        );
+        $categoryList[] = new PrivilegeCategory(
+            'orders',
+            'orob2b.account.accountuserrole.privilege.category.orders.label',
+            true,
+            40
+        );
+
+        $categoryList[] = new PrivilegeCategory(
+            'address',
+            'oro.user.privilege.category.address.label',
+            false,
+            60
+        );
+        $categoryList[] = new PrivilegeCategory(
+            'application',
+            'oro.user.privilege.category.application.label',
+            false,
+            70
+        );
+        $categoryList[] = new PrivilegeCategory(
+            'calendar',
+            'oro.user.privilege.category.calendar.label',
+            false,
+            80
+        );
+        $categoryList[] = new PrivilegeCategory(
+            'entity',
+            'oro.user.privilege.category.entity.label',
+            false,
+            90
+        );
 
         return $categoryList;
     }
