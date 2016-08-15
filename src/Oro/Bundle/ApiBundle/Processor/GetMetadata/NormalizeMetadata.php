cd83d3fcde552bf8df5b979d6aca3c5ca1e82ec1
Vova Soroka <vsoroka@magecore.com>
2016-08-15 08:56:02 +0300

    BAP-11304: Disable API resources that have no sense to have in application. Fix load metadata for virtual relationships

--- src/Oro/Bundle/ApiBundle/Processor/GetMetadata/NormalizeMetadata.php
+++ src/Oro/Bundle/ApiBundle/Processor/GetMetadata/NormalizeMetadata.php
@@ -5,8 +5,11 @@ namespace Oro\Bundle\ApiBundle\Processor\GetMetadata;
 use Oro\Component\ChainProcessor\ContextInterface;
 use Oro\Component\ChainProcessor\ProcessorInterface;
 use Oro\Bundle\ApiBundle\Config\EntityDefinitionConfig;
+use Oro\Bundle\ApiBundle\Config\EntityDefinitionFieldConfig;
+use Oro\Bundle\ApiBundle\Exception\RuntimeException;
 use Oro\Bundle\ApiBundle\Metadata\EntityMetadata;
 use Oro\Bundle\ApiBundle\Metadata\EntityMetadataFactory;
+use Oro\Bundle\ApiBundle\Provider\MetadataProvider;
 use Oro\Bundle\ApiBundle\Util\ConfigUtil;
 use Oro\Bundle\ApiBundle\Util\DoctrineHelper;
 
@@ -32,14 +35,22 @@ class NormalizeMetadata implements ProcessorInterface
     /** @var EntityMetadataFactory */
     protected $entityMetadataFactory;
 
+    /** @var MetadataProvider */
+    protected $metadataProvider;
+
     /**
      * @param DoctrineHelper        $doctrineHelper
      * @param EntityMetadataFactory $entityMetadataFactory
+     * @param MetadataProvider      $metadataProvider
      */
-    public function __construct(DoctrineHelper $doctrineHelper, EntityMetadataFactory $entityMetadataFactory)
-    {
+    public function __construct(
+        DoctrineHelper $doctrineHelper,
+        EntityMetadataFactory $entityMetadataFactory,
+        MetadataProvider $metadataProvider
+    ) {
         $this->doctrineHelper = $doctrineHelper;
         $this->entityMetadataFactory = $entityMetadataFactory;
+        $this->metadataProvider = $metadataProvider;
     }
 
     /**
@@ -58,7 +69,8 @@ class NormalizeMetadata implements ProcessorInterface
         $this->normalizeMetadata(
             $entityMetadata,
             $context->getConfig(),
-            $this->doctrineHelper->isManageableEntityClass($context->getClassName())
+            $this->doctrineHelper->isManageableEntityClass($context->getClassName()),
+            $context
         );
     }
 
@@ -66,11 +78,13 @@ class NormalizeMetadata implements ProcessorInterface
      * @param EntityMetadata         $entityMetadata
      * @param EntityDefinitionConfig $config
      * @param bool                   $processLinkedProperties
+     * @param MetadataContext        $context
      */
     protected function normalizeMetadata(
         EntityMetadata $entityMetadata,
         EntityDefinitionConfig $config,
-        $processLinkedProperties
+        $processLinkedProperties,
+        MetadataContext $context
     ) {
         $linkedPropertyNames = [];
         $fields = $config->getFields();
@@ -88,7 +102,13 @@ class NormalizeMetadata implements ProcessorInterface
                         && $pathCount > 1
                         && !$entityMetadata->hasProperty($fieldName)
                     ) {
-                        $addedPropertyName = $this->processLinkedProperty($entityMetadata, $fieldName, $path);
+                        $addedPropertyName = $this->processLinkedProperty(
+                            $entityMetadata,
+                            $fieldName,
+                            $path,
+                            $config,
+                            $context
+                        );
                         if ($addedPropertyName) {
                             $linkedPropertyNames[] = $addedPropertyName;
                         }
@@ -109,14 +129,21 @@ class NormalizeMetadata implements ProcessorInterface
     }
 
     /**
-     * @param EntityMetadata $entityMetadata
-     * @param string         $propertyName
-     * @param string[]       $propertyPath
+     * @param EntityMetadata         $entityMetadata
+     * @param string                 $propertyName
+     * @param string[]               $propertyPath
+     * @param EntityDefinitionConfig $config
+     * @param MetadataContext        $context
      *
      * @return string|null
      */
-    protected function processLinkedProperty(EntityMetadata $entityMetadata, $propertyName, array $propertyPath)
-    {
+    protected function processLinkedProperty(
+        EntityMetadata $entityMetadata,
+        $propertyName,
+        array $propertyPath,
+        EntityDefinitionConfig $config,
+        MetadataContext $context
+    ) {
         $addedPropertyName = null;
 
         $linkedProperty = array_pop($propertyPath);
@@ -131,6 +158,13 @@ class NormalizeMetadata implements ProcessorInterface
                     $linkedProperty
                 );
                 $associationMetadata->setName($propertyName);
+                $associationMetadata->setTargetMetadata(
+                    $this->getMetadata(
+                        $associationMetadata->getTargetClassName(),
+                        $this->getTargetConfig($config, $propertyName, array_merge($propertyPath, [$linkedProperty])),
+                        $context
+                    )
+                );
                 $entityMetadata->addAssociation($associationMetadata);
             } else {
                 $fieldMetadata = $this->entityMetadataFactory->createFieldMetadata(
@@ -144,5 +178,89 @@ class NormalizeMetadata implements ProcessorInterface
         }
 
         return $addedPropertyName;
+    }
+
+    /**
+     * @param string                 $entityClass
+     * @param EntityDefinitionConfig $config
+     * @param MetadataContext        $context
+     *
+     * @return EntityMetadata
+     */
+    protected function getMetadata($entityClass, EntityDefinitionConfig $config, MetadataContext $context)
+    {
+        $targetMetadata = $this->metadataProvider->getMetadata(
+            $entityClass,
+            $context->getVersion(),
+            $context->getRequestType(),
+            $config,
+            $context->getExtras()
+        );
+        if (null === $targetMetadata) {
+            throw new RuntimeException(sprintf('A metadata for "%s" entity does not exist.', $entityClass));
+        }
+
+        return $targetMetadata;
+    }
+
+    /**
+     * @param EntityDefinitionConfig $config
+     * @param string                 $propertyName
+     * @param string[]               $propertyPath
+     *
+     * @return EntityDefinitionConfig
+     *
+     * @throws RuntimeException if a configuration of the target entity does not exist
+     */
+    protected function getTargetConfig(EntityDefinitionConfig $config, $propertyName, array $propertyPath)
+    {
+        $targetConfig = $config->getField($propertyName)->getTargetEntity();
+        if (null === $targetConfig) {
+            $targetField = $this->findFieldByPropertyPath($config, $propertyPath);
+            if (null === $targetField) {
+                throw new RuntimeException(
+                    sprintf(
+                        'A configuration of "%s" field does not exist.',
+                        implode('.', $propertyPath)
+                    )
+                );
+            }
+            $targetConfig = $targetField->getTargetEntity();
+            if (null === $targetConfig) {
+                throw new RuntimeException(
+                    sprintf(
+                        'A configuration of the target entity for "%s" field does not exist.',
+                        implode('.', $propertyPath)
+                    )
+                );
+            }
+        }
+
+        return $targetConfig;
+    }
+
+    /**
+     * @param EntityDefinitionConfig $config
+     * @param string[]               $propertyPath
+     *
+     * @return EntityDefinitionFieldConfig|null
+     */
+    protected function findFieldByPropertyPath(EntityDefinitionConfig $config, array $propertyPath)
+    {
+        $pathCount = count($propertyPath);
+
+        $targetConfig = $config;
+        for ($i = 0; $i < $pathCount - 1; $i++) {
+            $fieldConfig = $targetConfig->findField($propertyPath[$i], true);
+            if (null === $fieldConfig) {
+                return null;
+            }
+            $targetConfig = $fieldConfig->getTargetEntity();
+            if (null === $targetConfig) {
+                return null;
+            }
+        }
+
+        return $targetConfig->findField($propertyPath[$pathCount - 1], true);
     }
 }
