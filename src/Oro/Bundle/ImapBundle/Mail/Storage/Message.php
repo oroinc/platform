60924b90f8ff0c7ee9a697a0ad7bfe4ddb15aab4
Valentine Velikiy <vvelykyi@magecore.com>
2016-08-16 10:47:05 +0300

    BAP-11306: Email body sync is hung

--- src/Oro/Bundle/ImapBundle/Mail/Storage/Message.php
+++ src/Oro/Bundle/ImapBundle/Mail/Storage/Message.php
@@ -5,7 +5,12 @@ namespace Oro\Bundle\ImapBundle\Mail\Storage;
 use \Zend\Mail\Header\ContentType;
 use \Zend\Mail\Header\HeaderInterface;
 use \Zend\Mail\Storage\Part;
-use \Zend\Mail\Headers;
+use \Zend\Stdlib\ErrorHandler;
+use \Zend\Mail\Storage\AbstractStorage;
+use \Zend\Mail\Storage\Exception;
+
+use Oro\Bundle\EmailBundle\Mail\Headers;
+use Oro\Bundle\EmailBundle\Mime;
 
 class Message extends \Zend\Mail\Storage\Message
 {
@@ -14,7 +19,56 @@ class Message extends \Zend\Mail\Storage\Message
      */
     public function __construct(array $params)
     {
-        parent::__construct($params);
+        if (isset($params['file'])) {
+            if (!is_resource($params['file'])) {
+                ErrorHandler::start();
+                $params['raw'] = file_get_contents($params['file']);
+                $error = ErrorHandler::stop();
+                if ($params['raw'] === false) {
+                    throw new Exception\RuntimeException('could not open file', 0, $error);
+                }
+            } else {
+                $params['raw'] = stream_get_contents($params['file']);
+            }
+        }
+
+        if (!empty($params['flags'])) {
+            // set key and value to the same value for easy lookup
+            $this->flags = array_combine($params['flags'], $params['flags']);
+        }
+
+        if (isset($params['handler'])) {
+            if (!$params['handler'] instanceof AbstractStorage) {
+                throw new Exception\InvalidArgumentException('handler is not a valid mail handler');
+            }
+            if (!isset($params['id'])) {
+                throw new Exception\InvalidArgumentException('need a message id with a handler');
+            }
+
+            $this->mail       = $params['handler'];
+            $this->messageNum = $params['id'];
+        }
+
+        $params['strict'] = isset($params['strict']) ? $params['strict'] : false;
+
+        if (isset($params['raw'])) {
+            Mime\Decode::splitMessage($params['raw'], $this->headers, $this->content, Mime\Mime::LINEEND, $params['strict']);
+        } elseif (isset($params['headers'])) {
+            if (is_array($params['headers'])) {
+                $this->headers = new Headers();
+                $this->headers->addHeaders($params['headers']);
+            } else {
+                if (empty($params['noToplines'])) {
+                    Mime\Decode::splitMessage($params['headers'], $this->headers, $this->topLines);
+                } else {
+                    $this->headers = Headers::fromString($params['headers']);
+                }
+            }
+
+            if (isset($params['content'])) {
+                $this->content = $params['content'];
+            }
+        }
     }
 
     /**
