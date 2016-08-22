91cb1bb297c46727730d2956b39d581fe9c205ea
Ivan Shirko <ivan.shirko@effective-soft.com>
2016-08-22 17:03:45 +0300

    BAP-11418: Implement Upload (Import) functionality - remove not needed import classes - add unit tests - add denormalizer - add strategies

---
+++ src/Oro/Bundle/TranslationBundle/ImportExport/Serializer/TranslationNormalizer.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Oro\Bundle\TranslationBundle\ImportExport\Serializer;
+
+use Oro\Bundle\ImportExportBundle\Exception\UnexpectedValueException;
+use Oro\Bundle\ImportExportBundle\Serializer\Normalizer\DenormalizerInterface;
+
+use Oro\Bundle\TranslationBundle\Entity\Translation;
+
+class TranslationNormalizer implements DenormalizerInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function denormalize($data, $class, $format = null, array $context = array())
+    {
+        if (!is_array($data) || !isset($data['key'], $data['locale'])) {
+            throw new UnexpectedValueException('Incorrect record format');
+        }
+        if ($data['locale'] !== $context['language_code']) {
+            throw new UnexpectedValueException('Locale should be ' . $context['language_code']);
+        }
+        $translation = new Translation();
+        $translation->setLocale($context['language_code']);
+        $translation->setDomain($data['domain']);
+        $translation->setKey($data['key']);
+        $translation->setValue($data['value']);
+
+        return $translation;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsDenormalization($data, $type, $format = null, array $context = array())
+    {
+        return $type === Translation::class && !empty($context['language_code']);
+    }
+}
--- src/Oro/Bundle/TranslationBundle/ImportExport/Strategy/TranslationImportStrategy.php
+++ src/Oro/Bundle/TranslationBundle/ImportExport/Strategy/TranslationImportStrategy.php
@@ -3,7 +3,23 @@
 namespace Oro\Bundle\TranslationBundle\ImportExport\Strategy;
 
 use Oro\Bundle\ImportExportBundle\Strategy\Import\ConfigurableAddOrReplaceStrategy;
+use Oro\Bundle\TranslationBundle\Entity\Translation;
 
 class TranslationImportStrategy extends ConfigurableAddOrReplaceStrategy
 {
+    /**
+     * {@inheritdoc}
+     * @param Translation $entity
+     */
+    protected function findExistingEntity($entity, array $searchContext = [])
+    {
+        return $this->databaseHelper->findOneBy(
+            Translation::class,
+            [
+               'locale' => $entity->getLocale(),
+               'domain' => $entity->getDomain(),
+               'key' => $entity->getKey(),
+            ]
+        );
+    }
 }
