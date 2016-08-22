d496d8b721c7baf25823a2ea726a1b5411b8eeeb
Alexander Kozienko <pascalamsg@gmail.com>
2016-08-22 11:59:33 +0300

    BAP-11162: unique jobs
    - DependentJobContext

--- src/Oro/Component/MessageQueue/Job/DependentJobService.php
+++ src/Oro/Component/MessageQueue/Job/DependentJobService.php
@@ -9,75 +9,24 @@ class DependentJobService
     private $jobStorage;
 
     /**
-     * @var Job
-     */
-    private $rootJob;
-
-    /**
      * @param JobStorage|null $jobStorage
-     * @param Job|null $rootJob
      */
-    public function __construct(JobStorage $jobStorage = null, Job $rootJob = null)
+    public function __construct(JobStorage $jobStorage)
     {
         $this->jobStorage = $jobStorage;
-        $this->rootJob = $rootJob;
     }
 
-    /**
-     * @param string $topic
-     * @param string|array $message
-     * @param int|null $priority
-     */
-    public function addDependentJob($topic, $message, $priority = null)
+    public function saveDependentJob(DependentJobContext $context)
     {
-        if (! $this->rootJob) {
-            throw new \LogicException('Root job is not set');
+        if (! $context->getJob()->isRoot()) {
+            throw new \LogicException('Only root job allowed');
         }
 
-        $data = $this->rootJob->getData();
+        $this->jobStorage->saveJob($context->getJob(), function (Job $job) use ($context) {
+            $data = $job->getData();
+            $data['dependentJobs'] = $context->getDependentJobs();
 
-        $dependentJob = [
-            'topic' => $topic,
-            'message' => $message,
-        ];
-
-        if ($priority) {
-            $dependentJob['priority'] = $priority;
-        }
-
-        $data['dependentJobs'][] = $dependentJob;
-
-        $this->rootJob->setData($data);
-    }
-
-    /**
-     * @param Job $rootJob
-     * @param \Closure $callback
-     */
-    public function setDependentJob(Job $rootJob, \Closure $callback)
-    {
-        if ($this->rootJob) {
-            throw new \LogicException('Is not allowed to call method if rootJob is set');
-        }
-
-        if (! $this->jobStorage) {
-            throw new \LogicException('Job storage is not set');
-        }
-
-        if (! $rootJob->isRoot()) {
-            throw new \InvalidArgumentException(sprintf(
-                'Only root jobs allowed but got child. id:"%s"',
-                $rootJob->getId()
-            ));
-        }
-
-        $this->jobStorage->saveJob($rootJob, function (Job $rootJob) use ($callback) {
-            $data = $rootJob->getData();
-            $data['dependentJobs'] = [];
-
-            $rootJob->setData($data);
-
-            $callback(new static(null, $rootJob));
+            $job->setData($data);
         });
     }
 }
