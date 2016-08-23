629f36434969aa891fc7de39327dbb7595ef2101
Alexander Pobereznichenko <opobereznichenko@magecore.com>
2016-08-23 16:20:51 +0300

    BAP-11493: Add kernel event listener to block access to disabled route

---
+++ src/Oro/Bundle/FeatureToggleBundle/EventListener/RequestListener.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Oro\Bundle\FeatureToggleBundle\EventListener;
+
+use Oro\Bundle\FeatureToggleBundle\Checker\FeatureChecker;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+
+class RequestListener
+{
+    /**
+     * @var FeatureChecker
+     */
+    protected $featureChecker;
+
+    /**
+     * @param FeatureChecker $featureChecker
+     */
+    public function __construct(FeatureChecker $featureChecker)
+    {
+        $this->featureChecker = $featureChecker;
+    }
+
+    /**
+     * @param GetResponseEvent $event
+     */
+    public function onRequest(GetResponseEvent $event)
+    {
+        if (!$event->isMasterRequest()) {
+            return;
+        }
+
+        $request = $event->getRequest();
+        $route = $request->get('_route');
+        if (!$this->featureChecker->isResourceEnabled($route, 'route', 'website')) {
+            $response = new Response('This feature is disabled', Response::HTTP_NOT_FOUND);
+            $event->setResponse($response);
+        }
+    }
+}
