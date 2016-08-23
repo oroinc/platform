3b892789d0e7c149dad75ba10df645488f2f86e8
Péter Vass <pvass@oroinc.com>
2016-08-23 11:08:06 +0200

    BB-4058: Add sorting support  - Created SearchSorterExtension + unit test

---
+++ src/Oro/Bundle/SearchBundle/Extension/Sorter/Configuration.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Oro\Bundle\SearchBundle\Extension\Sorter;
+
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+use Oro\Bundle\DataGridBundle\Extension\Formatter\Property\PropertyInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    const SORTERS_PATH         = '[sorters]';
+    const COLUMNS_PATH         = '[sorters][columns]';
+    const MULTISORT_PATH       = '[sorters][multiple_sorting]';
+    const DEFAULT_SORTERS_PATH = '[sorters][default]';
+    const TOOLBAR_SORTING_PATH = '[sorters][toolbar_sorting]';
+
+    /**
+     *{@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $builder = new TreeBuilder();
+
+        $builder->root('sorters')
+            ->children()
+                ->arrayNode('columns')
+                    ->prototype('array')
+                        ->children()
+                            ->scalarNode(PropertyInterface::DATA_NAME_KEY)->isRequired()->end()
+                            ->booleanNode(PropertyInterface::DISABLED_KEY)->end()
+                            ->scalarNode(PropertyInterface::TYPE_KEY)->end()
+                            ->variableNode('apply_callback')->end()
+                        ->end()
+                    ->end()
+                ->end()
+                ->arrayNode('default')
+                    ->prototype('enum')
+                        ->values([SearchSorterExtension::DIRECTION_DESC, SearchSorterExtension::DIRECTION_ASC])->end()
+                    ->end()
+                    ->booleanNode('multiple_sorting')->end()
+                    ->booleanNode('toolbar_sorting')->end()
+                ->end()
+            ->end()
+        ;
+
+        return $builder;
+    }
+}
