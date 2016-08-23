68ea956c66ae538b302ff44ba031d886ac91035e
Dincho Todorov <dincho@users.noreply.github.com>
2016-08-23 18:21:52 +0300

    CRM-5284: Paste HTML content into email templates
    - disable HTML sanitize in email and email template forms based on configuration

--- src/Oro/Bundle/FormBundle/Resources/public/js/app/views/wysiwig-editor/txt-html-transformer.js
+++ src/Oro/Bundle/FormBundle/Resources/public/js/app/views/wysiwig-editor/txt-html-transformer.js
@@ -7,7 +7,9 @@ define(function() {
             return '<p>' + String(content).replace(/(\n\r?|\r\n?)/g, '</p><p>') + '</p>';
         },
         html2text: function(content) {
-            return String(content).replace(/(<\/?[^>]+>|&[^;]+;)/g, '');
+            return String(content)
+                    .replace(/<head>[^]*<\/head>/, '')
+                    .replace(/(<\/?[^>]+>|&[^;]+;)/g, '');
         }
     };
 });
