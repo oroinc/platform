902f36f9964d166bb67124476c169bfdc33453ee
Andrii Muzalevskyi <face2world@gmail.com>
2016-08-25 12:08:47 +0300

    BAP-11327 allow linebreak between time and date part in datetime cell

--- src/Oro/Bundle/DataGridBundle/Resources/public/js/datagrid/formatter/datetime-formatter.js
+++ src/Oro/Bundle/DataGridBundle/Resources/public/js/datagrid/formatter/datetime-formatter.js
@@ -30,7 +30,8 @@ define(['underscore', 'backgrid', 'orolocale/js/formatter/datetime'
                 return '';
             }
             // Call one of formatDate formatTime formatDateTime
-            return this._getFormatterFunction('format').call(DateTimeFormatter, rawData, true);
+            return this._getFormatterFunction('format', this.type === 'dateTime' ? 'NBSP' : undefined)
+                .call(DateTimeFormatter, rawData, true);
         },
 
         /**
