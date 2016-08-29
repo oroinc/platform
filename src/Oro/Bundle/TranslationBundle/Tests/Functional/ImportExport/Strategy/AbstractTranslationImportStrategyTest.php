2b30763ff82552adbe5b9627019d968397a8c00d
Ivan Shirko <ivan.shirko@effective-soft.com>
2016-08-30 01:23:33 +0300

    BAP-11423: Update functional tests - add controller tests - add operations tests

---
+++ src/Oro/Bundle/TranslationBundle/Tests/Functional/ImportExport/Strategy/AbstractTranslationImportStrategyTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Oro\Bundle\TranslationBundle\Tests\Functional\ImportExport\Strategy;
+
+use Doctrine\Common\Persistence\ObjectManager;
+
+use Oro\Bundle\ImportExportBundle\Context\Context;
+use Oro\Bundle\ImportExportBundle\Strategy\Import\AbstractImportStrategy;
+use Oro\Bundle\TestFrameworkBundle\Test\WebTestCase;
+use Oro\Bundle\TranslationBundle\Entity\Translation;
+use Oro\Bundle\TranslationBundle\Tests\Functional\DataFixtures\LoadTranslations;
+
+/**
+ * @dbIsolation
+ */
+abstract class AbstractTranslationImportStrategyTest extends WebTestCase
+{
+    /**
+     * @var AbstractImportStrategy
+     */
+    protected $strategy;
+
+    protected function setUp()
+    {
+        $this->initClient([], $this->generateBasicAuthHeader());
+
+        $this->loadFixtures(
+            [
+                LoadTranslations::class,
+            ]
+        );
+
+        $this->strategy = $this->getStrategyObject();
+        $this->strategy->setImportExportContext(new Context([]));
+        $this->strategy->setEntityName(Translation::class);
+    }
+
+    protected function tearDown()
+    {
+        unset($this->strategy);
+    }
+
+    /**
+     * @param string $locale
+     * @return int
+     */
+    protected function getTranslationsByLocaleCount($locale)
+    {
+        return count($this->getEntityManager()->getRepository(Translation::class)->findBy(['locale' => $locale]));
+    }
+
+    /**
+     * @param Translation $translation
+     * @return Translation
+     */
+    protected function processTranslation(Translation $translation)
+    {
+        $translation = $this->strategy->process($translation);
+
+        $em = $this->getEntityManager();
+        $em->persist($translation);
+        $em->flush();
+
+        return $translation;
+    }
+
+    /**
+     * @return ObjectManager
+     */
+    private function getEntityManager()
+    {
+        return $this->getContainer()
+            ->get('doctrine')
+            ->getManagerForClass(Translation::class);
+    }
+
+    /**
+     * @return AbstractImportStrategy
+     */
+    abstract protected function getStrategyObject();
+}
