e0c7ddaa9a8029dcd684b0cc9fc4d9f97d832674
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2016-08-31 14:43:56 +0300

    BAP-11647: Add dbal lazy connection
    - add lazy dbal connection

--- src/Oro/Bundle/MessageQueueBundle/DependencyInjection/OroMessageQueueExtension.php
+++ src/Oro/Bundle/MessageQueueBundle/DependencyInjection/OroMessageQueueExtension.php
@@ -7,11 +7,11 @@ use Oro\Component\MessageQueue\Client\NullDriver;
 use Oro\Component\MessageQueue\Client\TraceableMessageProducer;
 use Oro\Component\MessageQueue\DependencyInjection\TransportFactoryInterface;
 use Oro\Component\MessageQueue\Transport\Dbal\DbalConnection;
+use Oro\Component\MessageQueue\Transport\Dbal\DbalLazyConnection;
 use Oro\Component\MessageQueue\Transport\Null\NullConnection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
@@ -66,6 +66,7 @@ class OroMessageQueueExtension extends Extension
             $driverFactory->replaceArgument(0, [
                 NullConnection::class => NullDriver::class,
                 DbalConnection::class => DbalDriver::class,
+                DbalLazyConnection::class => DbalDriver::class,
             ]);
 
             $configDef = $container->getDefinition('oro_message_queue.client.config');
@@ -77,16 +78,11 @@ class OroMessageQueueExtension extends Extension
             ]);
 
             if (false == empty($config['client']['traceable_producer'])) {
-                $container->setDefinition(
-                    'oro_message_queue.client.internal_message_producer',
-                    $container->getDefinition('oro_message_queue.client.message_producer')
-                );
-
-                $traceableMessageProducer = new Definition(TraceableMessageProducer::class, [
-                    new Reference('oro_message_queue.client.internal_message_producer')
-                ]);
-
-                $container->setDefinition('oro_message_queue.client.message_producer', $traceableMessageProducer);
+                $producerId = 'oro_message_queue.client.traceable_message_producer';
+                $container->register($producerId, TraceableMessageProducer::class)
+                    ->setDecoratedService('oro_message_queue.client.message_producer')
+                    ->addArgument(new Reference('oro_message_queue.client.traceable_message_producer.inner'))
+                ;
             }
 
             $delayRedeliveredExtension = $container->getDefinition(
---
+++ src/Oro/Component/MessageQueue/Tests/Unit/Transport/Dbal/DbalLazyConnectionTest.php
@@ -0,0 +1,171 @@
+<?php
+namespace Oro\Component\MessageQueue\Tests\Unit\Transport\Dbal;
+
+use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\DBAL\Connection;
+use Oro\Component\MessageQueue\Transport\Dbal\DbalConnection;
+use Oro\Component\MessageQueue\Transport\Dbal\DbalLazyConnection;
+use Oro\Component\Testing\ClassExtensionTrait;
+
+class DbalLazyConnectionTest extends \PHPUnit_Framework_TestCase
+{
+    use ClassExtensionTrait;
+
+    public function testShouldImplementConnectionInterface()
+    {
+        self::assertClassExtends(DbalConnection::class, DbalLazyConnection::class);
+    }
+    
+    public function testCouldBeConstructedWithRequiredArguments()
+    {
+        new DbalLazyConnection($this->createManagerRegistryStub(), 'connection', 'table');
+    }
+
+    public function testShouldNotBeInitOnConstruct()
+    {
+        $connection = new DbalLazyConnection($this->createManagerRegistryStub(), 'connection', 'table');
+
+        self::assertConnectionIsNotInit($connection);
+    }
+
+    public function testShouldInitDBALConnectionUsingRegistryAndConnectionName()
+    {
+        $dbalConnection = $this->createDBALConnectionMock();
+
+        $registry = $this->createManagerRegistryMock();
+        $registry
+            ->expects(self::once())
+            ->method('getConnection')
+            ->with('theConnection')
+            ->willReturn($dbalConnection)
+        ;
+
+        $connection = new DbalLazyConnection($registry, 'theConnection', 'table');
+
+        //guard
+        self::assertConnectionIsNotInit($connection);
+
+        self::assertSame($dbalConnection, $connection->getDBALConnection());
+        self::assertConnectionIsInit($connection);
+    }
+
+    public function testShouldNotInitOnCreateSessionMethodCall()
+    {
+        $connection = new DbalLazyConnection($this->createManagerRegistryStub(), 'connection', 'table');
+
+        //guard
+        self::assertConnectionIsNotInit($connection);
+
+        $session = $connection->createSession();
+
+        self::assertAttributeInstanceOf(DbalLazyConnection::class, 'connection', $session);
+        self::assertConnectionIsNotInit($connection);
+    }
+
+    public function testShouldNotInitOnGetTableNameCall()
+    {
+        $connection = new DbalLazyConnection($this->createManagerRegistryStub(), 'connection', 'theTable');
+
+        //guard
+        self::assertConnectionIsNotInit($connection);
+
+        self::assertEquals('theTable', $connection->getTableName());
+
+        self::assertConnectionIsNotInit($connection);
+    }
+
+    public function testShouldNotInitOnGetOptionsCall()
+    {
+        $options = ['foo' => 'fooVal', 'bar' => 'barVal'];
+
+        $connection = new DbalLazyConnection($this->createManagerRegistryStub(), 'connection', 'table', $options);
+
+        //guard
+        self::assertConnectionIsNotInit($connection);
+
+        self::assertEquals($options, $connection->getOptions());
+
+        self::assertConnectionIsNotInit($connection);
+    }
+
+    public function testShouldNotInitOnCloseIfNotInit()
+    {
+        $dbalConnection = $this->createDBALConnectionMock();
+        $dbalConnection
+            ->expects(self::never())
+            ->method('close')
+        ;
+
+        $registry = $this->createManagerRegistryStub($dbalConnection);
+
+        $connection = new DbalLazyConnection($registry, 'connection', 'table');
+
+        //guard
+        self::assertConnectionIsNotInit($connection);
+
+        $connection->close();
+
+        self::assertConnectionIsNotInit($connection);
+    }
+
+    public function testShouldCallCloseInitOnCloseIfNotInit()
+    {
+        $dbalConnection = $this->createDBALConnectionMock();
+        $dbalConnection
+            ->expects(self::once())
+            ->method('close')
+        ;
+
+        $registry = $this->createManagerRegistryStub($dbalConnection);
+
+        $connection = new DbalLazyConnection($registry, 'connection', 'table');
+
+        $connection->getDBALConnection();
+
+        //guard
+        self::assertConnectionIsInit($connection);
+
+        $connection->close();
+    }
+
+    private static function assertConnectionIsNotInit(DbalLazyConnection $connection)
+    {
+        self::assertAttributeSame(false, 'isInit', $connection);
+    }
+
+    private static function assertConnectionIsInit(DbalLazyConnection $connection)
+    {
+        self::assertAttributeSame(true, 'isInit', $connection);
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject|ManagerRegistry
+     */
+    private function createManagerRegistryMock()
+    {
+        return $this->getMock(ManagerRegistry::class);
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject|ManagerRegistry
+     */
+    private function createManagerRegistryStub($connection = null)
+    {
+        $registryMock = $this->createManagerRegistryMock();
+        $registryMock
+            ->expects(self::any())
+            ->method('getConnection')
+            ->willReturn($connection)
+        ;
+
+        return $registryMock;
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject|Connection
+     */
+    private function createDBALConnectionMock()
+    {
+        return $this->getMock(Connection::class, [], [], '', false);
+    }
+}
