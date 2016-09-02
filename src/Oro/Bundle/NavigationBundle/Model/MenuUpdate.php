e8db39d7fa4d598f007069088dd18d45d83f76f9
Vitaly Shutko <sokal32@gmail.com>
2016-09-02 09:56:32 +0300

    BB-4394: Create model MenuUpdate in NavigationBundle - created MenuUpdate model

---
+++ src/Oro/Bundle/NavigationBundle/Model/MenuUpdate.php
@@ -0,0 +1,251 @@
+<?php
+
+namespace Oro\Bundle\NavigationBundle\Model;
+
+abstract class MenuUpdate
+{
+    const OWNERSHIP_GLOBAL        = 1;
+    const OWNERSHIP_ORGANIZATION  = 2;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="key", type="string", length=100)
+     */
+    private $key;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="parent_id", type="string", length=100)
+     */
+    private $parentId;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="menu", type="string", length=100)
+     */
+    private $menu;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="ownership_type", type="integer")
+     */
+    private $ownershipType;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="owner_id", type="integer")
+     */
+    private $ownerId;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="is_active", type="boolean")
+     */
+    private $isActive;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="priority", type="integer")
+     */
+    private $priority;
+
+    /**
+     * Get array of extra data that is not declared in MenuUpdate model
+     * @return array
+     */
+    abstract public function getExtras();
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return MenuUpdate
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getKey()
+    {
+        return $this->key;
+    }
+
+    /**
+     * @param string $key
+     * @return MenuUpdate
+     */
+    public function setKey($key)
+    {
+        $this->key = $key;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getParentId()
+    {
+        return $this->parentId;
+    }
+
+    /**
+     * @param string $parentId
+     * @return MenuUpdate
+     */
+    public function setParentId($parentId)
+    {
+        $this->parentId = $parentId;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     * @return MenuUpdate
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMenu()
+    {
+        return $this->menu;
+    }
+
+    /**
+     * @param string $menu
+     * @return MenuUpdate
+     */
+    public function setMenu($menu)
+    {
+        $this->menu = $menu;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getOwnershipType()
+    {
+        return $this->ownershipType;
+    }
+
+    /**
+     * @param int $ownershipType
+     * @return MenuUpdate
+     */
+    public function setOwnershipType($ownershipType)
+    {
+        $this->ownershipType = $ownershipType;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getOwnerId()
+    {
+        return $this->ownerId;
+    }
+
+    /**
+     * @param int $ownerId
+     * @return MenuUpdate
+     */
+    public function setOwnerId($ownerId)
+    {
+        $this->ownerId = $ownerId;
+
+        return $this;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isIsActive()
+    {
+        return $this->isActive;
+    }
+
+    /**
+     * @param boolean $isActive
+     * @return MenuUpdate
+     */
+    public function setIsActive($isActive)
+    {
+        $this->isActive = $isActive;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPriority()
+    {
+        return $this->priority;
+    }
+
+    /**
+     * @param int $priority
+     * @return MenuUpdate
+     */
+    public function setPriority($priority)
+    {
+        $this->priority = $priority;
+
+        return $this;
+    }
+}
