fea6df6427a3e1a91ef30b97f0a3c1e22c149e2d
Dmitry Khrysev <dkhrysev@magecore.com>
2016-09-02 18:53:16 +0300

    BAP-11657: Move API exclusion provider logic to API processor - fixed service names

--- src/Oro/Bundle/ApiBundle/Processor/Shared/EntityTypeFeatureCheck.php
+++ src/Oro/Bundle/ApiBundle/Processor/Shared/EntityTypeFeatureCheck.php
@@ -10,7 +10,7 @@ use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 /**
  * Validates whether an feature is enabled for the type of entities specified
- * in the "parentClass" property of the Context.
+ * in the "class" property of the Context.
  */
 class EntityTypeFeatureCheck implements ProcessorInterface
 {
