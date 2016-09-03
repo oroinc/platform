ae21082096d259d2aff8b5d09392fe97bb819d97
anyt <yatsenco@gmail.com>
2016-09-03 13:05:04 +0300

    BAP-11644: Broken labels for grid-related actions and UI controls - updated DefaultTranslationStrategy to use all enabled languages in fallback strategy instead of only active strategy.

--- src/Oro/Bundle/TranslationBundle/Strategy/DefaultTranslationStrategy.php
+++ src/Oro/Bundle/TranslationBundle/Strategy/DefaultTranslationStrategy.php
@@ -2,28 +2,28 @@
 
 namespace Oro\Bundle\TranslationBundle\Strategy;
 
+use Oro\Bundle\ConfigBundle\Config\ConfigManager;
 use Oro\Bundle\LocaleBundle\Model\LocaleSettings;
 use Oro\Bundle\LocaleBundle\DependencyInjection\Configuration;
+use Oro\Bundle\TranslationBundle\Translation\TranslationStatusInterface;
 
 class DefaultTranslationStrategy implements TranslationStrategyInterface
 {
     const NAME = 'default';
 
-    /**
-     * @var LocaleSettings
-     */
-    protected $localeSettings;
+    /** @var ConfigManager */
+    protected $cm;
 
     /** @var bool */
     protected $installed = false;
 
     /**
      * @param LocaleSettings $localeSettings
-     * @param bool $installed
+     * @param bool           $installed
      */
-    public function __construct(LocaleSettings $localeSettings, $installed = false)
+    public function __construct(ConfigManager $cm, $installed = false)
     {
-        $this->localeSettings = $localeSettings;
+        $this->cm = $cm;
         $this->installed = (bool)$installed;
     }
 
@@ -41,12 +41,12 @@ class DefaultTranslationStrategy implements TranslationStrategyInterface
     public function getLocaleFallbacks()
     {
         // default strategy has only one fallback to default locale
+        $locales = [];
         if ($this->installed) {
-            $locales = [
-                Configuration::DEFAULT_LOCALE => [
-                    $this->localeSettings->getLanguage() => []
-                ]
-            ];
+            $installedLocales = (array)$this->cm->get(TranslationStatusInterface::CONFIG_KEY);
+            foreach ($installedLocales as $code => $installedLocale) {
+                $locales[Configuration::DEFAULT_LOCALE][$code] = [];
+            }
         } else {
             $locales = [
                 Configuration::DEFAULT_LOCALE => []
