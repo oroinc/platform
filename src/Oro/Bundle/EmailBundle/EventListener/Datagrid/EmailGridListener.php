3e5565dc2f1ae410041a8d8656aae0376c8855d5
Miloslav Nenadal <miloslav.nenadal@imatic.cz>
2016-09-05 11:13:19 +0200

    BAP-11005: Performance of My Emails grid with default 100 records per page

--- src/Oro/Bundle/EmailBundle/EventListener/Datagrid/EmailGridListener.php
+++ src/Oro/Bundle/EmailBundle/EventListener/Datagrid/EmailGridListener.php
@@ -2,19 +2,39 @@
 
 namespace Oro\Bundle\EmailBundle\EventListener\Datagrid;
 
+use Doctrine\Bundle\DoctrineBundle\Registry;
 use Doctrine\ORM\QueryBuilder;
 
-use Oro\Bundle\DataGridBundle\Datasource\Orm\OrmDatasource;
-use Oro\Bundle\DataGridBundle\Event\BuildAfter;
-use Oro\Bundle\EmailBundle\Datagrid\EmailQueryFactory;
 use Oro\Bundle\DataGridBundle\Datagrid\ParameterBag;
+use Oro\Bundle\DataGridBundle\Datasource\Orm\OrmDatasource;
+use Oro\Bundle\DataGridBundle\Datasource\ResultRecord;
+use Oro\Bundle\DataGridBundle\Event\BuildAfter;
+use Oro\Bundle\DataGridBundle\Event\OrmResultAfter;
+use Oro\Bundle\DataGridBundle\Event\OrmResultBeforeQuery;
+use Oro\Bundle\EmailBundle\Datagrid\EmailQueryFactory;
+use Oro\Component\DoctrineUtils\ORM\QueryUtils;
 
 class EmailGridListener
 {
     /**
+     * @var Registry
+     */
+    protected $registry;
+
+    /**
      * @var EmailQueryFactory
      */
     protected $factory;
+
+    /**
+     * @var QueryBuilder|null
+     */
+    protected $qb;
+
+    /**
+     * @var string|null
+     */
+    protected $select;
 
     /**
      * @param EmailQueryFactory $factory
@@ -47,6 +67,57 @@ class EmailGridListener
         }
 
         $this->prepareQueryToFilter($parameters, $queryBuilder);
+    }
+
+    /**
+     * @param OrmResultBeforeQuery $event
+     */
+    public function onResultBeforeQuery(OrmResultBeforeQuery $event)
+    {
+        $this->qb = $event->getQueryBuilder();
+
+        $selectParts = $this->qb->getDqlPart('select');
+        $stringSelectParts = [];
+        foreach ($selectParts as $selectPart) {
+            $stringSelectParts[] = (string) $selectPart;
+        }
+        $this->select = implode(', ', $stringSelectParts);
+
+        $this->qb->select('eu.id');
+    }
+
+    /**
+     * @param OrmResultAfter $event
+     */
+    public function onResultAfter(OrmResultAfter $event)
+    {
+        $originalRecords = $event->getRecords();
+        if (!$originalRecords) {
+            return;
+        }
+
+        $ids = [];
+        foreach ($originalRecords as $record) {
+            $ids[] = $record->getValue('id');
+        }
+
+        $this->qb
+            ->select($this->select)
+            ->resetDqlPart('groupBy')
+            ->where($this->qb->expr()->in('eu.id', ':ids'))
+            ->setMaxResults(null)
+            ->setFirstResult(null)
+            ->setParameter('ids', $ids);
+        QueryUtils::removeUnusedParameters($this->qb);
+        $result = $this->qb
+            ->getQuery()
+            ->getResult();
+
+        $records = [];
+        foreach ($result as $row) {
+            $records[] = new ResultRecord($row);
+        }
+        $event->setRecords($records);
     }
 
     /**
