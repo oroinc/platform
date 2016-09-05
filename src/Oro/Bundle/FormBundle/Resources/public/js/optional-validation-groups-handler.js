117c302d96ace43d506f50b0b5278a9e80043345
Alexandr Dmitriev <admitriev@oroinc.com>
2016-09-05 17:29:17 +0300

    BAP-11722: Add possibility to extend optional-validation-handler on form level

---
+++ src/Oro/Bundle/FormBundle/Resources/public/js/optional-validation-groups-handler.js
@@ -0,0 +1,22 @@
+define(['jquery', 'oroform/js/optional-validation-handler'], function($, defaultOptionalValidationHandler) {
+    'use strict';
+
+    return {
+        /**
+         * @constructor
+         */
+        initialize: function(formElement) {
+            var groups = formElement.find('[data-validation-optional-group]');
+            groups.each(function(key, group){
+                var $group = $(group);
+                var handlerId = $group.data('validation-optional-group-handler');
+                if (handlerId) {
+                    var handler =require(handlerId);
+                    handler.initialize($group);
+                } else {
+                    defaultOptionalValidationHandler.initialize($group);
+                }
+            });
+        }
+    };
+});
