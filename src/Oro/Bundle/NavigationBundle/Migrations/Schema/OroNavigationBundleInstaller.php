fc5ad78f96c226fb8a5eaa66df8d0fc947d12bd8
Vitaly Shutko <sokal32@gmail.com>
2016-09-06 13:47:03 +0300

    BB-4395: Create entity MenuUpdate that extends MenuUpdate model - updated migration and installer - refactored to use new model - mad entity extendable (also added translations for it) - added title field that can be read from extras

--- src/Oro/Bundle/NavigationBundle/Migrations/Schema/OroNavigationBundleInstaller.php
+++ src/Oro/Bundle/NavigationBundle/Migrations/Schema/OroNavigationBundleInstaller.php
@@ -147,13 +147,13 @@ class OroNavigationBundleInstaller implements Installation
         $table = $schema->createTable('oro_navigation_menu_update');
         $table->addColumn('id', 'integer', ['autoincrement' => true]);
         $table->addColumn('key', 'string', ['length' => 100]);
-        $table->addColumn('parent_id', 'string', ['length' => 100]);
+        $table->addColumn('parent_key', 'string', ['length' => 100, 'notnull' => false]);
         $table->addColumn('title', 'string', ['length' => 255, 'notnull' => false]);
         $table->addColumn('uri', 'string', ['length' => 255, 'notnull' => false]);
         $table->addColumn('menu', 'string', ['length' => 100]);
         $table->addColumn('ownership_type', 'integer');
         $table->addColumn('owner_id', 'integer', ['notnull' => false]);
-        $table->addColumn('is_active', 'boolean', ['notnull' => false]);
+        $table->addColumn('is_active', 'boolean', []);
         $table->addColumn('priority', 'integer', ['notnull' => false]);
         $table->setPrimaryKey(['id']);
     }
