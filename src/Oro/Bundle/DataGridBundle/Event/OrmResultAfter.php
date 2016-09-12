a1489876458867affbfde7476b863672f9a68b8d
Thomas Parys <thomas.parys@wwsh.io>
2016-09-12 16:55:41 +0200

    BB-4032: Product Search UI - syncing changes with BB-4032

--- src/Oro/Bundle/DataGridBundle/Event/OrmResultAfter.php
+++ src/Oro/Bundle/DataGridBundle/Event/OrmResultAfter.php
@@ -2,12 +2,76 @@
 
 namespace Oro\Bundle\DataGridBundle\Event;
 
+use Doctrine\ORM\AbstractQuery;
+
 use Symfony\Component\EventDispatcher\Event;
 
 use Oro\Bundle\DataGridBundle\Datagrid\DatagridInterface;
 use Oro\Bundle\DataGridBundle\Datasource\ResultRecordInterface;
 
-class OrmResultAfter extends AbstractResultAfter implements GridEventInterface
+class OrmResultAfter extends Event implements GridEventInterface
 {
     const NAME = 'oro_datagrid.orm_datasource.result.after';
+
+    /**
+     * @var DatagridInterface
+     */
+    protected $datagrid;
+
+    /**
+     * @var ResultRecordInterface[]
+     */
+    protected $records;
+
+    /**
+     * @var mixed
+     */
+    protected $query;
+
+    /**
+     * @param DatagridInterface $datagrid
+     * @param array             $records
+     * @param mixed     $query
+     */
+    public function __construct(
+        DatagridInterface $datagrid,
+        array $records = [],
+        $query = null
+    ) {
+        $this->datagrid = $datagrid;
+        $this->records  = $records;
+        $this->query    = $query;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getDatagrid()
+    {
+        return $this->datagrid;
+    }
+
+    /**
+     * @return ResultRecordInterface[]
+     */
+    public function getRecords()
+    {
+        return $this->records;
+    }
+
+    /**
+     * @param array $records
+     */
+    public function setRecords(array $records)
+    {
+        $this->records = $records;
+    }
+
+    /**
+     * @return AbstractQuery
+     */
+    public function getQuery()
+    {
+        return $this->query;
+    }
 }
