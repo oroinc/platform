d969e2537898daa1e6428b72984e10c8f75a4822
Alexandr Dmitriev <admitriev@oroinc.com>
2016-09-12 16:49:31 +0300

    BAP-11722: Add possibility to extend optional-validation-handler on form level - fix event bubbling

--- src/Oro/Bundle/FormBundle/Resources/public/js/optional-validation-handler.js
+++ src/Oro/Bundle/FormBundle/Resources/public/js/optional-validation-handler.js
@@ -14,16 +14,69 @@ define(['jquery'], function($) {
         initialize: function($group) {
             var self = this;
 
-            var labels = $group.find('label[data-required]');
+            var labels = this.findCurrentGroupInputLabels($group);
             labels.addClass('required');
 
             var labelAsterisk = labels.find('em');
             labelAsterisk.hide().html('*');
-            if (self.hasNotEmptyInput($group) || self.hasNotEmptySelect($group)) {
+            if (self.hasNotEmptyDescendantGroup($group) ||
+                self.hasNotEmptyInput($group) ||
+                self.hasNotEmptySelect($group)) {
                 labelAsterisk.show();
+
+                $group.data('group-validation-required', true);
             }
 
             return true;
+        },
+
+        /**
+         * @param {jQuery} $group
+         * @return {jQuery}
+         */
+        findCurrentGroupInputLabels: function($group) {
+            console.log($group);
+            console.log($group.find('label[data-required]'));
+            console.log($group.find('label[data-required]:not([data-validation-optional-group] label)'));
+            return $group.find('label[data-required]:not([data-validation-optional-group] label)');
+        },
+
+        /**
+         * @param {jQuery} $group
+         * @returns {boolean}
+         */
+        hasNotEmptyDescendantGroup: function($group) {
+            return $group.find('[data-validation-optional-group][data-group-validation-required]').length > 0;
+        },
+
+        /**
+         * @param {jQuery} $group
+         * @returns {boolean}
+         */
+        hasNotEmptyInput: function($group) {
+            var checkedElements = this.findCurrentGroupInputs($group);
+            for (var i = 0; i < checkedElements.length; i++) {
+                if (!this.isValueEmpty($(checkedElements[i]).val())) {
+                    return true;
+                }
+            }
+
+            return false;
+        },
+
+        /**
+         * @param {jQuery} $group
+         * @returns {boolean}
+         */
+        hasNotEmptySelect: function($group) {
+            var elements = this.findCurrentGroupSelects($group);
+            for (var i = 0; i < elements.length; i++) {
+                if (!this.isValueEmpty($(elements[i]).find('option:selected').val())) {
+                    return true;
+                }
+            }
+
+            return false;
         },
 
         /**
@@ -69,47 +122,14 @@ define(['jquery'], function($) {
          */
         handleGroupRequire: function($group, value) {
             if (this.isValueEmpty(value)) {
-                if (!this.hasNotEmptyInput($group) && !this.hasNotEmptySelect($group)) {
-                    $group.find('label[data-required] em').hide();
-                    this.clearValidationErrors($group);
-                }
+                $group.find('label[data-required] em').hide();
+                this.clearValidationErrors($group);
+                $group.data('group-validation-required', false);
             } else {
                 $group.find('label[data-required] em').show();
                 $group.find('input, select').data('ignore-validation', false);
+                $group.data('group-validation-required', true);
             }
-        },
-
-        /**
-         * @param {jQuery} $group
-         * @returns {boolean}
-         */
-        hasNotEmptyInput: function($group) {
-            var elementsSelector = 'input[type!="checkbox"][type!="radio"][type!="button"][data-required],' +
-                ' input[type="radio"][data-required]:checked,' +
-                ' input[type="checkbox"][data-required]:checked';
-            var checkedElements = $group.find(elementsSelector);
-            for (var i = 0; i < checkedElements.length; i++) {
-                if (!this.isValueEmpty($(checkedElements[i]).val())) {
-                    return true;
-                }
-            }
-
-            return false;
-        },
-
-        /**
-         * @param {jQuery} $group
-         * @returns {boolean}
-         */
-        hasNotEmptySelect: function($group) {
-            var elements = $group.find('select[data-required]');
-            for (var i = 0; i < elements.length; i++) {
-                if (!this.isValueEmpty($(elements[i]).find('option:selected').val())) {
-                    return true;
-                }
-            }
-
-            return false;
         },
 
         /**
@@ -126,13 +146,33 @@ define(['jquery'], function($) {
          */
         clearValidationErrors: function($group) {
             var validator = $group.validate();
-            var inputs = $group
-                .find('input, select');
+            var inputs = this.findCurrentGroupInputs($group)
+                .add(this.findCurrentGroupSelects($group));
 
             inputs.data('ignore-validation', true);
             inputs.each(function(key, element) {
                     validator.hideElementErrors($(element));
                 });
         },
+
+        /**
+         * @param {jQuery} $group
+         * @return {jQuery}
+         */
+        findCurrentGroupInputs: function($group) {
+            var elementsSelector = 'input[type!="checkbox"][type!="radio"][type!="button"][data-required],' +
+                ' input[type="radio"][data-required]:checked,' +
+                ' input[type="checkbox"][data-required]:checked';
+
+            return $group.find(elementsSelector).not('[data-validation-optional-group] input');
+        },
+
+        /**
+         * @param {jQuery} $group
+         * @return {jQuery}
+         */
+        findCurrentGroupSelects: function($group) {
+            return $group.find('select:not([data-validation-optional-group] select)');
+        }
     };
 });
