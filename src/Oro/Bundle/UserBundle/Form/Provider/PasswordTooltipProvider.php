12c762f644098b3f550638bf9cde9256a3019ef1
Ivaylo Badinov <venimus@users.noreply.github.com>
2016-09-13 11:50:13 +0300

    BAP-10931: Use class constants

--- src/Oro/Bundle/UserBundle/Form/Provider/PasswordTooltipProvider.php
+++ src/Oro/Bundle/UserBundle/Form/Provider/PasswordTooltipProvider.php
@@ -5,22 +5,28 @@ namespace Oro\Bundle\UserBundle\Form\Provider;
 use Symfony\Component\Translation\TranslatorInterface;
 
 use Oro\Bundle\ConfigBundle\Config\ConfigManager;
+use Oro\Bundle\UserBundle\Validator\PasswordComplexityValidator;
 
 /**
  * Generates a tooltip text for the system configured password complexity requirements
  */
 class PasswordTooltipProvider
 {
-    const PASSWORD_TOOLTIP_PREFIX = 'oro.user.password_complexity.tooltip.prefix';
+    const TOOLTIP_PREFIX = 'oro.user.password_complexity.tooltip.prefix';
+    const TOOLTIP_SEPARATOR = ', ';
+    const TOOLTIP_MIN_LENGTH = 'oro.user.password_complexity.tooltip.min_length';
+    const TOOLTIP_UPPER_CASE = 'oro.user.password_complexity.tooltip.upper_case';
+    const TOOLTIP_NUMBERS = 'oro.user.password_complexity.tooltip.numbers';
+    const TOOLTIP_SPECIAL_CHARS = 'oro.user.password_complexity.tooltip.special_chars';
 
     /**
-     * @var array Map of tooltip parts and config keys that enable them
+     * @var array Map of the config keys and their corresponding tooltip translation keys
      */
     public static $tooltipPartsMap = [
-        'oro_user.password_min_length' => 'oro.user.password_complexity.tooltip.min_length',
-        'oro_user.password_upper_case' => 'oro.user.password_complexity.tooltip.upper_case',
-        'oro_user.password_numbers' => 'oro.user.password_complexity.tooltip.numbers',
-        'oro_user.password_special_chars' => 'oro.user.password_complexity.tooltip.special_chars',
+        PasswordComplexityValidator::CONFIG_MIN_LENGTH => self::TOOLTIP_MIN_LENGTH,
+        PasswordComplexityValidator::CONFIG_UPPER_CASE => self::TOOLTIP_UPPER_CASE,
+        PasswordComplexityValidator::CONFIG_NUMBERS => self::TOOLTIP_NUMBERS,
+        PasswordComplexityValidator::CONFIG_SPECIAL_CHARS => self::TOOLTIP_SPECIAL_CHARS,
     ];
 
     /** @var ConfigManager */
@@ -43,13 +49,13 @@ class PasswordTooltipProvider
     public function getTooltip()
     {
         $tooltips = [];
-        foreach (self::$tooltipPartsMap as $configKey => $text) {
+        foreach (self::$tooltipPartsMap as $configKey => $transKey) {
             $config = $this->configManager->get($configKey);
-            if ((bool) $config) {
-                $tooltips[] = $this->translator->trans($text, ['{{ value }}' => $config]);
+            if ($config) {
+                $tooltips[] = $this->translator->trans($transKey, ['{{ value }}' => $config]);
             }
         }
 
-        return $this->translator->trans(self::PASSWORD_TOOLTIP_PREFIX) . ' ' . join(', ', $tooltips);
+        return $this->translator->trans(self::TOOLTIP_PREFIX) . join(self::TOOLTIP_SEPARATOR, $tooltips);
     }
 }
