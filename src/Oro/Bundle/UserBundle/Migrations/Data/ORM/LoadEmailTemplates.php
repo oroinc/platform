583126daa1234a52bdde75555a656778566dd758
Julian Atanasov <atanasov@vmandco.com>
2016-09-14 12:31:58 +0300

    OEE-949: Add template for force pass reset

--- src/Oro/Bundle/UserBundle/Migrations/Data/ORM/LoadEmailTemplates.php
+++ src/Oro/Bundle/UserBundle/Migrations/Data/ORM/LoadEmailTemplates.php
@@ -1,9 +1,12 @@
 <?php
 namespace Oro\Bundle\UserBundle\Migrations\Data\ORM;
 
+use Doctrine\Common\Persistence\ObjectManager;
+
+use Oro\Bundle\MigrationBundle\Fixture\VersionedFixtureInterface;
 use Oro\Bundle\EmailBundle\Migrations\Data\ORM\AbstractEmailFixture;
 
-class LoadEmailTemplates extends AbstractEmailFixture
+class LoadEmailTemplates extends AbstractEmailFixture implements VersionedFixtureInterface
 {
     /**
      * Return path to email templates
@@ -15,5 +18,28 @@ class LoadEmailTemplates extends AbstractEmailFixture
         return $this->container
             ->get('kernel')
             ->locateResource('@OroUserBundle/Migrations/Data/ORM/emails');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getVersion()
+    {
+        return '1.0';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function findExistingTemplate(ObjectManager $manager, array $template)
+    {
+        if (empty($template['params']['name'])) {
+            return null;
+        }
+
+        return $manager->getRepository('OroEmailBundle:EmailTemplate')->findOneBy([
+            'name' => $template['params']['name'],
+            'entityName' => 'Oro\Bundle\UserBundle\Entity\User',
+        ]);
     }
 }
