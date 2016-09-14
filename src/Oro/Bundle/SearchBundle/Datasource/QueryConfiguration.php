ed0e90581d085229716872153365b0ab4e463b8b
Péter Vass <pvass@oroinc.com>
2016-09-14 14:20:41 +0200

    BB-4334: Move OroSearchBundle fixes to separate branch, do code review and merge to master - add mising QueryConfiguration class

---
+++ src/Oro/Bundle/SearchBundle/Datasource/QueryConfiguration.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace Oro\Bundle\SearchBundle\Datasource;
+
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
+
+class QueryConfiguration implements ConfigurationInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $builder = new TreeBuilder();
+
+        $builder->root('query')
+            ->addDefaultsIfNotSet()
+            ->children()
+                ->booleanNode('distinct')
+                    ->defaultFalse()
+                ->end()
+                ->arrayNode('select')
+                    ->prototype('scalar')->end()
+                ->end()
+                ->arrayNode('from')
+                    ->requiresAtLeastOneElement()
+                    ->prototype('scalar')
+                    ->end()
+                ->end()
+                ->arrayNode('join')
+                    ->append($this->addJoinNode('left'))
+                    ->append($this->addJoinNode('inner'))
+                ->end()
+                ->arrayNode('where')
+                    ->append($this->addWhereNode('and'))
+                    ->append($this->addWhereNode('or'))
+                ->end()
+                ->scalarNode('groupBy')->end()
+                ->scalarNode('having')->end()
+                ->arrayNode('orderBy')
+                    ->prototype('array')
+                        ->children()
+                            ->scalarNode('column')
+                                ->isRequired()
+                                ->cannotBeEmpty()
+                            ->end()
+                            ->scalarNode('dir')
+                                ->defaultValue('asc')
+                            ->end()
+                        ->end()
+                    ->end()
+                ->end()
+            ->end();
+
+        return $builder;
+    }
+
+    /**
+     * @param  string $name Join type ('left', 'inner')
+     *
+     * @throws \Symfony\Component\Config\Definition\Exception\InvalidConfigurationException
+     * @return \Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition
+     */
+    protected function addJoinNode($name)
+    {
+        if (!in_array(strtolower($name), ['left', 'inner'])) {
+            throw new InvalidConfigurationException(sprintf('Invalid join type "%s"', $name));
+        }
+
+        $builder = new TreeBuilder();
+
+        return $builder->root($name)
+            ->requiresAtLeastOneElement()
+            ->prototype('array')
+                ->children()
+                    ->scalarNode('join')
+                        ->isRequired()
+                        ->cannotBeEmpty()
+                    ->end()
+                    ->scalarNode('alias')->end()
+                    ->scalarNode('condition')->end()
+                    ->scalarNode('conditionType')->end()
+                ->end()
+            ->end();
+    }
+
+    /**
+     * @param  string $name Where type ('and', 'or')
+     *
+     * @throws InvalidConfigurationException
+     * @return ArrayNodeDefinition
+     */
+    protected function addWhereNode($name)
+    {
+        if (!in_array($name, ['and', 'or'])) {
+            throw new InvalidConfigurationException(sprintf('Invalid where type "%s"', $name));
+        }
+
+        $builder = new TreeBuilder();
+
+        return $builder->root($name)
+            ->requiresAtLeastOneElement()
+            ->prototype('scalar')->end();
+    }
+}
