fd13f53c9d678cbd0ad2a9e85c9cae7c83447f40
Alexander Kozienko <pascalamsg@gmail.com>
2016-09-15 14:11:34 +0300

    BAP-10770: Async Search Index Reindex
    - add topic meta passes

--- src/Oro/Bundle/SearchBundle/OroSearchBundle.php
+++ src/Oro/Bundle/SearchBundle/OroSearchBundle.php
@@ -2,6 +2,8 @@
 
 namespace Oro\Bundle\SearchBundle;
 
+use Oro\Bundle\MessageQueueBundle\DependencyInjection\Compiler\AddTopicMetaPass;
+use Oro\Bundle\SearchBundle\Async\Topics;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -20,5 +22,14 @@ class OroSearchBundle extends Bundle
                 'Oro\Bundle\PlatformBundle\DependencyInjection\Compiler\UpdateDoctrineEventHandlersPass'
             );
         }
+
+        $addTopicPass = AddTopicMetaPass::create()
+            ->add(Topics::REINDEX, 'Search index reindex')
+            ->add(Topics::INDEX_ENTITIES, 'Index entities by id')
+            ->add(Topics::INDEX_ENTITY_TYPE, 'Index entities by class name')
+            ->add(Topics::INDEX_ENTITY_BY_RANGE, 'Index range of entities of specified class')
+            ->add(Topics::INDEX_ENTITY, 'Index single entity by id')
+        ;
+        $container->addCompilerPass($addTopicPass);
     }
 }
