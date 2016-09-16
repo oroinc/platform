75bbea43b0b7a889c849224e15a60de9b3d24a65
Alex Torchenko <torchenko@gmail.com>
2016-09-16 22:56:03 +0300

    BB-1953: Resize Product Images   - log image not found case

--- src/Oro/Bundle/AttachmentBundle/Resizer/ImageResizer.php
+++ src/Oro/Bundle/AttachmentBundle/Resizer/ImageResizer.php
@@ -4,13 +4,18 @@ namespace Oro\Bundle\AttachmentBundle\Resizer;
 
 use Liip\ImagineBundle\Imagine\Cache\CacheManager;
 
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
+
 use Oro\Bundle\AttachmentBundle\Entity\File;
 use Oro\Bundle\AttachmentBundle\Manager\AttachmentManager;
 use Oro\Bundle\AttachmentBundle\Manager\FileManager;
 use Oro\Bundle\AttachmentBundle\Tools\ImageFactory;
 
-class ImageResizer
+class ImageResizer implements LoggerAwareInterface
 {
+    use LoggerAwareTrait;
+
     /**
      * @var AttachmentManager
      */
@@ -74,6 +79,12 @@ class ImageResizer
         try {
             $content = $this->fileManager->getContent($image);
         } catch (\Exception $e) {
+            $this->logger->warning(sprintf(
+                'Image (id: %d, filename: %s) not found. Skipped during resize.',
+                $image->getId(),
+                $image->getFilename()
+            ));
+
             return false;
         }
         $filteredBinary = $this->imageFactory->createImage($content, $filterName);
