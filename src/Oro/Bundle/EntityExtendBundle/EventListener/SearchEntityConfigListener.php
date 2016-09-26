707a7df5f54d62d5518a9c367e6c32614549d3ef
Eugene Sodin <esodin@ukr.net>
2016-09-26 18:32:09 +0300

    BAP-11926 Replace JMSJobQueueBundle with OroMessageQueue in EntityBundle, EntityConfigBundle and EntityExtendedBundle
     - Replcaing  occurances
     - Refactoring listener

--- src/Oro/Bundle/EntityExtendBundle/EventListener/SearchEntityConfigListener.php
+++ src/Oro/Bundle/EntityExtendBundle/EventListener/SearchEntityConfigListener.php
@@ -2,37 +2,32 @@
 
 namespace Oro\Bundle\EntityExtendBundle\EventListener;
 
-use Doctrine\Common\Persistence\ManagerRegistry;
-use Doctrine\DBAL\Types\Type;
-
-use JMS\JobQueueBundle\Entity\Job;
-
-use Oro\Bundle\EntityConfigBundle\Config\ConfigManager;
 use Oro\Bundle\EntityConfigBundle\Event\PreFlushConfigEvent;
-use Oro\Bundle\EntityExtendBundle\EntityConfig\ExtendScope;
-use Oro\Bundle\SearchBundle\Command\ReindexCommand;
-use Oro\Bundle\SearchBundle\Entity\UpdateEntity;
+use Oro\Bundle\SearchBundle\Async\Topics;
 use Oro\Bundle\SearchBundle\Provider\SearchMappingProvider;
+use Oro\Component\MessageQueue\Client\MessageProducerInterface;
 
 class SearchEntityConfigListener
 {
-    /** @var ManagerRegistry */
-    protected $registry;
-
     /** @var SearchMappingProvider */
     protected $searchMappingProvider;
 
-    /** @var ConfigManager */
-    protected $configManager;
 
     /**
-     * @param ManagerRegistry       $registry
-     * @param SearchMappingProvider $searchMappingProvider
+     * @var MessageProducerInterface
      */
-    public function __construct(ManagerRegistry $registry, SearchMappingProvider $searchMappingProvider)
-    {
-        $this->registry              = $registry;
+    protected $producer;
+
+    /**
+     * @param SearchMappingProvider $searchMappingProvider
+     * @param MessageProducerInterface $producer
+     */
+    public function __construct(
+        SearchMappingProvider $searchMappingProvider,
+        MessageProducerInterface $producer
+    ) {
         $this->searchMappingProvider = $searchMappingProvider;
+        $this->producer = $producer;
     }
 
     /**
@@ -56,51 +51,15 @@ class SearchEntityConfigListener
          */
         $this->searchMappingProvider->clearMappingCache();
 
-        $className    = $config->getId()->getClassName();
-        $extendConfig = $configManager->getProvider('extend')->getConfig($className);
-        if ($extendConfig->get('state') === ExtendScope::STATE_ACTIVE) {
-            $this->addReindexJob($className);
-        } else {
-            $this->addPostponeJob($className);
-        }
+        $this->reindex($config->getId()->getClassName());
     }
+
 
     /**
      * @param string $entityClass
      */
-    protected function addPostponeJob($entityClass)
+    protected function reindex($entityClass)
     {
-        $em = $this->registry->getManagerForClass('OroSearchBundle:UpdateEntity');
-        $update = $em->getRepository('OroSearchBundle:UpdateEntity')->find($entityClass);
-        if (!$update) {
-            $update = new UpdateEntity();
-            $update->setEntity($entityClass);
-            $em->persist($update);
-            $em->flush($update);
-        }
-    }
-
-    /**
-     * @param string $entityClass
-     */
-    protected function addReindexJob($entityClass)
-    {
-        $job = $this->registry->getRepository('JMSJobQueueBundle:Job')->createQueryBuilder('job')
-            ->select('job')
-            ->where('job.command = :command')
-            ->andWhere('cast(job.args as text) = :args')
-            ->andWhere('job.state in (\'pending\', \'running\')')
-            ->setParameter('command', ReindexCommand::COMMAND_NAME)
-            ->setParameter('args', ['class' => $entityClass], Type::JSON_ARRAY)
-            ->setMaxResults(1)
-            ->getQuery()
-            ->getOneOrNullResult();
-
-        if (!$job) {
-            $job = new Job(ReindexCommand::COMMAND_NAME, ['class' => $entityClass]);
-            $em  = $this->registry->getManager();
-            $em->persist($job);
-            $em->flush($job);
-        }
+        $this->producer->send(Topics::REINDEX, [$entityClass]);
     }
 }
