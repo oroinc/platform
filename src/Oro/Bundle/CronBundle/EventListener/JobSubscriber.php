dd04f061290c72c05c9b673074ef8ba591f16c59
Miloslav Nenadal <miloslav.nenadal@imatic.cz>
2016-09-29 15:31:11 +0200

    CRM-6267: JMS Job Queue Errors logging via monolog

---
+++ src/Oro/Bundle/CronBundle/EventListener/JobSubscriber.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Oro\Bundle\CronBundle\EventListener;
+
+use JMS\JobQueueBundle\Event\NewOutputEvent;
+
+use Psr\Log\LoggerInterface;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class JobSubscriber implements EventSubscriberInterface
+{
+    /** @var LoggerInterface */
+    protected $logger;
+
+    /**
+     * @param LoggerInterface $logger
+     */
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            'jms_job_queue.new_job_output' => ['onNewOutput', -255],
+        ];
+    }
+
+    /**
+     * @param NewOutputEvent $event
+     */
+    public function onNewOutput(NewOutputEvent $event)
+    {
+        $type = $event->getType() === NewOutputEvent::TYPE_STDOUT ? 'debug' : 'error';
+        call_user_func([$this->logger, $type], $event->getNewOutput(), ['job' => $event->getJob()]);
+    }
+}
