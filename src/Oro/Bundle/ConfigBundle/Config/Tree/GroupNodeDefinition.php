dc10bef48b688d628b68fe69abf8f402cbb0b723
Vova Soroka <vsoroka@magecore.com>
2016-10-01 04:31:30 +0300

    BAP-11988: Can not update Client Secret in IMAP OAuth configuration - Improve 'configurator' option for system_configuration.yml - Add 'handler' option for system_configuration.yml - Fix updating of OAuth2 AccessToken after save Google settings on system configuration - Fix duplication of Account Type dropdown on User Configuration > Email Configuration when Google returns Unauthorized status code

--- src/Oro/Bundle/ConfigBundle/Config/Tree/GroupNodeDefinition.php
+++ src/Oro/Bundle/ConfigBundle/Config/Tree/GroupNodeDefinition.php
@@ -101,7 +101,7 @@ class GroupNodeDefinition extends AbstractNodeDefinition implements \Countable, 
             $this->getName() => array_intersect_key(
                 $this->definition,
                 array_flip(
-                    ['title', 'priority', 'description', 'configurator', 'page_reload', 'tooltip']
+                    ['title', 'priority', 'description', 'configurator', 'handler', 'page_reload', 'tooltip']
                 )
             )
         );
