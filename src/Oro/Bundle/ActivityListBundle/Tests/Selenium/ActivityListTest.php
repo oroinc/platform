ec1c60bf71b41ae69251dc73212189dd5d9612bc
Calin Bolea <calin.bolea@evozon.com>
2016-10-06 20:42:50 +0300

    CRM-6281: Investigate/remove dependencies of platform bundles on CalendarBundle - Removed dependend code from selenium test of AcivityList
    Conflicts:
    	package/platform/src/Oro/Bundle/ActivityListBundle/Tests/Selenium/ActivityListTest.php

--- src/Oro/Bundle/ActivityListBundle/Tests/Selenium/ActivityListTest.php
+++ src/Oro/Bundle/ActivityListBundle/Tests/Selenium/ActivityListTest.php
@@ -2,12 +2,31 @@
 
 namespace Oro\Bundle\ActivityListBundle\Tests\Selenium;
 
+use Oro\Bundle\CalendarBundle\Tests\Selenium\Pages\Calendar;
 use Oro\Bundle\EmailBundle\Tests\Selenium\Pages\Email;
+use Oro\Bundle\EntityConfigBundle\Tests\Selenium\Pages\ConfigEntities;
 use Oro\Bundle\TestFrameworkBundle\Test\Selenium2TestCase;
 use Oro\Bundle\UserBundle\Tests\Selenium\Pages\Users;
 
 class ActivityListTest extends Selenium2TestCase
 {
+    const USERNAME  = 'admin';
+
+    /**
+     * Test Send Email functionality availability for User entity
+     */
+    public function testActivitiesOn()
+    {
+        $entityName = 'User';
+
+        $login = $this->login();
+        /** @var Users $login */
+        $login->openUsers('Oro\Bundle\UserBundle')
+            ->filterBy('Username', PHPUNIT_TESTSUITE_EXTENSION_SELENIUM_LOGIN)
+            ->open([PHPUNIT_TESTSUITE_EXTENSION_SELENIUM_LOGIN])
+            ->checkActionInGroup(array('Send email'), true);
+    }
+
     public function testEmailActivity()
     {
         $subject = 'Subject_'.mt_rand();
