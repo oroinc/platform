e4a73a53f1f83eb0454d73aa2fa51f7b2ef12a9f
Hryhorii Hrebiniuk <hhrebiniuk@magecore.com>
2016-10-17 17:04:56 +0300

    BAP-12167: Utilize fxpio/composer-asset-plugin

--- src/Oro/Bundle/UIBundle/Resources/public/js/extend/jquery-ui.js
+++ src/Oro/Bundle/UIBundle/Resources/public/js/extend/jquery-ui.js
@@ -3,7 +3,40 @@ define(function(require) {
 
     var $ = require('jquery');
     var mask = require('oroui/js/dropdown-mask');
-    require('jquery-ui');
+    require('components/jquery-ui/ui/core');
+    require('components/jquery-ui/ui/widget');
+    require('components/jquery-ui/ui/mouse');
+    require('components/jquery-ui/ui/position');
+    require('components/jquery-ui/ui/draggable');
+    require('components/jquery-ui/ui/droppable');
+    require('components/jquery-ui/ui/resizable');
+    require('components/jquery-ui/ui/selectable');
+    require('components/jquery-ui/ui/sortable');
+    require('components/jquery-ui/ui/accordion');
+    require('components/jquery-ui/ui/autocomplete');
+    require('components/jquery-ui/ui/button');
+    require('components/jquery-ui/ui/datepicker');
+    require('components/jquery-ui/ui/dialog');
+    require('components/jquery-ui/ui/menu');
+    require('components/jquery-ui/ui/progressbar');
+    require('components/jquery-ui/ui/slider');
+    require('components/jquery-ui/ui/spinner');
+    require('components/jquery-ui/ui/tabs');
+    require('components/jquery-ui/ui/effect');
+    require('components/jquery-ui/ui/effect-blind');
+    require('components/jquery-ui/ui/effect-bounce');
+    require('components/jquery-ui/ui/effect-clip');
+    require('components/jquery-ui/ui/effect-drop');
+    require('components/jquery-ui/ui/effect-explode');
+    require('components/jquery-ui/ui/effect-fade');
+    require('components/jquery-ui/ui/effect-fold');
+    require('components/jquery-ui/ui/effect-highlight');
+    require('components/jquery-ui/ui/effect-pulsate');
+    require('components/jquery-ui/ui/effect-scale');
+    require('components/jquery-ui/ui/effect-shake');
+    require('components/jquery-ui/ui/effect-size');
+    require('components/jquery-ui/ui/effect-slide');
+    require('components/jquery-ui/ui/effect-transfer');
 
     /* datepicker extend:start */
     (function() {
