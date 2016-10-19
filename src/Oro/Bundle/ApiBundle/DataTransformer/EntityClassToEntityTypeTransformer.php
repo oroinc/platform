40ee4bf2665a525514c791fc3c814a866738badc
Vova Soroka <vsoroka@magecore.com>
2016-10-20 02:46:43 +0300

    BAP-12136: Add list of entity types to the channel's API response

--- src/Oro/Bundle/ApiBundle/DataTransformer/EntityClassToEntityTypeTransformer.php
+++ src/Oro/Bundle/ApiBundle/DataTransformer/EntityClassToEntityTypeTransformer.php
@@ -3,11 +3,9 @@
 namespace Oro\Bundle\ApiBundle\DataTransformer;
 
 use Oro\Component\EntitySerializer\DataTransformerInterface;
-use Oro\Bundle\ApiBundle\Exception\RuntimeException;
 use Oro\Bundle\ApiBundle\Processor\Context;
 use Oro\Bundle\ApiBundle\Request\DataType;
 use Oro\Bundle\ApiBundle\Request\ValueNormalizer;
-use Oro\Bundle\EntityBundle\Exception\EntityAliasNotFoundException;
 
 class EntityClassToEntityTypeTransformer implements DataTransformerInterface
 {
@@ -31,20 +29,10 @@ class EntityClassToEntityTypeTransformer implements DataTransformerInterface
             return $value;
         }
 
-        try {
-            return $this->valueNormalizer->normalizeValue(
-                $value,
-                DataType::ENTITY_TYPE,
-                $context[Context::REQUEST_TYPE]
-            );
-        } catch (EntityAliasNotFoundException $e) {
-            throw new RuntimeException(
-                sprintf(
-                    'The "%s" class cannot be converted to the entity type.'
-                    . 'Be sure that this entity is configured to be available through API.',
-                    $value
-                )
-            );
-        }
+        return $this->valueNormalizer->normalizeValue(
+            $value,
+            DataType::ENTITY_TYPE,
+            $context[Context::REQUEST_TYPE]
+        );
     }
 }
