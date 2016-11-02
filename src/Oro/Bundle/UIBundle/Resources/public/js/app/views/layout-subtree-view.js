8720c422f7b7e7e172ffd987625b20f5cb563fb5
Dmitry Panchenko <dpanchenko@oroinc.com>
2016-11-01 21:08:34 -0700

    BB-5130: Update LayoutSubtreeView usage for shopping lists - refactoring with review comments - removed mediator

--- src/Oro/Bundle/UIBundle/Resources/public/js/app/views/layout-subtree-view.js
+++ src/Oro/Bundle/UIBundle/Resources/public/js/app/views/layout-subtree-view.js
@@ -3,16 +3,13 @@ define(function(require) {
 
     var LayoutSubtreeView;
     var BaseView = require('oroui/js/app/views/base/view');
-    var mediator = require('oroui/js/mediator');
     var LoadingMaskView = require('oroui/js/app/views/loading-mask-view');
     var LayoutSubtreeManager = require('oroui/js/layout-subtree-manager');
     var $ = require('jquery');
 
     LayoutSubtreeView = BaseView.extend({
         options: {
-            url: window.location.href,
             rootId: '',
-            method: 'get',
             reloadEvents: [],
             showLoading: true
         },
@@ -20,39 +17,21 @@ define(function(require) {
         initialize: function(options) {
             this.options = $.extend(true, {}, this.options, options);
             LayoutSubtreeView.__super__.initialize.apply(this, arguments);
+            LayoutSubtreeManager.addLayoutSubtreeInstance(this);
         },
 
-        getOptions: function() {
-            return this.options;
-        },
-
-        delegateEvents: function() {
-            var result = LayoutSubtreeView.__super__.delegateEvents.apply(this, arguments);
-            LayoutSubtreeManager.addLayoutSubtreeInstance(this.options);
-            mediator.on('layout_subtree_reload_start', this._showLoading, this);
-            mediator.on('layout_subtree_reload_done', this._onContentLoad, this);
-            mediator.on('layout_subtree_reload_fail', this._hideLoading, this);
-            return result;
-        },
-
-        undelegateEvents: function() {
-            LayoutSubtreeManager.removeLayoutSubtreeInstance(this.options);
-            mediator.off('layout_subtree_reload_start', this._showLoading, this);
-            mediator.off('layout_subtree_reload_done', this._onContentLoad, this);
-            mediator.off('layout_subtree_reload_fail', this._hideLoading, this);
-            return LayoutSubtreeView.__super__.undelegateEvents.apply(this, arguments);
+        dispose: function() {
+            LayoutSubtreeManager.removeLayoutSubtreeInstance(this.options.rootId);
+            return LayoutSubtreeView.__super__.dispose.apply(this, arguments);
         },
 
         _onContentLoad: function(content) {
             this._hideLoading();
-            if (content.hasOwnProperty(this.options.rootId)) {
-                content = content[this.options.rootId];
-                this.$el.html($(content).children());
+            this.$el.html($(content).children());
 
-                this.disposePageComponents();
-                this.initLayout().done(function() {
-                });
-            }
+            this.disposePageComponents();
+            this.initLayout().done(function() {
+            });
         },
 
         _showLoading: function() {
