158a940ccfb7d092e50fcb1ff69adf02cc3fbb76
Julian Atanasov <atanasov@vmandco.com>
2016-11-02 17:57:05 +0200

    OEE-945: Periodic password change - show password expiration warning on login

---
+++ src/Oro/Bundle/UserBundle/EventListener/PasswordExpirySubscriber.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Oro\Bundle\UserBundle\EventListener;
+
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Translation\TranslatorInterface;
+use Symfony\Component\Security\Http\SecurityEvents;
+use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+use Oro\Bundle\UserBundle\Entity\User;
+
+class PasswordExpirySubscriber implements EventSubscriberInterface
+{
+    protected $periodMarkers = [1, 3, 7];
+
+    /** @var Session */
+    protected $session;
+
+    /** @var TranslatorInterface */
+    protected $translator;
+
+    /**
+     * @param Session $session
+     */
+    public function __construct(Session $session, TranslatorInterface $translator)
+    {
+        $this->session    = $session;
+        $this->translator = $translator;
+    }
+
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+            SecurityEvents::INTERACTIVE_LOGIN => 'onInteractiveLogin'
+        );
+    }
+
+    /**
+     * @param  InteractiveLoginEvent $event
+     */
+    public function onInteractiveLogin(InteractiveLoginEvent $event)
+    {
+        $user = $event->getAuthenticationToken()->getUser();
+
+        if ($user instanceof User && null !== $passwordExpiryDate = $user->getPasswordExpiresAt()) {
+            $now = new \DateTime('now', new \DateTimeZone('UTC'));
+            $interval = $now->diff($passwordExpiryDate);
+            $days = $interval ? ($interval->d + 1) : false;
+
+            if ($days && in_array($days, $this->periodMarkers)) {
+                $message = $this->translator->transChoice(
+                    'oro.user.password.expiration.message',
+                    $days,
+                    ['%count%' => $days]
+                );
+                $this->session->getFlashBag()->add('warning', $message);
+            }
+        }
+    }
+}
