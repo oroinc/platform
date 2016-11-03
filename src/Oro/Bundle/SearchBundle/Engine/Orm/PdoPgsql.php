516b82745b9632b98255262260f61ef1bb443af8
Max Malecki <mmalecki@oroinc.com>
2016-11-03 14:48:50 +0100

    BB-5285 Filtering by subcategories doesnt work on 4032 - Overreacting storm formater revert sorry

--- src/Oro/Bundle/SearchBundle/Engine/Orm/PdoPgsql.php
+++ src/Oro/Bundle/SearchBundle/Engine/Orm/PdoPgsql.php
@@ -2,9 +2,9 @@
 namespace Oro\Bundle\SearchBundle\Engine\Orm;
 
 use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Mapping\ClassMetadata;
-use Doctrine\ORM\QueryBuilder;
 
 use Oro\Bundle\SearchBundle\Query\Criteria\Criteria;
 use Oro\Bundle\SearchBundle\Query\Query;
@@ -14,19 +14,6 @@ class PdoPgsql extends BaseDriver
     public $columns = [];
     public $needle;
     public $mode;
-
-    /**
-     * Sql plain query to create fulltext index for Postgresql.
-     *
-     * @param string $tableName
-     * @param string $indexName
-     *
-     * @return string
-     */
-    public static function getPlainSql($tableName = 'oro_search_index_text', $indexName = 'value')
-    {
-        return sprintf('CREATE INDEX %s ON %s USING gin(to_tsvector(\'english\', value))', $indexName, $tableName);
-    }
 
     /**
      * Init additional doctrine functions
@@ -47,37 +34,16 @@ class PdoPgsql extends BaseDriver
     }
 
     /**
-     * {@inheritdoc}
+     * Sql plain query to create fulltext index for Postgresql.
+     *
+     * @param string $tableName
+     * @param string $indexName
+     *
+     * @return string
      */
-    public function addTextField(QueryBuilder $qb, $index, $searchCondition, $setOrderBy = true)
+    public static function getPlainSql($tableName = 'oro_search_index_text', $indexName = 'value')
     {
-        $useFieldName = $searchCondition['fieldName'] == '*' ? false : true;
-        $fieldValue = $this->filterTextFieldValue($searchCondition['fieldValue']);
-
-        // TODO Need to clarify search requirements in scope of CRM-214
-        if ($searchCondition['condition'] === Query::OPERATOR_CONTAINS) {
-            $searchString = $this->createContainsStringQuery($index, $useFieldName);
-            $this->createContainsQueryParameter($qb, $index, $fieldValue, $setOrderBy);
-        } elseif ($searchCondition['condition'] === Query::OPERATOR_EQUALS) {
-            $searchString = $this->createEqualsStringQuery($index, $useFieldName);
-            $this->createEqualsQueryParameter($qb, $index, $fieldValue, $setOrderBy);
-        } elseif ($searchCondition['condition'] === Query::OPERATOR_STARTS_WITH) {
-            $searchString = $this->createStartWithStringQuery($index, $useFieldName);
-            $this->createStartWithStringParameter($qb, $index, $fieldValue, $setOrderBy);
-        } else {
-            $searchString = $this->createNotContainsStringQuery($index, $useFieldName);
-            $this->createNotContainsQueryParameter($qb, $index, $fieldValue, $setOrderBy);
-        }
-
-        if ($useFieldName) {
-            $qb->setParameter('field' . $index, $searchCondition['fieldName']);
-        }
-
-        if ($setOrderBy) {
-            $this->setTextOrderBy($qb, $index);
-        }
-
-        return '(' . $searchString . ' ) ';
+        return sprintf('CREATE INDEX %s ON %s USING gin(to_tsvector(\'english\', value))', $indexName, $tableName);
     }
 
     /**
@@ -124,6 +90,113 @@ class PdoPgsql extends BaseDriver
         if ($isOrderBy) {
             $qb->setParameter('orderByValue' . $index, $stringParameter);
         }
+    }
+
+    /**
+     * @param QueryBuilder $qb
+     * @param string $index
+     * @param string $fieldValue
+     * @param bool $isOrderBy
+     */
+    protected function createNotContainsQueryParameter(QueryBuilder $qb, $index, $fieldValue, $isOrderBy)
+    {
+        $searchArray = explode(Query::DELIMITER, $fieldValue);
+
+        foreach ($searchArray as $key => $string) {
+            $searchArray[$key] = '!' . $string;
+        }
+
+        $stringParameter = implode(' & ', $searchArray);
+
+        $qb->setParameter('value' . $index, $stringParameter);
+
+        if ($isOrderBy) {
+            $qb->setParameter('orderByValue' . $index, $stringParameter);
+        }
+    }
+
+    /**
+     * Create search string for string parameters (not contains)
+     *
+     * @param integer $index
+     * @param bool $useFieldName
+     *
+     * @return string
+     */
+    protected function createNotContainsStringQuery($index, $useFieldName = true)
+    {
+        $joinAlias = $this->getJoinAlias(Query::TYPE_TEXT, $index);
+
+        $stringQuery = '(TsvectorTsquery(' . $joinAlias . '.value, :value' . $index . ')) = TRUE';
+
+        if ($useFieldName) {
+            $stringQuery .= ' AND ' . $joinAlias . '.field = :field' . $index;
+        }
+
+        return $stringQuery;
+    }
+
+    /**
+     * Set fulltext range order by
+     *
+     * @param QueryBuilder $qb
+     * @param int $index
+     */
+    protected function setTextOrderBy(QueryBuilder $qb, $index)
+    {
+        $joinAlias = $this->getJoinAlias(Query::TYPE_TEXT, $index);
+
+        $qb->addSelect(sprintf('TsRank(%s.value, :orderByValue%s) as rankField%s', $joinAlias, $index, $index))
+            ->addOrderBy(sprintf('rankField%s', $index), Criteria::DESC);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTruncateQuery(AbstractPlatform $dbPlatform, $tableName)
+    {
+        $query = parent::getTruncateQuery($dbPlatform, $tableName);
+
+        // cascade required to perform truncate of related entities
+        if (strpos($query, ' CASCADE') === false) {
+            $query .= ' CASCADE';
+        }
+
+        return $query;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function addTextField(QueryBuilder $qb, $index, $searchCondition, $setOrderBy = true)
+    {
+        $useFieldName = $searchCondition['fieldName'] == '*' ? false : true;
+        $fieldValue = $this->filterTextFieldValue($searchCondition['fieldValue']);
+
+        // TODO Need to clarify search requirements in scope of CRM-214
+        if ($searchCondition['condition'] === Query::OPERATOR_CONTAINS) {
+            $searchString = $this->createContainsStringQuery($index, $useFieldName);
+            $this->createContainsQueryParameter($qb, $index, $fieldValue, $setOrderBy);
+        } elseif ($searchCondition['condition'] === Query::OPERATOR_EQUALS) {
+            $searchString = $this->createEqualsStringQuery($index, $useFieldName);
+            $this->createEqualsQueryParameter($qb, $index, $fieldValue, $setOrderBy);
+        } elseif ($searchCondition['condition'] === Query::OPERATOR_STARTS_WITH) {
+            $searchString = $this->createStartWithStringQuery($index, $useFieldName);
+            $this->createStartWithStringParameter($qb, $index, $fieldValue, $setOrderBy);
+        } else {
+            $searchString = $this->createNotContainsStringQuery($index, $useFieldName);
+            $this->createNotContainsQueryParameter($qb, $index, $fieldValue, $setOrderBy);
+        }
+
+        if ($useFieldName) {
+            $qb->setParameter('field' . $index, $searchCondition['fieldName']);
+        }
+
+        if ($setOrderBy) {
+            $this->setTextOrderBy($qb, $index);
+        }
+
+        return '(' . $searchString . ' ) ';
     }
 
     /**
@@ -191,78 +264,5 @@ class PdoPgsql extends BaseDriver
         if ($isOrderBy) {
             $qb->setParameter('orderByValue' . $index, $fieldValue);
         }
-    }
-
-    /**
-     * Create search string for string parameters (not contains)
-     *
-     * @param integer $index
-     * @param bool $useFieldName
-     *
-     * @return string
-     */
-    protected function createNotContainsStringQuery($index, $useFieldName = true)
-    {
-        $joinAlias = $this->getJoinAlias(Query::TYPE_TEXT, $index);
-
-        $stringQuery = '(TsvectorTsquery(' . $joinAlias . '.value, :value' . $index . ')) = TRUE';
-
-        if ($useFieldName) {
-            $stringQuery .= ' AND ' . $joinAlias . '.field = :field' . $index;
-        }
-
-        return $stringQuery;
-    }
-
-    /**
-     * @param QueryBuilder $qb
-     * @param string $index
-     * @param string $fieldValue
-     * @param bool $isOrderBy
-     */
-    protected function createNotContainsQueryParameter(QueryBuilder $qb, $index, $fieldValue, $isOrderBy)
-    {
-        $searchArray = explode(Query::DELIMITER, $fieldValue);
-
-        foreach ($searchArray as $key => $string) {
-            $searchArray[$key] = '!' . $string;
-        }
-
-        $stringParameter = implode(' & ', $searchArray);
-
-        $qb->setParameter('value' . $index, $stringParameter);
-
-        if ($isOrderBy) {
-            $qb->setParameter('orderByValue' . $index, $stringParameter);
-        }
-    }
-
-    /**
-     * Set fulltext range order by
-     *
-     * @param QueryBuilder $qb
-     * @param int $index
-     */
-    protected function setTextOrderBy(QueryBuilder $qb, $index)
-    {
-        $joinAlias = $this->getJoinAlias(Query::TYPE_TEXT, $index);
-
-        $qb->addSelect(sprintf('TsRank(%s.value, :orderByValue%s) as rankField%s', $joinAlias, $index, $index))
-            ->addOrderBy(sprintf('rankField%s', $index), Criteria::DESC);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function getTruncateQuery(AbstractPlatform $dbPlatform, $tableName)
-    {
-        $query = parent::getTruncateQuery($dbPlatform, $tableName);
-
-        // cascade required to perform truncate of related entities
-        if (strpos($query, ' CASCADE') === false) {
-            $query .= ' CASCADE';
-        }
-
-        return $query;
     }
 }
