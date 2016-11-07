251fd438b3e9f20235690878fbbd04ab73ccc9ec
Vova Soroka <vsoroka@magecore.com>
2016-11-07 02:30:07 +0200

    Allow to use nelmio/alice to load data fixtures in functional tests - remove dependencies from AliceFileFixture

--- src/Oro/Bundle/TestFrameworkBundle/Test/DataFixtures/AliceFileFixture.php
+++ src/Oro/Bundle/TestFrameworkBundle/Test/DataFixtures/AliceFileFixture.php
@@ -2,19 +2,10 @@
 
 namespace Oro\Bundle\TestFrameworkBundle\Test\DataFixtures;
 
-use Doctrine\Common\DataFixtures\DependentFixtureInterface;
-use Symfony\Component\Yaml\Yaml;
-
-class AliceFileFixture extends AliceFixture implements DependentFixtureInterface
+class AliceFileFixture extends AliceFixture
 {
     /** @var string */
     private $fileName;
-
-    /** @var mixed */
-    private $data = false;
-
-    /** @var string[] */
-    private $dependencies;
 
     /**
      * @param string $fileName
@@ -37,36 +28,6 @@ class AliceFileFixture extends AliceFixture implements DependentFixtureInterface
      */
     protected function loadData()
     {
-        $this->ensureInitialized();
-
-        return $this->loader->load(null !== $this->data ? $this->data : $this->fileName);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getDependencies()
-    {
-        $this->ensureInitialized();
-
-        return $this->dependencies;
-    }
-
-    private function ensureInitialized()
-    {
-        if (false !== $this->data) {
-            return;
-        }
-
-        $this->dependencies = [];
-        if (1 === preg_match('/\.ya?ml$/', $this->fileName)) {
-            $this->data = Yaml::parse($this->fileName);
-            if (array_key_exists('dependencies', $this->data)) {
-                $this->dependencies = $this->data['dependencies'];
-                unset($this->data['dependencies']);
-            }
-        } else {
-            $this->data = null;
-        }
+        return $this->loader->load($this->fileName);
     }
 }
