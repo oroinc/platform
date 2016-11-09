471b71e86f666380575770bd21f032eca37e5779
Oleksandr Nechyporenko <onechyporenko@oroinc.com>
2016-11-09 14:10:05 +0200

    BB-5270: Update Products Categories design  - use collapse-widget instead of toggle

--- src/Oro/Bundle/UIBundle/Resources/public/js/widget/collapse-widget.js
+++ src/Oro/Bundle/UIBundle/Resources/public/js/widget/collapse-widget.js
@@ -7,6 +7,8 @@ define(['jquery', 'oroui/js/mediator', 'underscore', 'jquery-ui'], function($, m
         options: {
             trigger: '[data-collapse-trigger]',
             container: '[data-collapse-container]',
+            hideSibling: false,
+            breakpoint: 0,
             storageKey: '',
             open: null,
             uid: '',
@@ -49,6 +51,9 @@ define(['jquery', 'oroui/js/mediator', 'underscore', 'jquery-ui'], function($, m
         },
 
         _toggle: function(event) {
+            if (this.options.breakpoint && $(window).outerWidth() >= this.options.breakpoint) {
+                return;
+            }
             var self = this;
             var $trigger = $(event.currentTarget);
             var $container = this.$container;
@@ -71,6 +76,11 @@ define(['jquery', 'oroui/js/mediator', 'underscore', 'jquery-ui'], function($, m
                 };
 
                 self.$el.toggleClass(self.options.openClass, isOpen);
+
+                if (self.options.hideSibling) {
+                    self._hideSiblings(isOpen);
+                }
+
                 $trigger.trigger('collapse:toggle', params);
                 mediator.trigger('layout:adjustHeight');
 
@@ -78,6 +88,14 @@ define(['jquery', 'oroui/js/mediator', 'underscore', 'jquery-ui'], function($, m
                     localStorage.setItem(self.options.storageKey + self.options.uid, isOpen);
                 }
             });
+        },
+
+        _hideSiblings: function(isOpen) {
+            if (isOpen) {
+                this.$el.siblings().hide(this.options.animationSpeed);
+            } else {
+                this.$el.siblings().show(this.options.animationSpeed);
+            }
         }
     });
 
