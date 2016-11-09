51673c742881c26f1da1f3e9f42bc07f61e1bffb
Volodymyr Myrza <vmyrza@oroinc.com>
2016-11-09 14:50:51 +0200

    BAP-12126: Investigate SecurityBundle to improve performance of operations - change usage of deprecated class to recommended one

--- src/Oro/Bundle/SecurityBundle/Acl/Extension/EntityAclExtension.php
+++ src/Oro/Bundle/SecurityBundle/Acl/Extension/EntityAclExtension.php
@@ -2,11 +2,12 @@
 
 namespace Oro\Bundle\SecurityBundle\Acl\Extension;
 
+use Doctrine\Common\Util\ClassUtils;
+
 use Symfony\Component\Security\Acl\Domain\ObjectIdentity;
 use Symfony\Component\Security\Acl\Exception\InvalidDomainObjectException;
 use Symfony\Component\Security\Acl\Model\ObjectIdentityInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use Symfony\Component\Security\Core\Util\ClassUtils;
 
 use Oro\Bundle\EntityBundle\Exception\InvalidEntityException;
 use Oro\Bundle\EntityBundle\ORM\EntityClassResolver;
