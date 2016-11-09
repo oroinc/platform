bcac0b738748b298473bd9de1d540912065cfb99
Nikita Makarov <mesaverde228@gmail.com>
2016-11-09 17:20:11 +0300

    BAP-12471: Create TransitionButton class - Added "TransitionButton" class - Added Unit tests

---
+++ src/Oro/Bundle/WorkflowBundle/Model/TransitionButton.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Oro\Bundle\WorkflowBundle\Model;
+
+use Oro\Bundle\ActionBundle\Model\ButtonContext;
+use Oro\Bundle\ActionBundle\Model\ButtonInterface;
+
+class TransitionButton implements ButtonInterface
+{
+    //TODO: Must be updated at https://magecore.atlassian.net/browse/BAP-12481
+    const DEFAULT_TEMPLATE = 'OroWorkflowBundle::Button\transitionButton.html.twig';
+
+    /**
+     * @var Workflow
+     */
+    protected $workflow;
+
+    /**
+     * @var Transition
+     */
+    protected $transition;
+
+    /**
+     * @var ButtonContext
+     */
+    protected $buttonContext;
+
+    /**
+     * @param Transition $transition
+     * @param Workflow $workflow
+     * @param ButtonContext $buttonContext
+     */
+    public function __construct(Transition $transition, Workflow $workflow, ButtonContext $buttonContext)
+    {
+        $this->transition = $transition;
+        $this->workflow = $workflow;
+        $this->buttonContext = $buttonContext;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getOrder()
+    {
+        return $this->workflow->getDefinition()->getPriority();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTemplate()
+    {
+        return static::DEFAULT_TEMPLATE;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTemplateData()
+    {
+        return [
+            //TODO: Must be updated at https://magecore.atlassian.net/browse/BAP-12481
+            //'workflow' => $this->workflow,
+            //'transition' => $this->transition,
+            //'workflowItem' => null,
+            //'transitionData' => [
+            //    'workflow' => $this->workflow->getName(),
+            //    'transition' => $this->transition->getName(),
+            //    'transition-url' => $this->buttonContext->getExecutionUrl(),
+            //    'transition-condition-messages' => null,
+            //    'isAllowed' => true,
+            //    'enabled' => $this->buttonContext->isEnabled() ?: $this->buttonContext->isUnavailableHidden(),
+            //    'errors' => []
+            //]
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getButtonContext()
+    {
+        return $this->buttonContext;
+    }
+}
---
+++ src/Oro/Bundle/WorkflowBundle/Tests/Unit/Model/TransitionButtonTest.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Oro\Bundle\WorkflowBundle\Tests\Unit\Model;
+
+use Oro\Bundle\ActionBundle\Model\ButtonContext;
+
+use Oro\Bundle\WorkflowBundle\Entity\WorkflowDefinition;
+use Oro\Bundle\WorkflowBundle\Model\Transition;
+use Oro\Bundle\WorkflowBundle\Model\TransitionButton;
+use Oro\Bundle\WorkflowBundle\Model\Workflow;
+
+class TransitionButtonTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Workflow|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $workflow;
+
+    /**
+     * @var WorkflowDefinition|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $definition;
+
+    /**
+     * @var Transition|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $transition;
+
+    /**
+     * @var ButtonContext|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $buttonContext;
+
+    /**
+     * @var TransitionButton
+     */
+    protected $button;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+
+        $this->transition = $this->getMockBuilder(Transition::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->definition = $this->getMockBuilder(WorkflowDefinition::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->workflow = $this->getMockBuilder(Workflow::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->workflow->expects($this->any())->method('getDefinition')->willReturn($this->definition);
+
+        $this->buttonContext = $this->getMockBuilder(ButtonContext::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->button = new TransitionButton($this->transition, $this->workflow, $this->buttonContext);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function tearDown()
+    {
+        unset($this->workflow, $this->definition, $this->button, $this->buttonContext, $this->transition);
+    }
+
+    public function testGetOrder()
+    {
+        $this->definition->expects($this->once())->method('getPriority')->willReturn(1);
+        $this->assertEquals(1, $this->button->getOrder());
+    }
+
+    public function testGetTemplate()
+    {
+        //TODO: Must be updated https://magecore.atlassian.net/browse/BAP-12480
+        $this->assertEquals(TransitionButton::DEFAULT_TEMPLATE, $this->button->getTemplate());
+    }
+
+    public function testGetTemplateData()
+    {
+        //TODO: Must be updated https://magecore.atlassian.net/browse/BAP-12480
+        $this->assertInternalType('array', $this->button->getTemplateData());
+    }
+
+    public function testGetButtonContext()
+    {
+        $this->assertInstanceOf(ButtonContext::class, $this->button->getButtonContext());
+    }
+}
