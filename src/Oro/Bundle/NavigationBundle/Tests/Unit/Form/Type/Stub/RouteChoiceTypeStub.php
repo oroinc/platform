1d9b200dc0c2b08679b367824e5bfb48dd798fe1
Dmitry Khrysev <dkhrysev@magecore.com>
2016-11-10 18:02:29 +0200

    BB-5413: System Page form type - added for type for system page (scopes are stub for now) - updated content variant fields config to prevent form fields auto adding by extend bundle form extension - renamed PageTypeRegistry to ContentVariantTypeRegistry - renamed page type tag to content variant type - renamed interface for content variant type

---
+++ src/Oro/Bundle/NavigationBundle/Tests/Unit/Form/Type/Stub/RouteChoiceTypeStub.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Oro\Bundle\NavigationBundle\Tests\Unit\Form\Type\Stub;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RouteChoiceTypeStub extends AbstractType
+{
+    /**
+     * @var array
+     */
+    protected $choices;
+
+    /**
+     * @param array $choices
+     */
+    public function __construct(array $choices)
+    {
+        $this->choices = $choices;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefined(
+            [
+                'path_filter',
+                'name_filter',
+                'options_filter',
+                'without_parameters_only',
+                'add_titles',
+                'with_titles_only',
+            ]
+        );
+
+        $resolver->setDefault('choices', $this->choices);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return ChoiceType::class;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'oro_route_choice';
+    }
+}
