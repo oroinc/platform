ed20a36c98dd7cd9aaf07cdc18c43f99bab5734c
Alexandr Dmitriev <admitriev@oroinc.com>
2016-11-11 13:20:33 +0200

    AER-87: Migrate existing Notes

---
+++ src/Oro/Bundle/NoteBundle/Migrations/Schema/v1_3/UpdateNoteAssociationKind.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Oro\Bundle\NoteBundle\Migrations\Schema\v1_3;
+
+use Doctrine\DBAL\Schema\Schema;
+
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+
+class UpdateNoteAssociationKind implements Migration
+{
+    /**
+     * @param Schema   $schema
+     * @param QueryBag $queries
+     *
+     * @return void
+     */
+    public function up(Schema $schema, QueryBag $queries)
+    {
+
+    }
+}
