61849582b137fa187ecff45395e0b33ed6693251
Vladimir Tsykun <Vladimir.Tsykun@effective-soft.com>
2016-11-12 17:27:18 +0300

    BAP-12478: Update start transition logic - updated controller logic

--- src/Oro/Bundle/WorkflowBundle/Controller/Api/Rest/WorkflowController.php
+++ src/Oro/Bundle/WorkflowBundle/Controller/Api/Rest/WorkflowController.php
@@ -77,8 +77,15 @@ class WorkflowController extends FOSRestController
 
             $workflow = $workflowManager->getWorkflow($workflowName);
             $entityClass = $workflow->getDefinition()->getRelatedEntity();
-            $entity = $this->getEntityReference($entityClass, $entityId);
 
+            $transition = $workflow->getTransitionManager()->getTransition($transitionName);
+            if ($transition->getInitEntities() || $transition->getInitRoutes()) {
+                $contextAttribute = $transition->getInitContextAttribute();
+                $dataArray[$contextAttribute] = $this->get('oro_action.helper.context')->getContext();
+                $entityId = null;
+            }
+
+            $entity = $this->getOrCreateEntityReference($entityClass, $entityId);
             $workflowItem = $workflowManager->startWorkflow($workflowName, $entity, $transitionName, $dataArray);
         } catch (HttpException $e) {
             return $this->handleError($e->getMessage(), $e->getStatusCode());
@@ -105,14 +112,14 @@ class WorkflowController extends FOSRestController
     }
 
     /**
-     * Try to get reference to entity
+     * Try to get or create reference to entity
      *
      * @param string $entityClass
      * @param mixed $entityId
      * @throws BadRequestHttpException
      * @return mixed
      */
-    protected function getEntityReference($entityClass, $entityId)
+    protected function getOrCreateEntityReference($entityClass, $entityId)
     {
         /** @var DoctrineHelper $doctrineHelper */
         $doctrineHelper = $this->get('oro_entity.doctrine_helper');
