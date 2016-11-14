fa298be7bc8715bd03dae702c7ae376f31c49202
Mykhailo Sulyma <msulima@magecore.com>
2016-11-14 19:13:17 +0200

    BAP-12475: Add provider extension for ActionBundle  - fixed unit tests

--- src/Oro/Bundle/ActionBundle/Tests/Unit/Extension/OperationButtonProviderExtensionTest.php
+++ src/Oro/Bundle/ActionBundle/Tests/Unit/Extension/OperationButtonProviderExtensionTest.php
@@ -14,6 +14,15 @@ use Oro\Bundle\ActionBundle\Model\OperationRegistry;
 
 class OperationButtonProviderExtensionTest extends \PHPUnit_Framework_TestCase
 {
+    const ENTITY_CLASS = 'stdClass';
+    const ENTITY_ID = 42;
+    const ROUTE_NAME = 'test_route_name';
+    const EXECUTION_ROUTE_NAME = 'test_execution_route_name';
+    const DIALOG_ROUTE_NAME = 'test_dialog_route_name';
+    const DATAGRID_NAME = 'test_datagrid_name';
+    const REFERRER_URL = '/test/referrer/utl';
+    const GROUP = 'test_group';
+
     /** @var OperationButtonProviderExtension */
     protected $extension;
 
@@ -37,6 +46,10 @@ class OperationButtonProviderExtensionTest extends \PHPUnit_Framework_TestCase
         $this->contextHelper = $this->getMockBuilder(ContextHelper::class)->disableOriginalConstructor()->getMock();
 
         $this->applicationsHelper = $this->getMock(ApplicationsHelperInterface::class);
+        $this->applicationsHelper->expects($this->any())
+            ->method('getExecutionRoute')
+            ->willReturn(self::EXECUTION_ROUTE_NAME);
+        $this->applicationsHelper->expects($this->any())->method('getDialogRoute')->willReturn(self::DIALOG_ROUTE_NAME);
 
         $this->extension = new OperationButtonProviderExtension(
             $this->operationRegistry,
@@ -59,18 +72,13 @@ class OperationButtonProviderExtensionTest extends \PHPUnit_Framework_TestCase
      * @param array $operations
      * @param ButtonSearchContext $buttonSearchContext
      * @param array $expected
-     *
-     * @return ButtonContext
      */
     public function testFind(array $operations, ButtonSearchContext $buttonSearchContext, array $expected)
     {
         $this->assertOperationRegistryMethodsCalled($operations, $buttonSearchContext);
-
         $this->assertContextHelperCalled($operations);
 
-        $result = $this->extension->find($buttonSearchContext);
-
-        $this->assertEquals($result, $expected);
+        $this->assertEquals($expected, $this->extension->find($buttonSearchContext));
     }
 
     /**
@@ -79,43 +87,41 @@ class OperationButtonProviderExtensionTest extends \PHPUnit_Framework_TestCase
     public function findDataProvider()
     {
         $operation1 = $this->createOperationMock(true);
+        $operation2 = $this->createOperationMock(true, true);
+
         $buttonSearchContext = $this->createButtonSearchContext();
-        $buttonContext = $this->createButtonContext($buttonSearchContext);
+
+        $buttonContext1 = $this->createButtonContext($buttonSearchContext);
+        $buttonContext2 = $this->createButtonContext($buttonSearchContext, true);
 
         return [
             'single' => [
-                'operations' => [
-                    $operation1,
-                    $this->createOperationMock(false),
-                ],
+                'operations' => [$operation1, $this->createOperationMock(false), $operation2],
                 'buttonSearchContext' => $buttonSearchContext,
                 'expected' => [
-                    new OperationButton($operation1, $buttonContext),
+                    new OperationButton($operation1, $buttonContext1),
+                    new OperationButton($operation2, $buttonContext2)
                 ]
             ],
             'not available' => [
-                'operations' => [
-                    $this->createOperationMock(false),
-                ],
+                'operations' => [$this->createOperationMock(false)],
                 'buttonSearchContext' => $buttonSearchContext,
-                'expected' => [
-                ],
+                'expected' => []
             ]
         ];
     }
 
     /**
      * @param bool $isAvailable
+     * @param bool $withForm
      *
      * @return Operation|\PHPUnit_Framework_MockObject_MockObject
      */
-    private function createOperationMock($isAvailable = false)
+    private function createOperationMock($isAvailable = false, $withForm = false)
     {
         $operation = $this->getMockBuilder(Operation::class)->disableOriginalConstructor()->getMock();
-        $operation->expects($this->once())
-            ->method('isAvailable')
-            ->with(new ActionData())
-            ->willReturn($isAvailable);
+        $operation->expects($this->once())->method('isAvailable')->with(new ActionData())->willReturn($isAvailable);
+        $operation->expects($this->once())->method('hasForm')->willReturn($withForm);
 
         return $operation;
     }
@@ -127,25 +133,34 @@ class OperationButtonProviderExtensionTest extends \PHPUnit_Framework_TestCase
     {
         $buttonSearchContext = new ButtonSearchContext();
 
-        return $buttonSearchContext->setRouteName(uniqid())
-            ->setEntity(uniqid(), mt_rand(1, 100))
-            ->setGridName(uniqid())
-            ->setGroup(uniqid())
-            ->setReferrer(uniqid());
+        return $buttonSearchContext->setRouteName(self::ROUTE_NAME)
+            ->setEntity(self::ENTITY_CLASS, self::ENTITY_ID)
+            ->setGridName(self::DATAGRID_NAME)
+            ->setGroup(self::GROUP)
+            ->setReferrer(self::REFERRER_URL);
     }
 
     /**
      * @param ButtonSearchContext $buttonSearchContext
+     * @param bool $isForm
      *
      * @return ButtonContext
      */
-    private function createButtonContext(ButtonSearchContext $buttonSearchContext)
+    private function createButtonContext(ButtonSearchContext $buttonSearchContext, $isForm = false)
     {
-        return (new ButtonContext())->setUnavailableHidden(true)
+        $context = new ButtonContext();
+        $context->setUnavailableHidden(true)
             ->setDatagridName($buttonSearchContext->getGridName())
             ->setEntity($buttonSearchContext->getEntityClass(), $buttonSearchContext->getEntityId())
             ->setRouteName($buttonSearchContext->getRouteName())
-            ->setGroup($buttonSearchContext->getGroup());
+            ->setGroup($buttonSearchContext->getGroup())
+            ->setExecutionUrl(self::EXECUTION_ROUTE_NAME);
+
+        if ($isForm) {
+            $context->setDialogUrl(self::DIALOG_ROUTE_NAME);
+        }
+
+        return $context;
     }
 
     /**
