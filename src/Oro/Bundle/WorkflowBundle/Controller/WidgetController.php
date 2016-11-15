248656a16ad44fb30aa5547576cedf6670b4d0eb
Vladimir Tsykun <Vladimir.Tsykun@effective-soft.com>
2016-11-15 23:25:09 +0300

    BAP-12581: Skip all Start Transitions on related entity view if has init options - updated WidgetController for skip transition with init options - added method Transition:isEmptyInitOptions. it will be use in WidgetController - updated unit tests

--- src/Oro/Bundle/WorkflowBundle/Controller/WidgetController.php
+++ src/Oro/Bundle/WorkflowBundle/Controller/WidgetController.php
@@ -421,8 +421,10 @@ class WidgetController extends Controller
     protected function getStartTransitionData(Workflow $workflow, Transition $transition, $entity)
     {
         $errors = new ArrayCollection();
-        $isAllowed = $workflow->isStartTransitionAvailable($transition, $entity, [], $errors);
-        if ($isAllowed || !$transition->isUnavailableHidden()) {
+        $isAllowed = $workflow->isStartTransitionAvailable($transition, $entity, [], $errors) ||
+            !$transition->isUnavailableHidden();
+
+        if ($isAllowed && $transition->isEmptyInitOptions()) {
             return [
                 'workflow' => $workflow,
                 'transition' => $transition,
