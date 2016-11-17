eaa543a7039118adcfe5c3c0ab244a0ee898d2c6
Eugene Sodin <esodin@ukr.net>
2016-11-17 16:36:35 +0200

    BAP-11753: Import / export execution in the background
     - Adding message processors

---
+++ src/Oro/Bundle/ImportExportBundle/Async/CliImportMessageProcessor.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace Oro\Bundle\ImportExportBundle\Async;
+
+use Oro\Bundle\ConfigBundle\Config\ConfigManager;
+use Oro\Bundle\ImportExportBundle\Handler\CliImportHandler;
+use Oro\Bundle\ImportExportBundle\Job\JobExecutor;
+use Oro\Bundle\NotificationBundle\Async\Topics as NotificationTopics;
+use Oro\Component\MessageQueue\Client\MessageProducerInterface;
+use Oro\Component\MessageQueue\Client\TopicSubscriberInterface;
+use Oro\Component\MessageQueue\Consumption\MessageProcessorInterface;
+use Oro\Component\MessageQueue\Job\JobRunner;
+use Oro\Component\MessageQueue\Transport\MessageInterface;
+use Oro\Component\MessageQueue\Transport\SessionInterface;
+use Oro\Component\MessageQueue\Util\JSON;
+use Psr\Log\LoggerInterface;
+
+class CliImportMessageProcessor implements MessageProcessorInterface, TopicSubscriberInterface
+{
+    /**
+     * @var CliImportHandler
+     */
+    private $cliImportHandler;
+
+    /**
+     * @var JobRunner
+     */
+    private $jobRunner;
+
+    /**
+     * @var MessageProducerInterface
+     */
+    private $producer;
+
+    /**
+     * @var ConfigManager
+     */
+    private $configManager;
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    public function __construct(
+        CliImportHandler $cliImportHandler,
+        JobRunner $jobRunner,
+        MessageProducerInterface $producer,
+        ConfigManager $configManager,
+        LoggerInterface $logger
+    ) {
+        $this->cliImportHandler = $cliImportHandler;
+        $this->jobRunner = $jobRunner;
+        $this->producer = $producer;
+        $this->configManager = $configManager;
+        $this->logger = $logger;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function process(MessageInterface $message, SessionInterface $session)
+    {
+        $body = JSON::decode($message->getBody());
+
+        if (! isset($body['alias'], $body['file_name'])) {
+            $this->logger->critical(
+                sprintf('Invalid message: %s', $body),
+                ['message' => $message]
+            );
+
+            return self::REJECT;
+        }
+
+        $result = $this->jobRunner->runUnique(
+            $message->getMessageId(),
+            sprintf('oro:import:cli:%s:%s', $body['alias'], $message->getMessageId()),
+            function () use ($body) {
+                $this->cliImportHandler->setImportingFileName($body['file_name']);
+
+                $result = $this->cliImportHandler->handleImport(
+                    JobExecutor::JOB_IMPORT_FROM_CSV,
+                    $body['alias']
+                );
+
+                $summary = sprintf(
+                    'Import from file %s for the %s is completed, success: %s, counts: %d, errors: %d, message: %s',
+                    $body['file_name'],
+                    $result['success'],
+                    $result['counts'],
+                    $result['errors'],
+                    $result['message']
+                );
+
+                $this->logger->info($summary);
+
+                if (isset($body['notify_email'])) {
+                    $fromEmail = $this->configManager->get('oro_notification.email_notification_sender_email');
+                    $fromName = $this->configManager->get('oro_notification.email_notification_sender_name');
+                    $this->producer->send(
+                        NotificationTopics::SEND_NOTIFICATION_EMAIL,
+                        [
+                            'fromEmail' => $fromEmail,
+                            'fromName' => $fromName,
+                            'toEmail' => $body['notify_email'],
+                            'subject' => $result['$message'],
+                            'body' => $summary
+                        ]
+                    );
+                }
+
+                return $result['success'];
+            }
+        );
+
+        return $result ? self::ACK : self::REJECT;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedTopics()
+    {
+        return [Topics::IMPORT_CLI];
+    }
+}
---
+++ src/Oro/Bundle/ImportExportBundle/Async/Topics.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Oro\Bundle\ImportExportBundle\Async;
+
+class Topics
+{
+    const IMPORT_CLI = 'oro.importexport.import_cli';
+    const IMPORT_CLI_VALIDATION = 'oro.importexport.import_cli_validation';
+    const IMPORT_HTTP = 'oro.importexport.import_http';
+    const IMPORT_HTTP_VALIDATION = 'oro.importexport.import_http_validation';
+}
