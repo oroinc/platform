ca00bd4a9cd405630e292df8468e366a655a10ef
Hryhorii Hrebiniuk <hhrebiniuk@magecore.com>
2016-11-18 21:05:41 +0200

    CRM-6825: Synchronize End time of event and recurrence
     added validation on recurrence end time

--- src/Oro/Bundle/FormBundle/Resources/public/js/validator/datetime.js
+++ src/Oro/Bundle/FormBundle/Resources/public/js/validator/datetime.js
@@ -1,28 +1,89 @@
-define(['underscore', 'orotranslation/js/translator', 'orolocale/js/formatter/datetime'
-    ], function(_, __, datetimeFormatter) {
+define(function(require) {
     'use strict';
 
+    var moment = require('moment');
+    var _ = require('underscore');
+    var __ = require('orotranslation/js/translator');
+    var datetimeFormatter = require('orolocale/js/formatter/datetime');
     var defaultParam = {
-        message: 'This value is not a valid datetime.'
+        message: 'oro.form.datetime.invalid',
+        maxMessage: 'oro.form.datetime.max',
+        minMessage: 'oro.form.datetime.min'
     };
+
+    /**
+     * Check if datetime fits the range, then returns true, if not - returns one of numbers -1 0 1
+     *  -1 - less than minimum
+     *   0 - not exact as minimum or maximum (while min and max are equal)
+     *   1 - greater than maximum
+     *
+     * @param {moment} valueMoment
+     * @param {string|undefined} min
+     * @param {string|undefined} max
+     * @returns {boolean|number}
+     */
+    function between(valueMoment, min, max) {
+        var format = datetimeFormatter.getBackendDateTimeFormat();
+        var result = true;
+        if (!_.isUndefined(min) && min === max) {
+            result = moment(min, format).diff(valueMoment) === 0 || 0;
+        } else {
+            if (!_.isUndefined(min) && min !== null) {
+                result = moment(min, format).diff(valueMoment) <= 0 || -1;
+            }
+            if (result === true && !_.isUndefined(max) && max !== null) {
+                result = moment(max, format).diff(valueMoment) >= 0 || 1;
+            }
+        }
+        return result;
+    }
 
     /**
      * @export oroform/js/validator/datetime
      */
     return [
         'DateTime',
-        function(value, element) {
-            var format = element.getAttribute('data-format');
+        function(value, element, param) {
+            var format = element.getAttribute('data-format') === 'backend' ?
+                datetimeFormatter.getBackendDateTimeFormat() : datetimeFormatter.getDateTimeFormat();
+            var valueMoment = moment(String(value), format, true);
             return this.optional(element) ||
-                datetimeFormatter.isDateTimeValid(String(value)) ||
-                format === 'backend' && datetimeFormatter.isBackendDateTimeValid(String(value));
+                valueMoment.isValid() && between(valueMoment, param.min, param.max) === true;
         },
         function(param, element) {
-            var value = String(this.elementValue(element));
+            var message;
+            var datetime;
+            var value = this.elementValue(element);
+            var format = element.getAttribute('data-format') === 'backend' ?
+                datetimeFormatter.getBackendDateTimeFormat() : datetimeFormatter.getDateTimeFormat();
+            var valueMoment = moment(String(value), format, true);
             var placeholders = {};
             param = _.extend({}, defaultParam, param);
             placeholders.value = value;
-            return __(param.message, placeholders);
+
+            if (!valueMoment.isValid()) {
+                message = param.message;
+            } else {
+                switch (between(valueMoment, param.min, param.max)) {
+                    case 0:
+                        message = param.exactMessage;
+                        datetime = param.min;
+                        break;
+                    case 1:
+                        message = param.maxMessage;
+                        datetime = param.max;
+                        break;
+                    case -1:
+                        message = param.minMessage;
+                        datetime = param.min;
+                        break;
+                    default:
+                        return '';
+                }
+                placeholders.limit = datetimeFormatter.formatDateTime(datetime);
+            }
+
+            return __(message, placeholders);
         }
     ];
 });
