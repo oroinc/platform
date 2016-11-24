a4ef01621bef858467837e6c574ce7ba6eec3b59
Eugene Sodin <esodin@ukr.net>
2016-11-24 18:53:03 +0200

    CRM-5838: Migrate integration's logic from JMSJobQueue to message queue
     - Fixing topics names

--- src/Oro/Bundle/IntegrationBundle/Async/Topics.php
+++ src/Oro/Bundle/IntegrationBundle/Async/Topics.php
@@ -3,7 +3,7 @@ namespace Oro\Bundle\IntegrationBundle\Async;
 
 class Topics
 {
-    const SYNC_INTEGRATION = 'integration.sync_integration';
+    const SYNC_INTEGRATION = 'oro.integration.sync_integration';
 
     const REVERS_SYNC_INTEGRATION = 'oro.integration.revers_sync_integration';
 }
