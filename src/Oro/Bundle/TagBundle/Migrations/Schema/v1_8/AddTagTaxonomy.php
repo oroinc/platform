d8026944f8ca0e80618163c5b7a3e06e3e7a9825
Samuel Denis-D'Ortun <sam@sddproductions.com>
2016-12-06 16:02:44 -0500

    Add migrations

---
+++ src/Oro/Bundle/TagBundle/Migrations/Schema/v1_8/AddTagTaxonomy.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Oro\Bundle\TagBundle\Migrations\Schema\v1_8;
+
+use Doctrine\DBAL\Schema\Schema;
+
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+
+class AddTagNameOrganizationIndex implements Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        /** Generate table oro_tag_tag **/
+        $table = $schema->createTable('oro_tag_taxonomy');
+
+        $table->addColumn('id', 'integer', ['autoincrement' => true]);
+        $table->addColumn('organization_id', 'integer', ['notnull' => false]);
+        $table->addColumn('user_owner_id', 'integer', ['notnull' => false]);
+        $table->addColumn('name', 'string', ['length' => 50]);
+        $table->addColumn('background_color', 'string', ['length' => 7]);
+        $table->addColumn('created', 'datetime', []);
+        $table->addColumn('updated', 'datetime', []);
+        $table->setPrimaryKey(['id']);
+        $table->addUniqueIndex(['name','organization_id'], 'name_organization_idx');
+        $table->addIndex(['user_owner_id'], 'IDX_B18F16C79EB185F9', []);
+        /** End of generate table oro_tag_tag **/
+
+        $tagTable = $schema->getTable('oro_tag_tag');
+        $tagTable->addColumn('taxonomy_id', 'integer', ['notnull' => false]);
+
+        $tagTable->addForeignKeyConstraint($table, ['taxonomy_id'], ['id'], ['onDelete' => 'SET NULL', 'onUpdate' => null]);
+    }
+}
+
