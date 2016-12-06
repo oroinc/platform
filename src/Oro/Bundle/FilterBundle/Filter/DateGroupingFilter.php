e617d89bbc59c9865f3466f5e7978cfd98247986
Alexandru Puscas <apuscas@pitechnologies.ro>
2016-12-06 11:32:47 +0200

    BB-5445: Create filter. - Created filter query and column.

--- src/Oro/Bundle/FilterBundle/Filter/DateGroupingFilter.php
+++ src/Oro/Bundle/FilterBundle/Filter/DateGroupingFilter.php
@@ -2,11 +2,19 @@
 
 namespace Oro\Bundle\FilterBundle\Filter;
 
+use Doctrine\ORM\QueryBuilder;
+
 use Oro\Bundle\FilterBundle\Datasource\FilterDatasourceAdapterInterface;
+use Oro\Bundle\FilterBundle\Datasource\Orm\OrmFilterDatasourceAdapter;
 use Oro\Bundle\FilterBundle\Form\Type\Filter\DateGroupingFilterType;
 
 class DateGroupingFilter extends ChoiceFilter
 {
+    const COLUMN_NAME_SUFFIX = 'DateGroupingFilter';
+
+    /** @var array */
+    protected $groupingNames = [];
+
     /**
      * {@inheritdoc}
      */
@@ -16,11 +24,101 @@ class DateGroupingFilter extends ChoiceFilter
     }
 
     /**
-     * {@inheritdoc}
+     * {@inheritDoc}
      */
-    protected function buildExpr(FilterDatasourceAdapterInterface $ds, $comparisonType, $fieldName, $data)
+    public function apply(FilterDatasourceAdapterInterface $ds, $data)
     {
-        //TODO: Create the query for grouping the result by day
-        return parent::buildExpr($ds, $comparisonType, $fieldName, $data);
+        $data = $this->parseData($data);
+        if (!$data) {
+            return false;
+        }
+
+        /** @var OrmFilterDatasourceAdapter $ds */
+        $qb = $ds->getQueryBuilder();
+        $dataName = $this->generateDataName($this->get(FilterUtility::DATA_NAME_KEY) . self::COLUMN_NAME_SUFFIX);
+        $columnName = $this->getOr('column_name', $dataName);
+
+        switch ($data['value']) {
+            case DateGroupingFilterType::TYPE_DAY:
+                $this->addFilter(DateGroupingFilterType::TYPE_DAY, $qb);
+                $this->addFilter(DateGroupingFilterType::TYPE_MONTH, $qb);
+                $this->addFilter(DateGroupingFilterType::TYPE_YEAR, $qb);
+                $qb->addSelect(
+                    sprintf(
+                        "CONCAT(%s, ' ', %s, ' ', %s) as $columnName",
+                        $this->groupingNames[DateGroupingFilterType::TYPE_DAY],
+                        $this->groupingNames[DateGroupingFilterType::TYPE_MONTH],
+                        $this->groupingNames[DateGroupingFilterType::TYPE_YEAR]
+                    )
+                );
+                break;
+            case DateGroupingFilterType::TYPE_MONTH:
+                $this->addFilter(DateGroupingFilterType::TYPE_MONTH, $qb);
+                $this->addFilter(DateGroupingFilterType::TYPE_YEAR, $qb);
+                $qb->addSelect(
+                    sprintf(
+                        "CONCAT(%s, ' ', %s) as $columnName",
+                        $this->groupingNames[DateGroupingFilterType::TYPE_MONTH],
+                        $this->groupingNames[DateGroupingFilterType::TYPE_YEAR]
+                    )
+                );
+                break;
+            case DateGroupingFilterType::TYPE_QUARTER:
+                $this->addFilter(DateGroupingFilterType::TYPE_QUARTER, $qb);
+                $this->addFilter(DateGroupingFilterType::TYPE_YEAR, $qb);
+                $qb->addSelect(
+                    sprintf(
+                        "CONCAT(%s, ' ', %s) as $columnName",
+                        $this->groupingNames[DateGroupingFilterType::TYPE_QUARTER],
+                        $this->groupingNames[DateGroupingFilterType::TYPE_YEAR]
+                    )
+                );
+                break;
+            default:
+                $this->addFilter(DateGroupingFilterType::TYPE_YEAR, $qb);
+                $qb->addSelect(
+                    sprintf(
+                        "%s as $columnName",
+                        $this->groupingNames[DateGroupingFilterType::TYPE_YEAR]
+                    )
+                );
+        }
+
+        return true;
+    }
+
+    /**
+     * @param $groupBy
+     * @param QueryBuilder $qb
+     */
+    protected function addFilter($groupBy, QueryBuilder $qb)
+    {
+        $dataName = $this->generateDataName($this->get(FilterUtility::DATA_NAME_KEY) . self::COLUMN_NAME_SUFFIX);
+        $columnName = $this->getOr('column_name', $dataName) . $groupBy;
+        $this->groupingNames[$groupBy] = sprintf('%s(%s)', $groupBy, $this->get(FilterUtility::DATA_NAME_KEY));
+        $select = sprintf('%s as %s', $this->groupingNames[$groupBy], $columnName);
+        $qb->addSelect($select)->addGroupBy($columnName);
+    }
+
+    /**
+     * @param mixed $data
+     * @return mixed
+     */
+    protected function parseData($data)
+    {
+        if (is_array($data) && !$data['value']) {
+            $data['value'] = DateGroupingFilterType::TYPE_DAY;
+        }
+
+        return $data;
+    }
+
+    /**
+     * @param string $dataName
+     * @return mixed
+     */
+    private function generateDataName($dataName)
+    {
+        return str_replace('.', '', $dataName);
     }
 }
--- src/Oro/Bundle/FilterBundle/Form/Type/Filter/DateGroupingFilterType.php
+++ src/Oro/Bundle/FilterBundle/Form/Type/Filter/DateGroupingFilterType.php
@@ -2,6 +2,9 @@
 
 namespace Oro\Bundle\FilterBundle\Form\Type\Filter;
 
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class DateGroupingFilterType extends AbstractChoiceType
@@ -39,16 +42,28 @@ class DateGroupingFilterType extends AbstractChoiceType
     /**
      * {@inheritDoc}
      */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->addEventListener(FormEvents::POST_SUBMIT, function (FormEvent $event) {
+            $data = $event->getData();
+            if (array_key_exists('value', $data) && !$data['value']) {
+                $event->setData(self::TYPE_DAY);
+            }
+        });
+    }
+
+    /**
+     * {@inheritDoc}
+     */
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults(
             array(
                 'field_type'       => 'choice',
-                'field_options'    => ['choices' => $this->getGroupChoices()],
-                'default_value'    => self::TYPE_DAY,
-                'null_value'       => null,
-                'class'            => null,
-                'populate_default' => true,
+                'field_options'    => [
+                    'choices' => $this->getGroupChoices(),
+                ],
+                'default_value'    => ucfirst(self::TYPE_DAY),
             )
         );
     }
@@ -56,9 +71,10 @@ class DateGroupingFilterType extends AbstractChoiceType
     protected function getGroupChoices()
     {
         return [
-            self::TYPE_MONTH,
-            self::TYPE_QUARTER,
-            self::TYPE_YEAR,
+            self::TYPE_DAY => ucfirst(self::TYPE_DAY),
+            self::TYPE_MONTH => ucfirst(self::TYPE_MONTH),
+            self::TYPE_QUARTER => ucfirst(self::TYPE_QUARTER),
+            self::TYPE_YEAR => ucfirst(self::TYPE_YEAR),
         ];
     }
 }
