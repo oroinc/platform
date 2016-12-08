27d0530b30415a73405cb26856a7022776163bef
Nikita Makarov <mesaverde228@gmail.com>
2016-12-08 15:55:42 +0300

    BAP-12876: Fix failed functional tests - Updated FUnctional tests at ActionBundle

--- src/Oro/Bundle/ActionBundle/Tests/Functional/Stub/ButtonProviderExtensionStub.php
+++ src/Oro/Bundle/ActionBundle/Tests/Functional/Stub/ButtonProviderExtensionStub.php
@@ -2,8 +2,11 @@
 
 namespace Oro\Bundle\ActionBundle\Tests\Functional\Stub;
 
+use Doctrine\Common\Collections\Collection;
+use Oro\Bundle\ActionBundle\Button\ButtonInterface;
+use Oro\Bundle\ActionBundle\Button\ButtonSearchContext;
+use Oro\Bundle\ActionBundle\Exception\UnsupportedButtonException;
 use Oro\Bundle\ActionBundle\Extension\ButtonProviderExtensionInterface;
-use Oro\Bundle\ActionBundle\Model\ButtonSearchContext;
 
 class ButtonProviderExtensionStub implements ButtonProviderExtensionInterface
 {
@@ -13,5 +16,31 @@ class ButtonProviderExtensionStub implements ButtonProviderExtensionInterface
     public function find(ButtonSearchContext $buttonSearchContext)
     {
         return [new ButtonStub()];
+    }
+
+    /**
+     * @param ButtonInterface $button
+     * @param ButtonSearchContext $buttonSearchContext
+     * @param Collection $errors
+     *
+     * @return bool
+     * @throws UnsupportedButtonException
+     */
+    public function isAvailable(
+        ButtonInterface $button,
+        ButtonSearchContext $buttonSearchContext,
+        Collection $errors = null
+    ) {
+        return true;
+    }
+
+    /**
+     * @param ButtonInterface $button
+     *
+     * @return bool
+     */
+    public function supports(ButtonInterface $button)
+    {
+        return $button instanceof ButtonStub;
     }
 }
