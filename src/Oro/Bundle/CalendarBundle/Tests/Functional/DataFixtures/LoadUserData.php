fa6f56308d5659ca1768d0c335996cf06a6e18d8
Ievgenii Myshusta <imyshusta@magecore.com>
2016-12-08 12:13:07 +0200

    BAP-12146: Incorrect deletion of the Excepted Events from Event Grid in OroCRM by Mass Action

--- src/Oro/Bundle/CalendarBundle/Tests/Functional/DataFixtures/LoadUserData.php
+++ src/Oro/Bundle/CalendarBundle/Tests/Functional/DataFixtures/LoadUserData.php
@@ -4,11 +4,13 @@ namespace Oro\Bundle\CalendarBundle\Tests\Functional\DataFixtures;
 
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\Yaml\Yaml;
 
+use Doctrine\Common\Collections\Criteria;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
+
+use Oro\Bundle\UserBundle\Entity\UserApi;
 
 class LoadUserData extends AbstractFixture implements DependentFixtureInterface, ContainerAwareInterface
 {
@@ -55,5 +57,33 @@ class LoadUserData extends AbstractFixture implements DependentFixtureInterface,
             $userManager->updateUser($user);
             $this->setReference($user->getUsername(), $user);
         }
+
+        $user = $userManager->createUser();
+        $role = $manager->getRepository('OroUserBundle:Role')->findOneBy(['role' => 'ROLE_ADMINISTRATOR']);
+        $apiKey = new UserApi();
+        $apiKey->setApiKey('foo_user_1_api_key');
+        $apiKey->setOrganization($organization);
+        $user->setUsername('foo_user_1')
+            ->setPlainPassword('password')
+            ->setEmail('foo_user_1@example.com')
+            ->setFirstName('Billy')
+            ->setLastName('Wilf')
+            ->addApiKey($apiKey)
+            ->setOrganization($organization)
+            ->addOrganization($organization)
+            ->addRole($role)
+            ->setEnabled(true);
+        $userManager->updateUser($user);
+        $this->setReference('oro_calendar:user:foo_user_1', $user);
+
+        $calendar = $manager->getRepository('OroCalendarBundle:Calendar')
+            ->findOneBy(
+                [
+                    'owner' => $user,
+                    'organization' => $user->getOrganization(),
+                ]
+            );
+
+        $this->setReference('oro_calendar:calendar:foo_user_1', $calendar);
     }
 }
