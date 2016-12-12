aec51cf42e3bc030184bfd2c6cb9d34cac7076a0
Calin Bolea <calin.bolea@evozon.com>
2016-12-12 18:42:53 +0200

    CRM-6956: Jobs should be disabled along with email feature disabled - Added functional test for cron load command

--- src/Oro/Bundle/CronBundle/Tests/Functional/Command/CronDefinitionsLoadCommandTest.php
+++ src/Oro/Bundle/CronBundle/Tests/Functional/Command/CronDefinitionsLoadCommandTest.php
@@ -4,6 +4,7 @@ namespace Oro\Bundle\CronBundle\Tests\Functinal\Command;
 
 use Doctrine\ORM\EntityRepository;
 use Oro\Bundle\CronBundle\Entity\Schedule;
+use Oro\Bundle\FeatureToggleBundle\Checker\FeatureChecker;
 use Oro\Bundle\TestFrameworkBundle\Test\WebTestCase;
 
 /**
@@ -47,11 +48,35 @@ class CronDefinitionsLoadCommandTest extends WebTestCase
         );
     }
 
+    public function testShouldNotLoadCommandDefinitionFromApplicationIfFeatureIsDisabled()
+    {
+        $this->mockFeatureChecker();
+
+        $result = $this->runCommand('oro:cron:definitions:load');
+        $this->assertNotEmpty($result);
+
+        $this->assertEquals("Removing all previously loaded commands...\n", $result);
+    }
+
     /**
      * @return EntityRepository
      */
     private function getScheduleRepository()
     {
         return $this->getContainer()->get('doctrine')->getRepository(Schedule::class);
+    }
+
+    protected function mockFeatureChecker()
+    {
+        $featureChecker = $this->getMockBuilder(FeatureChecker::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $featureChecker->expects($this->any())
+            ->method('isResourceEnabled')
+            ->with($this->anything())
+            ->willReturn(false);
+
+        $this->getContainer()->set('oro_featuretoggle.checker.feature_checker', $featureChecker);
     }
 }
