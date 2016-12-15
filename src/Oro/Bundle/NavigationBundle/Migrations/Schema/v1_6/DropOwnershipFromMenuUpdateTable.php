f62528027d0d446f72852a41031fbb91d9535e41
Andrey Yatsenco <yatsenco@gmail.com>
2016-12-15 09:44:42 +0200

    BB-6091: Fix functional tests - fixed migration to delete removed columns configs - fixed tests

--- src/Oro/Bundle/NavigationBundle/Migrations/Schema/v1_6/DropOwnershipFromMenuUpdateTable.php
+++ src/Oro/Bundle/NavigationBundle/Migrations/Schema/v1_6/DropOwnershipFromMenuUpdateTable.php
@@ -2,9 +2,7 @@
 
 namespace Oro\Bundle\NavigationBundle\Migrations\Schema\v1_6;
 
-use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Schema\Schema;
-use Doctrine\DBAL\Types\Type;
 
 use Oro\Bundle\MigrationBundle\Migration\Migration;
 use Oro\Bundle\MigrationBundle\Migration\OrderedMigrationInterface;
@@ -33,43 +31,51 @@ class DropOwnershipFromMenuUpdateTable implements Migration, OrderedMigrationInt
             $table->dropIndex('oro_navigation_menu_upd_uidx');
         }
 
-        if ($schema->hasTable('oro_entity_config_index_value') && $schema->hasTable('oro_entity_config_field')) {
-            $queries->addPostQuery(
-                new ParametrizedSqlMigrationQuery(
-                    'DELETE FROM oro_entity_config_index_value '
-                    . 'WHERE entity_id IS NULL AND field_id IN ('
-                    . 'SELECT oecf.id FROM oro_entity_config_field AS oecf '
-                    . 'WHERE oecf.field_name IN (:field_names) '
-                    . 'AND oecf.entity_id = ('
-                    . 'SELECT oec.id FROM oro_entity_config AS oec WHERE oec.class_name = :class_name'
-                    . '))',
-                    [
-                        'field_names' => ['ownership_type', 'owner_id'],
-                        'class_name'  => 'Oro\Bundle\NavigationBundle\Entity\MenuUpdate',
-                    ],
-                    [
-                        'field_names' => Connection::PARAM_STR_ARRAY,
-                        'class_name'  => Type::STRING
-                    ]
-                )
-            );
-            $queries->addPostQuery(
-                new ParametrizedSqlMigrationQuery(
-                    'DELETE FROM oro_entity_config_field '
-                    . 'WHERE field_name IN (:field_names) '
-                    . 'AND entity_id IN ('
-                    . 'SELECT id FROM oro_entity_config WHERE class_name = :class_name'
-                    . ')',
-                    [
-                        'field_names' => ['ownership_type', 'owner_id'],
-                        'class_name'  => 'Oro\Bundle\NavigationBundle\Entity\MenuUpdate',
-                    ],
-                    [
-                        'field_names' => Connection::PARAM_STR_ARRAY,
-                        'class_name'  => Type::STRING
-                    ]
-                )
-            );
-        }
+        $queries->addPostQuery(
+            $this->getDropEntityConfigFieldQuery('Oro\Bundle\NavigationBundle\Entity\MenuUpdate', 'ownership_type')
+        );
+        $queries->addPostQuery(
+            $this->getDropEntityConfigFieldQuery('Oro\Bundle\NavigationBundle\Entity\MenuUpdate', 'owner_id')
+        );
+    }
+
+    /**
+     * @param string $className
+     * @param string $fieldName
+     *
+     * @return ParametrizedSqlMigrationQuery
+     */
+    protected function getDropEntityConfigFieldQuery($className, $fieldName)
+    {
+        $dropFieldIndexSql = 'DELETE FROM oro_entity_config_index_value'
+            . ' WHERE entity_id IS NULL AND field_id IN ('
+            . ' SELECT oecf.id FROM oro_entity_config_field AS oecf'
+            . ' WHERE oecf.field_name = :field'
+            . ' AND oecf.entity_id IN ('
+            . ' SELECT oec.id'
+            . ' FROM oro_entity_config AS oec'
+            . ' WHERE oec.class_name = :class'
+            . ' ))';
+        $dropFieldSql      = 'DELETE FROM oro_entity_config_field'
+            . ' WHERE field_name = :field'
+            . ' AND entity_id IN ('
+            . ' SELECT id'
+            . ' FROM oro_entity_config'
+            . ' WHERE class_name = :class'
+            . ' )';
+
+        $query = new ParametrizedSqlMigrationQuery();
+        $query->addSql(
+            $dropFieldIndexSql,
+            ['field' => $fieldName, 'class' => $className],
+            ['field' => 'string', 'class' => 'string']
+        );
+        $query->addSql(
+            $dropFieldSql,
+            ['field' => $fieldName, 'class' => $className],
+            ['field' => 'string', 'class' => 'string']
+        );
+
+        return $query;
     }
 }
