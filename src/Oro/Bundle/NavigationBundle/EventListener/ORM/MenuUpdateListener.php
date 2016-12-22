93131168b2122ff23a12e5f3ab8674ce6df53969
Vitaly Shutko <vshutko@oroinc.com>
2016-12-22 15:03:32 +0200

    BB-6573: Create entity listeners for entities that implement MenuUpdateInterface - fixed bugs and refactored code

--- src/Oro/Bundle/NavigationBundle/EventListener/ORM/MenuUpdateListener.php
+++ src/Oro/Bundle/NavigationBundle/EventListener/ORM/MenuUpdateListener.php
@@ -5,24 +5,17 @@ namespace Oro\Bundle\NavigationBundle\EventListener\ORM;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Event\LifecycleEventArgs;
 
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+
 use Oro\Bundle\NavigationBundle\Entity\MenuUpdateInterface;
 use Oro\Bundle\NavigationBundle\Entity\Repository\MenuUpdateRepository;
 use Oro\Bundle\NavigationBundle\Utils\MenuUpdateUtils;
 
 class MenuUpdateListener
 {
-    /**
-     * @var MenuUpdateRepository
-     */
-    private $menuUpdateRepository;
+    use ContainerAwareTrait;
 
-    /**
-     * @param MenuUpdateRepository $menuUpdateRepository
-     */
-    public function __construct(MenuUpdateRepository $menuUpdateRepository)
-    {
-        $this->menuUpdateRepository = $menuUpdateRepository;
-    }
+    const MENU_CACHE_SERVICE_ID = 'oro_navigation.menu_update.cache';
 
     /**
      * @param MenuUpdateInterface $update
@@ -57,10 +50,12 @@ class MenuUpdateListener
      */
     private function resetAndWarmupResultCache(EntityManagerInterface $em, MenuUpdateInterface $update)
     {
-        $em->getConfiguration()->getResultCacheImpl()->delete(
+        $this->container->get(self::MENU_CACHE_SERVICE_ID)->delete(
             MenuUpdateUtils::generateKey($update->getMenu(), $update->getScope())
         );
 
-        $this->menuUpdateRepository->findMenuUpdatesByScope($update->getMenu(), $update->getScope());
+        /** @var MenuUpdateRepository $repository */
+        $repository = $em->getRepository($em->getClassMetadata(get_class($update))->getName());
+        $repository->findMenuUpdatesByScope($update->getMenu(), $update->getScope());
     }
 }
--- src/Oro/Bundle/NavigationBundle/Tests/Unit/EventListener/ORM/MenuUpdateListenerTest.php
+++ src/Oro/Bundle/NavigationBundle/Tests/Unit/EventListener/ORM/MenuUpdateListenerTest.php
@@ -2,10 +2,12 @@
 
 namespace Oro\Bundle\NavigationBundle\Tests\Unit\EventListener\ORM;
 
-use Doctrine\Common\Cache\Cache;
-use Doctrine\ORM\Configuration;
+use Doctrine\Common\Cache\CacheProvider;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\ORM\Mapping\ClassMetadata;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 use Oro\Bundle\NavigationBundle\Entity\MenuUpdate;
 use Oro\Bundle\NavigationBundle\Entity\Repository\MenuUpdateRepository;
@@ -17,33 +19,48 @@ class MenuUpdateListenerTest extends \PHPUnit_Framework_TestCase
 {
     use EntityTrait;
 
-    /** @var MenuUpdateRepository|\PHPUnit_Framework_MockObject_MockObject */
-    private $menuUpdateRepository;
+    /** @var ContainerInterface|\PHPUnit_Framework_MockObject_MockObject */
+    private $container;
+
+    /** @var CacheProvider|\PHPUnit_Framework_MockObject_MockObject */
+    private $cache;
 
     /** @var MenuUpdateListener */
     private $listener;
 
     protected function setUp()
     {
-        $this->menuUpdateRepository = $this->getMock(MenuUpdateRepository::class, [], [], '', false);
+        $this->cache = $this->getMock(CacheProvider::class);
 
-        $this->listener = new MenuUpdateListener($this->menuUpdateRepository);
+        $this->container = $this->getMock(ContainerInterface::class);
+        $this->container->expects($this->any())
+            ->method('get')
+            ->with(MenuUpdateListener::MENU_CACHE_SERVICE_ID)
+            ->willReturn($this->cache);
+
+        $this->listener = new MenuUpdateListener();
+        $this->listener->setContainer($this->container);
     }
 
     public function testPostPersist()
     {
         $scope = $this->getEntity(Scope::class);
+        /** @var MenuUpdate $update */
         $update = $this->getEntity(MenuUpdate::class, ['menu' => 'test', 'scope' => $scope]);
 
-        $args = $this->getLifecycleEventArgs();
+        $repository = $this->getMock(MenuUpdateRepository::class, [], [], '', false);
+        $repository->expects($this->once())->method('findMenuUpdatesByScope')->with('test', $scope);
 
-        /** @var Cache|\PHPUnit_Framework_MockObject_MockObject $resultCacheImpl $resultCacheImpl */
-        $resultCacheImpl = $args->getEntityManager()->getConfiguration()->getResultCacheImpl();
-        $resultCacheImpl->expects($this->once())->method('delete');
+        $classMetadata = $this->getMock(ClassMetadata::class, [], [], '', false);
 
-        $this->menuUpdateRepository->expects($this->once())
-            ->method('findMenuUpdatesByScope')
-            ->with('test', $scope);
+        /** @var EntityManagerInterface|\PHPUnit_Framework_MockObject_MockObject $em */
+        $em = $this->getMock(EntityManagerInterface::class);
+        $em->expects($this->any())->method('getRepository')->willReturn($repository);
+        $em->expects($this->any())->method('getClassMetadata')->willReturn($classMetadata);
+
+        /** @var LifecycleEventArgs|\PHPUnit_Framework_MockObject_MockObject $args */
+        $args = $this->getMock(LifecycleEventArgs::class, [], [], '', false);
+        $args->expects($this->any())->method('getEntityManager')->willReturn($em);
 
         $this->listener->postPersist($update, $args);
     }
@@ -51,17 +68,22 @@ class MenuUpdateListenerTest extends \PHPUnit_Framework_TestCase
     public function testPostUpdate()
     {
         $scope = $this->getEntity(Scope::class);
+        /** @var MenuUpdate $update */
         $update = $this->getEntity(MenuUpdate::class, ['menu' => 'test', 'scope' => $scope]);
 
-        $args = $this->getLifecycleEventArgs();
+        $repository = $this->getMock(MenuUpdateRepository::class, [], [], '', false);
+        $repository->expects($this->once())->method('findMenuUpdatesByScope')->with('test', $scope);
 
-        /** @var Cache|\PHPUnit_Framework_MockObject_MockObject $resultCacheImpl $resultCacheImpl */
-        $resultCacheImpl = $args->getEntityManager()->getConfiguration()->getResultCacheImpl();
-        $resultCacheImpl->expects($this->once())->method('delete');
+        $classMetadata = $this->getMock(ClassMetadata::class, [], [], '', false);
 
-        $this->menuUpdateRepository->expects($this->once())
-            ->method('findMenuUpdatesByScope')
-            ->with('test', $scope);
+        /** @var EntityManagerInterface|\PHPUnit_Framework_MockObject_MockObject $em */
+        $em = $this->getMock(EntityManagerInterface::class);
+        $em->expects($this->any())->method('getRepository')->willReturn($repository);
+        $em->expects($this->any())->method('getClassMetadata')->willReturn($classMetadata);
+
+        /** @var LifecycleEventArgs|\PHPUnit_Framework_MockObject_MockObject $args */
+        $args = $this->getMock(LifecycleEventArgs::class, [], [], '', false);
+        $args->expects($this->any())->method('getEntityManager')->willReturn($em);
 
         $this->listener->postUpdate($update, $args);
     }
@@ -69,38 +91,23 @@ class MenuUpdateListenerTest extends \PHPUnit_Framework_TestCase
     public function testPostRemove()
     {
         $scope = $this->getEntity(Scope::class);
+        /** @var MenuUpdate $update */
         $update = $this->getEntity(MenuUpdate::class, ['menu' => 'test', 'scope' => $scope]);
 
-        $args = $this->getLifecycleEventArgs();
+        $repository = $this->getMock(MenuUpdateRepository::class, [], [], '', false);
+        $repository->expects($this->once())->method('findMenuUpdatesByScope')->with('test', $scope);
 
-        /** @var Cache|\PHPUnit_Framework_MockObject_MockObject $resultCacheImpl $resultCacheImpl */
-        $resultCacheImpl = $args->getEntityManager()->getConfiguration()->getResultCacheImpl();
-        $resultCacheImpl->expects($this->once())->method('delete');
-
-        $this->menuUpdateRepository->expects($this->once())
-            ->method('findMenuUpdatesByScope')
-            ->with('test', $scope);
-
-        $this->listener->postRemove($update, $args);
-    }
-
-    private function getLifecycleEventArgs()
-    {
-        /** @var Cache|\PHPUnit_Framework_MockObject_MockObject $resultCacheImpl */
-        $resultCacheImpl = $this->getMock(Cache::class);
-
-        /** @var Configuration|\PHPUnit_Framework_MockObject_MockObject $configuration */
-        $configuration = $this->getMock(Configuration::class);
-        $configuration->expects($this->any())->method('getResultCacheImpl')->willReturn($resultCacheImpl);
+        $classMetadata = $this->getMock(ClassMetadata::class, [], [], '', false);
 
         /** @var EntityManagerInterface|\PHPUnit_Framework_MockObject_MockObject $em */
         $em = $this->getMock(EntityManagerInterface::class);
-        $em->expects($this->any())->method('getConfiguration')->willReturn($configuration);
+        $em->expects($this->any())->method('getRepository')->willReturn($repository);
+        $em->expects($this->any())->method('getClassMetadata')->willReturn($classMetadata);
 
         /** @var LifecycleEventArgs|\PHPUnit_Framework_MockObject_MockObject $args */
         $args = $this->getMock(LifecycleEventArgs::class, [], [], '', false);
         $args->expects($this->any())->method('getEntityManager')->willReturn($em);
 
-        return $args;
+        $this->listener->postRemove($update, $args);
     }
 }
