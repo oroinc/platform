d1c797d762d61b9862cb2470525b2cbfea0ca06d
Dincho Todorov <dincho@users.noreply.github.com>
2016-12-23 16:28:38 +0200

    CRM-6723: Adapt Business customer to the new Customer association
    - fix several issues and cleanup

--- src/Oro/Bundle/EntityExtendBundle/Migration/Extension/ExtendExtension.php
+++ src/Oro/Bundle/EntityExtendBundle/Migration/Extension/ExtendExtension.php
@@ -913,7 +913,7 @@ class ExtendExtension implements NameGeneratorAwareInterface
      *
      * @throws SchemaException if valid primary key does not exist
      */
-    public function getPrimaryKeyColumnName(Table $table)
+    protected function getPrimaryKeyColumnName(Table $table)
     {
         if (!$table->hasPrimaryKey()) {
             throw new SchemaException(
