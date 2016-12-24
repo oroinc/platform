42dd1fa6d5ec800724f285f9579edfe4e7898d7a
Anton Khristiansen <akhristiansen@oroinc.com>
2016-12-24 19:59:00 +0200

    BB-6566: Installers have obsolete migrations (#5631)
     - fix installer versions
     - fix installer namespaces
     - fix missed call in OroLocaleBundleInstaller

--- src/Oro/Bundle/LocaleBundle/Migrations/Schema/v1_1/OroLocaleBundleScopeRelations.php
+++ src/Oro/Bundle/LocaleBundle/Migrations/Schema/v1_1/OroLocaleBundleScopeRelations.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Oro\Bundle\LocaleBundle\Migrations\Schema\Schema\v1_1;
+namespace Oro\Bundle\LocaleBundle\Migrations\Schema\v1_1;
 
 use Doctrine\DBAL\Schema\Schema;
 use Oro\Bundle\EntityExtendBundle\EntityConfig\ExtendScope;
