056c43de23e645541bdd2443a6533e793c3fd31c
Andrey Yatsenco <yatsenco@gmail.com>
2016-12-26 11:15:50 +0200

    BB-6622: Create PropertyAccess data provider - cs fix

--- src/Oro/Bundle/LayoutBundle/Tests/Unit/Layout/DataProvider/PropertyAccessDataProviderTest.php
+++ src/Oro/Bundle/LayoutBundle/Tests/Unit/Layout/DataProvider/PropertyAccessDataProviderTest.php
@@ -2,8 +2,9 @@
 
 namespace Oro\Bundle\LayoutBundle\Tests\Unit\Layout\DataProvider;
 
-use Oro\Bundle\LayoutBundle\Layout\DataProvider\PropertyAccessDataProvider;
 use Symfony\Component\PropertyAccess\PropertyAccessor;
+
+use Oro\Bundle\LayoutBundle\Layout\DataProvider\PropertyAccessDataProvider;
 
 class PropertyAccessDataProviderTest extends \PHPUnit_Framework_TestCase
 {
