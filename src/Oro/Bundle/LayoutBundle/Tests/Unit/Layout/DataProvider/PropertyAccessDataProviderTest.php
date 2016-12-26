d6ad07dbf6688c95dc6a3b8d7b89a73080e32c52
Andrey Yatsenco <yatsenco@gmail.com>
2016-12-26 11:15:00 +0200

    BB-6622: Create PropertyAccess data provider

---
+++ src/Oro/Bundle/LayoutBundle/Tests/Unit/Layout/DataProvider/PropertyAccessDataProviderTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Oro\Bundle\LayoutBundle\Tests\Unit\Layout\DataProvider;
+
+use Oro\Bundle\LayoutBundle\Layout\DataProvider\PropertyAccessDataProvider;
+use Symfony\Component\PropertyAccess\PropertyAccessor;
+
+class PropertyAccessDataProviderTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetValue()
+    {
+        $object = new \stdClass();
+        $propertyPath = 'foo.bar["fee"]';
+        $result = 'result';
+        $propertyAccess = $this->createMock(PropertyAccessor::class);
+        $propertyAccess->expects($this->once())
+            ->method('getValue')
+            ->with($object, $propertyPath)
+            ->willReturn($result);
+        $provider = new PropertyAccessDataProvider($propertyAccess);
+        $actual = $provider->getValue($object, $propertyPath);
+        $this->assertEquals($result, $actual);
+    }
+}
