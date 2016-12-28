fee4c388da76955a9cb5a7de8ec3a02219d31d79
Vladimir Seniuk <vseniuk@oroinc.com>
2016-12-28 15:18:42 +0200

    BB-6150: Apply block types for collections - fixed tests

--- src/Oro/Bundle/EntityConfigBundle/Tests/Unit/Layout/Mapper/ChainAttributeBlockTypeMapperTest.php
+++ src/Oro/Bundle/EntityConfigBundle/Tests/Unit/Layout/Mapper/ChainAttributeBlockTypeMapperTest.php
@@ -4,9 +4,6 @@ namespace Oro\Bundle\EntityConfigBundle\Tests\Unit\Layout\Mapper;
 
 use Doctrine\Common\Persistence\ManagerRegistry;
 
-use Doctrine\Common\Persistence\Mapping\ClassMetadata;
-use Doctrine\ORM\EntityManager;
-use Oro\Bundle\EntityConfigBundle\Entity\EntityConfigModel;
 use Oro\Bundle\EntityConfigBundle\Entity\FieldConfigModel;
 use Oro\Bundle\EntityConfigBundle\Layout\Mapper\AttributeBlockTypeMapperInterface;
 use Oro\Bundle\EntityConfigBundle\Layout\Mapper\ChainAttributeBlockTypeMapper;
@@ -48,67 +45,6 @@ class ChainAttributeBlockTypeMapperTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('attribute_string', $this->chainMapper->getBlockType($attribute));
     }
 
-    public function testGetBlockTypeFromAttributeTypesRegistry()
-    {
-        $this->chainMapper->addBlockType('string', 'attribute_string');
-
-        $attribute = new FieldConfigModel();
-        $attribute->setType('string');
-
-        $mapper = $this->getMockBuilder(AttributeBlockTypeMapperInterface::class)->getMock();
-        $mapper->expects($this->once())
-            ->method('getBlockType')
-            ->with($attribute)
-            ->willReturn(null);
-
-        $this->chainMapper->addMapper($mapper);
-
-        $this->assertEquals('attribute_string', $this->chainMapper->getBlockType($attribute));
-    }
-
-    public function testGetBlockTypeFromTargetClassesRegistry()
-    {
-        $this->chainMapper->addBlockTypeUsingMetadata(\stdClass::class, 'attribute_std_class');
-
-        $entity = new EntityConfigModel();
-        $entity->setClassName('EntityClass');
-
-        $attribute = new FieldConfigModel();
-        $attribute->setEntity($entity);
-
-        $mapper = $this->getMockBuilder(AttributeBlockTypeMapperInterface::class)->getMock();
-        $mapper->expects($this->once())
-            ->method('getBlockType')
-            ->with($attribute)
-            ->willReturn(null);
-
-        $metadata = $this->getMockBuilder(ClassMetadata::class)->getMock();
-        $metadata->expects($this->once())
-            ->method('getAssociationNames')
-            ->willReturn(['metadata_attribute']);
-
-        $metadata->expects($this->once())
-            ->method('getAssociationTargetClass')
-            ->with('metadata_attribute')
-            ->willReturn(\stdClass::class);
-
-        $objectManager = $this->getMockBuilder(EntityManager::class)->disableOriginalConstructor()->getMock();
-        $objectManager->expects($this->once())
-            ->method('getClassMetadata')
-            ->with('EntityClass')
-            ->willReturn($metadata);
-
-        $this->registry
-            ->expects($this->once())
-            ->method('getManagerForClass')
-            ->with('EntityClass')
-            ->willReturn($objectManager);
-
-        $this->chainMapper->addMapper($mapper);
-
-        $this->assertEquals('attribute_std_class', $this->chainMapper->getBlockType($attribute));
-    }
-
     public function testGetBlockTypeDefault()
     {
         $this->chainMapper->addBlockType('string', 'attribute_string');
@@ -125,6 +61,5 @@ class ChainAttributeBlockTypeMapperTest extends \PHPUnit_Framework_TestCase
         $this->chainMapper->addMapper($mapper);
 
         $this->assertEquals('default_block_type', $this->chainMapper->getBlockType($attribute));
-
     }
 }
