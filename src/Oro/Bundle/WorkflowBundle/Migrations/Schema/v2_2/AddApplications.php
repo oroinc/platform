4b366ca52e718e1eb8350e3a7d376a642070193b
Yauheni Kasenka <ykasenka@oroinc.com>
2017-01-04 20:47:18 +0300

    BB-6488: Remove status field and entity - cr updates

--- src/Oro/Bundle/WorkflowBundle/Migrations/Schema/v2_2/AddApplications.php
+++ src/Oro/Bundle/WorkflowBundle/Migrations/Schema/v2_2/AddApplications.php
@@ -4,8 +4,8 @@ namespace Oro\Bundle\WorkflowBundle\Migrations\Schema\v2_2;
 
 use Doctrine\DBAL\Schema\Comparator;
 use Doctrine\DBAL\Schema\Schema;
-
 use Doctrine\DBAL\Types\Type;
+
 use Oro\Bundle\ActionBundle\Provider\CurrentApplicationProviderInterface;
 use Oro\Bundle\MigrationBundle\Migration\Extension\DatabasePlatformAwareInterface;
 use Oro\Bundle\MigrationBundle\Migration\Extension\DatabasePlatformAwareTrait;
@@ -24,7 +24,7 @@ class AddApplications implements Migration, DatabasePlatformAwareInterface
     {
         $preSchema = clone $schema;
         $table = $preSchema->getTable('oro_workflow_definition');
-        $table->addColumn('applications', 'simple_array', ['notnull' => false, 'comment' => '(DC2Type:simple_array)',]);
+        $table->addColumn('applications', 'simple_array', ['notnull' => false, 'comment' => '(DC2Type:simple_array)']);
 
         foreach ($this->getSchemaDiff($schema, $preSchema) as $query) {
             $queries->addQuery($query);
