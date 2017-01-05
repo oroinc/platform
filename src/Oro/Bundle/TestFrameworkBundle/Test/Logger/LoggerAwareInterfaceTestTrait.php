68d763f67dfe820b50f2813943aac4d127a60698
Mykolay Miasnikov <mmiasnikov@oroinc.com>
2017-01-05 13:58:43 +0200

    BB-6811: Admin > Sales > Requests For Quote - value "Assigned Customers" was lost after use "Create Quote" - Modified \Oro\Bundle\RFPBundle\Storage\RequestToQuoteDataStorage::saveToStorage for support "assignedUsers" and "assignedAccountUsers" data - Modified \Oro\Bundle\ProductBundle\Form\Extension\AbstractProductDataStorageExtension::fillEntityData for support arrays and collections-type properties of restored entities - Added \Psr\Log\LoggerAwareTrait to \Oro\Bundle\ProductBundle\Form\Extension\AbstractProductDataStorageExtension for logging exceptions - Created \Oro\Bundle\TestFrameworkBundle\Test\Logger\LoggerAwareInterfaceTestTrait for testing classes that use LoggerAwareTrait and/or implement LoggerAwareInterface - Made the necessary changes it the unit-tests

---
+++ src/Oro/Bundle/TestFrameworkBundle/Test/Logger/LoggerAwareInterfaceTestTrait.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace Oro\Bundle\TestFrameworkBundle\Test\Logger;
+
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Use it for testing classes that use LoggerAwareTrait and/or implement LoggerAwareInterface
+ * Please, start by calling $this->setUpLoggerMock() method in the setUp() method of your testCase class.
+ * Then use assertions of this trait in the appropriate test methods.
+ */
+trait LoggerAwareInterfaceTestTrait
+{
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject|LoggerInterface
+     */
+    protected $loggerMock;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject|LoggerAwareInterface
+     */
+    protected $loggerAwareObject;
+
+    public function testSetLogger()
+    {
+        $this->assertNotNull(
+            $this->loggerAwareObject,
+            'LoggerAwareObject instance is null. May you have forgotten
+             to call $this->setUpLoggerMock() in $this->setUp() method?'
+        );
+
+        $loggerPropertyReflection = new \ReflectionProperty(
+            get_class($this->loggerAwareObject),
+            'logger'
+        );
+        $loggerPropertyReflection->setAccessible(true);
+
+        $this->assertInstanceOf(LoggerInterface::class, $this->loggerMock);
+
+        $this->assertSame(
+            $this->loggerMock,
+            $loggerPropertyReflection->getValue($this->loggerAwareObject)
+        );
+    }
+
+    /**
+     * Be sure to call this method in the setUp() method of the testCase class
+     * @param LoggerAwareInterface $testedObject
+     */
+    protected function setUpLoggerMock(LoggerAwareInterface $testedObject)
+    {
+        $this->loggerMock = $this->getMockBuilder(LoggerInterface::class)->getMock();
+        $this->loggerAwareObject = $testedObject;
+        $this->loggerAwareObject->setLogger($this->loggerMock);
+    }
+
+    protected function assertLoggerCalled()
+    {
+        $this->loggerMock->expects($this->atLeast(1))->method($this->anything());
+    }
+
+    protected function assertLoggerNotCalled()
+    {
+        $this->loggerMock->expects($this->never())->method($this->anything());
+    }
+
+    protected function assertLoggerEmergencyMethodCalled()
+    {
+        $this->loggerMock->expects($this->atLeast(1))->method('emergency');
+    }
+
+    protected function assertLoggerAlertMethodCalled()
+    {
+        $this->loggerMock->expects($this->atLeast(1))->method('alert');
+    }
+
+    protected function assertLoggerCriticalMethodCalled()
+    {
+        $this->loggerMock->expects($this->atLeast(1))->method('critical');
+    }
+
+    protected function assertLoggerErrorMethodCalled()
+    {
+        $this->loggerMock->expects($this->atLeast(1))->method('error');
+    }
+
+    protected function assertLoggerWarningMethodCalled()
+    {
+        $this->loggerMock->expects($this->atLeast(1))->method('warning');
+    }
+
+    protected function assertLoggerNoticeMethodCalled()
+    {
+        $this->loggerMock->expects($this->atLeast(1))->method('notice');
+    }
+
+    protected function assertLoggerInfoMethodCalled()
+    {
+        $this->loggerMock->expects($this->atLeast(1))->method('info');
+    }
+
+    protected function assertLoggerDebugMethodCalled()
+    {
+        $this->loggerMock->expects($this->atLeast(1))->method('debug');
+    }
+
+    protected function assertLoggerLogMethodCalled()
+    {
+        $this->loggerMock->expects($this->atLeast(1))->method('log');
+    }
+}
