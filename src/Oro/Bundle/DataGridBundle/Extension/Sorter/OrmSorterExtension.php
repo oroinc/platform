efab7a5bbc2d95e18a98004787c39402062b1bbd
Vova Soroka <vsoroka@magecore.com>
2017-01-14 01:20:01 +0200

    BAP-8103: Report/Segment builder add unexpected Order by to final query (#6539)

--- src/Oro/Bundle/DataGridBundle/Extension/Sorter/OrmSorterExtension.php
+++ src/Oro/Bundle/DataGridBundle/Extension/Sorter/OrmSorterExtension.php
@@ -2,6 +2,8 @@
 
 namespace Oro\Bundle\DataGridBundle\Extension\Sorter;
 
+use Doctrine\ORM\Query\Expr;
+
 use Oro\Bundle\DataGridBundle\Datagrid\Common\DatagridConfiguration;
 use Oro\Bundle\DataGridBundle\Datasource\DatasourceInterface;
 use Oro\Bundle\DataGridBundle\Datasource\Orm\OrmDatasource;
@@ -25,5 +27,43 @@ class OrmSorterExtension extends AbstractSorterExtension
     {
         /* @var OrmDatasource $datasource */
         $datasource->getQueryBuilder()->addOrderBy($sorter['data_name'], $direction);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function visitDatasource(DatagridConfiguration $config, DatasourceInterface $datasource)
+    {
+        parent::visitDatasource($config, $datasource);
+
+        // ensure that ORDER BY is specified explicitly, in case if sorting was not requested
+        // use sorting by
+        // - the primary key of the root table if the query does not have GROUP BY
+        // - the first column of GROUP BY if this clause exists
+        // if ORDER BY is not given, the order of rows is not predictable and they are returned
+        // in whatever order SQL server finds fastest to produce.
+        /** @var OrmDatasource $datasource */
+        $qb = $datasource->getQueryBuilder();
+        $orderBy = $qb->getDQLPart('orderBy');
+        if (empty($orderBy)) {
+            $groupBy = $qb->getDQLPart('groupBy');
+            if (empty($groupBy)) {
+                $rootEntities = $qb->getRootEntities();
+                $rootEntity = reset($rootEntities);
+                if ($rootEntity) {
+                    $rootAliases = $qb->getRootAliases();
+                    $rootAlias = reset($rootAliases);
+                    $rootIdFieldNames = $qb->getEntityManager()
+                        ->getClassMetadata($rootEntity)
+                        ->getIdentifierFieldNames();
+                    $qb->addOrderBy($rootAlias . '.' . reset($rootIdFieldNames));
+                }
+            } else {
+                /** @var Expr\GroupBy $firstExpr */
+                $firstExpr = reset($groupBy);
+                $exprParts = $firstExpr->getParts();
+                $qb->addOrderBy(reset($exprParts));
+            }
+        }
     }
 }
