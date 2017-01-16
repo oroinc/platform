b9f80b2c88f175c3299aa33c75eb847f6077cc6a
Vova Soroka <vsoroka@magecore.com>
2017-01-16 07:10:24 +0200

    Do not apply ACL for a root entity if API resource is based on another API resource

---
+++ src/Oro/Bundle/ApiBundle/Util/ConfigConverter.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Oro\Bundle\ApiBundle\Util;
+
+use Oro\Component\EntitySerializer\ConfigConverter as BaseConfigConverter;
+use Oro\Component\EntitySerializer\EntityConfig;
+use Oro\Bundle\ApiBundle\Config\EntityDefinitionConfig;
+
+class ConfigConverter extends BaseConfigConverter
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function buildEntityConfig(EntityConfig $result, array $config)
+    {
+        parent::buildEntityConfig($result, $config);
+
+        if (!empty($config[EntityDefinitionConfig::PARENT_RESOURCE_CLASS])) {
+            $result->set(AclProtectedQueryFactory::SKIP_ACL_FOR_ROOT_ENTITY, true);
+        }
+    }
+}
