535dddb466a726fca82c33f7b277b0fd029b9beb
Ivan Shirko <Ivan.Shyrko@effective-soft.com>
2017-01-19 17:43:56 +0300

    BAP-12213: Installation performance problem after merge new functional of Translations to master - fix functional test

--- src/Oro/Bundle/TranslationBundle/Tests/Functional/Command/OroTranslationLoadCommandTest.php
+++ src/Oro/Bundle/TranslationBundle/Tests/Functional/Command/OroTranslationLoadCommandTest.php
@@ -22,9 +22,8 @@ class OroTranslationLoadCommandTest extends WebTestCase
 
         $this->assertNotEmpty($result);
         $this->assertContains('Loading translations', $result);
+        $this->assertContains('loading [messages]', $result);
         $this->assertContains('loading [jsmessages]', $result);
-        $this->assertContains('loading [entities]', $result);
-        $this->assertContains('loading [workflows]', $result);
         $this->assertContains('All messages successfully loaded.', $result);
         $this->assertNotContains('Rebuilding cache', $result);
     }
