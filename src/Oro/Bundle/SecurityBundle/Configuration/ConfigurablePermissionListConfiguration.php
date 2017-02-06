637427d11fbaed1e0b3b06ad7a43093f814a1b2e
Vladimir Tsykun <vtsykun@oroinc.com>
2017-02-06 14:26:13 +0300

    BB-7526: Load configuration configurable_permissions (#7279)
    * BB-7526: Load configuration configurable_permissions
    - added abstract permissions configuration provider
    - added class ConfigurablePermissionListConfiguration
    - added configuration loader for tree  node oro_configurable_permissions
    - added & updated unit tests
    
    * BB-7526: Load configuration configurable_permissions
     - CR fixes

---
+++ src/Oro/Bundle/SecurityBundle/Configuration/ConfigurablePermissionListConfiguration.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Oro\Bundle\SecurityBundle\Configuration;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\Processor;
+
+class ConfigurablePermissionListConfiguration implements PermissionConfigurationInterface
+{
+    const ROOT_NODE_NAME = 'oro_configurable_permissions';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function processConfiguration(array $configs)
+    {
+        $processor = new Processor();
+
+        return $processor->processConfiguration($this, $configs);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $builder = new TreeBuilder();
+        $rootNode = $builder->root(static::ROOT_NODE_NAME);
+
+        $rootNode->useAttributeAsKey('name')
+            ->isRequired()
+            ->prototype('array')
+                ->children()
+                    ->booleanNode('default')
+                        ->defaultFalse()
+                    ->end()
+                    ->arrayNode('entities')
+                        ->useAttributeAsKey('name')
+                        ->prototype('variable')->end()
+                    ->end()
+                    ->arrayNode('capability')
+                        ->prototype('variable')->end()
+                    ->end()
+                ->end()
+            ->end();
+
+        return $builder;
+    }
+}
---
+++ src/Oro/Bundle/SecurityBundle/Tests/Unit/Configuration/ConfigurablePermissionListConfigurationTest.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Oro\Bundle\SecurityBundle\Tests\Unit\Configuration;
+
+use Oro\Bundle\SecurityBundle\Configuration\ConfigurablePermissionListConfiguration;
+
+class ConfigurablePermissionListConfigurationTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var ConfigurablePermissionListConfiguration */
+    protected $configuration;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        $this->configuration = new ConfigurablePermissionListConfiguration();
+    }
+
+    /**
+     * @dataProvider configurationProvider
+     *
+     * @param array $config
+     * @param array $expected
+     */
+    public function testProcessConfiguration(array $config, array $expected)
+    {
+        $config = [
+            ConfigurablePermissionListConfiguration::ROOT_NODE_NAME => $config
+        ];
+
+        $this->assertEquals($expected, $this->configuration->processConfiguration($config));
+    }
+
+    /**
+     * @return \Generator
+     */
+    public function configurationProvider()
+    {
+        yield 'configuration permissions list 1' => [
+            'config' => [
+                'commerce' => null
+            ],
+            'expected' => [
+                'commerce' => [
+                    'default' => false,
+                    'entities' => [],
+                    'capability' => [],
+                ]
+            ]
+        ];
+
+        yield 'configuration permissions list 2' => [
+            'config' => [
+                'commerce' => [
+                    'entities' => [
+                        'Entity1' => ['created' => false]
+                    ]
+                ]
+            ],
+            'expected' => [
+                'commerce' => [
+                    'default' => false,
+                    'entities' => [
+                        'Entity1' => ['created' => false]
+                    ],
+                    'capability' => [],
+                ]
+            ]
+        ];
+
+        yield 'configuration permissions list 3' => [
+            'config' => [
+                'commerce' => [
+                    'default' => true,
+                    'capability' => [
+                        'test1' => false,
+                        'test2' => false
+                    ]
+                ]
+            ],
+            'expected' => [
+                'commerce' => [
+                    'default' => true,
+                    'entities' => [],
+                    'capability' => [
+                        'test1' => false,
+                        'test2' => false
+                    ]
+                ]
+            ]
+        ];
+    }
+}
