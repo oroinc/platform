3c88421081cde01c83d0fc27705c4670784eabbe
Ivan Shirko <Ivan.Shirko@effective-soft.com>
2017-02-07 13:55:09 +0300

    BB-7528: Create Provider for process \ retrive configurable_permissions (#7292)
    * BB-7538: Create model ConfigurablePermission
    - add model
    - add unit test
    
    * BB-7538: Create model ConfigurablePermission
    - update phpdoc
    
    * BB-7526: Load configuration configurable_permissions
    - added abstract permissions configuration provider
    - added class ConfigurablePermissionListConfiguration
    - added configuration loader for tree  node oro_configurable_permissions
    - added & updated unit tests
    
    * BB-7538: Create model ConfigurablePermission
    - update model and unit tests
    
    * BB-7538: Create model ConfigurablePermission
    - CS fix
    
    * BB-7528: Create Provider for process \ retrive configurable_permissions
    - add provider
    
    * BB-7528: Create Provider for process \ retrive configurable_permissions
    - add unit tests
    
    * BB-7528: Create Provider for process \ retrive configurable_permissions
    - cs fixes
    
    * BB-7528: Create Provider for process \ retrive configurable_permissions
    - always return model
    
    * BB-7528: Create Provider for process \ retrive configurable_permissions
    - updates after merge
    
    * BB-7528: Create Provider for process \ retrive configurable_permissions
    - remove not needed var
    
    * BB-7528: Create Provider for process \ retrive configurable_permissions
    - cr fixes

---
+++ src/Oro/Bundle/SecurityBundle/Acl/Permission/ConfigurablePermissionProvider.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Oro\Bundle\SecurityBundle\Acl\Permission;
+
+use Doctrine\Common\Cache\CacheProvider;
+
+use Oro\Bundle\SecurityBundle\Configuration\ConfigurablePermissionConfigurationProvider;
+use Oro\Bundle\SecurityBundle\Model\ConfigurablePermission;
+
+class ConfigurablePermissionProvider
+{
+    const CACHE_ID = 'configurable_permissions';
+
+    /** @var ConfigurablePermissionConfigurationProvider */
+    private $configurationProvider;
+
+    /** @var CacheProvider */
+    private $cache;
+
+    /**
+     * @param ConfigurablePermissionConfigurationProvider $configurationProvider
+     * @param CacheProvider $cache
+     */
+    public function __construct(
+        ConfigurablePermissionConfigurationProvider $configurationProvider,
+        CacheProvider $cache
+    ) {
+        $this->configurationProvider = $configurationProvider;
+        $this->cache = $cache;
+    }
+
+    /**
+     * @param string $name name of Configurable Permission
+     * @return ConfigurablePermission
+     */
+    public function get($name)
+    {
+        $data = $this->getConfigurablePermissionsData();
+        if (!isset($data[$name]) || !is_array($data[$name])) {
+            return new ConfigurablePermission($name);
+        }
+
+        return new ConfigurablePermission(
+            $name,
+            array_key_exists('default', $data[$name]) ? $data[$name]['default'] : false,
+            array_key_exists('entities', $data[$name]) ? $data[$name]['entities'] : [],
+            array_key_exists('capabilities', $data[$name]) ? $data[$name]['capabilities'] : []
+        );
+    }
+
+    public function buildCache()
+    {
+        $this->cache->save(self::CACHE_ID, $this->configurationProvider->getConfiguration());
+    }
+
+    /**
+     * @return array
+     */
+    private function getConfigurablePermissionsData()
+    {
+        if (!$this->cache->contains(self::CACHE_ID)) {
+            $this->buildCache();
+        }
+
+        return $this->cache->fetch(self::CACHE_ID);
+    }
+}
