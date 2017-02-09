9897ce96dcb8d7687ad623f80a38c112b235f6f4
Valentyna Shabala <v.shabala@svitla.com>
2017-02-09 15:11:40 +0200

    BB-7607: Create ShippingRulesFilteredByMethodLinkGenerator in ShippingBundle which will generate link to filtered datagrid with "methodConfigs" as get parameter  - DatagridAwareRouteHelperInterface added  - FilteredDatagridRouteHelper

---
+++ src/Oro/Bundle/DataGridBundle/Tools/DatagridAwareRouteHelperInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Oro\Bundle\DataGridBundle\Tools;
+
+interface DatagridAwareRouteHelperInterface
+{
+    /**
+     * Generates URL or URI for the Datagrid filtered by parameters
+     *
+     * @param array  $params
+     * @param int $referenceType
+     *
+     * @return string
+     */
+    public function generate(array $params, $referenceType);
+}
