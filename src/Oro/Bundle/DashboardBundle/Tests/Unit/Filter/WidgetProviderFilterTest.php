0eb8e4f37b02f87116f4830038362fa61f26356a
Julian Atanasov <atanasov@vmandco.com>
2017-02-15 13:09:28 +0200

    OEE-934: Filter data by territory in dashboard widgets
    - adjust tests

--- src/Oro/Bundle/DashboardBundle/Tests/Unit/Filter/WidgetProviderFilterTest.php
+++ src/Oro/Bundle/DashboardBundle/Tests/Unit/Filter/WidgetProviderFilterTest.php
@@ -4,20 +4,17 @@ namespace Oro\Bundle\DashboardBundle\Tests\Unit\Filter;
 
 use Doctrine\ORM\QueryBuilder;
 
-use Oro\Bundle\UserBundle\Dashboard\OwnerHelper;
 use Oro\Bundle\SecurityBundle\ORM\Walker\AclHelper;
 use Oro\Bundle\DashboardBundle\Model\WidgetOptionBag;
-use Oro\Bundle\DashboardBundle\Filter\WidgetProviderFilter;
+use Oro\Bundle\DashboardBundle\Filter\WidgetProviderFilterManager;
+use Oro\Bundle\DashboardBundle\Tests\Unit\Fixtures\Entity\TestFilter;
 
 class WidgetProviderFilterTest extends \PHPUnit_Framework_TestCase
 {
     /** @var AclHelper|\PHPUnit_Framework_MockObject_MockObject */
     protected $aclHelper;
 
-    /** @var OwnerHelper|\PHPUnit_Framework_MockObject_MockObject */
-    protected $ownerHelper;
-
-    /** @var WidgetProviderFilter */
+    /** @var WidgetProviderFilterManager */
     protected $widgetProviderFilter;
 
     public function setUp()
@@ -25,27 +22,20 @@ class WidgetProviderFilterTest extends \PHPUnit_Framework_TestCase
         $this->aclHelper = $this->getMockBuilder('Oro\Bundle\SecurityBundle\ORM\Walker\AclHelper')
             ->disableOriginalConstructor()
             ->getMock();
-        $this->ownerHelper = $this->getMockBuilder('Oro\Bundle\UserBundle\Dashboard\OwnerHelper')
-            ->disableOriginalConstructor()
-            ->getMock();
 
-        $this->widgetProviderFilter = new WidgetProviderFilter($this->aclHelper, $this->ownerHelper);
+        $this->widgetProviderFilter = new WidgetProviderFilterManager($this->aclHelper);
+        $this->widgetProviderFilter->addFilter(new TestFilter());
     }
 
     public function testFilter()
     {
-        $this->ownerHelper->expects($this->once())
-            ->method('getOwnerIds')
-            ->willReturn([]);
         $this->aclHelper->expects($this->once())
             ->method('apply');
-
         $qb = $this->getMockBuilder(QueryBuilder::class)
             ->disableOriginalConstructor()
             ->getMock();
-        $qb->expects($this->any())
-            ->method('getRootAliases')
-            ->willReturn(['o']);
+        $qb->expects($this->once())
+            ->method('addSelect');
 
         $this->widgetProviderFilter->filter($qb, new WidgetOptionBag());
     }
