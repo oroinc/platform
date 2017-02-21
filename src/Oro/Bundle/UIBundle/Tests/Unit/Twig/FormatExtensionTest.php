bdc09cc88c914bad22c90654995853fc3b92f258
Vova Soroka <vsoroka@magecore.com>
2017-02-21 19:55:46 +0200

    BAP-13613: Investigate performance impact of dependencies in twig extensions (#7710)
    - inject DIC in TWIG extensions
    - merge some TWIG extensions
    - inject DIC in some Doctrine listeners
    - inject DIC in some request listeners
    - optimize Layout listener
    - reduce the number of initialy loaded services

--- src/Oro/Bundle/UIBundle/Tests/Unit/Twig/FormatExtensionTest.php
+++ src/Oro/Bundle/UIBundle/Tests/Unit/Twig/FormatExtensionTest.php
@@ -2,30 +2,38 @@
 
 namespace Oro\Bundle\UIBundle\Tests\Unit\Twig;
 
+use Symfony\Component\Translation\TranslatorInterface;
+
+use Oro\Bundle\UIBundle\Formatter\FormatterManager;
 use Oro\Bundle\UIBundle\Twig\FormatExtension;
-use Oro\Component\TestUtils\Mocks\ServiceLink;
+use Oro\Component\Testing\Unit\TwigExtensionTestCaseTrait;
 
 class FormatExtensionTest extends \PHPUnit_Framework_TestCase
 {
+    use TwigExtensionTestCaseTrait;
+
+    /** @var \PHPUnit_Framework_MockObject_MockObject */
+    protected $translator;
+
+    /** @var \PHPUnit_Framework_MockObject_MockObject */
+    protected $formatterManager;
+
     /** @var FormatExtension */
     protected $extension;
 
-    /** @var \PHPUnit_Framework_MockObject_MockObject */
-    protected $manager;
-
     protected function setUp()
     {
-        $this->manager = $this->getMockBuilder('Oro\Bundle\UIBundle\Formatter\FormatterManager')
+        $this->translator = $this->createMock(TranslatorInterface::class);
+        $this->formatterManager = $this->getMockBuilder(FormatterManager::class)
+            ->disableOriginalConstructor()
             ->getMock();
-        $this->extension = new FormatExtension(new ServiceLink($this->manager));
-    }
 
-    public function testGetFilters()
-    {
-        $this->assertEquals(
-            [new \Twig_SimpleFilter('oro_format', [$this->extension, 'format'], ['is_safe' => ['html']])],
-            $this->extension->getFilters()
-        );
+        $container = self::getContainerBuilder()
+            ->add('translator', $this->translator)
+            ->add('oro_ui.formatter', $this->formatterManager)
+            ->getContainer($this);
+
+        $this->extension = new FormatExtension($container);
     }
 
     public function testGetName()
@@ -38,12 +46,129 @@ class FormatExtensionTest extends \PHPUnit_Framework_TestCase
         $parameter = 'test';
         $formatterName = 'test_format';
         $formatterArguments = [];
+        $expected = 'result';
 
-        $this->manager
-            ->expects($this->once())
+        $this->formatterManager->expects($this->once())
             ->method('format')
-            ->with($parameter, $formatterName, $formatterArguments);
+            ->with($parameter, $formatterName, $formatterArguments)
+            ->willReturn($expected);
 
-        $this->extension->format($parameter, $formatterName, $formatterArguments);
+        self::assertEquals(
+            $expected,
+            self::callTwigFilter(
+                $this->extension,
+                'oro_format',
+                [$parameter, $formatterName, $formatterArguments]
+            )
+        );
+    }
+
+    /**
+     * @dataProvider formatFilenameProvider
+     */
+    public function testFormatFilename($filename, $result)
+    {
+        $this->assertEquals(
+            $result,
+            self::callTwigFunction($this->extension, 'oro_format_filename', [$filename])
+        );
+    }
+
+    public function formatFilenameProvider()
+    {
+        return [
+            [
+                'filename' => '',
+                'result' => '',
+            ],
+            [
+                'filename' => 'somename.jpg',
+                'result' => 'somename.jpg',
+            ],
+            [
+                'filename' => 'somename_very_long_file_name.jpg',
+                'result' => 'somenam..ame.jpg',
+            ],
+            [
+                'filename' => 'somename123.jpg',
+                'result' => 'somename123.jpg',
+            ],
+            [
+                'filename' => 'somename1234.jpg',
+                'result' => 'somenam..234.jpg',
+            ],
+            [
+                'filename' => 'тратата.jpg',
+                'result' => 'тратата.jpg',
+            ],
+            [
+                'filename' => 'тратататратататратата.jpg',
+                'result' => 'тратата..ата.jpg',
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider ageDataProvider
+     */
+    public function testGetAge($date, $options, $age)
+    {
+        $this->assertEquals(
+            $age,
+            self::callTwigFilter($this->extension, 'age', [$date, $options])
+        );
+    }
+
+    public function testGetAgeAsStringInvertDiff()
+    {
+        $date = new \DateTime('+1 year', new \DateTimeZone('UTC'));
+
+        $this->assertEquals(
+            '',
+            self::callTwigFilter($this->extension, 'age_string', [$date, []])
+        );
+        $this->assertEquals(
+            'N/A',
+            self::callTwigFilter($this->extension, 'age_string', [$date, ['default' => 'N/A']])
+        );
+    }
+
+    public function testGetAgeAsString()
+    {
+        $date = new \DateTime('-1 year -1 month', new \DateTimeZone('UTC'));
+
+        $this->translator->expects($this->once())
+            ->method('transChoice')
+            ->with('oro.age', 1, ['%count%' => 1])
+            ->will($this->returnValue('age 1'));
+
+        $this->assertEquals(
+            'age 1',
+            self::callTwigFilter($this->extension, 'age_string', [$date, []])
+        );
+    }
+
+    public function ageDataProvider()
+    {
+        $oneYearAgo = new \DateTime('-1 year', new \DateTimeZone('UTC'));
+        $oneMonthAgo = new \DateTime('-1 month', new \DateTimeZone('UTC'));
+        $oneYearTwoMonthAgo = new \DateTime('-1 year -2 months', new \DateTimeZone('UTC'));
+        $tenYearsAgo = new \DateTime('-10 years', new \DateTimeZone('UTC'));
+        $inFuture = new \DateTime('+1 year', new \DateTimeZone('UTC'));
+
+        return [
+            [$oneYearAgo->format('Y-m-d'), [], 1],
+            [$oneYearAgo->format('m/d/Y'), ['format' => 'm/d/Y'], 1],
+            [$tenYearsAgo->format('m/d/Y'), ['format' => 'm/d/Y', 'timezone' => 'UTC'], 10],
+            [$oneMonthAgo, [], 0],
+            [$oneYearAgo, [], 1],
+            [$oneMonthAgo, [], 0],
+            [$oneYearTwoMonthAgo, [], 1],
+            [$tenYearsAgo, [], 10],
+            [$inFuture, [], null],
+            [$inFuture, ['default' => 'N/A'], null],
+            [null, [], null],
+            ['', [], null],
+        ];
     }
 }
