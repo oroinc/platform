e7e0e9e597776eafcd11b271e1eff9bb985b2444
serdyuk <serdyuk.ae@gmail.com>
2017-02-22 11:41:59 +0200

    BAP-13716: interrupt consumption on schema update - added MQ extension InterruptConsumtion - added listener for schema update event

---
+++ src/Oro/Bundle/MessageQueueBundle/Consumption/InterruptConsumptionExtensionTrait.php
@@ -0,0 +1,34 @@
+<?php
+namespace Oro\Bundle\MessageQueueBundle\Consumption;
+
+use Oro\Component\MessageQueue\Consumption\Exception\LogicException;
+
+trait InterruptConsumptionExtensionTrait
+{
+    /**
+     * @var string
+     */
+    protected $filePath;
+
+    /**
+     * Create file if not exists
+     *
+     * @param string $filePath
+     *
+     * @throws LogicException
+     */
+    protected function touch($filePath)
+    {
+        if (! file_exists($filePath)) {
+            $directory = dirname($filePath);
+
+            if (! @mkdir($directory, 0777, true) && ! is_dir($directory)) {
+                throw new LogicException(
+                    sprintf('[InterruptConsumptionExtension] Cannot create directory %s', $directory)
+                );
+            }
+
+            touch($filePath);
+        }
+    }
+}
