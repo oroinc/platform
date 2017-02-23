d005bd6a6ab9399310343f56e9b876e7ffe486d5
Eugene Sodin <esodin@ukr.net>
2017-02-23 16:58:28 +0200

    OEE-1416: The OroCRMProDemoDataBundle fixtures loading cause consumer long work
      - use order

--- src/Oro/Bundle/MigrationBundle/Command/LoadDataFixturesCommand.php
+++ src/Oro/Bundle/MigrationBundle/Command/LoadDataFixturesCommand.php
@@ -3,19 +3,18 @@
 namespace Oro\Bundle\MigrationBundle\Command;
 
 use Doctrine\Common\DataFixtures\Executor\ORMExecutor;
-use Oro\Bundle\EmailBundle\Async\Topics as EmailTopics;
-use Oro\Bundle\MigrationBundle\Migration\Loader\DataFixturesLoader;
-use Oro\Bundle\PlatformBundle\Manager\OptionalListenerManager;
-use Oro\Bundle\SearchBundle\Engine\IndexerInterface;
-
-use Oro\Component\MessageQueue\Client\MessageProducerInterface;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-
 use Symfony\Component\HttpKernel\Bundle\BundleInterface;
+
+use Oro\Bundle\EmailBundle\Async\Topics as EmailTopics;
+use Oro\Bundle\MigrationBundle\Migration\Loader\DataFixturesLoader;
+use Oro\Bundle\PlatformBundle\Manager\OptionalListenerManager;
+use Oro\Bundle\SearchBundle\Engine\IndexerInterface;
+use Oro\Component\MessageQueue\Client\MessageProducerInterface;
 
 class LoadDataFixturesCommand extends ContainerAwareCommand
 {
