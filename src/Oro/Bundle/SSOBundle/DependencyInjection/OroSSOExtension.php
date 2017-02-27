e59e9c42c05c325a6cea6a8d795286d628d86652
Sergiu Parlea <sparlea@pitechnologies.ro>
2017-02-27 12:42:23 +0200

    BAP-13961: Backend Prefix is not added to Login With Google - refactor code

--- src/Oro/Bundle/SSOBundle/DependencyInjection/OroSSOExtension.php
+++ src/Oro/Bundle/SSOBundle/DependencyInjection/OroSSOExtension.php
@@ -5,34 +5,11 @@ namespace Oro\Bundle\SSOBundle\DependencyInjection;
 use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-class OroSSOExtension extends Extension implements PrependExtensionInterface
+class OroSSOExtension extends Extension
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function prepend(ContainerBuilder $container)
-    {
-        if (!$container->hasParameter('web_backend_prefix') || empty($container->getParameter('web_backend_prefix'))) {
-            return;
-        }
-
-        foreach ($container->getExtensionConfig('security') as $config) {
-            if (isset($config['firewalls']['main']['oauth']['resource_owners'])) {
-                $oauthResourceOwners = $config['firewalls']['main']['oauth']['resource_owners'];
-                foreach ($oauthResourceOwners as $name => $path) {
-                    $prefix = $container->getParameter('web_backend_prefix');
-                    $oauthResourceOwners[$name] = $prefix . $path;
-                }
-                $config['firewalls']['main']['oauth']['resource_owners'] = $oauthResourceOwners;
-                $container->setExtensionConfig('security', [$config]);
-            }
-        }
-    }
-
     /**
      * Load
      *
