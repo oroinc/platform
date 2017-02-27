4f159abe63b7849b9d45e53d46715e0f6ca60bd2
Sergiu Parlea <sparlea@pitechnologies.ro>
2017-02-27 15:17:06 +0200

    BAP-13961: Backend Prefix is not added to Login With Google - refactor code

--- src/Oro/Bundle/SSOBundle/DependencyInjection/OroSSOExtension.php
+++ src/Oro/Bundle/SSOBundle/DependencyInjection/OroSSOExtension.php
@@ -2,14 +2,44 @@
 
 namespace Oro\Bundle\SSOBundle\DependencyInjection;
 
+use Oro\Component\DependencyInjection\ExtendedContainerBuilder;
 use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-class OroSSOExtension extends Extension
+class OroSSOExtension extends Extension implements PrependExtensionInterface
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function prepend(ContainerBuilder $container)
+    {
+        if (!$container->hasParameter('web_backend_prefix') || empty($container->getParameter('web_backend_prefix'))) {
+            return;
+        }
+
+        if (!$container instanceof ExtendedContainerBuilder) {
+            return;
+        }
+
+        $formattedConfigs = [];
+        foreach ($container->getExtensionConfig('security') as $config) {
+            if (isset($config['firewalls']['main']['oauth']['resource_owners'])) {
+                $oauthResourceOwners = $config['firewalls']['main']['oauth']['resource_owners'];
+                foreach ($oauthResourceOwners as $name => $path) {
+                    $prefix = $container->getParameter('web_backend_prefix');
+                    $oauthResourceOwners[$name] = $prefix . $path;
+                }
+                $config['firewalls']['main']['oauth']['resource_owners'] = $oauthResourceOwners;
+            }
+            $formattedConfigs[] = $config;
+        }
+        $container->setExtensionConfig('security', $formattedConfigs);
+    }
+
     /**
      * Load
      *
