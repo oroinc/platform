a6fcc9f7458135a28ae0184afc5da97fb55aaf10
Daniel Nahrebecki <daniel.nahrebecki@gmail.com>
2017-03-01 11:37:17 +0100

    BB-7935: Fix travis error
    -  use generators in UoW

---
+++ src/Oro/Component/DoctrineUtils/ORM/ChangedEntityGeneratorTrait.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Oro\Component\DoctrineUtils\ORM;
+
+use Doctrine\ORM\UnitOfWork;
+
+trait ChangedEntityGeneratorTrait
+{
+    /**
+     * @param UnitOfWork $uow
+     *
+     * @return \Generator
+     */
+    protected function getChangedEntities(UnitOfWork $uow)
+    {
+        foreach ($uow->getScheduledEntityInsertions() as $entity) {
+            yield $entity;
+        }
+
+        foreach ($uow->getScheduledEntityUpdates() as $entity) {
+            yield $entity;
+        }
+
+        foreach ($uow->getScheduledEntityDeletions() as $entity) {
+            yield $entity;
+        }
+    }
+
+    /**
+     * @param UnitOfWork $uow
+     *
+     * @return \Generator
+     */
+    protected function getUpdatedEntities(UnitOfWork $uow)
+    {
+        foreach ($uow->getScheduledEntityUpdates() as $entity) {
+            yield $entity;
+        }
+    }
+}
