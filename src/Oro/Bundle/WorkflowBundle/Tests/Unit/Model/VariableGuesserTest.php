2991b83ee224d3293b68a09a912ec1efa98397bc
Balazs Csaba <csaba.balazs@evozon.com>
2017-03-03 11:54:57 +0200

    BAP-14058: Implement second review feedback Fixes: - [20]: Update VariableGuesserTest with all expects of $variable should be in setUp with any and test for empty values of getValue() and getLabel()

--- src/Oro/Bundle/WorkflowBundle/Tests/Unit/Model/VariableGuesserTest.php
+++ src/Oro/Bundle/WorkflowBundle/Tests/Unit/Model/VariableGuesserTest.php
@@ -2,44 +2,54 @@
 
 namespace Oro\Bundle\WorkflowBundle\Tests\Unit\Model;
 
-use Oro\Bundle\WorkflowBundle\Model\Variable;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-
-use Oro\Bundle\WorkflowBundle\Model\VariableGuesser;
 use Symfony\Component\Form\Guess\TypeGuess;
+
+use Oro\Bundle\WorkflowBundle\Model\Variable;
+use Oro\Bundle\WorkflowBundle\Model\VariableGuesser;
 
 class VariableGuesserTest extends \PHPUnit_Framework_TestCase
 {
-    /** @var  VariableGuesser|\PHPUnit_Framework_MockObject_MockObject */
+    /** @var VariableGuesser|\PHPUnit_Framework_MockObject_MockObject */
     protected $guesser;
 
+    /** @var Variable|\PHPUnit_Framework_MockObject_MockObject */
+    protected $variable;
+
+    /**
+     * Test setup.
+     */
     protected function setUp()
     {
         $this->guesser = new VariableGuesser();
+        $this->guesser->addFormTypeMapping('string', 'Symfony\Component\Form\Extension\Core\Type\TextType');
+
+        $this->variable = $this->createMock(Variable::class);
     }
 
+    /**
+     * Test variable form guessing.
+     */
     public function testGuessVariableForm()
     {
         $this->guesser->addFormTypeMapping('testType', 'formTestType', ['formOption' => 'optionValue']);
 
-        $variable = $this->createMock(Variable::class);
-        $variable->expects($this->once())
+        $this->variable->expects($this->once())
             ->method('getType')
             ->will($this->returnValue('testType'));
 
-        $variable->expects($this->once())
+        $this->variable->expects($this->once())
             ->method('getFormOptions')
             ->will($this->returnValue([]));
 
-        $variable->expects($this->exactly(2))
+        $this->variable->expects($this->exactly(2))
             ->method('getLabel')
             ->will($this->returnValue('testLabel'));
 
-        $variable->expects($this->exactly(2))
+        $this->variable->expects($this->exactly(2))
             ->method('getValue')
             ->will($this->returnValue('testValue'));
 
-        $typeGuess = $this->guesser->guessVariableForm($variable);
+        $typeGuess = $this->guesser->guessVariableForm($this->variable);
 
         $this->assertInstanceOf(TypeGuess::class, $typeGuess);
         $this->assertEquals($typeGuess->getType(), 'formTestType');
@@ -48,15 +58,64 @@ class VariableGuesserTest extends \PHPUnit_Framework_TestCase
         $this->assertContains('optionValue', $typeGuess->getOptions());
     }
 
+    /**
+     * Test guessing with missing type
+     */
     public function testGuessWithMissingType()
     {
-        $variable = $this->createMock(Variable::class);
-        $variable->expects($this->once())
+        $this->variable->expects($this->once())
             ->method('getType')
             ->will($this->returnValue('testType'));
 
-        $typeGuess = $this->guesser->guessVariableForm($variable);
+        $typeGuess = $this->guesser->guessVariableForm($this->variable);
 
         $this->assertNull($typeGuess);
+    }
+
+    /**
+     * Test guessing without options
+     *
+     * @dataProvider optionsDataProvider
+     * @param string $type
+     * @param string $property
+     * @param mixed $value
+     */
+    public function testGuessWithoutOptions($type, $property, $value)
+    {
+        $this->variable->expects($this->once())
+            ->method('getType')
+            ->willReturn($type);
+
+        $getter = 'get' . ucfirst($property);
+        $this->variable->expects($this->any())
+            ->method($getter)
+            ->willReturn($value);
+
+        $this->variable->expects($this->once())
+            ->method('getFormOptions')
+            ->willReturn([]);
+
+        $typeGuess = $this->guesser->guessVariableForm($this->variable);
+        $options = $typeGuess->getOptions();
+
+        if (null === $value) {
+            $this->assertEmpty($options);
+        } else {
+            $formOption = ('label' !== $property) ? 'data' : $property;
+            $this->assertEquals($value, $options[$formOption]);
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public function optionsDataProvider()
+    {
+        return [
+            ['string', 'label', 'test_label'],
+            ['string', 'value', 'test_value'],
+            ['string', 'label', null],
+            ['string', 'value', null]
+        ];
     }
 }
