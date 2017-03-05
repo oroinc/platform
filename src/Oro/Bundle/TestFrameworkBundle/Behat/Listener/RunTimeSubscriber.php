e77013b388daf94ef350f31ec39f07f5ccea8343
Serhii Polishchuk <spolishchuk@oroinc.com>
2017-03-05 22:55:20 +0700

    BB-7555: Added wait for message queue before each step

--- src/Oro/Bundle/TestFrameworkBundle/Behat/Listener/RunTimeSubscriber.php
+++ src/Oro/Bundle/TestFrameworkBundle/Behat/Listener/RunTimeSubscriber.php
@@ -4,9 +4,7 @@ namespace Oro\Bundle\TestFrameworkBundle\Behat\Listener;
 
 use Behat\Behat\EventDispatcher\Event\AfterOutlineTested;
 use Behat\Behat\EventDispatcher\Event\AfterScenarioTested;
-use Behat\Behat\EventDispatcher\Event\BeforeStepTested;
 use Behat\Mink\Mink;
-use Oro\Bundle\TestFrameworkBundle\Behat\Driver\OroSelenium2Driver;
 use Oro\Bundle\TestFrameworkBundle\Behat\Isolation\MessageQueueIsolatorAwareInterface;
 use Oro\Bundle\TestFrameworkBundle\Behat\Isolation\MessageQueueIsolatorInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
@@ -44,21 +42,14 @@ class RunTimeSubscriber implements EventSubscriberInterface, MessageQueueIsolato
     public static function getSubscribedEvents()
     {
         return [
-//            BeforeStepTested::BEFORE  => ['beforeStep', 50],
             AfterScenarioTested::AFTER => ['afterScenario', 50],
             AfterOutlineTested::AFTER => ['afterOutline', 50],
         ];
     }
 
     /**
-     * @param BeforeStepTested $scope
+     * @param AfterScenarioTested $scope
      */
-    public function beforeStep(BeforeStepTested $scope)
-    {
-        $this->waitForAjaxBeforeStep($scope);
-        $this->messageQueueIsolator->waitWhileProcessingMessages();
-    }
-
     public function afterScenario(AfterScenarioTested $scope)
     {
         if ($scope->getTestResult()->isPassed()) {
@@ -75,6 +66,9 @@ class RunTimeSubscriber implements EventSubscriberInterface, MessageQueueIsolato
         file_put_contents($screenshot, $this->mink->getSession()->getScreenshot());
     }
 
+    /**
+     * @param AfterOutlineTested $scope
+     */
     public function afterOutline(AfterOutlineTested $scope)
     {
         if ($scope->getTestResult()->isPassed()) {
@@ -98,36 +92,5 @@ class RunTimeSubscriber implements EventSubscriberInterface, MessageQueueIsolato
     public function setMessageQueueIsolator(MessageQueueIsolatorInterface $messageQueueIsolator)
     {
         $this->messageQueueIsolator = $messageQueueIsolator;
-    }
-
-    /**
-     * @param BeforeStepTested $scope
-     */
-    protected function waitForAjaxBeforeStep(BeforeStepTested $scope)
-    {
-        if (false === $this->mink->isSessionStarted('first_session')) {
-            return;
-        }
-
-        $session = $this->mink->getSession('first_session');
-        /** @var OroSelenium2Driver $driver */
-        $driver = $session->getDriver();
-
-        $url = $session->getCurrentUrl();
-
-        if (1 === preg_match('/^[\S]*\/user\/login\/?$/i', $url)) {
-            $driver->waitPageToLoad();
-
-            return;
-        } elseif (0 === preg_match('/^https?:\/\//', $url)) {
-            return;
-        }
-
-        // Don't wait when we need assert the flash message, because it can disappear until ajax in process
-        if (preg_match('/^(?:|I )should see ".+"(?:| flash message| error message)$/', $scope->getStep()->getText())) {
-            return;
-        }
-
-        $driver->waitForAjax();
     }
 }
