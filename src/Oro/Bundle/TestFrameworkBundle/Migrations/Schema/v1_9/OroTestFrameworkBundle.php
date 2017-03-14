f78f9e597384e3ed796ec9687bb86dfb675ecd67
Mykhailo Sulyma <msulima@magecore.com>
2017-03-15 01:17:23 +0200

    BB-7804: Test and merge to master  - fixed notice in builds of functional tests

--- src/Oro/Bundle/TestFrameworkBundle/Migrations/Schema/v1_9/OroTestFrameworkBundle.php
+++ src/Oro/Bundle/TestFrameworkBundle/Migrations/Schema/v1_9/OroTestFrameworkBundle.php
@@ -4,6 +4,7 @@ namespace Oro\Bundle\TestFrameworkBundle\Migrations\Schema\v1_9;
 
 use Doctrine\DBAL\Schema\Schema;
 
+use Oro\Bundle\EntityConfigBundle\Migration\RemoveFieldQuery;
 use Oro\Bundle\MigrationBundle\Migration\Migration;
 use Oro\Bundle\MigrationBundle\Migration\QueryBag;
 
@@ -15,5 +16,6 @@ class OroTestFrameworkBundle implements Migration
     public function up(Schema $schema, QueryBag $queries)
     {
         $queries->addPostQuery(new UpdateRelationsExtendConfig());
+        $queries->addPostQuery(new RemoveFieldQuery('Oro\Bundle\TestFrameworkBundle\Entity\Item', 'isActive'));
     }
 }
