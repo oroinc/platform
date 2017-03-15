eeaeed8ebf5f2973079f9fd4e0b8724b1e55829f
Mykhailo Sulyma <msulima@magecore.com>
2017-03-15 02:54:29 +0200

    BB-7804: Test and merge to master

--- src/Oro/Bundle/TestFrameworkBundle/Migrations/Schema/v1_9/OroTestFrameworkBundle.php
+++ src/Oro/Bundle/TestFrameworkBundle/Migrations/Schema/v1_9/OroTestFrameworkBundle.php
@@ -4,7 +4,6 @@ namespace Oro\Bundle\TestFrameworkBundle\Migrations\Schema\v1_9;
 
 use Doctrine\DBAL\Schema\Schema;
 
-use Oro\Bundle\EntityConfigBundle\Migration\RemoveFieldQuery;
 use Oro\Bundle\MigrationBundle\Migration\Migration;
 use Oro\Bundle\MigrationBundle\Migration\QueryBag;
 
@@ -16,6 +15,5 @@ class OroTestFrameworkBundle implements Migration
     public function up(Schema $schema, QueryBag $queries)
     {
         $queries->addPostQuery(new UpdateRelationsExtendConfig());
-        $queries->addPostQuery(new RemoveFieldQuery('Oro\Bundle\TestFrameworkBundle\Entity\Item', 'isActive'));
     }
 }
