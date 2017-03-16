20448426405bf2d3ecbd48a595402ab028f17706
Mykhailo Sulyma <msulima@magecore.com>
2017-03-16 17:34:53 +0200

    BB-8343: Updated demo data for languages, localizations and websites

--- src/Oro/Bundle/TranslationBundle/Migrations/Data/Demo/ORM/LoadLanguageDemoData.php
+++ src/Oro/Bundle/TranslationBundle/Migrations/Data/Demo/ORM/LoadLanguageDemoData.php
@@ -13,24 +13,24 @@ class LoadLanguageDemoData extends AbstractFixture
 {
     use UserUtilityTrait;
 
-    const LANG_DE = 'language.de';
-    const LANG_FR = 'language.fr';
+    const LANG_DE_DE = 'language.de_DE';
+    const LANG_FR_FR = 'language.fr_FR';
     const LANG_EN_US = 'language.en_US';
     const LANG_EN_CA = 'language.en_CA';
     const LANG_EN_GB = 'language.en_GB';
     const LANG_EN_AU = 'language.en_AU';
-    const LANG_ES_MX = 'language.es_MX';
+    const LANG_ES_AR = 'language.es_AR';
     const LANG_FR_CA = 'language.fr_CA';
 
     /** @var array */
     protected static $languages = [
-        self::LANG_DE => 'de',
-        self::LANG_FR => 'fr',
+        self::LANG_DE_DE => 'de_DE',
+        self::LANG_FR_FR => 'fr_FR',
         self::LANG_EN_US => 'en_US',
         self::LANG_EN_CA => 'en_CA',
         self::LANG_EN_GB => 'en_GB',
         self::LANG_EN_AU => 'en_AU',
-        self::LANG_ES_MX => 'es_MX',
+        self::LANG_ES_AR => 'es_AR',
         self::LANG_FR_CA => 'fr_CA'
     ];
 
