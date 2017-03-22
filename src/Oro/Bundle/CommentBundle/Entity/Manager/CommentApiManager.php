oroinc/platform

commit 96d100f4663c1e9a1e5848f0d1a05144f27fa793

BAP-10016: Fix inconsistency of adding/uploading files to activities - refactoring

src/Oro/Bundle/CommentBundle/Entity/Manager/CommentApiManager.php
@@ -12,7 +12,7 @@ use Doctrine\ORM\QueryBuilder;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\PropertyAccess\PropertyAccess;
 
-use Oro\Bundle\AttachmentBundle\Manager\AttachmentManager;
+use Oro\Bundle\AttachmentBundle\Provider\AttachmentProvider;
 use Oro\Bundle\CommentBundle\Entity\Comment;
 use Oro\Bundle\CommentBundle\Entity\Repository\CommentRepository;
 use Oro\Bundle\EntityBundle\Exception\InvalidEntityException;
@@ -21,7 +21,6 @@ use Oro\Bundle\EntityExtendBundle\Tools\ExtendHelper;
 use Oro\Bundle\DataGridBundle\Extension\Pager\Orm\Pager;
 use Oro\Bundle\SecurityBundle\SecurityFacade;
 use Oro\Bundle\SoapBundle\Entity\Manager\ApiEntityManager;
-use Oro\Bundle\AttachmentBundle\Entity\File;
 use Oro\Bundle\SecurityBundle\ORM\Walker\AclHelper;
 use Oro\Bundle\UserBundle\Entity\User;
 use Oro\Bundle\EntityConfigBundle\Config\ConfigManager;
@@ -42,8 +41,8 @@ class CommentApiManager extends ApiEntityManager
     /** @var EntityNameResolver */
     protected $entityNameResolver;
 
-    /** @var AttachmentManager */
-    protected $attachmentManager;
+    /** @var AttachmentProvider */
+    protected $attachmentProvider;
 
     /** @var aclHelper */
     protected $aclHelper;
@@ -57,7 +56,7 @@ class CommentApiManager extends ApiEntityManager
      * @param EntityNameResolver       $entityNameResolver
      * @param Pager                    $pager
      * @param EventDispatcherInterface $eventDispatcher
-     * @param AttachmentManager        $attachmentManager
+     * @param AttachmentProvider       $attachmentProvider
      * @param AclHelper                $aclHelper
      * @param ConfigManager            $configManager
      */
@@ -67,7 +66,7 @@ class CommentApiManager extends ApiEntityManager
         EntityNameResolver $entityNameResolver,
         Pager $pager,
         EventDispatcherInterface $eventDispatcher,
-        AttachmentManager $attachmentManager,
+        AttachmentProvider $attachmentProvider,
         AclHelper $aclHelper,
         ConfigManager $configManager
     ) {
@@ -75,7 +74,7 @@ class CommentApiManager extends ApiEntityManager
         $this->securityFacade     = $securityFacade;
         $this->entityNameResolver = $entityNameResolver;
         $this->pager              = $pager;
-        $this->attachmentManager  = $attachmentManager;
+        $this->attachmentProvider = $attachmentProvider;
         $this->aclHelper          = $aclHelper;
         $this->configManager      = $configManager;
 
@@ -206,7 +205,7 @@ class CommentApiManager extends ApiEntityManager
             'editable'      => $this->securityFacade->isGranted('EDIT', $entity),
             'removable'     => $this->securityFacade->isGranted('DELETE', $entity),
         ];
-        $result = array_merge($result, $this->getAttachmentInfo($entity));
+        $result = array_merge($result, $this->attachmentProvider->getAttachmentInfo($entity));
         $result = array_merge($result, $this->getCommentAvatarImageUrl($entity->getOwner()));
 
         return $result;
@@ -342,59 +341,6 @@ class CommentApiManager extends ApiEntityManager
         }
 
         return true;
-    }
-
-    /**
-     * @param Comment $entity
-     * @param File    $attachment
-     *
-     * @return string
-     */
-    protected function getAttachmentURL($entity, $attachment)
-    {
-        return $this->attachmentManager->getFileUrl($entity, 'attachment', $attachment, 'download');
-    }
-
-    /**
-     * @param $entity
-     *
-     * @return File
-     */
-    protected function getAttachment($entity)
-    {
-        $accessor   = PropertyAccess::createPropertyAccessor();
-        $attachment = $accessor->getValue($entity, 'attachment');
-
-        return $attachment;
-    }
-
-    /**
-     * @param Comment $entity
-     *
-     * @return array
-     */
-    protected function getAttachmentInfo(Comment $entity)
-    {
-        $result     = [];
-        $attachment = $this->getAttachment($entity);
-        if ($attachment) {
-            $thumbnail = '';
-            if ($this->attachmentManager->isImageType($attachment->getMimeType())) {
-                $thumbnail = $this->attachmentManager->getResizedImageUrl(
-                    $attachment,
-                    AttachmentManager::THUMBNAIL_WIDTH,
-                    AttachmentManager::THUMBNAIL_HEIGHT
-                );
-            }
-            $result = [
-                'attachmentURL'       => $this->getAttachmentURL($entity, $attachment),
-                'attachmentSize'      => $this->attachmentManager->getFileSize($attachment->getFileSize()),
-                'attachmentFileName'  => $attachment->getOriginalFilename(),
-                'attachmentIcon'      => $this->attachmentManager->getAttachmentIconClass($attachment),
-                'attachmentThumbnail' => $thumbnail
-            ];
-        }
-        return $result;
     }
 
     /**

src/Oro/Bundle/CommentBundle/Resources/config/services.yml
@@ -43,7 +43,7 @@ services:
             - '@oro_entity.entity_name_resolver'
             - '@oro_datagrid.extension.pager.orm.pager'
             - '@event_dispatcher'
-            - '@oro_attachment.manager'
+            - '@oro_attachment.provider.attachment'
             - '@oro_security.acl_helper'
             - '@oro_entity_config.config_manager'
 

src/Oro/Bundle/AttachmentBundle/Resources/config/services.yml
@@ -145,6 +145,7 @@ services:
         arguments:
             - '@doctrine.orm.entity_manager'
             - '@oro_attachment.association_helper'
+            - '@oro_attachment.manager'
 
     oro_attachment.exclusion_provider:
         class: Oro\Bundle\AttachmentBundle\Provider\AttachmentExclusionProvider

src/Oro/Bundle/NoteBundle/Resources/config/services.yml
@@ -20,7 +20,7 @@ services:
             - '@oro_security.security_facade'
             - '@oro_security.acl_helper'
             - '@oro_entity.entity_name_resolver'
-            - '@oro_attachment.manager'
+            - '@oro_attachment.provider.attachment'
 
     oro_note.association_helper:
         class: Oro\Bundle\NoteBundle\Tools\NoteAssociationHelper

src/Oro/Bundle/AttachmentBundle/Manager/AttachmentManager.php
@@ -511,4 +511,16 @@ class AttachmentManager
     {
         return sprintf('%s.%s', uniqid(), $extension);
     }
+
+    /**
+     * @param $entity
+     * @param File $attachment
+     *
+     * @return string
+     */
+    public function getAttachmentURL($entity, $attachment)
+    {
+        return $this->getFileUrl($entity, 'attachment', $attachment, 'download', true);
+    }
+
 }

src/Oro/Bundle/AttachmentBundle/Provider/AttachmentProvider.php
@@ -6,6 +6,9 @@ use Doctrine\Common\Util\ClassUtils;
 use Doctrine\ORM\EntityManager;
 
 use Oro\Bundle\AttachmentBundle\Entity\Attachment;
+use Oro\Bundle\AttachmentBundle\Entity\File;
+use Symfony\Component\PropertyAccess\PropertyAccess;
+use Oro\Bundle\AttachmentBundle\Manager\AttachmentManager;
 use Oro\Bundle\AttachmentBundle\Tools\AttachmentAssociationHelper;
 use Oro\Bundle\EntityExtendBundle\Tools\ExtendHelper;
 
@@ -20,13 +23,16 @@ class AttachmentProvider
     /**
      * @param EntityManager               $entityManager
      * @param AttachmentAssociationHelper $attachmentAssociationHelper
+     * @param AttachmentManager           $attachmentManager
      */
     public function __construct(
         EntityManager $entityManager,
-        AttachmentAssociationHelper $attachmentAssociationHelper
+        AttachmentAssociationHelper $attachmentAssociationHelper,
+        AttachmentManager $attachmentManager
     ) {
         $this->em                          = $entityManager;
         $this->attachmentAssociationHelper = $attachmentAssociationHelper;
+        $this->attachmentManager           = $attachmentManager;
     }
 
     /**
@@ -50,5 +56,37 @@ class AttachmentProvider
         }
 
         return [];
+    }
+
+    /**
+     * @param $entity
+     *
+     * @return array
+     */
+    public function getAttachmentInfo($entity)
+    {
+        $result     = [];
+        $attachment = $this->getAttachmentByEntity($entity);
+        if ($attachment) {
+            $result = [
+                'attachmentURL'      => $this->attachmentManager->getAttachmentURL($entity, $attachment),
+                'attachmentSize'     => $this->attachmentManager->getFileSize($attachment->getFileSize()),
+                'attachmentFileName' => $attachment->getOriginalFilename(),
+            ];
+        }
+        return $result;
+    }
+
+    /**
+     * @param $entity
+     *
+     * @return File
+     */
+    protected function getAttachmentByEntity($entity)
+    {
+        $accessor   = PropertyAccess::createPropertyAccessor();
+        $attachment = $accessor->getValue($entity, 'attachment');
+
+        return $attachment;
     }
 }

src/Oro/Bundle/CommentBundle/Resources/public/css/less/comment.less
@@ -38,9 +38,6 @@
     .attachment-item {
       margin-top: 5px;
     }
-    span.filename {
-      width: 160px;
-    }
   }
   .attachment-item {
     .remove-attachment {

src/Oro/Bundle/NoteBundle/Controller/NoteController.php
@@ -71,8 +71,8 @@ class NoteController extends Controller
      */
     public function infoAction(Note $entity)
     {
-        $manager = $this->getNoteManager();
-        $attachment = $manager->getAttachmentInfo($entity);
+        $attachmentProvider = $this->get('oro_attachment.provider.attachment');
+        $attachment = $attachmentProvider->getAttachmentInfo($entity);
         return ['entity' => $entity, 'attachment' => $attachment];
     }
 

commit 491022842eef65f970720a0cbe224d998c408349

Fix fatal error "Call to a member function getResizedImageUrl() on null"
Bug introduced in: 96d100f4663c1e9a1e5848f0d1a05144f27fa793

src/Oro/Bundle/CommentBundle/Entity/Manager/CommentApiManager.php
@@ -13,6 +13,7 @@ use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\PropertyAccess\PropertyAccess;
 
 use Oro\Bundle\AttachmentBundle\Provider\AttachmentProvider;
+use Oro\Bundle\AttachmentBundle\Manager\AttachmentManager;
 use Oro\Bundle\CommentBundle\Entity\Comment;
 use Oro\Bundle\CommentBundle\Entity\Repository\CommentRepository;
 use Oro\Bundle\EntityBundle\Exception\InvalidEntityException;
@@ -44,6 +45,9 @@ class CommentApiManager extends ApiEntityManager
     /** @var AttachmentProvider */
     protected $attachmentProvider;
 
+    /** @var AttachmentManager */
+    protected $attachmentManager;
+
     /** @var aclHelper */
     protected $aclHelper;
 
@@ -57,6 +61,7 @@ class CommentApiManager extends ApiEntityManager
      * @param Pager                    $pager
      * @param EventDispatcherInterface $eventDispatcher
      * @param AttachmentProvider       $attachmentProvider
+     * @param AttachmentManager        $attachmentManager
      * @param AclHelper                $aclHelper
      * @param ConfigManager            $configManager
      */
@@ -67,6 +72,7 @@ class CommentApiManager extends ApiEntityManager
         Pager $pager,
         EventDispatcherInterface $eventDispatcher,
         AttachmentProvider $attachmentProvider,
+        AttachmentManager $attachmentManager,
         AclHelper $aclHelper,
         ConfigManager $configManager
     ) {
@@ -75,6 +81,7 @@ class CommentApiManager extends ApiEntityManager
         $this->entityNameResolver = $entityNameResolver;
         $this->pager              = $pager;
         $this->attachmentProvider = $attachmentProvider;
+        $this->attachmentManager  = $attachmentManager;
         $this->aclHelper          = $aclHelper;
         $this->configManager      = $configManager;
 

src/Oro/Bundle/CommentBundle/Resources/config/services.yml
@@ -44,6 +44,7 @@ services:
             - '@oro_datagrid.extension.pager.orm.pager'
             - '@event_dispatcher'
             - '@oro_attachment.provider.attachment'
+            - '@oro_attachment.manager'
             - '@oro_security.acl_helper'
             - '@oro_entity_config.config_manager'
 