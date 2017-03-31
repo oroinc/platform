df84cf5db5887a180ab96f1278d5d628c0a27bb0
Nikita Makarov <nmakarov@oroinc.com>
2017-03-31 12:31:00 +0300

    BAP-14398: Create controller actions with layout renderer for transition form - Updated JS for Transition Dialog - Updated Controllers

--- src/Oro/Bundle/WorkflowBundle/Resources/public/js/app/widget/transition-dialog-widget.js
+++ src/Oro/Bundle/WorkflowBundle/Resources/public/js/app/widget/transition-dialog-widget.js
@@ -3,8 +3,10 @@ define(function(require) {
 
     var TransitionDialogWidget;
     var DialogWidget = require('oro/dialog-widget');
+    var mediator = require('oroui/js/mediator');
     var TransitionEventHandlers = require('oroworkflow/js/transition-event-handlers');
     var $ = require('jquery');
+    var _ = require('underscore');
 
     TransitionDialogWidget = DialogWidget.extend({
         listen: {
@@ -20,6 +22,16 @@ define(function(require) {
             var element = $('<div>');
             this.hide();
             TransitionEventHandlers.getOnFailure(element)(jqxhr);
+        },
+        _onContentLoad: function(content) {
+            if (_.has(content, 'workflowItem')) {
+                var element = $('<div>');
+                this.hide();
+                mediator.execute('showLoading');
+                TransitionEventHandlers.getOnSuccess(element)(content);
+            }
+
+            return TransitionDialogWidget.__super__._onContentLoad.apply(this, arguments);
         }
     });
 
