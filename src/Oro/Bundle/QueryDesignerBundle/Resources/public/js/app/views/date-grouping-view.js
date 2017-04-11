19f10ab259c211f57d24e42caf2f4ab55737e23c
Viacheslav Chekanovskiy <vchekanovskyi@oroinc.com>
2017-04-11 14:47:36 +0300

    BAP-13995: Improve date grouping javascript (#9106)
     - add include option to fieldChoice widget
     - refactor DateGroupingComponent to DateGroupingView

---
+++ src/Oro/Bundle/QueryDesignerBundle/Resources/public/js/app/views/date-grouping-view.js
@@ -0,0 +1,36 @@
+define(function(require) {
+    'use strict';
+
+    var DateGroupingView;
+    var BaseView = require('oroui/js/app/views/base/view');
+
+    DateGroupingView = BaseView.extend({
+        autoRender: true,
+
+        switcherCheckbox: '[data-name="field__use-date-group-filter"]',
+
+        fieldSelect: '[data-name="field__field-name"]',
+
+        allowEmptyCheckbox: '[data-name="field__use-skip-empty-periods-filter"]',
+
+        events: {
+            'change [data-name="field__use-date-group-filter"]': 'render'
+        },
+
+        listen: {
+            'page:afterChange mediator [data-name="field__use-date-group-filter"]': 'render'
+        },
+
+        render: function() {
+            if (this.$(this.switcherCheckbox).is(':checked')) {
+                this.$(this.allowEmptyCheckbox).removeAttr('disabled');
+                this.$(this.fieldSelect).inputWidget('disable', false);
+            } else {
+                this.$(this.allowEmptyCheckbox).attr('disabled', true);
+                this.$(this.fieldSelect).inputWidget('disable', true);
+            }
+        }
+    });
+
+    return DateGroupingView;
+});
