664f1607bccf0acaa5b0ad1aa0aefc632e0d1357
Iaroslav Bratsilo <ibratsilo@oroinc.com>
2017-04-13 12:38:20 +0300

    BB-8850: Performance degradation and possible memory leak in StaticSegmentManager  - changed Inset from Select for StaticSegmtentManager

--- src/Oro/Bundle/SegmentBundle/Entity/Manager/StaticSegmentManager.php
+++ src/Oro/Bundle/SegmentBundle/Entity/Manager/StaticSegmentManager.php
@@ -3,6 +3,7 @@
 namespace Oro\Bundle\SegmentBundle\Entity\Manager;
 
 use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping\ClassMetadata;
 use Doctrine\DBAL\Driver\Statement;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Query;
@@ -65,29 +66,100 @@ class StaticSegmentManager
         }
 
         $this->em->getRepository('OroSegmentBundle:SegmentSnapshot')->removeBySegment($segment);
+        try {
+            $this->em->beginTransaction();
+            $date       = new \DateTime('now', new \DateTimeZone('UTC'));
+            $dateString = '\'' . $date->format('Y-m-d H:i:s') . '\'';
 
-        $qb = $this->dynamicSegmentQB->getQueryBuilder($segment);
-        $this->applyOrganizationLimit($segment, $qb);
-        $qb->setMaxResults($segment->getRecordsLimit());
-        $result = $qb->getQuery()->getArrayResult();
-        $identifier = $entityMetadata->getSingleIdentifierFieldName();
-        $identifierType = $entityMetadata->getTypeOfField($identifier);
-        $ids = array_column($result, $identifier);
-
-        foreach ($ids as $id) {
-            $segmentSnapshot = new SegmentSnapshot($segment);
-            if ($identifierType === 'integer') {
-                $segmentSnapshot->setIntegerEntityId($id);
-            } else {
-                $segmentSnapshot->setEntityId($id);
+            if ($this->em->getConnection()->getDriver()->getName() === DatabaseDriverInterface::DRIVER_POSTGRESQL) {
+                $dateString = sprintf('TIMESTAMP %s', $dateString);
             }
-            $this->em->persist($segmentSnapshot);
+
+            $insertString = sprintf(
+                ', %d, %s ',
+                $segment->getId(),
+                $dateString
+            );
+
+            $qb = $this->dynamicSegmentQB->getQueryBuilder($segment);
+
+            $this->applyOrganizationLimit($segment, $qb);
+            $originalQuery = $qb->getQuery();
+            $identifier = $entityMetadata->getSingleIdentifierFieldName();
+            $selectSql = $this->getSelectSql($qb, $segment, $identifier);
+            $selectSql = substr_replace($selectSql, $insertString, stripos($selectSql, 'from'), 0);
+
+            $fieldToSelect = 'entity_id';
+
+            if ($entityMetadata->getTypeOfField($identifier) === 'integer') {
+                $fieldToSelect = 'integer_entity_id';
+            }
+
+            $dbQuery = 'INSERT INTO oro_segment_snapshot (' . $fieldToSelect . ', segment_id, createdat) (%s)';
+            $dbQuery = sprintf($dbQuery, $selectSql);
+
+            $statement = $this->em->getConnection()->prepare($dbQuery);
+            $this->bindParameters($statement, $originalQuery->getParameters());
+            $statement->execute();
+
+            $this->em->commit();
+        } catch (\Exception $exception) {
+            $this->em->rollback();
+
+            throw $exception;
         }
 
         $segment = $this->em->merge($segment);
         $segment->setLastRun(new \DateTime('now', new \DateTimeZone('UTC')));
         $this->em->persist($segment);
         $this->em->flush();
+    }
+
+    /**
+     * Returns select sql if limit applied wraps it in JOIN
+     *
+     * @param QueryBuilder $queryBuilder
+     * @param Segment $segment
+     * @param $identifier
+     * @return string
+     */
+    private function getSelectSql(QueryBuilder $queryBuilder, Segment $segment, $identifier)
+    {
+        $tableAlias = current($queryBuilder->getDQLPart('from'))->getAlias();
+        
+        if (!$segment->getRecordsLimit()) {
+             $queryBuilder->resetDQLParts(['orderBy', 'select']);
+             $queryBuilder->select($tableAlias . '.' . $identifier);
+             $finalSelectSql = $queryBuilder->getQuery()->getSQL();
+        } else {
+            $queryBuilder->setMaxResults($segment->getRecordsLimit());
+            $originalSelectSql = $queryBuilder->getQuery()->getSQL();
+
+            $queryBuilder->resetDQLParts(['orderBy', 'select', 'where']);
+            $queryBuilder->setMaxResults(null);
+            $queryBuilder->select($tableAlias . '.' . $identifier);
+            $purifiedSelectSql = $queryBuilder->getQuery()->getSQL();
+            $originalIdentifierDoctrineAlias = $this->getDoctrineIdentifierAlias($identifier, $originalSelectSql);
+
+            $finalSelectSql = "$purifiedSelectSql  JOIN ($originalSelectSql) " .
+                "AS result_table ON result_table.$originalIdentifierDoctrineAlias=$identifier";
+        }
+
+        return $finalSelectSql;
+    }
+
+    /**
+     * Returns doctrine's auto generated identifier alias for column - something like this "id_0"
+     * @param string $identifier
+     * @param string $sql
+     * @return string
+     */
+    private function getDoctrineIdentifierAlias($identifier, $sql)
+    {
+        $regex = "/(?<=\b.$identifier AS )(?:[\w-]+)/is";
+        preg_match($regex, $sql, $matches);
+
+        return current($matches);
     }
 
     /**
