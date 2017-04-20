6cb72bb31bf0483d93ee9664defb963030aabca4
Andrew Fetisov <brotherrrr@gmail.com>
2017-04-20 15:46:06 +0300

    BAP-14555: Backend Prefix is not added to Login With Google
    - Modified SSO configuration for login url check;
    - unit test added.

---
+++ src/Oro/Bundle/SSOBundle/Tests/Unit/DependencyInjection/OroSSOExtensionTest.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Oro\Bundle\SSOBundle\Tests\Unit\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+use Oro\Bundle\SSOBundle\DependencyInjection\OroSSOExtension;
+use Oro\Bundle\TestFrameworkBundle\Test\DependencyInjection\ExtensionTestCase;
+use Oro\Component\DependencyInjection\ExtendedContainerBuilder;
+
+class OroSSOExtensionTest extends ExtensionTestCase
+{
+
+    public function testLoad()
+    {
+        $this->loadExtension(new OroSSOExtension());
+
+        $expectedDefinitions = [
+            'oro_sso.oauth_provider',
+            'oro_sso.event_listener.user_email_change_listener.class',
+            'oro_sso.token.factory.oauth',
+        ];
+        $this->assertDefinitionsLoaded($expectedDefinitions);
+    }
+
+    public function testAddPrefix()
+    {
+        $backendPrefix = '/admin';
+        $originalConfig = [
+            0 =>[
+                'firewalls' => [
+                    'main' => [
+                        'oauth' => [
+                            'resource_owners' => [
+                                'google' => '/login/check-google',
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ];
+
+        $expectedConfig = [
+            0 =>[
+                'firewalls' => [
+                    'main' => [
+                        'oauth' => [
+                            'resource_owners' => [
+                                'google' => $backendPrefix . '/login/check-google',
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ];
+
+        $containerBuilder = new ExtendedContainerBuilder();
+        $containerBuilder->setParameter('web_backend_prefix', $backendPrefix);
+        $containerBuilder->setExtensionConfig('security', $originalConfig);
+
+        $ssoExtension = new OroSSOExtension();
+        $prependPrefix = new \ReflectionMethod(
+            OroSSOExtension::class,
+            'prepend'
+        );
+        $prependPrefix->invoke($ssoExtension, $containerBuilder);
+
+        $this->assertEquals($expectedConfig, $containerBuilder->getExtensionConfig('security'));
+    }
+
+    public function testAddPrefixWithoutBackendPrefixParameter()
+    {
+        $originalConfig = [
+            0 =>[
+                'firewalls' => [
+                    'main' => [
+                        'oauth' => [
+                            'resource_owners' => [
+                                'google' => '/login/check-google',
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ];
+
+        $containerBuilder = new ExtendedContainerBuilder();
+        $containerBuilder->setExtensionConfig('security', $originalConfig);
+
+        $ssoExtension = new OroSSOExtension();
+        $prependPrefix = new \ReflectionMethod(
+            OroSSOExtension::class,
+            'prepend'
+        );
+        $prependPrefix->invoke($ssoExtension, $containerBuilder);
+
+        $this->assertEquals($originalConfig, $containerBuilder->getExtensionConfig('security'));
+    }
+
+    public function testAddPrefixWithWrongContainerInstance()
+    {
+        $containerBuilder = new ContainerBuilder();
+        $containerBuilder->setParameter('web_backend_prefix', '/admin');
+
+        $ssoExtension = new OroSSOExtension();
+        $prependPrefix = new \ReflectionMethod(
+            OroSSOExtension::class,
+            'prepend'
+        );
+        $prependPrefix->invoke($ssoExtension, $containerBuilder);
+
+        $this->assertEmpty($containerBuilder->getExtensionConfig('security'));
+    }
+}
