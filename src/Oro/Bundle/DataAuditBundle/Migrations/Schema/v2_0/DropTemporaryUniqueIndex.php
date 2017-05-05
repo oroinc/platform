fe158484433b94905476eedc91c381d061c3abd7
Vladimir Seniuk <senyuk.vladimir@gmail.com>
2017-05-05 14:30:45 +0300

    CRM-8171: Incorrect Data Audit class namespaces migration from 2.0.2 to 2.0.7
    - fixed error with unique index

---
+++ src/Oro/Bundle/DataAuditBundle/Migrations/Schema/v2_0/DropTemporaryUniqueIndex.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Oro\Bundle\DataAuditBundle\Migrations\Schema\v2_0;
+
+use Doctrine\DBAL\Schema\Schema;
+
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\OrderedMigrationInterface;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+
+class DropTemporaryUniqueIndex implements Migration, OrderedMigrationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        $table = $schema->getTable('oro_audit');
+        $table->dropIndex('idx_oro_audit_version');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getOrder()
+    {
+        return 0;
+    }
+}
