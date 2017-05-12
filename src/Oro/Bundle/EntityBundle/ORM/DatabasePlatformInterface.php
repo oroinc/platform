b8bf2d70343eb8ee83ccd79ad99005c614e95b20
Vladimir Seniuk <vladimirseniuk@users.noreply.github.com>
2017-05-12 17:05:08 +0300

    CRM-8187: Magento customer is not displayed without data in orocrm_sales_customer table (#9975)
    * CRM-8187: Magento customer is not displayed without data in orocrm_sales_customer table
    - fixed 500 error if magento customer has no relation with sales customer
    - added migration that fixes problem

--- src/Oro/Bundle/EntityBundle/ORM/DatabasePlatformInterface.php
+++ src/Oro/Bundle/EntityBundle/ORM/DatabasePlatformInterface.php
@@ -6,4 +6,6 @@ interface DatabasePlatformInterface
 {
     const DATABASE_POSTGRESQL = 'pgsql';
     const DATABASE_MYSQL      = 'mysql';
+
+    const DATABASE_PLATFORM_POSTGRESQL = 'postgresql';
 }
