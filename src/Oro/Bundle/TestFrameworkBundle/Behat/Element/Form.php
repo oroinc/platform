39d06b50aafa717c01b1681e320d3259543950f2
Serhii Polishchuk <spolishchuk@magecore.com>
2017-05-17 09:14:58 +0300

    BAP-14746: Fixed OroSuite configuration for inerhited bundles (#10287)

--- src/Oro/Bundle/TestFrameworkBundle/Behat/Element/Form.php
+++ src/Oro/Bundle/TestFrameworkBundle/Behat/Element/Form.php
@@ -39,12 +39,7 @@ class Form extends Element
                 );
             }
 
-            if (isset($this->options['mapping'][$label]['element'])) {
-                $field = $this->elementFactory->wrapElement(
-                    $this->options['mapping'][$label]['element'],
-                    $field
-                );
-            }
+            $field = $this->wrapField($label, $field);
 
             $field->setValue($value);
         }
@@ -56,13 +51,17 @@ class Form extends Element
     public function assertFields(TableNode $table)
     {
         foreach ($table->getRows() as $row) {
-            $locator = isset($this->options['mapping'][$row[0]]) ? $this->options['mapping'][$row[0]] : $row[0];
+            list($label, $value) = $row;
+            $locator = isset($this->options['mapping'][$label]) ? $this->options['mapping'][$label] : $label;
             $field = $this->findField($locator);
-            self::assertNotNull($field, "Field with '$locator' locator not found");
+            self::assertNotNull($field, "Field with not found");
 
-            $expectedValue = self::normalizeValue($row[1]);
+            $field = $this->wrapField($label, $field);
+            echo $field->getOuterHtml();
+
+            $expectedValue = self::normalizeValue($value);
             $fieldValue = self::normalizeValue($field->getValue());
-            self::assertEquals($expectedValue, $fieldValue, sprintf('Field "%s" value is not as expected', $locator));
+            self::assertEquals($expectedValue, $fieldValue, sprintf('Field "%s" value is not as expected', $label));
         }
     }
 
@@ -323,5 +322,22 @@ class Form extends Element
         self::assertNotNull($errorSpan, "Field $fieldName has no validation errors");
 
         return $errorSpan->getText();
+    }
+
+    /**
+     * @param $label
+     * @param NodeElement $field
+     * @return NodeElement
+     */
+    private function wrapField($label, NodeElement $field): NodeElement
+    {
+        if (isset($this->options['mapping'][$label]['element'])) {
+            $field = $this->elementFactory->wrapElement(
+                $this->options['mapping'][$label]['element'],
+                $field
+            );
+        }
+
+        return $field;
     }
 }
