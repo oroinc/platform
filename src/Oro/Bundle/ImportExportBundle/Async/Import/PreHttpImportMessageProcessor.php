79e196778d006fcf63b4a2b108c8fbac2774c31e
Thomas Parys <thomas.parys@wwsh.io>
2017-05-30 07:04:54 +0200

    CRM-8243: Consumer is crashed on CSV validation (#10575)
    * CRM-8243: Consumer is crashed on CSV validation
    - added more logging
    - user friendly error message instead of exception
    - fix in unit test
    - removing unnecessary loggin

--- src/Oro/Bundle/ImportExportBundle/Async/Import/PreHttpImportMessageProcessor.php
+++ src/Oro/Bundle/ImportExportBundle/Async/Import/PreHttpImportMessageProcessor.php
@@ -151,7 +151,8 @@ class PreHttpImportMessageProcessor extends PreImportMessageProcessorAbstract
             'template' => ImportExportResultSummarizer::TEMPLATE_IMPORT_ERROR,
             'body' => [
                 'originFileName' => $body['originFileName'],
-                'error' => $error,
+                'error' =>  'The import file could not be imported due to a fatal error. ' .
+                    'Please check its integrity and try again!',
             ],
             'contentType' => 'text/html',
         ]);
