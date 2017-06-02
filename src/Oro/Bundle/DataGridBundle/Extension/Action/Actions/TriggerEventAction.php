8eb2823e73f3a01f2259b24f2e221d8d9e2c1107
Viacheslav Bemza <vbemza@magecore.com>
2017-06-02 16:26:25 +0300

    BB-9857: Add mass actions for grids in tabs and change current event namespace to scope (#10728)
    * BB-9857: Add mass actions for grids in tabs and change current event namespace to scope
    - improved mass action parameter passing to backend

--- src/Oro/Bundle/DataGridBundle/Extension/Action/Actions/TriggerEventAction.php
+++ src/Oro/Bundle/DataGridBundle/Extension/Action/Actions/TriggerEventAction.php
@@ -2,25 +2,10 @@
 
 namespace Oro\Bundle\DataGridBundle\Extension\Action\Actions;
 
-use Oro\Bundle\DataGridBundle\Extension\Action\ActionConfiguration;
-
 class TriggerEventAction extends AbstractAction
 {
     /**
      * {@inheritdoc}
      */
-    protected $requiredOptions = ['event_name', 'container'];
-
-    /**
-     * {@inheritDoc}
-     */
-    public function setOptions(ActionConfiguration $options)
-    {
-        if (!isset($options['container'])) {
-            $options['container'] = '[data-role="grid-sidebar-component-container"]';
-        }
-        parent::setOptions($options);
-
-        return $this;
-    }
+    protected $requiredOptions = ['event_name'];
 }
