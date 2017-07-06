83d2bd36ad3e5803dd97f5045a5e912767fdb57f
Yurii Muratov <ymuratov@oroinc.com>
2017-07-06 15:45:57 +0300

    BAP-14932: Enums are not translated in grids (#11395)

---
+++ src/Oro/Bundle/DataGridBundle/Tests/Unit/Extension/InlineEditing/InlineEditColumnOptions/ChoicesGuesserTest.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace Oro\Bundle\DataGridBundle\Tests\Unit\Extension\InlineEditing\InlineEditColumnOption;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Mapping\ClassMetadata;
+use Doctrine\ORM\Mapping\ClassMetadataInfo;
+
+use Oro\Bundle\DataGridBundle\Extension\InlineEditing\InlineEditColumnOptions\ChoicesGuesser;
+use Oro\Bundle\DataGridBundle\Tools\ChoiceFieldHelper;
+use Oro\Bundle\EntityBundle\ORM\DoctrineHelper;
+
+class ChoicesGuesserTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var \PHPUnit_Framework_MockObject_MockObject|DoctrineHelper */
+    protected $doctrineHelper;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $choiceHelper;
+
+    /** @var ChoicesGuesser */
+    protected $guesser;
+
+    public function setUp()
+    {
+        $this->choiceHelper = $this->createMock(ChoiceFieldHelper::class);
+
+        $this->doctrineHelper = $this->createMock(DoctrineHelper::class);
+
+        $this->guesser = new ChoicesGuesser($this->doctrineHelper, $this->choiceHelper);
+    }
+
+    public function testGuessTranslatableColumnOptions()
+    {
+        $metadata = new ClassMetadata('\oro\test');
+        $metadata->associationMappings = [
+            'relationField' => [
+                'fieldName' => 'relationField',
+                'targetEntity' => '\oro\target',
+                'type' => ClassMetadataInfo::MANY_TO_ONE
+            ]
+        ];
+
+        $targetMetadata = new ClassMetadata('\oro\target');
+        $targetMetadata->identifier = ['id'];
+
+        $em = $this->createMock(EntityManager::class);
+
+        $this->doctrineHelper->expects($this->once())
+            ->method('getEntityManager')
+            ->will($this->returnValue($em));
+
+        $em->expects($this->any())
+            ->method('getClassMetadata')
+            ->willReturnMap([
+                ['\oro\test', $metadata],
+                ['\oro\target', $targetMetadata],
+            ]);
+
+        $this->choiceHelper->expects($this->once())
+            ->method('guessLabelField')
+            ->willReturn('label');
+
+        $this->choiceHelper->expects($this->once())
+            ->method('getTranslatedChoices')
+            ->with('\oro\target', 'id', 'label')
+            ->willReturn(['a1' => 'A1_t', 'a2' => 'A2_t']);
+
+        $result = $this->guesser->guessColumnOptions('relationField', '\oro\test', ['translatable' => true], true);
+
+        $this->assertEquals(
+            [
+                'inline_editing' => [
+                    'enable' => true,
+                    'editor' => [
+                        'view' => 'oroform/js/app/views/editor/select-editor-view'
+                    ]
+                ],
+                'frontend_type' => 'select',
+                'type' => 'field',
+                'choices' => [
+                    'a1' => 'A1_t',
+                    'a2' => 'A2_t'
+                ]
+            ],
+            $result
+        );
+    }
+
+    public function testGuessNonTranslatableColumnOptions()
+    {
+        $metadata = new ClassMetadata('\oro\test');
+        $metadata->associationMappings = [
+            'relationField' => [
+                'fieldName' => 'relationField',
+                'targetEntity' => '\oro\target',
+                'type' => ClassMetadataInfo::MANY_TO_ONE
+            ]
+        ];
+
+        $targetMetadata = new ClassMetadata('\oro\target');
+        $targetMetadata->identifier = ['id'];
+
+        $em = $this->createMock(EntityManager::class);
+
+        $this->doctrineHelper->expects($this->once())
+            ->method('getEntityManager')
+            ->will($this->returnValue($em));
+
+        $em->expects($this->any())
+            ->method('getClassMetadata')
+            ->willReturnMap([
+                ['\oro\test', $metadata],
+                ['\oro\target', $targetMetadata],
+            ]);
+
+        $this->choiceHelper->expects($this->once())
+            ->method('guessLabelField')
+            ->willReturn('label');
+
+        $this->choiceHelper->expects($this->once())
+            ->method('getChoices')
+            ->with('\oro\target', 'id', 'label')
+            ->willReturn(['a1' => 'A1', 'a2' => 'A2']);
+
+        $result = $this->guesser->guessColumnOptions('relationField', '\oro\test', ['translatable' => false], true);
+
+        $this->assertEquals(
+            [
+                'inline_editing' => [
+                    'enable' => true,
+                    'editor' => [
+                        'view' => 'oroform/js/app/views/editor/select-editor-view'
+                    ]
+                ],
+                'frontend_type' => 'select',
+                'type' => 'field',
+                'choices' => [
+                    'a1' => 'A1',
+                    'a2' => 'A2'
+                ]
+            ],
+            $result
+        );
+    }
+}
--- src/Oro/Bundle/DataGridBundle/Tests/Unit/Tools/ChoiceFieldHelperTest.php
+++ src/Oro/Bundle/DataGridBundle/Tests/Unit/Tools/ChoiceFieldHelperTest.php
@@ -2,6 +2,14 @@
 
 namespace Oro\Bundle\DataGridBundle\Tests\Unit\Tools;
 
+use Doctrine\ORM\AbstractQuery;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Query;
+use Doctrine\ORM\QueryBuilder;
+
+use Gedmo\Translatable\Query\TreeWalker\TranslationWalker;
+
 use Oro\Bundle\DataGridBundle\Tools\ChoiceFieldHelper;
 
 class ChoiceFieldHelperTest extends \PHPUnit_Framework_TestCase
@@ -34,9 +42,9 @@ class ChoiceFieldHelperTest extends \PHPUnit_Framework_TestCase
             ->disableOriginalConstructor()
             ->getMock();
         $metadata->expects($this->once())
-                ->method('hasField')
-                ->with('label')
-                ->will($this->returnValue(true));
+            ->method('hasField')
+            ->with('label')
+            ->will($this->returnValue(true));
         $metadata->expects($this->never())->method('getFieldNames');
 
         $this->assertEquals('label', $this->choiceHelper->guessLabelField($metadata, 'column_name'));
@@ -48,11 +56,11 @@ class ChoiceFieldHelperTest extends \PHPUnit_Framework_TestCase
             ->disableOriginalConstructor()
             ->getMock();
         $metadata->expects($this->any())
-                ->method('hasField')
-                ->will($this->returnValue(false));
+            ->method('hasField')
+            ->will($this->returnValue(false));
         $metadata->expects($this->once())
-                ->method('getFieldNames')
-                ->will($this->returnValue(['stringField']));
+            ->method('getFieldNames')
+            ->will($this->returnValue(['stringField']));
         $metadata->expects($this->once())
             ->method('getTypeOfField')
             ->will($this->returnValue('string'));
@@ -66,11 +74,11 @@ class ChoiceFieldHelperTest extends \PHPUnit_Framework_TestCase
             ->disableOriginalConstructor()
             ->getMock();
         $metadata->expects($this->any())
-                ->method('hasField')
-                ->will($this->returnValue(false));
+            ->method('hasField')
+            ->will($this->returnValue(false));
         $metadata->expects($this->once())
-                ->method('getFieldNames')
-                ->will($this->returnValue([]));
+            ->method('getFieldNames')
+            ->will($this->returnValue([]));
 
         $this->expectException('\Exception');
         $this->choiceHelper->guessLabelField($metadata, 'column_name');
@@ -78,26 +86,20 @@ class ChoiceFieldHelperTest extends \PHPUnit_Framework_TestCase
 
     public function testGetChoices()
     {
-        $em = $this->getMockBuilder('Doctrine\ORM\EntityManager')
-            ->disableOriginalConstructor()
-            ->getMock();
+        $em = $this->createMock(EntityManager::class);
+        $qb = $this->createMock(QueryBuilder::class);
+        $query = $this->createMock(AbstractQuery::class);
+        $repo = $this->createMock(EntityRepository::class);
 
         $this->doctrineHelper->expects($this->any())
             ->method('getEntityManager')
             ->with('entity')
             ->will($this->returnValue($em));
-
-        $qb = $this->getMockBuilder('Doctrine\ORM\QueryBuilder')
-                ->setMethods(['getResult'])
-                ->disableOriginalConstructor()
-                ->getMock();
-        $repo = $this->getMockBuilder('Doctrine\ORM\EntityRepository')
-            ->disableOriginalConstructor()
-            ->getMock();
-
         $this->aclHelper->expects($this->any())
             ->method('apply')
-            ->will($this->returnValue($qb));
+            ->will($this->returnValue($query));
+        $query->expects($this->never())
+            ->method('setHint');
 
         $em->expects($this->any())
             ->method('getRepository')
@@ -105,7 +107,7 @@ class ChoiceFieldHelperTest extends \PHPUnit_Framework_TestCase
         $repo->expects($this->any())
             ->method('createQueryBuilder')
             ->will($this->returnValue($qb));
-        $qb->expects($this->once())
+        $query->expects($this->once())
             ->method('getResult')
             ->will($this->returnValue([['key' => 'a1', 'label' => 'A1'], ['key' => 'a2', 'label' => 'A2']]));
         $expected = [
@@ -113,5 +115,39 @@ class ChoiceFieldHelperTest extends \PHPUnit_Framework_TestCase
             'a2' => 'A2'
         ];
         $this->assertEquals($expected, $this->choiceHelper->getChoices('entity', 'key', 'label'));
+    }
+
+    public function testGetTranslatedChoices()
+    {
+        $em = $this->createMock(EntityManager::class);
+        $qb = $this->createMock(QueryBuilder::class);
+        $query = $this->createMock(AbstractQuery::class);
+        $repo = $this->createMock(EntityRepository::class);
+
+        $this->doctrineHelper->expects($this->any())
+            ->method('getEntityManager')
+            ->with('entity')
+            ->will($this->returnValue($em));
+        $this->aclHelper->expects($this->any())
+            ->method('apply')
+            ->will($this->returnValue($query));
+        $query->expects($this->once())
+            ->method('setHint')
+            ->with(Query::HINT_CUSTOM_OUTPUT_WALKER, TranslationWalker::class);
+
+        $em->expects($this->any())
+            ->method('getRepository')
+            ->will($this->returnValue($repo));
+        $repo->expects($this->any())
+            ->method('createQueryBuilder')
+            ->will($this->returnValue($qb));
+        $query->expects($this->once())
+            ->method('getResult')
+            ->will($this->returnValue([['key' => 'a1', 'label' => 'A1_t'], ['key' => 'a2', 'label' => 'A2_t']]));
+        $expected = [
+            'a1' => 'A1_t',
+            'a2' => 'A2_t'
+        ];
+        $this->assertEquals($expected, $this->choiceHelper->getTranslatedChoices('entity', 'key', 'label'));
     }
 }
