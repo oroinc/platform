c32405fdb90bde15681f87a4d2b1816a745f5b8d
Yurii Muratov <ymuratov@oroinc.com>
2017-07-07 13:27:23 +0300

    BAP-14932: Enums are not translated in grids (#11544)

--- src/Oro/Bundle/DataGridBundle/Tests/Unit/Tools/ChoiceFieldHelperTest.php
+++ src/Oro/Bundle/DataGridBundle/Tests/Unit/Tools/ChoiceFieldHelperTest.php
@@ -2,6 +2,10 @@
 
 namespace Oro\Bundle\DataGridBundle\Tests\Unit\Tools;
 
+use Doctrine\ORM\Query;
+
+use Gedmo\Translatable\Query\TreeWalker\TranslationWalker;
+
 use Oro\Bundle\DataGridBundle\Tools\ChoiceFieldHelper;
 
 class ChoiceFieldHelperTest extends \PHPUnit_Framework_TestCase
@@ -34,9 +38,9 @@ class ChoiceFieldHelperTest extends \PHPUnit_Framework_TestCase
             ->disableOriginalConstructor()
             ->getMock();
         $metadata->expects($this->once())
-                ->method('hasField')
-                ->with('label')
-                ->will($this->returnValue(true));
+            ->method('hasField')
+            ->with('label')
+            ->will($this->returnValue(true));
         $metadata->expects($this->never())->method('getFieldNames');
 
         $this->assertEquals('label', $this->choiceHelper->guessLabelField($metadata, 'column_name'));
@@ -48,11 +52,11 @@ class ChoiceFieldHelperTest extends \PHPUnit_Framework_TestCase
             ->disableOriginalConstructor()
             ->getMock();
         $metadata->expects($this->any())
-                ->method('hasField')
-                ->will($this->returnValue(false));
+            ->method('hasField')
+            ->will($this->returnValue(false));
         $metadata->expects($this->once())
-                ->method('getFieldNames')
-                ->will($this->returnValue(['stringField']));
+            ->method('getFieldNames')
+            ->will($this->returnValue(['stringField']));
         $metadata->expects($this->once())
             ->method('getTypeOfField')
             ->will($this->returnValue('string'));
@@ -66,11 +70,11 @@ class ChoiceFieldHelperTest extends \PHPUnit_Framework_TestCase
             ->disableOriginalConstructor()
             ->getMock();
         $metadata->expects($this->any())
-                ->method('hasField')
-                ->will($this->returnValue(false));
+            ->method('hasField')
+            ->will($this->returnValue(false));
         $metadata->expects($this->once())
-                ->method('getFieldNames')
-                ->will($this->returnValue([]));
+            ->method('getFieldNames')
+            ->will($this->returnValue([]));
 
         $this->setExpectedException('\Exception');
         $this->choiceHelper->guessLabelField($metadata, 'column_name');
@@ -81,23 +85,25 @@ class ChoiceFieldHelperTest extends \PHPUnit_Framework_TestCase
         $em = $this->getMockBuilder('Doctrine\ORM\EntityManager')
             ->disableOriginalConstructor()
             ->getMock();
+        $qb = $this->getMockBuilder('Doctrine\ORM\QueryBuilder')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $query = $this->getMockBuilder('Doctrine\ORM\AbstractQuery')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $repo = $this->getMockBuilder('Doctrine\ORM\EntityRepository')
+            ->disableOriginalConstructor()
+            ->getMock();
 
         $this->doctrineHelper->expects($this->any())
             ->method('getEntityManager')
             ->with('entity')
             ->will($this->returnValue($em));
-
-        $qb = $this->getMockBuilder('Doctrine\ORM\QueryBuilder')
-                ->setMethods(['getResult'])
-                ->disableOriginalConstructor()
-                ->getMock();
-        $repo = $this->getMockBuilder('Doctrine\ORM\EntityRepository')
-            ->disableOriginalConstructor()
-            ->getMock();
-
         $this->aclHelper->expects($this->any())
             ->method('apply')
-            ->will($this->returnValue($qb));
+            ->will($this->returnValue($query));
+        $query->expects($this->never())
+            ->method('setHint');
 
         $em->expects($this->any())
             ->method('getRepository')
@@ -105,7 +111,7 @@ class ChoiceFieldHelperTest extends \PHPUnit_Framework_TestCase
         $repo->expects($this->any())
             ->method('createQueryBuilder')
             ->will($this->returnValue($qb));
-        $qb->expects($this->once())
+        $query->expects($this->once())
             ->method('getResult')
             ->will($this->returnValue([['key' => 'a1', 'label' => 'A1'], ['key' => 'a2', 'label' => 'A2']]));
         $expected = [
@@ -113,5 +119,47 @@ class ChoiceFieldHelperTest extends \PHPUnit_Framework_TestCase
             'a2' => 'A2'
         ];
         $this->assertEquals($expected, $this->choiceHelper->getChoices('entity', 'key', 'label'));
+    }
+
+    public function testGetTranslatedChoices()
+    {
+        $em = $this->getMockBuilder('Doctrine\ORM\EntityManager')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $qb = $this->getMockBuilder('Doctrine\ORM\QueryBuilder')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $query = $this->getMockBuilder('Doctrine\ORM\AbstractQuery')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $repo = $this->getMockBuilder('Doctrine\ORM\EntityRepository')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->doctrineHelper->expects($this->any())
+            ->method('getEntityManager')
+            ->with('entity')
+            ->will($this->returnValue($em));
+        $this->aclHelper->expects($this->any())
+            ->method('apply')
+            ->will($this->returnValue($query));
+        $query->expects($this->once())
+            ->method('setHint')
+            ->with(Query::HINT_CUSTOM_OUTPUT_WALKER, TranslationWalker::class);
+
+        $em->expects($this->any())
+            ->method('getRepository')
+            ->will($this->returnValue($repo));
+        $repo->expects($this->any())
+            ->method('createQueryBuilder')
+            ->will($this->returnValue($qb));
+        $query->expects($this->once())
+            ->method('getResult')
+            ->will($this->returnValue([['key' => 'a1', 'label' => 'A1_t'], ['key' => 'a2', 'label' => 'A2_t']]));
+        $expected = [
+            'a1' => 'A1_t',
+            'a2' => 'A2_t'
+        ];
+        $this->assertEquals($expected, $this->choiceHelper->getTranslatedChoices('entity', 'key', 'label'));
     }
 }
