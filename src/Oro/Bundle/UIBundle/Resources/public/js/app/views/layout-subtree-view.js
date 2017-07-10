59426bd049630ccf086bbd2f22d6e1ebfe13deb8
Mykhailo Sulyma <msulima@oroinc.com>
2017-07-10 16:59:29 +0300

    BB-10676: FrontStore > Order > Some sections are disappear on the Order creation page (#11615)
    * BB-10676: FrontStore > Order > Some sections are disappear on the Order creation page
     - updated layout subtree components to provide ability to save form data inside block
    
    * BB-10676: FrontStore > Order > Some sections are disappear on the Order creation page
     - CR fixes

--- src/Oro/Bundle/UIBundle/Resources/public/js/app/views/layout-subtree-view.js
+++ src/Oro/Bundle/UIBundle/Resources/public/js/app/views/layout-subtree-view.js
@@ -6,13 +6,17 @@ define(function(require) {
     var LoadingMaskView = require('oroui/js/app/views/loading-mask-view');
     var LayoutSubtreeManager = require('oroui/js/layout-subtree-manager');
     var $ = require('jquery');
+    var _ = require('underscore');
 
     LayoutSubtreeView = BaseView.extend({
         options: {
             blockId: '',
             reloadEvents: [],
-            showLoading: true
+            showLoading: true,
+            restoreFormState: false
         },
+
+        formState: null,
 
         initialize: function(options) {
             this.options = $.extend(true, {}, this.options, options);
@@ -40,6 +44,14 @@ define(function(require) {
 
         beforeContentLoading: function() {
             this._showLoading();
+
+            if (this.options.restoreFormState) {
+                this._saveFormState();
+            }
+        },
+
+        afterContentLoading: function() {
+            this._restoreFormState();
         },
 
         contentLoadingFail: function() {
@@ -65,6 +77,51 @@ define(function(require) {
                 return;
             }
             this.removeSubview('loadingMask');
+        },
+
+        _saveFormState: function() {
+            this.formState = {};
+
+            _.each(this._getInputs(), _.bind(function(input) {
+                var name = input.name;
+                var value = input.value;
+
+                if ($(input).is(':checkbox, :radio')) {
+                    name += ':' + value;
+                    value = input.checked;
+                }
+                this.formState[name] = value;
+            }, this));
+        },
+
+        _restoreFormState: function() {
+            if (!this.formState) {
+                return;
+            }
+
+            _.each(this._getInputs(), _.bind(function(input) {
+                var name = input.name;
+                var isRadio = $(input).is(':checkbox, :radio');
+                if (isRadio) {
+                    name += ':' + input.value;
+                }
+
+                var savedInput = this.formState[name];
+                if (savedInput === undefined || (isRadio && !savedInput)) {
+                    return;
+                }
+
+                var $input = $(input);
+                if (isRadio) {
+                    $input.click();
+                } else {
+                    $input.val(savedInput).change();
+                }
+            }, this));
+        },
+
+        _getInputs: function() {
+            return this.$el.find('input:text, textarea, select').filter('[name!=""]');
         }
     });
 
