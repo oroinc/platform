e55d39ebfb88bef15cc81039e755cbe0125ab6f8
Vladimir Seniuk <vseniuk@oroinc.com>
2017-07-13 19:33:11 +0300

    BB-10615: Add default User Owner for guest checkouts to system configuration - added abstract DefaultUserSystemConfigListener - added default owner on entity pre persist event

---
+++ src/Oro/Bundle/UserBundle/Provider/DefaultUserProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Oro\Bundle\UserBundle\Provider;
+
+use Oro\Bundle\ConfigBundle\Config\ConfigManager;
+use Oro\Bundle\ConfigBundle\Utils\TreeUtils;
+use Oro\Bundle\EntityBundle\ORM\DoctrineHelper;
+use Oro\Bundle\UserBundle\Entity\User;
+
+class DefaultUserProvider
+{
+    /** @var ConfigManager */
+    private $configManager;
+
+    /** @var DoctrineHelper */
+    private $doctrineHelper;
+
+    /**
+     * @param ConfigManager $configManager
+     * @param DoctrineHelper $doctrineHelper
+     */
+    public function __construct(ConfigManager $configManager, DoctrineHelper $doctrineHelper)
+    {
+        $this->configManager = $configManager;
+        $this->doctrineHelper = $doctrineHelper;
+    }
+
+    /**
+     * @param string $alias
+     * @param string $configKey
+     *
+     * @return User|null
+     */
+    public function getDefaultUser($alias, $configKey)
+    {
+        $settingsKey = TreeUtils::getConfigKey($alias, $configKey);
+        $ownerId = $this->configManager->get($settingsKey);
+        $repository = $this->doctrineHelper->getEntityRepositoryForClass(User::class);
+
+        return $ownerId ? $repository->find($ownerId) : $repository->findOneBy([], ['id' => 'ASC']);
+    }
+}
---
+++ src/Oro/Bundle/UserBundle/Tests/Unit/Provider/DefaultUserProviderTest.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Oro\Bundle\UserBundle\Tests\Unit\Provider;
+
+use Doctrine\ORM\EntityRepository;
+
+use Oro\Bundle\ConfigBundle\Config\ConfigManager;
+use Oro\Bundle\EntityBundle\ORM\DoctrineHelper;
+use Oro\Bundle\UserBundle\Entity\User;
+use Oro\Bundle\UserBundle\Provider\DefaultUserProvider;
+
+use Oro\Component\Testing\Unit\EntityTrait;
+
+class DefaultUserProviderTest extends \PHPUnit_Framework_TestCase
+{
+    use EntityTrait;
+
+    /** @var ConfigManager|\PHPUnit_Framework_MockObject_MockObject */
+    private $configManager;
+
+    /** @var DoctrineHelper|\PHPUnit_Framework_MockObject_MockObject */
+    private $doctrineHelper;
+
+    /** @var DefaultUserProvider */
+    private $provider;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        $this->configManager = $this->createMock(ConfigManager::class);
+        $this->doctrineHelper = $this->createMock(DoctrineHelper::class);
+
+        $this->provider = new DefaultUserProvider($this->configManager, $this->doctrineHelper);
+    }
+
+    public function testGetDefaultUser()
+    {
+        $user = $this->getEntity(User::class, ['first_name' => 'first name']);
+
+        $this->configManager
+            ->expects($this->once())
+            ->method('get')
+            ->with('alias.key')
+            ->willReturn(null);
+
+        /** @var EntityRepository|\PHPUnit_Framework_MockObject_MockObject $repository */
+        $repository = $this->createMock(EntityRepository::class);
+        $repository->expects($this->once())
+            ->method('findOneBy')
+            ->with([], ['id' => 'ASC'])
+            ->willReturn($user);
+
+        $this->doctrineHelper
+            ->expects($this->once())
+            ->method('getEntityRepositoryForClass')
+            ->with(User::class)
+            ->willReturn($repository);
+
+        $this->assertSame($user, $this->provider->getDefaultUser('alias', 'key'));
+    }
+
+    public function testGetDefaultUserById()
+    {
+        $user = $this->getEntity(User::class, ['id' => 1, 'first_name' => 'first name']);
+
+        $this->configManager
+            ->expects($this->once())
+            ->method('get')
+            ->with('alias.key')
+            ->willReturn(1);
+
+        /** @var EntityRepository|\PHPUnit_Framework_MockObject_MockObject $repository */
+        $repository = $this->createMock(EntityRepository::class);
+        $repository->expects($this->once())
+            ->method('find')
+            ->with(1)
+            ->willReturn($user);
+
+        $this->doctrineHelper
+            ->expects($this->once())
+            ->method('getEntityRepositoryForClass')
+            ->with(User::class)
+            ->willReturn($repository);
+
+        $this->assertSame($user, $this->provider->getDefaultUser('alias', 'key'));
+    }
+}
