0c86ffe5a8a77b93e2468495d145f8848eb59f83
Adrian Caliman <acaliman@pitechplus.com>
2017-07-24 16:11:09 +0300

    BB-10936 - Create product API validation - removed unused dependency.

--- src/Oro/Bundle/EntityBundle/Form/Type/EntityFieldFallbackValueType.php
+++ src/Oro/Bundle/EntityBundle/Form/Type/EntityFieldFallbackValueType.php
@@ -2,7 +2,6 @@
 
 namespace Oro\Bundle\EntityBundle\Form\Type;
 
-use Oro\Bundle\EntityBundle\Validator\Constraints\EntityFieldFallbackValues;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
