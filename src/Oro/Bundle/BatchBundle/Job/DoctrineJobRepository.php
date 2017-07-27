05ab06c98c2ec52fe283f5db7cb881ae72bb80f8
Alexander Nezdoiminoga <alex@magecore.com>
2017-07-27 07:14:59 +0300

    BAP-14629: Entity manager is closed during import CSV (#11469)

--- src/Oro/Bundle/BatchBundle/Job/DoctrineJobRepository.php
+++ src/Oro/Bundle/BatchBundle/Job/DoctrineJobRepository.php
@@ -2,10 +2,9 @@
 
 namespace Oro\Bundle\BatchBundle\Job;
 
-use Doctrine\ORM\EntityManager;
+use Doctrine\Common\Persistence\ManagerRegistry;
 use Doctrine\ORM\UnitOfWork;
 
-use Akeneo\Bundle\BatchBundle\Entity\JobInstance;
 use Akeneo\Bundle\BatchBundle\Entity\JobExecution;
 use Akeneo\Bundle\BatchBundle\Entity\StepExecution;
 use Akeneo\Bundle\BatchBundle\Job\DoctrineJobRepository as BaseRepository;
@@ -16,15 +15,17 @@ use Akeneo\Bundle\BatchBundle\Job\DoctrineJobRepository as BaseRepository;
  */
 class DoctrineJobRepository extends BaseRepository
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function __construct(EntityManager $entityManager, $jobExecutionClass = JobExecution::class)
-    {
-        $this->jobManager = $entityManager;
-        $this->jobExecutionClass = $jobExecutionClass;
+    /** @var ManagerRegistry */
+    protected $doctrine;
 
-        $this->jobManager->getConfiguration()->setSQLLogger(null);
+    /**
+     * @param ManagerRegistry $doctrine
+     * @param string          $jobExecutionClass
+     */
+    public function __construct(ManagerRegistry $doctrine, $jobExecutionClass = JobExecution::class)
+    {
+        $this->doctrine = $doctrine;
+        $this->jobExecutionClass = $jobExecutionClass;
     }
 
     /**
@@ -32,11 +33,13 @@ class DoctrineJobRepository extends BaseRepository
      */
     public function updateJobExecution(JobExecution $jobExecution)
     {
-        $uow = $this->getJobManager()->getUnitOfWork();
+        $jobManager = $this->getJobManager();
+
         $jobInstance = $jobExecution->getJobInstance();
-        if ($jobInstance && UnitOfWork::STATE_DETACHED === $uow->getEntityState($jobInstance)) {
-            /** @var JobInstance $jobInstance */
-            $jobInstance = $uow->merge($jobInstance);
+        if ($jobInstance->getId()
+            && UnitOfWork::STATE_DETACHED === $jobManager->getUnitOfWork()->getEntityState($jobInstance)
+        ) {
+            $jobInstance = $jobManager->merge($jobInstance);
             $jobExecution->setJobInstance($jobInstance);
         }
 
@@ -48,11 +51,73 @@ class DoctrineJobRepository extends BaseRepository
      */
     public function updateStepExecution(StepExecution $stepExecution)
     {
+        $jobManager = $this->getJobManager();
+        if ($stepExecution->getId()
+            && UnitOfWork::STATE_DETACHED === $jobManager->getUnitOfWork()->getEntityState($stepExecution)
+        ) {
+            $stepExecution = $jobManager->merge($stepExecution);
+        }
+
+        /**
+         * @see \Akeneo\Bundle\BatchBundle\Step\AbstractStep::execute
+         * because of StepExecution is not configured to cascade persist JobExecution
+         * to avoid an error "A new entity was found through the relationship
+         * 'Akeneo\Bundle\BatchBundle\Entity\StepExecution#jobExecution' that was not configured to cascade
+         * persist operations".
+         */
         $jobExecution = $stepExecution->getJobExecution();
-        if ($jobExecution) {
+        if ($jobExecution->getId()
+            && UnitOfWork::STATE_DETACHED === $jobManager->getUnitOfWork()->getEntityState($jobExecution)
+        ) {
             $this->updateJobExecution($jobExecution);
         }
 
         parent::updateStepExecution($stepExecution);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getJobManager()
+    {
+        if (null !== $this->jobManager && !$this->jobManager->isOpen()) {
+            $this->resetEntityManager();
+            $this->jobManager = null;
+        }
+
+        if (null === $this->jobManager) {
+            $this->jobManager = $this->doctrine->getManagerForClass($this->jobExecutionClass);
+            $this->jobManager->getConfiguration()->setSQLLogger(null);
+        } else {
+            /**
+             * ensure that the transaction is fully rolled back
+             * in case if a nested transaction is rolled back but the entity manager is not closed
+             * this may happen if the EntityManager::rollback() method is called
+             * without the call of EntityManager::close() method
+             * @link http://docs.doctrine-project.org/projects/doctrine-orm/en/latest/reference/transactions-and-concurrency.html#exception-handling
+             */
+            $connection = $this->jobManager->getConnection();
+            if ($connection->getTransactionNestingLevel() > 0 && $connection->isRollbackOnly()) {
+                while ($connection->getTransactionNestingLevel() > 0) {
+                    $connection->rollBack();
+                }
+            }
+        }
+
+        return parent::getJobManager();
+    }
+
+    /**
+     * Replaces the closed entity manager with new instance of entity manager.
+     */
+    private function resetEntityManager()
+    {
+        $managers = $this->doctrine->getManagers();
+        foreach ($managers as $name => $manager) {
+            if (!$manager->getMetadataFactory()->isTransient($this->jobExecutionClass)) {
+                $this->doctrine->resetManager($name);
+                break;
+            }
+        }
     }
 }
