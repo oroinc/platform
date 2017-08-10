3d2bbfdd90b7f3605598996babbcb8c47aac6edf
Oleksandr Nechyporenko <onechyporenko@oroinc.com>
2017-08-10 13:45:45 +0300

    BB-9074: Disable Maps (Store Frontend)  - rename enableMapPreview to showMap

--- src/Oro/Bundle/AddressBundle/Resources/public/js/address-book.js
+++ src/Oro/Bundle/AddressBundle/Resources/public/js/address-book.js
@@ -32,8 +32,6 @@ define([
     return BaseView.extend({
         isEmpty: false,
 
-        showMap: true,
-
         options: {
             'mapOptions': {
                 zoom: 12
@@ -48,7 +46,7 @@ define([
             'allowToRemovePrimary': false,
             'confirmRemove': true,
             'confirmRemoveComponent': deleteConfirmation,
-            'enableMapPreview': true
+            showMap: true
         },
         noDataMessage: __('Empty Address Book'),
         attributes: {
@@ -67,7 +65,7 @@ define([
 
             this._initMainContainers();
 
-            if (this.showMap && this.options.enableMapPreview) {
+            if (this.options.showMap) {
                 this.initializeMap();
             }
         },
