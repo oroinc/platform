8759622d95a722d46ac334c261a3e903376a3f46
Alexandr Parkhomenko <alexandr-parkhomenko@users.noreply.github.com>
2017-08-11 15:32:53 +0300

    BAP-15032: Error on Contact import data template (2.2) (#12266)

--- src/Oro/Bundle/ImportExportBundle/Strategy/Import/ConfigurableAddOrReplaceStrategy.php
+++ src/Oro/Bundle/ImportExportBundle/Strategy/Import/ConfigurableAddOrReplaceStrategy.php
@@ -443,6 +443,10 @@ class ConfigurableAddOrReplaceStrategy extends AbstractImportStrategy
      */
     protected function combineIdentityValues($entity, $entityClass, array $searchContext)
     {
+        if (!$this->isSearchContextValid($searchContext)) {
+            return null;
+        }
+
         $identityValues = $searchContext;
         $identityValues += $this->fieldHelper->getIdentityValues($entity);
         $notEmptyValues     = [];
@@ -577,6 +581,34 @@ class ConfigurableAddOrReplaceStrategy extends AbstractImportStrategy
             $this->context->addError($error);
 
             return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * We shouldn't allow to search related entity in cache of `newEntitiesHelper`
+     * if main entity is not in db and
+     * main entity has one of next relations to current entity: ONE_TO_ONE or ONE_TO_MANY
+     * because in another case we can face with issue of resetting inversed
+     *
+     * @param array $searchContext
+     *
+     * @return bool
+     */
+    protected function isSearchContextValid(array $searchContext)
+    {
+        foreach ($searchContext as $identityValue) {
+            if (null === $identityValue || '' === $identityValue) {
+                return false;
+            }
+
+            if (is_object($identityValue)) {
+                $identifier = $this->databaseHelper->getIdentifier($identityValue);
+                if ($identifier === null) {
+                    return false;
+                }
+            }
         }
 
         return true;
