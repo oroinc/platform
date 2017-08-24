3d394eafd9e50a9e18c74852cd6cc4f5f8971eae
Marcin Łukaszewicz <mlukaszewicz@oroinc.com>
2017-08-24 09:52:20 +0200

    BAP-15246: Infinity oro_message_queue_job table size growth (#12779)
    * BAP-15246: Infinity oro_message_queue_job table size growth
    - added command to clear message-queue
    - added migration

--- src/Oro/Bundle/MessageQueueBundle/Migrations/Schema/OroMessageQueueBundleInstaller.php
+++ src/Oro/Bundle/MessageQueueBundle/Migrations/Schema/OroMessageQueueBundleInstaller.php
@@ -19,7 +19,7 @@ class OroMessageQueueBundleInstaller implements Installation, ContainerAwareInte
      */
     public function getMigrationVersion()
     {
-        return 'v1_3';
+        return 'v1_4';
     }
 
     /**
