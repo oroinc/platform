9880a7b71f809f14c73d0ea89ba218a9236c8e97
Serhii Polishchuk <spolishchuk@magecore.com>
2017-08-29 14:01:34 +0300

    BAP-15050: Separate behat Repository Initializers by bundles (#12899)
    * BAP-15050: Separate behat Repository Initializers by bundles  (#12878)
    
    * BAP-15050: Divide behat repository initializers by bundle
    
    * BAP-15050: Remove old behat Reference initializers
    
    * BAP-15050: Fixed deprecation warning in behat initializers
    
    * BAP-15050: Added missing DPD repository initializer
    
    * BAP-15050: Fixed behat initializers instatiation
    
    * BAP-15050: Fixed DPD behat initializer namespace
    
    * BAP-15050: Separate behat Repository Initializers by bundles:
    - Made method as non static in ReferenceRepositoryInitializerInterface
    - Throw exception when trying to add dependencies to Initializer
    - Fixed docblock in DoctrineIsolator
    
    # Conflicts:
    #	package/commerce/src/Oro/Bundle/ApplicationBundle/Tests/Behat/ReferenceRepositoryInitializer.php
    
    * BAP-15050: Separate behat Repository Initializers by bundles
    - Added available references behat controller
    - Added documentation
    
    * BAP-15050: Remove deprecated ReferenceInitializer behat parameter
    
    * BAP-15050: Fixed unit tests readability
    
    * BAP-15050: Fixed doc block
    
    * BAP-15050: Remove extra code, fixed code readability
    
    * BAP-15050: Restrict add new objects while behat references initialize
    
    * BAP-15050: Fixed error message
    
    * BAP-15050: Separate behat Repository Initializers by bundles
    - CR Fixes
    
    * BAP-15050: Separate behat Repository Initializers by bundles
    - reverted `if`
    
    * BAP-15050: Separate behat Repository Initializers by bundles
    - fixed unit test
    
    * BAP-15050: Separate behat Repository Initializers by bundles
    - Added 'Repository Initializer' for OroSegmentBundle

---
+++ src/Oro/Bundle/TestFrameworkBundle/Tests/Unit/Behat/Cli/AvailableReferencesControllerTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Oro\Bundle\TestFrameworkBundle\Tests\Unit\Behat\Cli;
+
+use Oro\Bundle\TestFrameworkBundle\Behat\Cli\AvailableReferencesController;
+use Oro\Bundle\TestFrameworkBundle\Behat\Fixtures\OroAliceLoader;
+use Oro\Bundle\TestFrameworkBundle\Behat\Isolation\DoctrineIsolator;
+use Oro\Bundle\TestFrameworkBundle\Tests\Unit\Stub\KernelStub;
+use Oro\Component\Testing\Unit\Command\Stub\InputStub;
+use Oro\Component\Testing\Unit\Command\Stub\OutputStub;
+use Symfony\Component\Console\Command\Command;
+
+class AvailableReferencesControllerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testConfigure()
+    {
+        $aliceLoader = new OroAliceLoader();
+        $doctrineIsolator = $this->createMock(DoctrineIsolator::class);
+        $kernel = new KernelStub();
+        $controller = new AvailableReferencesController($aliceLoader, $doctrineIsolator, $kernel);
+
+        $command = new Command('test');
+        $controller->configure($command);
+
+        $this->assertTrue($command->getDefinition()->hasOption('available-references'));
+        $this->assertFalse($command->getDefinition()->getOption('available-references')->isValueRequired());
+        $this->assertEmpty($command->getDefinition()->getOption('available-references')->getDefault());
+    }
+
+    public function testExecute()
+    {
+        $aliceLoader = new OroAliceLoader();
+        $doctrineIsolator = $this->createMock(DoctrineIsolator::class);
+        $doctrineIsolator->expects($this->once())->method('initReferences');
+        $kernel = new KernelStub();
+        $controller = new AvailableReferencesController($aliceLoader, $doctrineIsolator, $kernel);
+        $output = new OutputStub();
+        $returnCode = $controller->execute(new InputStub('', [], ['available-references' => true]), $output);
+        $this->assertSame(0, $returnCode);
+    }
+
+    public function testNotExecute()
+    {
+        $aliceLoader = new OroAliceLoader();
+        $doctrineIsolator = $this->createMock(DoctrineIsolator::class);
+        $doctrineIsolator->expects($this->never())->method('initReferences');
+        $kernel = new KernelStub();
+        $controller = new AvailableReferencesController($aliceLoader, $doctrineIsolator, $kernel);
+        $output = new OutputStub();
+        $returnCode = $controller->execute(new InputStub(), $output);
+        $this->assertNotSame(0, $returnCode);
+    }
+}
