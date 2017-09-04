f5848416d8a4308343d2a8c882c4bf0dbb879804
Thomas Parys <thomas.parys@wwsh.io>
2017-09-04 16:22:31 +0200

    BB-10471: Management Console > 500 error when apply filter 'Brand' on… (#13031)
    * BB-10471: Management Console > 500 error when apply filter 'Brand' on Product creation page.
    - filter bug fix
    - product-brands.feature updated
    - GridContext.php updated

--- src/Oro/Bundle/DataGridBundle/Tests/Behat/Context/GridContext.php
+++ src/Oro/Bundle/DataGridBundle/Tests/Behat/Context/GridContext.php
@@ -1612,10 +1612,10 @@ TEXT;
             $gridToolbarActions = $this->elementFactory->createElement($gridName . 'ToolbarActions');
             if ($gridToolbarActions->isVisible()) {
                 $gridToolbarActions->getActionByTitle('Filters')->click();
-            } else {
-                $filterState = $this->elementFactory->createElement($gridName . 'FiltersState');
-                self::assertNotNull($filterState);
+            }
 
+            $filterState = $this->elementFactory->createElement($gridName . 'FiltersState');
+            if ($filterState->isValid()) {
                 $filterState->click();
             }
         }
