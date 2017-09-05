1b908bb297f2ea832071c874d898d271b2821814
Mateusz Janiszewski <mjaniszewski@oroinc.com>
2017-09-05 14:41:53 +0200

    BAP-15319: Component for script in fields.html.twig (AddressBundle) (#12930)
    * BAP-15319: Component for script in fields.html.twig (AddressBundle)
    - removed inline js code
    
    * BAP-15319: Component for script in fields.html.twig (AddressBundle)
    - code refactoring
    
    * BAP-15319: Component for script in fields.html.twig (AddressBundle)
    - code refactoring
    
    * BAP-15319: Component for script in fields.html.twig (AddressBundle)
    - code refactoring

---
+++ src/Oro/Bundle/AddressBundle/Resources/public/js/app/views/address-collection-view.js
@@ -0,0 +1,28 @@
+define(function(require) {
+    'use strict';
+
+    var AddressCollectionView;
+    var BaseView = require('oroui/js/app/views/base/view');
+
+    AddressCollectionView = BaseView.extend({
+        events: {
+            'click [data-name="field__types"] input': 'onClicked'
+        },
+
+        onClicked: function(e) {
+            var currentTarget = e.currentTarget;
+
+            if (!currentTarget.checked) {
+                return;
+            }
+
+            this.$('[name$="[types][]"][value="' + currentTarget.value + '"]').each(function(idx, el) {
+                el.checked = false;
+            });
+
+            currentTarget.checked = true;
+        }
+    });
+
+    return AddressCollectionView;
+});
