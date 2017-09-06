06b785d21c8a96498a1c83cc130b64cca480cf06
Mateusz Janiszewski <mjaniszewski@oroinc.com>
2017-09-06 14:53:09 +0200

    BAP-15330: Component for script in macros.html.twig (EmailBundle) (#13079)
    - removed inline scripts

--- src/Oro/Bundle/EmailBundle/Resources/public/js/app/views/email-template-editor-view.js
+++ src/Oro/Bundle/EmailBundle/Resources/public/js/app/views/email-template-editor-view.js
@@ -4,7 +4,9 @@ define(function(require) {
     var EmailTemplateEditorView;
     var $ = require('jquery');
     var _ = require('underscore');
+    var __ = require('orotranslation/js/translator');
     var BaseView = require('oroui/js/app/views/base/view');
+    var DialogWidget = require('oro/dialog-widget');
 
     EmailTemplateEditorView = BaseView.extend({
         options: {
@@ -14,10 +16,11 @@ define(function(require) {
             emailVariableView: {} // link to app/views/email-variable-view
         },
         listen: {
-            'email-variable-view:click-variable mediator': '_onVariableClick'
+            'email-variable-view:click-variable mediator': 'onVariableClick'
         },
         events: {
-            'change input[name*=type]': '_onTypeChange'
+            'change input[name*=type]': 'onTypeChange',
+            'click .dialog-form-renderer': 'onPreview'
         },
 
         initialize: function(options) {
@@ -46,7 +49,58 @@ define(function(require) {
             }
         },
 
-        _onVariableClick: function(field, value) {
+        onPreview: function(event) {
+            event.preventDefault();
+            var $currentView = this.$el;
+
+            var iframeId = 'preview-frame';
+            var iframe = $('<iframe />', {
+                name: iframeId,
+                id: iframeId,
+                frameborder: 0,
+                marginwidth: 20,
+                marginheight: 20,
+                allowfullscreen: true
+            });
+
+            var formAction = $currentView.attr('action');
+
+            $currentView.one('submit', function(e) {
+                if (!e.result) {
+                    return;
+                }
+                var confirmModal = new DialogWidget({
+                    title: __('Preview'),
+                    'dialogOptions': {
+                        'modal': true,
+                        'resizable': true,
+                        'width': '85%',
+                        'height': '70%',
+                        'autoResize': true
+                    }
+                });
+                confirmModal.render();
+                confirmModal._onContentLoad('<div class="widget-content"></div>');
+                confirmModal._showLoading();
+                confirmModal.widget.addClass('dialog-single-iframe-container');
+                confirmModal.$el.append(iframe);
+                $currentView.attr('target', iframeId);
+                $currentView.attr('action', $(event.currentTarget).attr('href'));
+
+                iframe.one('load', function() {
+                    confirmModal._hideLoading();
+                    $currentView.removeAttr('target');
+                    $currentView.attr('action', formAction);
+                });
+
+                // prevent navigation form processing
+                e.stopImmediatePropagation();
+            });
+
+            $currentView.submit();
+        },
+
+        onVariableClick: function(field, value) {
             if (this.options.isWysiwygEnabled) {
                 this.forEachComponent(function(component) {
                     if (_.result(component.view, 'tinymceConnected') === true && component.view.$el.is(field)) {
@@ -78,7 +132,7 @@ define(function(require) {
             }
         },
 
-        _onTypeChange: function(e) {
+        onTypeChange: function(e) {
             if (this.options.hasWysiwyg) {
                 var target = $(e.target);
                 if (!target.is(':checked')) {
