a82803a6b204a6bc41ff6891f2d291fdb511fa59
Mateusz Janiszewski <mjaniszewski@oroinc.com>
2017-09-06 11:08:46 +0200

    BAP-15325: Component for script in fields.html.twig (DashboardBundle) (#13065)
    * BAP-15325: Component for script in fields.html.twig (DashboardBundle)
    - removed inline script
    
    * BAP-15325: Component for script in fields.html.twig (DashboardBundle)
    
    Remove unused callback argument

---
+++ src/Oro/Bundle/DashboardBundle/Resources/public/js/app/views/widget-tabs-view.js
@@ -0,0 +1,63 @@
+define(function(require) {
+    'use strict';
+
+    var WidgetTabsView;
+    var BaseView = require('oroui/js/app/views/base/view');
+    var $ = require('jquery');
+    var LoadingMask = require('oroui/js/app/views/loading-mask-view');
+
+    WidgetTabsView = BaseView.extend({
+        events: {
+            'click .tab-button': 'onTabClick'
+        },
+
+        loadingMask: null,
+
+        widgetComponentProcessingClass: 'widget-component-processing',
+
+        initialize: function() {
+            this.loadingMask = new LoadingMask({container: this._getTabContent()});
+
+            WidgetTabsView.__super__.initialize.apply(this, arguments);
+        },
+
+        _getTabContent: function() {
+            return this.$el.closest('.widget-content').find('.tab-content');
+        },
+
+        onTabClick: function(e) {
+            var $currentTarget = $(e.currentTarget);
+            var $prevTab = this.$('.nav-tabs').find('li.active');
+            var $currentTab = $currentTarget.closest('li');
+
+            $prevTab.removeClass('active');
+            $currentTab.addClass('active');
+
+            this.loadingMask.show();
+
+            // add style like on standard tabs realization through tabs-component
+            $currentTarget.addClass(this.widgetComponentProcessingClass);
+
+            $.ajax({
+                url: $currentTarget.data('url'),
+                dataType: 'html',
+                error: function() {
+                    $currentTab.removeClass('active');
+                    $prevTab.addClass('active');
+                },
+                success: function(data) {
+                    this._getTabContent().find('.content')
+                        .trigger('content:remove')
+                        .html(data)
+                        .trigger('content:changed');
+                }.bind(this),
+                complete: function() {
+                    this.loadingMask.hide();
+                    $currentTarget.removeClass(this.widgetComponentProcessingClass);
+                }.bind(this)
+            });
+        }
+    });
+
+    return WidgetTabsView;
+});
