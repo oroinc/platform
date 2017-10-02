66b154f65ff2c8f77c1e233b21350a723d7cb7c3
Mateusz Janiszewski <mjaniszewski@oroinc.com>
2017-10-02 11:11:30 +0200

    BAP-15456: Remove script tags from configurableTemplateExport.html.twig (ImportExportBundle) (#13595)
    - removed inline script

---
+++ src/Oro/Bundle/ImportExportBundle/Resources/public/js/app/views/export-configurable-template-view.js
@@ -0,0 +1,50 @@
+define(function(require) {
+    'use strict';
+
+    var ExportConfigurableTemplateView;
+    var BaseView = require('oroui/js/app/views/base/view');
+    var $ = require('jquery');
+    var widgetManager = require('oroui/js/widget-manager');
+    var exportHandler = require('oroimportexport/js/export-handler');
+    var messenger = require('oroui/js/messenger');
+
+    ExportConfigurableTemplateView = BaseView.extend({
+        optionNames: BaseView.prototype.optionNames.concat(['downloadMessage', 'wid']),
+
+        events: {
+            'submit form': 'onFormSubmit'
+        },
+
+        onFormSubmit: function(e) {
+            e.preventDefault();
+            var currentTarget = e.currentTarget;
+            var downloadingMessage = messenger.notificationMessage('info', this.downloadMessage);
+
+            widgetManager.getWidgetInstance(this.wid, function(widget) {
+                widget.remove();
+            });
+
+            $.ajax({
+                url: currentTarget.action,
+                method: currentTarget.method,
+                data: $(currentTarget).serialize(),
+                errorHandlerMessage: false,
+                success: function(data) {
+                    if (typeof data.url === 'undefined') {
+                        exportHandler.handleDataTemplateDownloadErrorMessage();
+                    } else {
+                        window.open(data.url, '_blank');
+                    }
+                },
+                error: function() {
+                    exportHandler.handleDataTemplateDownloadErrorMessage();
+                },
+                complete: function() {
+                    downloadingMessage.close();
+                }
+            });
+        }
+    });
+
+    return ExportConfigurableTemplateView;
+});
