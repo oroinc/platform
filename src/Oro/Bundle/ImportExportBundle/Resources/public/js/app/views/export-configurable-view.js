d5cd344614221da0712cbb4739ea22c371a92376
Mateusz Janiszewski <mjaniszewski@oroinc.com>
2017-10-02 10:17:38 +0200

    BAP-15455: Remove script tags from configurableExport.html.twig (ImportExportBundle) (#13583)
    * BAP-15455: Remove script tags from configurableExport.html.twig (ImportExportBundle)
    - removed inline script
    
    * BAP-15455: Remove script tags from configurableExport.html.twig (ImportExportBundle)
    - code refactoring
    
    * BAP-15455: Remove script tags from configurableExport.html.twig (ImportExportBundle)
    - code refactoring

---
+++ src/Oro/Bundle/ImportExportBundle/Resources/public/js/app/views/export-configurable-view.js
@@ -0,0 +1,43 @@
+define(function(require) {
+    'use strict';
+
+    var ExportConfigurableView;
+    var BaseView = require('oroui/js/app/views/base/view');
+    var $ = require('jquery');
+    var widgetManager = require('oroui/js/widget-manager');
+    var exportHandler = require('oroimportexport/js/export-handler');
+    var mediator = require('oroui/js/mediator');
+
+    ExportConfigurableView = BaseView.extend({
+        optionNames: BaseView.prototype.optionNames.concat(['errorMessage', 'wid']),
+
+        events: {
+            'submit form': 'onFormSubmit'
+        },
+
+        onFormSubmit: function(e) {
+            e.preventDefault();
+            var currentTarget = e.currentTarget;
+
+            mediator.execute('showLoading');
+
+            $.ajax({
+                url: currentTarget.action,
+                method: currentTarget.method,
+                data: $(currentTarget).serialize(),
+                errorHandlerMessage: this.errorMessage,
+                success: function(data) {
+                    exportHandler.handleExportResponse(data);
+                    widgetManager.getWidgetInstance(this.wid, function(widget) {
+                        widget.remove();
+                    });
+                }.bind(this),
+                complete: function() {
+                    mediator.execute('hideLoading');
+                }
+            });
+        }
+    });
+
+    return ExportConfigurableView;
+});
