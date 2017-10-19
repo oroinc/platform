e741f93f5fbe6e1caf8b31d73954c2b58dc5ee8e
jakubderdziak <jakubderdziak@users.noreply.github.com>
2017-10-19 16:38:54 +0200

    BAP-15740: Documentation (#14172)
    * BAP-15740: Documentation
    - added documentation for Stale Jobs

--- src/Oro/Component/MessageQueue/Provider/JobConfigurationProviderInterface.php
+++ src/Oro/Component/MessageQueue/Provider/JobConfigurationProviderInterface.php
@@ -5,7 +5,10 @@ interface JobConfigurationProviderInterface
 {
     /**
      * @param string $jobName
-     * @return mixed
+     * @return integer|null number of seconds
+     *
+     * Method should return number of seconds, after which job should be considered as stale.
+     * If it returns null or -1 job will never be staled.
      */
     public function getTimeBeforeStaleForJobName($jobName);
 }
