c2a1bf8a2b6c2da5224e1eafd401a9e166bc2cdf
Aleksey Solonenko <asolonenko@oroinc.com>
2017-10-30 17:56:24 +0200

    CRM-8576: Add field to edit form. Add ability to save, edit. (#14339)
    - added field to form
    - added email list to string transformer
    - Renamed `splitEmailList` to `sharedGuestEmailList`
    - Fixed CS issues
    - Removed unneeded migration
    - Updated comments in unit tests

---
+++ src/Oro/Bundle/FormBundle/Form/DataTransformer/AbstractArrayToStringTransformer.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Oro\Bundle\FormBundle\Form\DataTransformer;
+
+use Symfony\Component\Form\DataTransformerInterface;
+
+abstract class AbstractArrayToStringTransformer implements DataTransformerInterface
+{
+    protected $delimiter;
+
+    private $filterUniqueValues;
+
+    /**
+     * @param string $delimiter
+     * @param boolean $filterUniqueValues
+     */
+    public function __construct($delimiter, $filterUniqueValues)
+    {
+        $this->delimiter = $delimiter;
+        $this->filterUniqueValues = $filterUniqueValues;
+    }
+
+    /**
+     * Transforms string to array
+     *
+     * @param string $stringValue
+     *
+     * @return array
+     */
+    protected function transformStringToArray($stringValue)
+    {
+        if (trim($this->delimiter)) {
+            $separator = trim($this->delimiter);
+        } else {
+            $separator = $this->delimiter;
+        }
+        $arrayValue = explode($separator, $stringValue);
+        return $this->filterArrayValue($arrayValue);
+    }
+
+    /**
+     * Transforms array to string
+     *
+     * @param array $arrayValue
+     *
+     * @return string
+     */
+    protected function transformArrayToString(array $arrayValue)
+    {
+        if (trim($this->delimiter)) {
+            $separator = trim($this->delimiter);
+        } else {
+            $separator = $this->delimiter;
+        }
+        return implode($separator, $this->filterArrayValue($arrayValue));
+    }
+
+    /**
+     * Trims all elements and apply unique filter if needed
+     *
+     * @param array $arrayValue
+     * @return array
+     */
+    private function filterArrayValue(array $arrayValue)
+    {
+        if ($this->filterUniqueValues) {
+            $arrayValue = array_unique($arrayValue);
+        }
+        $arrayValue = array_filter(array_map('trim', $arrayValue));
+        return array_values($arrayValue);
+    }
+}
