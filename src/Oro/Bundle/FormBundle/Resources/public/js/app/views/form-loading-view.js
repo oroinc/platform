cde4ea98a2f86e0bb94606aeee6b184dfb79e596
Paweł Kowalewski <pkowalewski@oroinc.com>
2017-10-31 08:14:35 +0100

    BB-12673: Create `form-loading-view` to control page loading (#14406)
    * BB-12673: Create `form-loading-view` to control page loading
    - add view file
    - apply view to product form
    
    * BB-12673: Create `form-loading-view` to control page loading
    - move view to FormBundle
    - use macro for applying module

---
+++ src/Oro/Bundle/FormBundle/Resources/public/js/app/views/form-loading-view.js
@@ -0,0 +1,11 @@
+define(function(require) {
+    'use strict';
+
+    var FormLoadingView;
+    var BaseView = require('oroui/js/app/views/base/view');
+
+    FormLoadingView = BaseView.extend({
+    });
+
+    return FormLoadingView;
+});
