5872e3fb6fcd25b4e9c4eaaec280a74fcb34ae12
Mateusz Janiszewski <mjaniszewski@oroinc.com>
2017-11-02 15:09:04 +0100

    BB-12675: Add loading mask to each loading section, load them and remove loading when section is loaded (#14485)
    * BB-12675: Add loading mask to each loading section, load them and remove loading when section is loaded
    - added section loading view
    - added lazy section loading
    
    * BB-12675: Add loading mask to each loading section, load them and remove loading when section is loaded
    - code refactoring
    
    * BB-12675: Add loading mask to each loading section, load them and remove loading when section is loaded
    - code refactoring
    
    * BB-12675: Add loading mask to each loading section, load them and remove loading when section is loaded
    - code refactoring
    
    * BB-12675: Add loading mask to each loading section, load them and remove loading when section is loaded
    - code refactoring
    
    * BB-12675: Add loading mask to each loading section, load them and remove loading when section is loaded
    - code refactoring

--- src/Oro/Bundle/FormBundle/Resources/public/js/app/views/form-loading-view.js
+++ src/Oro/Bundle/FormBundle/Resources/public/js/app/views/form-loading-view.js
@@ -3,24 +3,52 @@ define(function(require) {
 
     var FormLoadingView;
     var BaseView = require('oroui/js/app/views/base/view');
+    var FormSectionLoadingView = require('oroform/js/app/views/form-section-loading-view');
     var $ = require('jquery');
+    var _ = require('underscore');
+    var mediator = require('oroui/js/mediator');
 
     FormLoadingView = BaseView.extend({
         autoRender: true,
 
-        render: function() {
+        initialize: function() {
+            var self = this;
             var index = this.$(window.location.hash).parents('.responsive-section').index();
+
             index = index !== -1 ? index : 0;
+
+            this.$('.responsive-section').not(':nth-child(' + (index + 1) + ')').each(function(index, el) {
+                self.subview('form-section-loading-' + index, new FormSectionLoadingView({
+                    el: $(el)
+                }));
+            });
+
+            FormLoadingView.__super__.initialize.apply(this, arguments);
+        },
+
+        render: function() {
+            FormLoadingView.__super__.render.apply(this, arguments);
 
             this.$el.removeAttr('data-skip-input-widgets');
 
-            this.$('.responsive-section').not(':nth-child(' + (index + 1) + ')').each(function() {
-                $(this).attr({'data-layout': 'separate', 'data-skip-input-widgets': true});
+            this.initLayout()
+                .then(function() {
+                    setTimeout(this.loadSubviews.bind(this), 0);
+                }.bind(this));
+
+            return this;
+        },
+
+        loadSubviews: function() {
+            //TODO disable save button
+            var promises = _.map(this.subviews, function(view) {
+                return view.startLoading();
             });
 
-            this.initLayout();
-
-            FormLoadingView.__super__.render.apply(this);
+            return $.when.apply($, promises).done(function() {
+                mediator.trigger('page:afterChange');
+                //TODO enable save button
+            });
         }
     });
 
