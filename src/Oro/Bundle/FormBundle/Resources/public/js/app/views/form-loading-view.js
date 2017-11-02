c36b1852ca34b8a066c6d63f7a58528b80c5d8de
Paweł Kowalewski <pkowalewski@oroinc.com>
2017-11-02 05:06:58 +0100

    BB-12674: Load first section (or selected) and hide main loading laye… (#14430)
    * BB-12674: Load first section (or selected) and hide main loading layer after it
    - add js code
    
    * BB-12674: Load first section (or selected) and hide main loading layer after it
    - fixes after cr

--- src/Oro/Bundle/FormBundle/Resources/public/js/app/views/form-loading-view.js
+++ src/Oro/Bundle/FormBundle/Resources/public/js/app/views/form-loading-view.js
@@ -3,8 +3,25 @@ define(function(require) {
 
     var FormLoadingView;
     var BaseView = require('oroui/js/app/views/base/view');
+    var $ = require('jquery');
 
     FormLoadingView = BaseView.extend({
+        autoRender: true,
+
+        render: function() {
+            var index = this.$(window.location.hash).parents('.responsive-section').index();
+            index = index !== -1 ? index : 0;
+
+            this.$el.removeAttr('data-skip-input-widgets');
+
+            this.$('.responsive-section').not(':nth-child(' + (index + 1) + ')').each(function() {
+                $(this).attr({'data-layout': 'separate', 'data-skip-input-widgets': true});
+            });
+
+            this.initLayout();
+
+            FormLoadingView.__super__.render.apply(this);
+        }
     });
 
     return FormLoadingView;
