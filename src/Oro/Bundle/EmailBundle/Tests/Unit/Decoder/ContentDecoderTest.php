b53f7bf11d032d5b89472ababae1d5b74b6a7616
Andrew Fetisov <afetisov@oroinc.com>
2017-11-07 14:16:48 +0200

    BAP-13098: Imap email sync fails due to iconv_mime_decode error (#14538)
    - added option to avoid `illegal character` iconv decoding error

--- src/Oro/Bundle/EmailBundle/Tests/Unit/Decoder/ContentDecoderTest.php
+++ src/Oro/Bundle/EmailBundle/Tests/Unit/Decoder/ContentDecoderTest.php
@@ -77,6 +77,27 @@ sdfsdf=E8dffdsd</FONT></DIV>',
                 'expected' => '<DIV><FONT face=Arial 
 size=2>khsdkjdsljkdsklščščščřčřččřffdssfdsfdsdfsdfsdfčdffdsd</FONT></DIV>'
             ],
+            'UTF-8 with illegal char' => [
+                'string' => 'This is the Euro symbol: €.',
+                'contentTransferEncoding' => null,
+                'fromEncode' => 'UTF-8',
+                'toEncode' => 'ISO-8859-1',
+                'expected' => 'This is the Euro symbol: EUR.'
+            ],
+            'UTF-8 base64 with illegal char' => [
+                'string' => 'VGhpcyBpcyB0aGUgRXVybyBzeW1ib2w6IOKCrC4=',
+                'contentTransferEncoding' => 'base64',
+                'fromEncode' => 'UTF-8',
+                'toEncode' => 'ISO-8859-1',
+                'expected' => 'This is the Euro symbol: EUR.'
+            ],
+            'UTF-8 quoted-printable with illegal char' => [
+                'string' => 'This is the Euro symbol: €.',
+                'contentTransferEncoding' => 'quoted-printable',
+                'fromEncode' => 'UTF-8',
+                'toEncode' => 'ISO-8859-1',
+                'expected' => 'This is the Euro symbol: EUR.'
+            ],
         ];
     }
 }
