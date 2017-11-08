e37ada5a2b39cbeb94388f9e320c99438282bb5e
Eugene Chetverikov <EugeneC@users.noreply.github.com>
2017-11-08 16:19:33 +0200

    OEE-1540: Disabled territories are available at filter (#14542)
    * OEE-1540: Disabled territories are available at filter

---
+++ src/Oro/Bundle/DataGridBundle/Tests/Behat/Element/GridFilterManager.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Oro\Bundle\DataGridBundle\Tests\Behat\Element;
+
+use Behat\Mink\Element\NodeElement;
+
+class GridFilterManager extends FrontendGridFilterManager
+{
+    /**
+     * @param string $title
+     * @return NodeElement|mixed|null
+     */
+    protected function getFilterCheckbox($title)
+    {
+        $filterCheckbox = $this->find(
+            'css',
+            'li label[title="' . $title . '"] input[type=checkbox]'
+        );
+
+        self::assertNotNull($filterCheckbox, 'Can not find filter: ' . $title);
+
+        return $filterCheckbox;
+    }
+
+    public function close()
+    {
+        if (!$this->isVisible()) {
+            return;
+        }
+
+        $button = $this->elementFactory->createElement('GridFilterManagerButton');
+        $button->click();
+    }
+}
