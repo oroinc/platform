3ee23dade9e2148019f844546ceeb8e7c16cd769
Mykhailo Sulyma <msulima@oroinc.com>
2017-11-30 18:23:48 +0200

    BAP-15167: Consumer queue works about 30 minutes after application installed  - reverted changes

--- src/Oro/Bundle/EntityExtendBundle/ORM/RelationMetadataBuilder.php
+++ src/Oro/Bundle/EntityExtendBundle/ORM/RelationMetadataBuilder.php
@@ -380,11 +380,6 @@ class RelationMetadataBuilder implements MetadataBuilderInterface
      */
     private function setCascadeOptions(AssociationBuilder $builder, array $cascades)
     {
-        // option "noDetach" added possibility to disable force detach for relations
-        if (in_array('noDetach', $cascades, true)) {
-            $cascades = array_diff($cascades, ['noDetach', 'detach']);
-        }
-
         foreach ($cascades as $cascade) {
             $builder->{'cascade' . ucfirst($cascade)}();
         }
