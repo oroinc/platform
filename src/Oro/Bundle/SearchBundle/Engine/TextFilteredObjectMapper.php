f7b342e6c7d7f391c4b955f38881a75767f7a3a2
Ivan Shirko <ishirko@oroinc.com>
2017-12-05 13:28:40 +0300

    BB-12927: Product.shortdescription and Product.description: html tags are trimmed on the front end (#15300)
     - added TextFilteredDataProvider for OrmIndexer (WebsiteSearch)
     - added TextFilteredMapper for Orm backend index
     - all_text field now filtered in basic DataProvider
     - update unit tests

---
+++ src/Oro/Bundle/SearchBundle/Engine/TextFilteredObjectMapper.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Oro\Bundle\SearchBundle\Engine;
+
+use Oro\Bundle\SearchBundle\Query\Query;
+
+/**
+ * TODO: Should be removed after https://magecore.atlassian.net/browse/BB-12955
+ */
+class TextFilteredObjectMapper extends ObjectMapper
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function setDataValue($alias, $objectData, $fieldConfig, $value, $isArray = false)
+    {
+        $objectData = parent::setDataValue($alias, $objectData, $fieldConfig, $value, $isArray);
+
+        if (isset($objectData[Query::TYPE_TEXT])) {
+            foreach ($objectData[Query::TYPE_TEXT] as $fieldName => &$fieldValue) {
+                $fieldValue = $this->clearTextValue($fieldName, $fieldValue);
+            }
+        }
+
+        return $objectData;
+    }
+
+    /**
+     * Clear HTML in text fields
+     *
+     * {@inheritdoc}
+     */
+    protected function clearTextValue($fieldName, $value)
+    {
+        return $this->htmlTagHelper->stripTags((string)$value);
+    }
+}
