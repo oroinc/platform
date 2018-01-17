11869ce1b7c7ef607b2538cdfd8526881698281a
Dmitry Khrysev <dkhrysev@oroinc.com>
2018-01-17 18:45:00 +0200

    OIS-112: Provide fix for insecure query usages - updated checks for validating insecure parameters - fixed insecure parameters

--- src/Oro/Bundle/EmailBundle/Entity/Manager/EmailActivityApiEntityManager.php
+++ src/Oro/Bundle/EmailBundle/Entity/Manager/EmailActivityApiEntityManager.php
@@ -91,9 +91,10 @@ class EmailActivityApiEntityManager extends ApiEntityManager
                     $qb->andWhere(
                         $qb->expr()->neq(
                             QueryBuilderUtil::getSelectExprByAlias($qb, 'entityId'),
-                            $currentUser->getId()
+                            ':currentUserId'
                         )
                     );
+                    $qb->setParameter('currentUserId', $currentUser->getId());
                 }
             }
         );
