97edead702dbfa07d66e44505e219cc6fb3dbdb3
Hryhorii Hrebiniuk <hhrebiniuk@oroinc.com>
2018-01-18 15:21:06 +0200

    BAP-16371: Translate ExpressionLanguage\Node\ArrayNode (#16209)

---
+++ src/Oro/Bundle/ExpressionLanguageBundle/Resources/public/js/library/node/array-node.js
@@ -0,0 +1,97 @@
+define(function(require) {
+    'use strict';
+
+    var Node = require('oroexpressionlanguage/js/library/node/node');
+    var ConstantNode = require('oroexpressionlanguage/js/library/node/constant-node');
+
+    function ArrayNode() {
+        this.index = -1;
+        ArrayNode.__super__.call(this);
+    }
+
+    ArrayNode.prototype = Object.create(Node.prototype);
+    ArrayNode.__super__ = Node;
+
+    Object.assign(ArrayNode.prototype, {
+        constructor: ArrayNode,
+
+        /**
+         * Adds value and key nodes as sub-nodes of array node
+         *
+         * @param {Node} value
+         * @param {Node} [key]
+         */
+        addElement: function(value, key) {
+            if (key === void 0) {
+                key = new ConstantNode(++this.index);
+            }
+
+            this.nodes.push(key, value);
+        },
+
+        /**
+         * @inheritDoc
+         */
+        compile: function(compiler) {
+            compiler.raw('{');
+            this._compileArguments(compiler);
+            compiler.raw('}');
+        },
+
+        /**
+         * @inheritDoc
+         */
+        evaluate: function(functions, values) {
+            var result = {};
+            var pairs = this._getKeyValuePairs();
+            pairs.forEach(function(pair) {
+                var key = pair.key.evaluate(functions, values);
+                result[key] = pair.value.evaluate(functions, values);
+            });
+            return result;
+        },
+
+        /**
+         * @return {Array.<{key: string, value: *}>}
+         * @protected
+         */
+        _getKeyValuePairs: function() {
+            var pairs = [];
+            this.nodes.forEach(function(node, i) {
+                if (i % 2) {
+                    pairs[pairs.length - 1].value = node;
+                } else {
+                    pairs.push({key: node});
+                }
+            });
+            return pairs;
+        },
+
+        /**
+         * @param {Compiler} compiler
+         * @param {boolean} [withKeys]
+         * @protected
+         */
+        _compileArguments: function(compiler, withKeys) {
+            if (withKeys === void 0) {
+                withKeys = true;
+            }
+            var pairs = this._getKeyValuePairs();
+            pairs.forEach(function(pair, i) {
+                if (i !== 0) {
+                    compiler.raw(', ');
+                }
+
+                if (withKeys) {
+                    compiler
+                        .compile(pair.key)
+                        .raw(': ');
+                }
+
+                compiler.compile(pair.value);
+            });
+        }
+    });
+
+    return ArrayNode;
+});
---
+++ src/Oro/Bundle/ExpressionLanguageBundle/Tests/JS/library/node/array-nodeSpec.js
@@ -0,0 +1,27 @@
+define(function(require) {
+    'use strict';
+
+    var ArrayNode = require('oroexpressionlanguage/js/library/node/array-node');
+    var ConstantNode = require('oroexpressionlanguage/js/library/node/constant-node');
+    var Compiler = require('oroexpressionlanguage/js/library/compiler');
+
+    describe('oroexpressionlanguage/js/library/node/array-node', function() {
+        var arrayNode;
+
+        beforeEach(function() {
+            arrayNode = new ArrayNode();
+            arrayNode.addElement(new ConstantNode('a'), new ConstantNode('b'));
+            arrayNode.addElement(new ConstantNode('b'));
+        });
+
+        it('evaluation', function() {
+            expect(arrayNode.evaluate()).toEqual({'b': 'a', 0: 'b'});
+        });
+
+        it('compilation', function() {
+            var compiler = new Compiler({});
+            arrayNode.compile(compiler);
+            expect(compiler.getSource()).toBe('{"b": "a", 0: "b"}');
+        });
+    });
+});
