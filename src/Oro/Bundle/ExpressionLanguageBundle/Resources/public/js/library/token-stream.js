ffb11ea6ab31e73dbc36711ce1d00c3c30b5cd50
Dmitrii Shykalenko <dshykalenko@oroinc.com>
2018-01-22 16:29:04 +0200

    BAP-16362: Translate ExpressionLanguage\Token (#16236)

---
+++ src/Oro/Bundle/ExpressionLanguageBundle/Resources/public/js/library/token-stream.js
@@ -0,0 +1,83 @@
+define(function(require) {
+    'use strict';
+
+    var ExpressionSyntaxError = require('oroexpressionlanguage/js/library/expression-syntax-error');
+    var Token = require('oroexpressionlanguage/js/library/token');
+
+    /**
+     * @param {Array.<Token>} tokens     An array of tokens
+     * @param {string} expression
+     */
+    function TokenStream(tokens, expression) {
+        this.tokens = tokens;
+        this.current = tokens[0];
+        this.expression = expression || '';
+        this.position = 0
+    }
+
+    TokenStream.prototype = {
+        constructor: TokenStream,
+
+        /**
+         * Returns a string representation of the token stream.
+         *
+         * @return {string}
+         */
+        toString: function() {
+            return this.tokens.join('\n');
+        },
+
+        /**
+         * Sets the pointer to the next token and returns the old one.
+         */
+        next: function() {
+            if (this.position + 1 >= this.tokens.length) {
+                throw new ExpressionSyntaxError('Unexpected end of expression', this.current.cursor, this.expression);
+            }
+
+            ++this.position;
+
+            this.current = this.tokens[this.position];
+        },
+
+        /**
+         * Tests a token.
+         *
+         * @param {string} type  The type to test
+         * @param {string} [value]  The token value
+         * @param {string} [message]  The syntax error message
+         */
+        expect: function(type, value, message) {
+            var token = this.current;
+            if (!token.test(type, value)) {
+                message = message ? message + '. ' : '';
+                message += 'Unexpected token "' + token.type + '" of value "' + token.value + '"';
+                message += ' ("' + type + '" expected';
+                if (value) {
+                    message += ' with value "' + value + '"';
+                }
+                message += ')';
+                throw new ExpressionSyntaxError(message, token.cursor, this.expression);
+            }
+            this.next();
+        },
+
+        /**
+         * Checks if end of stream was reached.
+         *
+         * @return {boolean}
+         */
+        isEOF: function() {
+            return Token.EOF_TYPE === this.current.type;
+        },
+
+        /**
+         * @return {string}
+         */
+        getExpression: function() {
+            return this.expression;
+        }
+    };
+
+    return TokenStream;
+});
---
+++ src/Oro/Bundle/ExpressionLanguageBundle/Resources/public/js/library/token.js
@@ -0,0 +1,51 @@
+define(function() {
+    'use strict';
+
+    /**
+     * @param {string} type   The type of the token (Token.*_TYPE)
+     * @param {string|number|null} value  The token value
+     * @param {number} cursor The cursor position in the source
+     */
+    function Token(type, value, cursor) {
+        this.type = type;
+        this.value = value;
+        this.cursor = cursor;
+    }
+
+    Object.defineProperties(Token, {
+        EOF_TYPE: {value: 'end of expression'},
+        NAME_TYPE: {value: 'name'},
+        NUMBER_TYPE: {value: 'number'},
+        STRING_TYPE: {value: 'string'},
+        OPERATOR_TYPE: {value: 'operator'},
+        PUNCTUATION_TYPE: {value: 'punctuation'}
+    });
+
+
+    Token.prototype = {
+        constructor: Token,
+
+        /**
+         * Returns a string representation of the token.
+         *
+         * @return {string}
+         */
+        toString: function() {
+            var value = this.value !== null && this.value !== void 0 ? this.value : '';
+            return String(this.cursor).padStart(3) + ' ' + this.type.toUpperCase().padEnd(11) + ' ' + value;
+        },
+
+        /**
+         * Tests the current token for a type and/or a value.
+         *
+         * @param {string} type  The type to test
+         * @param {string} [value] The token value
+         * @return {boolean}
+         */
+        test: function(type, value) {
+            return this.type === type && (value === void 0 || this.value === value);
+        }
+    };
+
+    return Token;
+});
