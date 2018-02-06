029e453123b1df97ad75e705a1748745c61b814f
ValeriyYustunyk <ValeriyYustunyk@users.noreply.github.com>
2018-02-06 17:08:57 +0200

    BB-13542: Admin > Quick Search - special characters should be escaped (#16399)
    - create special char escape
    - implement JS tests

--- src/Oro/Bundle/UIBundle/Tests/JS/highlight-text-viewSpec.js
+++ src/Oro/Bundle/UIBundle/Tests/JS/highlight-text-viewSpec.js
@@ -114,5 +114,27 @@ define(function(require) {
                 expect(this.view.$(this.view.findFoundClass).length).toEqual(0);
             });
         });
+
+        describe('check find text escaping special characters', function() {
+            beforeEach(function() {
+                this.view = createView({
+                    highlightSelectors: ['.settings-title', '.group', '.field'],
+                    toggleSelectors: {
+                        '.field': '.fields',
+                        '.group': '.settings-content'
+                    }
+                });
+            });
+
+            it('escaping special characters', function() {
+                mediator.trigger(':highlight-text:update', '-\\[]{}()*+?.,\\^$#');
+                expect(this.view.findText).toEqual(/\-\\\[\]\{\}\(\)\*\+\?\.\,\\\^\$\#/gi);
+            });
+
+            it('escaping mixed string', function() {
+                mediator.trigger(':highlight-text:update', 'Grou#$p 2.F\ield 2*.1');
+                expect(this.view.findText).toEqual(/Grou\#\$p 2\.Field 2\*\.1/gi);
+            });
+        });
     });
 });
