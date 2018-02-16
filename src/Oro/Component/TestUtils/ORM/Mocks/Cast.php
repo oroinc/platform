bb50e8f3fe0c99f2be14536dd07bc1e6670e4a75
Dmitry Khrysev <dkhrysev@oroinc.com>
2018-02-16 13:20:42 +0200

    OIS-10: fixed broken namespaces in Oro code, improved config.neon

--- src/Oro/Component/TestUtils/ORM/Mocks/Cast.php
+++ src/Oro/Component/TestUtils/ORM/Mocks/Cast.php
@@ -2,7 +2,7 @@
 /**
  * Declare custom SQL functions available in the Mock platform
  */
-namespace Oro\ORM\Query\AST\Platform\Functions\Mock;
+namespace Oro\Component\TestUtils\ORM\Mocks;
 
 use Oro\ORM\Query\AST\Platform\Functions\Mysql\Cast as BaseCast;
 
