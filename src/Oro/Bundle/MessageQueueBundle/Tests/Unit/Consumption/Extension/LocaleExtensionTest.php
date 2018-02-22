17e8f18b4d43147003522490cb61a480eb9d7040
Yurii Muratov <ymuratov@oroinc.com>
2018-02-22 16:08:14 +0200

    BAP-16459: EmailTemplate translations doesn't work (#16899)
    - add locale message queue extension
    - class doc

---
+++ src/Oro/Bundle/MessageQueueBundle/Tests/Unit/Consumption/Extension/LocaleExtensionTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Oro\Bundle\MessageQueueBundle\Tests\Unit\Consumption\Extension;
+
+use Gedmo\Translatable\TranslatableListener;
+use Oro\Bundle\LocaleBundle\Model\LocaleSettings;
+use Oro\Bundle\MessageQueueBundle\Consumption\Extension\LocaleExtension;
+use Oro\Component\MessageQueue\Consumption\Context;
+
+class LocaleExtensionTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSetLocaleOnBeforeReceive()
+    {
+        $localeSettings = $this->createMock(LocaleSettings::class);
+        $translatableListener = new TranslatableListener();
+
+        $localeSettings->expects($this->once())
+            ->method('getLocale')
+            ->willReturn('fr');
+
+        $localeSettings->expects($this->once())
+            ->method('getLanguage')
+            ->willReturn('FR_fr');
+
+        $extension = new LocaleExtension($localeSettings, $translatableListener);
+        $extension->onBeforeReceive($this->createMock(Context::class));
+
+        $this->assertEquals('fr', \Locale::getDefault());
+        $this->assertEquals('FR_fr', $translatableListener->getListenerLocale());
+    }
+}
