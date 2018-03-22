25883f4122ac85261ffb5f076562bca102774626
Galaev Igor <igalayev@oroinc.com>
2018-03-22 18:43:29 +0200

    BAP-16784: Fix form types issues for platform application - fix tests

--- src/Oro/Bundle/SegmentBundle/Form/Type/SegmentFilterBuilderType.php
+++ src/Oro/Bundle/SegmentBundle/Form/Type/SegmentFilterBuilderType.php
@@ -65,6 +65,14 @@ class SegmentFilterBuilderType extends AbstractType
     /**
      * {@inheritdoc}
      */
+    public function getName()
+    {
+        return $this->getBlockPrefix();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     public function getBlockPrefix()
     {
         return self::NAME;
