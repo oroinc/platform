858616e99f6c4f9e5b118073202ee34d7b454c59
Eugene Chetverikov <EugeneC@users.noreply.github.com>
2018-04-04 17:00:36 +0300

    BAP-16732: Consumers should process Schema Update status (#17224)
    - removes only the entity proxies and metadata from the cache
    - do not warmup cache during boot of the OroEntityExtendBundle while update config is in progress
     - added stubs for unit testing
     - updated unit tests

---
+++ src/Oro/Bundle/EntityExtendBundle/Tests/Unit/Stub/Oro/Bundle/EntityExtendBundle/Tools/ExtendClassLoadingUtils.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Oro\Bundle\EntityExtendBundle\Tools;
+
+use Oro\Bundle\EntityExtendBundle\Tests\Unit\Stub\AbstractStub;
+
+class ExtendClassLoadingUtils extends AbstractStub
+{
+}
---
+++ src/Oro/Bundle/EntityExtendBundle/Tests/Unit/Stub/Symfony/Component/Process/ProcessBuilder.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Symfony\Component\Process;
+
+use Oro\Bundle\EntityExtendBundle\Tests\Unit\Stub\AbstractStub;
+
+class ProcessBuilder extends AbstractStub
+{
+}
