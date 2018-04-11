b2eedefe88474b780c817a30a2f12fb4a7d54b40
Igor Galayev <igalayev@oroinc.com>
2018-04-11 17:35:46 +0300

    BAP-16867: Fix failed tests - set test session listener class using compiler pass

--- src/Oro/Bundle/TestFrameworkBundle/OroTestFrameworkBundle.php
+++ src/Oro/Bundle/TestFrameworkBundle/OroTestFrameworkBundle.php
@@ -4,6 +4,7 @@ namespace Oro\Bundle\TestFrameworkBundle;
 
 use Oro\Bundle\TestFrameworkBundle\DependencyInjection\Compiler\CheckReferenceCompilerPass;
 use Oro\Bundle\TestFrameworkBundle\DependencyInjection\Compiler\TagsInformationPass;
+use Oro\Bundle\TestFrameworkBundle\DependencyInjection\Compiler\TestSessionListenerCompilerPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -16,5 +17,6 @@ class OroTestFrameworkBundle extends Bundle
     {
         $container->addCompilerPass(new TagsInformationPass());
         $container->addCompilerPass(new CheckReferenceCompilerPass());
+        $container->addCompilerPass(new TestSessionListenerCompilerPass());
     }
 }
