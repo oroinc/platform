05315542b81140a128ffbb93182f343c916169a3
Viacheslav Chekanovskyi <vchekanovskyi@oroinc.com>
2018-05-16 16:10:21 +0300

    BB-14258: Refactor js - fix tests

--- src/Oro/Bundle/FormBundle/Resources/public/js/app/views/default-field-value-view.js
+++ src/Oro/Bundle/FormBundle/Resources/public/js/app/views/default-field-value-view.js
@@ -3,18 +3,35 @@ define(function(require) {
 
     var DefaultFieldValueView;
     var $ = require('jquery');
+    var _ = require('underscore');
     var tinyMCE = require('tinymce/tinymce');
     var BaseView = require('oroui/js/app/views/base/view');
 
     DefaultFieldValueView = BaseView.extend({
+        /**
+         * @inheritDoc
+         * @property
+         */
         autoRender: true,
 
+        /**
+         * @inheritDoc
+         */
         optionNames: BaseView.prototype.optionNames.concat(['prepareTinymce', 'fieldSelector', 'checkboxSelector']),
 
+        /**
+         * @property {Boolean}
+         */
         prepareTinymce: true,
 
+        /**
+         * @property {String}
+         */
         checkboxSelector: '[data-role="changeUseDefault"]',
 
+        /**
+         * @inheritDoc
+         */
         events: {
             'change [data-role="changeUseDefault"]': 'onDefaultCheckboxChange'
         },
@@ -26,16 +43,24 @@ define(function(require) {
             DefaultFieldValueView.__super__.constructor.apply(this, arguments);
         },
 
+        /**
+         * @inheritDoc
+         * @returns {*}
+         */
         render: function() {
             if (this.$(this.checkboxSelector).is(':checked')) {
                 this.$(this.checkboxSelector).filter(':checked').each(_.bind(function(i, e) {
                     this._setFieldsState($(e), true);
-                }, this))
+                }, this));
             }
 
             return DefaultFieldValueView.__super__.render.apply(this, arguments);
         },
 
+        /**
+         * On change checkbox handler
+         * @param e
+         */
         onDefaultCheckboxChange: function(e) {
             var $currentTarget = $(e.currentTarget);
             var value = $currentTarget.is(':checked');
@@ -43,6 +68,13 @@ define(function(require) {
             this._setFieldsState($currentTarget, value);
         },
 
+        /**
+         * Change field status
+         *
+         * @param $element
+         * @param value
+         * @private
+         */
         _setFieldsState: function($element, value) {
             var $controls = $element.parents('.controls');
             var valueEls = $controls.find(':input, a.btn, button')
@@ -65,6 +97,12 @@ define(function(require) {
             }
         },
 
+        /**
+         * Set enable/disable tinymce field
+         *
+         * @param $textareas
+         * @private
+         */
         _prepareTinymce: function($textareas) {
             $textareas.each(function(i, el) {
                 var editor = tinyMCE.get(el.id);
