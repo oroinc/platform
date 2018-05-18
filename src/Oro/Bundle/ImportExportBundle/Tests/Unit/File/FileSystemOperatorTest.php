4108d9bee7e6c70cf6bf2424ac3ab38204d8a7fb
Alexander Nezdoiminoga <alex@magecore.com>
2018-05-18 16:53:13 +0300

    BAP-16955: Multiple failed unit tests if run it in enviroment with restricted permission (#18118)

--- src/Oro/Bundle/ImportExportBundle/Tests/Unit/File/FileSystemOperatorTest.php
+++ src/Oro/Bundle/ImportExportBundle/Tests/Unit/File/FileSystemOperatorTest.php
@@ -6,52 +6,56 @@ use Oro\Bundle\ImportExportBundle\File\FileSystemOperator;
 
 class FileSystemOperatorTest extends \PHPUnit_Framework_TestCase
 {
-    /**
-     * @var string
-     */
+    /** @var string */
     protected $existingDir = 'existing';
 
-    /**
-     * @var string
-     */
+    /** @var string */
     protected $newDir = 'new';
 
-    /**
-     * @var string
-     */
+    /** @var string */
     protected $cacheDir;
 
     protected function setUp()
     {
-        $this->cacheDir = __DIR__ . '/fixtures';
+        $this->cacheDir = sys_get_temp_dir() . '/FileSystemOperatorTest';
+        \mkdir($this->cacheDir);
+        \mkdir($this->cacheDir . DIRECTORY_SEPARATOR . $this->existingDir);
+        \copy(
+            __DIR__ . '/fixtures' . DIRECTORY_SEPARATOR . $this->existingDir . DIRECTORY_SEPARATOR . 'file.csv',
+            $this->cacheDir . DIRECTORY_SEPARATOR . $this->existingDir . DIRECTORY_SEPARATOR . 'file.csv'
+        );
     }
 
     protected function tearDown()
     {
         $newDirPath = $this->cacheDir . DIRECTORY_SEPARATOR . $this->newDir;
         if (is_dir($newDirPath)) {
-            @rmdir($newDirPath);
+            @\rmdir($newDirPath);
         }
+        @\unlink($this->cacheDir . DIRECTORY_SEPARATOR . $this->existingDir . DIRECTORY_SEPARATOR . 'file.csv');
+        @\rmdir($this->cacheDir . DIRECTORY_SEPARATOR . $this->existingDir);
+        @\rmdir($this->cacheDir);
     }
 
     /**
      * @dataProvider dirDataProvider
+     *
      * @param string $dir
      */
     public function testGetTemporaryDirectory($dir)
     {
         $fs = new FileSystemOperator($this->cacheDir, $dir);
         $expectedDir = $this->cacheDir . DIRECTORY_SEPARATOR . $dir;
-        $this->assertEquals($expectedDir, $fs->getTemporaryDirectory());
-        $this->assertFileExists($expectedDir);
+        self::assertEquals($expectedDir, $fs->getTemporaryDirectory());
+        self::assertFileExists($expectedDir);
     }
 
     public function dirDataProvider()
     {
-        return array(
-            array($this->existingDir),
-            array($this->newDir),
-        );
+        return [
+            [$this->existingDir],
+            [$this->newDir],
+        ];
     }
 
     /**
@@ -67,15 +71,15 @@ class FileSystemOperatorTest extends \PHPUnit_Framework_TestCase
     public function testGetTemporaryFile()
     {
         $fs = new FileSystemOperator($this->cacheDir, $this->existingDir);
-        $this->assertInstanceOf('\SplFileObject', $fs->getTemporaryFile('file.csv'));
+        self::assertInstanceOf('\SplFileObject', $fs->getTemporaryFile('file.csv'));
     }
 
     public function testGenerateTemporaryFileName()
     {
         $fs = new FileSystemOperator($this->cacheDir, $this->existingDir);
         $fileName = $fs->generateTemporaryFileName('test', 'ext');
-        $this->assertStringEndsWith('ext', $fileName);
-        $this->assertContains(DIRECTORY_SEPARATOR . 'test', $fileName);
-        $this->assertContains(date('Y_m_d_H_i_'), $fileName);
+        self::assertStringEndsWith('ext', $fileName);
+        self::assertContains(DIRECTORY_SEPARATOR . 'test', $fileName);
+        self::assertContains(date('Y_m_d_H_i_'), $fileName);
     }
 }
