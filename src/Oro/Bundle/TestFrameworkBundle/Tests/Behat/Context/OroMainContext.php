1968958b30a3505bfb37101b5a5c7a1efbf574c1
Igor Galayev <igalayev@oroinc.com>
2018-05-21 13:17:13 +0300

    CRM-8977: Failed build on master package/crm/src/Oro/Bundle/ContactBundle/Tests/Behat/Features/contacts_crud.feature:18 - add skip of wait for ajax on afterStep hook if next step checks flash messages - add comment describing the goal of added functionality

--- src/Oro/Bundle/TestFrameworkBundle/Tests/Behat/Context/OroMainContext.php
+++ src/Oro/Bundle/TestFrameworkBundle/Tests/Behat/Context/OroMainContext.php
@@ -5,6 +5,8 @@ namespace Oro\Bundle\TestFrameworkBundle\Tests\Behat\Context;
 use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Behat\Hook\Scope\AfterStepScope;
 use Behat\Behat\Hook\Scope\BeforeStepScope;
+use Behat\Gherkin\Node\FeatureNode;
+use Behat\Gherkin\Node\StepNode;
 use Behat\Gherkin\Node\TableNode;
 use Behat\Mink\Driver\Selenium2Driver;
 use Behat\Mink\Element\NodeElement;
@@ -184,7 +186,10 @@ class OroMainContext extends MinkContext implements
      */
     public function afterStep(AfterStepScope $scope)
     {
-        if ($this->skipWait || !$this->getMink()->isSessionStarted()) {
+        if ($this->skipWait
+            || !$this->getMink()->isSessionStarted()
+            || $this->isNextStepNeedSkip($scope->getStep(), $scope->getFeature())
+        ) {
             return;
         }
 
@@ -220,6 +225,32 @@ class OroMainContext extends MinkContext implements
                 sprintf('There is an error message "%s" found on the page, something went wrong', $error->getText())
             );
         }
+    }
+
+    /**
+     * Returns true if the next step checks for a flash message to bypass wait for ajax on afterStep hook.
+     *
+     * This helps to overcome a delay introduced by ajax calls after current step execution which prevent flash
+     * message check (executed by the next step) to be done before flash message disappears.
+     *
+     * @param StepNode $currentStep
+     * @param FeatureNode $feature
+     * @return bool
+     */
+    private function isNextStepNeedSkip(StepNode $currentStep, FeatureNode $feature): bool
+    {
+        $isNextStep = false;
+        foreach ($feature->getScenarios() as $scenario) {
+            foreach ($scenario->getSteps() as $step) {
+                if ($isNextStep) {
+                    return preg_match(self::SKIP_WAIT_PATTERN, $step->getText());
+                }
+
+                $isNextStep = $currentStep->getLine() === $step->getLine();
+            }
+        }
+
+        return false;
     }
 
     /**
