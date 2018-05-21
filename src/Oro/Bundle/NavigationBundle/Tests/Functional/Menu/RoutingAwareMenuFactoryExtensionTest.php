ed8dd63db328e4c63cb65c1cad38ee9d47d297e6
Igor Galayev <igalayev@oroinc.com>
2018-05-21 12:28:04 +0300

    BAP-17042: Double 'app_dev.php' in link href to contact-us form - fix test

--- src/Oro/Bundle/NavigationBundle/Tests/Functional/Menu/RoutingAwareMenuFactoryExtensionTest.php
+++ src/Oro/Bundle/NavigationBundle/Tests/Functional/Menu/RoutingAwareMenuFactoryExtensionTest.php
@@ -26,7 +26,7 @@ class RoutingAwareMenuFactoryExtensionTest extends WebTestCase
 
     public function testBuildOptions()
     {
-        $router = self::getContainer()->get('router.default');
+        $router = self::getContainer()->get('router');
         $router->getContext()->setBaseUrl('index.php');
         $this->assertStringStartsWith(
             self::INDEX_PHP_FILE,
