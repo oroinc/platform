5e9db0f46723275b57e0e5baf987ca05b11759f9
Hryhorii Hrebiniuk <hhrebiniuk@oroinc.com>
2018-06-04 16:04:34 +0300

    BAP-16877: Multi-select editor does not have save and cancel buttons (#17810)

--- src/Oro/Bundle/FormBundle/Resources/public/js/app/views/editor/multi-checkbox-editor-view.js
+++ src/Oro/Bundle/FormBundle/Resources/public/js/app/views/editor/multi-checkbox-editor-view.js
@@ -92,20 +92,66 @@ define(function(require) {
                 autoOpen: true,
                 classes: _.result(this, 'className'),
                 header: '',
-                height: 'auto',
+                height: '',
                 position: {
                     my: 'left top',
                     at: 'left top',
                     of: this.$el
                 },
                 beforeclose: function() {
-                    return false;
-                }
+                    if (this.prestine) {
+                        this.trigger('cancelAction');
+                    }
+                }.bind(this)
             }).multiselectfilter({
                 label: '',
                 placeholder: __('oro.form.inlineEditing.multi_checkbox_editor.filter.placeholder'),
                 autoReset: true
             });
+
+            this.multiselect.multiselect('getMenu').find('label')
+                .bindFirst('keydown' + this.eventNamespace(), function(event) {
+                    this.prestine = false;
+
+                    switch (event.keyCode) {
+                        case this.ENTER_KEY_CODE:
+                            event.stopImmediatePropagation();
+                            event.preventDefault();
+
+                            this.multiselect.multiselect('close');
+
+                            this.onGenericEnterKeydown(event);
+                            break;
+                        case this.TAB_KEY_CODE:
+                            event.stopImmediatePropagation();
+                            event.preventDefault();
+
+                            this.multiselect.multiselect('close');
+
+                            this.onGenericTabKeydown(event);
+                            break;
+                        case this.ESCAPE_KEY_CODE:
+                            event.stopImmediatePropagation();
+                            event.preventDefault();
+
+                            this.multiselect.multiselect('close');
+
+                            this.onGenericEscapeKeydown(event);
+                            break;
+                    }
+
+                    this.onGenericArrowKeydown(event);
+                }.bind(this));
+
+            this.multiselect.multiselectfilter('instance').input
+                .on('keydown' + this.eventNamespace(), function(event) {
+                    this.prestine = false;
+
+                    this.onGenericEnterKeydown(event);
+                    this.onGenericTabKeydown(event);
+                    this.onGenericArrowKeydown(event);
+                    this.onGenericEscapeKeydown(event);
+                }.bind(this));
         },
 
         onUpdatePosition: function() {
@@ -114,8 +160,7 @@ define(function(require) {
             }
         },
 
-        getModelValue: function() {
-            var value = this.model.get(this.fieldName);
+        parseRawValue: function(value) {
             if (_.isString(value)) {
                 value = JSON.parse(value);
             } else if (_.isArray(value)) {
@@ -123,7 +168,7 @@ define(function(require) {
                     return item !== '';
                 });
             } else if (_.isNull(value) || value === void 0) {
-                return [];
+                value = [];
             }
             return value;
         },
