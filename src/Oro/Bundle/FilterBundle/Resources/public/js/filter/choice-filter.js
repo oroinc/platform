66f5773921bdf1273ec39e8e7736edeaab735445
Viacheslav Chekanovskyi <vchekanovskyi@oroinc.com>
2018-06-07 16:15:52 +0300

    BB-14353: Form elements - refactoring filters

--- src/Oro/Bundle/FilterBundle/Resources/public/js/filter/choice-filter.js
+++ src/Oro/Bundle/FilterBundle/Resources/public/js/filter/choice-filter.js
@@ -174,10 +174,10 @@ define(function(require) {
                 return;
             }
             // update left and right margins of value field frame
-            var leftWidth = this.$('.choice-filter .dropdown-toggle').outerWidth();
-            var rightWidth = this.$('.filter-update').outerWidth();
-            valueFrame.css('margin-left', leftWidth);
-            valueFrame.css('padding-right', rightWidth);
+            // var leftWidth = this.$('.choice-filter .dropdown-toggle').outerWidth();
+            // var rightWidth = this.$('.filter-update').outerWidth();
+            // valueFrame.css('margin-left', leftWidth);
+            // valueFrame.css('padding-right', rightWidth);
             // update class of criteria dropdown
             type = this.$(this.criteriaValueSelectors.type).val();
             isEmptyType = this.isEmptyType(type);
