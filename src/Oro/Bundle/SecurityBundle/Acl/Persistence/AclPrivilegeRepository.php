c4e089c68493c46a52df91379bef6c3226301bb1
Dmitry Khrysev <dkhrysev@oroinc.com>
2018-09-26 20:02:22 +0300

    BB-15168: Impossible to proceed to checkout with the custom Customer User Role - fixed default permissions for checkout* forckflows - fixed default selected permission for transitions/fields - fixed IsGrantedWorkflowTransition for start transitions with new empty entity

--- src/Oro/Bundle/SecurityBundle/Acl/Persistence/AclPrivilegeRepository.php
+++ src/Oro/Bundle/SecurityBundle/Acl/Persistence/AclPrivilegeRepository.php
@@ -363,10 +363,12 @@ class AclPrivilegeRepository
         $acl = $this->findAclByOid($acls, $oid);
         $this->addAclPermissions($sid, $field, $privilege, $allowedPermissions, $extension, null, $acl);
 
-        // add default permission for not found in db privileges. By default it should be the System access level.
+        // add default permission for not found in db privileges. By default it should be the maximum access level.
         foreach ($allowedPermissions as $permission) {
             if (!$privilege->hasPermission($permission)) {
-                $privilege->addPermission(new AclPermission($permission, AccessLevel::SYSTEM_LEVEL));
+                $accessLevels = $extension->getAccessLevelNames($oid, $permission);
+                $maxAccessLevel = max(array_keys($accessLevels));
+                $privilege->addPermission(new AclPermission($permission, $maxAccessLevel));
             }
         }
     }
