60feb2647b6cc62fed22db46f7d4ea0a1ed02e5f
Ruslan Panasiuk <rpanasiuk@oroinc.com>
2018-10-12 20:04:00 +0300

    BAP-16104: Escape Twig code in default email templates - print all templates data

--- src/Oro/Bundle/EmailBundle/Tests/Behat/Context/EmailContext.php
+++ src/Oro/Bundle/EmailBundle/Tests/Behat/Context/EmailContext.php
@@ -9,6 +9,7 @@ use Oro\Bundle\EmailBundle\Mailer\DirectMailer;
 use Oro\Bundle\EmailBundle\Tests\Behat\Mock\Mailer\DirectMailerDecorator;
 use Oro\Bundle\TestFrameworkBundle\Behat\Context\AssertTrait;
 use Oro\Bundle\TestFrameworkBundle\Behat\Context\OroFeatureContext;
+use Symfony\Component\VarDumper\VarDumper;
 
 class EmailContext extends OroFeatureContext implements KernelAwareContext
 {
@@ -56,6 +57,7 @@ class EmailContext extends OroFeatureContext implements KernelAwareContext
                 ['From', 'To', 'Cc', 'Bcc', 'Subject', 'Body']
             );
 
+            VarDumper::dump(implode(' ', $data));
             $found = (bool) preg_match($pattern, implode(' ', $data));
             if ($found !== false) {
                 break;
