ba347b2a386bfc67e5cf93c6baf47f07dde2ca8f
Vova Soroka <vsoroka@oroinc.com>
2018-10-12 09:10:18 +0300

    BAP-16692: Replace form aliases by FQCN in embedded forms (#20325)
    - remove unused data fixtures for functional tests

--- src/Oro/Bundle/EmbeddedFormBundle/Tests/Functional/DataFixtures/LoadEmbeddedFormData.php
+++ src/Oro/Bundle/EmbeddedFormBundle/Tests/Functional/DataFixtures/LoadEmbeddedFormData.php
@@ -2,22 +2,40 @@
 
 namespace Oro\Bundle\EmbeddedFormBundle\Tests\Functional\DataFixtures;
 
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Oro\Bundle\EmbeddedFormBundle\Entity\EmbeddedForm;
 use Oro\Bundle\EmbeddedFormBundle\Tests\Functional\Stubs\EmbeddedFormStub;
+use Oro\Bundle\TestFrameworkBundle\Tests\Functional\DataFixtures\LoadOrganization;
 
-class LoadEmbeddedFormData extends AbstractEmbeddedFormDataFixture
+class LoadEmbeddedFormData extends AbstractFixture implements DependentFixtureInterface
 {
-    const EMBEDDED_FORM = 'embedded_form';
+    public const EMBEDDED_FORM = 'embedded_form';
 
     /**
      * {@inheritdoc}
      */
-    protected function getEmbeddedFormData(): array
+    public function load(ObjectManager $manager)
     {
-        return [
-            [
-                'reference' => self::EMBEDDED_FORM,
-                'formType' => EmbeddedFormStub::class
-            ]
-        ];
+        $embeddedForm = new EmbeddedForm();
+        $embeddedForm->setFormType(EmbeddedFormStub::class);
+        $embeddedForm->setCss('input { color: red; }');
+        $embeddedForm->setSuccessMessage('Form has been submitted successfully');
+        $embeddedForm->setTitle('Send Feedback');
+        $embeddedForm->setOwner($this->getReference('organization'));
+
+        $this->addReference(self::EMBEDDED_FORM, $embeddedForm);
+
+        $manager->persist($embeddedForm);
+        $manager->flush();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDependencies()
+    {
+        return [LoadOrganization::class];
     }
 }
