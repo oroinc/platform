21242f441bfec0ad0aa8677589bd0570a450f996
Igor Galayev <igalayev@oroinc.com>
2018-10-23 17:48:19 +0300

    BAP-17733: Address type is not translatable in Address Book - add class descriptions

--- src/Oro/Bundle/AddressBundle/Entity/Repository/IdentityAwareTranslationRepositoryInterface.php
+++ src/Oro/Bundle/AddressBundle/Entity/Repository/IdentityAwareTranslationRepositoryInterface.php
@@ -2,6 +2,10 @@
 
 namespace Oro\Bundle\AddressBundle\Entity\Repository;
 
+/**
+ * Interface for dictionary entity repositories which adds support of translating entities based on data from message
+ * catalogue for the default locale.
+ */
 interface IdentityAwareTranslationRepositoryInterface
 {
     /**
--- src/Oro/Bundle/TranslationBundle/Entity/Repository/TranslationRepositoryInterface.php
+++ src/Oro/Bundle/TranslationBundle/Entity/Repository/TranslationRepositoryInterface.php
@@ -2,6 +2,10 @@
 
 namespace Oro\Bundle\TranslationBundle\Entity\Repository;
 
+/**
+ * Interface for translation dictionary entity repositories which adds support of translation entities based on data
+ * from message catalogue for the given locale.
+ */
 interface TranslationRepositoryInterface
 {
     /**
