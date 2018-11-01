e8b1be462dd76da142ede18ebdbf662a8cc7f56c
Vova Soroka <vsoroka@oroinc.com>
2018-11-01 20:35:13 +0200

    BAP-16436: Not possible to install application on DB with utf8mb4

--- src/Oro/Bundle/PlatformBundle/DependencyInjection/DbalConfigurationLoader.php
+++ src/Oro/Bundle/PlatformBundle/DependencyInjection/DbalConfigurationLoader.php
@@ -6,7 +6,9 @@ use Oro\Component\DependencyInjection\ExtendedContainerBuilder;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 /**
- * Checks whether an additional unnamed DBAL configuration exists,
+ * Sets "database_charset" parameter if it is not set yet. The charset is extracted
+ * from a default DBAL connection or set to utf8 if the charset of the default connection is unknown.
+ * Also checks whether an additional unnamed DBAL configuration exists,
  * and if so, adds it for all DBAL connections that have the same driver, DB name, host and port
  * as a default DBAL connection.
  * This is required because DoctrineBundle adds unnamed DBAL configuration only to a default DBAL connection.
@@ -32,53 +34,260 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
  *                      charset: utf8mb4
  *                      collate: utf8mb4_unicode_ci
  * </code>
- * this assitional configuration will be added just before the unnamed DBAL configuration
+ * this additional configuration will be added just before the unnamed DBAL configuration
  * to be able to override any of the added options for a specific DBAL connection.
+ * @SuppressWarnings(PHPMD.ExcessiveClassComplexity)
  */
 class DbalConfigurationLoader
 {
+    const DATABASE_CHARSET   = 'database_charset';
+    const DOCTRINE           = 'doctrine';
+    const DBAL               = 'dbal';
+    const DEFAULT_CONNECTION = 'default_connection';
+    const CONNECTIONS        = 'connections';
+    const CONNECTION         = 'connection';
+    const DRIVER             = 'driver';
+    const DBNAME             = 'dbname';
+    const HOST               = 'host';
+    const PORT               = 'port';
+    const CHARSET            = 'charset';
+
     /**
      * @param ContainerBuilder $container
      */
     public static function load(ContainerBuilder $container)
     {
-        if (!$container instanceof ExtendedContainerBuilder) {
-            return;
-        }
-        $doctrineConfig = $container->getExtensionConfig('doctrine');
+        $doctrineConfig = $container->getExtensionConfig(self::DOCTRINE);
         $commonConfig = self::getCommonDbalConfig($doctrineConfig);
-        if (empty($commonConfig)) {
-            return;
-        }
         $defaultConnectionName = self::getDefaultDbalConnectionName($doctrineConfig);
-        if (!$defaultConnectionName) {
-            return;
+        $defaultConnectionKey = $defaultConnectionName
+            ? self::getDbalConnectionKey($doctrineConfig, $defaultConnectionName)
+            : null;
+
+        if (!$container->hasParameter(self::DATABASE_CHARSET)) {
+            $container->setParameter(
+                self::DATABASE_CHARSET,
+                self::getDefaultCharset($doctrineConfig, $commonConfig, $defaultConnectionName)
+            );
         }
-        $defaultConnectionKey = self::getDbalConnectionKey($doctrineConfig, $defaultConnectionName);
-        if (!$defaultConnectionKey) {
-            return;
+
+        if ($container instanceof ExtendedContainerBuilder
+            && !empty($commonConfig)
+            && $defaultConnectionName
+            && $defaultConnectionKey
+        ) {
+            $doctrineConfig = self::applyCommonConfig(
+                $doctrineConfig,
+                $commonConfig,
+                $defaultConnectionName,
+                $defaultConnectionKey
+            );
+            $container->setExtensionConfig(self::DOCTRINE, $doctrineConfig);
         }
+    }
+
+    /**
+     * @param array       $doctrineConfig
+     * @param array       $commonConfig
+     * @param string|null $defaultConnectionName
+     *
+     * @return string
+     */
+    private static function getDefaultCharset(
+        $doctrineConfig,
+        $commonConfig,
+        $defaultConnectionName
+    ) {
+        $charset = self::getCommonOptionValue($commonConfig, self::CHARSET);
+        if ($defaultConnectionName && (!$charset || self::isParameter($charset))) {
+            $charset = self::getDbalConnectionOptionValue($doctrineConfig, $defaultConnectionName, self::CHARSET);
+        }
+        if (self::isParameter($charset)) {
+            $charset = null;
+        }
+
+        return $charset ?: 'utf8';
+    }
+
+    /**
+     * @param mixed $optionValue
+     *
+     * @return bool
+     */
+    private static function isParameter($optionValue)
+    {
+        return
+            is_string($optionValue)
+            && strpos($optionValue, '%') === 0;
+    }
+
+    /**
+     * @param array  $doctrineConfig
+     * @param array  $commonConfig
+     * @param string $defaultConnectionName
+     * @param string $defaultConnectionKey
+     *
+     * @return array
+     */
+    private static function applyCommonConfig(
+        $doctrineConfig,
+        $commonConfig,
+        $defaultConnectionName,
+        $defaultConnectionKey
+    ) {
         $indexOffset = 0;
         $connectionNames = self::getDbalConnectionNames($doctrineConfig);
         foreach ($commonConfig as $index => $config) {
-            $additionalConfig = [];
-            foreach ($connectionNames as $connectionName) {
-                if ($connectionName !== $defaultConnectionName
-                    && self::getDbalConnectionKey($doctrineConfig, $connectionName) === $defaultConnectionKey
-                ) {
-                    $additionalConfig[$connectionName] = $config;
-                }
-            }
+            $additionalConfig = self::getAdditionalConfig(
+                $doctrineConfig,
+                $config,
+                $index,
+                $connectionNames,
+                $defaultConnectionName,
+                $defaultConnectionKey
+            );
             if (!empty($additionalConfig)) {
-                $doctrineConfig = array_merge(
-                    array_slice($doctrineConfig, 0, $index + $indexOffset),
-                    [['dbal' => ['connections' => $additionalConfig]]],
-                    array_slice($doctrineConfig, $index + $indexOffset)
+                $doctrineConfig = self::insertConfigAt(
+                    $doctrineConfig,
+                    [self::DBAL => [self::CONNECTIONS => $additionalConfig]],
+                    $index + $indexOffset
                 );
                 $indexOffset++;
             }
         }
-        $container->setExtensionConfig('doctrine', $doctrineConfig);
+
+        return $doctrineConfig;
+    }
+
+    /**
+     * @param array  $doctrineConfig
+     * @param array  $config
+     * @param int    $index
+     * @param array  $connectionNames
+     * @param string $defaultConnectionName
+     * @param string $defaultConnectionKey
+     *
+     * @return array
+     */
+    private static function getAdditionalConfig(
+        $doctrineConfig,
+        $config,
+        $index,
+        $connectionNames,
+        $defaultConnectionName,
+        $defaultConnectionKey
+    ) {
+        $additionalConfig = [];
+        foreach ($connectionNames as $connectionName) {
+            if ((-1 === $index || $connectionName !== $defaultConnectionName)
+                && self::getDbalConnectionKey($doctrineConfig, $connectionName) === $defaultConnectionKey
+            ) {
+                $connectionConfig = [];
+                foreach ($config as $key => $value) {
+                    if (!self::hasDbalConnectionOption($doctrineConfig, $connectionName, $key, $index)) {
+                        $connectionConfig[$key] = $value;
+                    }
+                }
+                $additionalConfig[$connectionName] = $connectionConfig;
+            }
+        }
+
+        return $additionalConfig;
+    }
+
+    /**
+     * @param array  $doctrineConfig
+     * @param string $connectionName
+     * @param string $optionName
+     * @param int    $lastIndex
+     *
+     * @return bool
+     */
+    private static function hasDbalConnectionOption(
+        $doctrineConfig,
+        $connectionName,
+        $optionName,
+        $lastIndex
+    ) {
+        $hasOption = false;
+        foreach ($doctrineConfig as $index => $config) {
+            if ($index > $lastIndex) {
+                $hasOption = true;
+                break;
+            }
+            if (is_array($config) && isset($config[self::DBAL][self::CONNECTIONS][$connectionName])) {
+                $connection = $config[self::DBAL][self::CONNECTIONS][$connectionName];
+                if (is_array($connection) && array_key_exists($optionName, $connection)) {
+                    $hasOption = true;
+                    break;
+                }
+            }
+        }
+
+        return $hasOption;
+    }
+
+    /**
+     * @param array  $doctrineConfig
+     * @param string $connectionName
+     * @param string $optionName
+     *
+     * @return mixed
+     */
+    private static function getDbalConnectionOptionValue(
+        $doctrineConfig,
+        $connectionName,
+        $optionName
+    ) {
+        $optionValue = null;
+        foreach ($doctrineConfig as $index => $config) {
+            if (is_array($config) && isset($config[self::DBAL][self::CONNECTIONS][$connectionName])) {
+                $connection = $config[self::DBAL][self::CONNECTIONS][$connectionName];
+                if (is_array($connection) && array_key_exists($optionName, $connection)) {
+                    $optionValue = $connection[$optionName];
+                }
+            }
+        }
+
+        return $optionValue;
+    }
+
+    /**
+     * @param array  $commonConfig
+     * @param string $optionName
+     *
+     * @return mixed
+     */
+    private static function getCommonOptionValue($commonConfig, $optionName)
+    {
+        $optionValue = null;
+        foreach ($commonConfig as $config) {
+            if (is_array($config) && array_key_exists($optionName, $config)) {
+                $optionValue = $config[$optionName];
+            }
+        }
+
+        return $optionValue;
+    }
+
+    /**
+     * @param array $doctrineConfig
+     * @param array $config
+     * @param int   $index
+     *
+     * @return array
+     */
+    private static function insertConfigAt($doctrineConfig, $config, $index)
+    {
+        if (-1 === $index) {
+            return array_merge([$config], $doctrineConfig);
+        }
+
+        return array_merge(
+            array_slice($doctrineConfig, 0, $index),
+            [$config],
+            array_slice($doctrineConfig, $index)
+        );
     }
 
     /**
@@ -86,18 +295,18 @@ class DbalConfigurationLoader
      *
      * @return array
      */
-    private static function getCommonDbalConfig(array $doctrineConfig)
+    private static function getCommonDbalConfig($doctrineConfig)
     {
         /**
          * keys that should not be rewritten to the connection config
          * @see \Doctrine\Bundle\DoctrineBundle\DependencyInjection\Configuration::addDbalSection
          */
-        $excludedKeys = ['default_connection', 'types', 'type'];
+        $excludedKeys = [self::DEFAULT_CONNECTION, 'types', 'type'];
 
         $commonConfig = [];
         foreach ($doctrineConfig as $index => $config) {
             if (is_array($config) && self::hasCommonDbalConfig($config)) {
-                $dbal = $config['dbal'];
+                $dbal = $config[self::DBAL];
                 foreach ($excludedKeys as $key) {
                     unset($dbal[$key]);
                 }
@@ -115,16 +324,16 @@ class DbalConfigurationLoader
      *
      * @return bool
      */
-    private static function hasCommonDbalConfig(array $config)
+    private static function hasCommonDbalConfig($config)
     {
         /**
          * @see \Doctrine\Bundle\DoctrineBundle\DependencyInjection\Configuration::addDbalSection
          */
         return
-            !empty($config['dbal'])
-            && is_array($config['dbal'])
-            && !array_key_exists('connections', $config['dbal'])
-            && !array_key_exists('connection', $config['dbal']);
+            !empty($config[self::DBAL])
+            && is_array($config[self::DBAL])
+            && !array_key_exists(self::CONNECTIONS, $config[self::DBAL])
+            && !array_key_exists(self::CONNECTION, $config[self::DBAL]);
     }
 
     /**
@@ -132,12 +341,12 @@ class DbalConfigurationLoader
      *
      * @return string|null
      */
-    private static function getDefaultDbalConnectionName(array $doctrineConfig)
+    private static function getDefaultDbalConnectionName($doctrineConfig)
     {
         $defaultConnectionName = null;
         foreach ($doctrineConfig as $config) {
-            if (is_array($config) && !empty($config['dbal']['default_connection'])) {
-                $defaultConnectionName = $config['dbal']['default_connection'];
+            if (is_array($config) && !empty($config[self::DBAL][self::DEFAULT_CONNECTION])) {
+                $defaultConnectionName = $config[self::DBAL][self::DEFAULT_CONNECTION];
             }
         }
 
@@ -149,12 +358,12 @@ class DbalConfigurationLoader
      *
      * @return string[]
      */
-    private static function getDbalConnectionNames(array $doctrineConfig)
+    private static function getDbalConnectionNames($doctrineConfig)
     {
         $connectionNames = [];
         foreach ($doctrineConfig as $config) {
-            if (is_array($config) && !empty($config['dbal']) && is_array($config['dbal'])) {
-                $connections = self::getDbalConnections($config['dbal']);
+            if (is_array($config) && !empty($config[self::DBAL]) && is_array($config[self::DBAL])) {
+                $connections = self::getDbalConnections($config[self::DBAL]);
                 if (!empty($connections)) {
                     $connectionNames = array_merge($connectionNames, array_keys($connections));
                 }
@@ -170,28 +379,28 @@ class DbalConfigurationLoader
      *
      * @return string|null
      */
-    private static function getDbalConnectionKey(array $doctrineConfig, $connectionName)
+    private static function getDbalConnectionKey($doctrineConfig, $connectionName)
     {
         $driver = '';
         $dbname = '';
         $host = '';
         $port = '';
         foreach ($doctrineConfig as $config) {
-            if (is_array($config) && !empty($config['dbal']) && is_array($config['dbal'])) {
-                $connections = self::getDbalConnections($config['dbal']);
+            if (is_array($config) && !empty($config[self::DBAL]) && is_array($config[self::DBAL])) {
+                $connections = self::getDbalConnections($config[self::DBAL]);
                 if (!empty($connections) && !empty($connections[$connectionName])) {
                     $connection = $connections[$connectionName];
-                    if (array_key_exists('driver', $connection)) {
-                        $driver = $connection['driver'];
+                    if (array_key_exists(self::DRIVER, $connection)) {
+                        $driver = $connection[self::DRIVER];
                     }
-                    if (array_key_exists('dbname', $connection)) {
-                        $dbname = $connection['dbname'];
+                    if (array_key_exists(self::DBNAME, $connection)) {
+                        $dbname = $connection[self::DBNAME];
                     }
-                    if (array_key_exists('host', $connection)) {
-                        $host = $connection['host'];
+                    if (array_key_exists(self::HOST, $connection)) {
+                        $host = $connection[self::HOST];
                     }
-                    if (array_key_exists('port', $connection)) {
-                        $port = $connection['port'];
+                    if (array_key_exists(self::PORT, $connection)) {
+                        $port = $connection[self::PORT];
                     }
                 }
             }
@@ -209,13 +418,13 @@ class DbalConfigurationLoader
      *
      * @return array|null
      */
-    private static function getDbalConnections(array $dbalConfig)
+    private static function getDbalConnections($dbalConfig)
     {
-        if (!empty($dbalConfig['connections']) && is_array($dbalConfig['connections'])) {
-            return $dbalConfig['connections'];
+        if (!empty($dbalConfig[self::CONNECTIONS]) && is_array($dbalConfig[self::CONNECTIONS])) {
+            return $dbalConfig[self::CONNECTIONS];
         }
-        if (!empty($dbalConfig['connection']) && is_array($dbalConfig['connection'])) {
-            return $dbalConfig['connection'];
+        if (!empty($dbalConfig[self::CONNECTION]) && is_array($dbalConfig[self::CONNECTION])) {
+            return $dbalConfig[self::CONNECTION];
         }
 
         return null;
