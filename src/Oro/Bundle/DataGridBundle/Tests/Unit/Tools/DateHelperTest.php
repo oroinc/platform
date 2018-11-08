23eaa79ee65e96b555fe3b485e790ed552a52c6b
Roman Chaliuk <rchaliuk@oroinc.com>
2018-11-08 18:25:39 +0200

    BAP-17868: Switching form summer time to winter caused fail of the Unit tests
    - refactoring unit test

--- src/Oro/Bundle/DataGridBundle/Tests/Unit/Tools/DateHelperTest.php
+++ src/Oro/Bundle/DataGridBundle/Tests/Unit/Tools/DateHelperTest.php
@@ -20,17 +20,21 @@ class DateHelperTest extends \PHPUnit_Framework_TestCase
         $this->dateHelper = new DateHelper($this->localeSettings);
     }
 
+    private function getTimeZoneOffset($timeZone)
+    {
+        return (new \DateTime('now', new \DateTimeZone($timeZone)))->format('P');
+    }
+
     public function testGetTimeZoneOffset()
     {
-        $this->markTestSkipped('Will be fixed in BAP-17868');
-
         $this->localeSettings->expects(self::once())
             ->method('getTimeZone')
             ->willReturn('Europe/Kiev');
 
-        self::assertEquals('+03:00', $this->dateHelper->getTimeZoneOffset());
+        $offset = $this->getTimeZoneOffset('Europe/Kiev');
+        self::assertEquals($offset, $this->dateHelper->getTimeZoneOffset());
         // test that the offset is cached
-        self::assertEquals('+03:00', $this->dateHelper->getTimeZoneOffset());
+        self::assertEquals($offset, $this->dateHelper->getTimeZoneOffset());
     }
 
     public function testGetTimeZoneOffsetForUTC()
@@ -44,14 +48,13 @@ class DateHelperTest extends \PHPUnit_Framework_TestCase
 
     public function testGetConvertTimezoneExpression()
     {
-        $this->markTestSkipped('Will be fixed in BAP-17868');
-
         $this->localeSettings->expects(self::once())
             ->method('getTimeZone')
             ->willReturn('Europe/Kiev');
 
+        $offset = $this->getTimeZoneOffset('Europe/Kiev');
         self::assertEquals(
-            'CONVERT_TZ(e.createdAt, \'+00:00\', \'+03:00\')',
+            sprintf('CONVERT_TZ(e.createdAt, \'+00:00\', \'%s\')', $offset),
             $this->dateHelper->getConvertTimezoneExpression('e.createdAt')
         );
     }
