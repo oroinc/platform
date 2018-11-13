da51d0f1c870033b4b1f3cf01dc7d8d605d80902
Igor Galayev <igalayev@oroinc.com>
2018-11-13 20:12:41 +0200

    CRM-9077: Contact import fails in global organization - add decorator for listener - fix OwnerChecker

---
+++ src/Oro/Bundle/OrganizationBundle/Event/ImportStrategyListenerInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Oro\Bundle\OrganizationBundle\Event;
+
+use Oro\Bundle\ImportExportBundle\Event\StrategyEvent;
+
+/**
+ * Interface for ImportStrategyListener allows to easily decorate listener.
+ */
+interface ImportStrategyListenerInterface
+{
+    /**
+     * @param StrategyEvent $event
+     */
+    public function onProcessAfter(StrategyEvent $event);
+
+    /**
+     * Clear default organization on doctrine entity manager clear
+     * @return mixed
+     */
+    public function onClear();
+}
