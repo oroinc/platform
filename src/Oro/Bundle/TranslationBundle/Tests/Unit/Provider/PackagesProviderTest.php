9c193abe60b030bf48ef3020a2da10dac77880f3
Yurii Muratov <ymuratov@oroinc.com>
2018-11-29 14:55:32 +0200

    BAP-17675: Load translations when phar extension is disabled for php (#20972)

--- src/Oro/Bundle/TranslationBundle/Tests/Unit/Provider/PackagesProviderTest.php
+++ src/Oro/Bundle/TranslationBundle/Tests/Unit/Provider/PackagesProviderTest.php
@@ -9,20 +9,6 @@ use Oro\Bundle\TranslationBundle\Provider\PackagesProvider;
 
 class PackagesProviderTest extends \PHPUnit\Framework\TestCase
 {
-    /** @var PackageManager|\PHPUnit\Framework\MockObject\MockObject */
-    protected $pm;
-
-    protected function setUp()
-    {
-        $this->pm = $this->getMockBuilder('Oro\Bundle\DistributionBundle\Manager\PackageManager')
-            ->disableOriginalConstructor()->getMock();
-    }
-
-    protected function tearDown()
-    {
-        unset($this->pm);
-    }
-
     /**
      * @dataProvider installedPackagesProvider
      *
@@ -32,14 +18,11 @@ class PackagesProviderTest extends \PHPUnit\Framework\TestCase
      * @param array $expectedResult
      */
     public function testGetInstalledPackages(
-        array $packages = [],
         array $bundles = [],
         array $providers = [],
         array $expectedResult = []
     ) {
-        $provider = new PackagesProvider($this->pm, $bundles, 'rootDir', 'rootDir/cache/composer', $providers);
-        $this->pm->expects($this->once())->method('getInstalled')
-            ->will($this->returnValue($packages));
+        $provider = new PackagesProvider($bundles, 'rootDir', $providers);
 
         $this->assertEquals($expectedResult, $provider->getInstalledPackages());
     }
@@ -55,24 +38,20 @@ class PackagesProviderTest extends \PHPUnit\Framework\TestCase
 
         return [
             'empty result by default' => [],
-            'contains packages info' => [[new Package('testName', '1', '1')], [], [], ['testname']],
             'contains bundles first namespace part' => [
-                [],
                 ['Oro\Bundle\TranslationBundle\OroTranslationBundle'],
                 [],
                 ['Oro']
             ],
             'packages from other providers' => [
                 [],
-                [],
                 [$provider1, $provider2, $provider3],
                 ['package1', 'package2', 'package3']
             ],
-            'packages merged from both source' => [
-                [new Package('testName', '1', '1')],
+            'packages from providers and bundles' => [
                 ['Oro\Bundle\TranslationBundle\OroTranslationBundle'],
                 [$provider1, $provider2, $provider3],
-                ['testname', 'Oro', 'package1', 'package2', 'package3']
+                ['Oro', 'package1', 'package2', 'package3']
             ]
         ];
     }
