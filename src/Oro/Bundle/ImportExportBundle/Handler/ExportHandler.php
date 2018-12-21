9c6af62a80e1987f8b6b324fb84a7142b29e8741
Andrey Yatsenco <ayatsenko@oroinc.com>
2018-12-21 18:34:41 +0200

    Revert "Better handling for file downloads (#21374)"
    This reverts commit 872d2ed3678054a9472e0c1a0efea663204b030f.

--- src/Oro/Bundle/ImportExportBundle/Handler/ExportHandler.php
+++ src/Oro/Bundle/ImportExportBundle/Handler/ExportHandler.php
@@ -14,7 +14,6 @@ use Oro\Bundle\ImportExportBundle\Reader\BatchIdsReaderInterface;
 use Oro\Bundle\ImportExportBundle\Writer\FileStreamWriter;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
@@ -105,9 +104,9 @@ class ExportHandler extends AbstractHandler
         if ($errorsCount > 0) {
             $errors = array_merge($errors, $jobResult->getFailureExceptions());
         }
-
+        
         $errors = array_slice($errors, 0, 100);
-
+        
         if (($writer = $this->writerChain->getWriter($outputFormat)) && $writer instanceof ClosableInterface) {
             $writer->close();
         }
@@ -249,20 +248,13 @@ class ExportHandler extends AbstractHandler
         } catch (FileNotFound $exception) {
             throw new NotFoundHttpException();
         }
-        $response = new Response($content);
-
-        $disposition = $response->headers->makeDisposition(
-            ResponseHeaderBag::DISPOSITION_ATTACHMENT,
-            $fileName
-        );
-        $response->headers->set('Content-Disposition', $disposition);
-
-        $contentType = $this->fileManager->getMimeType($fileName);
+        $headers     = [];
+        $contentType = $this->getFileContentType($fileName);
         if ($contentType !== null) {
-            $response->headers->set('Content-Type', $contentType);
+            $headers['Content-Type'] = $contentType;
         }
 
-        return $response;
+        return new Response($content, 200, $headers);
     }
 
     /**
