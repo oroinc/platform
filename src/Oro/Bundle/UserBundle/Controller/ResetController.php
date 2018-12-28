d660b450de7da1ca26d734dc1771082b482d48d3
Yurii Muratov <ymuratov@oroinc.com>
2018-12-28 23:03:33 +0200

    BAP-17151: User and Customer User token regeneration (#21560)

--- src/Oro/Bundle/UserBundle/Controller/ResetController.php
+++ src/Oro/Bundle/UserBundle/Controller/ResetController.php
@@ -49,9 +49,7 @@ class ResetController extends Controller
             }
         }
 
-        if (null === $user->getConfirmationToken()) {
-            $user->setConfirmationToken($user->generateToken());
-        }
+        $user->setConfirmationToken($user->generateToken());
 
         $this->get('session')->set(static::SESSION_EMAIL, $this->getObfuscatedEmail($user));
         try {
