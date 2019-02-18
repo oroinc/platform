30b7b275369d50df3158511502fbcf0f7d223895
rchaliuk <rchaliuk@oroinc.com>
2019-02-18 19:37:58 +0200

    BAP-16189: CSRF at Reset Request (#22428)

--- src/Oro/Bundle/UserBundle/Controller/ResetController.php
+++ src/Oro/Bundle/UserBundle/Controller/ResetController.php
@@ -34,6 +34,11 @@ class ResetController extends Controller
      */
     public function sendEmailAction(Request $request)
     {
+        if (!$this->isCsrfTokenValid('oro-user-password-reset-request', $request->get('_csrf_token'))) {
+            $this->get('session')->getFlashBag()
+                ->add('warn', 'The CSRF token is invalid. Please try to resubmit the form.');
+            return $this->redirect($this->generateUrl('oro_user_reset_request'));
+        }
         $email = $request->request->get('username');
         $frontend = $request->get('frontend', false);
         /** @var User $user */
