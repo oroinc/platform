e59bdcc00883051410e3e528cf0827bc880797a3
Mykhailo Sulyma <msulima@oroinc.com>
2019-02-20 16:35:57 +0200

    BAP-18207: Add missing warmers for cache providers in WorkflowBundle (#22577)
    - added wormers and clearers for `oro_process_trigger`, `oro_workflow` and `oro_workflow_transition_trigger_event` caches

---
+++ src/Oro/Bundle/WorkflowBundle/Cache/EventTriggerCacheClearer.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Oro\Bundle\WorkflowBundle\Cache;
+
+use Symfony\Component\HttpKernel\CacheClearer\CacheClearerInterface;
+
+/**
+ * Clears event triggers caches.
+ */
+class EventTriggerCacheClearer implements CacheClearerInterface
+{
+    /**
+     * @var EventTriggerCache[]
+     */
+    private $eventTriggerCaches = [];
+
+    /**
+     * @param EventTriggerCache $eventTriggerCache
+     */
+    public function addEventTriggerCache(EventTriggerCache $eventTriggerCache): void
+    {
+        $this->eventTriggerCaches[] = $eventTriggerCache;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function clear($cacheDir)
+    {
+        foreach ($this->eventTriggerCaches as $eventTriggerCache) {
+            $eventTriggerCache->invalidateCache();
+        }
+    }
+}
---
+++ src/Oro/Bundle/WorkflowBundle/Tests/Unit/Cache/EventTriggerCacheClearerTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Oro\Bundle\WorkflowBundle\Tests\Unit\Cache;
+
+use Oro\Bundle\WorkflowBundle\Cache\EventTriggerCache;
+use Oro\Bundle\WorkflowBundle\Cache\EventTriggerCacheClearer;
+
+class EventTriggerCacheClearerTest extends \PHPUnit\Framework\TestCase
+{
+    /** @var EventTriggerCacheClearer */
+    private $clearer;
+
+    protected function setUp()
+    {
+        $this->clearer = new EventTriggerCacheClearer();
+    }
+
+    public function testClear(): void
+    {
+        /** @var EventTriggerCache|\PHPUnit\Framework\MockObject\MockObject $eventTriggerCache1 */
+        $eventTriggerCache1 = $this->createMock(EventTriggerCache::class);
+        $eventTriggerCache1->expects($this->once())
+            ->method('invalidateCache');
+
+        /** @var EventTriggerCache|\PHPUnit\Framework\MockObject\MockObject $eventTriggerCache2 */
+        $eventTriggerCache2 = $this->createMock(EventTriggerCache::class);
+        $eventTriggerCache2->expects($this->once())
+            ->method('invalidateCache');
+
+        $this->clearer->addEventTriggerCache($eventTriggerCache1);
+        $this->clearer->addEventTriggerCache($eventTriggerCache2);
+        $this->clearer->clear('test');
+    }
+}
