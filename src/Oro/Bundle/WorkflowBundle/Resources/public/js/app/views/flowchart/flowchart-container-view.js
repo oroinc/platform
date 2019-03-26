6a2f710000cf9eac2629581628bbb5651a3c525a
Hryhorii Hrebiniuk <hhrebiniuk@oroinc.com>
2019-03-26 17:19:45 +0200

    BAP-18428: Hide workflow graph if there are too many transitions to render
     - added error message

--- src/Oro/Bundle/WorkflowBundle/Resources/public/js/app/views/flowchart/flowchart-container-view.js
+++ src/Oro/Bundle/WorkflowBundle/Resources/public/js/app/views/flowchart/flowchart-container-view.js
@@ -5,6 +5,10 @@ define(function(require) {
     var FlowchartControlView = require('oroworkflow/js/app/views/flowchart/viewer/flowchart-control-view');
     var ZoomableAreaView = require('oroui/js/app/views/zoomable-area-view');
     var BaseView = require('oroui/js/app/views/base/view');
+    var ComplexityError = require('oroworkflow/js/tools/path-finder/complexity-error');
+    var error = require('oroui/js/error');
+    var mediator = require('oroui/js/mediator');
+    var __ = require('orotranslation/js/translator');
 
     FlowchartContainerView = BaseView.extend({
         autoRender: true,
@@ -20,9 +24,23 @@ define(function(require) {
 
         createFlowchartView: function(FlowchartView, flowchartOptions) {
             var flowchartView = new FlowchartView(_.extend({
-                autoRender: true,
                 el: this.$('.workflow-flowchart')
             }, flowchartOptions));
+
+            try {
+                flowchartView.render();
+            } catch (e) {
+                if (e instanceof ComplexityError) {
+                    this.$el.html(this.template({message: __('oro.workflow.error.complexity')}));
+                } else {
+                    this.$el.html('');
+                    mediator.execute('showErrorMessage', __('oro.workflow.error.initialization'));
+                    error.showErrorInConsole(e);
+                }
+                flowchartView.dispose();
+                return;
+            }
+
             this.subview('chart', flowchartView);
 
             var controlView = new FlowchartControlView({
