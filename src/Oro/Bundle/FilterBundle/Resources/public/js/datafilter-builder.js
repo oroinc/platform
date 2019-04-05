937592f749bc6f8588b00b00fd3e7a23905c522e
Dmitrii Shykalenko <dshykalenko@oroinc.com>
2019-04-05 12:33:47 +0300

    BB-14976: Sticky filters in the products grid isn't displayed on the … (#23315)

--- src/Oro/Bundle/FilterBundle/Resources/public/js/datafilter-builder.js
+++ src/Oro/Bundle/FilterBundle/Resources/public/js/datafilter-builder.js
@@ -65,12 +65,17 @@ define(function(require) {
                     'useFiltersStateAnimationOnInit')
             );
 
-            var $filterContainer;
+            var filterContainer;
 
-            if (this.filterContainerSelector && this.$el.find(this.filterContainerSelector).length) {
-                $filterContainer = this.$el.find(this.filterContainerSelector);
-            } else {
-                $filterContainer = this.$el;
+            if (this.filterContainerSelector) {
+                // Since potentially filter container can be moved outside grid container by another component
+                // we try to find it everywhere before deciding to use grid container instead
+                filterContainer = this.$el.find(this.filterContainerSelector)[0] ||
+                    $(this.filterContainerSelector)[0];
+            }
+
+            if (!filterContainer) {
+                filterContainer = this.$el[0];
             }
 
             if (!this.enableToggleFilters || _.result(this.metadata.options.toolbarOptions, 'hide') === true) {
@@ -81,12 +86,12 @@ define(function(require) {
                 var $container = this.$el.closest('body, .ui-dialog').find(options.filtersStateElement).first();
 
                 options.filtersStateElement = $container.length
-                    ? $container : $('<div/>').prependTo($filterContainer);
+                    ? $container : $('<div/>').prependTo(filterContainer);
             }
 
             var filtersList = new FiltersManager(options);
             filtersList.render();
-            filtersList.$el.prependTo($filterContainer);
+            filtersList.$el.prependTo(filterContainer);
 
             mediator.trigger('datagrid_filters:rendered', this.collection, this.$el);
             this.metadata.state.filters = this.metadata.state.filters || [];
