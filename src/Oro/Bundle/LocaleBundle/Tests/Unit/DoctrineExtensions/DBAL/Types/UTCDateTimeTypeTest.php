db08c8d98b4c13a3a5cb49ed743a2e4e04e3d43a
Yefim Yevtushenko <webevt@users.noreply.github.com>
2019-04-05 18:27:22 +0300

    BAP-16821: Failed build on CI in Oro\Bundle\LocaleBundle\Tests\Unit\DoctrineExtensions\DBAL\Types\UTCTimeTypeTest::testConvertToDatabaseValue with data set "positive shift" (#23308)
    BAP-16821: Failed build on CI in Oro\Bundle\LocaleBundle\Tests\Unit\DoctrineExtensions\DBAL\Types\UTCTimeTypeTest::testConvertToDatabaseValue with data set "positive shift"
    - fixed tests which might fail when executed on date near DST shift

--- src/Oro/Bundle/LocaleBundle/Tests/Unit/DoctrineExtensions/DBAL/Types/UTCDateTimeTypeTest.php
+++ src/Oro/Bundle/LocaleBundle/Tests/Unit/DoctrineExtensions/DBAL/Types/UTCDateTimeTypeTest.php
@@ -3,29 +3,29 @@
 namespace Oro\Bundle\LocaleBundle\Tests\Unit\DoctrineExtensions\DBAL\Types;
 
 use Doctrine\DBAL\Platforms\AbstractPlatform;
-use Oro\Bundle\LocaleBundle\DoctrineExtensions\DBAL\Types\UTCTimeType;
+use Oro\Bundle\LocaleBundle\DoctrineExtensions\DBAL\Types\UTCDateTimeType;
 
 class UTCDateTimeTypeTest extends \PHPUnit\Framework\TestCase
 {
     /**
-     * @var UTCTimeType
+     * @var UTCDateTimeType|\PHPUnit\Framework\MockObject\MockObject
      */
     protected $type;
 
     /**
-     * @var AbstractPlatform
+     * @var AbstractPlatform|\PHPUnit\Framework\MockObject\MockObject
      */
     protected $platform;
 
     protected function setUp()
     {
         // class has private constructor
-        $this->type = $this->getMockBuilder('Oro\Bundle\LocaleBundle\DoctrineExtensions\DBAL\Types\UTCDateTimeType')
+        $this->type = $this->getMockBuilder(UTCDateTimeType::class)
             ->setMethods(null)
             ->disableOriginalConstructor()
             ->getMock();
 
-        $this->platform = $this->getMockBuilder('Doctrine\DBAL\Platforms\AbstractPlatform')
+        $this->platform = $this->getMockBuilder(AbstractPlatform::class)
             ->disableOriginalConstructor()
             ->setMethods(array('getDateTimeFormatString'))
             ->getMockForAbstractClass();
@@ -34,45 +34,57 @@ class UTCDateTimeTypeTest extends \PHPUnit\Framework\TestCase
             ->will($this->returnValue('Y-m-d H:i:s'));
     }
 
-    protected function tearDown()
-    {
-        unset($this->type);
-        unset($this->platform);
-    }
-
     /**
-     * @param \DateTime $source
+     * @param string $sourceDateTime
+     * @param string $sourceTimeZone
      * @param string $expected
-     * @dataProvider convertToDatabaseValueDataProvider
+     *
+     * @dataProvider convertToDatabaseValueWhenDataProvider
      */
-    public function testConvertToDatabaseValue($source, $expected)
+    public function testConvertToDatabaseValue(string $sourceDateTime, string $sourceTimeZone, string $expected)
     {
+        $source = new \DateTime($sourceDateTime, new \DateTimeZone($sourceTimeZone));
+
         $this->assertEquals($expected, $this->type->convertToDatabaseValue($source, $this->platform));
     }
 
     /**
      * @return array
      */
-    public function convertToDatabaseValueDataProvider()
+    public function convertToDatabaseValueWhenDataProvider()
     {
-        return array(
-            'null' => array(
-                'source'   => null,
-                'expected' => null,
-            ),
-            'UTC' => array(
-                'source' => new \DateTime('2013-01-01 00:00:00', new \DateTimeZone('UTC')),
+        return [
+            'UTC' => [
+                'sourceDateTime' => '2013-01-01 00:00:00',
+                'sourceTimeZone' => 'UTC',
                 'expected' => '2013-01-01 00:00:00',
-            ),
-            'positive shift' => array(
-                'source' => new \DateTime('2013-01-01 00:00:00', new \DateTimeZone('Europe/Athens')),
+            ],
+            'positive shift when no DST' => [
+                'sourceDateTime' => '2013-01-01 00:00:00',
+                'sourceTimeZone' => 'Europe/Athens',
                 'expected' => '2012-12-31 22:00:00',
-            ),
-            'negative shift' => array(
-                'source' => new \DateTime('2013-01-01 00:00:00', new \DateTimeZone('America/Los_Angeles')),
+            ],
+            'negative shift when no DST' => [
+                'sourceDateTime' => '2013-01-01 00:00:00',
+                'sourceTimeZone' => 'America/Los_Angeles',
                 'expected' => '2013-01-01 08:00:00',
-            ),
-        );
+            ],
+            'positive shift when DST' => [
+                'sourceDateTime' => '2013-06-01 00:00:00',
+                'sourceTimeZone' => 'Europe/Athens',
+                'expected' => '2013-05-31 21:00:00',
+            ],
+            'negative shift when DST' => [
+                'sourceDateTime' => '2013-06-01 00:00:00',
+                'sourceTimeZone' => 'America/Los_Angeles',
+                'expected' => '2013-06-01 07:00:00',
+            ],
+        ];
+    }
+
+    public function testConvertToDatabaseValueWhenNull()
+    {
+        $this->assertNull($this->type->convertToDatabaseValue(null, $this->platform));
     }
 
     public function testConvertToPHPValue()
