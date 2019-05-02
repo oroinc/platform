70071776ed70ee4eebe92ca894eddbde35173a99
Dmitry Khrysev <x86demon@users.noreply.github.com>
2019-05-02 14:14:45 +0300

    BAP-14207: Multiple conditions applied to same related entity in reports/segments (#23558)
    * BAP-14207: Multiple conditions applied to same related entity in reports/segments 
    - added compatibility config to master, disabled by default
    - added grouping config option

--- src/Oro/Bundle/QueryDesignerBundle/QueryDesigner/RestrictionBuilder.php
+++ src/Oro/Bundle/QueryDesignerBundle/QueryDesigner/RestrictionBuilder.php
@@ -2,21 +2,32 @@
 
 namespace Oro\Bundle\QueryDesignerBundle\QueryDesigner;
 
+use Oro\Bundle\ConfigBundle\Config\ConfigManager;
 use Oro\Bundle\FilterBundle\Filter\FilterInterface;
 use Oro\Bundle\FilterBundle\Filter\FilterUtility;
 use Oro\Bundle\QueryDesignerBundle\Grid\Extension\GroupingOrmFilterDatasourceAdapter;
 
+/**
+ * Build and apply query designer filters to given Data source.
+ */
 class RestrictionBuilder implements RestrictionBuilderInterface
 {
     /** @var Manager */
     protected $manager;
 
     /**
-     * @param Manager $manager
+     * @var ConfigManager
      */
-    public function __construct(Manager $manager)
+    private $configManager;
+
+    /**
+     * @param Manager $manager
+     * @param ConfigManager $configManager
+     */
+    public function __construct(Manager $manager, ConfigManager $configManager)
     {
         $this->manager = $manager;
+        $this->configManager = $configManager;
     }
 
     /**
@@ -31,60 +42,105 @@ class RestrictionBuilder implements RestrictionBuilderInterface
     /**
      * Recursive iterates through filters and builds an expression to be applied to the given data source
      *
-     * @param array                              $filters
+     * @param array $filters
      * @param GroupingOrmFilterDatasourceAdapter $ds
      */
     protected function doBuildRestrictions(array $filters, GroupingOrmFilterDatasourceAdapter $ds)
     {
         $operatorStack = [FilterUtility::CONDITION_AND];
+        $isInGroup = !empty($filters['in_group']);
+        unset($filters['in_group']);
+
         foreach ($filters as $item) {
             if (is_string($item)) {
-                array_push($operatorStack, $item);
-            } elseif (!isset($item['filter'])) {
-                $ds->beginRestrictionGroup(array_pop($operatorStack));
-                $this->doBuildRestrictions($item, $ds);
-                $ds->endRestrictionGroup();
-            } else {
-                $operator = array_pop($operatorStack);
-
-                $params = [];
-                if ($operator !== null) {
-                    $params[FilterUtility::CONDITION_KEY] = $operator;
-                }
-                if (isset($item['filterData']['params'])) {
-                    $params = $item['filterData']['params'];
-                    unset($item['filterData']['params']);
-                }
-                /** @var FilterInterface $filter */
-                $filter = $this->getFilterObject($item['filter'], $item['column'], $params);
-
-                $form = $filter->getForm();
-                if (!$form->isSubmitted()) {
-                    $form->submit($item['filterData']);
-                }
-                if ($form->isValid()) {
-                    $ds->beginRestrictionGroup($operator);
-                    $originalValues=[];
-                    if (isset($item['filterData']['value']['start'])) {
-                        $originalValues['value']['start_original'] = $item['filterData']['value']['start'];
-                    }
-                    if (isset($item['filterData']['value']['end'])) {
-                        $originalValues['value']['end_original'] = $item['filterData']['value']['end'];
-                    }
-                    $data = array_merge_recursive($form->getData(), $originalValues);
-                    $filter->apply($ds, $data);
-                    $ds->endRestrictionGroup();
-                }
+                $operatorStack[] = $item;
+                continue;
             }
+
+            $operator = array_pop($operatorStack);
+            if (isset($item['filter'])) {
+                $this->buildSingleRestriction($ds, $operator, $item, $isInGroup);
+            } else {
+                $this->buildGroupedRestrictions($ds, $operator, $item);
+            }
+        }
+    }
+
+    /**
+     * @param GroupingOrmFilterDatasourceAdapter $ds
+     * @param string $operator
+     * @param array $item
+     */
+    protected function buildGroupedRestrictions(GroupingOrmFilterDatasourceAdapter $ds, $operator, array $item)
+    {
+        if (count($item) === 1) {
+            // Skip nested conditions groups without filters
+            $this->doBuildRestrictions($item, $ds);
+        } else {
+            // Process conditions group
+            $ds->beginRestrictionGroup($operator);
+
+            if ($this->isConditionsGroupingEnabled()) {
+                // Group toMany under same EXISTS and filter the main Query by it
+                $conditionsGroupFilter = $this->manager->getFilter('conditions_group');
+                $conditionsGroupFilter->apply($ds, ['filters' => $item]);
+            } else {
+                $this->doBuildRestrictions($item, $ds);
+            }
+
+            $ds->endRestrictionGroup();
+        }
+    }
+
+    /**
+     * @param GroupingOrmFilterDatasourceAdapter $ds
+     * @param string $operator
+     * @param array $item
+     * @param bool $isInGroup
+     */
+    protected function buildSingleRestriction(
+        GroupingOrmFilterDatasourceAdapter $ds,
+        $operator,
+        array $item,
+        bool $isInGroup
+    ) {
+        $params = [];
+        if ($operator !== null) {
+            $params[FilterUtility::CONDITION_KEY] = $operator;
+        }
+        if (isset($item['filterData']['params'])) {
+            $params = $item['filterData']['params'];
+            unset($item['filterData']['params']);
+        }
+        $filter = $this->getFilterObject($item['filter'], $item['column'], $params);
+
+        $form = $filter->getForm();
+        if (!$form->isSubmitted()) {
+            $form->submit($item['filterData']);
+        }
+        if ($form->isValid()) {
+            $ds->beginRestrictionGroup($operator);
+
+            $originalValues = ['in_group' => $isInGroup];
+            if (isset($item['filterData']['value']['start'])) {
+                $originalValues['value']['start_original'] = $item['filterData']['value']['start'];
+            }
+            if (isset($item['filterData']['value']['end'])) {
+                $originalValues['value']['end_original'] = $item['filterData']['value']['end'];
+            }
+            $data = array_merge_recursive($form->getData(), $originalValues);
+
+            $filter->apply($ds, $data);
+            $ds->endRestrictionGroup();
         }
     }
 
     /**
      * Returns prepared filter object.
      *
-     * @param string $name       A filter name.
+     * @param string $name A filter name.
      * @param string $columnName A column name this filter should be applied.
-     * @param array  $params     The filter parameters.
+     * @param array $params The filter parameters.
      *
      * @return FilterInterface
      */
@@ -93,5 +149,13 @@ class RestrictionBuilder implements RestrictionBuilderInterface
         $params[FilterUtility::DATA_NAME_KEY] = $columnName;
 
         return $this->manager->createFilter($name, $params);
+    }
+
+    /**
+     * @return bool
+     */
+    private function isConditionsGroupingEnabled(): bool
+    {
+        return (bool)$this->configManager->get('oro_query_designer.conditions_group_merge_same_entity_conditions');
     }
 }
---
+++ src/Oro/Bundle/SegmentBundle/Tests/Functional/Grid/SegmentWithConditionsGroupTest.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Oro\Bundle\SegmentBundle\Tests\Functional\Grid;
+
+use Oro\Bundle\ConfigBundle\Config\ConfigManager;
+use Oro\Bundle\DataGridBundle\Datasource\ResultRecordInterface;
+use Oro\Bundle\SegmentBundle\Entity\Segment;
+use Oro\Bundle\SegmentBundle\Tests\Functional\DataFixtures\LoadOrganizationsWithUsersData;
+use Oro\Bundle\SegmentBundle\Tests\Functional\DataFixtures\LoadSegmentWithToManyFiltersData;
+use Oro\Bundle\TestFrameworkBundle\Test\WebTestCase;
+
+/**
+ * @dbIsolationPerTest
+ */
+class SegmentWithConditionsGroupTest extends WebTestCase
+{
+    /**
+     * @var bool
+     */
+    private $groupingEnabled;
+
+    /**
+     * @var ConfigManager
+     */
+    private $configManager;
+
+    protected function setUp()
+    {
+        $this->initClient();
+        $this->loadFixtures([
+            LoadOrganizationsWithUsersData::class,
+            LoadSegmentWithToManyFiltersData::class
+        ]);
+        $this->configManager = $this->getContainer()->get('oro_config.manager');
+        $this->groupingEnabled = $this->configManager
+            ->get('oro_query_designer.conditions_group_merge_same_entity_conditions');
+    }
+
+    protected function tearDown()
+    {
+        $this->configManager
+            ->set('oro_query_designer.conditions_group_merge_same_entity_conditions', $this->groupingEnabled);
+    }
+
+    public function testSegmentWithGroupedFilters()
+    {
+        $this->assertSegmentResults(
+            $this->getReference(LoadSegmentWithToManyFiltersData::SEGMENT_FILTER_GROUP),
+            [
+                'segment.organization.1'
+            ]
+        );
+    }
+
+    public function testSegmentWithoutGroupedFilters()
+    {
+        $this->assertSegmentResults(
+            $this->getReference(LoadSegmentWithToManyFiltersData::SEGMENT_FILTER_NO_GROUP),
+            [
+                'segment.organization.1',
+                'segment.organization.2'
+            ]
+        );
+    }
+
+    public function testSegmentWithGroupedFiltersGroupingDisabled()
+    {
+        $this->configManager->set('oro_query_designer.conditions_group_merge_same_entity_conditions', false);
+
+        $this->assertSegmentResults(
+            $this->getReference(LoadSegmentWithToManyFiltersData::SEGMENT_FILTER_GROUP),
+            [
+                'segment.organization.1',
+                'segment.organization.2'
+            ]
+        );
+    }
+
+    /**
+     * @param Segment $segment
+     * @param array $expected
+     */
+    protected function assertSegmentResults(Segment $segment, array $expected)
+    {
+        $gridManager = $this->getContainer()->get('oro_datagrid.datagrid.manager');
+
+        $grid = $gridManager->getDatagrid('oro_segment_grid_' . $segment->getId());
+        $ds = $grid->getAcceptedDatasource();
+        $actual = array_map(
+            function (ResultRecordInterface $record) {
+                return $record->getValue('c1');
+            },
+            $ds->getResults()
+        );
+
+        $this->assertEquals($expected, $actual);
+    }
+}
