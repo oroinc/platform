eec010576d0119f100627e351dac2da6d9d89c69
Mykhailo Sulyma <msulima@oroinc.com>
2019-06-19 12:49:00 +0300

    BAP-18791: Cover by behat tests all test cases from an organization switching story  - added browser tab manager  - updated behat test

---
+++ src/Oro/Bundle/TestFrameworkBundle/Behat/Context/Initializer/BrowserTabManagerAwareInitializer.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Oro\Bundle\TestFrameworkBundle\Behat\Context\Initializer;
+
+use Behat\Behat\Context\Context;
+use Behat\Behat\Context\Initializer\ContextInitializer;
+use Oro\Bundle\TestFrameworkBundle\Behat\Context\BrowserTabManager;
+use Oro\Bundle\TestFrameworkBundle\Behat\Context\BrowserTabManagerAwareInterface;
+
+/**
+ * Sets manager to the behat context.
+ */
+class BrowserTabManagerAwareInitializer implements ContextInitializer
+{
+    /**
+     * @var BrowserTabManager
+     */
+    private $manager;
+
+    /**
+     * @param BrowserTabManager $manager
+     */
+    public function __construct(BrowserTabManager $manager)
+    {
+        $this->manager = $manager;
+    }
+
+    /**
+     * @param Context $context
+     */
+    public function initializeContext(Context $context)
+    {
+        if ($context instanceof BrowserTabManagerAwareInterface) {
+            $context->setBrowserTabManager($this->manager);
+        }
+    }
+}
--- src/Oro/Bundle/TestFrameworkBundle/Tests/Behat/Context/BrowserTabContext.php
+++ src/Oro/Bundle/TestFrameworkBundle/Tests/Behat/Context/BrowserTabContext.php
@@ -2,86 +2,63 @@
 
 namespace Oro\Bundle\TestFrameworkBundle\Tests\Behat\Context;
 
+use Oro\Bundle\TestFrameworkBundle\Behat\Context\BrowserTabManager;
+use Oro\Bundle\TestFrameworkBundle\Behat\Context\BrowserTabManagerAwareInterface;
 use Oro\Bundle\TestFrameworkBundle\Behat\Context\OroFeatureContext;
 
 /**
  * Allows to manage browser's tabs
  */
-class BrowserTabContext extends OroFeatureContext
+class BrowserTabContext extends OroFeatureContext implements BrowserTabManagerAwareInterface
 {
+    /** @var BrowserTabManager */
+    private $browserTabManager;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setBrowserTabManager(BrowserTabManager $browserTabManager)
+    {
+        $this->browserTabManager = $browserTabManager;
+    }
+
     /**
      * Opens current url in the new tab and switches to that tab
      *
-     * Example: And I open a new browser tab
-     * @When /^(?:|I )open a new browser tab$/
+     * Example: And I open a new browser tab and set "tab1" alias for it
+     * @When /^(?:|I )open a new browser tab and set "(?P<alias>[^"]+)" alias for it$/
+     *
+     * @param string $alias
      */
-    public function iOpenANewWindow()
+    public function iOpenANewWindow(string $alias)
     {
-        $driver = $this->getSession()->getDriver();
-        $url = $this->getSession()->getCurrentUrl();
-        $driver->executeScript("window.open('{$url}', '_blank')");
-        $windowNames = $driver->getWindowNames();
-        $driver->switchToWindow(end($windowNames));
+        $this->browserTabManager->openTab($this->getMink(), $alias);
+    }
+
+    /**
+     * Sets alias for the current browser tab
+     *
+     * Example: And I set alias "tab1"f or the current browser tab
+     * @When /^(?:|I )set alias "(?P<alias>[^"]+)" for (?:|the )current browser tab$/
+     *
+     * @param string $alias
+     */
+    public function iSetAliasForTheCurrentWindow(string $alias)
+    {
+        $this->browserTabManager->addAliasForCurrentTab($this->getMink(), $alias);
     }
 
     /**
      * Switches to any opened tab of the window by its index, starting from 1 to the length of windowNames array
      *
      * Example: And I switch to the browser tab "3"
-     * @When /^(?:|I )switch to (?:|the )browser tab "(?P<tabIndex>\d+)"$/
-     * @param int $tabIndex
-     * @throws \Exception
-     */
-    public function iSwitchToTheWindow(int $tabIndex)
-    {
-        $driver = $this->getSession()->getDriver();
-        $windowNames = $driver->getWindowNames();
-
-        if ($tabIndex < 1 || $tabIndex > count($windowNames)) {
-            throw new \Exception(
-                sprintf(
-                    "Index of window out of bounds, given '%d' but number of windows from '1' tp '%d'",
-                    $tabIndex,
-                    count($windowNames)
-                )
-            );
-        }
-
-        $driver->switchToWindow($windowNames[$tabIndex - 1]);
-    }
-
-    /**
-     * Switches to nearest sibling browser tab on the left or on the right
-     * If the current tab is the first, switching to left will make the last tab active
-     * If the current tab is the last, switching to right will make the first tab active
+     * @When /^(?:|I )switch to (?:|the )browser tab "(?P<alias>[^"]+)"$/
      *
-     * Example: And I switch to the right browser tab
-     * Example: And I switch to the left browser tab
-     * @When /^(?:|I )switch to (?:|the )(?P<direction>right|left) browser tab$/
-     * @param string $direction 'left' or 'right'
-     * @throws \Exception when direction is not 'left' or 'right'
+     * @param string $alias
      */
-    public function iSwitchToTheSiblingWindow($direction)
+    public function iSwitchToTheWindow(string $alias)
     {
-        $driver = $this->getSession()->getDriver();
-
-        $windowNames = $driver->getWindowNames();
-        $currentTabIndex = array_search($driver->getWindowName(), $windowNames);
-
-        switch ($direction) {
-            case 'right':
-                $windowName = $currentTabIndex === count($windowNames) - 1 ? 0 : $windowNames[$currentTabIndex + 1];
-                break;
-            case 'left':
-                $windowName = $windowNames[($currentTabIndex === 0 ? count($windowNames) : $currentTabIndex) - 1];
-                break;
-            default:
-                throw new \Exception(
-                    sprintf('Direction to sibling "%s" is not supported, use "right" or "left"', $direction)
-                );
-        }
-
-        $driver->switchToWindow($windowName);
+        $this->browserTabManager->switchTabForAlias($this->getMink(), $alias);
     }
 
     /**
@@ -89,9 +66,12 @@ class BrowserTabContext extends OroFeatureContext
      *
      * Example: And I close the current browser tab
      * @When /^(?:|I )close (?:|the )current browser tab$/
+     * @When /^(?:|I )close (?:|the ) browser tab "(?P<alias>[^"]+)"$/
+     *
+     * @param string|null $alias
      */
-    public function iCloseTheCurrentWindow()
+    public function iCloseTheCurrentWindow(string $alias = null)
     {
-        $this->getSession()->getDriver()->executeScript("setTimeout(window.close)");
+        $this->browserTabManager->closeTab($this->getMink(), $alias);
     }
 }
