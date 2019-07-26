cc69e0f8a2b7db63f6bae543c9098282d00b0d3e
Viacheslav Chekanovskyi <vchekanovskyi@oroinc.com>
2019-07-26 13:44:53 +0300

    BB-16392: Refactor breakpoints - code style

--- src/Oro/Bundle/UIBundle/Resources/public/js/app/modules/viewport-module.js
+++ src/Oro/Bundle/UIBundle/Resources/public/js/app/modules/viewport-module.js
@@ -2,7 +2,7 @@ define(function(require) {
     'use strict';
 
     var viewportManager = require('oroui/js/viewport-manager');
-    var cssVariablesManager  = require('oroui/js/css-variables-manager');
+    var cssVariablesManager = require('oroui/js/css-variables-manager');
     cssVariablesManager.onReady(function(cssVariables) {
         viewportManager.initialize({
             cssVariables: cssVariables
