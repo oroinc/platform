7b77ba65704cdfb0e69b23dc0e4ad5a5e9547850
Vitalii Yeromenko <vyeromenko@oroinc.com>
2019-07-30 18:38:27 +0300

    BB-17207: Extend html editor whitelist (#24629)
    - added support for HTML5 tags to HTMLPurifier; added more allowed tags; added support for iframe with youtube or vimeo
    - updated dev.locks
    - added strict, extended and disabled mode for html purification

--- src/Oro/Bundle/FormBundle/Provider/HtmlTagProvider.php
+++ src/Oro/Bundle/FormBundle/Provider/HtmlTagProvider.php
@@ -2,17 +2,49 @@
 
 namespace Oro\Bundle\FormBundle\Provider;
 
+/**
+ * Used for getting white list elements and tags
+ * Also provides HTMLPurifier modes
+ */
 class HtmlTagProvider
 {
+    const HTML_PURIFIER_MODE_STRICT = 'strict';
+    const HTML_PURIFIER_MODE_EXTENDED = 'extended';
+    const HTML_PURIFIER_MODE_DISABLED = 'disabled';
+
+    /** elements forbidden in strict mode */
+    const STRICT_ELEMENTS = ['iframe', 'style'];
+
     /** @var array */
     protected $elements = [];
 
+    /** @var string */
+    private $htmlPurifierMode;
+
+    /** @var array */
+    private $iframeDomains;
+
+    /** @var array */
+    private $uriSchemes;
+
     /**
      * @param array $elements
+     * @param string $htmlPurifierMode
+     * @param array $iframeDomains
+     * @param array $uriSchemes
      */
-    public function __construct(array $elements)
-    {
+    public function __construct(
+        array $elements,
+        $htmlPurifierMode = self::HTML_PURIFIER_MODE_STRICT,
+        $iframeDomains = [],
+        $uriSchemes = []
+    ) {
         $this->elements = $elements;
+        $this->htmlPurifierMode = $htmlPurifierMode;
+        $this->iframeDomains = $iframeDomains;
+        $this->uriSchemes = $uriSchemes;
+
+        $this->filterElementsForStrictMode($this->elements);
     }
 
     /**
@@ -55,5 +87,60 @@ class HtmlTagProvider
         }
 
         return $allowedTags;
+    }
+
+    private function filterElementsForStrictMode(array &$allowedElements)
+    {
+        if ($this->htmlPurifierMode === self::HTML_PURIFIER_MODE_STRICT) {
+            $allowedElements = array_filter(
+                $allowedElements,
+                function ($allowedElement) {
+                    return !in_array($allowedElement, self::STRICT_ELEMENTS);
+                },
+                ARRAY_FILTER_USE_KEY
+            );
+        }
+    }
+
+    /**
+     * @return bool
+     */
+    public function isPurificationNeeded()
+    {
+        return $this->htmlPurifierMode !== self::HTML_PURIFIER_MODE_DISABLED;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isExtendedPurification()
+    {
+        return $this->htmlPurifierMode === self::HTML_PURIFIER_MODE_EXTENDED;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIframeRegexp()
+    {
+        if (!$this->iframeDomains) {
+            return '';
+        }
+
+        return sprintf('<^https?://(www.)?(%s)>', implode('|', $this->iframeDomains));
+    }
+
+    /**
+     * @return array
+     */
+    public function getUriSchemes()
+    {
+        $result = [];
+
+        foreach ($this->uriSchemes as $scheme) {
+            $result[$scheme] = true;
+        }
+
+        return $result;
     }
 }
