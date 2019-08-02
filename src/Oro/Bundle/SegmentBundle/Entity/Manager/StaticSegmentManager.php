a06a11292e5730ff031894abb441d8d6e78cbc55
Dmitry Khrysev <x86demon@users.noreply.github.com>
2019-08-02 17:17:49 +0300

    BB-17220: Recalculate Product's visibility is extremely slow [3.1] (#24697)
    * BB-17220: Recalculate Product's visibility is extremely slow - static segment actualization for defined set of entities refactored to use non blocking INSERT VALUES instead of INSERT FROM SELECT
    
    * BB-17220: Recalculate Product's visibility is extremely slow - added tests - improved to use segments only assigned to web catalog actual for given website
    
    * BB-17220: Recalculate Product's visibility is extremely slow - fixed tests

--- src/Oro/Bundle/SegmentBundle/Entity/Manager/StaticSegmentManager.php
+++ src/Oro/Bundle/SegmentBundle/Entity/Manager/StaticSegmentManager.php
@@ -2,14 +2,13 @@
 
 namespace Oro\Bundle\SegmentBundle\Entity\Manager;
 
-use Doctrine\DBAL\Connection;
-use Doctrine\DBAL\Types\Type;
 use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\Query\Parameter;
+use Doctrine\ORM\Mapping\ClassMetadata;
 use Doctrine\ORM\QueryBuilder;
-use Oro\Bundle\EntityBundle\ORM\DatabaseDriverInterface;
+use Oro\Bundle\EntityBundle\ORM\NativeQueryExecutorHelper;
 use Oro\Bundle\SecurityBundle\Owner\Metadata\OwnershipMetadataProviderInterface;
 use Oro\Bundle\SegmentBundle\Entity\Segment;
+use Oro\Bundle\SegmentBundle\Entity\SegmentSnapshot;
 use Oro\Bundle\SegmentBundle\Query\DynamicSegmentQueryBuilder;
 
 /**
@@ -17,10 +16,14 @@ use Oro\Bundle\SegmentBundle\Query\DynamicSegmentQueryBuilder;
  */
 class StaticSegmentManager
 {
-    /** @var EntityManager */
+    /**
+     * @var EntityManager
+     */
     protected $em;
 
-    /** @var DynamicSegmentQueryBuilder */
+    /**
+     * @var DynamicSegmentQueryBuilder
+     */
     protected $dynamicSegmentQB;
 
     /**
@@ -29,9 +32,14 @@ class StaticSegmentManager
     protected $ownershipMetadataProvider;
 
     /**
-     * @param EntityManager                       $em
-     * @param DynamicSegmentQueryBuilder          $dynamicSegmentQB
-     * @param OwnershipMetadataProviderInterface  $ownershipMetadataProvider
+     * @var NativeQueryExecutorHelper
+     */
+    protected $nativeQueryExecutorHelper;
+
+    /**
+     * @param EntityManager $em
+     * @param DynamicSegmentQueryBuilder $dynamicSegmentQB
+     * @param OwnershipMetadataProviderInterface $ownershipMetadataProvider
      */
     public function __construct(
         EntityManager $em,
@@ -41,6 +49,14 @@ class StaticSegmentManager
         $this->em = $em;
         $this->dynamicSegmentQB = $dynamicSegmentQB;
         $this->ownershipMetadataProvider = $ownershipMetadataProvider;
+    }
+
+    /**
+     * @param NativeQueryExecutorHelper $nativeQueryExecutorHelper
+     */
+    public function setNativeQueryExecutorHelper(NativeQueryExecutorHelper $nativeQueryExecutorHelper)
+    {
+        $this->nativeQueryExecutorHelper = $nativeQueryExecutorHelper;
     }
 
     /**
@@ -58,64 +74,24 @@ class StaticSegmentManager
             throw new \LogicException('Only entities with single identifier supports.');
         }
 
-        $this->em->getRepository('OroSegmentBundle:SegmentSnapshot')->removeBySegment($segment, $entityIds);
+        $identifier = $entityMetadata->getSingleIdentifierFieldName();
+        $qb = $this->getQueryBuilderForSegment($segment, $identifier, $entityIds);
+        $selectSql = $this->getSelectSql(clone $qb, $segment, $identifier, count($entityIds) === 0);
 
+        $fieldToSelect = $this->getFieldToSelect($entityMetadata, $identifier);
+        [$values, $types] = $this->nativeQueryExecutorHelper->processParameterMappings($qb->getQuery());
         try {
             $this->em->beginTransaction();
+            // Remove existing SegmentSnapshot records for given $entityIds
+            $this->em->getRepository(SegmentSnapshot::class)->removeBySegment($segment, $entityIds);
 
-            $date       = new \DateTime('now', new \DateTimeZone('UTC'));
-            $dateString = '\'' . $date->format('Y-m-d H:i:s') . '\'';
-
-            if ($this->em->getConnection()->getDriver()->getName() === DatabaseDriverInterface::DRIVER_POSTGRESQL) {
-                $dateString = sprintf('TIMESTAMP %s', $dateString);
-            }
-
-            $insertString = sprintf(
-                ', %d, %s ',
-                $segment->getId(),
-                $dateString
-            );
-
-            $qb = $this->dynamicSegmentQB->getQueryBuilder($segment);
-
-            $this->applyOrganizationLimit($segment, $qb);
-
-            $tableAlias = $this->getFromTableAlias($qb);
-            $identifier = $entityMetadata->getSingleIdentifierFieldName();
-
+            // When segment should be partially updated for defined set of entities execute INSERT VALUE
+            // to prevent table locking. On full segment rebuild use INSERT FROM SELECT.
             if ($entityIds) {
-                $qb->andWhere($qb->expr()->in($tableAlias . '.' . $identifier, ':entityIds'))
-                    ->setParameter('entityIds', $entityIds, Type::TARRAY);
+                $this->executeInsertValues($fieldToSelect, $selectSql, $values, $types, $segment);
+            } else {
+                $this->executeInsertFromSelect($fieldToSelect, $selectSql, $values, $types);
             }
-
-            $originalQuery = $qb->getQuery();
-
-            $selectSql = $this->getSelectSql($qb, $segment, $identifier);
-            $selectSql = substr_replace($selectSql, $insertString, stripos($selectSql, 'from'), 0);
-
-            $fieldToSelect = 'entity_id';
-
-            if ($entityMetadata->getTypeOfField($identifier) === 'integer') {
-                $fieldToSelect = 'integer_entity_id';
-            }
-
-            $dbQuery = 'INSERT INTO oro_segment_snapshot (' . $fieldToSelect . ', segment_id, createdat) (%s)';
-            $dbQuery = sprintf($dbQuery, $selectSql);
-
-            $values = [];
-            $types = [];
-            foreach ($originalQuery->getParameters() as $parameter) {
-                /* @var $parameter Parameter */
-                $value = $parameter->getValue();
-                $type  = $parameter->getType() == Type::TARRAY ? Connection::PARAM_STR_ARRAY : $parameter->getType();
-                if (\PDO::PARAM_STR === $type && $value instanceof Segment) {
-                    $value = $value->getId();
-                }
-                $values[] = $value;
-                $types[]  = $type;
-            }
-
-            $this->em->getConnection()->executeQuery($dbQuery, $values, $types);
 
             $this->em->commit();
         } catch (\Exception $exception) {
@@ -124,10 +100,10 @@ class StaticSegmentManager
             throw $exception;
         }
 
-        $segment = $this->em->merge($segment);
-        $segment->setLastRun(new \DateTime('now', new \DateTimeZone('UTC')));
-        $this->em->persist($segment);
-        $this->em->flush($segment);
+        // Do not update last run on partial segment snapshot update.
+        if (!$entityIds) {
+            $this->updateSegmentLastRun($segment);
+        }
     }
 
     /**
@@ -135,28 +111,34 @@ class StaticSegmentManager
      *
      * @param QueryBuilder $queryBuilder
      * @param Segment $segment
-     * @param $identifier
+     * @param string $identifier
+     * @param bool $addSnapshotFields
      * @return string
      */
-    private function getSelectSql(QueryBuilder $queryBuilder, Segment $segment, $identifier)
-    {
-        $tableAlias = $this->getFromTableAlias($queryBuilder);
-
+    private function getSelectSql(
+        QueryBuilder $queryBuilder,
+        Segment $segment,
+        $identifier,
+        $addSnapshotFields = true
+    ): string {
         if (!$segment->getRecordsLimit()) {
-            $queryBuilder->resetDQLParts(['orderBy', 'select']);
-            $queryBuilder->select($tableAlias . '.' . $identifier);
+            if ($addSnapshotFields) {
+                $this->addSegmentSnapshotFields($queryBuilder, $segment);
+            }
             $finalSelectSql = $queryBuilder->getQuery()->getSQL();
         } else {
             $queryBuilder->setMaxResults($segment->getRecordsLimit());
             $originalSelectSql = $queryBuilder->getQuery()->getSQL();
 
-            $queryBuilder->resetDQLParts(['orderBy', 'select', 'where']);
+            if ($addSnapshotFields) {
+                $this->addSegmentSnapshotFields($queryBuilder, $segment);
+            }
+            $queryBuilder->resetDQLPart('where');
             $queryBuilder->setMaxResults(null);
-            $queryBuilder->select($tableAlias . '.' . $identifier);
             $purifiedSelectSql = $queryBuilder->getQuery()->getSQL();
             $originalIdentifierDoctrineAlias = $this->getDoctrineIdentifierAlias($identifier, $originalSelectSql);
 
-            $finalSelectSql = "$purifiedSelectSql  JOIN ($originalSelectSql) " .
+            $finalSelectSql = "$purifiedSelectSql JOIN ($originalSelectSql) " .
                 "AS result_table ON result_table.$originalIdentifierDoctrineAlias=$identifier";
         }
 
@@ -189,7 +171,7 @@ class StaticSegmentManager
     /**
      * Limit segment data by segment's organization
      *
-     * @param Segment      $segment
+     * @param Segment $segment
      * @param QueryBuilder $qb
      */
     protected function applyOrganizationLimit(Segment $segment, QueryBuilder $qb)
@@ -207,5 +189,128 @@ class StaticSegmentManager
                 )
             );
         }
+    }
+
+    /**
+     * @param ClassMetadata $entityMetadata
+     * @param string $identifier
+     * @return string
+     */
+    private function getFieldToSelect(ClassMetadata $entityMetadata, string $identifier): string
+    {
+        $fieldToSelect = 'entity_id';
+        if ($entityMetadata->getTypeOfField($identifier) === 'integer') {
+            $fieldToSelect = 'integer_entity_id';
+        }
+
+        return $fieldToSelect;
+    }
+
+    /**
+     * @param Segment $segment
+     * @param string $identifier
+     * @param array $entityIds
+     * @return QueryBuilder
+     */
+    private function getQueryBuilderForSegment(
+        Segment $segment,
+        string $identifier,
+        array $entityIds = []
+    ): QueryBuilder {
+        $queryBuilder = $this->dynamicSegmentQB->getQueryBuilder($segment);
+        $this->applyOrganizationLimit($segment, $queryBuilder);
+        $tableAlias = $this->getFromTableAlias($queryBuilder);
+
+        if ($entityIds) {
+            $queryBuilder->andWhere($queryBuilder->expr()->in($tableAlias, ':entityIds'))
+                ->setParameter('entityIds', $entityIds);
+        }
+
+        $queryBuilder->resetDQLPart('orderBy');
+        $queryBuilder->resetDQLPart('select');
+        $queryBuilder->select($tableAlias . '.' . $identifier);
+
+        return $queryBuilder;
+    }
+
+    /**
+     * @param QueryBuilder $queryBuilder
+     * @param Segment $segment
+     */
+    private function addSegmentSnapshotFields(QueryBuilder $queryBuilder, Segment $segment): void
+    {
+        $queryBuilder->addSelect((string)$queryBuilder->expr()->literal($segment->getId()));
+        $queryBuilder->addSelect('CURRENT_TIMESTAMP()');
+    }
+
+    /**
+     * @param string $fieldToSelect
+     * @param string $selectSql
+     * @param array $values
+     * @param array $types
+     * @throws \Doctrine\DBAL\DBALException
+     */
+    private function executeInsertFromSelect(
+        string $fieldToSelect,
+        string $selectSql,
+        array $values,
+        array $types
+    ) {
+        $dbQuery = 'INSERT INTO oro_segment_snapshot (' . $fieldToSelect . ', segment_id, createdat) (%s)';
+        $dbQuery = sprintf($dbQuery, $selectSql);
+
+        $this->em->getConnection()->executeQuery($dbQuery, $values, $types);
+    }
+
+    /**
+     * @param string $fieldToSelect
+     * @param string $selectSql
+     * @param array $values
+     * @param array $types
+     * @param Segment $segment
+     * @throws \Doctrine\DBAL\DBALException
+     */
+    private function executeInsertValues(
+        string $fieldToSelect,
+        string $selectSql,
+        array $values,
+        array $types,
+        Segment $segment
+    ) {
+        $stmt = $this->em->getConnection()->executeQuery($selectSql, $values, $types);
+        $insertValues = [];
+        $isIntegerId = $fieldToSelect === 'integer_entity_id';
+        while ($row = $stmt->fetch()) {
+            $entityId = reset($row);
+            $insertValues[] = sprintf(
+                '(%s, %d, %s)',
+                $isIntegerId ? $entityId : "'{$entityId}'",
+                $segment->getId(),
+                'NOW()'
+            );
+        }
+
+        if ($insertValues) {
+            $this->em->getConnection()->executeUpdate(
+                'INSERT INTO oro_segment_snapshot (' . $fieldToSelect . ', segment_id, createdat) VALUES'
+                . implode(',', $insertValues),
+                $values,
+                $types
+            );
+        }
+    }
+
+    /**
+     * @param Segment $segment
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     */
+    private function updateSegmentLastRun(Segment $segment): void
+    {
+        /** @var Segment $segment */
+        $segment = $this->em->merge($segment);
+        $segment->setLastRun(new \DateTime('now', new \DateTimeZone('UTC')));
+        $this->em->persist($segment);
+        $this->em->flush($segment);
     }
 }
