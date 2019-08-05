06c50da590c5a5599145308eda580ee99b381e85
Viacheslav Chekanovskyi <vchekanovskyi@oroinc.com>
2019-08-05 16:37:20 +0300

    BB-17281: Load styles from GrapesJS library - load node_modules dependency from npmassets dir

--- build/webpack-config-builder/oro-webpack-config.js
+++ build/webpack-config-builder/oro-webpack-config.js
@@ -91,8 +91,12 @@ class ConfigBuilder {
                     modules: [
                         resolvedPublicPath,
                         resolvedPublicPath + '/bundles',
-                        path.join(__dirname, '../node_modules'),
+                        path.join(__dirname, '../node_modules')
                     ],
+                    extensions: ['*', '.css', '.scss', '.js'],
+                    alias: {
+                        'node_modules':  resolvedPublicPath + '/bundles/npmassets'
+                    },
                     symlinks: false
                 },
                 resolveLoader: {
@@ -138,7 +142,6 @@ class ConfigBuilder {
                                     includePaths: [
                                         resolvedPublicPath + '/bundles',
                                         path.resolve(__dirname, '../node_modules'),
-
                                     ],
                                     sourceMap: true
                                 }
