bad1063af71b5a6f1ec5b112e72797b4420c161a
Mykhailo Sulyma <msulima@oroinc.com>
2019-08-09 16:11:47 +0300

    BB-17327: Additional Product Fields (#24851)
    - added attributeFamily field
    - updated product create page and category info placeholder templates
     - added label for type field in the second step product form
     - added grid listener
     - updated grid configuration
     - updated products-grid listener
     - added attribute family provider
     - updated products-grid configuration with new `attributeFamily` field
     - updated PriceListRequestHandler::getPriceListSelectedCurrencies() to return empty array by default
     - added unit tests
     - taxCode field disabled by default
     - show listing product image on grid and main product image in the gallery
     - currencies list does not change state while price list changed
     - updated attributeFamily filter in products-grid configuration
     - added additional method to the product repository to get both main and listing images for productIds
     - added search handler for the product family search
     - updated product image template
     - added grid listener
     - updated grid configuration
     - updated products-grid listener
     - added attribute family provider
     - updated products-grid configuration with new `attributeFamily` field
     - updated PriceListRequestHandler::getPriceListSelectedCurrencies() to return empty array by default
     - added unit tests
     - added attributeFamily field
     - updated product create page and category info placeholder templates
     - added label for type field in the second step product form
     - taxCode field disabled by default
     - show listing product image on grid and main product image in the gallery
     - currencies list does not change state while price list changed
     - updated attributeFamily filter in products-grid configuration
     - updated products-grid event listener
     - added additional method to the product repository to get both main and listing images for productIds
     - updated product image template
     - added behat tests for the create and view product pages
     - fixed failed builds
     - updated grid configuration
     - added behat scenarios for the products-grid
     - added scenarios to check product image on grid
     - made `updated at` fields renderable by default and enable filter for it

---
+++ src/Oro/Bundle/DataGridBundle/Tests/Behat/Element/ChoiceTreeInput.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Oro\Bundle\DataGridBundle\Tests\Behat\Element;
+
+use Behat\Mink\Element\NodeElement;
+use Oro\Bundle\TestFrameworkBundle\Behat\Element\Element;
+
+class ChoiceTreeInput extends Element
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getValue()
+    {
+        $valueElements = $this->getParent()->getParent()->findAll('css', 'li.select2-search-choice');
+
+        return array_map(function (NodeElement $element) {
+            return $element->getText();
+        }, $valueElements);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setValue($values)
+    {
+        $this->getDriver()->waitForAjax();
+        $this->focus();
+        $values = true === is_array($values) ? $values : [$values];
+
+        foreach ($values as $value) {
+            $this->type($value);
+            $searchResults = $this->getSearchResults();
+            self::assertCount(
+                1,
+                $searchResults,
+                sprintf(
+                    'Too many results "%s" for "%s"',
+                    implode(', ', array_map([$this, 'elementText'], $searchResults)),
+                    $value
+                )
+            );
+            $result = array_shift($searchResults);
+            self::assertNotEquals('No matches found', $result->getText(), sprintf('No matches found for "%s"', $value));
+            $result->click();
+        }
+    }
+
+    /**
+     * Focus on input and wait until it will be focused
+     */
+    public function focus()
+    {
+        $mask = $this->getPage()->find('css', '#select2-drop-mask');
+        if ($mask && $mask->isVisible()) {
+            $mask->click();
+        }
+
+        if (!empty($this->getSearchResults())) {
+            parent::click();
+        }
+    }
+
+    /**
+     * Type into input
+     * @param string $value
+     */
+    private function type($value)
+    {
+        $this->getDriver()->typeIntoInput($this->getXpath(), $value);
+    }
+
+    /**
+     * @return array|NodeElement[]
+     */
+    private function getSearchResults()
+    {
+        $this->getPage()->waitFor(5, function (NodeElement $element) {
+            return null === $element->find('css', 'ul.select2-results li.select2-searching');
+        });
+
+        $resultHolder = $this->getPage()->findVisible('css', 'div.select2-drop-active ul.select2-results');
+        if (!$resultHolder) {
+            return [];
+        }
+
+        // Wait for show one result or go further
+        $resultHolder->waitFor(5, function (NodeElement $element) {
+            return 1 === count($element->findAll('css', 'li'));
+        });
+
+        return $resultHolder->findAll('css', 'li');
+    }
+
+    /**
+     * Return element text
+     * @param NodeElement $element
+     * @return string
+     */
+    public static function elementText(NodeElement $element)
+    {
+        return $element->getText();
+    }
+}
