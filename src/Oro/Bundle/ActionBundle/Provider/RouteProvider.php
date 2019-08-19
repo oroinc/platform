36464a1b5ee9f5401fe289ddf8aad75363e4218c
Vova Soroka <vsoroka@oroinc.com>
2019-08-20 02:55:29 +0300

    BB-16920: Organization Identification via Security Context on the Storefront (#24919)
    - add Oro\Bundle\CustomerBundle\Security\TokenAwareFrontendHelper class and use if instead of Oro\Bundle\FrontendBundle\Request\FrontendHelper; it allows to use FrontendHelper::isFrontendRequest() in web requests and in MQ consumer to check if a code works in the storefront of the management console security context
    - fix usage of Oro\Bundle\FrontendBundle\Request\FrontendHelper
    - fix usage of Oro\Bundle\ActionBundle\Provider\CurrentApplicationProviderInterface

--- src/Oro/Bundle/ActionBundle/Provider/RouteProvider.php
+++ src/Oro/Bundle/ActionBundle/Provider/RouteProvider.php
@@ -2,9 +2,22 @@
 
 namespace Oro\Bundle\ActionBundle\Provider;
 
+/**
+ * The provider for action routes.
+ */
 class RouteProvider implements RouteProviderInterface
 {
-    use RouteProviderTrait;
+    /** @var string */
+    private $formDialogRoute;
+
+    /** @var string */
+    private $formPageRoute;
+
+    /** @var string */
+    private $executionRoute;
+
+    /** @var string|null */
+    private $widgetRoute;
 
     /**
      * @param string $formDialogRoute
@@ -18,5 +31,37 @@ class RouteProvider implements RouteProviderInterface
         $this->formPageRoute = $formPageRoute;
         $this->executionRoute = $executionRoute;
         $this->widgetRoute = $widgetRoute;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getWidgetRoute()
+    {
+        return $this->widgetRoute;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFormDialogRoute()
+    {
+        return $this->formDialogRoute;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFormPageRoute()
+    {
+        return $this->formPageRoute;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getExecutionRoute()
+    {
+        return $this->executionRoute;
     }
 }
