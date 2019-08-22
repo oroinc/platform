0f79289a4ebdef6ca858212a471471082bac4ab2
Viacheslav Bemza <vbemza@oroinc.com>
2019-08-22 17:44:53 +0300

    BB-17296: Incorrect cache key generation in PromotionExecutor and TaxManager (#24836)
    - build cache key providers with registry
    - used for promotion executor
    - use serializer component instead deep copy
    - remove BC breaks for 3.1 branch

--- src/Oro/Bundle/CacheBundle/DependencyInjection/OroCacheExtension.php
+++ src/Oro/Bundle/CacheBundle/DependencyInjection/OroCacheExtension.php
@@ -2,11 +2,17 @@
 
 namespace Oro\Bundle\CacheBundle\DependencyInjection;
 
+use Oro\Component\Config\Loader\CumulativeConfigLoader;
+use Oro\Component\Config\Loader\YamlCumulativeFileLoader;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\Serializer\Mapping\Factory\ClassMetadataFactory;
+use Symfony\Component\Serializer\Mapping\Loader\LoaderChain;
+use Symfony\Component\Serializer\Mapping\Loader\YamlFileLoader as SerializerYamlFileLoader;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -20,6 +26,8 @@ class OroCacheExtension extends Extension implements PrependExtensionInterface
      */
     public function load(array $configs, ContainerBuilder $container)
     {
+        $this->loadMetadataFactoryDefinition($container);
+
         $configuration = new Configuration();
         $this->processConfiguration($configuration, $configs);
 
@@ -54,5 +62,24 @@ class OroCacheExtension extends Extension implements PrependExtensionInterface
             'jms_serializer',
             ['metadata' => ['cache' => 'oro_cache.jms_serializer_adapter']]
         );
+    }
+
+    /**
+     * @param ContainerBuilder $container
+     */
+    protected function loadMetadataFactoryDefinition(ContainerBuilder $container)
+    {
+        $configLoader = new CumulativeConfigLoader(
+            'oro_cache_attributes',
+            new YamlCumulativeFileLoader('Resources/config/oro/cache_metadata.yml')
+        );
+        $resources = $configLoader->load();
+        $serializerFileLoaders = [];
+        foreach ($resources as $resource) {
+            $serializerFileLoaders[] = new Definition(SerializerYamlFileLoader::class, [$resource->path]);
+        }
+        $loader = new Definition(LoaderChain::class, [$serializerFileLoaders]);
+        $definition = new Definition(ClassMetadataFactory::class, [$loader]);
+        $container->setDefinition('oro.cache.serializer.mapping.factory.class_metadata', $definition);
     }
 }
