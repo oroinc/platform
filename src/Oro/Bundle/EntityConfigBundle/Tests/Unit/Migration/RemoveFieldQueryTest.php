fa510d9090a30a1a5f194c188cf5825cd83667f0
Viacheslav Bemza <vbemza@oroinc.com>
2019-08-28 15:41:35 +0300

    BAP-19177: Cannot programmaticaly remove extended field with index (#24859)
    * BAP-19177: Cannot programmaticaly remove extended field with index
    - clear class metadata after field removing

--- src/Oro/Bundle/EntityConfigBundle/Tests/Unit/Migration/RemoveFieldQueryTest.php
+++ src/Oro/Bundle/EntityConfigBundle/Tests/Unit/Migration/RemoveFieldQueryTest.php
@@ -45,11 +45,92 @@ class RemoveFieldQueryTest extends \PHPUnit\Framework\TestCase
 
     public function testExecute()
     {
-        $migration = new RemoveFieldQuery('TestClassName', 'TestFieldName');
+        $entityClass = 'TestClassName';
+        $fieldName = 'TestFieldName';
+        $migration = new RemoveFieldQuery($entityClass, $fieldName);
         $migration->setConnection($this->connector);
 
-        $this->connector->expects(self::once())->method('fetchAssoc')->willReturn(['id' => 1]);
-        $this->connector->expects(self::once())->method('prepare')->willReturn($this->statement);
+        $dbData = 'someDbData';
+        $this->connector->expects(self::exactly(2))
+            ->method('fetchAssoc')
+            ->willReturnOnConsecutiveCalls(
+                ['id' => 1],
+                ['data' => $dbData]
+            );
+        $phpData = [
+            'someKey' => 'someData',
+            'extend' => [
+                'index' => [
+                    $fieldName => 'someIndexData',
+                    'otherFieldName' => 'otherIndexData',
+                ],
+                'schema' => [
+                    'entity' => 'EX_' . $entityClass,
+                    'property' => [
+                        $fieldName => 'propertyData',
+                        'otherFieldName' => 'otherPropertyData',
+                    ],
+                    'doctrine' => [
+                        'EX_' . $entityClass => [
+                            'fields' => [
+                                $fieldName => 'someDoctrineData',
+                                'otherFieldName' => 'otherDoctrineData',
+                            ]
+                        ],
+                    ],
+                ]
+            ],
+        ];
+        $this->connector->expects(self::once())
+            ->method('convertToPHPValue')
+            ->with($dbData, 'array')
+            ->willReturn($phpData);
+
+        $updatedData = [
+            'someKey' => 'someData',
+            'extend' => [
+                'index' => [
+                    'otherFieldName' => 'otherIndexData',
+                ],
+                'schema' => [
+                    'entity' => 'EX_' . $entityClass,
+                    'property' => [
+                        'otherFieldName' => 'otherPropertyData',
+                    ],
+                    'doctrine' => [
+                        'EX_' . $entityClass => [
+                            'fields' => [
+                                'otherFieldName' => 'otherDoctrineData',
+                            ]
+                        ],
+                    ],
+                ]
+            ],
+        ];
+        $toDbData = 'resultDataToDb';
+        $this->connector->expects(self::once())
+            ->method('convertToDatabaseValue')
+            ->with($updatedData, 'array')
+            ->willReturn($toDbData);
+
+        $statement1 = $this->createMock(Statement::class);
+        $statement1->expects($this->once())
+            ->method('execute')
+            ->with([1]);
+        $statement2 = $this->createMock(Statement::class);
+        $statement2->expects($this->once())
+            ->method('execute')
+            ->with([$toDbData, $entityClass]);
+        $this->connector->expects(self::exactly(2))
+            ->method('prepare')
+            ->withConsecutive(
+                ['DELETE FROM oro_entity_config_field WHERE id = ?'],
+                ['UPDATE oro_entity_config SET data = ? WHERE class_name = ?']
+            )
+            ->willReturnOnConsecutiveCalls(
+                $statement1,
+                $statement2
+            );
         $migration->execute($this->logger);
     }
 }
