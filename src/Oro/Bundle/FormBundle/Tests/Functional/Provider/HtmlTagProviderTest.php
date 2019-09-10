8b567aa2c26fe93cd0cd1afe5dae53beb0e448b4
Viacheslav Chekanovskyi <vchekanovskyi@oroinc.com>
2019-09-10 14:25:43 +0300

    BB-17314: Add base table styles for content blocks and landing pages - add tables for cms pages

--- src/Oro/Bundle/FormBundle/Tests/Functional/Provider/HtmlTagProviderTest.php
+++ src/Oro/Bundle/FormBundle/Tests/Functional/Provider/HtmlTagProviderTest.php
@@ -101,10 +101,11 @@ class HtmlTagProviderTest extends WebTestCase
             '<font></font><i></i><br><span></span><img><h1></h1><h2></h2><h3></h3><h4></h4><h5></h5><h6></h6>' .
             '<hgroup></hgroup><abbr></abbr><address></address><article></article><audio></audio><bdo></bdo>' .
             '<blockquote></blockquote><caption></caption><cite></cite><code></code><col></col><colgroup></colgroup>' .
-            '<dd></dd><del></del><details></details><dfn></dfn><figure></figure><figcaption></figcaption><picture></picture>' .
-            '<footer></footer><header></header><hr></hr><ins></ins><kbd></kbd><mark></mark><menu></menu><nav></nav>' .
-            '<pre></pre><q></q><samp></samp><section></section><small></small><source></source><sub></sub><sup></sup>' .
-            '<time></time><tfoot></tfoot><var></var><video></video><aside></aside>';
+            '<dd></dd><del></del><details></details><dfn></dfn><figure></figure><figcaption></figcaption>' .
+            '<picture></picture><footer></footer><header></header><hr></hr><ins></ins><kbd></kbd><mark></mark>' .
+            '<menu></menu><nav></nav><pre></pre><q></q><samp></samp><section></section><small></small>' .
+            '<source></source><sub></sub><sup></sup><time></time><tfoot></tfoot><var></var><video></video>' .
+            '<aside></aside>';
     }
 
     public function testGetAllowedElements()
