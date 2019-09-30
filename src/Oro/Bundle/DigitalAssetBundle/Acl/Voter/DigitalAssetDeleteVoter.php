bafd7ecb19578d7df7e9002fe96c5ecb08420a02
Yaroslav Bracilo <ibratsilo@oroinc.com>
2019-09-30 14:38:56 +0300

    BB-17844: Add delete functionality with ACL voter (#25338)
    - added DigitalAssetDeleteVoter

---
+++ src/Oro/Bundle/DigitalAssetBundle/Acl/Voter/DigitalAssetDeleteVoter.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Oro\Bundle\DigitalAssetBundle\Acl\Voter;
+
+use Oro\Bundle\DigitalAssetBundle\Entity\DigitalAsset;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\VoterInterface;
+
+/**
+ * Checks if DigitalAsset can be deleted depending of exisiting child files
+ */
+class DigitalAssetDeleteVoter implements VoterInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function vote(TokenInterface $token, $subject, array $attributes): int
+    {
+        if (!$subject instanceof DigitalAsset) {
+            return self::ACCESS_ABSTAIN;
+        }
+
+        foreach ($attributes as $attribute) {
+            if ($attribute !== 'DELETE') {
+                continue;
+            }
+
+            if ($subject->getChildFiles()->count()) {
+                return self::ACCESS_DENIED;
+            }
+        }
+
+        return self::ACCESS_ABSTAIN;
+    }
+}
