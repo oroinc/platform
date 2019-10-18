85a4bf0fd8b94a20398954ed16ee7738055d379f
Andrey Sevastianov <mrpkmail@gmail.com>
2019-10-18 12:22:15 +0300

    BB-17267: Contact ID displayed on grid and exported with thousands Separator (#25375)
     - added primary key guesser for save original column formatting, unit test for PrimaryKeyColumnOptionsGuesser
     - added behat for check id formatting
     - updated fixture loader

---
+++ src/Oro/Bundle/DataGridBundle/Tests/Unit/Datagrid/PrimaryKeyColumnOptionsGuesserTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Oro\Bundle\DataGridBundle\Tests\Unit\Datagrid;
+
+use Doctrine\Bundle\DoctrineBundle\Registry;
+use Doctrine\Common\Persistence\Mapping\ClassMetadata;
+use Doctrine\Common\Persistence\ObjectManager;
+use Oro\Bundle\DataGridBundle\Datagrid\DefaultColumnOptionsGuesser;
+use Oro\Bundle\DataGridBundle\Datagrid\Guess\ColumnGuess;
+use Oro\Bundle\DataGridBundle\Datagrid\PrimaryKeyColumnOptionsGuesser;
+
+class PrimaryKeyColumnOptionsGuesserTest extends \PHPUnit\Framework\TestCase
+{
+    /** @var Registry|\PHPUnit\Framework\MockObject\MockObject */
+    private $registry;
+
+    /** @var DefaultColumnOptionsGuesser */
+    protected $guesser;
+
+    protected function setUp(): void
+    {
+        $this->registry = $this->createMock(Registry::class);
+        $this->guesser = new PrimaryKeyColumnOptionsGuesser($this->registry);
+    }
+
+    /**
+     * @dataProvider guessFormatterProvider
+     *
+     * @param array|null $identifier
+     * @param ColumnGuess|null $expected
+     */
+    public function testGuessFormatter(?array $identifier, ?ColumnGuess $expected): void
+    {
+        $classMetadata = $this->createMock(ClassMetadata::class);
+        $classMetadata->expects($this->once())
+            ->method('getIdentifier')
+            ->willReturn($identifier);
+
+        $manager = $this->createMock(ObjectManager::class);
+        $manager->expects($this->once())
+            ->method('getClassMetadata')
+            ->with('TestClass')
+            ->willReturn($classMetadata);
+
+        $this->registry->expects($this->any())
+            ->method('getManagerForClass')
+            ->with('TestClass')
+            ->willReturn($manager);
+
+        $this->assertEquals($expected, $this->guesser->guessFormatter('TestClass', 'testProp', 'testType'));
+    }
+
+    /**
+     * @return array
+     */
+    public function guessFormatterProvider(): array
+    {
+        return [
+            'testProp is primary key' => [
+                'identifier' => ['testProp'],
+                'expected' => new ColumnGuess(['frontend_type' => 'string'], ColumnGuess::MEDIUM_CONFIDENCE),
+            ],
+
+            'testProp not primary key' => [
+                'identifier' => ['id'],
+                'expected' => null,
+            ],
+
+            'testProp in part of composite primary key' => [
+                'identifier' => ['propA', 'testProp', 'propC'],
+                'expected' => new ColumnGuess(['frontend_type' => 'string'], ColumnGuess::MEDIUM_CONFIDENCE),
+            ],
+
+            'testProp not in part of composite primary key' => [
+                'identifier' => ['propA', 'propB', 'propC'],
+                'expected' => null,
+            ],
+        ];
+    }
+}
