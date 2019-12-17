2d9b2ecd44c2133fbe7be9c2c65306ade20907bd
Yefim Y <webevt@users.noreply.github.com>
2019-12-17 15:53:06 +0200

    BB-18432: Composer JS Dependencies (#26112)
    - added npm install/ci in post install script
    - migrated bower dependencies to npm
    - updated dev.json, composer.json files
    - got rid of asset-packagist configs in composer.json files
    - updated 3rd-party-dependencies.md
    - added ORO_NODEJS=12 to jenkins builds configs
    - locked doctrine/persistence to 1.2.*
    - removed unused repo asset-packagist

--- src/Oro/Bundle/InstallerBundle/Composer/ScriptHandler.php
+++ src/Oro/Bundle/InstallerBundle/Composer/ScriptHandler.php
@@ -2,45 +2,160 @@
 
 namespace Oro\Bundle\InstallerBundle\Composer;
 
+use Composer\Composer;
+use Composer\IO\IOInterface;
 use Composer\Script\Event;
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Process\Process;
 use Symfony\Component\Yaml\Parser;
 use Symfony\Component\Yaml\Yaml;
 
 /**
- * Installs the assets for installer bundle, bower and npm dependencies
+ * Script handler for composer.
+ * - installs npm assets, copies them to public directory
+ * - sets assets version in parameters.yml
+ * - sets permission on app directories
  */
 class ScriptHandler
 {
     /**
-     * Installs the assets for installer bundle, bower and npm dependencies
+     * Installs npm assets
      *
      * @param Event $event A instance
      */
     public static function installAssets(Event $event)
     {
         $options = self::getOptions($event);
-        $webDir = $options['symfony-web-dir'];
-        $vendorDir = $event->getComposer()->getConfig()->get('vendor-dir');
-
-        $config = [
-            [
-                'from' => $vendorDir.'/bower-asset',
-                'to' => $webDir.'/bundles/bowerassets',
-            ],
-            [
-                'from' => $vendorDir.'/npm-asset',
-                'to' => $webDir.'/bundles/npmassets',
-            ],
-        ];
+        $npmAssets = self::collectNpmAssets($event->getComposer());
+        if (!$npmAssets) {
+            return;
+        }
 
         $filesystem = new Filesystem();
 
-        foreach ($config as $item) {
-            if ($filesystem->exists($item['from'])) {
-                $filesystem->remove($item['to']);
-                $filesystem->mirror($item['from'], $item['to']);
+        // File package.json with actual dependencies is required for correct work of npm.
+        $packageJson = [
+            'description' => 'FILE IS GENERATED PROGRAMMATICALLY, NOT TRACKED BY VCS, ALL MANUAL CHANGES WILL BE LOST',
+            'dependencies' => $npmAssets,
+            'private' => true,
+        ];
+        $filesystem->dumpFile('package.json', json_encode($packageJson));
+
+        if (!$filesystem->exists('package-lock.json')) {
+            // Creates lock file, installs assets.
+            self::npmInstall($event->getIO(), $options['process-timeout'], $event->isDevMode());
+        } else {
+            // Installs assets using lock file.
+            self::npmCi($event->getIO(), $options['process-timeout'], $event->isDevMode());
+        }
+
+        self::copyAssets('node_modules', $options['symfony-web-dir'] . '/bundles/npmassets');
+    }
+
+    /**
+     * Collects npm assets from extra.npm section of installed packages.
+     *
+     * @param Composer $composer
+     *
+     * @return array
+     */
+    private static function collectNpmAssets(Composer $composer): array
+    {
+        $rootPackage = $composer->getPackage();
+
+        // Gets array of installed packages.
+        $packages = $composer->getRepositoryManager()->getLocalRepository()->getCanonicalPackages();
+
+        $npmAssets = [[]];
+        $packageNpm = $rootPackage->getExtra()['npm'] ?? [];
+        if (is_array($packageNpm)) {
+            $npmAssets[$rootPackage->getName()] = $packageNpm;
+        }
+
+        foreach ($packages as $package) {
+            $packageNpm = $package->getExtra()['npm'] ?? [];
+
+            if ($packageNpm && is_array($packageNpm) && !isset($npmAssets[$package->getName()])) {
+                $npmAssets[$package->getName()] = $packageNpm;
             }
+        }
+
+        $npmAssets = array_merge(...array_values($npmAssets));
+
+        return $npmAssets;
+    }
+
+    /**
+     * Runs "npm install", updates package-lock.json, installs assets to "node_modules/"
+     *
+     * @param IOInterface $inputOutput
+     * @param int $timeout
+     * @param bool $verbose
+     */
+    private static function npmInstall(
+        IOInterface $inputOutput,
+        int $timeout = 60,
+        bool $verbose = false
+    ): void {
+        $logLevel = $verbose ? 'info' : 'error';
+        $npmInstallCmd = 'npm install --no-audit --save-exact --no-optional --loglevel ' . $logLevel;
+
+        if (self::runProcess($inputOutput, $npmInstallCmd, $timeout) !== 0) {
+            throw new \RuntimeException('Failed to generate package-lock.json');
+        }
+    }
+
+    /**
+     * @param IOInterface $inputOutput
+     * @param string $cmd
+     * @param int $timeout
+     *
+     * @return int
+     */
+    private static function runProcess(IOInterface $inputOutput, string $cmd, int $timeout): int
+    {
+        $inputOutput->write($cmd);
+
+        $npmInstall = new Process($cmd, null, null, null, $timeout);
+        $npmInstall->run(function ($outputType, string $data) use ($inputOutput) {
+            if ($outputType === Process::OUT) {
+                $inputOutput->write($data, false);
+            } else {
+                $inputOutput->writeError($data, false);
+            }
+        });
+
+        return $npmInstall->getExitCode();
+    }
+
+    /**
+     * Runs "npm ci", installs assets to "node_modules/" using only package-lock.json
+     *
+     * @param IOInterface $inputOutput
+     * @param int $timeout
+     * @param bool $verbose
+     */
+    private static function npmCi(IOInterface $inputOutput, int $timeout = 60, bool $verbose = false): void
+    {
+        $logLevel = $verbose ? 'info' : 'error';
+        $npmCiCmd = 'npm ci --loglevel ' . $logLevel;
+
+        if (self::runProcess($inputOutput, $npmCiCmd, $timeout) !== 0) {
+            throw new \RuntimeException('Failed to install npm assets');
+        }
+    }
+
+    /**
+     * @param string $from
+     * @param string $to
+     */
+    private static function copyAssets(string $from, string $to): void
+    {
+        $filesystem = new Filesystem();
+
+        if ($filesystem->exists($from)) {
+            $filesystem->remove($to);
+            $filesystem->mirror($from, $to);
         }
     }
 
