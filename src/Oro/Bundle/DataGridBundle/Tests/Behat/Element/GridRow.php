e28f3e2f924cfcd0b2e62287577eccef45d75882
Yaroslav Bracilo <ibratsilo@oroinc.com>
2020-02-04 12:13:50 +0200

    BB-13601: Scenario Quickly create Lead in a single form failed  - added spin to avoid premature ElementNotFoundException

--- src/Oro/Bundle/DataGridBundle/Tests/Behat/Element/GridRow.php
+++ src/Oro/Bundle/DataGridBundle/Tests/Behat/Element/GridRow.php
@@ -62,10 +62,15 @@ class GridRow extends TableRow
     {
         $cell = $this->startInlineEditing($header);
 
-        $this->getElement('OroForm')->fillField(
-            'value',
-            new InputValue(InputMethod::TYPE, $value)
-        );
+        //Tries to locate element several times to prevent premature ElementNotFoundException
+        $this->spin(function () use ($value) {
+            $this->getElement('OroForm')->fillField(
+                'value',
+                new InputValue(InputMethod::TYPE, $value)
+            );
+
+            return true;
+        });
 
         $this->getDriver()->waitForAjax();
 
