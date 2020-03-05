98bff50d988e9a80fb39955340b9f845f930a8a9
Oleg Zinchenko <ozinchenko@oroinc.com>
2020-03-05 12:25:57 +0200

    BAP-18470: Checking access in layout ignores guest roles permission - added tests to expose the issue - tuned AclProvider to check permissions for guest user - tuned shopping list widget visible condition - tuned shopping list buttons visible condition - removed deprecations from AclProvider and its test

--- src/Oro/Bundle/SecurityBundle/Layout/DataProvider/AclProvider.php
+++ src/Oro/Bundle/SecurityBundle/Layout/DataProvider/AclProvider.php
@@ -2,35 +2,31 @@
 
 namespace Oro\Bundle\SecurityBundle\Layout\DataProvider;
 
-use Doctrine\Common\Persistence\ManagerRegistry;
 use Doctrine\ORM\EntityManager;
-use Oro\Bundle\SecurityBundle\Authentication\TokenAccessorInterface;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Security\Acl\Util\ClassUtils;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
+/**
+ * Layout ACL provider Class
+ */
 class AclProvider
 {
     /** @var AuthorizationCheckerInterface */
     protected $authorizationChecker;
-
-    /** @var TokenAccessorInterface */
-    protected $tokenAccessor;
 
     /** @var ManagerRegistry */
     protected $doctrine;
 
     /**
      * @param AuthorizationCheckerInterface $authorizationChecker
-     * @param TokenAccessorInterface        $tokenAccessor
      * @param ManagerRegistry               $doctrine
      */
     public function __construct(
         AuthorizationCheckerInterface $authorizationChecker,
-        TokenAccessorInterface $tokenAccessor,
         ManagerRegistry $doctrine
     ) {
         $this->authorizationChecker = $authorizationChecker;
-        $this->tokenAccessor = $tokenAccessor;
         $this->doctrine = $doctrine;
     }
 
@@ -39,12 +35,8 @@ class AclProvider
      * @param mixed           $object
      * @return bool
      */
-    public function isGranted($attributes, $object = null)
+    public function isGranted($attributes, $object = null): bool
     {
-        if (!$this->tokenAccessor->hasUser()) {
-            return false;
-        }
-
         if (is_object($object)) {
             $class = ClassUtils::getRealClass($object);
             $objectManager = $this->doctrine->getManagerForClass($class);
