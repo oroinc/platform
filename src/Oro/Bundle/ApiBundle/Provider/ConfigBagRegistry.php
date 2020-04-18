b693654b83664925f86de589b59f5a58bc7c8014
Vova Soroka <vsoroka@oroinc.com>
2020-04-19 01:48:29 +0300

    Small performance improvement of API ConfigBag (#27575)

--- src/Oro/Bundle/ApiBundle/Provider/ConfigBagRegistry.php
+++ src/Oro/Bundle/ApiBundle/Provider/ConfigBagRegistry.php
@@ -5,12 +5,13 @@ namespace Oro\Bundle\ApiBundle\Provider;
 use Oro\Bundle\ApiBundle\Request\RequestType;
 use Oro\Bundle\ApiBundle\Util\RequestExpressionMatcher;
 use Psr\Container\ContainerInterface;
+use Symfony\Contracts\Service\ResetInterface;
 
 /**
  * The registry that allows to get the API resources configuration bag
  * for a specific request type.
  */
-class ConfigBagRegistry
+class ConfigBagRegistry implements ResetInterface
 {
     /** @var array [[config bag service id, request type expression], ...] */
     private $configBags;
@@ -55,23 +56,35 @@ class ConfigBagRegistry
             return $this->cache[$cacheKey];
         }
 
-        $configBagServiceId = null;
-        foreach ($this->configBags as list($serviceId, $expression)) {
+        /** @var ConfigBagInterface|null $configBag */
+        $configBag = null;
+        foreach ($this->configBags as [$serviceId, $expression]) {
             if (!$expression || $this->matcher->matchValue($expression, $requestType)) {
-                $configBagServiceId = $serviceId;
+                $configBag = $this->container->get($serviceId);
                 break;
             }
         }
-        if (null === $configBagServiceId) {
+        if (null === $configBag) {
             throw new \LogicException(
                 sprintf('Cannot find a config bag for the request "%s".', (string)$requestType)
             );
         }
 
-        /** @var ConfigBagInterface $configBag */
-        $configBag = $this->container->get($configBagServiceId);
         $this->cache[$cacheKey] = $configBag;
 
         return $configBag;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function reset()
+    {
+        foreach ($this->cache as $configBag) {
+            if ($configBag instanceof ResetInterface) {
+                $configBag->reset();
+            }
+        }
+        $this->cache = [];
     }
 }
