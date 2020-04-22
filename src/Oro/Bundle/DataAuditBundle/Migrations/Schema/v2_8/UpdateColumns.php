c3ceac23ae747163389b3698ea2ebadd80d2385e
Dima Sherstuyk <43877109+DimaSherstuyk@users.noreply.github.com>
2020-04-22 16:52:33 +0300

    BAP-18992: Exception "More than one result was found for query althou… (#27591)
    * BAP-18992: Exception "More than one result was found for query although one row or none was expected" in EntityChangesToAuditEntryConverter::findOrCreateAuditEntry
    
    - Added migration
    - Changed length for type and transaction_id fields for oro_audit
    
    * BAP-18992: Exception "More than one result was found for query although one row or none was expected" in EntityChangesToAuditEntryConverter::findOrCreateAuditEntry
    
    - Changed length for fields in annotations
    
    * BAP-18992: Exception "More than one result was found for query although one row or none was expected" in EntityChangesToAuditEntryConverter::findOrCreateAuditEntry
    
    - Changed migration version logic
    - Added translation for missed keys

---
+++ src/Oro/Bundle/DataAuditBundle/Migrations/Schema/v2_8/UpdateColumns.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Oro\Bundle\DataAuditBundle\Migrations\Schema\v2_8;
+
+use Doctrine\DBAL\Schema\Schema;
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+
+/**
+ * Changed length for type and transaction_id fields for oro_audit table
+ */
+class UpdateColumns implements Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function up(Schema $schema, QueryBag $queries): void
+    {
+        $auditTable = $schema->getTable('oro_audit');
+        $auditTable->changeColumn('type', ['length' => 30]);
+        $auditTable->changeColumn('transaction_id', ['length' => 36]);
+    }
+}
