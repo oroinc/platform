e4efaac1057e4f82fe6ada2a7761d205d6c29fb4
Vladimir Seniuk <vladimirseniuk@users.noreply.github.com>
2020-04-29 21:18:13 +0300

    BAP-19808: Extra DB connections created by message consumer (#27618)
    - reset DB connection on start consumer
    - fixed extra default connection after message was processed
    - limit to 1 connection when consumer in Idle state (default connection is used in heartbeat and interrupt consumption extensions)

--- src/Oro/Bundle/MessageQueueBundle/Consumption/Extension/DoctrineClearIdentityMapExtension.php
+++ src/Oro/Bundle/MessageQueueBundle/Consumption/Extension/DoctrineClearIdentityMapExtension.php
@@ -2,53 +2,67 @@
 
 namespace Oro\Bundle\MessageQueueBundle\Consumption\Extension;
 
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ObjectManager;
 use Oro\Component\MessageQueue\Consumption\AbstractExtension;
 use Oro\Component\MessageQueue\Consumption\Context;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class DoctrineClearIdentityMapExtension extends AbstractExtension implements ResettableExtensionInterface
+/**
+ * Clear all entity managers after each message was processed to ensure that all managed entities was detached.
+ */
+class DoctrineClearIdentityMapExtension extends AbstractExtension
 {
     /** @var ContainerInterface */
     private $container;
 
-    /** @var ManagerRegistry|null */
-    private $doctrine;
+    /** @var array [manager name => manager service id, ...] */
+    private $managers;
 
     /**
      * @param ContainerInterface $container
+     * @param array $managers
      */
-    public function __construct(ContainerInterface $container)
+    public function __construct(ContainerInterface $container, array $managers)
     {
         $this->container = $container;
+        $this->managers = $managers;
     }
 
     /**
      * {@inheritdoc}
      */
-    public function reset()
+    public function onPostReceived(Context $context): void
     {
-        $this->doctrine = null;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function onPostReceived(Context $context)
-    {
-        if (null === $this->doctrine) {
-            $this->doctrine = $this->container->get('doctrine');
-        }
+        $managers = $this->getAliveManagers();
 
         $logger = $context->getLogger();
-        $managers = $this->doctrine->getManagerNames();
-        foreach ($managers as $name => $serviceId) {
-            if ($this->container->initialized($serviceId)) {
-                $logger->debug(sprintf('Clear identity map for manager "%s"', $name));
+        $logger->info('Clear entity managers identity map.', ['entity_managers' => array_keys($managers)]);
+        $this->clear($managers);
+    }
 
-                $manager = $this->doctrine->getManager($name);
-                $manager->clear();
+    /**
+     * @return ObjectManager[]
+     */
+    private function getAliveManagers(): array
+    {
+        $aliveManagers = [];
+
+        foreach ($this->managers as $name => $serviceId) {
+            if ($this->container->initialized($serviceId)) {
+                $aliveManagers[$name] = $this->container->get($serviceId);
             }
+        }
+
+        return $aliveManagers;
+    }
+
+    /**
+     * @param ObjectManager[] $managers
+     */
+    private function clear(array $managers): void
+    {
+        foreach ($managers as $manager) {
+            $manager->clear();
         }
     }
 }
