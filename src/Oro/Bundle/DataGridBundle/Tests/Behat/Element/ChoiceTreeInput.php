510e9d264d2503f41bb6f5cac2b98edeeed59f38
Mykhailo Sulyma <msulima@oroinc.com>
2020-05-26 10:56:57 +0300

    BB-19206: Add context of customer user to emails automatically (#27758)
     - added email body listener
     - covered with unit and behat tests

--- src/Oro/Bundle/DataGridBundle/Tests/Behat/Element/ChoiceTreeInput.php
+++ src/Oro/Bundle/DataGridBundle/Tests/Behat/Element/ChoiceTreeInput.php
@@ -31,7 +31,13 @@ class ChoiceTreeInput extends Element
 
         foreach ($values as $value) {
             $this->type($value);
-            $searchResults = $this->getSearchResults();
+            $searchResults = array_filter(
+                $this->getSearchResults(),
+                static function ($element) {
+                    return !empty(trim($element->getText()));
+                }
+            );
+
             self::assertCount(
                 1,
                 $searchResults,
