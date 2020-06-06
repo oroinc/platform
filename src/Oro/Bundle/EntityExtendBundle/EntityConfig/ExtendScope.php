8ee4ce7a8e1f40850e9299cba5694dff401f5462
Vova Soroka <vsoroka@oroinc.com>
2020-06-06 13:11:17 +0300

    BAP-19971: Remove deprecated constants from Oro\Bundle\EntityExtendBundle\EntityConfig\ExtendScope (#27994)
    - remove STATE_UPDATED and STATE_DELETED constants that are deprecated since v1.4

--- src/Oro/Bundle/EntityExtendBundle/EntityConfig/ExtendScope.php
+++ src/Oro/Bundle/EntityExtendBundle/EntityConfig/ExtendScope.php
@@ -2,19 +2,22 @@
 
 namespace Oro\Bundle\EntityExtendBundle\EntityConfig;
 
+/**
+ * Provides constants that represent a state of extended entities and fields.
+ */
 class ExtendScope
 {
     /**
      * New entity or field is registered in entity config and database schema update is required
      * to apply changes to a database and update Doctrine's metadata and proxies.
      */
-    const STATE_NEW = 'New';
+    public const STATE_NEW = 'New';
 
     /**
      * An entity or field properties were modified and database schema update is required
      * to apply this changes to a database and update Doctrine's metadata and proxies.
      */
-    const STATE_UPDATE = 'Requires update';
+    public const STATE_UPDATE = 'Requires update';
 
     /**
      * An entity or field was marked as "to be deleted" and database schema update is required
@@ -23,48 +26,38 @@ class ExtendScope
      * After database schema is updated the deleted entity or field is still available and can be
      * restored if it will be needed. To check if an entity or field is deleted use extend.is_deleted
      */
-    const STATE_DELETE = 'Deleted';
+    public const STATE_DELETE = 'Deleted';
 
     /**
      * The previously deleted entity (not implemented yet) or field was marked as "to be restored"
      * and database schema update is required to apply this changes to a database
      * and update Doctrine's metadata and proxies.
      */
-    const STATE_RESTORE = 'Restored';
+    public const STATE_RESTORE = 'Restored';
 
     /**
      * An entity or field properties and database schema, Doctrine's metadata and proxies are up-to-date.
      */
-    const STATE_ACTIVE  = 'Active';
-
-    /**
-     * @deprecated since 1.4. Will be removed in 2.0. Use STATE_UPDATE instead.
-     */
-    const STATE_UPDATED = 'Requires update';
-
-    /**
-     * @deprecated since 1.4. Will be removed in 2.0. Use STATE_DELETE instead.
-     */
-    const STATE_DELETED = 'Deleted';
+    public const STATE_ACTIVE  = 'Active';
 
     /**
      * The system entities and fields means that they cannot be removed by an administrator
      * and a developer who created them should take care about UI for them.
      */
-    const OWNER_SYSTEM = 'System';
+    public const OWNER_SYSTEM = 'System';
 
     /**
      * The system is fully responsible how the custom entities and fields are used on UI.
      */
-    const OWNER_CUSTOM = 'Custom';
+    public const OWNER_CUSTOM = 'Custom';
 
     /**
      * Changes are made with UI.
      */
-    const ORIGIN_CUSTOM = 'Custom';
+    public const ORIGIN_CUSTOM = 'Custom';
 
     /**
      * Changes are made on System level.
      */
-    const ORIGIN_SYSTEM = 'System';
+    public const ORIGIN_SYSTEM = 'System';
 }
