5a2257a9555645c01f265913caac264494c59213
Vova Soroka <vsoroka@oroinc.com>
2020-06-13 11:31:39 +0300

    BAP-19979: Fix empty namespace for application caches (#28073)
    - add compiler pass to validate that all application caches have namespaces

--- src/Oro/Bundle/CacheBundle/OroCacheBundle.php
+++ src/Oro/Bundle/CacheBundle/OroCacheBundle.php
@@ -3,6 +3,8 @@
 namespace Oro\Bundle\CacheBundle;
 
 use Oro\Bundle\CacheBundle\DependencyInjection\Compiler\CacheConfigurationPass;
+use Oro\Bundle\CacheBundle\DependencyInjection\Compiler\ValidateCacheConfigurationPass;
+use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -19,5 +21,6 @@ class OroCacheBundle extends Bundle
         parent::build($container);
 
         $container->addCompilerPass(new CacheConfigurationPass());
+        $container->addCompilerPass(new ValidateCacheConfigurationPass(), PassConfig::TYPE_BEFORE_REMOVING);
     }
 }
