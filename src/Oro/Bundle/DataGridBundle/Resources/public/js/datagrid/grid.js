c6fb74ad5ff1fa9505758b0c5a373ab3b8e4fadd
ValeriyYustunyk <ValeriyYustunyk@users.noreply.github.com>
2020-09-04 11:15:21 +0300

    BB-19708: Incorrect data in grid when removing simple variant (#28792)

--- src/Oro/Bundle/DataGridBundle/Resources/public/js/datagrid/grid.js
+++ src/Oro/Bundle/DataGridBundle/Resources/public/js/datagrid/grid.js
@@ -1340,7 +1340,7 @@ define(function(require) {
 
             if (collection) {
                 const fetchKeys = ['mode', 'parse', 'reset', 'wait', 'uniqueOnly',
-                    'add', 'remove', 'merge', 'toggleLoading'];
+                    'add', 'remove', 'merge', 'toggleLoading', 'recountTotalRecords'];
                 let fetchOptions = {
                     reset: true
                 };
