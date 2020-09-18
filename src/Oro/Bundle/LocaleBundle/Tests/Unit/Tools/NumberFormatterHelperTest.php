c791ad829bc3694e1fe2d2354945aca02ad481ac
Yefim Yevtushenko <yyevtushenko@oroinc.com>
2020-09-18 16:22:19 +0300

    BAP-20088: Provide support of the libicu65-65.1
    - decomposed NumberFormatter, added factory
    - added tests
    - fixed behat test

---
+++ src/Oro/Bundle/LocaleBundle/Tests/Unit/Tools/NumberFormatterHelperTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Oro\Bundle\LocaleBundle\Tests\Unit\Tools;
+
+use NumberFormatter as IntlNumberFormatter;
+use Oro\Bundle\LocaleBundle\Tools\NumberFormatterHelper;
+
+class NumberFormatterHelperTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @dataProvider parseConstantValueDataProvider
+     *
+     * @param $name
+     */
+    public function testParseConstantValue($name): void
+    {
+        $this->assertEquals(
+            IntlNumberFormatter::CURRENCY,
+            NumberFormatterHelper::parseConstantValue($name)
+        );
+    }
+
+    /**
+     * @return array
+     */
+    public function parseConstantValueDataProvider(): array
+    {
+        return [
+            ['currency'],
+            ['CuRrEnCy'],
+            [IntlNumberFormatter::CURRENCY],
+        ];
+    }
+
+    public function testParseConstantValueWhenInvalidConstant(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('NumberFormatter has no constant \'invalid\'');
+
+        NumberFormatterHelper::parseConstantValue('invalid');
+    }
+}
