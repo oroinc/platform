61a1a1ae548ef20d0ab6e7b772f277fe93cab865
Yefim Yevtushenko <yyevtushenko@oroinc.com>
2020-09-20 19:45:38 +0300

    BAP-20137: Performance: Preloading manager

---
+++ src/Oro/Bundle/EntityBundle/Tests/Functional/DataFixtures/LoadUserEmailData.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Oro\Bundle\EntityBundle\Tests\Functional\DataFixtures;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Oro\Bundle\UserBundle\Entity\Email;
+use Oro\Bundle\UserBundle\Entity\UserInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class LoadUserEmailData extends AbstractFixture implements ContainerAwareInterface
+{
+    /** @var ContainerInterface */
+    protected $container;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $userManager = $this->container->get('oro_user.manager');
+
+        $email = new Email();
+        $email->setEmail('simple_user@example.org');
+
+        /** @var UserInterface $user */
+        $user = $this->getReference('simple_user');
+        $user->addEmail($email);
+        $userManager->updateUser($user);
+
+        $this->setReference('simple_user_email', $email);
+    }
+}
