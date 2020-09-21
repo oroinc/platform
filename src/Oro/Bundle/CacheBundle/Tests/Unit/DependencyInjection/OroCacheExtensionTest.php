02a620f52b589b467659f54d32d82eb8ab30c17d
Andrey Yatsenco <ayatsenko@oroinc.com>
2020-09-21 15:06:42 +0300

    BAP-20121: The application reads cache_metadata.yml files in runtime (#28837)
    - added cache for CacheClassMetadataFactory
    - registered the cache wormer for the new cache
    - updated test

--- src/Oro/Bundle/CacheBundle/Tests/Unit/DependencyInjection/OroCacheExtensionTest.php
+++ src/Oro/Bundle/CacheBundle/Tests/Unit/DependencyInjection/OroCacheExtensionTest.php
@@ -4,14 +4,39 @@ namespace Oro\Bundle\CacheBundle\Tests\Unit\DependencyInjection\Compiler;
 
 use Oro\Bundle\CacheBundle\DependencyInjection\OroCacheExtension;
 use Oro\Bundle\TestFrameworkBundle\Test\DependencyInjection\ExtensionTestCase;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 
 class OroCacheExtensionTest extends ExtensionTestCase
 {
+    /**
+     * @var OroCacheExtension
+     */
     private $extension;
 
     protected function setUp()
     {
         $this->extension = new OroCacheExtension();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function buildContainerMock()
+    {
+        $mockBuilder = $this->createMock(ContainerBuilder::class);
+
+        $warmerServiceDefinition = $this->createMock(Definition::class);
+        $warmerServiceDefinition->expects($this->once())
+            ->method('replaceArgument')
+            ->with(0, $this->isType('array'));
+        $mockBuilder
+            ->expects($this->once())
+            ->method('getDefinition')
+            ->with('oro.cache.serializer.mapping.cache_warmer')
+            ->willReturn($warmerServiceDefinition);
+
+        return $mockBuilder;
     }
 
     public function testLoad()
@@ -23,6 +48,8 @@ class OroCacheExtensionTest extends ExtensionTestCase
             'oro_cache.action.handler.schedule_arguments_builder',
             'oro_cache.action.provider.invalidate_cache_time',
             'oro_cache.action.transformer.datetime_to_cron_format',
+            'oro.cache.serializer.mapping.cache_warmer',
+            'oro.cache.serializer.mapping.factory.class_metadata',
         ];
 
         $this->assertDefinitionsLoaded($expectedDefinitions);
