dfa50fc2526d93ff9cb7c0fd359f4ce247468555
Volodymyr Soroka <vsoroka@oroinc.com>
2020-10-29 19:32:47 +0000

    CRM-9224: Extra "Log in with office365" button on login form with "Google Single Sign-On" enabled - refactor SSOBundle to allow adding new SSO providers

---
+++ src/Oro/Bundle/SSOBundle/Security/Core/User/OAuthUserProviderInterface.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Oro\Bundle\SSOBundle\Security\Core\User;
+
+use HWI\Bundle\OAuthBundle\OAuth\Response\UserResponseInterface;
+use Oro\Bundle\UserBundle\Entity\UserInterface;
+
+/**
+ * Represents OAuth single sign-on authentication user provider for a specific resource owner.
+ */
+interface OAuthUserProviderInterface
+{
+    /**
+     * Checks if this OAuth user provider is enabled.
+     *
+     * @return bool
+     */
+    public function isEnabled(): bool;
+
+    /**
+     * Gets a list of allowed domains.
+     *
+     * @return string[]
+     */
+    public function getAllowedDomains(): array;
+
+    /**
+     * Finds a user.
+     *
+     * @param UserResponseInterface $response
+     *
+     * @return UserInterface|null
+     */
+    public function findUser(UserResponseInterface $response): ?UserInterface;
+}
