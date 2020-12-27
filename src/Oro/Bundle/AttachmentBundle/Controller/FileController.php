7032e98a584c45938c31f313510dfd8d6ca42839
Vova Soroka <vsoroka@oroinc.com>
2020-12-27 10:03:49 +0000

    Refactor attachment file name providers - rename "oro_attachment.manager.media_cache_manager_registry" service to "oro_attachment.media_cache_manager_registry" - rename "oro_attachment.provider.attachment_file_name_provider" service to "oro_attachment.provider.file_name" - rename Oro\Bundle\AttachmentBundle\Provider\AttachmentFileNameProvider to Oro\Bundle\AttachmentBundle\Provider\FileNameProvider - refactor Oro\Bundle\ProductBundle\Provider\ProductImageFileNameProvider - update comments

--- src/Oro/Bundle/AttachmentBundle/Controller/FileController.php
+++ src/Oro/Bundle/AttachmentBundle/Controller/FileController.php
@@ -6,7 +6,6 @@ use Oro\Bundle\AttachmentBundle\Entity\File;
 use Oro\Bundle\AttachmentBundle\Manager\FileManager;
 use Oro\Bundle\AttachmentBundle\Manager\ImageResizeManager;
 use Oro\Bundle\AttachmentBundle\Manager\ImageResizeManagerInterface;
-use Oro\Bundle\AttachmentBundle\Provider\AttachmentFileNameProvider;
 use Oro\Bundle\AttachmentBundle\Provider\FileNameProviderInterface;
 use Oro\Bundle\AttachmentBundle\Provider\FileUrlProviderInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -121,7 +120,7 @@ class FileController extends AbstractController
             || (
                 $fileName !== $file->getFilename()
                 && $fileName !== $file->getOriginalFilename()
-                && $fileName !== $this->getAttachmentFileNameProvider()->getFileName($file)
+                && $fileName !== $this->getFileNameProvider()->getFileName($file)
             )) {
             throw $this->createNotFoundException('File not found');
         }
@@ -151,9 +150,9 @@ class FileController extends AbstractController
         return $this->get('oro_attachment.manager.image_resize');
     }
 
-    private function getAttachmentFileNameProvider(): FileNameProviderInterface
+    private function getFileNameProvider(): FileNameProviderInterface
     {
-        return $this->get('oro_attachment.provider.attachment_file_name_provider');
+        return $this->get('oro_attachment.provider.file_name');
     }
 
     /**
@@ -170,9 +169,9 @@ class FileController extends AbstractController
     public static function getSubscribedServices()
     {
         return array_merge(parent::getSubscribedServices(), [
-            'oro_attachment.file_manager'                           => FileManager::class,
-            'oro_attachment.manager.image_resize'                   => ImageResizeManager::class,
-            'oro_attachment.provider.attachment_file_name_provider' => AttachmentFileNameProvider::class
+            'oro_attachment.file_manager'         => FileManager::class,
+            'oro_attachment.manager.image_resize' => ImageResizeManager::class,
+            'oro_attachment.provider.file_name'   => FileNameProviderInterface::class
         ]);
     }
 }
