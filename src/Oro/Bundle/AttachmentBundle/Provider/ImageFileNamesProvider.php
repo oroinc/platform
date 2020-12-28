89c3b83caeb14e0cbb69ac515480bb9aa9391b25
Vova Soroka <vsoroka@oroinc.com>
2020-12-28 18:50:00 +0200

    BAP-20035: File storage component enablement and documentation - reimplement product images removal functionality to make it work with new storage component and to make this functionality extendable and reusable

---
+++ src/Oro/Bundle/AttachmentBundle/Provider/ImageFileNamesProvider.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Oro\Bundle\AttachmentBundle\Provider;
+
+use Liip\ImagineBundle\Imagine\Filter\FilterConfiguration;
+use Oro\Bundle\AttachmentBundle\Entity\File;
+
+/**
+ * Provides filenames of all resized/filtered images for a specific File entity.
+ */
+class ImageFileNamesProvider implements FileNamesProviderInterface
+{
+    /** @var FilterConfiguration */
+    private $filterConfiguration;
+
+    /** @var ResizedImagePathProviderInterface */
+    private $imagePathProvider;
+
+    /**
+     * @param FilterConfiguration               $filterConfiguration
+     * @param ResizedImagePathProviderInterface $imagePathProvider
+     */
+    public function __construct(
+        FilterConfiguration $filterConfiguration,
+        ResizedImagePathProviderInterface $imagePathProvider
+    ) {
+        $this->filterConfiguration = $filterConfiguration;
+        $this->imagePathProvider = $imagePathProvider;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getFileNames(File $file): array
+    {
+        $fileNames = [];
+        $dimensions = $this->filterConfiguration->all();
+        foreach ($dimensions as $dimension => $dimensionConfig) {
+            $fileNames[] = $this->normalizeFileName(
+                $this->imagePathProvider->getPathForFilteredImage($file, $dimension)
+            );
+        }
+        $fileNames[] = $this->normalizeFileName(
+            $this->imagePathProvider->getPathForResizedImage($file, 1, 1)
+        );
+
+        return array_values(array_unique($fileNames));
+    }
+
+    /**
+     * @param string $fileName
+     *
+     * @return string
+     */
+    private function normalizeFileName(string $fileName): string
+    {
+        return ltrim($fileName, '/');
+    }
+}
