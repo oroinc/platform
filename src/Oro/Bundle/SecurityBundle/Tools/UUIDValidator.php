e081d2dee45d40044dde0dc2df5f06da6424b1c4
Yefim Yevtushenko <yyevtushenko@oroinc.com>
2021-01-28 15:33:16 +0000

    BB-20246: Save saved search state as a result set - added UUIDValidator for checking UUID v4 before using in dbal - added ChunkCallbackHelper for processing large collections in chunks

---
+++ src/Oro/Bundle/SecurityBundle/Tools/UUIDValidator.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Oro\Bundle\SecurityBundle\Tools;
+
+/**
+ * Checks if UUID is valid.
+ */
+class UUIDValidator
+{
+    /**
+     * @param string $uuid
+     * @return bool
+     */
+    public static function isValidV4(string $uuid): bool
+    {
+        return $uuid && preg_match('/^[0-9a-f]{8}-[0-9a-f]{4}-[4][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i', $uuid);
+    }
+}
