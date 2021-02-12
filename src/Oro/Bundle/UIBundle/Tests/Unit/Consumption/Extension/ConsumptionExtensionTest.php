102da3bad574215630f5ce29b21a14aa738cb60a
Mykhailo Sulyma <msulima@oroinc.com>
2021-02-12 17:08:19 +0200

    BB-20360: Images are not rendered at the storefront when the application is installed in subfolder (#29122)
    - set base url only for ImportExport MQ messages
    - added compiler pass to add list of supported MQ topics

--- src/Oro/Bundle/UIBundle/Tests/Unit/Consumption/Extension/ConsumptionExtensionTest.php
+++ src/Oro/Bundle/UIBundle/Tests/Unit/Consumption/Extension/ConsumptionExtensionTest.php
@@ -4,7 +4,9 @@ namespace Oro\Bundle\UIBundle\Tests\Unit\Consumption\Extension;
 
 use Oro\Bundle\ConfigBundle\Config\ConfigManager;
 use Oro\Bundle\UIBundle\Consumption\Extension\ConsumptionExtension;
+use Oro\Component\MessageQueue\Client\Config;
 use Oro\Component\MessageQueue\Consumption\Context;
+use Oro\Component\MessageQueue\Transport\Message;
 use Oro\Component\MessageQueue\Transport\SessionInterface;
 use Symfony\Component\Routing\RequestContext;
 
@@ -13,42 +15,83 @@ class ConsumptionExtensionTest extends \PHPUnit\Framework\TestCase
     /** @var string */
     private const URL = 'https://test.host:444/index.php/admin/path';
 
-    /** @var RequestContext */
-    private $context;
-
-    /** @var ConfigManager|\PHPUnit\Framework\MockObject\MockObject */
-    private $configManager;
-
-    /** @var ConsumptionExtension */
-    private $extension;
+    private RequestContext $requestContext;
+    private ConsumptionExtension $extension;
+    private Context $context;
 
     protected function setUp(): void
     {
-        $this->context = new RequestContext();
+        $this->requestContext = new RequestContext();
 
-        $this->configManager = $this->createMock(ConfigManager::class);
-        $this->configManager->expects($this->any())
+        $configManager = $this->createMock(ConfigManager::class);
+        $configManager
             ->method('get')
             ->with('oro_ui.application_url')
             ->willReturn(self::URL);
 
-        $this->extension = new ConsumptionExtension($this->context, $this->configManager);
+        $this->extension = new ConsumptionExtension($this->requestContext, $configManager);
+        $this->extension->addTopicName('test-topic');
+
+        $this->context = new Context($this->createMock(SessionInterface::class));
     }
 
-    public function testOnPreReceived(): void
+    /**
+     * @dataProvider onPreReceivedDataProvider
+     */
+    public function testOnPreReceived(?Message $message, bool $changed): void
     {
-        $this->assertEquals('http', $this->context->getScheme());
-        $this->assertEquals('localhost', $this->context->getHost());
-        $this->assertEquals('80', $this->context->getHttpPort());
-        $this->assertEquals('443', $this->context->getHttpsPort());
-        $this->assertEquals('', $this->context->getBaseUrl());
+        $this->assertEquals('http', $this->requestContext->getScheme());
+        $this->assertEquals('localhost', $this->requestContext->getHost());
+        $this->assertEquals('80', $this->requestContext->getHttpPort());
+        $this->assertEquals('443', $this->requestContext->getHttpsPort());
+        $this->assertEquals('', $this->requestContext->getBaseUrl());
 
-        $this->extension->onPreReceived(new Context($this->createMock(SessionInterface::class)));
+        if ($message) {
+            $this->context->setMessage($message);
+        }
 
-        $this->assertEquals('https', $this->context->getScheme());
-        $this->assertEquals('test.host', $this->context->getHost());
-        $this->assertEquals('80', $this->context->getHttpPort());
-        $this->assertEquals('444', $this->context->getHttpsPort());
-        $this->assertEquals('/index.php/admin/path', $this->context->getBaseUrl());
+        $this->extension->onPreReceived($this->context);
+
+        if ($changed) {
+            $this->assertEquals('https', $this->requestContext->getScheme());
+            $this->assertEquals('test.host', $this->requestContext->getHost());
+            $this->assertEquals('80', $this->requestContext->getHttpPort());
+            $this->assertEquals('444', $this->requestContext->getHttpsPort());
+            $this->assertEquals('/index.php/admin/path', $this->requestContext->getBaseUrl());
+        } else {
+            $this->assertEquals('http', $this->requestContext->getScheme());
+            $this->assertEquals('localhost', $this->requestContext->getHost());
+            $this->assertEquals('80', $this->requestContext->getHttpPort());
+            $this->assertEquals('443', $this->requestContext->getHttpsPort());
+            $this->assertEquals('', $this->requestContext->getBaseUrl());
+        }
+    }
+
+    public function onPreReceivedDataProvider(): array
+    {
+        $message1 = new Message();
+        $message1->setProperties([Config::PARAMETER_TOPIC_NAME => 'unknown-topic']);
+
+        $message2 = new Message();
+        $message2->setProperties([Config::PARAMETER_TOPIC_NAME => 'test-topic']);
+
+        return [
+            [
+                'message' => null,
+                'changed' => false,
+            ],
+            [
+                'message' => new Message(),
+                'changed' => false,
+            ],
+            [
+                'message' => $message1,
+                'changed' => false,
+            ],
+            [
+                'message' => $message2,
+                'changed' => true,
+            ],
+        ];
     }
 }
