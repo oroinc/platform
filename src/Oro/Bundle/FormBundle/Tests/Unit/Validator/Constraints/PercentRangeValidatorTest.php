90762d87687a9ece4e53c4355feeca64658dca0a
Vova Soroka <vsoroka@oroinc.com>
2021-02-19 23:30:51 +0200

    BAP-20437: Create a range validator for percentage values (#29214)
    - replace "fractional" option with "type" option for PercentRange validation constraint

--- src/Oro/Bundle/FormBundle/Tests/Unit/Validator/Constraints/PercentRangeValidatorTest.php
+++ src/Oro/Bundle/FormBundle/Tests/Unit/Validator/Constraints/PercentRangeValidatorTest.php
@@ -14,6 +14,7 @@ use Symfony\Component\Validator\Test\ConstraintValidatorTestCase;
 /**
  * @SuppressWarnings(PHPMD.TooManyMethods)
  * @SuppressWarnings(PHPMD.TooManyPublicMethods)
+ * @SuppressWarnings(PHPMD.ExcessivePublicCount)
  */
 class PercentRangeValidatorTest extends ConstraintValidatorTestCase
 {
@@ -50,7 +51,7 @@ class PercentRangeValidatorTest extends ConstraintValidatorTestCase
     {
         $this->expectException(MissingOptionsException::class);
         $this->expectExceptionMessage(sprintf(
-            'Either option "min" or "max" must be given for constraint "%s".',
+            'Either option "min" or "max" must be given for the constraint "%s".',
             PercentRange::class
         ));
 
@@ -197,21 +198,21 @@ class PercentRangeValidatorTest extends ConstraintValidatorTestCase
             ->assertRaised();
     }
 
-    public function testValueLessThanMaxLimitForNotFractionalConstraint(): void
+    public function testValueLessThanMaxLimitForFractional100Constraint(): void
     {
-        $this->validator->validate(110.999, new PercentRange(['fractional' => false, 'max' => 111]));
+        $this->validator->validate(110.999, new PercentRange(['type' => 'fractional_100', 'max' => 111]));
         $this->assertNoViolation();
     }
 
-    public function testValueEqualsToMaxLimitForNotFractionalConstraint(): void
+    public function testValueEqualsToMaxLimitForFractional100Constraint(): void
     {
-        $this->validator->validate(111, new PercentRange(['fractional' => false, 'max' => 111]));
+        $this->validator->validate(111, new PercentRange(['type' => 'fractional_100', 'max' => 111]));
         $this->assertNoViolation();
     }
 
-    public function testValueGreaterThanMaxLimitForNotFractionalConstraint(): void
+    public function testValueGreaterThanMaxLimitForFractional100Constraint(): void
     {
-        $constraint = new PercentRange(['fractional' => false, 'max' => 111]);
+        $constraint = new PercentRange(['type' => 'fractional_100', 'max' => 111]);
         $this->validator->validate(111.001, $constraint);
         $this->buildViolation($constraint->maxMessage)
             ->setParameter('{{ value }}', '111.001')
@@ -220,21 +221,21 @@ class PercentRangeValidatorTest extends ConstraintValidatorTestCase
             ->assertRaised();
     }
 
-    public function testValueGreaterThanMinLimitForNotFractionalConstraint(): void
+    public function testValueGreaterThanMinLimitForFractional100Constraint(): void
     {
-        $this->validator->validate(-110.999, new PercentRange(['fractional' => false, 'min' => -111]));
+        $this->validator->validate(-110.999, new PercentRange(['type' => 'fractional_100', 'min' => -111]));
         $this->assertNoViolation();
     }
 
-    public function testValueEqualsToMinLimitForNotFractionalConstraint(): void
+    public function testValueEqualsToMinLimitForFractional100Constraint(): void
     {
-        $this->validator->validate(-111, new PercentRange(['fractional' => false, 'min' => -111]));
+        $this->validator->validate(-111, new PercentRange(['type' => 'fractional_100', 'min' => -111]));
         $this->assertNoViolation();
     }
 
-    public function testValueLessThanMinLimitForNotFractionalConstraint(): void
+    public function testValueLessThanMinLimitForFractional100Constraint(): void
     {
-        $constraint = new PercentRange(['fractional' => false, 'min' => -111]);
+        $constraint = new PercentRange(['type' => 'fractional_100', 'min' => -111]);
         $this->validator->validate(-111.001, $constraint);
         $this->buildViolation($constraint->minMessage)
             ->setParameter('{{ value }}', '-111.001')
@@ -243,27 +244,27 @@ class PercentRangeValidatorTest extends ConstraintValidatorTestCase
             ->assertRaised();
     }
 
-    public function testValueInRangeLimitForNotFractionalConstraint(): void
+    public function testValueInRangeLimitForFractional100Constraint(): void
     {
-        $this->validator->validate(0, new PercentRange(['fractional' => false, 'min' => -111, 'max' => 111]));
+        $this->validator->validate(0, new PercentRange(['type' => 'fractional_100', 'min' => -111, 'max' => 111]));
         $this->assertNoViolation();
     }
 
-    public function testValueEqualsToMinRangeLimitForNotFractionalConstraint(): void
+    public function testValueEqualsToMinRangeLimitForFractional100Constraint(): void
     {
-        $this->validator->validate(-111, new PercentRange(['fractional' => false, 'min' => -111, 'max' => 111]));
+        $this->validator->validate(-111, new PercentRange(['type' => 'fractional_100', 'min' => -111, 'max' => 111]));
         $this->assertNoViolation();
     }
 
-    public function testValueEqualsToMaxRangeLimitForNotFractionalConstraint(): void
+    public function testValueEqualsToMaxRangeLimitForFractional100Constraint(): void
     {
-        $this->validator->validate(111, new PercentRange(['fractional' => false, 'min' => -111, 'max' => 111]));
+        $this->validator->validate(111, new PercentRange(['type' => 'fractional_100', 'min' => -111, 'max' => 111]));
         $this->assertNoViolation();
     }
 
-    public function testValueGreaterThanRangeLimitForNotFractionalConstraint(): void
+    public function testValueGreaterThanRangeLimitForFractional100Constraint(): void
     {
-        $constraint = new PercentRange(['fractional' => false, 'min' => -111, 'max' => 111]);
+        $constraint = new PercentRange(['type' => 'fractional_100', 'min' => -111, 'max' => 111]);
         $this->validator->validate(111.001, $constraint);
         $this->buildViolation($constraint->notInRangeMessage)
             ->setParameter('{{ value }}', '111.001')
@@ -273,15 +274,113 @@ class PercentRangeValidatorTest extends ConstraintValidatorTestCase
             ->assertRaised();
     }
 
-    public function testValueLessThanRangeLimitForNotFractionalConstraint(): void
+    public function testValueLessThanRangeLimitForFractional100Constraint(): void
     {
-        $constraint = new PercentRange(['fractional' => false, 'min' => -111, 'max' => 111]);
+        $constraint = new PercentRange(['type' => 'fractional_100', 'min' => -111, 'max' => 111]);
         $this->validator->validate(-111.001, $constraint);
         $this->buildViolation($constraint->notInRangeMessage)
             ->setParameter('{{ value }}', '-111.001')
             ->setParameter('{{ min }}', '-111%')
             ->setParameter('{{ max }}', '111%')
             ->setCode(Range::NOT_IN_RANGE_ERROR)
+            ->assertRaised();
+    }
+
+    public function testValueLessThanMaxLimitForIntegerConstraint(): void
+    {
+        $this->validator->validate(110, new PercentRange(['type' => 'integer', 'max' => 111]));
+        $this->assertNoViolation();
+    }
+
+    public function testValueEqualsToMaxLimitForIntegerConstraint(): void
+    {
+        $this->validator->validate(111, new PercentRange(['type' => 'integer', 'max' => 111]));
+        $this->assertNoViolation();
+    }
+
+    public function testValueGreaterThanMaxLimitForIntegerConstraint(): void
+    {
+        $constraint = new PercentRange(['type' => 'integer', 'max' => 111]);
+        $this->validator->validate(112, $constraint);
+        $this->buildViolation($constraint->maxMessage)
+            ->setParameter('{{ value }}', '112')
+            ->setParameter('{{ limit }}', '111%')
+            ->setCode(Range::TOO_HIGH_ERROR)
+            ->assertRaised();
+    }
+
+    public function testValueGreaterThanMinLimitForIntegerConstraint(): void
+    {
+        $this->validator->validate(-110, new PercentRange(['type' => 'integer', 'min' => -111]));
+        $this->assertNoViolation();
+    }
+
+    public function testValueEqualsToMinLimitForIntegerConstraint(): void
+    {
+        $this->validator->validate(-111, new PercentRange(['type' => 'integer', 'min' => -111]));
+        $this->assertNoViolation();
+    }
+
+    public function testValueLessThanMinLimitForIntegerConstraint(): void
+    {
+        $constraint = new PercentRange(['type' => 'integer', 'min' => -111]);
+        $this->validator->validate(-112, $constraint);
+        $this->buildViolation($constraint->minMessage)
+            ->setParameter('{{ value }}', '-112')
+            ->setParameter('{{ limit }}', '-111%')
+            ->setCode(Range::TOO_LOW_ERROR)
+            ->assertRaised();
+    }
+
+    public function testValueInRangeLimitForIntegerConstraint(): void
+    {
+        $this->validator->validate(0, new PercentRange(['type' => 'integer', 'min' => -111, 'max' => 111]));
+        $this->assertNoViolation();
+    }
+
+    public function testValueEqualsToMinRangeLimitForIntegerConstraint(): void
+    {
+        $this->validator->validate(-111, new PercentRange(['type' => 'integer', 'min' => -111, 'max' => 111]));
+        $this->assertNoViolation();
+    }
+
+    public function testValueEqualsToMaxRangeLimitForIntegerConstraint(): void
+    {
+        $this->validator->validate(111, new PercentRange(['type' => 'integer', 'min' => -111, 'max' => 111]));
+        $this->assertNoViolation();
+    }
+
+    public function testValueGreaterThanRangeLimitForIntegerConstraint(): void
+    {
+        $constraint = new PercentRange(['type' => 'integer', 'min' => -111, 'max' => 111]);
+        $this->validator->validate(112, $constraint);
+        $this->buildViolation($constraint->notInRangeMessage)
+            ->setParameter('{{ value }}', '112')
+            ->setParameter('{{ min }}', '-111%')
+            ->setParameter('{{ max }}', '111%')
+            ->setCode(Range::NOT_IN_RANGE_ERROR)
+            ->assertRaised();
+    }
+
+    public function testValueLessThanRangeLimitForIntegerConstraint(): void
+    {
+        $constraint = new PercentRange(['type' => 'integer', 'min' => -111, 'max' => 111]);
+        $this->validator->validate(-112, $constraint);
+        $this->buildViolation($constraint->notInRangeMessage)
+            ->setParameter('{{ value }}', '-112')
+            ->setParameter('{{ min }}', '-111%')
+            ->setParameter('{{ max }}', '111%')
+            ->setCode(Range::NOT_IN_RANGE_ERROR)
+            ->assertRaised();
+    }
+
+    public function testValueHasFractionalPartForIntegerConstraint(): void
+    {
+        $constraint = new PercentRange(['type' => 'integer', 'min' => -111, 'max' => 111]);
+        $this->validator->validate(11.001, $constraint);
+        $this->buildViolation($constraint->notIntegerMessage)
+            ->setParameter('{{ value }}', '11.001')
+            ->setCode(Range::INVALID_CHARACTERS_ERROR)
             ->assertRaised();
     }
 }
