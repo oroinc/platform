006cabf0fa68f7f76c5693e90a7d5a3b734a4fba
Vitalii Yeromenko <vyeromenko@oroinc.com>
2021-02-22 13:50:09 +0200

    BB-20362 Incorrect HTMLPurifier error collector during products import (#29201)

--- src/Oro/Bundle/UIBundle/Tests/Unit/Tools/HTMLPurifier/HTMLPurifierTest.php
+++ src/Oro/Bundle/UIBundle/Tests/Unit/Tools/HTMLPurifier/HTMLPurifierTest.php
@@ -40,16 +40,17 @@ class HTMLPurifierTest extends \PHPUnit\Framework\TestCase
 
     public function testPurify(): void
     {
-        $htmlValue = '<div><h1>Hello World!</h1></div>';
+        $htmlValue = '<div><div><h1>Hello World!</h1></div>';
 
         $resultHtml = $this->purifier->purify($htmlValue);
 
-        $this->assertEquals('<div>Hello World!</div>', $resultHtml);
+        $this->assertEquals('<div><div>Hello World!</div></div>', $resultHtml);
 
         /** @var ErrorCollector $errorCollector */
         $errorCollector = $this->purifier->context->get('ErrorCollector');
         $this->assertNotNull($errorCollector);
         $expectedErrors = [
+            new Error('<div> tag started on line 1 closed by end of document', '<div><div><h1>Hello World'),
             new Error('Unrecognized <h1> tag removed', '<h1>Hello World!</h1></di'),
             new Error('Unrecognized </h1> tag removed', '</h1></div>'),
         ];
