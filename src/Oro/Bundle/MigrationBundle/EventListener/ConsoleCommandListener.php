8a1aa1908dbbd7a3ec3c0dd663f48d586831a917
Vova Soroka <vsoroka@oroinc.com>
2021-02-26 15:00:49 +0200

    BAP-19938: doctrine:schema:validate returns error (#29254)

--- src/Oro/Bundle/MigrationBundle/EventListener/ConsoleCommandListener.php
+++ src/Oro/Bundle/MigrationBundle/EventListener/ConsoleCommandListener.php
@@ -5,6 +5,9 @@ namespace Oro\Bundle\MigrationBundle\EventListener;
 use Oro\Bundle\EntityExtendBundle\Tools\SchemaTrait;
 use Symfony\Component\Console\Event\ConsoleCommandEvent;
 
+/**
+ * Substitutes SchemaDiff service for "doctrine:schema:update" and "doctrine:schema:validate" CLI commands.
+ */
 class ConsoleCommandListener
 {
     use SchemaTrait;
@@ -14,7 +17,8 @@ class ConsoleCommandListener
      */
     public function onConsoleCommand(ConsoleCommandEvent $event)
     {
-        if ('doctrine:schema:update' === $event->getCommand()->getName()) {
+        $commandName = $event->getCommand()->getName();
+        if ('doctrine:schema:update' === $commandName || 'doctrine:schema:validate' === $commandName) {
             $this->overrideSchemaDiff();
         }
     }
