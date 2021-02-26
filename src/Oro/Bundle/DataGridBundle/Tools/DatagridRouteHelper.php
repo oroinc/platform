a9df2d29ae60862a285d6bf30e88a3b9771dc92b
Yefim Yevtushenko <yyevtushenko@oroinc.com>
2021-02-26 21:21:27 +0200

    BB-20364: View link is missing on Saved Searches datagrid (#29252)
    - added contentVariantId to route data of CategoryPageContentVariantType to distiguish category page from content variant category page
    - added data migration for updating web catalog slugs
    - added content variant type checks to SearchCategoryFilteringEventListener and ProductCollectionContentVariantFilteringEventListener to prevent false positive adding of parameters
    - added saved search url generators
    - added DatagridRouteHelper::appendGridParams() for adding minified grid parameters to a url
    - added SavedSearchResultsUrlProvider for using in viewLink property of datagrid
    - removed categoryId url param from view in ContentVariantUrlParamViewTransformer
    - added view_url to email notification variables in AlertToNotificationProcessor, updated email template migration
    
    Co-authored-by: Mykhailo Sulyma <msulima@oroinc.com>

--- src/Oro/Bundle/DataGridBundle/Tools/DatagridRouteHelper.php
+++ src/Oro/Bundle/DataGridBundle/Tools/DatagridRouteHelper.php
@@ -2,11 +2,16 @@
 
 namespace Oro\Bundle\DataGridBundle\Tools;
 
+use GuzzleHttp\Psr7\Uri as GuzzleUri;
+use Oro\Bundle\DataGridBundle\Datagrid\RequestParameterBagFactory;
 use Symfony\Component\Routing\Exception\InvalidParameterException;
 use Symfony\Component\Routing\Exception\MissingMandatoryParametersException;
 use Symfony\Component\Routing\Exception\RouteNotFoundException;
 use Symfony\Component\Routing\RouterInterface;
 
+/**
+ * Contains handy methods for working with datagrid URL.
+ */
 class DatagridRouteHelper
 {
     /** @var RouterInterface */
@@ -42,6 +47,29 @@ class DatagridRouteHelper
             $routeName,
             ['grid' => [$gridName => http_build_query($params)]],
             $referenceType
+        );
+    }
+
+    /**
+     * Adds minified grid parameters to the specified URL.
+     *
+     * @param string $originalUrl
+     * @param string $gridName
+     * @param array $params
+     * @return string
+     */
+    public function appendGridParams(string $originalUrl, string $gridName, array $params): string
+    {
+        if (!$gridName || !$params) {
+            return $originalUrl;
+        }
+
+        $uri = new GuzzleUri($originalUrl);
+
+        return GuzzleUri::withQueryValue(
+            $uri,
+            sprintf('%s[%s]', RequestParameterBagFactory::DEFAULT_ROOT_PARAM, $gridName),
+            http_build_query($params)
         );
     }
 }
