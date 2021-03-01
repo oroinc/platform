98ee1bd5407ca74df367c867fe3e8fb70a029076
Vova Soroka <vsoroka@oroinc.com>
2021-03-01 15:56:24 +0200

    BAP-9473: Error handling in ApiBundle (#29274)
    - fix handling of exceptions that happen before any public API action is started

---
+++ src/Oro/Bundle/ApiBundle/Processor/UnhandledError/BuildResultDocument.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Oro\Bundle\ApiBundle\Processor\UnhandledError;
+
+use Oro\Bundle\ApiBundle\Exception\RuntimeException;
+use Oro\Bundle\ApiBundle\Processor\Context;
+use Oro\Bundle\ApiBundle\Processor\Shared\BuildResultDocument as BaseBuildResultDocument;
+use Oro\Bundle\ApiBundle\Request\DocumentBuilderInterface;
+
+/**
+ * Builds the response for "unhandled_error" action.
+ */
+class BuildResultDocument extends BaseBuildResultDocument
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function processResult(DocumentBuilderInterface $documentBuilder, Context $context)
+    {
+        throw new RuntimeException('Invalid error handling: the context must contain an error object.');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getExceptionLoggingContext(Context $context): array
+    {
+        return ['action' => $context->getAction()];
+    }
+}
