5df319b60134ddd866a89650aef4c5c1ea4af127
Vova Soroka <vsoroka@oroinc.com>
2021-05-01 21:10:56 +0300

    BAP-20474: Remove at() usage in components (#29779)

--- src/Oro/Component/Layout/Tests/Unit/Extension/Theme/Visitor/ImportVisitorTest.php
+++ src/Oro/Component/Layout/Tests/Unit/Extension/Theme/Visitor/ImportVisitorTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Oro\Component\Layout\Tests\Unit\Extension\Visitor;
+namespace Oro\Component\Layout\Tests\Unit\Extension\Theme\Visitor;
 
 use Oro\Component\Layout\ContextInterface;
 use Oro\Component\Layout\Extension\Theme\Model\DependencyInitializer;
@@ -20,23 +20,20 @@ use Oro\Component\Layout\Tests\Unit\Extension\Theme\Stubs\NotApplicableImportAwa
 class ImportVisitorTest extends \PHPUnit\Framework\TestCase
 {
     /** @var ImportVisitor */
-    protected $visitor;
+    private $visitor;
 
     /** @var LayoutUpdateLoaderInterface|\PHPUnit\Framework\MockObject\MockObject */
-    protected $loader;
+    private $loader;
 
     /** @var DependencyInitializer|\PHPUnit\Framework\MockObject\MockObject */
-    protected $dependencyInitializer;
+    private $dependencyInitializer;
 
     /** @var ResourceProviderInterface|\PHPUnit\Framework\MockObject\MockObject */
-    protected $resourceProvider;
+    private $resourceProvider;
 
     /** @var ThemeManager|\PHPUnit\Framework\MockObject\MockObject */
-    protected $themeManager;
+    private $themeManager;
 
-    /**
-     * {@inheritdoc}
-     */
     protected function setUp(): void
     {
         $this->loader = $this->createMock(LayoutUpdateLoaderInterface::class);
@@ -72,55 +69,49 @@ class ImportVisitorTest extends \PHPUnit\Framework\TestCase
         $context->expects($this->once())
             ->method('get')
             ->with(ThemeExtension::THEME_KEY)
-            ->will($this->returnValue($themeName));
+            ->willReturn($themeName);
 
         $update = $this->createMock(LayoutUpdateWithImports::class);
         $update->expects($this->once())
             ->method('getImports')
-            ->will($this->returnValue(
+            ->willReturn([
                 [
-                    [
-                        ImportsAwareLayoutUpdateInterface::ID_KEY => 'import_id',
-                        ImportsAwareLayoutUpdateInterface::ROOT_KEY => 'root_block_id',
-                        ImportsAwareLayoutUpdateInterface::NAMESPACE_KEY => 'import_namespace'
-                    ]
+                    ImportsAwareLayoutUpdateInterface::ID_KEY => 'import_id',
+                    ImportsAwareLayoutUpdateInterface::ROOT_KEY => 'root_block_id',
+                    ImportsAwareLayoutUpdateInterface::NAMESPACE_KEY => 'import_namespace'
                 ]
-            ));
+            ]);
 
         /** @var Theme|\PHPUnit\Framework\MockObject\MockObject $theme */
         $theme = $this->getMockBuilder(Theme::class)->setConstructorArgs([$themeName])->getMock();
         $theme->expects($this->any())
             ->method('getName')
-            ->will($this->returnValue($themeName));
+            ->willReturn($themeName);
 
-        $this->themeManager
-            ->expects($this->any())
+        $this->themeManager->expects($this->any())
             ->method('getTheme')
             ->with($themeName)
-            ->will($this->returnValue($theme));
+            ->willReturn($theme);
 
         $path = implode(
             DIRECTORY_SEPARATOR,
             [$theme->getName(), ImportVisitor::IMPORT_FOLDER, 'import_id']
         );
 
-        $this->resourceProvider
-            ->expects($this->once())
+        $this->resourceProvider->expects($this->once())
             ->method('findApplicableResources')
             ->with([$path])
-            ->will($this->returnValue(['import/file']));
+            ->willReturn(['import/file']);
 
         /** @var ImportedLayoutUpdate|\PHPUnit\Framework\MockObject\MockObject $importUpdate */
         $importUpdate = $this->createMock(ImportedLayoutUpdate::class);
 
-        $this->loader
-            ->expects($this->once())
+        $this->loader->expects($this->once())
             ->method('load')
             ->with('import/file')
-            ->will($this->returnValue($importUpdate));
+            ->willReturn($importUpdate);
 
-        $this->dependencyInitializer
-            ->expects($this->once())
+        $this->dependencyInitializer->expects($this->once())
             ->method('initialize')
             ->with($importUpdate);
 
@@ -145,92 +136,62 @@ class ImportVisitorTest extends \PHPUnit\Framework\TestCase
         $context->expects($this->exactly(2))
             ->method('get')
             ->with(ThemeExtension::THEME_KEY)
-            ->will($this->returnValue($themeName));
+            ->willReturn($themeName);
 
         $updateWithImports = $this->createMock(LayoutUpdateWithImports::class);
         $updateWithImports->expects($this->once())
             ->method('getImports')
-            ->will($this->returnValue(
+            ->willReturn([
                 [
-                    [
-                        ImportsAwareLayoutUpdateInterface::ID_KEY => 'first_import',
-                        ImportsAwareLayoutUpdateInterface::ROOT_KEY => 'root_block_id',
-                        ImportsAwareLayoutUpdateInterface::NAMESPACE_KEY => 'import_namespace'
-                    ],
-                    [
-                        ImportsAwareLayoutUpdateInterface::ID_KEY => 'second_import',
-                        ImportsAwareLayoutUpdateInterface::ROOT_KEY => 'root_block_id',
-                        ImportsAwareLayoutUpdateInterface::NAMESPACE_KEY => 'import_namespace'
-                    ]
+                    ImportsAwareLayoutUpdateInterface::ID_KEY => 'first_import',
+                    ImportsAwareLayoutUpdateInterface::ROOT_KEY => 'root_block_id',
+                    ImportsAwareLayoutUpdateInterface::NAMESPACE_KEY => 'import_namespace'
+                ],
+                [
+                    ImportsAwareLayoutUpdateInterface::ID_KEY => 'second_import',
+                    ImportsAwareLayoutUpdateInterface::ROOT_KEY => 'root_block_id',
+                    ImportsAwareLayoutUpdateInterface::NAMESPACE_KEY => 'import_namespace'
                 ]
-            ));
+            ]);
 
         $updateWithoutImports = $this->createMock(LayoutUpdateWithImports::class);
         $updateWithoutImports->expects($this->once())
             ->method('getImports')
-            ->will($this->returnValue([]));
+            ->willReturn([]);
 
         /** @var Theme|\PHPUnit\Framework\MockObject\MockObject $theme */
         $theme = $this->getMockBuilder(Theme::class)->setConstructorArgs([$themeName])->getMock();
         $theme->expects($this->any())
             ->method('getName')
-            ->will($this->returnValue($themeName));
+            ->willReturn($themeName);
 
-        $this->themeManager
-            ->expects($this->any())
+        $this->themeManager->expects($this->any())
             ->method('getTheme')
             ->with($themeName)
-            ->will($this->returnValue($theme));
+            ->willReturn($theme);
 
-        $path = implode(
-            DIRECTORY_SEPARATOR,
-            [$theme->getName(), ImportVisitor::IMPORT_FOLDER, 'second_import']
-        );
-
-        $this->resourceProvider
-            ->expects($this->at(0))
-            ->method('findApplicableResources')
-            ->with([$path])
-            ->will($this->returnValue(['import/second_file']));
-
-        $path = implode(
+        $firstImportPath = implode(
             DIRECTORY_SEPARATOR,
             [$theme->getName(), ImportVisitor::IMPORT_FOLDER, 'first_import']
         );
-
-        $this->resourceProvider
-            ->expects($this->at(1))
+        $secondImportPath = implode(
+            DIRECTORY_SEPARATOR,
+            [$theme->getName(), ImportVisitor::IMPORT_FOLDER, 'second_import']
+        );
+        $this->resourceProvider->expects($this->exactly(2))
             ->method('findApplicableResources')
-            ->with([$path])
-            ->will($this->returnValue(['import/first_file']));
+            ->withConsecutive([[$secondImportPath]], [[$firstImportPath]])
+            ->willReturnOnConsecutiveCalls(['import/second_file'], ['import/first_file']);
 
-        /** @var ImportedLayoutUpdate|\PHPUnit\Framework\MockObject\MockObject $firstImportUpdate */
         $firstImportUpdate = $this->createMock(ImportedLayoutUpdate::class);
-
-        /** @var ImportedLayoutUpdate|\PHPUnit\Framework\MockObject\MockObject $secondImportUpdate */
         $secondImportUpdate = $this->createMock(ImportedLayoutUpdate::class);
-
-        $this->loader
-            ->expects($this->at(0))
+        $this->loader->expects($this->exactly(2))
             ->method('load')
-            ->with('import/second_file')
-            ->will($this->returnValue($secondImportUpdate));
-
-        $this->loader
-            ->expects($this->at(1))
-            ->method('load')
-            ->with('import/first_file')
-            ->will($this->returnValue($firstImportUpdate));
-
-        $this->dependencyInitializer
-            ->expects($this->at(0))
+            ->withConsecutive(['import/second_file'], ['import/first_file'])
+            ->willReturnOnConsecutiveCalls($secondImportUpdate, $firstImportUpdate);
+        $this->dependencyInitializer->expects($this->exactly(2))
             ->method('initialize')
-            ->with($secondImportUpdate);
-
-        $this->dependencyInitializer
-            ->expects($this->at(1))
-            ->method('initialize')
-            ->with($firstImportUpdate);
+            ->withConsecutive([$secondImportUpdate], [$firstImportUpdate]);
 
         $updates = ['root' => [
             $updateWithImports,
@@ -257,52 +218,46 @@ class ImportVisitorTest extends \PHPUnit\Framework\TestCase
         $context->expects($this->once())
             ->method('get')
             ->with(ThemeExtension::THEME_KEY)
-            ->will($this->returnValue($themeName));
+            ->willReturn($themeName);
 
         $update = $this->createMock(LayoutUpdateWithImports::class);
         $update->expects($this->once())
             ->method('getImports')
-            ->will($this->returnValue(
-                ['import_id']
-            ));
+            ->willReturn(['import_id']);
 
         /** @var Theme|\PHPUnit\Framework\MockObject\MockObject $theme */
         $theme = $this->getMockBuilder(Theme::class)->setConstructorArgs([$themeName])->getMock();
         $theme->expects($this->any())
             ->method('getName')
-            ->will($this->returnValue($themeName));
+            ->willReturn($themeName);
 
-        $this->themeManager
-            ->expects($this->any())
+        $this->themeManager->expects($this->any())
             ->method('getTheme')
             ->with($themeName)
-            ->will($this->returnValue($theme));
+            ->willReturn($theme);
 
         $path = implode(
             DIRECTORY_SEPARATOR,
             [$theme->getName(), ImportVisitor::IMPORT_FOLDER, 'import_id']
         );
 
-        $this->resourceProvider
-            ->expects($this->once())
+        $this->resourceProvider->expects($this->once())
             ->method('findApplicableResources')
             ->with([$path])
-            ->will($this->returnValue(['import/file']));
+            ->willReturn(['import/file']);
 
         /** @var ImportedLayoutUpdateWithImports|\PHPUnit\Framework\MockObject\MockObject $importUpdate */
         $importUpdate = $this->createMock(ImportedLayoutUpdateWithImports::class);
         $importUpdate->expects($this->once())
             ->method('getImports')
-            ->will($this->returnValue([]));
+            ->willReturn([]);
 
-        $this->loader
-            ->expects($this->once())
+        $this->loader->expects($this->once())
             ->method('load')
             ->with('import/file')
-            ->will($this->returnValue($importUpdate));
+            ->willReturn($importUpdate);
 
-        $this->dependencyInitializer
-            ->expects($this->once())
+        $this->dependencyInitializer->expects($this->once())
             ->method('initialize')
             ->with($importUpdate);
 
@@ -326,7 +281,7 @@ class ImportVisitorTest extends \PHPUnit\Framework\TestCase
         $update = $this->createMock(LayoutUpdateWithImports::class);
         $update->expects($this->once())
             ->method('getImports')
-            ->will($this->returnValue('string'));
+            ->willReturn('string');
 
         $updates = ['root' => [$update]];
 
@@ -341,7 +296,8 @@ class ImportVisitorTest extends \PHPUnit\Framework\TestCase
         /** @var ContextInterface|\PHPUnit\Framework\MockObject\MockObject $context */
         $context = $this->createMock(ContextInterface::class);
 
-        $this->themeManager->expects($this->never())->method('getTheme');
+        $this->themeManager->expects($this->never())
+            ->method('getTheme');
 
         $this->visitor->walkUpdates($updates, $context);
     }
