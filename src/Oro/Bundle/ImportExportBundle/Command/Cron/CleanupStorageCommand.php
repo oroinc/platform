ffa4f04a77fee4bfd5c27655034fa5d4898c0003
Igor Krynychanskyi <ikrynychanskyi@oroinc.com>
2021-05-19 12:52:08 +0300

    BB-20592: BC break in CleanupStorageCommand (#29878)
    - refactored according to avoid BC breaks

--- src/Oro/Bundle/ImportExportBundle/Command/Cron/CleanupStorageCommand.php
+++ src/Oro/Bundle/ImportExportBundle/Command/Cron/CleanupStorageCommand.php
@@ -4,14 +4,21 @@ declare(strict_types=1);
 namespace Oro\Bundle\ImportExportBundle\Command\Cron;
 
 use Gaufrette\File;
+use Oro\Bundle\CronBundle\Command\CronCommandInterface;
 use Oro\Bundle\ImportExportBundle\File\FileManager;
 use Oro\Bundle\ImportExportBundle\Manager\ImportExportResultManager;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
 
 /**
  * Deletes old temporary import/export files.
  */
-class CleanupStorageCommand extends CleanupStorageCommandAbstract
+class CleanupStorageCommand extends Command implements CronCommandInterface
 {
+    private const DEFAULT_PERIOD = 14; // days
+
     /** @var string */
     protected static $defaultName = 'oro:cron:import-clean-up-storage';
 
@@ -31,20 +38,61 @@ class CleanupStorageCommand extends CleanupStorageCommandAbstract
         return '0 0 */1 * *';
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    protected function getFilesForDeletion($from, $to): array
+    public function isActive()
     {
-        $this->importExportResultManager->markResultsAsExpired($from, $to);
-        return $this->fileManager->getFilesByPeriod($from, $to);
+        return true;
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    protected function deleteFile(File $file): void
+    /** @noinspection PhpMissingParentCallCommonInspection */
+    protected function configure()
     {
-        $this->fileManager->deleteFile($file);
+        $this
+            ->addOption(
+                'interval',
+                'i',
+                InputOption::VALUE_OPTIONAL,
+                'Time interval (days) to keep the import and export files.'.
+                ' Will be removed files older than today-interval.',
+                self::DEFAULT_PERIOD
+            )
+            ->setDescription('Deletes old temporary import/export files.')
+            ->setHelp(
+                <<<'HELP'
+The <info>%command.name%</info> command deletes old temporary import/export files.
+
+  <info>php %command.full_name%</info>
+
+The <info>--interval</info> option can be used to override the default time period (14 days)
+past which the temporary import files are considered old:
+
+  <info>php %command.full_name% --interval=<days></info>
+
+HELP
+            )
+            ->addUsage('--interval=<days>')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $period = (int)$input->getOption('interval');
+
+        $from = new \DateTime('@0');
+        $to = new \DateTime();
+        $to->modify(sprintf('-%d days', $period));
+
+        $this->importExportResultManager->markResultsAsExpired($from, $to);
+
+        $files = $this->fileManager->getFilesByPeriod($from, $to);
+        /** @var File $file*/
+        foreach ($files as $fileName => $file) {
+            $this->fileManager->deleteFile($file);
+            $output->writeln(
+                sprintf('<info> File "%s" was removed.</info>', $fileName),
+                OutputInterface::VERBOSITY_DEBUG
+            );
+        }
+
+        $output->writeln(sprintf('<info>Were removed "%s" files.</info>', count($files)));
     }
 }
