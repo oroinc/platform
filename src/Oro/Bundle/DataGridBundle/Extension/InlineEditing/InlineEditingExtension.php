1d38ae949579bb342a28f0278d074e174058cd38
Dmitry Khrysev <x86demon@users.noreply.github.com>
2021-08-20 18:39:11 +0300

    BAP-20724: Tag are not displayed and cannot be edited in My Emails grid (#30420)

--- src/Oro/Bundle/DataGridBundle/Extension/InlineEditing/InlineEditingExtension.php
+++ src/Oro/Bundle/DataGridBundle/Extension/InlineEditing/InlineEditingExtension.php
@@ -13,6 +13,9 @@ use Symfony\Component\Security\Acl\Domain\ObjectIdentity;
 use Symfony\Component\Security\Acl\Voter\FieldVote;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
+/**
+ * Enables inline editing on supported columns for grids which has inline editing enabled.
+ */
 class InlineEditingExtension extends AbstractExtension
 {
     /** @var InlineEditColumnOptionsGuesser */
@@ -40,25 +43,17 @@ class InlineEditingExtension extends AbstractExtension
     }
 
     /**
-     * {@inheritdoc}
-     */
-    public function isApplicable(DatagridConfiguration $config)
-    {
-        return
-            parent::isApplicable($config)
-            && $config->offsetGetByPath(Configuration::ENABLED_CONFIG_PATH);
-    }
-
-    /**
      * Validate configs nad fill default values
      */
     public function processConfigs(DatagridConfiguration $config)
     {
-        $configItems = $config->offsetGetOr(Configuration::BASE_CONFIG_KEY, []);
-
-        if (empty($configItems[Configuration::CONFIG_ENTITY_KEY])) {
-            $configItems[Configuration::CONFIG_ENTITY_KEY] = $config->getExtendedEntityClassName();
+        if (!$config->offsetGetByPath(Configuration::ENABLED_CONFIG_PATH)) {
+            return;
         }
+
+        $entityName = $this->getEntityName($config);
+        $configItems = $config->offsetGetOr(Configuration::BASE_CONFIG_KEY, []);
+        $configItems[Configuration::CONFIG_ENTITY_KEY] = $entityName;
 
         $configuration = new Configuration(Configuration::BASE_CONFIG_KEY);
         $normalizedConfigItems = $this->validateConfiguration(
@@ -75,49 +70,11 @@ class InlineEditingExtension extends AbstractExtension
         $resultConfigItems = array_replace_recursive($configItems, $normalizedConfigItems);
         if (is_null($resultConfigItems['save_api_accessor']['default_route_parameters']['className'])) {
             $resultConfigItems['save_api_accessor']['default_route_parameters']['className'] =
-                $this->entityClassNameHelper->getUrlSafeClassName($configItems['entity_name']);
+                $this->entityClassNameHelper->getUrlSafeClassName($entityName);
         }
         $config->offsetSet(Configuration::BASE_CONFIG_KEY, $resultConfigItems);
 
-        // add inline editing where it is possible, do not use ACL, because additional parameters for columns needed
-        $columns = $config->offsetGetOr(FormatterConfiguration::COLUMNS_KEY, []);
-        $blackList = $configuration->getBlackList();
-        $behaviour = $config->offsetGetByPath(Configuration::BEHAVIOUR_CONFIG_PATH);
-        $objectIdentity = new ObjectIdentity('entity', $configItems['entity_name']);
-
-        foreach ($columns as $columnName => &$column) {
-            if (!in_array($columnName, $blackList, true)) {
-                $newColumn = $this->guesser->getColumnOptions(
-                    $columnName,
-                    $configItems['entity_name'],
-                    $column,
-                    $behaviour
-                );
-
-                // Check access to edit field in Class level.
-                // If access not granted - skip inline editing for such field.
-                if (!$this->isFieldEditable($newColumn, $objectIdentity, $columnName, $column)) {
-                    $column = $this->disableColumnEdit($column);
-                    continue;
-                }
-
-                // frontend type, type, data_name keys must not be replaced with default value
-                $keys = [
-                    PropertyInterface::FRONTEND_TYPE_KEY,
-                    PropertyInterface::TYPE_KEY,
-                    PropertyInterface::DATA_NAME_KEY,
-                ];
-                foreach ($keys as $key) {
-                    if (!empty($newColumn[$key])) {
-                        $column[$key] = $newColumn[$key];
-                    }
-                }
-
-                $column = array_replace_recursive($newColumn, $column);
-            }
-        }
-
-        $config->offsetSet(FormatterConfiguration::COLUMNS_KEY, $columns);
+        $this->applyInlineEditingForColumns($config, $configuration, $entityName);
     }
 
     /**
@@ -138,6 +95,10 @@ class InlineEditingExtension extends AbstractExtension
      */
     public function visitMetadata(DatagridConfiguration $config, MetadataObject $data)
     {
+        if (!$config->offsetGetByPath(Configuration::ENABLED_CONFIG_PATH)) {
+            return;
+        }
+
         $data->offsetSet(
             Configuration::BASE_CONFIG_KEY,
             $config->offsetGetOr(Configuration::BASE_CONFIG_KEY, [])
@@ -222,5 +183,65 @@ class InlineEditingExtension extends AbstractExtension
                 'EDIT',
                 new FieldVote($objectIdentity, $this->getColumnFieldName($columnName, $column))
             );
+    }
+
+    private function getEntityName(DatagridConfiguration $config): ?string
+    {
+        $configItems = $config->offsetGetOr(Configuration::BASE_CONFIG_KEY, []);
+
+        if (!empty($configItems[Configuration::CONFIG_ENTITY_KEY])) {
+            return $configItems[Configuration::CONFIG_ENTITY_KEY];
+        }
+
+        return  $config->getExtendedEntityClassName();
+    }
+
+    /**
+     * Add inline editing where it is possible, do not use ACL, because additional parameters for columns needed
+     */
+    private function applyInlineEditingForColumns(
+        DatagridConfiguration $config,
+        Configuration $configuration,
+        string $entityName
+    ): void {
+        $columns = $config->offsetGetOr(FormatterConfiguration::COLUMNS_KEY, []);
+        $blackList = $configuration->getBlackList();
+        $behaviour = $config->offsetGetByPath(Configuration::BEHAVIOUR_CONFIG_PATH);
+        $objectIdentity = new ObjectIdentity('entity', $entityName);
+
+        foreach ($columns as $columnName => &$column) {
+            if (!in_array($columnName, $blackList, true)) {
+                $newColumn = $this->guesser->getColumnOptions(
+                    $columnName,
+                    $entityName,
+                    $column,
+                    $behaviour
+                );
+
+                // Check access to edit field in Class level.
+                // If access not granted - skip inline editing for such field.
+                if (!$this->isFieldEditable($newColumn, $objectIdentity, $columnName, $column)) {
+                    $column = $this->disableColumnEdit($column);
+                    continue;
+                }
+
+                // frontend type, type, data_name keys must not be replaced with default value
+                $keys = [
+                    PropertyInterface::FRONTEND_TYPE_KEY,
+                    PropertyInterface::TYPE_KEY,
+                    PropertyInterface::DATA_NAME_KEY,
+                ];
+                foreach ($keys as $key) {
+                    if (!empty($newColumn[$key])) {
+                        $column[$key] = $newColumn[$key];
+                    }
+                }
+
+                $column = array_replace_recursive($newColumn, $column);
+            }
+        }
+        unset($column);
+
+        $config->offsetSet(FormatterConfiguration::COLUMNS_KEY, $columns);
     }
 }
