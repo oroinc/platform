847b70e06dc0a0534e0a21ed04923371851e9c6f
smiasnoi <smiasnoi@oroinc.com>
2021-09-19 14:54:39 +0300

    BAP-20830: Simplify installation and upgrade in the TEST environment (maintenance 4.2) (#30743)
     - simplified install and update commands options preset on a test environment to run these commands with no interaction
     - updated functional tests and platform update documentation

--- src/Oro/Bundle/InstallerBundle/Command/InstallCommand.php
+++ src/Oro/Bundle/InstallerBundle/Command/InstallCommand.php
@@ -187,6 +187,17 @@ HELP
         parent::configure();
     }
 
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
+    protected function initialize(InputInterface $input, OutputInterface $output)
+    {
+        if ($this->getContainer()->getParameter('kernel.environment') === 'test') {
+            $this->presetTestEnvironmentOptions($input, $output);
+        }
+    }
+
     /** @noinspection PhpMissingParentCallCommonInspection */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
@@ -835,5 +846,38 @@ HELP
         }
 
         return $this->assetsCommandProcess->getExitCode();
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
+    private function presetTestEnvironmentOptions(InputInterface $input, OutputInterface $output): void
+    {
+        $testEnvDefaultOptionValuesMap = [
+            'user-name'         => 'admin',
+            'user-email'        => 'admin@example.com',
+            'user-firstname'    => 'John',
+            'user-lastname'     => 'Doe',
+            'user-password'     => 'admin',
+            'sample-data'       => 'n',
+            'organization-name' => 'OroInc',
+            'application-url'   => 'http://localhost/',
+            'skip-assets'       => true,
+            'skip-translations' => true,
+            'timeout'           => '600',
+            'language'          => 'en',
+            'formatting-code'   => 'en_US'
+        ];
+
+        foreach ($testEnvDefaultOptionValuesMap as $optionName => $optionValue) {
+            if ($input->hasParameterOption('--' . $optionName)) {
+                continue;
+            }
+
+            $input->setOption($optionName, $optionValue);
+        }
+
+        $input->setInteractive(false);
     }
 }
