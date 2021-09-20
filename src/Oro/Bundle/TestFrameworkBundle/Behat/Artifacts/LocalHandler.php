d515cc85a623e50a32a9753f6b34908ade820db4
Andrey Yatsenco <ayatsenko@oroinc.com>
2021-09-20 14:06:34 +0300

    BAP-19975: Behat tests should not depend on bash scripts (#30788)
    - fixed LocalHandler.php

--- src/Oro/Bundle/TestFrameworkBundle/Behat/Artifacts/LocalHandler.php
+++ src/Oro/Bundle/TestFrameworkBundle/Behat/Artifacts/LocalHandler.php
@@ -4,35 +4,20 @@ namespace Oro\Bundle\TestFrameworkBundle\Behat\Artifacts;
 
 use Oro\Bundle\TestFrameworkBundle\Behat\Isolation\TokenGenerator;
 use Symfony\Component\Filesystem\Filesystem;
-use Symfony\Component\HttpKernel\KernelInterface;
 
 /**
  * Saves test artifacts to the local filesystem
  */
 class LocalHandler implements ArtifactsHandlerInterface
 {
-    /**
-     * @var string
-     */
-    protected $directory;
+    private string $directory;
+    private ?string $baseUrl;
 
-    /**
-     * @var string
-     */
-    protected $baseUrl;
-
-    /**
-     * @var bool
-     */
-    protected $autoClear;
-
-    private KernelInterface $kernel;
-
-    public function __construct(array $config, KernelInterface $kernel)
+    public function __construct(array $config)
     {
         $this->directory = rtrim($config['directory'], DIRECTORY_SEPARATOR);
-        $this->baseUrl = trim($config['base_url'], " \t\n\r\0\x0B\\");
-
+        $this->baseUrl = $config['base_url'] ? trim($config['base_url'], " \t\n\r\0\x0B\\") : null;
+        $this->baseUrl = rtrim($this->baseUrl, '/').'/';
         $filesystem = new Filesystem();
         if ($config['auto_clear']) {
             $filesystem->remove($this->directory);
@@ -41,7 +26,6 @@ class LocalHandler implements ArtifactsHandlerInterface
         if (!$filesystem->exists($this->directory)) {
             $filesystem->mkdir($this->directory, 0777);
         }
-        $this->kernel = $kernel;
     }
 
     /**
@@ -54,27 +38,11 @@ class LocalHandler implements ArtifactsHandlerInterface
 
         file_put_contents($screenshotName, $file);
 
-        if ($this->useLocalUrl()) {
-            return $this->getLink('file://'.$screenshotName, $screenshotName);
-        }
-        $url = trim($this->baseUrl, '/').'/'.trim($fileName, '/');
-
-        return $this->getLink($url, $url);
-    }
-
-    private function useLocalUrl(): bool
-    {
-        $container = $this->kernel->getContainer();
-        $installed = (bool)$container->getParameter('installed');
-        if (!$installed) {
-            return true;
-        }
-        $applicationUrl = $container->get('oro_config.manager')->get('oro_ui.application_url');
-        if (!str_contains($this->baseUrl, $applicationUrl)) {
-            return true;
+        if ($this->baseUrl) {
+            return $this->baseUrl.$fileName;
         }
 
-        return false;
+        return 'file://'.$screenshotName;
     }
 
     /**
@@ -83,10 +51,5 @@ class LocalHandler implements ArtifactsHandlerInterface
     public static function getConfigKey()
     {
         return 'local';
-    }
-
-    private function getLink(string $url, $title): string
-    {
-        return "\033]8;;$url\033\\$title\033]8;;\033\\";
     }
 }
