5309ba30426cf96ed1aea4b9e5ed68c208a61eb4
oro-panasiukr <39378223+oro-panasiukr@users.noreply.github.com>
2021-10-22 08:10:25 +0300

    BAP-20799: Wrong error log level for out of memory and fatal errors (#30941)
    * BAP-20799: Wrong error log level for out of memory and fatal errors

---
+++ src/Oro/Bundle/LoggerBundle/Tests/Unit/EventSubscriber/ConsoleErrorSubscriberTest.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Oro\Bundle\LoggerBundle\Tests\Unit\EventSubscriber;
+
+use Oro\Bundle\LoggerBundle\EventSubscriber\ConsoleErrorSubscriber;
+use Oro\Bundle\MessageQueueBundle\Command\CleanupCommand;
+use Oro\Component\MessageQueue\Client\ConsumeMessagesCommand;
+use Psr\Log\LoggerInterface;
+use Psr\Log\LogLevel;
+use Symfony\Component\Console\Event\ConsoleCommandEvent;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\ErrorHandler\ErrorHandler;
+
+class ConsoleErrorSubscriberTest extends \PHPUnit\Framework\TestCase
+{
+    /** @var ConsoleErrorSubscriber */
+    private $subscriber;
+
+    /** @var LoggerInterface|\PHPUnit\Framework\MockObject\MockObject */
+    private $logger;
+
+    /** @var ErrorHandler */
+    private $handler;
+
+    /** @var InputInterface|\PHPUnit\Framework\MockObject\MockObject */
+    private $input;
+
+    /** @var OutputInterface|\PHPUnit\Framework\MockObject\MockObject */
+    private $output;
+
+    /** @var array[] */
+    private static $defaultLoggers = [
+        E_DEPRECATED => [null, LogLevel::INFO],
+        E_USER_DEPRECATED => [null, LogLevel::INFO],
+        E_NOTICE => [null, LogLevel::WARNING],
+        E_USER_NOTICE => [null, LogLevel::WARNING],
+        E_STRICT => [null, LogLevel::WARNING],
+        E_WARNING => [null, LogLevel::WARNING],
+        E_USER_WARNING => [null, LogLevel::WARNING],
+        E_COMPILE_WARNING => [null, LogLevel::WARNING],
+        E_CORE_WARNING => [null, LogLevel::WARNING],
+    ];
+
+    protected function setUp(): void
+    {
+        $this->logger = $this->createMock(LoggerInterface::class);
+        $this->subscriber = new ConsoleErrorSubscriber($this->logger);
+
+        $this->handler = ErrorHandler::register();
+        $this->input = $this->createMock(InputInterface::class);
+        $this->output = $this->createMock(OutputInterface::class);
+    }
+
+    protected function tearDown(): void
+    {
+        $this->handler = null;
+        restore_error_handler();
+        restore_exception_handler();
+    }
+
+    /**
+     * @dataProvider commandDataProvider
+     */
+    public function testConfigure(string $command): void
+    {
+        $this->subscriber->configure(
+            new ConsoleCommandEvent($this->createMock($command), $this->input, $this->output)
+        );
+
+        $this->assertSame(
+            self::$defaultLoggers + [
+                E_USER_ERROR => [$this->logger, LogLevel::CRITICAL],
+                E_RECOVERABLE_ERROR => [$this->logger, LogLevel::CRITICAL],
+                E_COMPILE_ERROR => [$this->logger, LogLevel::CRITICAL],
+                E_PARSE => [$this->logger, LogLevel::CRITICAL],
+                E_ERROR => [$this->logger, LogLevel::CRITICAL],
+                E_CORE_ERROR => [$this->logger, LogLevel::CRITICAL],
+            ],
+            $this->handler->setLoggers([])
+        );
+    }
+
+    /**
+     * @return array|\string[][]
+     */
+    public function commandDataProvider(): array
+    {
+        return [
+            [ConsumeMessagesCommand::class],
+            [CleanupCommand::class]
+        ];
+    }
+}
