648bda2e351eea0e1534a6f230a9b00ecb95ec4d
Viacheslav Chekanovskiy <vchekanovskyi@oroinc.com>
2021-11-12 19:00:41 +0200

    BAP-20575: Doc: Guide for enabling popular JS frameworks on Storefront (#31007)

---
+++ src/Oro/Bundle/UIBundle/Resources/public/js/app/modules/react-module.js
@@ -0,0 +1,6 @@
+import ComponentShortcutsManager from 'oroui/js/component-shortcuts-manager';
+
+ComponentShortcutsManager.add('react-app', {
+    moduleName: 'oroui/js/app/components/react-app-component',
+    scalarOption: 'reactApp'
+});
