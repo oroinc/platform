4addcb2ee0f4cb616f867d17abf792dc0cc72660
Yefim Yevtushenko <yyevtushenko@oroinc.com>
2022-01-21 15:57:29 +0200

    BAP-20374: Errors in logs when starting message consumer after clean installation with sample data (#31445)
    - fixed LoadProductDefaultAttributeFamilyData should not delete attribute groups
    - added DbalTypeDefaultValueProvider for getting default value for DBAL type
    - made use of DbalTypeDefaultValueProvider in AttributeValueProvider
    - added DbalTypeDefaultValuePass to provide default values for WYSIWYG-related DBAL types
    - made use of spaceship operator in FolderContentCumulativeLoader

--- src/Oro/Bundle/EntityConfigBundle/Provider/AttributeValueProvider.php
+++ src/Oro/Bundle/EntityConfigBundle/Provider/AttributeValueProvider.php
@@ -2,37 +2,86 @@
 
 namespace Oro\Bundle\EntityConfigBundle\Provider;
 
+use Doctrine\ORM\Mapping\ClassMetadataInfo;
 use Oro\Bundle\EntityBundle\ORM\DoctrineHelper;
 use Oro\Bundle\EntityConfigBundle\Attribute\Entity\AttributeFamily;
+use Oro\Bundle\PlatformBundle\Provider\DbalTypeDefaultValueProvider;
 use Oro\Component\DoctrineUtils\ORM\QueryBuilderUtil;
 
+/**
+ * Sets attributes of the specified {@see AttributeFamily} to NULL.
+ */
 class AttributeValueProvider implements AttributeValueProviderInterface
 {
-    /**
-     * @var DoctrineHelper
-     */
-    protected $doctrineHelper;
+    protected DoctrineHelper $doctrineHelper;
+
+    private DbalTypeDefaultValueProvider $dbalTypeDefaultValueProvider;
 
     public function __construct(DoctrineHelper $doctrineHelper)
     {
         $this->doctrineHelper = $doctrineHelper;
+
+        // BC layer in case provider is not set via setter.
+        $this->dbalTypeDefaultValueProvider = new DbalTypeDefaultValueProvider();
     }
 
-    public function removeAttributeValues(AttributeFamily $attributeFamily, array $names)
+    public function setDbalTypeDefaultValueProvider(DbalTypeDefaultValueProvider $dbalTypeDefaultValueProvider): void
     {
-        $manager = $this->doctrineHelper->getEntityManagerForClass($attributeFamily->getEntityClass());
+        $this->dbalTypeDefaultValueProvider = $dbalTypeDefaultValueProvider;
+    }
 
-        $queryBuilder = $manager->createQueryBuilder();
+    /**
+     * @param AttributeFamily $attributeFamily
+     * @param string[] $names
+     */
+    public function removeAttributeValues(AttributeFamily $attributeFamily, array $names): void
+    {
+        $entityClass = $attributeFamily->getEntityClass();
+        $entityManager = $this->doctrineHelper->getEntityManagerForClass($entityClass);
+
+        $queryBuilder = $entityManager->createQueryBuilder();
         $queryBuilder
-            ->update($attributeFamily->getEntityClass(), 'entity')
+            ->update($entityClass, 'entity')
             ->where($queryBuilder->expr()->eq('entity.attributeFamily', ':attributeFamily'))
-            ->setParameter('attributeFamily', $attributeFamily)
-            ->setParameter('null', null);
+            ->setParameter('attributeFamily', $attributeFamily);
 
+        $classMetadata = $entityManager->getClassMetadata($entityClass);
+        $doExecute = false;
         foreach ($names as $name) {
-            $queryBuilder->set(QueryBuilderUtil::getField('entity', $name), ':null');
+            if ($classMetadata->hasField($name)) {
+                $mapping = $classMetadata->getFieldMapping($name);
+                $default = $mapping['default'] ?? null;
+
+                if (!isset($default) && !$classMetadata->isNullable($name)) {
+                    if (!$this->dbalTypeDefaultValueProvider->hasDefaultValueForDbalType($mapping['type'])) {
+                        // Skips because default value cannot be found.
+                        continue;
+                    }
+
+                    $default = $this->dbalTypeDefaultValueProvider->getDefaultValueForDbalType($mapping['type']);
+                }
+            } elseif ($classMetadata->hasAssociation($name)) {
+                $mapping = $classMetadata->getAssociationMapping($name);
+                if (!($mapping['type'] & ClassMetadataInfo::TO_ONE)) {
+                    // Skips because to-many association type is not supported yet.
+                    continue;
+                }
+
+                $default = null;
+            } else {
+                // Skips because field or association is missing.
+                continue;
+            }
+
+            $parameterName = QueryBuilderUtil::sprintf(':%s', $name);
+            $queryBuilder
+                ->set(QueryBuilderUtil::getField('entity', $name), $parameterName)
+                ->setParameter($parameterName, $default);
+            $doExecute = true;
         }
 
-        $queryBuilder->getQuery()->execute();
+        if ($doExecute) {
+            $queryBuilder->getQuery()->execute();
+        }
     }
 }
