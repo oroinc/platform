2f795ef14f073c1c903c4404285a398f65494909
Yefim Yevtushenko <yyevtushenko@oroinc.com>
2022-01-31 11:13:42 +0200

    BB-20874: Missing validation message for date field on customer edit profile (#31626)
    - added jsValidation payload to Type constraints checking DateTimeInterface type
    - added use type specified in jsValidation payload in ConstraintConverter
    - updated behat test

--- src/Oro/Bundle/FormBundle/Form/Extension/JsValidation/ConstraintConverter.php
+++ src/Oro/Bundle/FormBundle/Form/Extension/JsValidation/ConstraintConverter.php
@@ -2,18 +2,40 @@
 
 namespace Oro\Bundle\FormBundle\Form\Extension\JsValidation;
 
+use Oro\Bundle\FormBundle\Validator\ConstraintFactory;
 use Symfony\Component\Validator\Constraint;
 
 /**
  * The base implementation of a service to convert validation constraint to a form suitable for JS validation.
+ * Creates a constraint based on jsValidation payload if any, returns as-is otherwise.
  */
 class ConstraintConverter implements ConstraintConverterInterface
 {
+    private ConstraintFactory $constraintFactory;
+
+    public function __construct()
+    {
+        // BC workaround to ensure that constraint factory is always set.
+        $this->constraintFactory = new ConstraintFactory();
+    }
+
+    public function setConstraintFactory(ConstraintFactory $constraintFactory): void
+    {
+        $this->constraintFactory = $constraintFactory;
+    }
+
     /**
      * {@inheritDoc}
      */
     public function convertConstraint(Constraint $constraint): ?Constraint
     {
+        if ($this->constraintFactory && isset($constraint->payload['jsValidation']['type'])) {
+            return $this->constraintFactory->create(
+                $constraint->payload['jsValidation']['type'],
+                $constraint->payload['jsValidation']['options'] ?? []
+            );
+        }
+
         return $constraint;
     }
 }
