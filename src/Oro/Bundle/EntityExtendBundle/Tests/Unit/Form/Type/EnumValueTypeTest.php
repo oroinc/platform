05a36e45dd16001b4a0322d64ab6c16255455314
Vova Soroka <vsoroka@oroinc.com>
2022-02-12 23:04:48 +0200

    Refactor some unit tests that use FormIntegrationTestCase (#31973)
    - refactor Oro\Component\Testing\Unit\FormIntegrationTestCase and unit tests that use this test case

--- src/Oro/Bundle/EntityExtendBundle/Tests/Unit/Form/Type/EnumValueTypeTest.php
+++ src/Oro/Bundle/EntityExtendBundle/Tests/Unit/Form/Type/EnumValueTypeTest.php
@@ -7,6 +7,7 @@ use Oro\Bundle\EntityConfigBundle\Config\Id\FieldConfigId;
 use Oro\Bundle\EntityConfigBundle\Provider\ConfigProvider;
 use Oro\Bundle\EntityExtendBundle\Form\Type\EnumValueType;
 use Oro\Bundle\TranslationBundle\Translation\IdentityTranslator;
+use Oro\Component\Testing\Unit\FormIntegrationTestCase;
 use Oro\Component\Testing\Unit\PreloadedExtension;
 use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\Extension\Validator\Type\FormTypeValidatorExtension;
@@ -14,29 +15,18 @@ use Symfony\Component\Form\Extension\Validator\ValidatorExtension;
 use Symfony\Component\Form\FormConfigInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\Form\Test\FormInterface;
-use Symfony\Component\Form\Test\TypeTestCase;
-use Symfony\Component\Validator\Constraint;
-use Symfony\Component\Validator\Constraints\CollectionValidator;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\ConstraintValidatorFactory;
-use Symfony\Component\Validator\ConstraintValidatorFactoryInterface;
-use Symfony\Component\Validator\ConstraintValidatorInterface;
 use Symfony\Component\Validator\Context\ExecutionContextFactory;
-use Symfony\Component\Validator\Mapping\ClassMetadata;
 use Symfony\Component\Validator\Mapping\Factory\LazyLoadingMetadataFactory;
 use Symfony\Component\Validator\Mapping\Loader\LoaderChain;
-use Symfony\Component\Validator\Mapping\Loader\LoaderInterface;
-use Symfony\Component\Validator\Mapping\Loader\YamlFileLoader;
 use Symfony\Component\Validator\Validator\RecursiveValidator;
 
-class EnumValueTypeTest extends TypeTestCase
+class EnumValueTypeTest extends FormIntegrationTestCase
 {
     /** @var EnumValueType */
     private $type;
-
-    /** @var ConstraintValidatorInterface[] */
-    private $validators;
 
     protected function setUp(): void
     {
@@ -47,7 +37,7 @@ class EnumValueTypeTest extends TypeTestCase
     /**
      * {@inheritDoc}
      */
-    protected function getExtensions()
+    protected function getExtensions(): array
     {
         $validator = new RecursiveValidator(
             new ExecutionContextFactory(new IdentityTranslator()),
@@ -317,74 +307,6 @@ class EnumValueTypeTest extends TypeTestCase
                 ],
             ],
         ];
-    }
-
-    private function getValidator(): RecursiveValidator
-    {
-        $loader = $this->createMock(LoaderInterface::class);
-        $loader->expects($this->any())
-            ->method('loadClassMetadata')
-            ->willReturnCallback(function (ClassMetadata $meta) {
-                $this->loadMetadata($meta);
-            });
-
-        return new RecursiveValidator(
-            new ExecutionContextFactory(new IdentityTranslator()),
-            new LazyLoadingMetadataFactory($loader),
-            $this->getConstraintValidatorFactory()
-        );
-    }
-
-    private function loadMetadata(ClassMetadata $meta): void
-    {
-        $configFile = $this->getConfigFile($meta->name);
-        if ($configFile) {
-            $loader = new YamlFileLoader($configFile);
-            $loader->loadClassMetadata($meta);
-        }
-    }
-
-    private function getConstraintValidatorFactory(): ConstraintValidatorFactoryInterface
-    {
-        $factory = $this->createMock(ConstraintValidatorFactoryInterface::class);
-        $factory->expects($this->any())
-            ->method('getInstance')
-            ->willReturnCallback(function (Constraint $constraint) {
-                $className = $constraint->validatedBy();
-
-                if (!isset($this->validators[$className]) || CollectionValidator::class === $className) {
-                    $this->validators[$className] = new $className();
-                }
-
-                return $this->validators[$className];
-            });
-
-        return $factory;
-    }
-
-    private function getConfigFile(string $class): ?string
-    {
-        $path = $this->getBundleRootPath($class);
-        if ($path) {
-            $path .= '/Resources/config/validation.yml';
-            if (!is_readable($path)) {
-                $path = null;
-            }
-        }
-
-        return $path;
-    }
-
-    private function getBundleRootPath(string $class): ?string
-    {
-        $refl = new \ReflectionClass($class);
-        $path = null;
-        $pos = strrpos($refl->getFileName(), 'Bundle');
-        if (false !== $pos) {
-            $path = substr($refl->getFileName(), 0, $pos) . 'Bundle';
-        }
-
-        return $path;
     }
 
     private function getConfigProvider(
