4a4c07b117cded95889a20c878dd50d9d9f533dc
Dima Shatovskiy <dshatovskiy@oroinc.com>
2022-02-15 10:29:58 +0200

    BAP-21171: Shipping and ShoppingList bundles migration to Symfony cache (#31956)

--- src/Oro/Bundle/CacheBundle/OroCacheBundle.php
+++ src/Oro/Bundle/CacheBundle/OroCacheBundle.php
@@ -3,6 +3,7 @@
 namespace Oro\Bundle\CacheBundle;
 
 use Oro\Bundle\CacheBundle\DependencyInjection\Compiler\CacheConfigurationPass;
+use Oro\Bundle\CacheBundle\DependencyInjection\Compiler\CachePoolConfigurationPass;
 use Oro\Bundle\CacheBundle\DependencyInjection\Compiler\ValidateCacheConfigurationPass;
 use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -20,6 +21,7 @@ class OroCacheBundle extends Bundle
     {
         parent::build($container);
 
+        $container->addCompilerPass(new CachePoolConfigurationPass(), PassConfig::TYPE_BEFORE_OPTIMIZATION, 255);
         $container->addCompilerPass(new CacheConfigurationPass());
         $container->addCompilerPass(new ValidateCacheConfigurationPass(), PassConfig::TYPE_BEFORE_REMOVING);
     }
