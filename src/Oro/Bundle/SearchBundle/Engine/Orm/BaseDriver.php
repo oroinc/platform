618e951594a44d701f03eba926704bbc6fecb231
Yevhen Shyshkin <yshyshkin@oroinc.com>
2022-02-25 12:47:36 +0100

    BB-20451: Elasticsearch static mapping mode  - fixed aggregation field behaviour for flat object fields in API  - fixed minor issue in ORM search driver

--- src/Oro/Bundle/SearchBundle/Engine/Orm/BaseDriver.php
+++ src/Oro/Bundle/SearchBundle/Engine/Orm/BaseDriver.php
@@ -250,6 +250,7 @@ abstract class BaseDriver implements DBALPersisterInterface
             $field = $options['field'];
             $function = $options['function'];
             [$fieldType, $fieldName] = Criteria::explodeFieldTypeName($field);
+            $fieldName = str_replace('.', self::SPECIAL_SEPARATOR, $fieldName);
             QueryBuilderUtil::checkField($fieldName);
 
             // prepare query builder to apply grouping
