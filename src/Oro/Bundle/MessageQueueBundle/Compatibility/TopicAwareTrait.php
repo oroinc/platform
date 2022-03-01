b183baf11980d93a2158338a33f6d5d85c77bd47
Dmitry Khrysev <x86demon@users.noreply.github.com>
2022-03-01 17:55:07 +0200

    BB-18221: Improve combined price list operations scalability (#31852)
    * BB-18221: Improve combined price list operations scalability (#31503)
    
     - replaced uniques index with index for oro_price_product_combined to get rid of locks on PgSQL
     - removed unused code
     - added fallback CPL usage improvement with enhanced logic of fallback CPL detection
     - added fallback CPL processing for products
     - new MQ Topics for pricing bundle
     - added logger channel to oro_pricing
     - unique products indexation

---
+++ src/Oro/Bundle/MessageQueueBundle/Compatibility/TopicAwareTrait.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Oro\Bundle\MessageQueueBundle\Compatibility;
+
+use Oro\Component\MessageQueue\Transport\MessageInterface;
+use Oro\Component\MessageQueue\Util\JSON;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * This trait introduces simplified way to get resolved message body based on MQ Topic services introduced in 5.0
+ */
+trait TopicAwareTrait
+{
+    /**
+     * @var TopicInterface
+     */
+    protected $topic;
+
+    public function setTopic(TopicInterface $topic): void
+    {
+        $this->topic = $topic;
+    }
+
+    protected function getResolvedBody(
+        MessageInterface $message,
+        ?LoggerInterface $logger = null
+    ): ?array {
+        if (!$this->topic) {
+            throw new \InvalidArgumentException('Required topic dependency was not added');
+        }
+
+        try {
+            $body = JSON::decode($message->getBody());
+            $resolver = new OptionsResolver();
+            $this->topic->configureMessageBody($resolver);
+
+            return $resolver->resolve($body);
+        } catch (\Exception $e) {
+            if ($logger) {
+                $logger->critical(
+                    'Got invalid message.',
+                    [
+                        'topic' => $this->topic::getName(),
+                        'message' => $message->getBody(),
+                        'exception' => $e
+                    ]
+                );
+            }
+
+            return null;
+        }
+    }
+}
