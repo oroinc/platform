5fdbd93640000c425670d96faf005eb6ed33b292
Andrey Yatsenco <ayatsenko@oroinc.com>
2022-03-08 18:33:33 +0200

    BAP-21055: Functional indexes for Doctrine entities documentation
    - updated required PHP version check
    
    Co-authored-by: Victor Tihonchuk <victor@oroinc.com>

--- src/Oro/Bundle/DistributionBundle/OroKernel.php
+++ src/Oro/Bundle/DistributionBundle/OroKernel.php
@@ -30,7 +30,7 @@ use Symfony\Component\Yaml\Yaml;
  */
 abstract class OroKernel extends Kernel
 {
-    public const REQUIRED_PHP_VERSION = '8.0';
+    public const REQUIRED_PHP_VERSION = '8.1';
 
     /** @var string|null */
     private $warmupDir;
