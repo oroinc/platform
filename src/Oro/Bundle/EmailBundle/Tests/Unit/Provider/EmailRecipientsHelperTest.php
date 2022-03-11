25c0d8bd6f3d66af82d687301314930f8cb4f4d6
Dmitry Khrysev <dkhrysev@oroinc.com>
2022-03-11 17:53:28 +0200

    BB-21196: Email recipients list is not retrieved for quotes with added shipping address

--- src/Oro/Bundle/EmailBundle/Tests/Unit/Provider/EmailRecipientsHelperTest.php
+++ src/Oro/Bundle/EmailBundle/Tests/Unit/Provider/EmailRecipientsHelperTest.php
@@ -14,6 +14,7 @@ use Oro\Bundle\EmailBundle\Model\EmailRecipientsProviderArgs;
 use Oro\Bundle\EmailBundle\Model\Recipient;
 use Oro\Bundle\EmailBundle\Model\RecipientEntity;
 use Oro\Bundle\EmailBundle\Provider\EmailRecipientsHelper;
+use Oro\Bundle\EmailBundle\Tests\Unit\Stub\AddressStub;
 use Oro\Bundle\EmailBundle\Tests\Unit\Stub\CustomerStub;
 use Oro\Bundle\EmailBundle\Tools\EmailAddressHelper;
 use Oro\Bundle\EntityBundle\Tests\Unit\ORM\Stub\ReflectionProperty;
@@ -28,6 +29,8 @@ use Oro\Bundle\SearchBundle\Query\Query;
 use Oro\Bundle\SearchBundle\Query\Result;
 use Oro\Bundle\SearchBundle\Query\Result\Item;
 use Oro\Bundle\SecurityBundle\ORM\Walker\AclHelper;
+use Oro\Bundle\UserBundle\Entity\Repository\UserRepository;
+use Oro\Bundle\UserBundle\Entity\User;
 use Oro\Bundle\UserBundle\Entity\Repository\UserRepository;
 use Oro\Bundle\UserBundle\Entity\User;
 use Symfony\Component\PropertyAccess\PropertyAccessor;
@@ -136,8 +139,8 @@ class EmailRecipientsHelperTest extends \PHPUnit\Framework\TestCase
         $stubResult = new Result($searchQueryMock, [$searchResultMock]);
 
         $this->indexer->expects($this->once())
-             ->method('simpleSearch')
-             ->willReturn($stubResult);
+            ->method('simpleSearch')
+            ->willReturn($stubResult);
 
         $this->emailRecipientsHelper->getRecipients($args, $userRepository, 'u', User::class);
     }
@@ -149,9 +152,9 @@ class EmailRecipientsHelperTest extends \PHPUnit\Framework\TestCase
                 new EmailRecipientsProviderArgs(null, null, 1),
                 [
                     [
-                        'name'  => 'Recipient <recipient@example.com>',
+                        'name' => 'Recipient <recipient@example.com>',
                         'email' => 'recipient@example.com',
-                        'entityId'     => 1,
+                        'entityId' => 1,
                         'organization' => 'org',
                     ],
                 ],
@@ -268,9 +271,9 @@ class EmailRecipientsHelperTest extends \PHPUnit\Framework\TestCase
             [
                 [
                     [
-                        'name'  => 'Recipient',
+                        'name' => 'Recipient',
                         'email' => 'recipient@example.com',
-                        'entityId'     => 1,
+                        'entityId' => 1,
                         'organization' => 'org',
                     ],
                 ],
@@ -308,9 +311,9 @@ class EmailRecipientsHelperTest extends \PHPUnit\Framework\TestCase
             [
                 [
                     [
-                        'name'  => 'Recipient',
+                        'name' => 'Recipient',
                         'email' => 'recipient@example.com',
-                        'entityId'     => 1,
+                        'entityId' => 1,
                         'organization' => 'org',
                     ],
                 ],
@@ -355,27 +358,7 @@ class EmailRecipientsHelperTest extends \PHPUnit\Framework\TestCase
             ->with($object)
             ->willReturn('Customer Name');
 
-        $this->configManager->expects(self::once())
-            ->method('hasConfig')
-            ->with(CustomerStub::class)
-            ->willReturn(true);
-
-        $config = $this->createMock(ConfigInterface::class);
-
-        $this->configManager->expects(self::once())
-            ->method('getConfig')
-            ->with(new EntityConfigId('entity', CustomerStub::class))
-            ->willReturn($config);
-
-        $config->expects(self::once())
-            ->method('get')
-            ->with('label')
-            ->willReturn('CustomerStub');
-
-        $this->translator->expects(self::once())
-            ->method('trans')
-            ->with('CustomerStub')
-            ->willReturn('Customer Stub');
+        $this->assertGetClassLabelCalls(CustomerStub::class, 'Customer Stub');
 
         $expected = [
             '"John Doe" <admin@example.com>|Oro\Bundle\EmailBundle\Tests\Unit\Stub\CustomerStub|ORO' => new Recipient(
@@ -464,5 +447,82 @@ class EmailRecipientsHelperTest extends \PHPUnit\Framework\TestCase
             $expected,
             $this->emailRecipientsHelper->createRecipientsFromEmails($emails, $object)
         );
+    }
+
+    /**
+     * @dataProvider creteRecipientEntityDataProvider
+     */
+    public function testCreateRecipientEntity(object $object, RecipientEntity $recipientEntity)
+    {
+        $className = get_class($object);
+        $objectMetadata = $this->getEntity(ClassMetadata::class, [
+            'name' => $className,
+            'identifier' => ['id'],
+            'reflFields' => [
+                'id' => new ReflectionProperty($className, 'id', [spl_object_hash($object) => 1])
+            ],
+        ], [null]);
+        $this->nameFormatter->expects(self::once())
+            ->method('format')
+            ->with($object)
+            ->willReturn('NAME');
+
+        $this->assertGetClassLabelCalls($className, 'Object Stub');
+
+        $this->assertEquals(
+            $recipientEntity,
+            $this->emailRecipientsHelper->createRecipientEntity($object, $objectMetadata)
+        );
+    }
+
+    public function creteRecipientEntityDataProvider()
+    {
+        $organization = (new Organization())->setName('ORO');
+        $customer = new CustomerStub('Customer Name', $organization);
+
+        yield [
+            $customer,
+            new RecipientEntity(
+                CustomerStub::class,
+                1,
+                'NAME (Object Stub)',
+                'ORO'
+            )
+        ];
+
+        yield [
+            new AddressStub('Address Name', 'OrgName'),
+            new RecipientEntity(
+                AddressStub::class,
+                1,
+                'NAME (Object Stub)',
+                null
+            )
+        ];
+    }
+
+    protected function assertGetClassLabelCalls(string $className, string $label): void
+    {
+        $this->configManager->expects(self::once())
+            ->method('hasConfig')
+            ->with($className)
+            ->willReturn(true);
+
+        $config = $this->createMock(ConfigInterface::class);
+
+        $this->configManager->expects(self::once())
+            ->method('getConfig')
+            ->with(new EntityConfigId('entity', $className))
+            ->willReturn($config);
+
+        $config->expects(self::once())
+            ->method('get')
+            ->with('label')
+            ->willReturn('ObjectStub');
+
+        $this->translator->expects(self::once())
+            ->method('trans')
+            ->with('ObjectStub')
+            ->willReturn($label);
     }
 }
