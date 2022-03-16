3fa2465b59e8afb1d9168e7b7a5dbd30dcfd9c11
Yefim Yevtushenko <yyevtushenko@oroinc.com>
2022-03-16 17:07:48 +0200

    BB-21069: Randomly failed regression test due to message queue time out (#31702)
    - added waitUtil to mq process
    - changed ps ax to pgrep to avoid hanging
    - added skip checking for running processes if no pid file info available
    - added catching runtime exceptions during checking for running processes
    - updated tests

--- src/Oro/Bundle/TestFrameworkBundle/Behat/Processor/MessageQueueProcessor.php
+++ src/Oro/Bundle/TestFrameworkBundle/Behat/Processor/MessageQueueProcessor.php
@@ -5,6 +5,7 @@ namespace Oro\Bundle\TestFrameworkBundle\Behat\Processor;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\Process\Exception\RuntimeException;
 use Symfony\Component\Process\PhpExecutableFinder;
 use Symfony\Component\Process\Process;
 
@@ -20,17 +21,21 @@ class MessageQueueProcessor implements MessageQueueProcessorInterface
      */
     private const MAX_RETRIES = 20;
 
-    /** @var KernelInterface */
-    private $kernel;
+    /**
+     * Maximum number of retries to start a consumer.
+     */
+    private const MAX_START_RETRIES = 3;
+
+    private ?KernelInterface $kernel = null;
 
     /** @var Process[] */
-    private $processes = [];
+    private array $processes = [];
 
-    /** @var \DateTime */
-    private $lastCacheStateChangeDate;
+    private ?\DateTimeInterface $lastCacheStateChangeDate = null;
 
-    /** @var int */
-    private $retries = 0;
+    private int $retries = 0;
+
+    private int $startRetries = 0;
 
     public function __construct(KernelInterface $kernel)
     {
@@ -43,37 +48,84 @@ class MessageQueueProcessor implements MessageQueueProcessorInterface
     public function startMessageQueue()
     {
         // start processes if they are not initialized
-        // lazy loading, allows to start MQ without status control
+        // lazy loading, allows starting MQ without status control
+
         if (!$this->processes) {
-            /** @var Filesystem $filesystem */
-            $filesystem = $this->kernel->getContainer()->get('filesystem');
-            $phpBinaryFinder = new PhpExecutableFinder();
-            $phpBinaryPath = $phpBinaryFinder->find();
-
-            $command = [
-                $phpBinaryPath,
-                sprintf('%s/console', realpath($this->kernel->getProjectDir()) . '/bin'),
-                'oro:message-queue:consume',
-                '-vv',
-                sprintf('--env=%s', $this->kernel->getEnvironment())
-            ];
-
             for ($i = 1; $i <= self::CONSUMERS_AMOUNT; $i++) {
-                $process = new Process($command);
-
-                $process->setTimeout(self::TIMEOUT);
-                $process->setIdleTimeout(self::TIMEOUT);
-                $process->start(function ($type, $buffer) use ($filesystem) {
-                    if (Process::ERR === $type) {
-                        $this->getLogger()->error($buffer);
-                    }
-
-                    $filesystem->appendToFile(sprintf('%s/mq.log', realpath($this->kernel->getLogDir())), $buffer);
-                });
-
-                $this->processes[] = $process;
+                $this->processes[] = $this->startConsumerProcess();
             }
         }
+    }
+
+    protected function startConsumerProcess(): Process
+    {
+        /** @var Filesystem $filesystem */
+        $filesystem = $this->kernel->getContainer()->get('filesystem');
+
+        $phpBinaryFinder = new PhpExecutableFinder();
+        $phpBinaryPath = $phpBinaryFinder->find();
+
+        $command = [
+            $phpBinaryPath,
+            sprintf('%s/console', realpath($this->kernel->getProjectDir()) . '/bin'),
+            'oro:message-queue:consume',
+            '-vv',
+            sprintf('--env=%s', $this->kernel->getEnvironment())
+        ];
+
+        $logDir = realpath($this->kernel->getLogDir());
+
+        $process = new Process($command);
+        $process->setTimeout(self::TIMEOUT);
+        $process->setIdleTimeout(self::IDLE_TIMEOUT);
+
+        $this->getLogger()->warning(
+            'Starting consumer with the command: {command}',
+            ['command' => $process->getCommandLine()]
+        );
+
+        $process->start(function ($type, $buffer) use ($filesystem, $logDir) {
+            if (Process::ERR === $type) {
+                $this->getLogger()->error($buffer);
+            }
+
+            $filesystem->appendToFile(sprintf('%s/mq.log', $logDir), $buffer);
+        });
+
+        try {
+            $process->waitUntil(function ($type, $buffer) use ($filesystem, $logDir) {
+                $isError = Process::ERR === $type;
+                if ($isError) {
+                    $this->getLogger()->error($buffer);
+                }
+
+                $filesystem->appendToFile(sprintf('%s/mq.log', $logDir), $buffer);
+
+                return true;
+            });
+        } catch (RuntimeException $exception) {
+            $this->getLogger()->error(
+                'Failed starting a message queue consumer: {message}',
+                ['message' => $exception->getMessage(), 'exception' => $exception]
+            );
+
+            ++$this->startRetries;
+
+            if ($this->startRetries > self::MAX_START_RETRIES) {
+                throw new \RuntimeException('Failed starting a message queue consumer');
+            }
+
+            $this->getLogger()->warning(
+                'Retrying to start a message queue consumer. Try #{try}',
+                ['try' => $this->startRetries]
+            );
+
+            return $this->startConsumerProcess();
+        }
+
+        $this->startRetries = 0;
+
+        return $process;
     }
 
     /**
@@ -145,6 +197,6 @@ class MessageQueueProcessor implements MessageQueueProcessorInterface
      */
     private function getLogger()
     {
-        return $this->kernel->getContainer()->get('logger');
+        return $this->kernel->getContainer()->get('monolog.logger.consumer');
     }
 }
