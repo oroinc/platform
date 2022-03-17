c9fe23e03b2c6c1ccc9745a35deee40ffaf918d2
Yefim Yevtushenko <yyevtushenko@oroinc.com>
2022-03-17 18:12:31 +0200

    BAP-20436: Gracefully handle websocket server connection failure in WampClient (#32142)
    - added string type casting in WampClient::connect
    - added catching and logging of all exceptions in ConnectionChecker
    - added 30sec time lag when checking token validity in TicketAuthenticationProvider
    - updated tests

--- src/Oro/Bundle/SyncBundle/Tests/Unit/Client/ConnectionCheckerTest.php
+++ src/Oro/Bundle/SyncBundle/Tests/Unit/Client/ConnectionCheckerTest.php
@@ -2,12 +2,14 @@
 
 namespace Oro\Bundle\SyncBundle\Tests\Unit\Client;
 
-use Gos\Component\WebSocketClient\Exception\BadResponseException;
 use Oro\Bundle\SyncBundle\Client\ConnectionChecker;
 use Oro\Bundle\SyncBundle\Client\WebsocketClientInterface;
+use Oro\Bundle\TestFrameworkBundle\Test\Logger\LoggerAwareTraitTestTrait;
 
 class ConnectionCheckerTest extends \PHPUnit\Framework\TestCase
 {
+    use LoggerAwareTraitTestTrait;
+
     /** @var WebsocketClientInterface|\PHPUnit\Framework\MockObject\MockObject */
     protected $client;
 
@@ -17,12 +19,15 @@ class ConnectionCheckerTest extends \PHPUnit\Framework\TestCase
     protected function setUp(): void
     {
         $this->client = $this->createMock(WebsocketClientInterface::class);
+
         $this->checker = new ConnectionChecker($this->client);
+        $this->setUpLoggerMock($this->checker);
     }
 
-    public function testCheckConnection()
+    public function testCheckConnection(): void
     {
-        $this->client->expects($this->once())->method('connect');
+        $this->client->expects($this->once())
+            ->method('connect');
         $this->client->expects($this->once())
             ->method('isConnected')
             ->willReturn(true);
@@ -30,9 +35,10 @@ class ConnectionCheckerTest extends \PHPUnit\Framework\TestCase
         $this->assertTrue($this->checker->checkConnection());
     }
 
-    public function testWsConnectedFail()
+    public function testWsConnectedFail(): void
     {
-        $this->client->expects($this->once())->method('connect');
+        $this->client->expects($this->once())
+            ->method('connect');
         $this->client->expects($this->once())
             ->method('isConnected')
             ->willReturn(false);
@@ -40,13 +46,21 @@ class ConnectionCheckerTest extends \PHPUnit\Framework\TestCase
         $this->assertFalse($this->checker->checkConnection());
     }
 
-    public function testWsConnectedException()
+    public function testWsConnectedException(): void
     {
+        $exception = new \Exception('sample message');
         $this->client->expects($this->once())
             ->method('connect')
-            ->willThrowException(new BadResponseException());
+            ->willThrowException($exception);
         $this->client->expects($this->never())
             ->method('isConnected');
+        $this->loggerMock
+            ->expects(self::once())
+            ->method('warning')
+            ->with(
+                'Failed to connect to websocket server: {message}',
+                ['message' => $exception->getMessage(), 'e' => $exception]
+            );
 
         $this->assertFalse($this->checker->checkConnection());
     }
