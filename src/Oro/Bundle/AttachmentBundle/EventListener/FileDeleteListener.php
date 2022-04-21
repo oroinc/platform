7537209d4f8f5943daa15ef2cb6bd653acc2b9bd
Yurii Muratov <ymuratov@oroinc.com>
2022-04-21 14:35:04 +0300

    BAP-21110: Imported attachments are not properly removed after error during import (#32244)

--- src/Oro/Bundle/AttachmentBundle/EventListener/FileDeleteListener.php
+++ src/Oro/Bundle/AttachmentBundle/EventListener/FileDeleteListener.php
@@ -3,6 +3,8 @@
 namespace Oro\Bundle\AttachmentBundle\EventListener;
 
 use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\ORM\Event\OnFlushEventArgs;
+use Doctrine\ORM\Event\PostFlushEventArgs;
 use Oro\Bundle\AttachmentBundle\Entity\File;
 use Oro\Bundle\AttachmentBundle\Manager\FileManager;
 use Psr\Log\LoggerAwareInterface;
@@ -19,23 +21,55 @@ class FileDeleteListener implements LoggerAwareInterface
     /** @var FileManager */
     private $fileManager;
 
+    private array $filesShouldBeDeleted = [];
+
     public function __construct(FileManager $fileManager)
     {
         $this->fileManager = $fileManager;
         $this->logger = new NullLogger();
     }
 
-    public function postRemove(File $file, LifecycleEventArgs $args): void
+    /**
+     * Clear collected file names that could stay after previous failed flush.
+     */
+    public function onFlush(OnFlushEventArgs $args): void
     {
-        $this->deleteFromFilesystem((string)$file->getFilename());
+        $this->filesShouldBeDeleted = [];
     }
 
+    /**
+     * Collects file names should be removed because the file deletes at all.
+     * The files cannot be removed at this method to be able to cancel deleting if the remove of the
+     * File entity transaction was failed.
+     */
+    public function postRemove(File $file, LifecycleEventArgs $args): void
+    {
+        $this->filesShouldBeDeleted[] = $file->getFilename();
+    }
+
+    /**
+     * Collects file names should be removed because the file entity will have another file.
+     * The files cannot be removed at this method to be able to cancel deleting if the remove of the
+     * File entity transaction was failed.
+     */
     public function postUpdate(File $file, LifecycleEventArgs $args): void
     {
         $changeSet = $args->getEntityManager()->getUnitOfWork()->getEntityChangeSet($file);
         if (!empty($changeSet['filename'][0])) {
-            $this->deleteFromFilesystem($changeSet['filename'][0]);
+            $this->filesShouldBeDeleted[] = $changeSet['filename'][0];
         }
+    }
+
+    /**
+     * Physically removes the files by the list of collected file names.
+     */
+    public function postFlush(PostFlushEventArgs $args): void
+    {
+        foreach ($this->filesShouldBeDeleted as $item) {
+            $this->deleteFromFilesystem($item);
+        }
+
+        $this->filesShouldBeDeleted = [];
     }
 
     private function deleteFromFilesystem(string $filename): void
