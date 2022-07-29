4680445176da7d8c540847bf77276f9efddd8f5a
Dmytro Syrvachov <20385010+dsyrvachov@users.noreply.github.com>
2022-07-29 15:34:18 +0300

    BAP-21518: Incorrect state of local cache during install/upgrade - 5.0 (#33331)
    - introduced Oro\Bundle\MigrationBundle\Migration\ResetContainerMigration interface

---
+++ src/Oro/Bundle/MigrationBundle/Event/PostUpMigrationLifeCycleEvent.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Oro\Bundle\MigrationBundle\Event;
+
+/**
+ * Event triggered after migrations` 'up' method call
+ */
+class PostUpMigrationLifeCycleEvent extends MigrationLifeCycleEvent
+{
+}
