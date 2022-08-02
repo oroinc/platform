896fd8868f13efffcc132b3f11fb44a6a48c5684
Yurii Muratov <ymuratov@oroinc.com>
2022-08-02 18:56:41 +0300

    BAP-21439: Duplicate email is created after sending email with Office365 email sync enabled (#33219)

--- src/Oro/Bundle/EmailBundle/Tests/Unit/Mailer/MailerTest.php
+++ src/Oro/Bundle/EmailBundle/Tests/Unit/Mailer/MailerTest.php
@@ -6,7 +6,12 @@ use Oro\Bundle\EmailBundle\Mailer\Mailer;
 use Symfony\Component\Mailer\Envelope;
 use Symfony\Component\Mailer\SentMessage;
 use Symfony\Component\Mailer\Transport\TransportInterface;
+use Symfony\Component\Mime\Address;
 use Symfony\Component\Mime\Address as SymfonyAddress;
+use Symfony\Component\Mime\Header\Headers;
+use Symfony\Component\Mime\Header\MailboxListHeader;
+use Symfony\Component\Mime\Message;
+use Symfony\Component\Mime\Part\MessagePart;
 use Symfony\Component\Mime\RawMessage;
 
 class MailerTest extends \PHPUnit\Framework\TestCase
@@ -45,5 +50,31 @@ class MailerTest extends \PHPUnit\Framework\TestCase
             ->willReturn($this->createMock(SentMessage::class));
 
         $this->mailer->send($message, $envelope);
+    }
+
+    public function testSendWithMessageObjectShouldHaveRealMessageId(): void
+    {
+        $messageId = 'c9c159856ded379eb75222de5d246041@example.com';
+
+        $toHeader = new MailboxListHeader('To', [new Address('test@test.com', 'test')]);
+        $fromHeader = new MailboxListHeader('From', [new Address('test@test.com', 'test')]);
+        $headers = new Headers($toHeader, $fromHeader);
+        $message = new Message($headers, new MessagePart(new RawMessage('sample body')));
+        $envelope = new Envelope(new SymfonyAddress('from@example.com'), [new SymfonyAddress('to@example.com')]);
+
+        $sentMessage = new SentMessage($message, $envelope);
+        $sentMessage->setMessageId($messageId);
+
+        $this->transport->expects(self::once())
+            ->method('send')
+            ->with($message, $envelope)
+            ->willReturn($sentMessage);
+
+        $this->mailer->send($message, $envelope);
+
+        self::assertEquals(
+            '<' . $messageId . '>',
+            $message->getHeaders()->get('Message-ID')->getBodyAsString()
+        );
     }
 }
--- src/Oro/Bundle/NotificationBundle/Mailer/MassNotificationsMailer.php
+++ src/Oro/Bundle/NotificationBundle/Mailer/MassNotificationsMailer.php
@@ -12,6 +12,7 @@ use Symfony\Component\Mailer\Envelope;
 use Symfony\Component\Mailer\Exception\ExceptionInterface;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mailer\Transport\TransportInterface;
+use Symfony\Component\Mime\Message;
 use Symfony\Component\Mime\RawMessage;
 
 /**
@@ -39,6 +40,13 @@ class MassNotificationsMailer implements MailerInterface, LoggerAwareInterface
             $sentMessage = $this->transport->send($message, $envelope);
 
             $sentCount = count($sentMessage->getEnvelope()->getRecipients());
+            // copy the Message ID header to be able to correct detect sent from the ORO side emails
+            // and avoid duplicates.
+            if ($message instanceof Message) {
+                $headers = $message->getHeaders();
+                $headers->addIdHeader('Message-ID', $sentMessage->getMessageId());
+                $message->setHeaders($headers);
+            }
         } catch (ExceptionInterface $transportException) {
             $sentCount = 0;
 
