ce2ba268a90f6b0122cae7cdd53e82ab2ea1ebd6
Vova Soroka <vsoroka@oroinc.com>
2022-08-06 01:47:52 +0300

    BAP-1675: Remove role from user group (#33478)
    - remove commented code in Oro\Bundle\UserBundle\Form\Type\GroupType and \Oro\Bundle\UserBundle\Migrations\Data\ORM\LoadGroupData

--- src/Oro/Bundle/UserBundle/Form/Type/GroupType.php
+++ src/Oro/Bundle/UserBundle/Form/Type/GroupType.php
@@ -3,83 +3,68 @@
 namespace Oro\Bundle\UserBundle\Form\Type;
 
 use Oro\Bundle\FormBundle\Form\Type\EntityIdentifierType;
+use Oro\Bundle\UserBundle\Entity\Group;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+/**
+ * The fort type for Group entity.
+ */
 class GroupType extends AbstractType
 {
     /**
-     * {@inheritdoc}
+     * {@inheritdoc}
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        /**
-         * Roles was commended due a task BAP-1675
-         */
         $builder
             ->add(
                 'name',
                 TextType::class,
-                array(
+                [
                     'label' => 'oro.user.group.name.label',
                     'required' => true,
-                )
+                ]
             )
-            /*->add(
-                'roles',
-                'entity',
-                array(
-                    'label'    => 'Roles',
-                    'class'    => 'OroUserBundle:Role',
-                    'query_builder' => function ($builder) {
-                        return $builder->createQueryBuilder('r')
-                            ->where('r.role != :anonRole')
-                            ->setParameter('anonRole', User::ROLE_ANONYMOUS);
-                    },
-                    'choice_label' => 'label',
-                    'required' => true,
-                    'multiple' => true,
-                )
-            )*/
             ->add(
                 'appendUsers',
                 EntityIdentifierType::class,
-                array(
+                [
                     'class'    => 'OroUserBundle:User',
                     'required' => false,
                     'mapped'   => false,
                     'multiple' => true,
-                )
+                ]
             )
             ->add(
                 'removeUsers',
                 EntityIdentifierType::class,
-                array(
+                [
                     'class'    => 'OroUserBundle:User',
                     'required' => false,
                     'mapped'   => false,
                     'multiple' => true,
-                )
+                ]
             );
     }
 
     /**
-     * {@inheritdoc}
+     * {@inheritdoc}
      */
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(
-            array(
-                'data_class' => 'Oro\Bundle\UserBundle\Entity\Group',
+            [
+                'data_class' => Group::class,
                 'csrf_token_id' => 'group',
-            )
+            ]
         );
     }
 
     /**
-     * {@inheritdoc}
+     * {@inheritdoc}
      */
     public function getName()
     {
