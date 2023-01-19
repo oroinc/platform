f556d10a3c8bbdbb8030ae470bda6a97d8645194
Alex Yegerev <1755578+lexxcode@users.noreply.github.com>
2023-01-19 14:31:37 +0200

    BB-22048: Use MediaQueryList in ViewportManager (#34563)

--- src/Oro/Bundle/UIBundle/Resources/public/js/app/components/viewport-component.js
+++ src/Oro/Bundle/UIBundle/Resources/public/js/app/components/viewport-component.js
@@ -1,101 +1,90 @@
-define(function(require) {
-    'use strict';
+import viewportManager from 'oroui/js/viewport-manager';
+import BaseComponent from 'oroui/js/app/components/base/component';
+import loadModules from 'oroui/js/app/services/load-modules';
+import _ from 'underscore';
+import mediator from 'oroui/js/mediator';
 
-    const viewportManager = require('oroui/js/viewport-manager');
-    const BaseComponent = require('oroui/js/app/components/base/component');
-    const loadModules = require('oroui/js/app/services/load-modules');
-    const mediator = require('oroui/js/mediator');
-    const _ = require('underscore');
-
-    const ViewportComponent = BaseComponent.extend({
-        /**
-         * @property {Object}
-         */
-        options: {
-            viewport: {},
-            component: null,
-            componentOptions: {}
-        },
-
-        /**
-         * @property {Function} Component constructor
-         */
-        Component: null,
-
-        /**
-         * @property {Object} Component instance
-         */
+const ViewportComponent = BaseComponent.extend({
+    /**
+     * @property {Object}
+     */
+    options: {
+        viewport: 'all',
         component: null,
+        componentOptions: {}
+    },
 
-        /**
-         * @inheritdoc
-         */
-        constructor: function ViewportComponent(options) {
-            ViewportComponent.__super__.constructor.call(this, options);
-        },
+    /**
+     * @property {Function} Component constructor
+     */
+    Component: null,
 
-        /**
-         * @inheritdoc
-         */
-        initialize: function(options) {
-            this.options = _.extend({}, this.options, _.pick(options, ['viewport', 'component']));
-            this.options.componentOptions = _.omit(options, _.keys(this.options));
+    /**
+     * @property {Object} Component instance
+     */
+    component: null,
 
-            this.resolveComponent();
+    /**
+     * @inheritdoc
+     */
+    constructor: function ViewportComponent(options) {
+        ViewportComponent.__super__.constructor.call(this, options);
+    },
 
-            loadModules(this.options.component, this.onComponentLoaded, this);
-        },
+    /**
+     * @inheritdoc
+     */
+    initialize: function(options) {
+        this.options = _.extend({}, this.options, _.pick(options, ['viewport', 'component']));
+        this.options.componentOptions = _.omit(options, _.keys(this.options));
 
-        resolveComponent: function() {
-            if (this.options.component) {
-                return;
-            }
-            if (this.options.componentOptions.view) {
-                this.options.component = 'oroui/js/app/components/view-component';
-            } else if (this.options.componentOptions.widgetModule) {
-                this.options.component = 'oroui/js/app/components/jquery-widget-component';
-            }
-        },
+        // Bind context for callback
+        this.onMediaQueryListChange = this.onMediaQueryListChange.bind(this);
 
-        /**
-         * @inheritdoc
-         */
-        dispose: function() {
-            if (this.disposed) {
-                return;
-            }
-            mediator.off(null, null, this);
-            return ViewportComponent.__super__.dispose.call(this);
-        },
+        this.resolveComponent();
 
-        onComponentLoaded: function(Component) {
-            this.Component = Component;
-            mediator.on('viewport:change', this.onViewportChange, this);
-            this.onViewportChange(viewportManager.getViewport());
-        },
+        loadModules(this.options.component, this.onComponentLoaded, this);
+    },
 
-        onViewportChange: function(viewport) {
-            if (viewport.isApplicable(this.options.viewport)) {
-                this.initializeComponent();
-            } else {
-                this.disposeComponent();
-            }
-        },
-
-        initializeComponent: function() {
-            if (this.component && !this.component.disposed) {
-                return;
-            }
-            this.component = new this.Component(this.options.componentOptions);
-        },
-
-        disposeComponent: function() {
-            if (!this.component || this.component.disposed) {
-                return;
-            }
-            this.component.dispose();
+    resolveComponent: function() {
+        if (this.options.component) {
+            return;
         }
-    });
+        if (this.options.componentOptions.view) {
+            this.options.component = 'oroui/js/app/components/view-component';
+        } else if (this.options.componentOptions.widgetModule) {
+            this.options.component = 'oroui/js/app/components/jquery-widget-component';
+        }
+    },
 
-    return ViewportComponent;
+    onComponentLoaded: function(Component) {
+        this.Component = Component;
+
+        this.listenTo(mediator, 'viewport:change', this.onMediaQueryListChange);
+        this.onMediaQueryListChange();
+    },
+
+    onMediaQueryListChange() {
+        if (viewportManager.isApplicable(this.options.viewport)) {
+            this.initializeComponent();
+        } else {
+            this.disposeComponent();
+        }
+    },
+
+    initializeComponent: function() {
+        if (this.component && !this.component.disposed) {
+            return;
+        }
+        this.component = new this.Component(this.options.componentOptions);
+    },
+
+    disposeComponent: function() {
+        if (!this.component || this.component.disposed) {
+            return;
+        }
+        this.component.dispose();
+    }
 });
+
+export default ViewportComponent;
--- src/Oro/Bundle/UIBundle/Resources/public/js/app/modules/viewport-module.js
+++ src/Oro/Bundle/UIBundle/Resources/public/js/app/modules/viewport-module.js
@@ -1,8 +1,3 @@
 import viewportManager from 'oroui/js/viewport-manager';
-import cssVariablesManager from 'oroui/js/css-variables-manager';
 
-cssVariablesManager.onReady(function(cssVariables) {
-    viewportManager.initialize({
-        cssVariables: cssVariables
-    });
-});
+viewportManager.initialize();
