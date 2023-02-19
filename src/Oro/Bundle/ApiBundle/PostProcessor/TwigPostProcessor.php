6a870446eab97e2db6ddd19e0248eddcd6039cfc
Vova Soroka <vsoroka@oroinc.com>
2023-02-19 22:04:29 +0200

    BAP-21889: Fix loading of TWIG services for API requests (#34904)
    - do not load TWIG services for API requests that do not need them

--- src/Oro/Bundle/ApiBundle/PostProcessor/TwigPostProcessor.php
+++ src/Oro/Bundle/ApiBundle/PostProcessor/TwigPostProcessor.php
@@ -2,18 +2,20 @@
 
 namespace Oro\Bundle\ApiBundle\PostProcessor;
 
+use Psr\Container\ContainerInterface;
+use Symfony\Contracts\Service\ServiceSubscriberInterface;
 use Twig\Environment;
 
 /**
  * Applies a TWIG template to a field value.
  */
-class TwigPostProcessor implements PostProcessorInterface
+class TwigPostProcessor implements PostProcessorInterface, ServiceSubscriberInterface
 {
-    private Environment $twig;
+    private ContainerInterface $container;
 
-    public function __construct(Environment $twig)
+    public function __construct(ContainerInterface $container)
     {
-        $this->twig = $twig;
+        $this->container = $container;
     }
 
     /**
@@ -29,6 +31,25 @@ class TwigPostProcessor implements PostProcessorInterface
         $twigContent['value'] = $value;
         unset($twigContent['template']);
 
-        return $this->twig->render($options['template'], $twigContent);
+        return $this->getTwig()->render($options['template'], $twigContent);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public static function getSubscribedServices(): array
+    {
+        /**
+         * Inject TWIG service via the service locator because it is optional and not all API requests use it,
+         * This solution improves performance of API requests that do not need TWIG.
+         */
+        return [
+            Environment::class
+        ];
+    }
+
+    private function getTwig(): Environment
+    {
+        return $this->container->get(Environment::class);
     }
 }
