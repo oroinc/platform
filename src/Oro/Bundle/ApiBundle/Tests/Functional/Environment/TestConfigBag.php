baad5ec076d712e65886feeae281d7a16b468d37
Vova Soroka <vsoroka@oroinc.com>
2023-04-19 20:35:00 +0300

    BAP-22012: Impossible to rename an association that represented in API as a field (#35407)

--- src/Oro/Bundle/ApiBundle/Tests/Functional/Environment/TestConfigBag.php
+++ src/Oro/Bundle/ApiBundle/Tests/Functional/Environment/TestConfigBag.php
@@ -4,6 +4,7 @@ namespace Oro\Bundle\ApiBundle\Tests\Functional\Environment;
 
 use Oro\Bundle\ApiBundle\Config\EntityConfigMerger;
 use Oro\Bundle\ApiBundle\Provider\ConfigBagInterface;
+use Oro\Bundle\ApiBundle\Util\ConfigUtil;
 
 class TestConfigBag implements ConfigBagInterface
 {
@@ -47,7 +48,9 @@ class TestConfigBag implements ConfigBagInterface
     {
         $result = $this->configBag->getConfig($className, $version);
         if (!empty($this->appendedConfig['entities'][$className])) {
-            $result = $this->entityConfigMerger->merge($this->appendedConfig['entities'][$className], $result);
+            $result = $this->updateRenamedFields(
+                $this->entityConfigMerger->merge($this->appendedConfig['entities'][$className], $result)
+            );
         }
 
         return $result;
@@ -76,5 +79,86 @@ class TestConfigBag implements ConfigBagInterface
         $this->hasChanges = false;
 
         return true;
+    }
+
+    private function updateRenamedFields(array $config): array
+    {
+        if (empty($config[ConfigUtil::FIELDS])) {
+            return $config;
+        }
+
+        $fields = $this->getFieldsWithPropertyPath($config[ConfigUtil::FIELDS]);
+        if (!$fields) {
+            return $config;
+        }
+
+        $toRemoveFields = [];
+        $processedFields = [];
+        foreach ($fields as $field) {
+            $processedFields[] = $field;
+            $propertyPath = $config[ConfigUtil::FIELDS][$field][ConfigUtil::PROPERTY_PATH];
+            $toRenameField = $this->findFieldToRename(
+                $fields,
+                $config[ConfigUtil::FIELDS],
+                $propertyPath,
+                $processedFields
+            );
+            if ($toRenameField) {
+                $config[ConfigUtil::FIELDS][$toRenameField] = array_merge(
+                    $config[ConfigUtil::FIELDS][$field],
+                    $config[ConfigUtil::FIELDS][$toRenameField]
+                );
+                $toRemoveFields[] = $field;
+            } elseif (\array_key_exists($propertyPath, $config[ConfigUtil::FIELDS])) {
+                $mainFieldConfig = $config[ConfigUtil::FIELDS][$propertyPath] ?? [];
+                if (ConfigUtil::IGNORE_PROPERTY_PATH !== ($mainFieldConfig[ConfigUtil::PROPERTY_PATH] ?? null)) {
+                    /** @noinspection SlowArrayOperationsInLoopInspection */
+                    $config[ConfigUtil::FIELDS][$field] = array_merge(
+                        $mainFieldConfig,
+                        $config[ConfigUtil::FIELDS][$field]
+                    );
+                    $toRemoveFields[] = $propertyPath;
+                }
+            }
+        }
+        foreach ($toRemoveFields as $field) {
+            unset($config[ConfigUtil::FIELDS][$field]);
+        }
+
+        return $config;
+    }
+
+    private function getFieldsWithPropertyPath(array $fieldConfigs): array
+    {
+        $fields = [];
+        foreach ($fieldConfigs as $field => $fieldConfig) {
+            if (empty($fieldConfig[ConfigUtil::PROPERTY_PATH])) {
+                continue;
+            }
+            $propertyPath = $fieldConfig[ConfigUtil::PROPERTY_PATH];
+            if (ConfigUtil::IGNORE_PROPERTY_PATH === $propertyPath || $field === $propertyPath) {
+                continue;
+            }
+            $fields[] = $field;
+        }
+
+        return $fields;
+    }
+
+    private function findFieldToRename(
+        array $fields,
+        array $fieldConfigs,
+        string $propertyPath,
+        array $processedFields
+    ): ?string {
+        foreach ($fields as $field) {
+            if ($fieldConfigs[$field][ConfigUtil::PROPERTY_PATH] === $propertyPath
+                && !\in_array($field, $processedFields, true)
+            ) {
+                return $field;
+            }
+        }
+
+        return null;
     }
 }
