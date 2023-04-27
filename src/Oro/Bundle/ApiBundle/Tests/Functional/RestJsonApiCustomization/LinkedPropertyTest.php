a8e9305bd9e24ab7d686f67a7ee4a52be02f7f07
Vova Soroka <vsoroka@oroinc.com>
2023-04-27 21:48:32 +0300

    BAP-22030: Linked field is not returned by API when it is renamed in target entity (#35490)

---
+++ src/Oro/Bundle/ApiBundle/Tests/Functional/RestJsonApiCustomization/LinkedPropertyTest.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Oro\Bundle\ApiBundle\Tests\Functional\RestJsonApiCustomization;
+
+use Oro\Bundle\ApiBundle\Tests\Functional\Environment\Entity\TestEntityForNestedObjects as TestEntity;
+use Oro\Bundle\ApiBundle\Tests\Functional\RestJsonApiTestCase;
+
+/**
+ * @dbIsolationPerTest
+ */
+class LinkedPropertyTest extends RestJsonApiTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->loadFixtures(['@OroApiBundle/Tests/Functional/DataFixtures/linked_property.yml']);
+    }
+
+    public function testGet(): void
+    {
+        /** @var TestEntity $entity */
+        $entity = $this->getReference('test_entity');
+        $entityType = $this->getEntityType(TestEntity::class);
+
+        $response = $this->get(['entity' => $entityType, 'id' => (string)$entity->getId()]);
+
+        $result = self::jsonToArray($response->getContent());
+        self::assertEquals((string)$entity->getId(), $result['data']['id']);
+        self::assertEquals(
+            'item1',
+            $result['data']['attributes']['linkedField']
+        );
+    }
+
+    public function testGetWithoutLinkedPropertyData(): void
+    {
+        /** @var TestEntity $entity */
+        $entity = $this->getReference('test_no_data_entity');
+        $entityType = $this->getEntityType(TestEntity::class);
+
+        $response = $this->get(['entity' => $entityType, 'id' => (string)$entity->getId()]);
+
+        $result = self::jsonToArray($response->getContent());
+        self::assertEquals((string)$entity->getId(), $result['data']['id']);
+        self::assertNull(
+            $result['data']['attributes']['linkedField']
+        );
+    }
+
+    public function testCreateShouldIgnoreLinkedProperty(): void
+    {
+        $entityType = $this->getEntityType(TestEntity::class);
+
+        $data = [
+            'data' => [
+                'type'       => $entityType,
+                'attributes' => [
+                    'linkedField' => 'item1'
+                ]
+            ]
+        ];
+
+        $response = $this->post(['entity' => $entityType], $data);
+
+        $result = self::jsonToArray($response->getContent());
+        self::assertNull(
+            $result['data']['attributes']['linkedField']
+        );
+
+        $this->getEntityManager()->clear();
+        $entity = $this->getEntityManager()->find(TestEntity::class, (int)$result['data']['id']);
+        self::assertNull($entity->getLinked());
+    }
+
+    public function testUpdateShouldIgnoreLinkedProperty(): void
+    {
+        /** @var TestEntity $entity */
+        $entity = $this->getReference('test_entity');
+        $entityType = $this->getEntityType(TestEntity::class);
+
+        $data = [
+            'data' => [
+                'type'       => $entityType,
+                'id'         => (string)$entity->getId(),
+                'attributes' => [
+                    'linkedField' => 'new item'
+                ]
+            ]
+        ];
+
+        $response = $this->patch(
+            ['entity' => $entityType, 'id' => (string)$entity->getId()],
+            $data
+        );
+
+        $result = self::jsonToArray($response->getContent());
+        self::assertEquals(
+            'item1',
+            $result['data']['attributes']['linkedField']
+        );
+
+        $this->getEntityManager()->clear();
+        $entity = $this->getEntityManager()->find(TestEntity::class, $entity->getId());
+        self::assertEquals('item1', $entity->getLinked()->key);
+    }
+}
