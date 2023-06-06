d46b69d26d2369c5d8bfd1bbd84a3ee90c90854c
Vova Soroka <vsoroka@oroinc.com>
2023-06-06 10:45:17 +0300

    BAP-22090: Move old REST API functional tests to Controller namespace (#35776)

---
+++ src/Oro/Bundle/EmailBundle/Tests/Functional/Controller/Api/Rest/EmailActivitySearchControllerTest.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace Oro\Bundle\EmailBundle\Tests\Functional\Controller\Api\Rest;
+
+use Oro\Bundle\EmailBundle\Tests\Functional\DataFixtures\LoadEmailActivityData;
+use Oro\Bundle\TestFrameworkBundle\Test\WebTestCase;
+
+class EmailActivitySearchControllerTest extends WebTestCase
+{
+    /** @var string */
+    private $baseUrl;
+
+    protected function setUp(): void
+    {
+        $this->markTestSkipped('Due to BAP-8365');
+
+        $this->initClient([], $this->generateWsseAuthHeader());
+        $this->loadFixtures([LoadEmailActivityData::class]);
+        $this->baseUrl = $this->getUrl('oro_api_get_email_search_relations');
+    }
+
+    public function testEmailSearch()
+    {
+        $entityClasses = [];
+
+        // No search string - should return all entities:
+        // 3 user loaded by data fixture + admin + 2 simple users from user dependencies fixture
+        $this->client->jsonRequest('GET', $this->baseUrl);
+        $entities = $this->getJsonResponseContent($this->client->getResponse(), 200);
+
+        $this->assertNotEmpty($entities);
+        $this->assertCount(6, $entities);
+        foreach ($entities as $entity) {
+            if (!isset($entityClasses[$entity['entity']])) {
+                $entityClasses[$entity['entity']] = $entity['entity'];
+            }
+        }
+        // Check using multiple entities in from filter. Should return all entities.
+        $this->client->jsonRequest('GET', $this->baseUrl . sprintf('?from=%s', implode(',', $entityClasses)));
+        $this->assertCount(count($entities), $this->getJsonResponseContent($this->client->getResponse(), 200));
+
+        // Check search by user name
+        $this->client->jsonRequest('GET', $this->baseUrl . '?search=Richard');
+        $entities = $this->getJsonResponseContent($this->client->getResponse(), 200);
+        $this->assertCount(1, $entities);
+
+        // Check search by user name filtered by User entity only
+        $this->client->jsonRequest(
+            'GET',
+            $this->baseUrl . '?search=Richard&from=Oro\Bundle\UserBundle\Entity\User'
+        );
+        $entities = $this->getJsonResponseContent($this->client->getResponse(), 200);
+        $this->assertCount(1, $entities);
+
+        // Check searching by non-existing user name. Should return no results.
+        $this->client->jsonRequest('GET', $this->baseUrl . sprintf('?search=%s&page=1', 'NonExistentEntityTitle'));
+        $entities = $this->getJsonResponseContent($this->client->getResponse(), 200);
+        $this->assertEmpty($entities);
+    }
+
+    public function testEmailSearchWithEmailFilter()
+    {
+        // Check search by email
+        $this->client->jsonRequest('GET', $this->baseUrl . '?email=richard_bradley@example.com');
+        $entities = $this->getJsonResponseContent($this->client->getResponse(), 200);
+        $this->assertCount(1, $entities);
+
+        // Check search by 2 emails
+        $emails = ['richard_bradley@example.com', 'brenda_brock@example.com'];
+        $this->client->jsonRequest('GET', $this->baseUrl . '?email=' . implode(',', $emails));
+        $entities = $this->getJsonResponseContent($this->client->getResponse(), 200);
+        $this->assertCount(2, $entities);
+
+        // Check search by email filtered by User entity only
+        $this->client->jsonRequest(
+            'GET',
+            $this->baseUrl . '?email=richard_bradley@example.com&from=Oro\Bundle\UserBundle\Entity\User'
+        );
+        $entities = $this->getJsonResponseContent($this->client->getResponse(), 200);
+        $this->assertCount(1, $entities);
+
+        // Check searching by non-existing email. Should return no results.
+        $this->client->jsonRequest(
+            'GET',
+            $this->baseUrl . sprintf('?email=%s&page=1', 'non_existent_email@example.com')
+        );
+        $entities = $this->getJsonResponseContent($this->client->getResponse(), 200);
+        $this->assertEmpty($entities);
+    }
+
+    public function testEmailSearchWithEmailAndSearchFilter()
+    {
+        $this->markTestSkipped('Due to BAP-8497');
+
+        // Check search by user name and email
+        $this->client->request(
+            'GET',
+            $this->baseUrl,
+            [
+                'search' => 'Richard',
+                'email'  => 'richard_bradley@example.com'
+            ]
+        );
+        $entities = $this->getJsonResponseContent($this->client->getResponse(), 200);
+        $this->assertCount(1, $entities);
+
+        // Check search by user name and another email
+        $this->client->request(
+            'GET',
+            $this->baseUrl,
+            [
+                'search' => 'Brenda',
+                'email'  => 'richard_bradley@example.com'
+            ]
+        );
+        $entities = $this->getJsonResponseContent($this->client->getResponse(), 200);
+        $this->assertEmpty($entities);
+    }
+
+    public function testEmailSearchWithPaging()
+    {
+        $this->client->jsonRequest(
+            'GET',
+            $this->baseUrl . '?page=2&limit=2',
+            [],
+            ['HTTP_X-Include' => 'totalCount']
+        );
+        $response = $this->client->getResponse();
+        $entities = $this->getJsonResponseContent($response, 200);
+        $this->assertCount(2, $entities);
+        $this->assertEquals(6, $response->headers->get('X-Include-Total-Count'));
+    }
+}
