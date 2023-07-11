e6aafe5985e1289098c548027efe5c411f9f4dc7
Yefim Yevtushenko <yyevtushenko@oroinc.com>
2023-07-11 17:26:02 +0300

    BB-22621: [Backend] Add validation mechanism to product kit line items on checkout (#35996)
    - added CheckoutAwareValidatorDecorator to enable ability to restrict checkout validator by steps
    - added ValidationGroupUtils to preprocess validation groups with group sequence and placeholders
    - added ValidationGroupSequenceExtension form type extension to make nested validation groups be treated as GroupSequence
    - added CheckoutValidationGroupsBySourceEntityProvider to provide validation groups checkout taking into account its source entity
    - removed DatagridLineItemsDataViolationsListener, switched checkout to DatagridLineItemsDataValidationListener instead
    - removed not used LineItemCollection constraint, validator, event and provider
    - added HasEnoughInventoryLevel, IsLowInventoryLevel, IsUpcoming constraint validators
    - made use of new constraints for checkout instead of outdated listeners
    - added ValidateCheckoutOnStartEventListener, ValidateCheckoutPreOrderCreateEventListener, ValidateCheckoutBeforeOrderCreateEventListener checkout listeners that control the checkout validation instead of outdated listeners
    - removed unused unused checkout event extendable_condition.checkout
    - added check for empty line items collection on place_order transition definition
    - improved validation configs for shopping list and checkout
    
    * BB-22621: [Backend] Add validation mechanism to product kit line items on checkout
    - fixed typo

---
+++ src/Oro/Bundle/FormBundle/Tests/Unit/Utils/ValidationGroupUtilsTest.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Oro\Bundle\FormBundle\Tests\Unit\Utils;
+
+use Oro\Bundle\FormBundle\Utils\ValidationGroupUtils;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Validator\Constraints\GroupSequence;
+
+class ValidationGroupUtilsTest extends TestCase
+{
+    /**
+     * @dataProvider resolveValidationGroupsDataProvider
+     */
+    public function testResolveValidationGroups(
+        array $validationGroups,
+        array $placeholders,
+        array $expected
+    ): void {
+        self::assertEquals(
+            $expected,
+            ValidationGroupUtils::resolveValidationGroups($validationGroups, $placeholders)
+        );
+    }
+
+    public function resolveValidationGroupsDataProvider(): iterable
+    {
+        yield 'empty' => [[], [], []];
+
+        yield 'not empty groups' => [['group1', 'group2'], [], ['group1', 'group2']];
+
+        yield 'not empty groups, without placeholders' => [
+            ['group1%placeholder%', 'group2'],
+            [],
+            ['group1%placeholder%', 'group2'],
+        ];
+
+        yield 'not empty groups with placeholder, with placeholders' => [
+            ['group1%placeholder%', 'group2'],
+            ['%placeholder%' => '_user'],
+            ['group1_user', 'group2'],
+        ];
+
+        yield 'not empty groups with group sequence, with placeholders' => [
+            [['group1%placeholder%', 'group2']],
+            ['%placeholder%' => '_user'],
+            [new GroupSequence(['group1_user', 'group2'])],
+        ];
+    }
+}
