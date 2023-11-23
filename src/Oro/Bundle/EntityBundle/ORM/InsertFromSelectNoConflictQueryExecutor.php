5697d1065cbd19d56c942cc8f6d0fd602a85f79b
Eugene Zibrov <106163563+eugene-zibrov@users.noreply.github.com>
2023-11-23 17:23:39 +0200

    BB-23016: Category visibility calculation may create duplicate records resulting in potential errors (#36935)
    - added no conflict query executor to avoid unique constraint violation in parallel insertions when running two or more consumers
    - changed type in CategoryRepository::insertStaticValues to InsertQueryExecutorInterface
    - modified CategoryResolvedCacheBuilder to use CategoryVisibility scope

---
+++ src/Oro/Bundle/EntityBundle/ORM/InsertFromSelectNoConflictQueryExecutor.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Oro\Bundle\EntityBundle\ORM;
+
+use Doctrine\ORM\QueryBuilder;
+
+/**
+ * Compiles and executes "insert from select ... on conflict(field) do nothing" query
+ */
+class InsertFromSelectNoConflictQueryExecutor extends InsertFromSelectQueryExecutor
+{
+    private array $onConflictIgnoredFields = [];
+
+    public function __construct(
+        private NativeQueryExecutorHelper $helper
+    ) {
+        parent::__construct($helper);
+    }
+
+    public function setOnConflictIgnoredFields(array $onConflictIgnoredFields): void
+    {
+        $this->onConflictIgnoredFields = $onConflictIgnoredFields;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function execute(string $className, array $fields, QueryBuilder $selectQueryBuilder): int
+    {
+        $insertToTableName = $this->helper->getTableName($className);
+        $columns = $this->helper->getColumns($className, $fields);
+        $selectQuery = $selectQueryBuilder->getQuery();
+
+        $ignoredColumns = $this->onConflictIgnoredFields ?
+            $this->helper->getColumns($className, $this->onConflictIgnoredFields) : [];
+
+        $onConflictSqlPart = $ignoredColumns ?
+            sprintf('ON CONFLICT(%s) DO NOTHING', implode(', ', $ignoredColumns)) : '';
+
+        $sql = sprintf(
+            'INSERT INTO %s (%s) %s %s',
+            $insertToTableName,
+            implode(', ', $columns),
+            $selectQuery->getSQL(),
+            $onConflictSqlPart
+        );
+        list($params, $types) = $this->helper->processParameterMappings($selectQuery);
+        // No possibility to use createNativeQuery with rsm http://www.doctrine-project.org/jira/browse/DDC-962
+        return $this->helper->getManager($className)->getConnection()->executeStatement($sql, $params, $types);
+    }
+}
