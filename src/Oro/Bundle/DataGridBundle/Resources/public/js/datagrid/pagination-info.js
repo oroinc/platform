2d306b6022b873d1513733c0e3301519df8e76c9
Vadym Balan <144042893+vadymbalanoroinc@users.noreply.github.com>
2024-01-25 15:07:53 +0100

    BB-23414: Update toolbars of datagrid (#37364)
    * BB-23414: Update toolbars of datagrid

---
+++ src/Oro/Bundle/DataGridBundle/Resources/public/js/datagrid/pagination-info.js
@@ -0,0 +1,33 @@
+import BaseView from 'oroui/js/app/views/base/view';
+import template from 'tpl-loader!orodatagrid/templates/datagrid/pagination-info.html';
+
+const PaginationInfoView = BaseView.extend({
+    optionNames: BaseView.prototype.optionNames.concat(['collection']),
+
+    autoRender: true,
+
+    template,
+
+    listen: {
+        'add collection': 'render',
+        'remove collection': 'render',
+        'reset collection': 'render'
+    },
+
+    constructor: function PaginationInfoView(...args) {
+        PaginationInfoView.__super__.constructor.apply(this, args);
+    },
+
+    getTemplateData() {
+        const {pageSize, totalRecords, currentPage} = this.collection.state;
+        return {
+            minRangeThreshold: (currentPage - 1) * pageSize + 1,
+            maxRangeThreshold: currentPage * pageSize > totalRecords
+                ? totalRecords
+                : currentPage * pageSize,
+            totalRecords
+        };
+    }
+});
+
+export default PaginationInfoView;
