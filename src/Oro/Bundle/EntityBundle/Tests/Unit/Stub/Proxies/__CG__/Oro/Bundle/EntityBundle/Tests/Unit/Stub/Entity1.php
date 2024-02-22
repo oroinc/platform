6ee3c643a6c29b503686fb4961a41a3b8cf5de32
Vova Soroka <vsoroka@oroinc.com>
2024-02-22 22:26:06 +0200

    BAP-22467: Warm up validation metadata cache for entities (#37641)

---
+++ src/Oro/Bundle/EntityBundle/Tests/Unit/Stub/Proxies/__CG__/Oro/Bundle/EntityBundle/Tests/Unit/Stub/Entity1.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Oro\Bundle\EntityBundle\Tests\Unit\Stub\Proxies\__CG__\Oro\Bundle\EntityBundle\Tests\Unit\Stub;
+
+use Oro\Bundle\EntityBundle\Tests\Unit\Stub\Entity1 as Entity;
+
+class Entity1 extends Entity
+{
+}
