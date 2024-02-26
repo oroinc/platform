e1b5fc130ed80213846b8c9112263a941f97e0fc
Maksym Maksymenko <fubolg.ua@gmail.com>
2024-02-26 16:14:57 +0200

    BAP-22330: EntityExtendUpdateProcessor should not turn maintenance mode (#37216)
    ---------
    
    Co-authored-by: Maksym Maksymenko <mmaksymenko@oroinc.com>

--- src/Oro/Bundle/EntityExtendBundle/Tests/Unit/Extend/EntityExtendUpdateHandlerTest.php
+++ src/Oro/Bundle/EntityExtendBundle/Tests/Unit/Extend/EntityExtendUpdateHandlerTest.php
@@ -5,24 +5,38 @@ namespace Oro\Bundle\EntityExtendBundle\Tests\Unit\Extend;
 use Oro\Bundle\EntityExtendBundle\Extend\EntityExtendUpdateHandler;
 use Oro\Bundle\EntityExtendBundle\Extend\EntityExtendUpdateProcessor;
 use Oro\Bundle\EntityExtendBundle\Extend\EntityExtendUpdateProcessorResult;
+use Oro\Bundle\MaintenanceBundle\Maintenance\Mode as MaintenanceMode;
 
 class EntityExtendUpdateHandlerTest extends \PHPUnit\Framework\TestCase
 {
-    /** @var EntityExtendUpdateProcessor|\PHPUnit\Framework\MockObject\MockObject */
+    /**
+     * @var EntityExtendUpdateProcessor|\PHPUnit\Framework\MockObject\MockObject
+     */
     private $entityExtendUpdateProcessor;
 
-    /** @var EntityExtendUpdateHandler */
+    /**
+     * @var MaintenanceMode|\PHPUnit\Framework\MockObject\MockObject
+     */
+    private $maintenance;
+
+    /**
+     * @var EntityExtendUpdateHandler
+     */
     private $handler;
 
     protected function setUp(): void
     {
         $this->entityExtendUpdateProcessor = $this->createMock(EntityExtendUpdateProcessor::class);
+        $this->maintenance = $this->createMock(MaintenanceMode::class);
 
-        $this->handler = new EntityExtendUpdateHandler($this->entityExtendUpdateProcessor);
+        $this->handler = new EntityExtendUpdateHandler($this->entityExtendUpdateProcessor, $this->maintenance);
     }
 
     public function testUpdate()
     {
+        $this->maintenance->expects(self::once())
+            ->method('activate');
+
         $this->entityExtendUpdateProcessor->expects(self::once())
             ->method('processUpdate')
             ->willReturn(new EntityExtendUpdateProcessorResult(true));
@@ -34,6 +48,9 @@ class EntityExtendUpdateHandlerTest extends \PHPUnit\Framework\TestCase
 
     public function testUpdateWhenUpdateFailed()
     {
+        $this->maintenance->expects(self::once())
+            ->method('activate');
+
         $this->entityExtendUpdateProcessor->expects(self::once())
             ->method('processUpdate')
             ->willReturn(new EntityExtendUpdateProcessorResult(false));
