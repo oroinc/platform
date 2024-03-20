fdf4b30cd1a477c69c88647ed394f65f59534579
yevhenii101991 <146938608+yevhenii101991@users.noreply.github.com>
2024-03-20 13:44:57 +0200

    BAP-22122: Perfomance issue due opcache and php files larger than 2Mb (#37683)
    - updated logic for cache data during install
    - set auto generator class for consume command in test env
    
    ---------
    
    Co-authored-by: Yevhenii Zhadan <yzhadan@oroinc.com>

--- src/Oro/Bundle/PlatformBundle/Configurator/OrmConfigurationConfigurator.php
+++ src/Oro/Bundle/PlatformBundle/Configurator/OrmConfigurationConfigurator.php
@@ -4,19 +4,22 @@ namespace Oro\Bundle\PlatformBundle\Configurator;
 
 use Doctrine\ORM\Configuration;
 use Oro\Bundle\DistributionBundle\Handler\ApplicationState;
+use Oro\Bundle\InstallerBundle\CommandExecutor;
 
 /**
  * Changes Configuration->autoGenerateProxyClasses to true if application isn't installed
  */
 class OrmConfigurationConfigurator
 {
-    public function __construct(private ApplicationState $applicationState)
+    public function __construct(private ApplicationState $applicationState, private string $env)
     {
     }
 
     public function configure(Configuration $configuration): void
     {
-        if (!$this->applicationState->isInstalled()) {
+        if (!$this->applicationState->isInstalled()
+            || (CommandExecutor::isCurrentCommand('oro:message-queue:consume') && $this->env === 'test')
+        ) {
             $configuration->setAutoGenerateProxyClasses(true);
         }
     }
