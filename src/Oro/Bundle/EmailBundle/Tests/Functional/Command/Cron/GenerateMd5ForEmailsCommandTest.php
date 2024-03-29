d48fa3ae9a81af84e243bfebd3b4df9f22bfcd9d
oro-panasiukr <39378223+oro-panasiukr@users.noreply.github.com>
2024-03-29 15:17:57 +0100

    BB-23812: Update of localized email template does not change its MD5 hash value (#37894)

---
+++ src/Oro/Bundle/EmailBundle/Tests/Functional/Command/Cron/GenerateMd5ForEmailsCommandTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Functional\Command\Cron;
+
+use Oro\Bundle\ConfigBundle\Tests\Functional\Traits\ConfigManagerAwareTestTrait;
+use Oro\Bundle\EmailBundle\Tests\Functional\DataFixtures\LoadEmailTemplateWithTranslationsData;
+use Oro\Bundle\TestFrameworkBundle\Test\WebTestCase;
+use Oro\Component\Testing\Command\CommandTestingTrait;
+
+class GenerateMd5ForEmailsCommandTest extends WebTestCase
+{
+    use ConfigManagerAwareTestTrait;
+    use CommandTestingTrait;
+
+    protected function setUp(): void
+    {
+        $this->initClient();
+        $this->loadFixtures([LoadEmailTemplateWithTranslationsData::class]);
+    }
+
+    public function testExecute(): void
+    {
+        $commandTester = $this->doExecuteCommand('oro:email:generate-md5');
+
+        $this->assertSuccessReturnCode($commandTester);
+        $this->assertOutputContains($commandTester, 'default_template:fd59a055a31bf807723b55396d1b1398');
+    }
+}
