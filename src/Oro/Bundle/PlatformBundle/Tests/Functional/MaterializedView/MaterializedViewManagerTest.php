9c9edf740ea6a2ba178e72f8143164ca9695acf1
Viacheslav Dubrovskyi <vdubrovskyi@oroinc.com>
2024-04-09 17:48:30 +0300

    OPI-654 Update Components of the CI and OroCloud environment to the latest minor versions (#37859)
    * OPI-654 Update Components of the CI and OroCloud environment to the latest minor versions
    - update version for base images, PG, RMQ
    * Update version
    * OPI-1307 Fix MaterializedViewManagerTest.
    * Add prefer-offline for npm
    * Add COMPOSER_RUNTIME_ENV=virtualbox
    * Skip install assets for behat_wiring
    
    ---------
    
    Co-authored-by: Zsombor Boldizsar-Zeik <zsombor.boldizsar.zeik@oroinc.com>
    Co-authored-by: Julia Khrysieva <ikhrysieva@oroinc.com>

--- src/Oro/Bundle/PlatformBundle/Tests/Functional/MaterializedView/MaterializedViewManagerTest.php
+++ src/Oro/Bundle/PlatformBundle/Tests/Functional/MaterializedView/MaterializedViewManagerTest.php
@@ -43,8 +43,13 @@ class MaterializedViewManagerTest extends WebTestCase
 
         $materializedViewInfo = self::getMaterializedViewInfo(self::getContainer(), $materializedViewName);
         self::assertNotNull($materializedViewInfo);
+        $expectedSql = mb_strtolower(QueryUtil::getExecutableSql($query));
+        // strip table alias, because postgresql 16 does this as well when inserting the materialized view,
+        // if only one table is used, to simplify the query
+        $expectedSql = str_replace('o0_.', '', $expectedSql);
+
         self::assertEquals(
-            mb_strtolower(QueryUtil::getExecutableSql($query)),
+            $expectedSql,
             mb_strtolower(preg_replace('/\s+/', ' ', trim($materializedViewInfo['definition'], ' ;')))
         );
         self::assertTrue($materializedViewInfo['ispopulated']);
