eb0c07f4e7d96738122c2c601d888ee6794d35eb
Oleksandr Protsiuk <44535554+OleksandrProtsiuk@users.noreply.github.com>
2024-04-18 13:01:28 +0300

    BAP-22561: Failed migration ChangeTranslationKeyUniqueIndex while platform update command is running (#38129)
    - Added checkup before deletion of the old index to avoid throwing an exception.

--- src/Oro/Bundle/TranslationBundle/Migrations/Schema/v1_7/ChangeTranslationKeyUniqueIndex.php
+++ src/Oro/Bundle/TranslationBundle/Migrations/Schema/v1_7/ChangeTranslationKeyUniqueIndex.php
@@ -14,7 +14,9 @@ class ChangeTranslationKeyUniqueIndex implements Migration
     public function up(Schema $schema, QueryBag $queries): void
     {
         $table = $schema->getTable('oro_translation_key');
-        $table->dropIndex('key_domain_uniq');
+        if ($table->hasIndex('key_domain_uniq')) {
+            $table->dropIndex('key_domain_uniq');
+        }
         $table->addUniqueIndex(['domain', 'key'], 'oro_translation_key_uidx');
     }
 }
