9ef4c4e62fc20db2182977f936d0223dba0fa5e4
yevhenii101991 <146938608+yevhenii101991@users.noreply.github.com>
2024-05-01 15:03:39 +0300

    BAP-22504: User agent strings longer than 255 chars cause error on login_attempt save (#38184)

---
+++ src/Oro/Bundle/UserBundle/Migrations/Schema/v2_11_1/UpdateUserAgentLength.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Oro\Bundle\UserBundle\Migrations\Schema\v2_11_1;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\DBAL\Types\Type;
+use Doctrine\DBAL\Types\Types;
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+
+/**
+ * Updates the length of user agent, as it could be more than 255 symbols
+ */
+class UpdateUserAgentLength implements Migration
+{
+    public function up(Schema $schema, QueryBag $queries): void
+    {
+        $table = $schema->getTable('oro_user_login');
+        $column =  $table->getColumn('user_agent');
+        // Column type already changed
+        if ($column->getType()->getName() === Types::TEXT) {
+            return;
+        }
+
+        //use text field cause user-agent has no restrictions for length
+        $table->changeColumn('user_agent', ['type' => Type::getType(Types::TEXT), 'notnull' => false, 'default' => '']);
+    }
+}
