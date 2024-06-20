2d4d352ac2c05e77829cf1019a10058469df9815
Oleksandr Protsiuk <44535554+OleksandrProtsiuk@users.noreply.github.com>
2024-06-20 15:49:54 +0300

    BAP-22655: Do not require a writable public/media folder (#38747)
    - public/media requirement check moved to PublicMediaDirectoryRequirementsProvider
    - Added MongoDB requirements provider
    - Added config to override PublicMediaDirectoryRequirementsProvider with MongoDb provider when GridFSConfigBundle is used

---
+++ src/Oro/Bundle/InstallerBundle/Tests/Unit/Provider/PublicMediaDirectoryRequirementsProviderTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Oro\Bundle\InstallerBundle\Tests\Unit\Provider;
+
+use Oro\Bundle\InstallerBundle\Provider\PublicMediaDirectoryRequirementsProvider;
+use Symfony\Requirements\RequirementCollection;
+
+class PublicMediaDirectoryRequirementsProviderTest extends \PHPUnit\Framework\TestCase
+{
+    private string $mediaDirectory;
+    private PublicMediaDirectoryRequirementsProvider $provider;
+
+    protected function setUp(): void
+    {
+        $projectDirectory = sys_get_temp_dir();
+        $this->mediaDirectory = '/public/media';
+
+        $this->provider = new PublicMediaDirectoryRequirementsProvider($projectDirectory);
+
+        $this->tempDir = sys_get_temp_dir() . $this->mediaDirectory;
+        mkdir($this->tempDir, 0777, true);
+    }
+
+    protected function tearDown(): void
+    {
+        chmod($this->tempDir, 0777);
+        rmdir($this->tempDir);
+        rmdir(sys_get_temp_dir() . '/public');
+    }
+
+    public function testGetOroRequirements(): void
+    {
+        $collection = $this->provider->getOroRequirements();
+
+        $this->assertInstanceOf(RequirementCollection::class, $collection);
+
+        $requirements = $collection->all();
+        $this->assertNotEmpty($requirements);
+        $this->assertStringContainsString(
+            'public/media directory must be writable',
+            $requirements[0]->getTestMessage()
+        );
+    }
+
+    public function testAddPathWritableRequirementIsFulfilled(): void
+    {
+        $reflection = new \ReflectionClass($this->provider);
+        $method = $reflection->getMethod('addPathWritableRequirement');
+
+        $collection = new RequirementCollection();
+        $method->invoke($this->provider, $collection, $this->mediaDirectory);
+
+        $requirements = $collection->all();
+        $this->assertNotEmpty($requirements);
+        $this->assertTrue($requirements[0]->isFulfilled());
+    }
+
+    public function testAddPathWritableRequirementIsNotFulfilled(): void
+    {
+        // Change permissions to Read-Only
+        chmod($this->tempDir, 0555);
+
+        $reflection = new \ReflectionClass($this->provider);
+        $method = $reflection->getMethod('addPathWritableRequirement');
+
+        $collection = new RequirementCollection();
+        $method->invoke($this->provider, $collection, $this->mediaDirectory);
+
+        $requirements = $collection->all();
+        $this->assertNotEmpty($requirements);
+        $this->assertFalse($requirements[0]->isFulfilled());
+    }
+}
