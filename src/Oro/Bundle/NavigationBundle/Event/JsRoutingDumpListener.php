bfea52fe5b27740794f5cb094d5a572b755e8ca8
oro-panasiukr <39378223+oro-panasiukr@users.noreply.github.com>
2024-07-16 15:54:07 +0200

    BAP-22667: Error on JS routes build during upgrade (#38855)

--- src/Oro/Bundle/NavigationBundle/Event/JsRoutingDumpListener.php
+++ src/Oro/Bundle/NavigationBundle/Event/JsRoutingDumpListener.php
@@ -3,7 +3,9 @@
 namespace Oro\Bundle\NavigationBundle\Event;
 
 use Oro\Bundle\UIBundle\Asset\DynamicAssetVersionManager;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Event\ConsoleCommandEvent;
+use Symfony\Component\Console\Event\ConsoleTerminateEvent;
 
 /**
  * Renews the "routing" assets version before running the "fos:js-routing:dump" command.
@@ -17,9 +19,21 @@ class JsRoutingDumpListener
         $this->assetVersionManager = $assetVersionManager;
     }
 
+    /**
+     * @deprecated use {@see onConsoleTerminate} instead.
+     */
     public function onConsoleCommand(ConsoleCommandEvent $event): void
     {
-        $command = $event->getCommand();
+        $this->updateAssetVersion($event->getCommand());
+    }
+
+    public function onConsoleTerminate(ConsoleTerminateEvent $event): void
+    {
+        $this->updateAssetVersion($event->getCommand());
+    }
+
+    private function updateAssetVersion(?Command $command): void
+    {
         if (null === $command || 'fos:js-routing:dump' !== $command->getName()) {
             return;
         }
