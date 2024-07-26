18465ff94229bd443966c0eb35c13d54deb543e1
Valerii Yustyniuk <ValeriyYustunyk@users.noreply.github.com>
2024-07-26 07:16:03 -0500

    BB-24082: Improved SVG icons (#38347)
    * BB-24086: Create a new list of available icons for frontend menus (#38334)
    
    * BB-24084: Render separate SVG sprite for RTL mode (#38332)
    
    * BB-24084: Render separate SVG sprite for RTL mode
    - improve sprite generator
    - update svg sprite rtl usage
    
    * BB-24085: Highlight SVG icon if it is not avaliable (#38340)
    
    * BB-24085: Highlight SVG icon if it is not avaliable
    - generate svg list json
    - try passing assetHelper inside layout extension class
    - comment assetHelper param
    - get svg list for frontend sprite macros
    - remove php changes
    - validate svg sprite ids
    - check if svg icon has not found class
    - add debug option
    - improve svg highlighting solution and fix comments
    - check is theme sprite url exists
    - updated documentation
    
    * BB-24082: Improved SVG icons
    - updated documentation
    
    * Apply suggestions from code review
    
    * BB-24082: Improved SVG icons
    * Updated dev.locks
    
    * BB-24144: [Backend] Separate list of available icons for frontend menus (#38700)
    
    - added icons.yml theme config via IconsThemeConfigurationExtension layout theme config extension
    - added StorefrontIconsMappingProvider
    - added StorefrontIconType form type, made use of it in MenuUpdate form via MenuUpdateIconsExtension
    - added MenuIconsBuilder menu builder to replace icon with corresponding storefront icon
    
    * BB-23513: Get rid of FA prefix for datagrid actions at frontstore (#38733)
    
    - added FrontendMassActionDatagridExtension that adds frontend flag to mass actions in storefront datagrids
    - removed fa prefix in AbstractMassAction for frontend mass actions
    - removed unnecessary workarounds in action-component-dropdown-launcher.html, action-launcher.html
    - removed unneeded config_svg_icon.yml
    
    * BB-24126: Issues with SVG sprite rendering in Refreshing Teal theme (#38787)
    
    - added rendering svg icons for frontend menu
    - reverted back icons in unused menu
    
    * BB-23693: [Backend] Separate file.html.twig (#38766)
    
    - added file_icons configuration option to icons section of theme configuration;
    - added FilesTemplateProvider and ImagesTemplateProvider to get template for rendering view html block;
    - added FrontendBundle\FileIconProvider that used separate mapping file to provide file type icon for storefront;
    - added caching to storefront FileIconProvider;
    - used file_icons option value for current theme in the FileIconProvider
    
    * Updated dev.locks
    ---------
    
    Co-authored-by: Vadym Balan <144042893+vadymbalanoroinc@users.noreply.github.com>
    Co-authored-by: Kateryna Isakova <kisakova@users.noreply.github.com>
    Co-authored-by: orocrmdeployer <orocrmdeployer@oroinc.com>
    Co-authored-by: Yefim Yevtushenko <yyevtushenko@oroinc.com>
    Co-authored-by: Chetverikov Yevhen <103036822+chetverikovyevhen@users.noreply.github.com>

---
+++ src/Oro/Bundle/AttachmentBundle/Tests/Unit/Provider/FilesTemplateProviderTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Oro\Bundle\AttachmentBundle\Tests\Unit\Provider;
+
+use Oro\Bundle\AttachmentBundle\Provider\FilesTemplateProvider;
+use PHPUnit\Framework\TestCase;
+
+class FilesTemplateProviderTest extends TestCase
+{
+    private FilesTemplateProvider $provider;
+
+    protected function setUp(): void
+    {
+        $this->provider = new FilesTemplateProvider();
+    }
+
+    public function testGetTemplate(): void
+    {
+        self::assertEquals('@OroAttachment/Twig/file.html.twig', $this->provider->getTemplate());
+    }
+
+    public function testSetTemplate(): void
+    {
+        $template = '@ACMEAttachment/Twig/file.html.twig';
+
+        $this->provider->setTemplate($template);
+
+        self::assertEquals($template, $this->provider->getTemplate());
+    }
+}
