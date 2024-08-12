91aae671c764c052cc16d27cd6985e063b9224d8
oro-panasiukr <39378223+oro-panasiukr@users.noreply.github.com>
2024-08-12 14:13:10 +0200

    BB-24399: Uploading translation files produces (#39047)

--- src/Oro/Bundle/TranslationBundle/ImportExport/Writer/TranslationWriter.php
+++ src/Oro/Bundle/TranslationBundle/ImportExport/Writer/TranslationWriter.php
@@ -6,23 +6,28 @@ use Doctrine\ORM\EntityManager;
 use Doctrine\Persistence\ManagerRegistry;
 use Oro\Bundle\BatchBundle\Item\ItemWriterInterface;
 use Oro\Bundle\TranslationBundle\Entity\Translation;
+use Oro\Bundle\TranslationBundle\Event\InvalidateDynamicTranslationCacheEvent;
+use Oro\Bundle\TranslationBundle\EventListener\InvalidateDynamicJsTranslationListener;
 use Oro\Bundle\TranslationBundle\Manager\TranslationManager;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 
 /**
  * Batch job's writer.
  */
 class TranslationWriter implements ItemWriterInterface
 {
-    /** @var ManagerRegistry */
-    protected $registry;
+    public function __construct(
+        protected ManagerRegistry $registry,
+        protected TranslationManager $translationManager,
+        protected EventDispatcher $eventDispatcher
+    ) {
+    }
 
-    /** @var TranslationManager */
-    protected $translationManager;
-
-    public function __construct(ManagerRegistry $registry, TranslationManager $translationManager)
+    public function setEventDispatcher(EventDispatcher $eventDispatcher): self
     {
-        $this->registry = $registry;
-        $this->translationManager = $translationManager;
+        $this->eventDispatcher = $eventDispatcher;
+
+        return $this;
     }
 
     /**
@@ -51,7 +56,21 @@ class TranslationWriter implements ItemWriterInterface
                 );
             }
 
-            $this->translationManager->flush(true);
+            /**
+             * Disable translation dumping.
+             *
+             * Since the cache is updated and translations are dumped for each batch of imports, this leads to
+             * significant resource consumption.
+             * Also, parallel recording to GridFS has its limitations on simultaneous recording,
+             * so it is better to record one file after all translations have been updated.
+             * (see @Oro\Bundle\TranslationBundle\EventListener\FinishImportListener::onFinishImport).
+             *
+             * Please note that disabling the listener must be global to avoid a situation where the cache is cleared
+             * after one of the parties is finished.
+             * (see @Oro\Bundle\TranslationBundle\Translation\DynamicTranslationCache)
+             */
+            $this->removeListener();
+            $this->translationManager->flushWithoutDumpJsTranslations(true);
 
             $em->commit();
             $em->clear();
@@ -62,6 +81,15 @@ class TranslationWriter implements ItemWriterInterface
             }
 
             throw $exception;
+        }
+    }
+
+    private function removeListener(): void
+    {
+        foreach ($this->eventDispatcher->getListeners(InvalidateDynamicTranslationCacheEvent::NAME) as $listener) {
+            if ($listener[0] instanceof InvalidateDynamicJsTranslationListener) {
+                $this->eventDispatcher->removeListener(InvalidateDynamicTranslationCacheEvent::NAME, $listener);
+            }
         }
     }
 }
