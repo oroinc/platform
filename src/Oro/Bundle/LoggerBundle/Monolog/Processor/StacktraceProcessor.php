120285dc7d9ba32bf17a0206044fcf94ace65f59
Vova Soroka <vsoroka@oroinc.com>
2024-09-02 14:14:53 +0300

    BAP-21948: Implement full stacktrace for Doctrine\ORM\NonUniqueResultException (#39240)

---
+++ src/Oro/Bundle/LoggerBundle/Monolog/Processor/StacktraceProcessor.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Oro\Bundle\LoggerBundle\Monolog\Processor;
+
+use Monolog\Logger;
+use Monolog\Processor\ProcessorInterface;
+
+/**
+ * Adds an exception stacktrace into records.
+ */
+class StacktraceProcessor implements ProcessorInterface
+{
+    private int $level;
+
+    public function __construct(?string $level)
+    {
+        $this->level = $level && $level !== 'none'
+            ? Logger::toMonologLevel($level)
+            : PHP_INT_MAX;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function __invoke(array $record): array
+    {
+        if (isset($record['context']['exception']) && $record['level'] >= $this->level) {
+            $exception = $record['context']['exception'];
+            if ($exception instanceof \Throwable) {
+                $record['context']['stacktrace'] = $exception->getTraceAsString();
+            }
+        }
+
+        return $record;
+    }
+}
