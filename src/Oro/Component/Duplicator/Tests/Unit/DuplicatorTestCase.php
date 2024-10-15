11dcec7801d2e0d14d2e6df9a7b9a2f736b8450c
Vova Soroka <vsoroka@oroinc.com>
2024-10-15 17:35:44 +0300

    BB-24650: Allow the /admin/api/orderlineitems API resource to be filtered by createdAt/updatedAt fields (#39506)

---
+++ src/Oro/Component/Duplicator/Tests/Unit/DuplicatorTestCase.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Oro\Component\Duplicator\Tests\Unit;
+
+use DeepCopy\Filter\Doctrine\DoctrineCollectionFilter;
+use DeepCopy\Filter\Doctrine\DoctrineEmptyCollectionFilter;
+use DeepCopy\Filter\KeepFilter;
+use DeepCopy\Filter\SetNullFilter;
+use DeepCopy\Matcher\PropertyMatcher;
+use DeepCopy\Matcher\PropertyNameMatcher;
+use DeepCopy\Matcher\PropertyTypeMatcher;
+use Oro\Component\Duplicator\DuplicatorFactory;
+use Oro\Component\Duplicator\Filter\FilterFactory;
+use Oro\Component\Duplicator\Filter\ReplaceValueFilter;
+use Oro\Component\Duplicator\Filter\ShallowCopyFilter;
+use Oro\Component\Duplicator\Matcher\MatcherFactory;
+use Oro\Component\Duplicator\ObjectType;
+
+abstract class DuplicatorTestCase extends \PHPUnit\Framework\TestCase
+{
+    protected function createMatcherFactory(): MatcherFactory
+    {
+        $factory = new MatcherFactory();
+        $factory->addObjectType(new ObjectType('property', PropertyMatcher::class));
+        $factory->addObjectType(new ObjectType('propertyName', PropertyNameMatcher::class));
+        $factory->addObjectType(new ObjectType('propertyType', PropertyTypeMatcher::class));
+
+        return $factory;
+    }
+
+    protected function createFilterFactory(): FilterFactory
+    {
+        $factory = new FilterFactory();
+        $factory->addObjectType(new ObjectType('setNull', SetNullFilter::class));
+        $factory->addObjectType(new ObjectType('keep', KeepFilter::class));
+        $factory->addObjectType(new ObjectType('collection', DoctrineCollectionFilter::class));
+        $factory->addObjectType(new ObjectType('emptyCollection', DoctrineEmptyCollectionFilter::class));
+        $factory->addObjectType(new ObjectType('replaceValue', ReplaceValueFilter::class));
+        $factory->addObjectType(new ObjectType('shallowCopy', ShallowCopyFilter::class));
+
+        return $factory;
+    }
+
+    protected function createDuplicatorFactory(): DuplicatorFactory
+    {
+        return new DuplicatorFactory($this->createMatcherFactory(), $this->createFilterFactory());
+    }
+}
