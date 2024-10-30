e56636264c1c289d784481f161f4921548cbf0e3
yevhenii101991 <146938608+yevhenii101991@users.noreply.github.com>
2024-10-30 09:49:47 +0200

    BAP-22747: "Unrecognized option title_field" error when editing entity fields after upgrade from versions older than 5.0.1 (#39561)

---
+++ src/Oro/Bundle/EntityConfigBundle/Tests/Unit/Migration/RemoveTitleFieldOptionTest.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Oro\Bundle\EntityConfigBundle\Tests\Unit\Migration;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Oro\Bundle\EntityConfigBundle\Migrations\Schema\v1_18\RemoveTitleFieldOption;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerInterface;
+
+class RemoveTitleFieldOptionTest extends TestCase
+{
+    private LoggerInterface|MockObject $logger;
+
+    private Connection|MockObject $connector;
+
+    #[\Override]
+    protected function setUp(): void
+    {
+        $this->connector = $this->createMock(Connection::class);
+        $this->logger = $this->createMock(LoggerInterface::class);
+    }
+
+    /**
+     * @dataProvider configFieldDataDataProvider
+     */
+    public function testExecute($phpData, $expectedUpdateCalls)
+    {
+        $dbPlatform = $this->createMock(AbstractPlatform::class);
+        $migration = new RemoveTitleFieldOption();
+        $migration->setConnection($this->connector);
+
+        $dbData = base64_encode(serialize($phpData));
+        $this->connector->expects(self::once())
+            ->method('fetchAllAssociative')
+            ->willReturn([['id' => 1, 'data' => $dbData]]);
+
+        $this->connector->expects(self::once())
+            ->method('convertToPHPValue')
+            ->with($dbData, 'array')
+            ->willReturn($phpData);
+        $this->connector->expects(self::any())
+            ->method('getDatabasePlatform')
+            ->willReturn($dbPlatform);
+
+        $expectedData = [
+            'extend' => [
+                'owner' => 'Custom',
+                'state' => 'Active'
+            ],
+            'search' => [
+                'immutable' => 1,
+                'searchable' => 1,
+            ]
+        ];
+
+        $this->connector->expects(self::exactly($expectedUpdateCalls))
+            ->method('executeStatement')
+            ->with(
+                'UPDATE oro_entity_config_field SET data = :data WHERE id = :id',
+                ['data' => $expectedData, 'id' => 1],
+                ['id' => 'integer', 'data' => 'array']
+            )
+            ->willReturn(1);
+        $migration->execute($this->logger);
+    }
+
+    public function configFieldDataDataProvider()
+    {
+        $dataWithTitleField = [
+            'extend' => [
+                'owner' => 'Custom',
+                'state' => 'Active'
+            ],
+            'search' => [
+                'immutable' => 1,
+                'searchable' => 1,
+                'title_field' => 1,
+            ]
+        ];
+
+        $dataWithoutTitleField = [
+            'extend' => [
+                'owner' => 'Custom',
+                'state' => 'Active'
+            ],
+            'search' => [
+                'immutable' => 1,
+                'searchable' => 1,
+            ]
+        ];
+
+        return [
+            [$dataWithTitleField, 1],
+            [$dataWithoutTitleField, 0]
+        ];
+    }
+}
