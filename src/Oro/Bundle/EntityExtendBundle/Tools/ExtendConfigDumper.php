e477152ea641da2c181458a2fb083b74106cee80
timofiyprisyazhnyuk <33844707+timofiyprisyazhnyuk@users.noreply.github.com>
2024-11-20 10:48:31 +0200

    BAP-22815 Product attribute getter not working in some circumstances (#39805)

--- src/Oro/Bundle/EntityExtendBundle/Tools/ExtendConfigDumper.php
+++ src/Oro/Bundle/EntityExtendBundle/Tools/ExtendConfigDumper.php
@@ -412,6 +412,7 @@ class ExtendConfigDumper
         $relationProperties = isset($schema['relation']) && null !== $filter ? $schema['relation'] : [];
         $defaultProperties  = isset($schema['default']) && null !== $filter ? $schema['default'] : [];
         $addRemoveMethods   = isset($schema['addremove']) && null !== $filter ? $schema['addremove'] : [];
+        $attribute          = $schema['attribute'] ?? null;
 
         $fieldConfigs = null === $filter
             ? $configProvider->getConfigs($className, true)
@@ -480,6 +481,9 @@ class ExtendConfigDumper
             'addremove' => $addRemoveMethods,
             'doctrine'  => $doctrine,
         ];
+        if (null !== $attribute) {
+            $schema['attribute'] = $attribute;
+        }
 
         if ($type === 'Extend') {
             $parentClassName = class_exists($className) ? get_parent_class($className) : false;
