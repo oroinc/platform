27c6f9e2de7e918f5f35cc570e4539411e5a2393
Vova Soroka <vsoroka@oroinc.com>
2025-03-13 19:29:14 +0200

    BAP-22964: Replace Symfony\Bridge\Doctrine\ManagerRegistry with Doctrine\Persistence\ManagerRegistry (#40731)

--- src/Oro/Bundle/EntityExtendBundle/Tests/Unit/EventListener/Cache/EnumOptionListenerTest.php
+++ src/Oro/Bundle/EntityExtendBundle/Tests/Unit/EventListener/Cache/EnumOptionListenerTest.php
@@ -2,93 +2,132 @@
 
 namespace Oro\Bundle\EntityExtendBundle\Tests\Unit\EventListener\Cache;
 
-use Oro\Bundle\EntityExtendBundle\Entity\EnumOptionTranslation;
+use Doctrine\ORM\EntityManager;
+use Doctrine\Persistence\ManagerRegistry;
+use Oro\Bundle\EntityConfigBundle\Translation\ConfigTranslationHelper;
+use Oro\Bundle\EntityExtendBundle\Cache\EnumTranslationCache;
 use Oro\Bundle\EntityExtendBundle\EventListener\Cache\EnumOptionListener;
 use Oro\Bundle\EntityExtendBundle\Test\EntityExtendTestInitializer;
 use Oro\Bundle\EntityExtendBundle\Tests\Unit\Fixtures\TestEnumValue;
-use Oro\Component\Testing\Unit\EntityTrait;
+use Oro\Bundle\TranslationBundle\Entity\Repository\TranslationRepository;
+use Oro\Bundle\TranslationBundle\Entity\Translation;
+use Oro\Bundle\TranslationBundle\Manager\TranslationManager;
+use Oro\Component\MessageQueue\Client\MessageProducerInterface;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
 
-class EnumOptionListenerTest extends EnumValueListenerTestCase
+class EnumOptionListenerTest extends TestCase
 {
-    use EntityTrait;
+    private const ENUM_CODE = 'test_enum_code';
 
+    private EntityManager&MockObject $em;
+    private EnumTranslationCache&MockObject $cache;
+    private TranslationManager&MockObject $translationManager;
+    private MessageProducerInterface&MockObject $messageProducer;
     private EnumOptionListener $listener;
 
     #[\Override]
     protected function setUp(): void
     {
-        parent::setUp();
+        $this->em = $this->createMock(EntityManager::class);
+        $this->cache = $this->createMock(EnumTranslationCache::class);
+        $this->translationManager = $this->createMock(TranslationManager::class);
+        $this->messageProducer = $this->createMock(MessageProducerInterface::class);
+
+        $translationHelper = $this->createMock(ConfigTranslationHelper::class);
+        $translationHelper->expects(self::any())
+            ->method('getLocale')
+            ->willReturn('fr');
+
+        $doctrine = $this->createMock(ManagerRegistry::class);
+        $doctrine->expects(self::any())
+            ->method('getManagerForClass')
+            ->willReturn($this->em);
 
         $this->listener = new EnumOptionListener(
-            $this->doctrine,
+            $doctrine,
             $this->cache,
             $this->translationManager,
-            $this->translationHelper,
+            $translationHelper,
             $this->messageProducer
         );
         EntityExtendTestInitializer::initialize();
     }
 
-    public function testPostPersist()
+    private function getEntityInstance(): TestEnumValue
     {
-        $this->assertClearCacheCalled();
+        return new TestEnumValue(self::ENUM_CODE, 'Test', 'test', 1);
+    }
+
+    public function testPostPersist(): void
+    {
+        $this->cache->expects(self::once())
+            ->method('invalidate')
+            ->with(self::ENUM_CODE);
 
         $this->listener->postPersist($this->getEntityInstance());
     }
 
-    public function testPostPersistNotSupportedClass()
+    public function testPostPersistNotSupportedClass(): void
     {
-        $this->assertClearCacheNotCalled();
+        $this->cache->expects(self::never())
+            ->method('invalidate');
 
         $this->listener->postPersist(new \stdClass());
     }
 
-    public function testPostUpdate()
+    public function testPostUpdate(): void
     {
-        $this->assertClearCacheCalled();
+        $this->cache->expects(self::once())
+            ->method('invalidate')
+            ->with(self::ENUM_CODE);
 
         $this->listener->postUpdate($this->getEntityInstance());
     }
 
-    public function testPostUpdateNotSupportedClass()
+    public function testPostUpdateNotSupportedClass(): void
     {
-        $this->assertClearCacheNotCalled();
+        $this->cache->expects(self::never())
+            ->method('invalidate');
 
         $this->listener->postUpdate(new \stdClass());
     }
 
-    public function testPostRemove()
+    public function testPostRemove(): void
     {
-        $this->assertClearCacheCalled();
+        $this->cache->expects(self::once())
+            ->method('invalidate')
+            ->with(self::ENUM_CODE);
 
         $this->listener->postRemove($this->getEntityInstance());
     }
 
-    public function testPostFlush()
+    public function testPostFlush(): void
     {
-        $this->assetTranslationRemoved();
-        $this->assertClearCacheCalled();
+        $repo = $this->createMock(TranslationRepository::class);
+        $this->em->expects(self::once())
+            ->method('getRepository')
+            ->with(Translation::class)
+            ->willReturn($repo);
+        $repo->expects(self::once())
+            ->method('findTranslation')
+            ->willReturn((new Translation()));
+        $this->em->expects(self::once())
+            ->method('remove');
+
+        $this->cache->expects(self::once())
+            ->method('invalidate')
+            ->with(self::ENUM_CODE);
 
         $this->listener->postRemove($this->getEntityInstance());
         $this->listener->postFlush();
     }
 
-    public function testPostRemoveNotSupportedClass()
+    public function testPostRemoveNotSupportedClass(): void
     {
-        $this->assertClearCacheNotCalled();
+        $this->cache->expects(self::never())
+            ->method('invalidate');
 
         $this->listener->postRemove(new \stdClass());
-    }
-
-    /**
-     * @return EnumOptionTranslation|object
-     */
-    private function getEntityInstance()
-    {
-        return $this->getEntity(
-            TestEnumValue::class,
-            ['enumCode' => self::ENUM_CODE, 'name' => 'Test', 'internalId' => 'test', 'priority' => 1],
-            ['enumCode', 'name', 'internalId', 1]
-        );
     }
 }
