8a3f8d2d49187a8bb984b5befc6d03bd1adf5212
oro-panasiukr <39378223+oro-panasiukr@users.noreply.github.com>
2025-04-25 16:04:14 +0200

    BB-25564: Translation files are loaded multiple times per request (#41232)
    * BB-25564: Translation files are loaded multiple times per request
    
    * BAP-22701: Functional tests performance optimization

--- src/Oro/Bundle/TranslationBundle/Translation/Translator.php
+++ src/Oro/Bundle/TranslationBundle/Translation/Translator.php
@@ -43,7 +43,7 @@ class Translator extends BaseTranslator
     private ?string $appliedStrategyName = null;
     private ?string $appliedLocale = null;
     private bool $enableDumpCatalogue = false;
-    private bool $disableResetCatalogues = false;
+    private bool $disableResetCatalogues = true;
 
     public function __construct(
         ContainerInterface $container,
@@ -401,6 +401,11 @@ class Translator extends BaseTranslator
             $locale = $this->getLocale();
         }
         $strategyName = $this->strategyProvider->getStrategy()->getName();
+
+        if ($this->appliedStrategyName !== $strategyName) {
+            $this->catalogues = [];
+        }
+
         if ($this->appliedLocale !== $locale || $this->appliedStrategyName !== $strategyName) {
             $this->appliedLocale = $locale;
             $this->appliedStrategyName = $strategyName;
