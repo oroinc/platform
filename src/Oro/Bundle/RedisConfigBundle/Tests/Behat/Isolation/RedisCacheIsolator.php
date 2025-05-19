5f615ccc8ec00b83c21476cfa6cffbeae8031812
Viacheslav Dubrovskyi <vdubrovskyi@oroinc.com>
2025-05-19 14:49:56 +0300

    * OPI-1440 Test dragonfly in addition to Redis for 6.0 branch (#41440)
    * Add possibility use dragonfly as option
    
    ---------
    
    Co-authored-by: Andrii Yatsenko <ayatsenko@oroinc.com>

--- src/Oro/Bundle/RedisConfigBundle/Tests/Behat/Isolation/RedisCacheIsolator.php
+++ src/Oro/Bundle/RedisConfigBundle/Tests/Behat/Isolation/RedisCacheIsolator.php
@@ -37,7 +37,8 @@ class RedisCacheIsolator implements IsolatorInterface
     /** {@inheritdoc} */
     public function isApplicable(ContainerInterface $container)
     {
-        return \getenv(self::REDIS_ENABLED_ENV_VAR) === 'REDIS' && (bool)$this->knownClients;
+        $cacheValue = \getenv(self::REDIS_ENABLED_ENV_VAR);
+        return ($cacheValue === 'REDIS' || $cacheValue === 'DRAGONFLY') && (bool)$this->knownClients;
     }
 
     /** {@inheritdoc} */
