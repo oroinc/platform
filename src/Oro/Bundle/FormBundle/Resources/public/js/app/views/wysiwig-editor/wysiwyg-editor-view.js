eea1c9307d1ecea065d10067cc85736a56da297d
Viacheslav Chekanovskiy <vchekanovskyi@oroinc.com>
2025-06-19 05:45:16 -0600

    BB-25787: Fix TinyMCE vulnerabilities in maintenance branches (#41689)
    * BB-25787: Fix TinyMCE vulnerabilities in maintenance branches
    
    * Updated dev.locks
    
    | Production Changes               | From    | To      | Compare                                                                              |
    |----------------------------------|---------|---------|--------------------------------------------------------------------------------------|
    | composer/ca-bundle               | 1.5.6   | 1.5.7   | [...](https://github.com/composer/ca-bundle/compare/1.5.6...1.5.7)                   |
    | composer/spdx-licenses           | 1.5.8   | 1.5.9   | [...](https://github.com/composer/spdx-licenses/compare/1.5.8...1.5.9)               |
    | symfony/polyfill-intl-grapheme   | v1.31.0 | v1.32.0 | [...](https://github.com/symfony/polyfill-intl-grapheme/compare/v1.31.0...v1.32.0)   |
    | symfony/polyfill-intl-icu        | v1.31.0 | v1.32.0 | [...](https://github.com/symfony/polyfill-intl-icu/compare/v1.31.0...v1.32.0)        |
    | symfony/polyfill-intl-idn        | v1.31.0 | v1.32.0 | [...](https://github.com/symfony/polyfill-intl-idn/compare/v1.31.0...v1.32.0)        |
    | symfony/polyfill-intl-normalizer | v1.31.0 | v1.32.0 | [...](https://github.com/symfony/polyfill-intl-normalizer/compare/v1.31.0...v1.32.0) |
    | symfony/polyfill-mbstring        | v1.31.0 | v1.32.0 | [...](https://github.com/symfony/polyfill-mbstring/compare/v1.31.0...v1.32.0)        |
    | symfony/polyfill-php73           | v1.31.0 | v1.32.0 | [...](https://github.com/symfony/polyfill-php73/compare/v1.31.0...v1.32.0)           |
    | symfony/polyfill-php80           | v1.31.0 | v1.32.0 | [...](https://github.com/symfony/polyfill-php80/compare/v1.31.0...v1.32.0)           |
    | symfony/polyfill-php81           | v1.31.0 | v1.32.0 | [...](https://github.com/symfony/polyfill-php81/compare/v1.31.0...v1.32.0)           |
    
    | Dev Changes      | From    | To      | Compare                                                              |
    |------------------|---------|---------|----------------------------------------------------------------------|
    | phpspec/prophecy | v1.20.0 | v1.22.0 | [...](https://github.com/phpspec/prophecy/compare/v1.20.0...v1.22.0) |
    
    * Updated dev.locks
    
    | Production Changes  | From   | To     | Compare                                                               |
    |---------------------|--------|--------|-----------------------------------------------------------------------|
    | phpseclib/phpseclib | 3.0.43 | 3.0.44 | [...](https://github.com/phpseclib/phpseclib/compare/3.0.43...3.0.44) |
    
    ---------
    
    Co-authored-by: orocrmdeployer <orocrmdeployer@oroinc.com>

--- src/Oro/Bundle/FormBundle/Resources/public/js/app/views/wysiwig-editor/wysiwyg-editor-view.js
+++ src/Oro/Bundle/FormBundle/Resources/public/js/app/views/wysiwig-editor/wysiwyg-editor-view.js
@@ -37,6 +37,7 @@ define(function(require) {
             branding: false,
             browser_spellcheck: true,
             convert_unsafe_embeds: true,
+            sandbox_iframes: true,
             file_picker_types: 'image',
             file_picker_callback: function(callback, value, meta) {
                 const input = document.createElement('input');
