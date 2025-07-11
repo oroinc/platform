d74497034723a5b427dc828386a0bea39e179283
timofiyprisyazhnyuk <33844707+timofiyprisyazhnyuk@users.noreply.github.com>
2025-07-11 15:24:48 +0300

    BAP-23087 Unable to rebuild cache after upgrading application to 6.1 (#41854)

--- src/Oro/Bundle/EntityExtendBundle/Migration/Enum/UpdateBaseEnumRelatedDataMigration.php
+++ src/Oro/Bundle/EntityExtendBundle/Migration/Enum/UpdateBaseEnumRelatedDataMigration.php
@@ -48,7 +48,8 @@ class UpdateBaseEnumRelatedDataMigration implements Migration, ConnectionAwareIn
             if (!isset($entityConfigData['extend']['is_extend'])
                 || !$entityConfigData['extend']['is_extend']
                 || $entityConfig['class_name'] === EnumOption::class
-                || str_starts_with($entityConfig['class_name'], ExtendHelper::ENTITY_NAMESPACE)) {
+                || ExtendHelper::isOutdatedEnumOptionEntity($entityConfig['class_name'])
+            ) {
                 continue;
             }
             $fieldConfigs = $this->connection->fetchAllAssociative(
