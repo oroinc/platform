8f6602fff8a5ed7de5a3829f40a77bd071746572
Yevhenii Zhadan <146938608+yevhenii101991@users.noreply.github.com>
2025-07-11 18:47:18 +0300

    BB-25765: Product search index is not updated with new images after r… (#41803)

--- src/Oro/Bundle/AttachmentBundle/Tests/Functional/Controller/FileControllerTest.php
+++ src/Oro/Bundle/AttachmentBundle/Tests/Functional/Controller/FileControllerTest.php
@@ -244,8 +244,7 @@ class FileControllerTest extends WebTestCase
         $this->client->request('GET', $url);
         $result = $this->client->getResponse();
 
-        self::assertTrue($result->headers->getCacheControlDirective('public'));
-        self::assertTrue($result->headers->hasCacheControlDirective('max-age'));
+        self::assertEquals(0, $result->headers->getCacheControlDirective('max-age'));
         self::assertResponseContentTypeEquals($result, $file->getMimeType());
         self::assertResponseStatusCodeEquals($result, 200);
     }
@@ -259,8 +258,7 @@ class FileControllerTest extends WebTestCase
         $this->client->request('GET', $url);
         $result = $this->client->getResponse();
 
-        self::assertTrue($result->headers->getCacheControlDirective('public'));
-        self::assertTrue($result->headers->hasCacheControlDirective('max-age'));
+        self::assertEquals(0, $result->headers->getCacheControlDirective('max-age'));
         self::assertResponseContentTypeEquals($result, 'image/webp');
         self::assertResponseStatusCodeEquals($result, 200);
     }
@@ -281,8 +279,7 @@ class FileControllerTest extends WebTestCase
         $this->client->request('GET', $url);
         $result = $this->client->getResponse();
 
-        self::assertTrue($result->headers->getCacheControlDirective('public'));
-        self::assertTrue($result->headers->hasCacheControlDirective('max-age'));
+        self::assertEquals(0, $result->headers->getCacheControlDirective('max-age'));
         self::assertResponseContentTypeEquals($result, 'image/png');
         self::assertResponseStatusCodeEquals($result, 200);
     }
@@ -303,9 +300,23 @@ class FileControllerTest extends WebTestCase
         $this->client->request('GET', $url);
         $result = $this->client->getResponse();
 
-        self::assertTrue($result->headers->getCacheControlDirective('public'));
-        self::assertTrue($result->headers->hasCacheControlDirective('max-age'));
+        self::assertEquals(0, $result->headers->getCacheControlDirective('max-age'));
         self::assertResponseContentTypeEquals($result, 'image/webp');
+        self::assertResponseStatusCodeEquals($result, 200);
+    }
+
+    public function testGetFilteredAvatarImage(): void
+    {
+        /** @var File $file */
+        $file = $this->getReference(LoadImageData::IMAGE_JPG);
+        $url = self::getContainer()->get(FileUrlProviderInterface::class)
+            ->getFilteredImageUrl($file, 'avatar_med', '');
+        $this->client->request('GET', $url);
+        $result = $this->client->getResponse();
+
+        self::assertTrue($result->headers->getCacheControlDirective('public'));
+        self::assertNotEquals(0, $result->headers->getCacheControlDirective('max-age'));
+        self::assertResponseContentTypeEquals($result, $file->getMimeType());
         self::assertResponseStatusCodeEquals($result, 200);
     }
 }
