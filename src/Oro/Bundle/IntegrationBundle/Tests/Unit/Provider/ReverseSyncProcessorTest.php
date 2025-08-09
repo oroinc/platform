35536447bd11c9fbd1a8bd4e2714692e6df63ce7
Vitalii Yeromenko <vyeromenko@oroinc.com>
2025-08-09 16:52:19 +0300

    BAP-23113 Add a flag to integration sync settings to log warnings during sync - unit test - Documentation - Change log_warnings.tooltip

--- src/Oro/Bundle/IntegrationBundle/Tests/Unit/Provider/ReverseSyncProcessorTest.php
+++ src/Oro/Bundle/IntegrationBundle/Tests/Unit/Provider/ReverseSyncProcessorTest.php
@@ -3,6 +3,7 @@
 namespace Oro\Bundle\IntegrationBundle\Tests\Unit\Provider;
 
 use Doctrine\Persistence\ManagerRegistry;
+use Oro\Bundle\ImportExportBundle\Context\ContextInterface;
 use Oro\Bundle\ImportExportBundle\Job\JobResult;
 use Oro\Bundle\ImportExportBundle\Processor\ProcessorRegistry;
 use Oro\Bundle\IntegrationBundle\Entity\Channel as Integration;
@@ -12,6 +13,7 @@ use Oro\Bundle\IntegrationBundle\Manager\TypesRegistry;
 use Oro\Bundle\IntegrationBundle\Provider\ReverseSyncProcessor;
 use Oro\Bundle\IntegrationBundle\Tests\Unit\Fixture\TestContext;
 use Oro\Bundle\IntegrationBundle\Tests\Unit\Fixture\TestTwoWayConnector as TestConnector;
+use Oro\Component\Config\Common\ConfigObject;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 class ReverseSyncProcessorTest extends \PHPUnit\Framework\TestCase
@@ -108,6 +110,212 @@ class ReverseSyncProcessorTest extends \PHPUnit\Framework\TestCase
 
         $processor = $this->getReverseSyncProcessor(['addConnectorStatusAndFlush']);
         $processor->process($this->integration, $connector, ['testParameter' => 'testValue']);
+    }
+
+    public function testProcessExportWithErrorsAndWarningsLoggingEnabled(): void
+    {
+        $connector = 'testConnector';
+        $errors = ['Error 1', 'Error 2'];
+
+        $this->integration->expects($this->once())
+            ->method('getId')
+            ->willReturn('testChannel');
+
+        $realConnector = new TestConnector();
+
+        $expectedAlias = 'test_alias';
+        $this->processorRegistry->expects($this->once())
+            ->method('getProcessorAliasesByEntity')
+            ->with(ProcessorRegistry::TYPE_EXPORT)
+            ->willReturn([$expectedAlias]);
+
+        $this->registry->expects($this->once())
+            ->method('getConnectorType')
+            ->willReturn($realConnector);
+
+        $this->integration->expects($this->once())
+            ->method('isEnabled')
+            ->willReturn(true);
+
+        $context = $this->createMock(ContextInterface::class);
+        $context->expects($this->once())
+            ->method('getErrors')
+            ->willReturn($errors);
+
+        $jobResult = new JobResult();
+        $jobResult->setContext($context);
+        $jobResult->setSuccessful(true);
+
+        $this->jobExecutor->expects($this->once())
+            ->method('executeJob')
+            ->with(
+                'export',
+                'tstJobName',
+                [
+                    'export' => [
+                        'entityName'     => 'testEntity',
+                        'channel'        => 'testChannel',
+                        'processorAlias' => $expectedAlias,
+                    ]
+                ]
+            )
+            ->willReturn($jobResult);
+
+        $syncSettings = $this->createMock(ConfigObject::class);
+        $syncSettings->expects($this->once())
+            ->method('offsetGetOr')
+            ->with('logWarnings', false)
+            ->willReturn(true);
+
+        $this->integration->expects($this->once())
+            ->method('getSynchronizationSettings')
+            ->willReturn($syncSettings);
+
+        $this->log->expects($this->exactly(2))
+            ->method('error')
+            ->withConsecutive(
+                ['Error 1'],
+                ['Error 2']
+            );
+
+        $processor = $this->getReverseSyncProcessor(['addConnectorStatusAndFlush']);
+
+        $this->assertTrue(
+            $processor->process($this->integration, $connector)
+        );
+    }
+
+    public function testProcessExportWithErrorsAndWarningsLoggingDisabled(): void
+    {
+        $connector = 'testConnector';
+        $errors = ['Error 1', 'Error 2'];
+
+        $this->integration->expects($this->once())
+            ->method('getId')
+            ->willReturn('testChannel');
+
+        $realConnector = new TestConnector();
+
+        $expectedAlias = 'test_alias';
+        $this->processorRegistry->expects($this->once())
+            ->method('getProcessorAliasesByEntity')
+            ->with(ProcessorRegistry::TYPE_EXPORT)
+            ->willReturn([$expectedAlias]);
+
+        $this->registry->expects($this->once())
+            ->method('getConnectorType')
+            ->willReturn($realConnector);
+
+        $this->integration->expects($this->once())
+            ->method('isEnabled')
+            ->willReturn(true);
+
+        $context = $this->createMock(ContextInterface::class);
+        $context->expects($this->once())
+            ->method('getErrors')
+            ->willReturn($errors);
+
+        $jobResult = new JobResult();
+        $jobResult->setContext($context);
+        $jobResult->setSuccessful(true);
+
+        $this->jobExecutor->expects($this->once())
+            ->method('executeJob')
+            ->with(
+                'export',
+                'tstJobName',
+                [
+                    'export' => [
+                        'entityName'     => 'testEntity',
+                        'channel'        => 'testChannel',
+                        'processorAlias' => $expectedAlias,
+                    ]
+                ]
+            )
+            ->willReturn($jobResult);
+
+        $syncSettings = $this->createMock(ConfigObject::class);
+        $syncSettings->expects($this->once())
+            ->method('offsetGetOr')
+            ->with('logWarnings', false)
+            ->willReturn(false);
+
+        $this->integration->expects($this->once())
+            ->method('getSynchronizationSettings')
+            ->willReturn($syncSettings);
+
+        $this->log->expects($this->never())
+            ->method('error');
+
+        $processor = $this->getReverseSyncProcessor(['addConnectorStatusAndFlush']);
+
+        $this->assertTrue(
+            $processor->process($this->integration, $connector)
+        );
+    }
+
+    public function testProcessExportWithJobFailure(): void
+    {
+        $connector = 'testConnector';
+        $failureExceptions = ['Exception 1', 'Exception 2'];
+
+        $this->integration->expects($this->once())
+            ->method('getId')
+            ->willReturn('testChannel');
+
+        $realConnector = new TestConnector();
+
+        $expectedAlias = 'test_alias';
+        $this->processorRegistry->expects($this->once())
+            ->method('getProcessorAliasesByEntity')
+            ->with(ProcessorRegistry::TYPE_EXPORT)
+            ->willReturn([$expectedAlias]);
+
+        $this->registry->expects($this->once())
+            ->method('getConnectorType')
+            ->willReturn($realConnector);
+
+        $this->integration->expects($this->once())
+            ->method('isEnabled')
+            ->willReturn(true);
+
+        $context = $this->createMock(ContextInterface::class);
+        $context->expects($this->once())
+            ->method('getErrors')
+            ->willReturn([]);
+
+        $jobResult = new JobResult();
+        $jobResult->setContext($context);
+        $jobResult->setSuccessful(false); // Job failed
+        $jobResult->setFailureExceptions($failureExceptions);
+
+        $this->jobExecutor->expects($this->once())
+            ->method('executeJob')
+            ->with(
+                'export',
+                'tstJobName',
+                [
+                    'export' => [
+                        'entityName'     => 'testEntity',
+                        'channel'        => 'testChannel',
+                        'processorAlias' => $expectedAlias,
+                    ]
+                ]
+            )
+            ->willReturn($jobResult);
+
+        $this->log->expects($this->exactly(2))
+            ->method('error')
+            ->withConsecutive(
+                ['Errors have occurred:'],
+                ['Exception 1' . PHP_EOL . 'Exception 2']
+            );
+
+        $processor = $this->getReverseSyncProcessor(['addConnectorStatusAndFlush']);
+
+        $this->assertFalse(
+            $processor->process($this->integration, $connector)
+        );
     }
 
     private function getReverseSyncProcessor(array $mockedMethods): ReverseSyncProcessor
