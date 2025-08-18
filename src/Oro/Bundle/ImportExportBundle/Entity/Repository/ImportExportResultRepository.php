d8b226c79e24199963bc2015419dfb04a8a25830
oro-panasiukr <39378223+oro-panasiukr@users.noreply.github.com>
2025-08-18 08:44:54 +0200

    BAP-22947: Cleanup of import expired results can lead to out of space (#42306)

--- src/Oro/Bundle/ImportExportBundle/Entity/Repository/ImportExportResultRepository.php
+++ src/Oro/Bundle/ImportExportBundle/Entity/Repository/ImportExportResultRepository.php
@@ -12,19 +12,26 @@ class ImportExportResultRepository extends EntityRepository
 {
     public function updateExpiredRecords(\DateTime $from, \DateTime $to): void
     {
+        $this->updateExpiredRecordsAndReturnCount($from, $to);
+    }
+
+    public function updateExpiredRecordsAndReturnCount(\DateTime $from, \DateTime $to): int
+    {
         $qb = $this->createQueryBuilder('importExportResult');
         $qb->update()
             ->set('importExportResult.expired', ':expired')
             ->where(
                 $qb->expr()->andX(
                     $qb->expr()->gte('importExportResult.createdAt', ':from'),
-                    $qb->expr()->lte('importExportResult.createdAt', ':to')
+                    $qb->expr()->lte('importExportResult.createdAt', ':to'),
+                    $qb->expr()->eq('importExportResult.expired', ':isExpired')
                 )
             )
             ->setParameter('expired', true)
+            ->setParameter('isExpired', false)
             ->setParameter('from', $from, Types::DATETIME_MUTABLE)
             ->setParameter('to', $to, Types::DATETIME_MUTABLE);
 
-        $qb->getQuery()->execute();
+        return (int) $qb->getQuery()->execute();
     }
 }
