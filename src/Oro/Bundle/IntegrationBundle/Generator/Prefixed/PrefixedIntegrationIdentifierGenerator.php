8a6e84d47b27877870afb594c1d6c42cce68d426
Eugene Zibrov <106163563+eugene-zibrov@users.noreply.github.com>
2025-09-11 11:33:31 +0200

    BB-25652: Installed Stripe package changes behavior of refund and cancel actions globally (#42517)

--- src/Oro/Bundle/IntegrationBundle/Generator/Prefixed/PrefixedIntegrationIdentifierGenerator.php
+++ src/Oro/Bundle/IntegrationBundle/Generator/Prefixed/PrefixedIntegrationIdentifierGenerator.php
@@ -24,4 +24,26 @@ class PrefixedIntegrationIdentifierGenerator implements IntegrationIdentifierGen
     {
         return sprintf('%s_%s', $this->prefix, $channel->getId());
     }
+
+    /**
+     * @param string $identifier
+     *
+     * @return array{?string,?int} Array with payment method prefix and corresponding integration channel ID.
+     */
+    public static function parseIdentifier(string $identifier): array
+    {
+        $pos = strrpos($identifier, '_');
+        if ($pos !== false) {
+            $id = substr($identifier, $pos + 1);
+            if (is_numeric($id)) {
+                $prefix = substr($identifier, 0, $pos);
+                $id = (int) $id;
+            } else {
+                $prefix = $identifier;
+                $id = null;
+            }
+        }
+
+        return [$prefix ?? null, $id ?? null];
+    }
 }
