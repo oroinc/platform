48602b227e014d0f55a8b8c644c25168e91d693c
Yefim Yevtushenko <yyevtushenko@oroinc.com>
2025-10-23 12:15:19 +0300

    BAP-23176: Websocket server must be stopped after system configuration changes (master) (#42911)
    - added oro_sync_websocket_server_state table
    - added WebsocketServerSharedStateManager to manage shared state
    - added WebsocketServerCacheStateManager to manage cache state
    - added reusable WebsocketServerStateCheckPeriodic to stop websocket server when state is changed
    - added WebsocketServerStateSystemConfigUpdateListener to update system_config state
    - added WebsocketServerStopper to gracefully stop websocket server
    
    ---------
    
    Co-authored-by: Mykhailo Kotliar <mkotliar@oroinc.com>

---
+++ src/Oro/Bundle/SyncBundle/Migrations/Schema/OroSyncBundleInstaller.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Oro\Bundle\SyncBundle\Migrations\Schema;
+
+use Doctrine\DBAL\Schema\Schema;
+use Oro\Bundle\MigrationBundle\Migration\Installation;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+
+class OroSyncBundleInstaller implements Installation
+{
+    #[\Override]
+    public function getMigrationVersion(): string
+    {
+        return 'v7_0_0_1';
+    }
+
+    #[\Override]
+    public function up(Schema $schema, QueryBag $queries): void
+    {
+        $this->createStateTable($schema);
+    }
+
+    private function createStateTable(Schema $schema): void
+    {
+        $table = $schema->createTable('oro_sync_websocket_server_state');
+        $table->addColumn('id', 'string', ['length' => 15, 'notnull' => true]);
+        $table->addColumn('updated_at', 'datetime', ['notnull' => false]);
+        $table->setPrimaryKey(['id']);
+    }
+}
