b60dec3c920cf5fbbfb131a782fb7d4c7ea3c301
Yefim Yevtushenko <yyevtushenko@oroinc.com>
2025-10-23 12:16:35 +0300

    BAP-23176: Websocket server must be stopped after system configuration changes (5.0) (#42914)
    - added oro_sync_websocket_server_state table
    - added WebsocketServerSharedStateManager to manage shared state
    - added WebsocketServerCacheStateManager to manage cache state
    - added reusable WebsocketServerStateCheckPeriodic to stop websocket server when state is changed
    - added WebsocketServerStateSystemConfigUpdateListener to update system_config state
    - added WebsocketServerStopper to gracefully stop websocket server
    
    ---------
    
    Co-authored-by: Mykhailo Kotliar <mkotliar@oroinc.com>

---
+++ src/Oro/Bundle/SyncBundle/WebsocketServerState/WebsocketServerSharedStateManager.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Oro\Bundle\SyncBundle\WebsocketServerState;
+
+use Doctrine\DBAL\Platforms\MySQLPlatform;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * Manages the state of the Websocket server by storing and retrieving the last updated timestamp
+ * for a given state ID in the database.
+ */
+class WebsocketServerSharedStateManager implements WebsocketServerStateManagerInterface
+{
+    public function __construct(private readonly ManagerRegistry $doctrine)
+    {
+    }
+
+    #[\Override]
+    public function updateState(string $stateId): \DateTimeInterface
+    {
+        $stateDate = new \DateTime('now', new \DateTimeZone('UTC'));
+        $connection = $this->doctrine->getConnection();
+
+        $sql = $this->getUpsertSql($connection->getDatabasePlatform() instanceof MySQLPlatform);
+
+        $connection->executeStatement(
+            $sql,
+            [
+                'id' => $stateId,
+                'date' => $stateDate,
+            ],
+            [
+                'id' => Types::STRING,
+                'date' => Types::DATETIME_MUTABLE,
+            ]
+        );
+
+        return $stateDate;
+    }
+
+    #[\Override]
+    public function getState(string $stateId): ?\DateTimeInterface
+    {
+        $result = $this->doctrine->getConnection()->createQueryBuilder()
+            ->from('oro_sync_websocket_server_state')
+            ->select('updated_at')
+            ->where('id = :id')
+            ->setParameter('id', $stateId, Types::STRING)
+            ->execute()
+            ->fetchOne() ?: null;
+
+        if ($result !== null) {
+            $result = new \DateTime($result, new \DateTimeZone('UTC'));
+        }
+
+        return $result;
+    }
+
+    private function getUpsertSql(bool $isMySQL): string
+    {
+        if ($isMySQL) {
+            return 'INSERT INTO oro_sync_websocket_server_state (id, updated_at) VALUES (:id, :date)
+                    ON DUPLICATE KEY UPDATE updated_at = VALUES(updated_at)';
+        }
+
+        return 'INSERT INTO oro_sync_websocket_server_state (id, updated_at) VALUES (:id, :date)
+                ON CONFLICT (id) DO UPDATE SET updated_at = EXCLUDED.updated_at';
+    }
+}
