c1da5ec3c674b39412a47c0f8a6b0a066a5b8eeb
Alex Yegerev <1755578+lexxcode@users.noreply.github.com>
2025-10-28 18:30:02 +0400

    BB-26286: Convert for ConfigBundle, CronBundle, CurrencyBundle (#42749)
    * BB-26286: Convert for ConfigBundle, CronBundle, CurrencyBundle

--- src/Oro/Bundle/CronBundle/Resources/public/js/app/views/schedule-intervals-view.js
+++ src/Oro/Bundle/CronBundle/Resources/public/js/app/views/schedule-intervals-view.js
@@ -1,45 +1,41 @@
-define(function(require) {
-    'use strict';
+import BaseView from 'oroui/js/app/views/base/view';
+import _ from 'underscore';
 
-    const BaseView = require('oroui/js/app/views/base/view');
-    const _ = require('underscore');
-
-    const ScheduleIntervalsView = BaseView.extend({
-        options: {
-            selectors: {
-                row: '[data-role="schedule-interval-row"]',
-                rowError: '[data-role="schedule-interval-row-error"]'
-            }
-        },
-
-        /**
-         * @inheritdoc
-         */
-        constructor: function ScheduleIntervalsView(options) {
-            ScheduleIntervalsView.__super__.constructor.call(this, options);
-        },
-
-        /**
-         * @inheritdoc
-         */
-        initialize: function(options) {
-            this.options = _.defaults(options || {}, this.options);
-            this.delegate('change content:remove', this._handleErrors);
-        },
-
-        _handleErrors: function() {
-            this.$(this.options.selectors.rowError).remove();
-            this.$(this.options.selectors.row).removeClass('has-row-error');
-        },
-
-        dispose: function() {
-            if (this.disposed) {
-                return;
-            }
-
-            this.undelegate('change content:remove', this._handleErrors);
+const ScheduleIntervalsView = BaseView.extend({
+    options: {
+        selectors: {
+            row: '[data-role="schedule-interval-row"]',
+            rowError: '[data-role="schedule-interval-row-error"]'
         }
-    });
+    },
 
-    return ScheduleIntervalsView;
+    /**
+     * @inheritdoc
+     */
+    constructor: function ScheduleIntervalsView(options) {
+        ScheduleIntervalsView.__super__.constructor.call(this, options);
+    },
+
+    /**
+     * @inheritdoc
+     */
+    initialize: function(options) {
+        this.options = _.defaults(options || {}, this.options);
+        this.delegate('change content:remove', this._handleErrors);
+    },
+
+    _handleErrors: function() {
+        this.$(this.options.selectors.rowError).remove();
+        this.$(this.options.selectors.row).removeClass('has-row-error');
+    },
+
+    dispose: function() {
+        if (this.disposed) {
+            return;
+        }
+
+        this.undelegate('change content:remove', this._handleErrors);
+    }
 });
+
+export default ScheduleIntervalsView;
