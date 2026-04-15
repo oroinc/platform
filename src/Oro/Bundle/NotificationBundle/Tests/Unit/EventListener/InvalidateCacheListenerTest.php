c5e97f14c6b2ae69ca7064d9a4b2e9c2d9a88448
Yevhenii Zhadan <146938608+yevhenii101991@users.noreply.github.com>
2026-04-15 16:25:24 +0300

    BB-27184: Existing email notification rule stops working after trigger event type is modified (#44750)

--- src/Oro/Bundle/NotificationBundle/Tests/Unit/EventListener/InvalidateCacheListenerTest.php
+++ src/Oro/Bundle/NotificationBundle/Tests/Unit/EventListener/InvalidateCacheListenerTest.php
@@ -166,7 +166,7 @@ class InvalidateCacheListenerTest extends \PHPUnit\Framework\TestCase
         $this->uow->expects(self::once())
             ->method('getEntityChangeSet')
             ->with($emailNotification)
-            ->willReturn(['event' => []]);
+            ->willReturn(['eventName' => []]);
 
         $this->notificationManager->expects(self::once())
             ->method('clearCache');
