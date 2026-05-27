c041121d968dc0698f1658fd56d5e546969918e2
Vova Soroka <vsoroka@oroinc.com>
2026-05-27 20:04:05 +0300

    BAP-23365: Improve data audit performance during entity import (#45355)

--- src/Oro/Bundle/DataAuditBundle/Tests/Functional/Async/AuditChangedEntitiesInverseCollectionsProcessorTest.php
+++ src/Oro/Bundle/DataAuditBundle/Tests/Functional/Async/AuditChangedEntitiesInverseCollectionsProcessorTest.php
@@ -2,192 +2,63 @@
 
 namespace Oro\Bundle\DataAuditBundle\Tests\Functional\Async;
 
-use Oro\Bundle\DataAuditBundle\Async\AuditChangedEntitiesInverseCollectionsChunkProcessor;
 use Oro\Bundle\DataAuditBundle\Async\AuditChangedEntitiesInverseCollectionsProcessor;
 use Oro\Bundle\DataAuditBundle\Async\Topic\AuditChangedEntitiesInverseCollectionsChunkTopic;
-use Oro\Bundle\DataAuditBundle\Async\Topic\AuditChangedEntitiesInverseCollectionsTopic;
-use Oro\Bundle\DataAuditBundle\Entity\Audit;
-use Oro\Bundle\DataAuditBundle\Exception\WrongDataAuditEntryStateException;
-use Oro\Bundle\DataAuditBundle\Provider\AuditConfigProvider;
-use Oro\Bundle\DataAuditBundle\Service\EntityChangesToAuditEntryConverter;
 use Oro\Bundle\DataAuditBundle\Tests\Functional\DataFixtures\LoadTestAuditDataWithOneToManyData;
+use Oro\Bundle\DataAuditBundle\Tests\Functional\Environment\Entity\TestAuditDataChild;
 use Oro\Bundle\DataAuditBundle\Tests\Functional\Environment\Entity\TestAuditDataOwner;
-use Oro\Bundle\MessageQueueBundle\Test\Functional\JobsAwareTestTrait;
 use Oro\Bundle\MessageQueueBundle\Test\Functional\MessageQueueAssertTrait;
+use Oro\Bundle\MessageQueueBundle\Test\Functional\MessageQueueConsumerTestTrait;
 use Oro\Bundle\MessageQueueBundle\Test\Functional\MessageQueueExtension;
 use Oro\Bundle\TestFrameworkBundle\Test\WebTestCase;
-use Oro\Component\MessageQueue\Client\Config as MessageQueueConfig;
-use Oro\Component\MessageQueue\Topic\JobAwareTopicInterface;
-use Oro\Component\MessageQueue\Transport\ConnectionInterface;
 use Oro\Component\MessageQueue\Transport\Message;
 use Oro\Component\MessageQueue\Transport\MessageInterface;
-use Psr\Log\NullLogger;
+use Oro\Component\MessageQueue\Transport\SessionInterface;
 
 class AuditChangedEntitiesInverseCollectionsProcessorTest extends WebTestCase
 {
     use MessageQueueExtension;
     use MessageQueueAssertTrait;
+    use MessageQueueConsumerTestTrait;
     use AuditChangedEntitiesExtensionTrait;
-    use JobsAwareTestTrait;
 
-    /** @var AuditChangedEntitiesInverseCollectionsProcessor */
-    private $processor;
-
-    /** @var AuditChangedEntitiesInverseCollectionsChunkProcessor */
-    private $chunkProcessor;
+    private AuditChangedEntitiesInverseCollectionsProcessor $processor;
 
     #[\Override]
     protected function setUp(): void
     {
-        $this->initClient([], $this->generateBasicAuthHeader());
+        $this->initClient();
         $this->setUpMessageCollector();
         $this->loadFixtures([LoadTestAuditDataWithOneToManyData::class]);
-        $this->processor = $this->getContainer()
+        $this->processor = self::getContainer()
             ->get('oro_dataaudit.async.audit_changed_entities_inverse_collections');
-        $this->chunkProcessor = $this->getContainer()
-            ->get('oro_dataaudit.async.audit_changed_entities_inverse_collections_chunk');
     }
 
-    public function testChunkProcessorProcessResult(): void
+    private function getTestAuditDataOwner(string $reference): TestAuditDataOwner
     {
-        $testAuditOwner = $this->getReference('testAuditOwner');
-        $session = $this->getConnection()->createSession();
-        $job = $this->createUniqueJob();
-        $message = $this->createMessage([
-            'jobId' => $job->getId(),
-            'timestamp' => time(),
-            'transaction_id' => 'aTransactionId',
-            'entityData' => [
-                'entity_class' => TestAuditDataOwner::class,
-                'entity_id' => $testAuditOwner->getId(),
-                'change_set' => ['stringProperty' => [null, 'aNewValue']],
-                'set' => 'updated',
-                'fields' => [],
-            ]
-        ]);
-
-        $converter = $this->createMock(EntityChangesToAuditEntryConverter::class);
-        $logger = $this->createMock(NullLogger::class);
-        $auditConfigProvider = $this->createMock(AuditConfigProvider::class);
-        $chunkProcessor = new AuditChangedEntitiesInverseCollectionsChunkProcessor(
-            $converter,
-            $this->getJobRunner(),
-            $auditConfigProvider
-        );
-        $chunkProcessor->setLogger($logger);
-
-        $logger->expects($this->never())->method('log');
-        $processResult = $chunkProcessor->process($message, $session);
-        $this->assertEquals(AuditChangedEntitiesInverseCollectionsChunkProcessor::ACK, $processResult);
-
-        $logger->expects($this->once())->method('warning');
-        $exception = new WrongDataAuditEntryStateException((new Audit())->setObjectName('ObjectName'));
-        $converter->method('convert')->willThrowException($exception);
-
-        $processResult = $chunkProcessor->process($message, $session);
-        $this->assertEquals(AuditChangedEntitiesInverseCollectionsChunkProcessor::REQUEUE, $processResult);
-
-        $logger->expects($this->once())->method('error');
-        $converter->method('convert')->willThrowException(new \Exception('exception'));
-        $processResult = $chunkProcessor->process($message, $session);
-        $this->assertEquals(AuditChangedEntitiesInverseCollectionsChunkProcessor::REJECT, $processResult);
+        return $this->getReference($reference);
     }
 
-    public function testCouldBeGetInverseCollectionFromContainerAsService(): void
+    private function getTestAuditDataChild(TestAuditDataOwner $owner, int $childIndex): TestAuditDataChild
     {
-        $this->assertInstanceOf(AuditChangedEntitiesInverseCollectionsProcessor::class, $this->processor);
+        return $owner->getChildrenOneToMany()->get($childIndex);
     }
 
-    public function testCouldBeGetInverseCollectionsChunkFromContainerAsService(): void
+    private function getEntityData(TestAuditDataOwner $owner, array $childIds): array
     {
-        $this->assertInstanceOf(AuditChangedEntitiesInverseCollectionsChunkProcessor::class, $this->chunkProcessor);
-    }
-
-    public function testProcessingWithEmptyMessage(): void
-    {
-        $message = $this->createMessage([
-            'timestamp' => time(),
-            'transaction_id' => 'aTransactionId',
-        ]);
-
-        $this->processor->process($message, $this->getConnection()->createSession());
-        $this->assertMessagesEmpty(AuditChangedEntitiesInverseCollectionsTopic::getName());
-    }
-
-    public function testProcessorSplitCollectionToChunkAndSaveAudit(): void
-    {
-        $batchSize = 2;
-        /** @var TestAuditDataOwner $testAuditOwner */
-        $testAuditOwner = $this->getReference('testAuditOwner');
-        $expectedCount = ceil($testAuditOwner->getChildrenOneToMany()->count() / $batchSize);
-        $message = $this->createMessage([
-            'timestamp' => time(),
-            'transaction_id' => 'aTransactionId',
-            'entities_inserted' => [],
-            'entities_updated' => [
-                '000000001f4c2232000000006d016312' => [
-                    'entity_class' => TestAuditDataOwner::class,
-                    'entity_id' => $testAuditOwner->getId(),
-                    'change_set' => ['stringProperty' => [null, 'aNewValue']],
+        return [
+            'entity_class' => TestAuditDataOwner::class,
+            'entity_id' => $owner->getId(),
+            'change_set' => ['stringProperty' => [null, 'aNewValue']],
+            'set' => 'changed',
+            'fields' => [
+                'childrenOneToMany' => [
+                    'field_name' => 'ownerManyToOne',
+                    'entity_class' => TestAuditDataChild::class,
+                    'entity_ids' => $childIds
                 ]
-            ],
-            'entities_deleted' => [],
-            'collections_updated' => [],
-        ], [
-            MessageQueueConfig::PARAMETER_TOPIC_NAME => AuditChangedEntitiesInverseCollectionsTopic::getName(),
-            JobAwareTopicInterface::UNIQUE_JOB_NAME => 'fakeJobID'
-        ]);
-
-        $this->createRootJobMyMessage($message);
-
-        $this->processor->setBatchSize($batchSize);
-        $this->processor->process($message, $this->getConnection()->createSession());
-
-        $this->assertMessagesCount(AuditChangedEntitiesInverseCollectionsChunkTopic::getName(), $expectedCount);
-        $this->assertMessagesCreatedAndEntityIdsIsSplitting($batchSize);
-
-        $this->processedMessages();
-
-        $this->assertStoredAuditCount(4);
-        $this->assertStoredAuditHasOwnerChanged();
-    }
-
-    private function processedMessages(): void
-    {
-        $session = $this->getConnection()->createSession();
-        foreach (self::getSentMessages() as $sentMessage) {
-            $this->chunkProcessor->process($session->createMessage($sentMessage['message']), $session);
-        }
-    }
-
-    private function assertMessagesCreatedAndEntityIdsIsSplitting(int $batchSize): void
-    {
-        foreach (self::getSentMessages() as $sentMessage) {
-            $topic = $sentMessage['topic'];
-            $messageBody = $sentMessage['message'];
-
-            $this->assertCount($batchSize, $messageBody['entityData']['fields']['childrenOneToMany']['entity_ids']);
-            $this->assertEquals(AuditChangedEntitiesInverseCollectionsChunkTopic::getName(), $topic);
-        }
-    }
-
-    private function assertStoredAuditHasOwnerChanged(): void
-    {
-        foreach ($this->findStoredAudits() as $audit) {
-            $fields = $audit->getFields();
-            $this->assertCount(1, $fields);
-
-            $field = $audit->getField('ownerManyToOne');
-            $changedDiffs = $field->getCollectionDiffs()['changed'];
-            $this->assertCount(1, $changedDiffs);
-            $diff = reset($changedDiffs);
-
-            $this->assertEquals(TestAuditDataOwner::class, $diff['entity_class']);
-            $this->assertEquals(
-                ['stringProperty' => [null,'aNewValue']],
-                $diff['change_set']
-            );
-        }
+            ]
+        ];
     }
 
     private function createMessage(array $body, array $properties = []): MessageInterface
@@ -195,14 +66,90 @@ class AuditChangedEntitiesInverseCollectionsProcessorTest extends WebTestCase
         $message = new Message();
         $message->setBody($body);
         $message->setMessageId('some_message_id');
-
         $message->setProperties($properties);
 
         return $message;
     }
 
-    private function getConnection(): ConnectionInterface
+    private function assertStoredAuditHasOwnerChanged(): void
     {
-        return self::getContainer()->get('oro_message_queue.transport.connection');
+        foreach ($this->findStoredAudits() as $audit) {
+            $this->assertCount(1, $audit->getFields());
+
+            $field = $audit->getField('ownerManyToOne');
+            $changedDiffs = $field->getCollectionDiffs()['changed'];
+            $this->assertCount(1, $changedDiffs);
+            $diff = reset($changedDiffs);
+
+            $this->assertEquals(TestAuditDataOwner::class, $diff['entity_class']);
+            $this->assertEquals(['stringProperty' => [null, 'aNewValue']], $diff['change_set']);
+        }
+    }
+
+    public function testProcessWhenNoChanges(): void
+    {
+        $message = $this->createMessage([
+            'timestamp' => time(),
+            'transaction_id' => 'aTransactionId',
+            'entities_inserted' => [],
+            'entities_updated' => [],
+            'entities_deleted' => [],
+            'collections_updated' => []
+        ]);
+
+        $this->processor->process($message, $this->createMock(SessionInterface::class));
+        self::assertMessagesEmpty(AuditChangedEntitiesInverseCollectionsChunkTopic::getName());
+    }
+
+    public function testProcess(): void
+    {
+        $batchSize = 3;
+        $owner = $this->getTestAuditDataOwner('test_audit_owner_1');
+        $child1 = $this->getTestAuditDataChild($owner, 0);
+        $child2 = $this->getTestAuditDataChild($owner, 1);
+        $child3 = $this->getTestAuditDataChild($owner, 2);
+        $child4 = $this->getTestAuditDataChild($owner, 3);
+        $timestamp = time();
+        $message = $this->createMessage([
+            'timestamp' => $timestamp,
+            'transaction_id' => 'aTransactionId',
+            'entities_inserted' => [],
+            'entities_updated' => [
+                '000000001f4c2232000000006d016312' => [
+                    'entity_class' => TestAuditDataOwner::class,
+                    'entity_id' => $owner->getId(),
+                    'change_set' => ['stringProperty' => [null, 'aNewValue']]
+                ]
+            ],
+            'entities_deleted' => [],
+            'collections_updated' => []
+        ]);
+
+        $this->processor->setBatchSize($batchSize);
+        $this->processor->process($message, $this->createMock(SessionInterface::class));
+
+        self::assertMessagesSent(
+            AuditChangedEntitiesInverseCollectionsChunkTopic::getName(),
+            [
+                [
+                    'timestamp' => $timestamp,
+                    'transaction_id' => 'aTransactionId',
+                    'entityData' => [
+                        $this->getEntityData($owner, [$child1->getId(), $child2->getId(), $child3->getId()])
+                    ]
+                ],
+                [
+                    'timestamp' => $timestamp,
+                    'transaction_id' => 'aTransactionId',
+                    'entityData' => [
+                        $this->getEntityData($owner, [$child4->getId()])
+                    ]
+                ]
+            ]
+        );
+
+        self::consume();
+        $this->assertStoredAuditCount(4);
+        $this->assertStoredAuditHasOwnerChanged();
     }
 }
--- src/Oro/Bundle/DataAuditBundle/Tests/Functional/DataFixtures/LoadTestAuditDataWithOneToManyData.php
+++ src/Oro/Bundle/DataAuditBundle/Tests/Functional/DataFixtures/LoadTestAuditDataWithOneToManyData.php
@@ -12,21 +12,20 @@ use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 class LoadTestAuditDataWithOneToManyData extends AbstractFixture implements ContainerAwareInterface
 {
     use ContainerAwareTrait;
-    private const CHILD_COUNT = 4;
 
     #[\Override]
-    public function load(ObjectManager $manager)
+    public function load(ObjectManager $manager): void
     {
-        $owner = new TestAuditDataOwner();
-        for ($childIndex = 1; $childIndex <= self::CHILD_COUNT; $childIndex++) {
-            $child = new TestAuditDataChild();
-            $owner->addChildrenOneToMany($child);
-            $manager->persist($child);
+        for ($ownerIndex = 0; $ownerIndex < 3; $ownerIndex++) {
+            $owner = new TestAuditDataOwner();
+            for ($childIndex = 0; $childIndex < 4; $childIndex++) {
+                $child = new TestAuditDataChild();
+                $owner->addChildrenOneToMany($child);
+                $manager->persist($child);
+            }
+            $this->addReference('test_audit_owner_' . ($ownerIndex + 1), $owner);
+            $manager->persist($owner);
         }
-
-        $manager->persist($owner);
         $manager->flush();
-
-        $this->addReference('testAuditOwner', $owner);
     }
 }
