95b8b77f4efbd8fa3ddc40efde2480c9c3566f4d
Roman Kukla <kukla.roman@gmail.com>
2026-06-01 13:57:55 +0300

    BB-27426: Permissions-Policy header contains unsupported features (#45468)
    - removed the unsupported battery directive
    - added ->normalizeKeys(false) in Configuration.php to prevent Symfony from converting valid directive names with hyphens to invalid names with underscores
    - added new supported Permissions-Policy directives to the commented config block

--- src/Oro/Bundle/SecurityBundle/DependencyInjection/Configuration.php
+++ src/Oro/Bundle/SecurityBundle/DependencyInjection/Configuration.php
@@ -117,6 +117,7 @@ class Configuration implements ConfigurationInterface
                 ->children()
                     ->booleanNode('enable')->defaultFalse()->end()
                     ->arrayNode('directives')
+                        ->normalizeKeys(false)
                         ->useAttributeAsKey('name')
                         ->prototype('variable')
                             ->beforeNormalization()->castToArray()->end()
