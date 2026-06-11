e7d3860a12276f517d755d61b3d33963e7363b0e
Yefim Yevtushenko <yyevtushenko@oroinc.com>
2026-06-11 14:41:31 +0300

    BB-27440: Allow to configure behat application base url, selenium, and chromedriver endpoints via env vars (#45519)
    - added ORO_BEHAT_BASE_URL, ORO_SELENIUM_URL, ORO_CHROMEDRIVER_URL env vars to behat.yml.dist

--- src/Oro/Bundle/TestFrameworkBundle/Behat/ServiceContainer/OroTestFrameworkExtension.php
+++ src/Oro/Bundle/TestFrameworkBundle/Behat/ServiceContainer/OroTestFrameworkExtension.php
@@ -338,8 +338,7 @@ class OroTestFrameworkExtension implements TestworkExtension
             'oro_behat_services',
             new NullCumulativeFileLoader('Tests/Behat/services.yml')
         );
-        $resources = array_reverse($configLoader->load());
-        foreach ($resources as $resource) {
+        foreach ($configLoader->load() as $resource) {
             $loader = new YamlFileLoader($container, new FileLocator(rtrim($resource->path, 'services.yml')));
             $loader->load('services.yml');
         }
