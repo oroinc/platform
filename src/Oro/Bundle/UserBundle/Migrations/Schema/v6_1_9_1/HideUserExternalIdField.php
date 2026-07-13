fc3355c98a084ef8110b302fe391a5f5ce74d08e
Mykhailo Kotliar <mkotliar@oroinc.com>
2026-07-13 20:43:29 +0300

    BB-27517: "External ID" field should be not visible by default (6.1) (#45871)

---
+++ src/Oro/Bundle/UserBundle/Migrations/Schema/v6_1_9_1/HideUserExternalIdField.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Oro\Bundle\UserBundle\Migrations\Schema\v6_1_9_1;
+
+use Doctrine\DBAL\Schema\Schema;
+use Oro\Bundle\EntityBundle\EntityConfig\DatagridScope;
+use Oro\Bundle\EntityConfigBundle\Migration\UpdateEntityConfigFieldValueQuery;
+use Oro\Bundle\EntityExtendBundle\Migration\ExtendOptionsManagerAwareInterface;
+use Oro\Bundle\EntityExtendBundle\Migration\ExtendOptionsManagerAwareTrait;
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+use Oro\Bundle\UserBundle\Entity\User;
+
+/**
+ * Fully hides externalId field of User entity from grid, filters, view and edit form.
+ */
+class HideUserExternalIdField implements Migration, ExtendOptionsManagerAwareInterface
+{
+    use ExtendOptionsManagerAwareTrait;
+
+    #[\Override]
+    public function up(Schema $schema, QueryBag $queries): void
+    {
+        // Works in case when the affected field does not yet exist.
+        $this->extendOptionsManager->mergeColumnOptions(
+            'oro_user',
+            'external_id',
+            [
+                'datagrid' => ['is_visible' => DatagridScope::IS_VISIBLE_FALSE],
+                'form' => ['is_enabled' => false],
+                'view' => ['is_displayable' => false],
+            ]
+        );
+
+        // Works in case when the affected field already exists.
+        $queries->addPostQuery(new UpdateEntityConfigFieldValueQuery(
+            User::class,
+            'external_id',
+            'datagrid',
+            'is_visible',
+            DatagridScope::IS_VISIBLE_FALSE
+        ));
+        $queries->addPostQuery(new UpdateEntityConfigFieldValueQuery(
+            User::class,
+            'external_id',
+            'form',
+            'is_enabled',
+            false
+        ));
+        $queries->addPostQuery(new UpdateEntityConfigFieldValueQuery(
+            User::class,
+            'external_id',
+            'view',
+            'is_displayable',
+            false
+        ));
+    }
+}
